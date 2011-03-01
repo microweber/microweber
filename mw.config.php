b99575ddd9ac5d7134d3d4bdcaa773fd1e7c72a3
peter <peter@ooyes.net>
2011-03-01 22:01:10 +0200

    commit

---
+++ mw.config.php
@@ -0,0 +1,18 @@
+<?php
+
+$mw_config = array ();
+
+#$mw_config['system_folder_shared'] = '/usr/share/php/system';
+$mw_config ['system_folder'] = 'system';
+$mw_config ['application_folder'] = 'application';
+
+$mw_config ['db_hostname'] = 'localhost';
+
+$mw_config ['db_username'] = 'root';
+
+$mw_config ['db_password'] = '123456';
+
+
+$mw_config ['db_database'] = 'cms2';
+
+?>
