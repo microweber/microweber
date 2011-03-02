49d8fdd889f21e611e293ac41c2959c3fbf4d862
peter <peter@ooyes.net>
2011-03-02 13:41:24 +0200

    some progress on cart module

--- userfiles/modules/cart/items.php
+++ userfiles/modules/cart/items.php
@@ -1,15 +1,9 @@
-
-
-
-
-
-
 <?
- 
-$cart_items = get_cart_items();
+$cart_items = get_cart_items(); 
 //var_dump($sid,$cart_items);    
  ?>
 <? if(!empty($cart_items)): ?>
+
 <table cellpadding="0" cellspacing="0">
   <thead>
     <tr>
@@ -20,19 +14,15 @@ $cart_items = get_cart_items();
     </tr>
   </thead>
   <tbody>
-     <? foreach(($cart_items) as $item): ?>
+    <? foreach($cart_items as $item): ?>
     <tr id="cart_item_row_<? print $item['id'] ?>">
       <td class="name_of_product"><? print $item['item_name'] ?></td>
-      <td class="product_qty"> 
-        
-        
-           <select class="select_qty" name="qty" style="width: 38px;" id="qty_for_cart_item_id_<? print $item['id'] ?>" onchange="cart_modify_item_properties('<? print $item['id'] ?>', 'qty', $(this).val());">
-      <? if(intval($item['qty']) > 100) { $max = intval($item['qty']) + 50 ; } else { $max = 100; }  ?>
-      <? for ($x = 1; $x <= $max; $x++) : ?>
-      <option  <? if($item['qty'] == $x): ?> value="<? print $x; ?>" selected="selected" <? endif; ?>  ><? print $x; ?></option>
-      <? endfor; ?>
-    </select>
-        </td>
+      <td class="product_qty"><select class="select_qty" name="qty" style="width: 38px;" id="qty_for_cart_item_id_<? print $item['id'] ?>" onchange="cart_modify_item_properties('<? print $item['id'] ?>', 'qty', $(this).val());">
+          <? if(intval($item['qty']) > 100) { $max = intval($item['qty']) + 50 ; } else { $max = 100; }  ?>
+          <? for ($x = 1; $x <= $max; $x++) : ?>
+          <option  <? if($item['qty'] == $x): ?> value="<? print $x; ?>" selected="selected" <? endif; ?>  ><? print $x; ?></option>
+          <? endfor; ?>
+        </select></td>
       <td class="product_price">$<? print $item['price'] ?></td>
       <td class="product_remove"><a href="#" onclick="mw.cart.remove_item('<? print $item['id'] ?>');" class="remove_from_cart">&nbsp;</a></td>
     </tr>
@@ -40,9 +30,6 @@ $cart_items = get_cart_items();
   </tbody>
 </table>
 <div id="total"> <span><strong>Total price:</strong></span> <b>$<? print ($this->cart_model->itemsGetTotal(false)); ?></b> </div>
-
-
- 
 <? else: ?>
 <span class="cartico">The cart is empty. Please add some products.</span>
 <? endif; ?>
