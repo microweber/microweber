a2db75a88477ef8903bde739fbfb745449d2ab67
ooyes <info@ooyes.net>
2012-02-02 03:30:58 +0200

    update from jan

---
+++ mw.config.php
@@ -0,0 +1,24 @@
+<?php
+
+$mw_config = array ();
+ 
+ 
+
+$mw_config ['site_url'] = 'http://your url';
+
+ $mw_config ['system_folder'] = 'system';
+$mw_config ['application_folder'] = 'application';
+ 
+$mw_config ['db_hostname'] = 'localhost';
+
+$mw_config ['db_username'] = 'demo';
+
+$mw_config ['db_password'] = 'pass';
+
+ 
+$mw_config ['db_database'] = 'demo_feb_2012';
+
+
+ 
+
+?>
