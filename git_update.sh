0aed56521747433614388c7cdc75d802d06d26df
Peter Ivanov <peter@ooyes.net>
2012-09-17 20:01:26 +0300

    update

--- git_update.sh
+++ git_update.sh
@@ -1,2 +1,3 @@
- #!/bin/sh
-cd ~/domains/update.microweber.us/public_html/ && git pull -f origin master
+#!/bin/sh
+#cd /home/microweber/domains/update.microweber.me/public_html &&  git clean -f && git pull -f origin master
+cd /home/microweber/domains/update.microweber.me/public_html &&  git fetch origin && git reset --hard origin/master && git pull origin master --force
