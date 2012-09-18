93f71611c9a2ff88052e1107b84257bef0d2d544
Peter Ivanov <info@ooyes.net>
2012-09-18 17:05:34 +0300

    update

--- application/functions/updates.php
+++ application/functions/updates.php
@@ -113,7 +113,7 @@ function mw_check_for_update() {
     $data['elements'] = $t;
 
 
-    $serv = 'http://update.microweber.me/update.php';
+    $serv = 'http://update.microweber.com/update.php';
     $p = url_download($serv, $data);
     // print $p;
     return (json_decode($p, true));
--- application/includes/js/sortable.js
+++ application/includes/js/sortable.js
@@ -112,7 +112,7 @@ mw.drag = {
            mw.mm_target = event.target;
            mw.$mm_target = $(mw.mm_target);
 
-           mw.mm_target_current = {}
+
 
 
            if(!mw.isDrag){
@@ -154,14 +154,16 @@ mw.drag = {
 
                    //trigger on item
                    if((mw.isDragItem(mw.mm_target) && mw.$mm_target.parent().hasClass("element")) || mw.mm_target.className.contains('mw_item')){
-                     if(mw.mm_target_current!=mw.mm_target){
+                     if(!mw.mm_target.entered){
+                       mw.mm_target.entered = true;
                      $(window).trigger("onItemOver", mw.mm_target);
-                     mw.$mm_target.addClass("mw_item");
+                        mw.$mm_target.addClass("mw_item");
                      }
                    }
                    else if(mw.$mm_target.parents(".mw_item").length>0){
-                     if(mw.mm_target_current!=mw.$mm_target.parents(".mw_item")[0]){
-                     $(window).trigger("onItemOver", mw.$mm_target.parents(".mw_item")[0]);
+                     if(!mw.$mm_target.parents(".mw_item")[0].entered){
+                       mw.$mm_target.parents(".mw_item")[0].entered = true;
+                        $(window).trigger("onItemOver", mw.$mm_target.parents(".mw_item")[0]);
                      }
                    }
                    else if(mw.mm_target.id!='items_handle' && mw.$mm_target.parents("#items_handle").length==0){
@@ -356,7 +358,6 @@ mw.drag = {
         $(window).bind("onItemOver", function(a, element){
           var el = $(element);
           var o = el.offset();
-          mw.log(mw.random());
           var pleft = parseFloat(el.css("paddingLeft"));
           $(mw.handle_item).css({
             top:o.top,
