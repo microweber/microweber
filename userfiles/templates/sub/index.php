5ad7115ec12e8ed563304369420afb2199c324e3
Peter Ivanov <info@ooyes.net>
2012-10-14 18:32:29 +0300

    update

---
+++ userfiles/templates/sub/index.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<!-- saved from url=(0078)file:///C:/My%20Web%20Sites/http___blog.svbtle.com_/blog.svbtle.com/index.html -->
+<html lang="en" class="wf-proximanova-n6-inactive wf-proximanova-n7-inactive wf-freightsanspro-n7-inactive wf-proximanova-n4-inactive wf-inactive">
+<!-- Mirrored from blog.svbtle.com/ by HTTrack Website Copier/3.x [XR&CO'2010], Sun, 14 Oct 2012 14:53:03 GMT --><!-- Added by HTTrack -->
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<!-- /Added by HTTrack -->
+
+<meta charset="utf-8">
+<script data-cfasync="false" src="{TEMPLATE_URL}/index_files/jquery.min.js"></script>
+ <title>The Official Svbtle Blog by Svbtle Network</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="generator" content="Svbtle.com">
+<meta name="description" content="The future of news and opinion on the web • Svbtle Network’s blog">
+<link href="{TEMPLATE_URL}/index_files/build.blog-377bdd960caf2c9e59e54c8d2cecd3bc.css" media="screen" rel="stylesheet" type="text/css">
+<style>
+blockquote {
+	border-color: #000000;
+}
+aside#logo, aside#logo div a, ul#user_meta a:hover span.link_logo_inside, ul#user_meta a:hover span.link_logo, aside.kudo.complete span.circle {
+	background-color: #000000;
+}
+aside#logo div a, aside.kudo.complete span.circle {
+ }
+section.preview header#begin h2, ul#user_meta a:hover, nav.pagination span.next a, nav.pagination span.prev a {
+	color: #000000;
+}
+ul#user_meta a:hover, nav.pagination span.next a, nav.pagination span.prev a {
+	border-color: #000000;
+}
+</style>
+<script data-cfasync="false" src="{TEMPLATE_URL}/index_files/fww1mvh.js"></script>
+<style type="text/css">
+.tk-freight-sans-pro {
+	font-family:"freight-sans-pro", sans-serif;
+}
+.tk-proxima-nova {
+	font-family:"proxima-nova", sans-serif;
+}
+</style>
+<link rel="stylesheet" href="http://use.typekit.com/k/fww1mvh-d.css?3bb2a6e53c9684ffdc9a98f01b5b2a62e205eae2cde7d9d1445a914d5137800c614ec9db6b0e1513a7e343706b39043c4f98af351c9f6f383d730f3affcc76a58d417bf4a68e6d9d646ae2f0652adca5f58806700a62102080c4cb360bfce43229a29031c9f4b7bf18eda77b197f6e7c8b79d1a8b9c3be0505296b6b4d91d643d0913bb1b44fdd76e1c15bf19f53997b54d081942ae9e9b9a092d233b8d9012fa217b7983b00d0fa623be32141cf17b0e61d9fa7595d64f1c0e17158a9b90cb3aaddf19b6f0d1a2a7fa9111d931fec7c46be3b002d8dae8dcaf5bcb7b4e96149984741bfc0b9a688c9b9c06bae0cdcf6998db58a404dd632a83613888d10b2f251962126fe">
+<script data-cfasync="false">
+  try{Typekit.load();}catch(e){} // GA << ANALYTICS
+  var _gaq = _gaq || []; _gaq.push( ['_setAccount', 'UA-28994039-1'],['_setDomainName', 'none'],['_setAllowLinker', true],['_setUsername', 'blog'],['_trackPageview']);(function() { var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js'; var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);})();
+  </script>
+</head>
+<body class="blog">
+<header id="sidebar">
+  <aside id="logo" class="clearfix">
+    <div class="clearfix"> <a href="{TEMPLATE_URL}/index_files/index.php">The Official Svbtle Blog</a> </div>
+  </aside>
+  <ul id="user_meta">
+    <li class="blog_name">
+      <h1 id="blog_name"> <a href="{TEMPLATE_URL}/index_files/index.php">The Official Svbtle Blog</a> </h1>
+    </li>
+    <li class="blog_owner">
+      <h2 id="blog_owner"> <a href="{TEMPLATE_URL}/index_files/index.php">Svbtle Network</a> </h2>
+    </li>
+    <li class="tagline">
+      <h2 id="tagline"> The future of news and opinion on the web </h2>
+    </li>
+    <li class="link website"> <a href="http://svbtle.com/"> svbtle.com </a> </li>
+    <li class="link twitter"> <a href="http://twitter.com/svbtle"> @svbtle </a> </li>
+    <li class="link email"> <a href="mailto:hello@svbtle.com?subject=hi"> say hello </a> </li>
+    <li class="link feed"> <a href="file:///C:/My%20Web%20Sites/http___blog.svbtle.com_/blog.svbtle.com/feedhtml.html"> feed </a> </li>
+  </ul>
+  <aside id="svbtle_linkback"> <a href="https://svbtle.com/"> <span class="logo_square"><span class="logo_circle">&nbsp;</span></span>&nbsp;<span class="svbtle">Svbtle</span> </a> </aside>
+</header>
+<section id="river">
+  <header id="begin">
+    <time datetime="2012-09-27" id="top_time">September 27, 2012</time>
+  </header>
+  <? $cont =  get_posts(); 
+  
+ // d($cont);
+  ?>
+  <? foreach($cont as $item): ?>
+  <article id="eidEPbDNHCf7HcO1IgzovgucHUQ5XFDZsgdXMAaX<? print $item['id'] ?>" class="post">
+    <h2> <a href="http://blog.alexmaccaw.com/svbtle-image-uploading" class="title"><? print $item['title'] ?></a></h2>
+    <div class="edit" id="sdasd<? print $item['id'] ?>" field="content"><div class="element">My post</div></div>
+  </article>
+  <? endforeach; ?>
+</section>
+ 
+</body>
+</html>
