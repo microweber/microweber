ddc4cb7986a38c789f2c0a0d2bdecc1008d4bc3e
Peter Ivanov <peter@ooyes.net>
2012-11-05 19:02:41 +0200

    update

--- userfiles/modules/custom_fields/custom_fields/checkbox.php
+++ userfiles/modules/custom_fields/custom_fields/checkbox.php
@@ -20,10 +20,12 @@ include('empty_field_vals.php');
    <? if(!empty($data['custom_field_values'])) : ?>
    
    <div class="mw-custom-field-group">
-  <label class="mw-custom-field-label" for="custom_field_help_text<? print $rand ?>"><? print $data["custom_field_name"]; ?></label>
+  <span class="mw-custom-field-label" for="custom_field_help_text<? print $rand ?>"><? print $data["custom_field_name"]; ?></span>
   <? foreach($data['custom_field_values'] as $v): ?>
       <div class="mw-custom-field-form-controls">
+      <label>
         <input type="checkbox" name="<? print $data["custom_field_name"]; ?>[]"  data-custom-field-id="<? print $data["id"]; ?>" value="<? print $v; ?>"><? print ($v); ?>
+         </label>
       </div>
   <? endforeach; ?>
 </div>
