f1943d4f937cebbbb04d25e5207a2771ccbcf2e6
Peter Ivanov <peter@ooyes.net>
2013-04-16 19:29:42 +0300

    update

--- userfiles/modules/admin/backup/index.php
+++ userfiles/modules/admin/backup/index.php
@@ -1,6 +1,7 @@
 <? if(!is_admin()){error("must be admin");}; ?>
 <script  type="text/javascript">
     mw.require("<? print $config['url_to_module']; ?>backup.js");
+	mw.require("files.js");
 </script>
 
 <div id="mw_backups_settings">
@@ -18,7 +19,29 @@
         <a href="javascript:mw.admin_backup.create('.mw_edit_page_right')" class="mw-ui-btn"><span class="ico iplus"></span><span>New Database Backup</span></a>
         
         
-        <a href="javascript:mw.admin_backup.create_full('.mw_edit_page_right')" class="mw-ui-btn"><span class="ico iplus"></span><span>New FULL Backup</span></a> 
+        <a href="javascript:mw.admin_backup.create_full('.mw_edit_page_right')" class="mw-ui-btn"><span class="ico iplus"></span><span>Upload backup</span></a> 
+        
+        
+        <div id="mw_uploader" class="mw-ui-btn">Browse files</div>
+        
+        <script type="text/javascript">
+		var uploader = mw.files.uploader({
+			filetypes:"zip,sql",
+			multiple:false
+		});
+		
+		
+		$(document).ready(function(){
+		mw.$("#mw_uploader").append(uploader);
+				$(uploader).bind("FileUploaded", function(obj, data){
+					alert(data.src);	
+				});			
+		});
+
+		
+		</script>
+        
+          <a href="javascript:mw.admin_backup.create_full('.mw_edit_page_right')" class="mw-ui-btn"><span class="ico iplus"></span><span>New FULL Backup</span></a> 
          </div>
     </div>
   </div>
