323390532a7444e4c39e52698bfc44d8573c05b0
Peter Ivanov <peter@ooyes.net>
2013-04-19 19:39:19 +0300

    update

--- userfiles/modules/admin/backup/log.php
+++ userfiles/modules/admin/backup/log.php
@@ -3,30 +3,30 @@
 
 	$check = get_log("order_by=created_on desc&one=true&no_cache=true&is_system=y&created_on=[mt]30 min ago&field=upload_size&rel=uploader&user_ip=" . USER_IP);
 	if(isset($check['value'])){
-		  print "Uploaded: ".file_size_nice($check['value']) ; 
+		  mw_notif("Uploaded: ".file_size_nice($check['value']));
 	}
-	
-	
-	
-	
+
+
+
+
 	$check = get_log("order_by=created_on desc&one=true&no_cache=true&is_system=y&created_on=[mt]30 min ago&field=action&rel=backup&user_ip=" . USER_IP);
 	if(isset($check['value'])){
 		if($check['value'] == 'reload'){
-			?> 
+			?>
 <script type="text/javascript">
- 
+
 		$(document).ready(function(){
-			
+
 				mw.reload_module('admin/backup/manage');
-			
-		});	
-		
+
+		});
+
 </script>
 			<?
 		} else {
-			print html_entity_decode($check['value']) ; 
+			  mw_notif(html_entity_decode($check['value'])) ;
 
-			
+
 		}
 	}
 
--- userfiles/modules/shop/payments/gateways/Paypal.php
+++ userfiles/modules/shop/payments/gateways/Paypal.php
@@ -13,12 +13,16 @@ function paypal_checkout_callback(data,selector){
 $paypal_is_test = (get_option('paypalexpress_testmode', 'payments')) == 'y';
  
 ?>
-<p><b>Thank you for your order.</b>  </p>
 
-<p>You will be redirected to PayPal's website. </p>
+<div class="edit" rel="global" field="paypal_checkout_html">
+    <p><b>Thank you for your order.</b></p>
+    <p>Please complete your order. </p>
+    <p class="alert alert-warning"><small style="font-size: x-small"><strong> *Note </strong>Your shopping cart will be emptied when you compleate the order</small> </p>
+</div>
 
 
-Your shopping cart will be emptied
+
+
 <? if($paypal_is_test == true and is_admin()): ?>
 <small><? print mw_warn("You are using Paypal Express in test mode!"); ?></small>
 <? endif; ?>
