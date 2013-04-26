c25652136f841c7810a9c5814294e82b25f5f000
Peter Ivanov <peter@ooyes.net>
2013-04-26 17:49:20 +0300

    update

--- application/includes/js/toolbar.js
+++ application/includes/js/toolbar.js
@@ -680,23 +680,14 @@ $(window).load(function(){
 if(typeof mw.hasDraft === 'object'){
    var html = ""
    + "<div class='hasdraft'>"
-      //+ "<p>Load last Draft?</p>"
-      + "<span class='mw-ui-btn mw-ui-btn-green'>Load last Draft ... <span id='load_draft_timer'>12</span></span>"
+      + "<p>Load last Draft?</p>"
+      + "<span class='mw-ui-btn mw-ui-btn-small mw-ui-btn-green'>Yes</span>"
+      + "<span class='mw-close'></span>"
    +"</div>";
 
    mw.$("#mw_tabs").after(html);
 
-   var Timer = $("#load_draft_timer");
-   _Timer = setInterval(function(){
-     var curr = parseFloat(Timer.html());
-     if(curr > 0){
-          Timer.html(curr - 1);
-     }
-     else{
-        clearInterval(_Timer);
-        Timer.parent().parent().remove();
-     }
-   }, 1000);
+
 
  }
 
