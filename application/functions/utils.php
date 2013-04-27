ce47c7ec839d3a3ffabf1aff647a69e600cb4fec
Peter Ivanov <peter@ooyes.net>
2013-04-27 11:04:02 +0300

    session cookie hijack fix

--- application/functions/utils.php
+++ application/functions/utils.php
@@ -778,11 +778,23 @@ function session_del($name) {
 }
 
 function session_end() {
-	setcookie (session_id(), "", time() - 3600);
- 
-
-
+	
+	
+	
 	$_SESSION = array();
+
+	// If it's desired to kill the session, also delete the session cookie.
+	// Note: This will destroy the session, and not just the session data!
+	if (ini_get("session.use_cookies")) {
+	    $params = session_get_cookie_params();
+	    setcookie(session_name(), '', time() - 42000,
+	        $params["path"], $params["domain"],
+	        $params["secure"], $params["httponly"]
+	    );
+	}
+
+
+ 
 	session_destroy();
 	session_write_close();
 	unset($_SESSION);
