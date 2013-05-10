4f09be45ad0aa0253b436e0e914a1257a19c5587
Peter Ivanov <peter@ooyes.net>
2013-05-10 17:41:13 +0300

    up

---
+++ userfiles/modules/categories/templates/liveedit_toolbar.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+
+type: layout
+
+name: Liveedit Toolbar
+
+description: List Navigation for Liveedit Toolbar
+
+*/
+
+?>
+
+<?php
+    $params['ul_class'] = '';
+	$params['ul_class_deep'] = '';
+?>
+
+<div class="category-nav category-nav-default">
+
+		<?php  category_tree($params);  ?>
+
+</div>
+
