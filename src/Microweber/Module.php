e5dd615e39ded02c02f7189b82a58d98c236c266
Peter Ivanov <peter@ooyes.net>
2013-08-18 15:11:20 +0300

    fix of #82 - cli install

--- src/Microweber/Module.php
+++ src/Microweber/Module.php
@@ -3,7 +3,7 @@ namespace Microweber;
 
 
 $_mw_modules_info_register = array();
- event_bind('mw_db_init', mw('Microweber\Module')->db_init());
+ //event_bind('mw_db_init', mw('Microweber\Module')->db_init());
 
 
 $mw_mod_counter = 0;
@@ -45,6 +45,7 @@ class Module
             }
 
         }
+		$this->db_init(); 
     }
 
     public function get($params = false)
