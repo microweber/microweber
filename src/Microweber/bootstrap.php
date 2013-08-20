280e5a5e47e0839b6b45b7c7a9e6657c23273ac6
Peter Ivanov <peter@ooyes.net>
2013-08-20 13:56:36 +0300

    email fix

--- src/Microweber/bootstrap.php
+++ src/Microweber/bootstrap.php
@@ -9,7 +9,7 @@ if (!defined('MW_ROOTPATH')) {
 
 
 if (!defined('MW_VERSION')) {
-    define('MW_VERSION', 0.801);
+    define('MW_VERSION', 0.802);
 }
 
 if (version_compare(phpversion(), "5.3.0", "<=")) {
