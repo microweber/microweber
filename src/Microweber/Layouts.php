436d835b836896e841508a825f088cd61d01e3f4
Peter Ivanov <peter@ooyes.net>
2013-09-03 17:03:51 +0300

    bugfix in the add page layout selector

--- src/Microweber/Layouts.php
+++ src/Microweber/Layouts.php
@@ -70,12 +70,16 @@ class Layouts
                 if (is_dir($check_dir)) {
                     $the_active_site_template = $tmpl;
                 } else {
-                    $the_active_site_template = $this->app->option->get('curent_template');
+                    $the_active_site_template = $this->app->option->get('curent_template','template');
                 }
             } else {
-                $the_active_site_template = $this->app->option->get('curent_template');
+                $the_active_site_template = $this->app->option->get('curent_template','template');
+            }
+            if($the_active_site_template == ''){
+                $the_active_site_template = 'default';
             }
             $path = normalize_path(MW_TEMPLATES_DIR . $the_active_site_template);
+
         } else {
             $path = $options['path'];
         }
