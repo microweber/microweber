91c6d64bf30e8f14dbce9cf7daf5118a32e47598
Peter Ivanov <peter@microweber.com>
2013-10-04 17:54:26 +0300

    shipping cost template

---
+++ userfiles/modules/shop/shipping/gateways/country/templates/cost.php
@@ -0,0 +1,14 @@
+<?php
+
+/*
+
+type: layout
+
+name: Shipping cost
+
+description: Shipping cost value
+
+*/
+?>
+
+  <span class="shipping_cost"><?php print mw('shop')->currency_format($shipping_cost); ?></span>  
