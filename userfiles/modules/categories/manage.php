bd2b00d859f329db05952ac793d624b7e85c72ab
Peter Ivanov <peter@microweber.com>
2013-10-14 18:18:11 +0300

    update on categories module #116

--- userfiles/modules/categories/manage.php
+++ userfiles/modules/categories/manage.php
@@ -7,7 +7,7 @@ $field_name="categories";
 $selected = 0;
 $tree = array();
 $tree['rel'] = 'content';
-$tree['link'] = "<a href='javascript:mw.load_quick_cat_edit({id})'>{title}</a>";
+$tree['link'] = "<a   href='javascript:mw.load_quick_cat_edit({id})'>{title}</a>";
 
 mw('category')->tree($tree);
 ?>
@@ -17,6 +17,48 @@ mw('category')->tree($tree);
 	     mw.load_module('categories/edit_category', '#mw_quick_edit_category', function(){
        })
  	}
+	
+	
+mw.manage_cat_sort = function(){
+   
+	  
+   mw.$("#<?php print $params['id'] ?>").sortable({
+     items: '.category_element',
+     axis:'y',
+     handle:'a',
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	//  connectWith: "#<?php print $params['id'] ?> .category_tree ul",
+     update:function(){
+       var obj = {ids:[]}
+       $(this).find('.category_element').each(function(){
+        var id = this.attributes['value'].nodeValue;
+        obj.ids.push(id);
+      });
+
+       $.post("<?php print site_url('api/category/reorder'); ?>", obj, function(){});
+     },
+     start:function(a,ui){
+      $(this).height($(this).outerHeight());
+      $(ui.placeholder).height($(ui.item).outerHeight())
+      $(ui.placeholder).width($(ui.item).outerWidth())
+    },
+
+       //placeholder: "custom-field-main-table-placeholder",
+       scroll:false
+
+
+     });
+
+ 
+}	
+mw.manage_cat_sort();	
 </script>
 
 <a href='javascript:mw.load_quick_cat_edit(0)'>+Add new category</a>
