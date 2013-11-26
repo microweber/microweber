0b05c319199db07c25d9ab7ee7fe04e579a6ff31
Peter Ivanov <peter@microweber.com>
2013-11-26 18:53:40 +0200

    posts module skin fix

--- userfiles/templates/default/layouts/page_right_sidebar.php
+++ userfiles/templates/default/layouts/page_right_sidebar.php
@@ -18,7 +18,7 @@ description: Page with right sidebar
 <div id="content">
 	<div class="container">
 		<div class="row row-fluid">
-			<div class="span7 col-sm-7 col-md-7">
+			<div class="span9 col-sm-9 col-md-9">
 				<h2 class="edit"  field="title" rel="content">New page</h2>
 				<div class="edit"  field="content" rel="content">
 					<p>You can edit this text</p>
