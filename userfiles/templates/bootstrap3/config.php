d666a8d2bb02131d7d1f9b33e60143d122cc7876
Peter Ivanov <peter@microweber.com>
2014-01-15 20:20:19 +0200

    removed old bs3 theme

--- userfiles/templates/bootstrap3/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$config = array();
-$config['name'] = "Bootstrap 3";
-$config['author'] = "Microweber";
-$config['version'] = 0.1;
-$config['url'] = "http://microweber.com/";
--- userfiles/templates/bootstrap3/sidebar.php
+++
@@ -1,6 +0,0 @@
-<div class="blog-sidebar edit" field="sidebar" rel="inherit">
-	<h2>Categories</h2>
-	<module type="categories" />
-	<h2>Other pages</h2>
-	<module type="pages" />
-</div>
