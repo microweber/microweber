84d2bed1c6fddb0c650cf76bf5db53e5b5560a39
Peter Ivanov <peter@microweber.com>
2014-02-04 10:34:52 +0200

    fixed bug in db delete

--- src/Microweber/tests/functions/ShopTest.php
+++ src/Microweber/tests/functions/ShopTest.php
@@ -10,6 +10,60 @@ class ShopTest extends \PHPUnit_Framework_TestCase
         // session_provider for unit tests,
         // does not persist values between requests
         mw('user')->session_provider = 'array';
+
+        // empty existing cart for the test
+        empty_cart();
+    }
+
+    public function testAddProducts()
+    {
+        $params = array(
+            'title' => 'this-is-my-test-shop',
+            'content_type' => 'page',
+            'is_shop' => 'y',
+            // 'debug' => 1,
+            'is_active' => 'y');
+
+        //creating our shop page
+        $my_shop = save_content($params);
+
+
+        $params = array(
+            'title' => 'this-is-my-test-product',
+            'content_type' => 'post',
+            'subtype' => 'product',
+            'parent' => $my_shop,
+            // 'debug' => 1,
+            'is_active' => 'y');
+        //adding a product to our shop page
+        $my_product = save_content($params);
+        $product = get_content_by_id($my_product);
+
+        $price = rand();
+
+        $add_price_field = array(
+            'field_name' => 'My price',
+            'field_value' => $price,
+            'field_type' => 'price',
+            // 'debug' => 1,
+            'content_id' => $my_product);
+        //adding a custom field "price" to product
+        $field = save_custom_field($add_price_field);
+
+        $custom_fields = get_custom_fields('content', $my_product);
+
+
+        //PHPUnit
+        $this->assertEquals(true, is_array($product));
+        $this->assertEquals(true, is_array($custom_fields));
+        $this->assertEquals(true, isset($custom_fields['My price']));
+        $this->assertEquals(true, intval($custom_fields['My price']) == $price);
+        $this->assertEquals(true, intval($my_product) > 0);
+        $this->assertEquals(true, intval($field) > 0);
+        $this->assertEquals(true, intval($my_shop) > 0);
+
+        $this->delete_content[] = $my_shop;
+        $this->delete_content[] = $my_product;
     }
 
     public function testAddToCart()
@@ -22,11 +76,8 @@ class ShopTest extends \PHPUnit_Framework_TestCase
             // 'debug' => 1,
             'is_active' => 'y');
 
-        //saving
+        //creating our shop page
         $my_shop = save_content($params);
-
-
-
         $params = array(
             'title' => 'this-is-my-test-product',
             'content_type' => 'post',
@@ -34,10 +85,8 @@ class ShopTest extends \PHPUnit_Framework_TestCase
             'parent' => $my_shop,
             // 'debug' => 1,
             'is_active' => 'y');
-
-        //saving
+        //adding a product to our shop page
         $my_product = save_content($params);
-
 
         $product = get_content_by_id($my_product);
         $add_price_field = array(
@@ -46,30 +95,21 @@ class ShopTest extends \PHPUnit_Framework_TestCase
             'field_type' => 'price',
             // 'debug' => 1,
             'content_id' => $my_product);
+        //adding a custom field "price" to product
         $field = save_custom_field($add_price_field);
 
-        $add_price_field_2 = array(
-            'field_name' => 'My other price',
-            'field_value' => '20',
-            'field_type' => 'price',
-            // 'debug' => 1,
-            'content_id' => $my_product);
-        $field_2 = save_custom_field($add_price_field_2);
-
-        $check_field = get_custom_fields('content', $my_product);
+        $custom_fields = get_custom_fields('content', $my_product);
 
 
         $add_to_cart = array(
-            'My price' => '20',
-            // 'debug' => 1,
-            'content_id' => $my_product);
-
+            'content_id' => $my_product
+        );
         $cart_add = update_cart($add_to_cart);
 
         $cart = get_cart();
+
         empty_cart();
         $cart_emptied = get_cart();
-
 
 
         $delete_shop = delete_content($my_shop);
@@ -78,12 +118,11 @@ class ShopTest extends \PHPUnit_Framework_TestCase
         $check_deleted_product = get_content_by_id($my_product);
 
 
-
         //PHPUnit
         $this->assertEquals(true, is_array($product));
-        $this->assertEquals(true, is_array($check_field));
-        $this->assertEquals(true, isset($check_field['My price']));
-        $this->assertEquals(true, intval($check_field['My price']) == 10);
+        $this->assertEquals(true, is_array($custom_fields));
+        $this->assertEquals(true, isset($custom_fields['My price']));
+        $this->assertEquals(true, intval($custom_fields['My price']) == 10);
         $this->assertEquals(true, intval($my_product) > 0);
         $this->assertEquals(true, intval($field) > 0);
         $this->assertEquals(true, intval($my_shop) > 0);
@@ -98,6 +137,26 @@ class ShopTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals(true, !is_array($check_deleted_shop));
         $this->assertEquals(true, !is_array($check_deleted_product));
 
+    }
+
+    protected function setUp()
+    {
+
+    }
+
+    protected function tearDown()
+    {
+        if (isset($this->delete_content) and is_array($this->delete_content)) {
+            foreach ($this->delete_content as $item) {
+
+                $delete_content = delete_content($item);
+                $check_deleted = get_content_by_id($item);
+                $this->assertEquals(true, is_array($delete_content));
+                $this->assertEquals(true, !is_array($check_deleted));
+
+
+            }
+        }
     }
 
 
