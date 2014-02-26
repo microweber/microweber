8575899765847ca349629e9cb9ad90aab4996ee0
Peter Ivanov <peter@microweber.com>
2014-02-26 15:38:25 +0200

    some skins moved to template folder

---
+++ userfiles/templates/default/modules/posts/templates/portfolio.php
@@ -0,0 +1,39 @@
+<?php
+
+/*
+
+type: layout
+
+name: Portfolio
+
+description: Portfolio
+
+*/
+?>
+
+<div class="row-fluid clearfix">
+  <ul class="portfolio-post-grid holder">
+    <?php if (!empty($data)): ?>
+    <?php foreach ($data as $item): ?>
+    <li class="span6 col-sm-6 col-md-6 portfolio-item" data-id="id-<?php print $item['id'] ?>" data-type="business">
+      <?php if(!isset($show_fields) or $show_fields == false or in_array('thumbnail', $show_fields)): ?>
+      <span class="4col"><img src="<?php print $item['image'] ?>" height="340" alt=""></span>
+      <?php endif; ?>
+      <span class="portfolio-item-meta">
+      <?php if(!isset($show_fields) or $show_fields == false or in_array('title', $show_fields)): ?>
+      <a href="<?php print $item['link'] ?>"><?php print $item['title'] ?></a>
+      <?php endif; ?>
+      <?php if(!isset($show_fields) or $show_fields == false or in_array('description', $show_fields)): ?>
+      <?php print $item['description'] ?>
+      <?php endif; ?>
+      </span> </li>
+    <?php endforeach; ?>
+    <?php endif; ?>
+  </ul>
+  
+  
+  <?php if (isset($pages_count) and $pages_count > 1 and isset($paging_param)): ?>
+    <?php print paging("num={$pages_count}&paging_param={$paging_param}&curent_page={$curent_page}") ?>
+    
+ <?php endif; ?>
+</div>
---
+++ userfiles/templates/default/modules/posts/templates/sidebar.php
@@ -0,0 +1,65 @@
+<?php
+
+/*
+
+type: layout
+
+name: sidebar
+
+description: sidebar
+
+*/
+?>
+<?php
+
+
+
+$tn = $tn_size;
+if(!isset($tn[0]) or ($tn[0]) == 150){
+     $tn[0] = 70;
+}
+if(!isset($tn[1])){
+     $tn[1] = $tn[0];
+}
+?>
+<div class="module-posts-template-sidebar">
+  <?php if (!empty($data)): ?>
+  <ul>
+  <?php foreach ($data as $item): ?>
+
+
+
+            <li class="media">
+              <a href="<?php print $item['link'] ?>" class="pull-left">
+              <?php if(!isset($show_fields) or $show_fields == false or in_array('thumbnail', $show_fields)): ?>
+                <img src="<?php print thumbnail($item['image'], $tn[0], $tn[1]); ?>" alt="" class="img-rounded img-polaroid"  />
+              <?php endif; ?>
+              </a>
+              <div class="media-body extra-wrap">
+               <?php if(!isset($show_fields) or $show_fields == false or in_array('title', $show_fields)): ?>
+                    <h5><a class="link media-heading" href="<?php print $item['link'] ?>"><?php print $item['title'] ?></a></h5>
+               <?php endif; ?>
+                <?php if(!isset($show_fields) or $show_fields == false or in_array('created_on', $show_fields)): ?>
+                    <small class="date"><?php print $item['created_on'] ?></small>
+                <?php endif; ?>
+
+               <?php if(!isset($show_fields) or $show_fields == false or in_array('description', $show_fields)): ?>
+                    <p><?php print $item['description'] ?></p>
+               <?php endif; ?>
+
+                <?php if(!isset($show_fields) or $show_fields == false or in_array('read_more', $show_fields)): ?>
+                    <a href="<?php print $item['link'] ?>" class="btn btn-default btn-mini"><?php $read_more_text ? print $read_more_text : print _e('Continue Reading', true); ?></a>
+                <?php  endif; ?>
+               </div>
+            </li>
+  <?php endforeach; ?>
+   </ul> 
+  <?php endif; ?>
+</div>
+<?php if (isset($pages_count) and $pages_count > 1 and isset($paging_param)): ?>
+    <?php print paging("num={$pages_count}&paging_param={$paging_param}&curent_page={$curent_page}") ?>
+ <?php endif; ?>
+
+
+
+
