01a89f937af74c63343d8061ae6924f806bf7ff2
Peter Ivanov <peter@microweber.com>
2014-03-21 13:46:28 +0200

    version increase

--- src/Microweber/bootstrap.php
+++ src/Microweber/bootstrap.php
@@ -23,7 +23,7 @@ if (!defined('MW_ROOTPATH')) {
 
 
 if (!defined('MW_VERSION')) {
-    define('MW_VERSION', 0.931);
+    define('MW_VERSION', 0.932);
 }
 
 if (version_compare(phpversion(), "5.3.0", "<=")) {
