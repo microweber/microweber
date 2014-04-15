68a629fca7e458e1e391c2c69566a8e3f7ac2c6a
Peter Ivanov <peter@microweber.com>
2014-04-15 19:13:11 +0300

    fixes

--- userfiles/modules/admin/backup/index.php
+++ userfiles/modules/admin/backup/index.php
@@ -4,48 +4,45 @@
     mw.require("files.js");
 </script>
 <style>
-    #mw_upsdfsdloader.disabled iframe {
-        top: -9999px;
-    }
-
-    .back-up-nav-btns .mw-ui-btn {
-        width: 170px;
-        text-align: left;
-    }
+#mw_upsdfsdloader.disabled iframe {
+	top: -9999px;
+}
+.back-up-nav-btns .mw-ui-btn {
+	width: 170px;
+	text-align: left;
+}
 </style>
 
 <div id="mw-admin-content">
-    <div class="mw_edit_page_default" id="mw_edit_page_left">
-        <div class="mw-admin-sidebar">
-            <h2><span class="ico imanage-module"></span>&nbsp;
-                <?php _e("Backups"); ?>
-            </h2>
-        </div>
-        <div class="mw-admin-side-nav">
-            <ul>
-                <li><a href="<?php print $config['url']; ?>?backup_action=manage">
-                        <?php _e("Manage"); ?>
-                    </a></li>
-                <li><a href="<?php print $config['url']; ?>?backup_action=settings">
-                        <?php _e("Settings"); ?>
-                    </a></li>
-            </ul>
-            <div>
-                <div class="back-up-nav-btns">
-                    <div class="vSpace"></div>
-                    <a href="javascript:mw.admin_backup.create('.mw_edit_page_right')"
+  <div class="mw_edit_page_default" id="mw_edit_page_left">
+    <div class="mw-admin-sidebar">
+      <h2><span class="ico imanage-module"></span>&nbsp;
+        <?php _e("Backups"); ?>
+      </h2>
+    </div>
+    <div class="mw-admin-side-nav">
+      <ul>
+        <li><a href="<?php print $config['url']; ?>?backup_action=manage">
+          <?php _e("Manage"); ?>
+          </a></li>
+        <li><a href="<?php print $config['url']; ?>?backup_action=settings">
+          <?php _e("Settings"); ?>
+          </a></li>
+      </ul>
+      <div>
+        <div class="back-up-nav-btns">
+          <div class="vSpace"></div>
+          <a href="javascript:mw.admin_backup.create('.mw_edit_page_right')"
                        class="mw-ui-btn mw-ui-btn-green"><span class="ico iplus"></span><span>
           <?php _e("Database Backup"); ?>
           </span></a>
-
-                    <div class="vSpace"></div>
-                    <a href="javascript:mw.admin_backup.create_full('.mw_edit_page_right')"
+          <div class="vSpace"></div>
+          <a href="javascript:mw.admin_backup.create_full('.mw_edit_page_right')"
                        class="mw-ui-btn mw-ui-btn-blue"><span class="ico iplus"></span><span>
           <?php _e("Create Full Backup"); ?>
           </span></a>
-
-                    <div class="vSpace"></div>
-                    <script type="text/javascript">
+          <div class="vSpace"></div>
+          <script type="text/javascript">
                         var uploader = mw.files.uploader({
                             filetypes: "zip,sql",
                             multiple: false
@@ -75,23 +72,24 @@
                             });
                         });
 
-                    </script>
+                    </script> 
           <span id="mw_uploader" class="mw-ui-btn"><span class="ico iupload"></span><span>
           <?php _e("Upload backup"); ?>
-                  <span id="upload_backup_info"></span></span></span></div>
-                <div id="mw_uploader_loading" class="mw-ui-btn" style="display:none;">
-                    <?php _e("Uploading files"); ?>
-                </div>
-                <div class="vSpace">&nbsp;</div>
-                <div id="mw_backup_log" type="admin/backup/log"></div>
-            </div>
+          <span id="upload_backup_info"></span></span></span></div>
+        <div id="mw_uploader_loading" class="mw-ui-btn" style="display:none;">
+          <?php _e("Uploading files"); ?>
         </div>
+        <div class="vSpace">&nbsp;</div>
+        <div id="mw_backup_log" type="admin/backup/log"></div>
+      </div>
     </div>
-    <div class="mw_edit_page_right" style="padding: 20px;">
-        <?php if (isset($_GET['backup_action'])): ?>
-            <module type="admin/backup/<?php print $_GET['backup_action'] ?>"/>
-        <?php else : ?>
-            <module type="admin/backup/manage"/>
-        <?php endif;?>
-    </div>
+  </div>
+  <div class="mw_edit_page_right" style="padding: 20px;">
+    <?php if (isset($_GET['backup_action'])): ?>
+    <?php $action= htmlentities( mw()->format->strip_unsafe($_GET['backup_action'])); ?>
+    <module type="admin/backup/<?php print $action ?>"/>
+    <?php else : ?>
+    <module type="admin/backup/manage"/>
+    <?php endif;?>
+  </div>
 </div>
