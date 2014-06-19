85c98129933e23d99375c6f7863e3bdd117e6c99
Peter Ivanov <peter@microweber.com>
2014-06-19 19:53:37 +0300

    added missing files

---
+++ userfiles/modules/admin/import/config.php
@@ -0,0 +1,11 @@
+<?php
+$config = array();
+$config['name'] = "Import";
+$config['author'] = "Microweber";
+ 
+$config['categories'] = "admin"; 
+$config['version'] = 0.3;
+$config['ui_admin'] = true;
+$config['position'] = 99;
+ 
+ 
