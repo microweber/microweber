621b86e59dd56b1a22ba14413fdabe280e41cb8c
Ash Rain <ash@netshell.me>
2014-12-17 23:31:26 +0200

    env fix

--- src/Microweber/MicroweberServiceProvider.php
+++ src/Microweber/MicroweberServiceProvider.php
@@ -61,19 +61,15 @@ class MicroweberServiceProvider extends ServiceProvider
         $this->app->singleton('event_manager', function ($app) {
             return new Providers\Event($app);
         });
-
         $this->app->singleton('database_manager', function ($app) {
             return new Providers\DatabaseManager($app);
         });
-
         $this->app->singleton('format', function ($app) {
             return new Utils\Format($app);
         });
-
         $this->app->singleton('parser', function ($app) {
             return new Utils\Parser($app);
         });
-
         $this->app->singleton('url_manager', function ($app) {
             return new Providers\UrlManager($app);
         });
@@ -83,8 +79,6 @@ class MicroweberServiceProvider extends ServiceProvider
         $this->app->singleton('content_manager', function ($app) {
             return new Providers\ContentManager($app);
         });
-
-
         $this->app->singleton('update', function ($app) {
             return new Providers\UpdateManager($app);
         });
@@ -103,31 +97,23 @@ class MicroweberServiceProvider extends ServiceProvider
         $this->app->singleton('forms_manager', function ($app) {
             return new Providers\FormsManager($app);
         });
-
         $this->app->singleton('notifications_manager', function ($app) {
             return new Providers\NotificationsManager($app);
         });
-
         $this->app->singleton('log_manager', function ($app) {
             return new Providers\LogManager($app);
         });
         $this->app->singleton('option_manager', function ($app) {
             return new Providers\OptionManager($app);
         });
-
         $this->app->singleton('database', function () {
             return new \Database();
         });
-
-        // remove me //
-
+        // ---- remove me ---- //
         $this->app->singleton('db_model', function () {
             return new \DbModel();
         });
-
         // end of remove me //
-
-
         $this->app->bind('template', function ($app) {
             return new Providers\Template($app);
         });
@@ -144,11 +130,9 @@ class MicroweberServiceProvider extends ServiceProvider
         $this->app->singleton('user_manager', function ($app) {
             return new Providers\UserManager($app);
         });
-
         $this->app->singleton('shop_manager', function ($app) {
             return new Providers\ShopManager($app);
         });
-
         $this->app->singleton('layouts_manager', function ($app) {
             return new Providers\LayoutsManager($app);
         });
@@ -160,39 +144,32 @@ class MicroweberServiceProvider extends ServiceProvider
     }
 
 
-    public function boot()
+    public function boot(Request $request)
     {
         parent::boot();
 
+        // Set environment
+        $domain = $request->server('HTTP_HOST');
+        $this->app->detectEnvironment(function() use($domain){ return $domain; });
+
+        // Register routes
         $this->registerRoutes();
+
+        // public = /
         \App::instance('path.public', base_path());
-        \Cache::extend('file', function($app)
-        {
+
+        // Extend the cache
+        \Cache::extend('file', function($app) {
             return new Providers\CacheTags;
         });
 
-        $is_installed = mw_is_installed();
-
-        if ($is_installed)
-        {
+        // If installed load module functions
+        if (mw_is_installed()) {
             $modules = load_all_functions_files_for_modules();
-
-            /*
-            Event::listen('illuminate.query', function ($sql, $bindings, $time) {
-                echo $sql; // select * from my_table where id=?
-                print_r($bindings); // Array ( [0] => 4 )
-                echo $time; // 0.58
-
-                // To get the full sql query with bindings inserted
-                $sql = str_replace(array('%', '?'), array('%%', '%s'), $sql);
-                $full_sql = vsprintf($sql, $bindings);
-            });
-            */
         }
-        else
-        {
+        // Otherwise register the install command
+        else {
             $this->commands('Microweber\Commands\InstallCommand');
-            //$this->commands('microweber.install');
         }
     }
 
@@ -206,8 +183,7 @@ class MicroweberServiceProvider extends ServiceProvider
         }
         return false;
     }
-    
-    
+
     function autoloadModules($className)
     {
         $filename = modules_path() . $className . ".php";
