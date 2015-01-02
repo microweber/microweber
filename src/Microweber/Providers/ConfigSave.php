7dfa9a4049e70df5d246ff06af2df064fe95f703
Peter Ivanov <peter@microweber.com>
2015-01-02 23:43:01 +0200

    cli install

--- src/Microweber/Providers/ConfigSave.php
+++ src/Microweber/Providers/ConfigSave.php
@@ -23,10 +23,9 @@ class ConfigSave extends Repository
     private function init()
     {
         $this->items = array();
-        foreach (Finder::create()->files()->name('*.php')->in($this->app->configPath()) as $file)
-        {
+        foreach (Finder::create()->files()->name('*.php')->in($this->app->configPath()) as $file) {
             $path = $file->getRelativePath();
-            if($path && $path != $this->app->environment())
+            if ($path && $path != $this->app->environment())
                 continue;
             $key = basename($file->getRealPath(), '.php');
             $this->set($key, require $file->getRealPath());
@@ -55,14 +54,25 @@ class ConfigSave extends Repository
             array_set($aggr, $key, $value);
         }
 
+        $allow_in_cli = array('database', 'microweber');
         // Preparing data
         foreach ($aggr as $file => $items) {
-            $path = $this->app->configPath() .'/'. $this->app->environment() .'/';
-            if(!file_exists($path))
-                File::makeDirectory($path);
-            $path.= $file .'.php';
+            $path = $this->app->configPath() . '/' . $this->app->environment() . '/';
 
-            $code = '<?php return '. var_export($this->items[$file], true) .';';
+            if ($this->app->runningInConsole()) {
+                if (!in_array($file, $allow_in_cli)) {
+                    continue;
+                }
+            }
+
+            if (!file_exists($path)) {
+                File::makeDirectory($path);
+            }
+
+
+            $path .= $file . '.php';
+
+            $code = '<?php return ' . var_export($this->items[$file], true) . ';';
 
             // Storing data
             File::put($path, $code);
