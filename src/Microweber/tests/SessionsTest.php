bf14c040839cea20cacbec3c362ae018f585a246
Peter Ivanov <peter@microweber.com>
2015-03-29 00:13:48 +0200

    Sessions Test

---
+++ src/Microweber/tests/SessionsTest.php
@@ -0,0 +1,17 @@
+<?php
+
+
+namespace Microweber\tests;
+
+class SessionsTest extends TestCase
+{
+
+    public function testSessions()
+    {
+        $expected = "Session var " . rand();
+        session_set('my_sess_var', $expected);
+        $session_var = session_get('my_sess_var');
+        $this->assertEquals($session_var, $expected);
+    }
+
+}
