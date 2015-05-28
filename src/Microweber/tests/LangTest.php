396f52a6ebd804a318b386594dd4c614ac2d816e
Peter Ivanov <peter@microweber.com>
2015-05-28 15:28:56 +0300

    test: ensure language set is working

---
+++ src/Microweber/tests/LangTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Microweber\tests;
+
+
+class LangTest extends TestCase
+{
+
+
+    public function testLang()
+    {
+
+        $current_lang = current_lang();
+        set_current_lang('bg');
+        $new_current_lang = current_lang();
+
+        $this->assertEquals('en', $current_lang);
+        $this->assertEquals('bg', $new_current_lang);
+
+        $lang_string_test = lang("Select country");
+        $this->assertEquals('Избери държава', $lang_string_test);
+    }
+
+
+
+
+}
