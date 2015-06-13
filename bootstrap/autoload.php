fb6261d4e133176338aa06cc0370efaffeffca51
Ash <balkanski@outlook.com>
2015-06-13 17:32:07 +0300

    Laravel 5.1

--- bootstrap/autoload.php
+++ bootstrap/autoload.php
@@ -27,7 +27,7 @@ require __DIR__.'/../vendor/autoload.php';
 |
 */
 
-$compiledPath = __DIR__.'/../storage/framework/compiled.php';
+$compiledPath = __DIR__.'/bootstrap/cache/compiled.php';
 
 if (file_exists($compiledPath))
 {
