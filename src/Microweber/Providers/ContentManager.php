bf2b8775b23cb48eea784632058eee77b1996710
Peter Ivanov <peter@microweber.com>
2015-09-15 22:05:58 +0300

    fix of preg_replace bug
    preg_replace(): The /e modifier is deprecated, use preg_replace_callback instead

--- src/Microweber/Providers/ContentManager.php
+++ src/Microweber/Providers/ContentManager.php
@@ -4054,8 +4054,8 @@ class ContentManager {
 
     public function _decode_entities($text) {
         $text = html_entity_decode($text, ENT_QUOTES, "ISO-8859-1"); #NOTE: UTF-8 does not work!
-        $text = preg_replace('/&#(\d+);/me', "chr(\\1)", $text); #decimal notation
-        $text = preg_replace('/&#x([a-f0-9]+);/mei', "chr(0x\\1)", $text); #hex notation
+        $text = preg_replace('/&#(\d+);/m', "chr(\\1)", $text); #decimal notation
+        $text = preg_replace('/&#x([a-f0-9]+);/mi', "chr(0x\\1)", $text); #hex notation
         return $text;
     }
 
