49356ce0b49ed576a1e4f41e841f0ce63ded118d
Peter Ivanov <peter@microweber.com>
2015-09-25 02:29:49 +0300

    cache write bug fix

--- src/Microweber/Providers/CacheStore.php
+++ src/Microweber/Providers/CacheStore.php
@@ -4,8 +4,7 @@ use Illuminate\Contracts\Cache\Store,
     Illuminate\Filesystem\Filesystem,
     Closure;
 
-class CacheStore implements Store
-{
+class CacheStore implements Store {
     protected $files;
     protected $prefix;
     protected $tags;
@@ -16,8 +15,7 @@ class CacheStore implements Store
     public $deleted_tags = array();
 
 
-    public function __construct($prefix = '')
-    {
+    public function __construct($prefix = '') {
         $this->files = new Filesystem;
         $this->prefix = $prefix;
 
@@ -29,30 +27,34 @@ class CacheStore implements Store
 
 
     private function appendLocale($key) {
-      return $key . '_' . app()->getLocale();
+//        static $locale_suffix;
+//        if ($locale_suffix){
+//            $locale_suffix = '_' . app()->getLocale();
+//        }
+        return $key;
     }
 
     /**
      * Retrieve an item from the cache by key.
      *
      * @param  string $key
+     *
      * @return mixed
      */
-    public function get($key)
-    {
+    public function get($key) {
         $key = $this->appendLocale($key);
 
-        if (!empty($this->tags)) {
+        if (!empty($this->tags)){
             foreach ($this->tags as $tag) {
-                if (in_array($tag, $this->deleted_tags)) {
+                if (in_array($tag, $this->deleted_tags)){
                     // return;
                 }
             }
         }
 
-        if (!isset($this->memory[$key])) {
+        if (!isset($this->memory[ $key ])){
             $path = $this->path($key);
-            if (!$this->files->exists($path)) {
+            if (!$this->files->exists($path)){
                 return null;
             }
             try {
@@ -64,27 +66,27 @@ class CacheStore implements Store
             // If the current time is greater than expiration timestamps we will delete
             // the file and return null. This helps clean up the old files and keeps
             // this directory much cleaner for us as old files aren't hanging out.
-            if (time() >= $expire) {
+            if (time() >= $expire){
                 return $this->forget($key);
             }
-            if ($contents) {
-                $this->memory[$key] = unserialize(substr($contents, 10));
+            if ($contents){
+                $this->memory[ $key ] = unserialize(substr($contents, 10));
             }
         }
 
-        return $this->memory[$key];
+        return $this->memory[ $key ];
     }
 
     /**
      * Store an item in the cache for a given number of minutes.
      *
      * @param  string $key
-     * @param  mixed $value
-     * @param  int $minutes
+     * @param  mixed  $value
+     * @param  int    $minutes
+     *
      * @return void
      */
-    public function put($key, $value, $minutes)
-    {
+    public function put($key, $value, $minutes) {
         $key = $this->appendLocale($key);
 
         $value = $this->expiration($minutes) . serialize($value);
@@ -93,16 +95,16 @@ class CacheStore implements Store
 
         $this->createCacheDirectory($path);
         $skip = false;
-        if (!empty($this->tags)) {
+        if (!empty($this->tags)){
             foreach ($this->tags as $tag) {
-                if (in_array($tag, $this->deleted_tags)) {
+                if (in_array($tag, $this->deleted_tags)){
                     $skip = true;
                 }
             }
 
             $this->_setTags($path);
         }
-        if (!$skip) {
+        if (!$skip){
             @file_put_contents($path, $value);
         }
         // $this->files->put($path, $value);
@@ -112,14 +114,14 @@ class CacheStore implements Store
      * Tags for cache.
      *
      * @param  string $string
+     *
      * @return object
      */
-    public function tags($tags)
-    {
+    public function tags($tags) {
         $string_array = array();
-        if (is_string($tags)) {
+        if (is_string($tags)){
             $string_array = explode(',', $tags);
-        } else if (is_array($tags)) {
+        } else if (is_array($tags)){
             $string_array = $tags;
             array_walk($string_array, 'trim');
         }
@@ -133,10 +135,10 @@ class CacheStore implements Store
      * Save Tags for cache.
      *
      * @param  string $path
+     *
      * @return null
      */
-    private function _setTags($path)
-    {
+    private function _setTags($path) {
 
         foreach ($this->tags as $tg) {
 
@@ -144,15 +146,15 @@ class CacheStore implements Store
             $file = $this->directoryTags . '/' . $tg;
             $file = $this->normalize_path($file, false);
 
-            if (!is_dir(dirname($path))) {
+            if (!is_dir(dirname($path))){
                 $this->createCacheDirectory($file);
                 //$this->files->put($file, $path);
                 @file_put_contents($file, "$path");
 
             } else {
-                if (is_file($file)) {
+                if (is_file($file)){
                     $farr = file($file);
-                    if (!in_array($path, $farr)) {
+                    if (!in_array($path, $farr)){
                         @file_put_contents($file, "\n$path", FILE_APPEND);
                     }
                 }
@@ -166,19 +168,19 @@ class CacheStore implements Store
     /**
      * Get an item from the cache, or store the default value.
      *
-     * @param  string $key
+     * @param  string        $key
      * @param  \DateTime|int $minutes
-     * @param  Closure $callback
+     * @param  Closure       $callback
+     *
      * @return mixed
      */
-    public function remember($key, $minutes, Closure $callback)
-    {
+    public function remember($key, $minutes, Closure $callback) {
         $key = $this->appendLocale($key);
 
         // If the item exists in the cache we will just return this immediately
         // otherwise we will execute the given Closure and cache the result
         // of that execution for the given number of minutes in storage.
-        if (!is_null($value = $this->get($key))) {
+        if (!is_null($value = $this->get($key))){
             return $value;
         }
 
@@ -190,18 +192,18 @@ class CacheStore implements Store
     /**
      * Get an item from the cache, or store the default value forever.
      *
-     * @param  string $key
+     * @param  string  $key
      * @param  Closure $callback
+     *
      * @return mixed
      */
-    public function rememberForever($key, Closure $callback)
-    {
+    public function rememberForever($key, Closure $callback) {
         $key = $this->appendLocale($key);
 
         // If the item exists in the cache we will just return this immediately
         // otherwise we will execute the given Closure and cache the result
         // of that execution for the given number of minutes. It's easy.
-        if (!is_null($value = $this->get($key))) {
+        if (!is_null($value = $this->get($key))){
             return $value;
         }
 
@@ -214,10 +216,10 @@ class CacheStore implements Store
      * Create the file cache directory if necessary.
      *
      * @param  string $path
+     *
      * @return void
      */
-    protected function createCacheDirectory($path)
-    {
+    protected function createCacheDirectory($path) {
 
         return $this->mkdir_recursive(dirname($path));
 
@@ -227,13 +229,13 @@ class CacheStore implements Store
      * Increment the value of an item in the cache.
      *
      * @param  string $key
-     * @param  mixed $value
+     * @param  mixed  $value
+     *
      * @return void
      *
      * @throws \LogicException
      */
-    public function increment($key, $value = 1)
-    {
+    public function increment($key, $value = 1) {
         $key = $this->appendLocale($key);
 
         throw new \LogicException("Not supported by this driver.");
@@ -243,13 +245,13 @@ class CacheStore implements Store
      * Increment the value of an item in the cache.
      *
      * @param  string $key
-     * @param  mixed $value
+     * @param  mixed  $value
+     *
      * @return void
      *
      * @throws \LogicException
      */
-    public function decrement($key, $value = 1)
-    {
+    public function decrement($key, $value = 1) {
         $key = $this->appendLocale($key);
 
         throw new \LogicException("Not supported by this driver.");
@@ -259,11 +261,11 @@ class CacheStore implements Store
      * Store an item in the cache indefinitely.
      *
      * @param  string $key
-     * @param  mixed $value
+     * @param  mixed  $value
+     *
      * @return void
      */
-    public function forever($key, $value)
-    {
+    public function forever($key, $value) {
         $key = $this->appendLocale($key);
 
         return $this->put($key, $value, 0);
@@ -273,25 +275,25 @@ class CacheStore implements Store
      * Remove an item from the cache by tags.
      *
      * @param  string $string
+     *
      * @return void
      */
-    public function forgetTags($string)
-    {
+    public function forgetTags($string) {
         $string_array = explode(',', $string);
-        if(is_array($string_array)){
-            foreach($string_array as $k=>$v){
-                $string_array[$k] = trim($v);
+        if (is_array($string_array)){
+            foreach ($string_array as $k => $v) {
+                $string_array[ $k ] = trim($v);
             }
         }
         foreach ($string_array as $sa) {
             $this->deleted_tags[] = $sa;
             $file = $this->directoryTags . '/' . $sa;
-            if ($this->files->exists($file)) {
+            if ($this->files->exists($file)){
                 $farr = file($file);
                 foreach ($farr as $f) {
-                    if ($f != false) {
+                    if ($f!=false){
                         $f = $this->normalize_path($f, false);
-                        if (is_file($f)) {
+                        if (is_file($f)){
                             @unlink($f);
                         }
                     }
@@ -305,19 +307,19 @@ class CacheStore implements Store
      * Remove an item from the cache.
      *
      * @param  string $key
+     *
      * @return void
      */
-    public function forget($key)
-    {
+    public function forget($key) {
         $key = $this->appendLocale($key);
 
         $file = $this->path($key);
 
-        if ($this->files->exists($file)) {
+        if ($this->files->exists($file)){
             @$this->files->delete($file);
         } else {
-            $folder = substr($file, 0, -6);
-            if ($this->files->exists($folder)) {
+            $folder = substr($file, 0, - 6);
+            if ($this->files->exists($folder)){
                 @$this->files->deleteDirectory($folder);
             }
         }
@@ -329,21 +331,21 @@ class CacheStore implements Store
      * Remove all items from the cache.
      *
      * @param  string $tag
+     *
      * @return void
      */
-    public function flush($all = false)
-    {
+    public function flush($all = false) {
 
         $this->memory = array();
-        if (empty($this->tags) or $all == true) {
-            if (is_dir($this->directory)) {
+        if (empty($this->tags) or $all==true){
+            if (is_dir($this->directory)){
                 $this->rmdir($this->directory);
             }
         } else {
 
 
             foreach ($this->tags as $tag) {
-                if (in_array($tag, $this->deleted_tags)) {
+                if (in_array($tag, $this->deleted_tags)){
                     //   break;
                 }
 
@@ -362,8 +364,7 @@ class CacheStore implements Store
      *
      * @return \Illuminate\Filesystem\Filesystem
      */
-    public function getFilesystem()
-    {
+    public function getFilesystem() {
         return $this->files;
     }
 
@@ -372,8 +373,7 @@ class CacheStore implements Store
      *
      * @return string
      */
-    public function getDirectory()
-    {
+    public function getDirectory() {
         return $this->directory;
     }
 
@@ -382,8 +382,7 @@ class CacheStore implements Store
      *
      * @return string
      */
-    public function getPrefix()
-    {
+    public function getPrefix() {
         return $this->prefix;
     }
 
@@ -391,15 +390,15 @@ class CacheStore implements Store
      * Get the full path for the given cache key.
      *
      * @param  string $key
+     *
      * @return string
      */
-    protected function path($key)
-    {
+    protected function path($key) {
         $key = $this->appendLocale($key);
 
         $prefix = !empty($this->prefix) ? $this->prefix . '/' : '';
         $subdir = 'global';
-        if (!empty($this->tags)) {
+        if (!empty($this->tags)){
             $subdir = reset($this->tags);
         }
 
@@ -411,35 +410,36 @@ class CacheStore implements Store
      * Get the expiration time based on the given minutes.
      *
      * @param  int $minutes
+     *
      * @return int
      */
-    protected function expiration($minutes)
-    {
-        if ($minutes === 0) return 9999999999;
+    protected function expiration($minutes) {
+        if ($minutes===0){
+            return 9999999999;
+        }
 
         return time() + ($minutes * 60);
     }
 
 
-    function normalize_path($path, $slash_it = true)
-    {
+    function normalize_path($path, $slash_it = true) {
         $path_original = $path;
         $s = DIRECTORY_SEPARATOR;
         $path = preg_replace('/[\/\\\]/', $s, $path);
         $path = str_replace($s . $s, $s, $path);
-        if (strval($path) == '') {
+        if (strval($path)==''){
             $path = $path_original;
         }
-        if ($slash_it == false) {
+        if ($slash_it==false){
             $path = rtrim($path, DIRECTORY_SEPARATOR);
         } else {
             $path .= DIRECTORY_SEPARATOR;
             $path = rtrim($path, DIRECTORY_SEPARATOR . DIRECTORY_SEPARATOR);
         }
-        if (strval(trim($path)) == '' or strval(trim($path)) == '/') {
+        if (strval(trim($path))=='' or strval(trim($path))=='/'){
             $path = $path_original;
         }
-        if ($slash_it == false) {
+        if ($slash_it==false){
         } else {
             $path = $path . DIRECTORY_SEPARATOR;
             $path = $this->reduce_double_slashes($path);
@@ -450,36 +450,34 @@ class CacheStore implements Store
     }
 
 
-    function reduce_double_slashes($str)
-    {
+    function reduce_double_slashes($str) {
         return preg_replace("#([^:])//+#", "\\1/", $str);
     }
 
-    function mkdir_recursive($pathname)
-    {
-        if ($pathname == '') {
+    function mkdir_recursive($pathname) {
+        if ($pathname==''){
             return false;
         }
         is_dir(dirname($pathname)) || $this->mkdir_recursive(dirname($pathname));
+
         return is_dir($pathname) || @mkdir($pathname);
     }
 
-    public function rmdir($directory, $empty = true)
-    {
+    public function rmdir($directory, $empty = true) {
         // if the path has a slash at the end we remove it here
-        if (substr($directory, -1) == DIRECTORY_SEPARATOR) {
-            $directory = substr($directory, 0, -1);
+        if (substr($directory, - 1)==DIRECTORY_SEPARATOR){
+            $directory = substr($directory, 0, - 1);
         }
 
         // if the path is not valid or is not a directory ...
-        if (!is_dir($directory)) {
+        if (!is_dir($directory)){
             // ... we return false and exit the function
-            return FALSE;
+            return false;
 
             // ... if the path is not readable
         } elseif (!is_readable($directory)) {
             // ... we return false and exit the function
-            return FALSE;
+            return false;
 
             // ... else if the path is readable
         } else {
@@ -487,15 +485,15 @@ class CacheStore implements Store
             $handle = opendir($directory);
 
             // and scan through the items inside
-            while (FALSE !== ($item = readdir($handle))) {
+            while (false!==($item = readdir($handle))) {
                 // if the filepointer is not the current directory
                 // or the parent directory
-                if ($item != '.' && $item != '..') {
+                if ($item!='.' && $item!='..'){
                     // we build the new path to delete
                     $path = $directory . DIRECTORY_SEPARATOR . $item;
 
                     // if the new path is a directory
-                    if (is_dir($path)) {
+                    if (is_dir($path)){
                         // we call this function with the new path
                         $this->rmdir($path, $empty);
                         // if the new path is a file
@@ -513,7 +511,7 @@ class CacheStore implements Store
             closedir($handle);
 
             // if the option to empty is not set to true
-            if ($empty == FALSE) {
+            if ($empty==false){
                 @rmdir($directory);
                 // try to delete the now empty directory
                 //            if (!rmdir($directory)) {
@@ -524,7 +522,7 @@ class CacheStore implements Store
             }
 
             // return success
-            return TRUE;
+            return true;
         }
     }
 
