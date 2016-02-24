de4733b9d4b5942d2b54790f5a57bc0b04b60b50
Peter Ivanov <peter@microweber.com>
2016-02-24 16:43:25 +0200

    added omnipay

---
+++ userfiles/modules/shop/payments/gateways/stripe/config.php
@@ -0,0 +1,10 @@
+<?php
+
+$config = array();
+$config['name'] = "Stripe payment gateway";
+$config['author'] = "Microweber";
+$config['ui'] = false;
+$config['categories'] = "online shop";
+$config['position'] = 132;
+$config['type'] = "payment_gateway";
+ 
---
+++ userfiles/modules/shop/payments/gateways/stripe/index.php
@@ -0,0 +1,15 @@
+<?php
+
+$pay_on_delivery_show_msg = (get_option('pay_on_delivery_show_msg', 'payments')) == 'y';
+$pay_on_delivery_msg = get_option('pay_on_delivery_msg', 'payments');
+?>
+
+<div>
+    <p class="alert alert-warning"><small><strong> *<?php _e("Note"); ?> </strong><?php _e("Your shopping cart will be emptied when you complete the order"); ?></small> </p>
+</div>
+
+
+
+<?php 
+
+include(dirname(__DIR__).DS.'partials'.DS.'cc_form_fields.php'); 
