0c7edbecda9c39b241cec5ddd2e89ea0e25e8a42
Peter Ivanov <peter@microweber.com>
2016-03-03 19:42:25 +0200

    fixed a bug when table prefix starts with number

--- src/Microweber/Controllers/InstallController.php
+++ src/Microweber/Controllers/InstallController.php
@@ -12,14 +12,12 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Artisan;
 use Cache;
 
-class InstallController extends Controller
-{
+class InstallController extends Controller {
     public $app;
 
-    public function __construct($app = null)
-    {
-        if (!is_object($this->app)) {
-            if (is_object($app)) {
+    public function __construct($app = null) {
+        if (!is_object($this->app)){
+            if (is_object($app)){
                 $this->app = $app;
             } else {
                 $this->app = mw();
@@ -27,69 +25,73 @@ class InstallController extends Controller
         }
     }
 
-    public function index($input = null)
-    {
-        if (!is_array($input) || empty($input)) {
+    public function index($input = null) {
+        if (!is_array($input) || empty($input)){
             $input = Input::all();
         }
         $allowed_configs = array('database', 'microweber');
         $is_installed = mw_is_installed();
-        if ($is_installed) {
+        if ($is_installed){
             return 'Microweber is already installed!';
         }
 
-        $view = MW_PATH.'Views/install.php';
+        $view = MW_PATH . 'Views/install.php';
 
         $connection = Config::get('database.connections');
         $this->install_log('Preparing to install');
-        if (isset($input['make_install'])) {
-            if (!isset($input['db_pass'])) {
+        if (isset($input['make_install'])){
+            if (!isset($input['db_pass'])){
                 $input['db_pass'] = '';
             }
-            if (!isset($input['table_prefix'])) {
+            if (!isset($input['table_prefix'])){
                 $input['table_prefix'] = '';
             }
 
+            if (is_numeric(substr($input['table_prefix'], 0, 1))
+            ){
+                $input['table_prefix'] = 'p' . $input['table_prefix'];
+            }
+ 
             $errors = array();
-            if (!isset($input['db_host'])) {
+            if (!isset($input['db_host'])){
                 $errors[] = 'Parameter "db_host" is required';
             } else {
                 $input['db_host'] = trim($input['db_host']);
             }
-            if (!isset($input['db_name'])) {
+            if (!isset($input['db_name'])){
                 $errors[] = 'Parameter "db_name" is required';
             } else {
                 $input['db_name'] = trim($input['db_name']);
             }
-            if (!isset($input['db_user'])) {
+            if (!isset($input['db_user'])){
                 $errors[] = 'Parameter "db_user" is required';
             }
-            if (!isset($input['admin_email'])) {
+            if (!isset($input['admin_email'])){
                 $errors[] = 'Parameter "admin_email" is required';
             }
-            if (!isset($input['admin_password'])) {
+            if (!isset($input['admin_password'])){
                 $errors[] = 'Parameter "admin_password" is required';
             }
-            if (!isset($input['admin_username'])) {
+            if (!isset($input['admin_username'])){
                 $errors[] = 'Parameter "admin_username" is required';
             }
 
-            if (!empty($errors)) {
+            if (!empty($errors)){
                 return implode("\n", $errors);
             }
-            if (isset($input['db_driver'])) {
+            if (isset($input['db_driver'])){
                 $dbDriver = $input['db_driver'];
             } else {
                 $dbDriver = 'mysql';
             }
 
             Config::set('database.default', $dbDriver);
-            if ($dbDriver == 'sqlite') {
-                if (isset($input['db_name_sqlite'])) {
+            if ($dbDriver=='sqlite'){
+                if (isset($input['db_name_sqlite'])){
                     $input['db_name'] = $input['db_name_sqlite'];
                 }
                 Config::set("database.connections.$dbDriver.database", $input['db_name']);
-                if (!file_exists($input['db_name'])) {
+                if (!file_exists($input['db_name'])){
                     touch($input['db_name']);
                 }
             }
@@ -100,23 +102,23 @@ class InstallController extends Controller
             Config::set("database.connections.$dbDriver.database", $input['db_name']);
             Config::set("database.connections.$dbDriver.prefix", $input['table_prefix']);
 
-            if (defined('MW_VERSION')) {
+            if (defined('MW_VERSION')){
                 Config::set('microweber.version', MW_VERSION);
             }
 
-            if (isset($input['default_template']) and $input['default_template'] != false) {
+            if (isset($input['default_template']) and $input['default_template']!=false){
                 Config::set('microweber.install_default_template', $input['default_template']);
             }
-            if (isset($input['with_default_content']) and $input['with_default_content'] != false) {
+            if (isset($input['with_default_content']) and $input['with_default_content']!=false){
                 Config::set('microweber.install_default_template_content', 1);
             }
 
-            if (!isset($input['developer_mode'])) {
+            if (!isset($input['developer_mode'])){
                 Config::set('microweber.compile_assets', 1);
             }
 
-            if (Config::get('app.key') == 'YourSecretKey!!!') {
-                if (!$this->app->runningInConsole()) {
+            if (Config::get('app.key')=='YourSecretKey!!!'){
+                if (!$this->app->runningInConsole()){
                     $_SERVER['argv'] = array();
                 }
                 Artisan::call('key:generate');
@@ -129,12 +131,12 @@ class InstallController extends Controller
             try {
                 DB::connection($dbDriver)->getDatabaseName();
             } catch (\PDOException $e) {
-                return 'Error: '.$e->getMessage()."\n";
+                return 'Error: ' . $e->getMessage() . "\n";
             } catch (\Exception $e) {
-                return 'Error: '.$e->getMessage()."\n";
+                return 'Error: ' . $e->getMessage() . "\n";
             }
 
-            if (function_exists('set_time_limit')) {
+            if (function_exists('set_time_limit')){
                 @set_time_limit(0);
             }
 
@@ -153,7 +155,7 @@ class InstallController extends Controller
             $installer = new Install\DefaultOptionsInstaller();
             $installer->run();
 
-            if (isset($input['admin_password']) && strlen($input['admin_password'])) {
+            if (isset($input['admin_password']) && strlen($input['admin_password'])){
                 $this->install_log('Adding admin user');
 
                 $adminUser = new \User();
@@ -179,47 +181,47 @@ class InstallController extends Controller
         $layout = new View($view);
 
         $defaultDbEngine = Config::get('database.default');
-        if (extension_loaded('pdo_sqlite')) {
+        if (extension_loaded('pdo_sqlite')){
             // $defaultDbEngine = 'sqlite';
         }
 
         $dbEngines = Config::get('database.connections');
         foreach ($dbEngines as $driver => $v) {
-            if (!extension_loaded("pdo_$driver")) {
+            if (!extension_loaded("pdo_$driver")){
                 unset($dbEngines[ $driver ]);
             }
         }
         $viewData = [
-            'config' => $dbEngines[ $defaultDbEngine ],
+            'config'          => $dbEngines[ $defaultDbEngine ],
             'dbDefaultEngine' => $defaultDbEngine,
-            'dbEngines' => array_keys($dbEngines),
-            'dbEngineNames' => [
-                'mysql' => 'MySQL',
+            'dbEngines'       => array_keys($dbEngines),
+            'dbEngineNames'   => [
+                'mysql'  => 'MySQL',
                 'sqlite' => 'SQLite',
                 'sqlsrv' => 'Microsoft SQL Server',
-                'pgsql' => 'PostgreSQL',
+                'pgsql'  => 'PostgreSQL',
             ],
         ];
         $domain = false;
-        if (isset($_SERVER['HTTP_HOST'])) {
+        if (isset($_SERVER['HTTP_HOST'])){
             $domain = $_SERVER['HTTP_HOST'];
             $domain = str_replace('www.', '', $domain);
             $domain = str_replace('.', '_', $domain);
             $domain = str_replace('-', '_', $domain);
             $domain = substr($domain, 0, 10);
         }
-        if (!$viewData['config']['prefix'] and $domain) {
-            $viewData['config']['prefix'] = $domain.'_';
+        if (!$viewData['config']['prefix'] and $domain){
+            $viewData['config']['prefix'] = $domain . '_';
         }
-        if (extension_loaded('pdo_sqlite') and $domain) {
-            $sqlite_path = normalize_path(storage_path().DS.$domain.'.sqlite', false);
+        if (extension_loaded('pdo_sqlite') and $domain){
+            $sqlite_path = normalize_path(storage_path() . DS . $domain . '.sqlite', false);
             $viewData['config']['db_name_sqlite'] = $sqlite_path;
         }
 
         $layout->set($viewData);
 
         $is_installed = mw_is_installed();
-        if ($is_installed) {
+        if ($is_installed){
             App::abort(403, 'Unauthorized action. Microweber is already installed.');
         }
         $layout->assign('done', $is_installed);
@@ -228,19 +230,18 @@ class InstallController extends Controller
         return $layout;
     }
 
-    private function install_log($text)
-    {
-        $log_file = userfiles_path().'install_log.txt';
-        if (!is_file($log_file)) {
+    private function install_log($text) {
+        $log_file = userfiles_path() . 'install_log.txt';
+        if (!is_file($log_file)){
             @touch($log_file);
         }
-        if (is_file($log_file)) {
+        if (is_file($log_file)){
             $json = array('date' => date('H:i:s'), 'msg' => $text);
 
-            if ($text == 'done' or $text == 'Preparing to install') {
-                @file_put_contents($log_file, $text."\n");
+            if ($text=='done' or $text=='Preparing to install'){
+                @file_put_contents($log_file, $text . "\n");
             } else {
-                @file_put_contents($log_file, $text."\n", FILE_APPEND);
+                @file_put_contents($log_file, $text . "\n", FILE_APPEND);
             }
         }
     }
