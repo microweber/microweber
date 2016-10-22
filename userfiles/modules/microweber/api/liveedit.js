6cb0983b13499fd62dfcb7459ec4f29409684e1d
raikov <raikov@ymail.com>
2016-10-22 21:39:13 +0300

    mw.currentRedirected disabled when on empty element or empty column.

--- userfiles/modules/microweber/api/liveedit.js
+++ userfiles/modules/microweber/api/liveedit.js
@@ -270,12 +270,17 @@ hasAbilityToDropElementsInside = function(target) {
     }
     return true;
 }
+setTimeout(function(){
 
+    mw.gridComponents =  mw.drag.external_grids_row_classes.concat(mw.drag.external_grids_col_classes);
+    mw.gridComponents.push('edit','mw-row','mw-ui-row','wrap_blocks_index', 'element');
+}, 50);
 
 mw.drag = {
     external_grids_row_classes: ['row'],
     external_grids_col_classes: ['col-lg-1', 'col-lg-10', 'col-lg-11', 'col-lg-12', 'col-lg-2', 'col-lg-3', 'col-lg-4', 'col-lg-5', 'col-lg-6', 'col-lg-7', 'col-lg-8', 'col-lg-9', 'col-md-1', 'col-md-10', 'col-md-11', 'col-md-12', 'col-md-2', 'col-md-3', 'col-md-4', 'col-md-5', 'col-md-6', 'col-md-7', 'col-md-8', 'col-md-9', 'col-sm-1', 'col-sm-10', 'col-sm-11', 'col-sm-12', 'col-sm-2', 'col-sm-3', 'col-sm-4', 'col-sm-5', 'col-sm-6', 'col-sm-7', 'col-sm-8', 'col-sm-9', 'col-xs-1', 'col-xs-10', 'col-xs-11', 'col-xs-12', 'col-xs-2', 'col-xs-3', 'col-xs-4', 'col-xs-5', 'col-xs-6', 'col-xs-7', 'col-xs-8', 'col-xs-9'],
     external_css_no_element_classes: ['navbar', 'navbar-header', 'navbar-collapse', 'navbar-static', 'navbar-static-top', 'navbar-default', 'navbar-text', 'navbar-right', 'navbar-center', 'navbar-left', 'nav navbar-nav', 'collapse', 'header-collapse', 'panel-heading', 'panel-body', 'panel-footer'],
+
     columnout: false,
     noop: mwd.createElement('div'),
     create: function() {
@@ -552,18 +557,24 @@ mw.drag = {
                         mw.currentDragMouseOver = mw.mm_target;
 
                     }
-                    mw.currentRedirected = false;
-                    var off =  mw.$mm_target.offset(), theight = mw.$mm_target.height();
-                    mw.tools.foreachParents(mw.mm_target, function(loop, i){
-                        if(mw.tools.hasAnyOfClasses(this, ['edit','mw-row','mw-ui-row','wrap_blocks_index', 'element'])){
-                            if((off.top - 40) < $(this).offset().top){
-                                mw.currentDragMouseOver = mw.mm_target = this;
-                                mw.$mm_target = $(mw.mm_target);
-                                mw.tools.stopLoop(loop);
-                                mw.currentRedirected = true;
-                            }
+                    else{
+                        mw.currentRedirected = false;
+                        if(!mw.tools.hasClass(mw.mm_target, 'mw-empty')){
+                            var off =  mw.$mm_target.offset(), theight = mw.$mm_target.height();
+                            mw.tools.foreachParents(mw.mm_target, function(loop, i){
+                                if(mw.tools.hasAnyOfClasses(this, mw.gridComponents)){
+                                    if((off.top - 30) < $(this).offset().top){
+                                        mw.currentDragMouseOver = mw.mm_target = this;
+                                        mw.$mm_target = $(mw.mm_target);
+                                        mw.tools.stopLoop(loop);
+                                        mw.currentRedirected = true;
+                                    }
+                                }
+                            });
                         }
-                    });
+                    }
+
+
 
                 }
 
