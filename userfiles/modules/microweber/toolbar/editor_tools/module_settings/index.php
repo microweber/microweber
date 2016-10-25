031e55793d23e4282ad2c30164f0d31d03dc8453
Peter Ivanov <peter@microweber.com>
2016-10-25 11:05:14 +0300

    fix of skin change bug

--- userfiles/modules/microweber/toolbar/editor_tools/module_settings/index.php
+++ userfiles/modules/microweber/toolbar/editor_tools/module_settings/index.php
@@ -5,36 +5,36 @@
 <?php  $module_info = false;
 if (isset($params['module'])): ?>
 <?php $module_info = mw()->modules->get('one=1&ui=any&module=' . $params['module']); ?>
-<?php endif; ?>   
-       
-               
+<?php endif; ?>
+
+
 <script type="text/javascript" src="<?php print(mw()->template->get_apijs_settings_url()); ?>"></script>
-                                                          
+
 <script type="text/javascript" src="<?php print(mw()->template->get_apijs_url()); ?>"></script>
 
 <script src="<?php print mw_includes_url(); ?>api/jquery-ui.js"></script>
 <script type="text/javascript">
     liveEditSettings = true;
-	
-     
+
+
 
      mw.require('<?php print mw_includes_url(); ?>default.css');
     mw.require('<?php print mw_includes_url(); ?>css/components.css');
     mw.require('<?php print mw_includes_url(); ?>css/admin.css');
     mw.require('<?php print mw_includes_url(); ?>css/popup.css');
-	
+
     mw.require("events.js");
     mw.require("url.js");
     mw.require("tools.js");
 	 mw.require('admin.js');
- 
+
 
     mw.require("liveadmin.js");
     mw.require("forms.js");
     mw.require('wysiwyg.js');
     mw.require('options.js');
 
-   
+
 </script>
 <style>
 #settings-main {
@@ -76,8 +76,8 @@ if (isset($_GET['type'])) {
     window.onbeforeunload = function () {
         $(mwd.body).addClass("mw-external-loading")
     }
- 
-	
+
+
     mw_module_settings_info = "";
     <?php if(is_array( $module_info)): ?>
 
@@ -96,16 +96,16 @@ if (isset($_GET['type'])) {
     <?php endif; ?>
 
     if (typeof thismodal == 'undefined' && self !== parent && typeof this.name != 'undefined' && this.name != '') {
-		 
+
         var frame = parent.mw.$('#' + this.name)[0];
         thismodal = parent.mw.tools.modal.get(mw.tools.firstParentWithClass(frame, 'mw_modal'));
-		 
+
     }
 
 
     if (typeof thismodal != 'undefined' && thismodal != false) {
-		
-		 
+
+
 
         var modal_title_str = '';
         if(typeof(mw_module_settings_info.name) == "undefined"){
@@ -113,18 +113,18 @@ if (isset($_GET['type'])) {
         } else {
             modal_title_str = mw_module_settings_info.name;
         }
-		
-		
+
+
 		var ex_title =  $(thismodal.main).find(".mw_modal_title").html();
-		 
+
 		if(ex_title == ''){
         $(thismodal.main).find(".mw_modal_title").html(modal_title_str+'');
 		}
 		if (typeof thismodal.main.scrollTop == 'function') {
 			 thismodal.main.scrollTop(0);
 		}
-		 
-       
+
+
         __autoresize = function (force) {
             var force = force || false;
             var _old = thismodal.main.height();
@@ -132,11 +132,11 @@ if (isset($_GET['type'])) {
 			if (typeof thismodal.main.scrollTop == 'function') {
 				 thismodal.main.scrollTop(0);
 			}
-			
+
 			if (typeof thismodal.main[0] != 'undefined') {
-			 
-			 
-				 
+
+
+
 					 parent.mw.tools.modal.resize("#" + thismodal.main[0].id, false, mw.$('#settings-container').height() + 25, false);
 					setTimeout(function () {
 						var _new = thismodal.main.height();
@@ -144,22 +144,22 @@ if (isset($_GET['type'])) {
 							parent.mw.tools.modal.center("#" + thismodal.main[0].id, 'vertical');
 						}
 					}, 400)
-				 
-			 
-			 
-			 
-			 
+
+
+
+
+
 			}
-			
-			
-            
+
+
+
 
 
         }
-		
 
-				
-		
+
+
+
 if (typeof thismodal.main[0] != 'undefined') {
 
         var toolbar = thismodal.main[0].querySelector('.mw_modal_toolbar');
@@ -168,7 +168,7 @@ if (typeof thismodal.main[0] != 'undefined') {
             var dd = mwd.createElement('div');
             dd.id = 'module-modal-settings-menu-holder';
             dd.className = 'mw-dropdown mw-dropdown-default mw-dropdown-click';
-			 
+
            /*******************************************************
             Do not delete !!! Module template: list and 'Crete Module Template'
 
@@ -180,15 +180,15 @@ if (typeof thismodal.main[0] != 'undefined') {
 
 
         <?php if(is_array( $module_info)): ?>
-		
-		
+
+
         <?php $mod_adm =  admin_url('load_module:').module_name_encode($module_info['module']);; ?>
-		
-		
-		
-		
+
+
+
+
         is_module_tml_holder = $(toolbar).find("#module-modal-settings-menu-holder");
-		
+
         if (is_module_tml_holder.length > 0) {
 
 
@@ -203,57 +203,57 @@ if (typeof thismodal.main[0] != 'undefined') {
                 + "</div>"
                 + "<hr>"
                 + "<div id='module-modal-settings-menu-holder-2'><a class='mw-ui-btn mw-ui-btn-small' href='<?php print $mod_adm  ?>'><?php _e("Go to admin"); ?></a></div>"
-				
-				
-			
-				
+
+
+
+
 			window.parent.modal_preset_manager_html_placeholder_for_reload = function(){
 			var modal_preset_manager_html_placeholder_for_reload = ""
                 + "<div id='module-modal-settings-menu-items' module_id='<?php print $params['id'] ?>' module_name='<?php print $module_info['module'] ?>'>"
                 + "</div>"
-				
-				
+
+
 				mw_admin_edit_tax_item_popup_modal_opened = window.parent.mw.modal({
 					content:   modal_preset_manager_html_placeholder_for_reload,
 					title:     'Edit module presets',
 					id:        'modal_preset_manager_html_placeholder_for_reload_pop'
 				});
-   
- 
-	
-				
+
+
+
+
 				 window.parent.mw.load_module("admin/modules/saved_templates", '#module-modal-settings-menu-items');
-			
-				
-				}	
+
+
+				}
 				var html = ""
-              
+
                 + "<div id='module-modal-settings-menu-holder-2'><a class='mw-ui-btn mw-ui-btn-small' href='javascript:modal_preset_manager_html_placeholder_for_reload();'>Presets</a></div>"
-			
-	
-	
- 
+
+
+
+
             var btn = "<a class='mw-ui-btn-small'  oxxxnclick='$(this).toggleClass(\"active\")'><span class='mw-icon-dropdown right'></span></a>";
 
 
 			var module_has_editable_parent = window.parent.$('#<?php print $params['id'] ?>');
-		 
+
 			if(typeof(module_has_editable_parent[0]) != 'undefined' && window.parent.mw.tools.hasParentsWithClass(module_has_editable_parent[0],'edit')){
 				      $(holder).append(html);
 
 					$(dd).prepend(btn);
-		
+
 					is_module_tml_holder.append(holder);
-		
+
 				   // parent.mw.load_module("admin/modules/saved_templates", '#module-modal-settings-menu-items');
 					mw.dropdown(toolbar);
 			}
-			
-			
-      
+
+
+
 
         }
-		
+
         <?php endif; ?>
 }
 
@@ -261,7 +261,7 @@ if (typeof thismodal.main[0] != 'undefined') {
 if (typeof thismodal.main[0] != 'undefined') {
 
             if (autoSize) {
- 
+
                 parent.mw.tools.modal.resize("#" + thismodal.main[0].id, false, $('#settings-container').height() + 25, true);
 
                 $(mwd.body).bind('mouseup click DOMNodeInserted',function () {
@@ -312,30 +312,70 @@ if (typeof thismodal.main[0] != 'undefined') {
 //var_dump($params);
 
 ?>
+
+</head>
+<body class="mw-external-loading loading">
+<div id="settings-main">
+  <div id="settings-container">
+    <div class="mw-module-live-edit-settings <?php print $params['id'] ?>"
+             id="module-id-<?php print $params['id'] ?>">{content} </div>
+  </div>
+</div>
+<form method="get" id="mw_reload_this_module_popup_form" style="display:none">
+  <?php $mpar = $params;
+    if (isset($mpar['module_settings'])) {
+        unset($mpar['module_settings']);
+    }
+
+    ?>
+  <?php if (is_array($params)): ?>
+  <?php foreach ($params as $k => $item): ?>
+  <input type="text" name="<?php print $k ?>" value="<?php print $item ?>"/>
+  <?php endforeach; ?>
+  <input type="submit" />
+  <?php endif; ?>
+</form>
+<script type="text/javascript">
+    $(document).ready(function () {
+
+        __global_options_save_msg = function () {
+            if (mw.notification != undefined) {
+                mw.notification.success('Settings are updated!');
+            }
+
+            if (window.parent.mw != undefined && window.parent.mw.reload_module != undefined) {
+                window.parent.mw.reload_module("#<?php print $params['id'] ?>");
+            }
+
+        }
+
+        // mw.options.form('#settings-container');
+    });
+</script>
 <script type="text/javascript">
 
 
 function mw_option_save_rebind_form_fields(){
-	
-	
 
 
-		mw.$(".mw_option_field").addClass('mw-options-form-binded'); 
 
 
-        
-        mw.$(".mw_option_field").bind("change", function (e) {
-			
+
+
+
+
+      mw.$(".mw_option_field").not('.mw-options-form-binded-custom').not('.mw-options-form-binded').bind("change", function (e) {
+
 			if($(this).hasClass('mw-options-form-binded-custom')){
 			return;
 			}
-			
-		 
-			
-			
-			
-			
-			
+
+     	$(this).addClass('mw-options-form-binded');
+ 
+
+
+
+
             if (mw.notification != undefined) {
                 mw.notification.success('<?php _e('Settings are saved') ?>');
             }
@@ -409,14 +449,14 @@ function mw_option_save_rebind_form_fields(){
 						 if(typeof(top.mweditor) != 'undefined'  && typeof(top.mweditor) == 'object'   && typeof(top.mweditor.contentWindow) != 'undefined'){
 							 top.mweditor.contentWindow.mw.reload_module('#<?php print $params['id'] ?>')
 							}
-						 
+
 						 }
                         if (window.parent.mw != undefined) {
-							
+
 							if(self !== top){
 								mw.reload_module_parent('#<?php print $params['id'] ?>');
 							}
-							
+
                             if (window.parent.mw.reload_module != undefined) {
                                 if(!!mw.admin){
                                   window.parent.mw.reload_module('#<?php print $params['id'] ?>');
@@ -450,7 +490,7 @@ function mw_option_save_rebind_form_fields(){
 
 
                             if (curm == also_reload) {
-								
+
 								// window.mw.reload_module(also_reload);
 								 window.location.href = window.location.href;
                                 //$('#mw_reload_this_module_popup_form').submit();
@@ -460,11 +500,11 @@ function mw_option_save_rebind_form_fields(){
 
 
                                     window.mw.reload_module(also_reload, function(){
-										
-										 mw_option_save_rebind_form_fields()	
+
+										 mw_option_save_rebind_form_fields()
 									});
-								
-									
+
+
 									if(self !== top){
 										mw.reload_module_parent(also_reload);
 									}
@@ -482,8 +522,8 @@ function mw_option_save_rebind_form_fields(){
             });
 
         });
-    
-	
+
+
 }
 
 
@@ -491,52 +531,12 @@ function mw_option_save_rebind_form_fields(){
 
 
     $(document).ready(function () {
-		
-		
-		
-	mw_option_save_rebind_form_fields()	
-		
+
+
+
+	mw_option_save_rebind_form_fields()
+
 	});
 </script>
-</head>
-<body class="mw-external-loading loading">
-<div id="settings-main">
-  <div id="settings-container">
-    <div class="mw-module-live-edit-settings <?php print $params['id'] ?>"
-             id="module-id-<?php print $params['id'] ?>">{content} </div>
-  </div>
-</div>
-<form method="get" id="mw_reload_this_module_popup_form" style="display:none">
-  <?php $mpar = $params;
-    if (isset($mpar['module_settings'])) {
-        unset($mpar['module_settings']);
-    }
-
-    ?>
-  <?php if (is_array($params)): ?>
-  <?php foreach ($params as $k => $item): ?>
-  <input type="text" name="<?php print $k ?>" value="<?php print $item ?>"/>
-  <?php endforeach; ?>
-  <input type="submit" />
-  <?php endif; ?>
-</form>
-<script type="text/javascript">
-    $(document).ready(function () {
-
-        __global_options_save_msg = function () {
-            if (mw.notification != undefined) {
-                mw.notification.success('Settings are updated!');
-            }
-
-            if (window.parent.mw != undefined && window.parent.mw.reload_module != undefined) {
-                window.parent.mw.reload_module("#<?php print $params['id'] ?>");
-            }
-
-        }
-
-        // mw.options.form('#settings-container');
-    });
-</script>
- 
 </body>
 </html>
