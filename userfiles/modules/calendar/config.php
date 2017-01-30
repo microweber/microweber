72290b9b1130094e25fd1c198549830bb5a16558
nickwuk <nick@estores.org.uk>
2017-01-30 13:35:33 +0000

    Calendar admin & config updates
    admin.php: time options am/pm
    config.php: fields types

--- userfiles/modules/calendar/config.php
+++ userfiles/modules/calendar/config.php
@@ -7,16 +7,15 @@ $config['ui_admin'] = true; //if set to true, module will be visible in the admi
 $config['categories'] = "content";
 $config['position'] = 99;
 $config['version'] = 0.1;
- 
+
 $config['tables'] = array(
     "calendar" => array(
-    '$id' => "integer",
-     'content_id' => "integer",
-
-    'title' => "text",
-		'startdate' => "text",
-		'enddate' => "text",
-		'allDay' => "text",
+        '$id' => "integer",
+        'content_id' => "integer",
+        'title' => "string",
+		'startdate' => "char",
+		'enddate' => "char",
+		'allDay' => "string",
 		'description' => "text",
     )
 );
