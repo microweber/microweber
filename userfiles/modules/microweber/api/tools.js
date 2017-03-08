819b1bdc5f8f9419362ca2bb443e0977bccbb752
Peter Ivanov <peter@microweber.com>
2017-03-08 19:21:49 +0200

    fix for mod_security
    error triggered by cookie having the value as json

--- userfiles/modules/microweber/api/tools.js
+++ userfiles/modules/microweber/api/tools.js
@@ -94,6 +94,30 @@ mw.external_tool = function (url) {
     return !url.contains("/") ? mw.settings.site_url + "editor_tools/" + url : url;
 }
 
+// Polyfill for escape/unescape
+if( !window.unescape ){
+    window.unescape = function( s ){
+        return s.replace( /%([0-9A-F]{2})/g, function( m, p ) {
+            return String.fromCharCode( '0x' + p );
+        } );
+    };
+}
+if( !window.escape ){
+    window.escape = function( s ){
+        var chr, hex, i = 0, l = s.length, out = '';
+        for( ; i < l; i ++ ){
+            chr = s.charAt( i );
+            if( chr.search( /[A-Za-z0-9\@\*\_\+\-\.\/]/ ) > -1 ){
+                out += chr; continue; }
+            hex = s.charCodeAt( i ).toString( 16 );
+            out += '%' + ( hex.length % 2 != 0 ? '0' : '' ) + hex;
+        }
+        return out;
+    };
+}
+
+
+
 mw.tools = {
     isEditable:function(item){
         var el = item;
@@ -3401,7 +3425,144 @@ mw.wait('jQuery', function () {
     };
 });
 
+mw.tools.base64 = {
 
+// private property
+    _keyStr : "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",
+
+// public method for encoding
+    encode : function (input) {
+        var output = "";
+        var chr1, chr2, chr3, enc1, enc2, enc3, enc4;
+        var i = 0;
+
+        input = mw.tools.base64._utf8_encode(input);
+
+        while (i < input.length) {
+
+            chr1 = input.charCodeAt(i++);
+            chr2 = input.charCodeAt(i++);
+            chr3 = input.charCodeAt(i++);
+
+            enc1 = chr1 >> 2;
+            enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);
+            enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);
+            enc4 = chr3 & 63;
+
+            if (isNaN(chr2)) {
+                enc3 = enc4 = 64;
+            } else if (isNaN(chr3)) {
+                enc4 = 64;
+            }
+
+            output = output +
+                this._keyStr.charAt(enc1) + this._keyStr.charAt(enc2) +
+                this._keyStr.charAt(enc3) + this._keyStr.charAt(enc4);
+
+        }
+
+        return output;
+    },
+
+// public method for decoding
+    decode : function (input) {
+        if(typeof input == 'undefined'){
+            return;
+        }
+        var output = "";
+        var chr1, chr2, chr3;
+        var enc1, enc2, enc3, enc4;
+        var i = 0;
+
+        input = input.replace(/[^A-Za-z0-9\+\/\=]/g, "");
+
+        while (i < input.length) {
+
+            enc1 = this._keyStr.indexOf(input.charAt(i++));
+            enc2 = this._keyStr.indexOf(input.charAt(i++));
+            enc3 = this._keyStr.indexOf(input.charAt(i++));
+            enc4 = this._keyStr.indexOf(input.charAt(i++));
+
+            chr1 = (enc1 << 2) | (enc2 >> 4);
+            chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
+            chr3 = ((enc3 & 3) << 6) | enc4;
+
+            output = output + String.fromCharCode(chr1);
+
+            if (enc3 != 64) {
+                output = output + String.fromCharCode(chr2);
+            }
+            if (enc4 != 64) {
+                output = output + String.fromCharCode(chr3);
+            }
+
+        }
+
+        output = mw.tools.base64._utf8_decode(output);
+
+        return output;
+
+    },
+
+// private method for UTF-8 encoding
+    _utf8_encode : function (string) {
+        string = string.replace(/\r\n/g,"\n");
+        var utftext = "";
+
+        for (var n = 0; n < string.length; n++) {
+
+            var c = string.charCodeAt(n);
+
+            if (c < 128) {
+                utftext += String.fromCharCode(c);
+            }
+            else if((c > 127) && (c < 2048)) {
+                utftext += String.fromCharCode((c >> 6) | 192);
+                utftext += String.fromCharCode((c & 63) | 128);
+            }
+            else {
+                utftext += String.fromCharCode((c >> 12) | 224);
+                utftext += String.fromCharCode(((c >> 6) & 63) | 128);
+                utftext += String.fromCharCode((c & 63) | 128);
+            }
+
+        }
+
+        return utftext;
+    },
+
+// private method for UTF-8 decoding
+    _utf8_decode : function (utftext) {
+        var string = "";
+        var i = 0;
+        var c = c1 = c2 = 0;
+
+        while ( i < utftext.length ) {
+
+            c = utftext.charCodeAt(i);
+
+            if (c < 128) {
+                string += String.fromCharCode(c);
+                i++;
+            }
+            else if((c > 191) && (c < 224)) {
+                c2 = utftext.charCodeAt(i+1);
+                string += String.fromCharCode(((c & 31) << 6) | (c2 & 63));
+                i += 2;
+            }
+            else {
+                c2 = utftext.charCodeAt(i+1);
+                c3 = utftext.charCodeAt(i+2);
+                string += String.fromCharCode(((c & 15) << 12) | ((c2 & 63) << 6) | (c3 & 63));
+                i += 3;
+            }
+
+        }
+
+        return string;
+    }
+
+}
 mw.cookie = {
     get: function (name) {
         var cookies = mwd.cookie.split(";"), i = 0, l = cookies.length;
@@ -3424,16 +3585,23 @@ mw.cookie = {
         var expires_date = new Date(now.getTime() + (expires));
         document.cookie = name + "=" + escape(value) + ( ( expires ) ? ";expires=" + expires_date.toGMTString() : "" ) + ( ( path ) ? ";path=" + path : ";path=/" ) + ( ( domain ) ? ";domain=" + domain : "" ) + ( ( secure ) ? ";secure" : "" );
     },
-    setBase64:function(name, value, expires, path, domain, secure){
-        value = btoa(value);
+    setEncoded:function(name, value, expires, path, domain, secure){
+       // value = encodeURIComponent(value);
+       // value = escape(value);
+        //value = mw.tools.base64.encode( unescape( encodeURIComponent( value ) ) )
+        value = mw.tools.base64.encode(  value )
         return this.set(name, value, expires, path, domain, secure)
     },
-    getBase64:function(name){
-        value = this.get(name);
-        return atob(value);
+    getEncoded:function(name){
+        var value = this.get(name);
+       // value = decodeURIComponent(value);
+        //value = unescape(value);
+        //value = decodeURIComponent( escape( mw.tools.base64.decode( value ) ) )
+        value = mw.tools.base64.decode( value  )
+        return value;
     },
     ui: function (a, b) {
-        var mwui = mw.cookie.get("mwui");
+        var mwui = mw.cookie.getEncoded("mwui");
         var mwui = (!mwui || mwui == '') ? {} : $.parseJSON(mwui);
         if (typeof a === 'undefined') {
             return mwui;
@@ -3444,7 +3612,7 @@ mw.cookie = {
         else {
             mwui[a] = b;
             var tostring = JSON.stringify(mwui);
-            mw.cookie.set("mwui", tostring, false, "/");
+            mw.cookie.setEncoded("mwui", tostring, false, "/");
             if (typeof mw.cookie.uievents[a] !== 'undefined') {
                 var funcs = mw.cookie.uievents[a], l = funcs.length, i = 0;
                 for (; i < l; i++) {
@@ -3469,7 +3637,7 @@ mw.cookie = {
 
 mw.recommend = {
     get: function () {
-        var cookie = mw.cookie.get("recommend");
+        var cookie = mw.cookie.getEncoded("recommend");
         if (!cookie) {
             return {}
         }
@@ -3487,7 +3655,7 @@ mw.recommend = {
             json[item_name] += 1;
         }
         var tostring = JSON.stringify(json);
-        mw.cookie.set("recommend", tostring, false, "/");
+        mw.cookie.setEncoded("recommend", tostring, false, "/");
     },
     orderRecommendObject: function () {
         var obj = mw.recommend.get();
