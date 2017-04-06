2376936ce96c19c3608decbd99e410fe57f3a53a
Petko Yovchevski <pyovchevski@gmail.com>
2017-04-06 12:33:33 +0300

    fix before after module

--- userfiles/modules/beforeafter/admin.php
+++ userfiles/modules/beforeafter/admin.php
@@ -1,55 +1,52 @@
 <?php only_admin_access(); ?>
 
-<style>
-
-</style>
-
 <?php
-
-    $before =  get_option('before', $params['id']);
-    $after =  get_option('after', $params['id']);
-
-
-
+$before = get_option('before', $params['id']);
+$after = get_option('after', $params['id']);
 ?>
+
 <div class="module-live-edit-settings">
 
+    <div class="mw-ui-box mw-ui-box-content">
+        <label class="mw-ui-label"><?php _e('Upload 2 images'); ?></label>
+        <div class="mw-ui-field-holder">
+            <span class="mw-ui-btn" id="before"><span class="mw-icon-upload"></span><?php _e('Before'); ?></span>
+            vs.
+            <span class="mw-ui-btn" id="after"><span class="mw-icon-upload"></span><?php _e('After'); ?></span>
+        </div>
+    </div>
 
 
-<div class="mw-ui-box mw-ui-box-content">
-<label class="mw-ui-label"><?php _e('Upload 2 images'); ?></label>
-<div class="mw-ui-field-holder">
-  <span class="mw-ui-btn" id="before"><span class="mw-icon-upload"></span><?php _e('Before'); ?></span>
-      vs.
-  <span class="mw-ui-btn" id="after"><span class="mw-icon-upload"></span><?php _e('After'); ?></span>
-</div>
-</div>
-
-
-
-<input type="hidden" class="mw_option_field" name="before" id="beforeval" value="<?php print $before; ?>" />
-<input type="hidden" class="mw_option_field" name="after" id="afterval" value="<?php print $after; ?>" />
+    <input type="hidden" class="mw_option_field" name="before" id="beforeval" value="<?php print $before; ?>"/>
+    <input type="hidden" class="mw_option_field" name="after" id="afterval" value="<?php print $after; ?>"/>
 
 </div>
 
 <script>
+    $(document).ready(function () {
+        var before = mw.uploader({
+            filetypes: "images,videos",
+            multiple: false,
+            element: "#before"
+        });
+        $(before).bind('FileUploaded', function (a, b) {
+            preload_image(b.src)
 
-$(document).ready(function(){
-    var before = mw.uploader({
-          filetypes:"images,videos",
-          multiple:false,
-          element:"#before"
+            mw.$("#beforeval").val(b.src).trigger('change');
+        });
+        var after = mw.uploader({
+            filetypes: "images,videos",
+            multiple: false,
+            element: "#after"
+        });
+        $(after).bind('FileUploaded', function (a, b) {
+            preload_image(b.src)
+            mw.$("#afterval").val(b.src).trigger('change');
+        });
     });
-    $(before).bind('FileUploaded', function(a,b){
-        mw.$("#beforeval").val(b.src).trigger('change');
-    });
-    var after = mw.uploader({
-          filetypes:"images,videos",
-          multiple:false,
-          element:"#after"
-    });
-    $(after).bind('FileUploaded', function(a,b){
-        mw.$("#afterval").val(b.src).trigger('change');
-    });
-});
+
+    preload_image = function (src) {
+        var elem = document.createElement("img");
+        elem.setAttribute("src", src);
+    }
 </script>
--- userfiles/modules/beforeafter/index.php
+++ userfiles/modules/beforeafter/index.php
@@ -5,32 +5,30 @@ $after = get_option('after', $params['id']);
 $rand = $params['id'];
 
 if ($before == false) {
-    $before = 'http://www.bmw.bg/content/dam/bmw/common/all-models/1-series/5-door/2015/model-card/BMW-1-Series%205-door_Modelcard.png';
-    }
+    $before = module_url() . 'img/white-car.jpg';
+}
 
 if ($after == false) {
-    $after = 'http://www.bmw.bg/content/dam/bmw/common/all-models/1-series/5-door/2015/model-card/BMW-1-Series%205-door_Modelcard.png';
-    }
-
-
+    $after = module_url() . 'img/blue-car.jpg';
+}
 ?>
 <script>
-
-
     mw.moduleCSS('<?php print module_url(); ?>css/twentytwenty.css');
     mw.moduleJS('<?php print module_url(); ?>js/jquery.event.move.js');
     mw.moduleJS('<?php print module_url(); ?>js/jquery.twentytwenty.js');
 
     $(document).ready(function () {
-        mw.$("#mw-before-after-<?php print $rand; ?>").twentytwenty({default_offset_pct: 0});
+        mw.$("#mw-before-after-<?php print $rand; ?>").twentytwenty({default_offset_pct: 0.5});
     });
-
 </script>
 
 <div class="mw-before-after" id="mw-before-after-<?php print $params['id']; ?>">
-    <style scoped="scoped">.twentytwenty-overlay {
-            display: none !important;
-        } </style>
-    <img src="<?php print $before; ?>"/>
-    <img src="<?php print $after; ?>"/>
+
+    <style scoped="scoped">
+        .twentytwenty-container {
+            min-height: 100px !important;
+        }
+    </style>
+    <img src="<?php print $before; ?>" alt="<?php _e('before image'); ?>"/>
+    <img src="<?php print $after; ?>" alt="<?php _e('after image'); ?>"/>
 </div>
