f3e28604b92f78d6ec7d4367f8222e0f1e0a5ef9
Petko Yovchevski <pyovchevski@gmail.com>
2017-04-10 16:27:13 +0300

    UI Bugs fix

--- userfiles/modules/btn/templates/default.php
+++ userfiles/modules/btn/templates/default.php
@@ -1,3 +1,8 @@
 <a id="<?php print $btn_id ?>" href="<?php print $url; ?>" <?php if ($blank) {
     print ' target="_blank" ';
-} ?> class="btn <?php print $style . ' ' . $size; ?>"><?php print $text; ?></a>
+} ?> class="btn <?php print $style . ' ' . $size; ?>">
+    <?php if($icon): ?>
+        <span class="<?php print $icon; ?>"></span>
+    <?php endif; ?>
+    <?php print $text; ?>
+</a>
