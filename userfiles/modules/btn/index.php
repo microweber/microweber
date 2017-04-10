95236048405168918ad1c7c1cbf373bc867b9ee7
Petko Yovchevski <pyovchevski@gmail.com>
2017-04-10 16:22:55 +0300

    UI Bugs fix

--- userfiles/modules/btn/index.php
+++ userfiles/modules/btn/index.php
@@ -1,54 +1,88 @@
 <?php
+$btn_id = 'btn-' . $params['id'];
+$style = get_option('button_style', $params['id']);
+$size = get_option('button_size', $params['id']);
+$action = get_option('button_action', $params['id']);
+$action_content = get_option('popupcontent', $params['id']);
+$url = get_option('url', $params['id']);
+$blank = get_option('url_blank', $params['id']) == 'y';
+$text = get_option('text', $params['id']);
+$icon = get_option('icon', $params['id']);
+$popup_function_id = 'btn_popup'.uniqid();
+if ($text == '') {
+    $text = 'Button';
+}
+if ($style == '') {
+    $style = 'btn-default';
+}
 
-    $style =  get_option('button_style', $params['id']);
-    $size =  get_option('button_size', $params['id']);
-    $action =  get_option('button_action', $params['id']);
-    $action_content =  get_option('popupcontent', $params['id']);
-    $url =  get_option('url', $params['id']);
-    $blank =   get_option('url_blank', $params['id']);
-    $text =  get_option('text', $params['id']);
-    $icon =  get_option('icon', $params['id']);
+if ($size == false and isset($params['button_size'])) {
+    $size = $params['button_size'];
 
-     if ($text == ''){ $text = 'Button';}
-     if ($style == ''){ $style = 'btn-default';}
+}
 
-     if($size == false and isset($params['button_size'])){
-        $size = $params['button_size'];
+if ($text == false and isset($params['text'])) {
+    $text = $params['text'];
+}
 
-     }
+if ($style == false and isset($params['button_style'])) {
+    $style = $params['button_style'];
+}
 
+if ($action == 'popup') {
+    $url ='javascript:'.$popup_function_id.'()';
+}
 ?>
-<?php if($action=='url' or $action==''){ ?>
 
-<a href="<?php print $url; ?>" <?php if($blank=='y'){print ' target="_blank" ';} ?> class="btn <?php print $style. ' '. $size; ?>"><?php print $text; ?></a>
-<?php } else if($action=='popup') { ?>
+    <script>
+        mw.require('tools.js', true);
+        mw.require('ui.css', true);
+    </script>
+
+
 <?php
 
-     $rand = uniqid();
+$module_template = get_option('data-template', $params['id']);
+if ($module_template == false and isset($params['template'])) {
+    $module_template = $params['template'];
+}
+if ($module_template != false) {
+    $template_file = module_templates($config['module'], $module_template);
+} else {
+    $template_file = module_templates($config['module'], 'default');
+}
 
-     ?>
-<script>
-  mw.require('tools.js', true);
-  mw.require('ui.css', true);
-</script>
-<a href="javascript:;" id="btn<?php print $rand; ?>" class="btn <?php print $style. ' '. $size; ?>"><?php print $text; ?></a>
 
-<script type="text/microweber" id="area<?php print $rand; ?>">
+if (is_file($template_file) != false) {
+    include($template_file);
+} else {
+    print lnotif("No template found. Please choose template.");
+}
+ ?>
+
+<?php if ($action == 'popup') { ?>
+
+
+
+
+
+    <script type="text/microweber" id="area<?php print $btn_id; ?>">
     <?php print $action_content; ?>
-</script>
-<script>
 
-$(document).ready(function(){
-  mw.$("#btn<?php print $rand; ?>").click(function(){
-      mw.modal({
-        name:'frame<?php print $rand; ?>',
-        html:$(mwd.getElementById('area<?php print $rand; ?>')).html(),
-        template:'basic',
-        title:"<?php print $text; ?>"
-      });
-  })
 
-});
 
-</script>
+    </script>
+    <script>
+
+        function <?php print $popup_function_id ?>() {
+            mw.modal({
+                name: 'frame<?php print $btn_id; ?>',
+                html: $(mwd.getElementById('area<?php print $btn_id; ?>')).html(),
+                template: 'basic',
+                title: "<?php print $text; ?>"
+            });
+        }
+
+
+    </script>
 <?php } ?>
