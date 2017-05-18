fea955fcb841db018a0668a7f156b43521d40a11
Petko Yovchevski <pyovchevski@gmail.com>
2017-05-18 17:02:40 +0300

    updates

--- userfiles/modules/layouts/index.php
+++ userfiles/modules/layouts/index.php
@@ -1,28 +1,26 @@
 <script>
     mw.lib.require('bootstrap3ns');
+    mw.require('https://fonts.googleapis.com/icon?family=Material+Icons&.css', 'material_icons');
 </script>
-<link rel="stylesheet" href="<?php print $config['url_to_module'] ?>/templates/layouts.css" type="text/css" media="all">
-
 
 <div class="bootstrap3ns">
-    <?php
+    <div class="edit">
+        <?php
+        $template = get_option('data-template', $params['id']);
+        if ($template == false and isset($params['template'])) {
+            $template = $params['template'];
+        }
 
-
-    $template = get_option('data-template', $params['id']);
-    if ($template == false and isset($params['template'])) {
-        $template = $params['template'];
-    }
-
-    $template_file = false;
-    if ($template != false and strtolower($template) != 'none') {
-        $template_file = module_templates($config['module'], $template);
-    }
-    if ($template_file == false) {
-        $template_file = module_templates($config['module'], 'default');
-    }
-    if ($template_file != false and is_file($template_file)) {
-        include($template_file);
-    }
-
-    ?>
+        $template_file = false;
+        if ($template != false and strtolower($template) != 'none') {
+            $template_file = module_templates($config['module'], $template);
+        }
+        if ($template_file == false) {
+            $template_file = module_templates($config['module'], 'default');
+        }
+        if ($template_file != false and is_file($template_file)) {
+            include($template_file);
+        }
+        ?>
+    </div>
 </div>
