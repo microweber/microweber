86aeff14c3bfaddacf72d04b9a059e3afef5690d
Petko Yovchevski <pyovchevski@gmail.com>
2017-08-30 18:13:53 +0300

    position fixed

--- userfiles/modules/bxslider/config.php
+++ userfiles/modules/bxslider/config.php
@@ -1,1 +1,1 @@
-<?php$config = array();$config['name'] = "bxSlider";$config['author'] = "Microweber";$config['varsion'] = "0.1";$config['no_cache'] = true;$config['ui'] = true;$config['ui_admin'] = false;
+<?php$config = array();$config['name'] = "bxSlider";$config['author'] = "Microweber";$config['version'] = "0.1";$config['no_cache'] = true;$config['ui'] = true;$config['ui_admin'] = false;$config['position'] = 12;
