396165aa0d456ffe0bc0400d041062c136b7664d
nickwuk <nick@estores.org.uk>
2017-09-04 13:39:18 +0100

    Viewport meta tag added to Liteness and default templates

--- userfiles/templates/liteness/header.php
+++ userfiles/templates/liteness/header.php
@@ -4,6 +4,7 @@
     <title>{content_meta_title}</title>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta property="og:title" content="{content_meta_title}" />
     <meta name="keywords" content="{content_meta_keywords}" />
     <meta name="description" content="{content_meta_description}" />
