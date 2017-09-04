a4440f0f95183657f62b54f9852bb03721895432
nickwuk <nick@estores.org.uk>
2017-09-04 13:04:20 +0100

    Stripe payment gateway extended to work with Stripe's 'checkout' option.

--- userfiles/modules/shop/payments/gateways/omnipay_stripe/admin.php
+++ userfiles/modules/shop/payments/gateways/omnipay_stripe/admin.php
@@ -1,11 +1,39 @@
 <?php only_admin_access(); ?>
 
 
-<label class="mw-ui-label">Api Key: </label>
-
+<label class="mw-ui-label">Public Api Key: </label>
 
 <input type="text" class="mw-ui-field mw_option_field" name="stripe_api_key"
        placeholder="" data-option-group="payments"
        value="<?php print get_option('stripe_api_key', 'payments'); ?>">
 
 
+<label class="mw-ui-label">Secret Api Key: </label>
+
+<input type="text" class="mw-ui-field mw_option_field" name="stripe_secret_api_key"
+       placeholder="" data-option-group="payments"
+       value="<?php print get_option('stripe_secret_api_key', 'payments'); ?>">
+
+    <ul class="mw-ui-inline-list">
+        <li><label class="mw-ui-label"><?php _e("Checkout or Direct"); ?>:</label></li>
+
+        <li><label class="mw-ui-check">
+                <input name="stripe_checkout" class="mw_option_field"    data-option-group="payments"  value="y"  type="radio"  <?php if(get_option('stripe_checkout', 'payments') == 'y'): ?> checked="checked" <?php endif; ?> >
+                <span></span><span><?php _e("Yes"); ?></span></label></li>
+
+        <li><label class="mw-ui-check">
+                <input name="stripe_checkout" class="mw_option_field"    data-option-group="payments"  value="n"  type="radio"  <?php if(get_option('stripe_checkout', 'payments') != 'y'): ?> checked="checked" <?php endif; ?> >
+                <span></span><span><?php _e("No"); ?></span></label></li>
+    </ul>
+
+    <ul class="mw-ui-inline-list">
+        <li><label class="mw-ui-label"><?php _e("Test mode"); ?>:</label></li>
+
+        <li><label class="mw-ui-check">
+                <input name="stripe_testmode" class="mw_option_field"    data-option-group="payments"  value="y"  type="radio"  <?php if(get_option('stripe_testmode', 'payments') == 'y'): ?> checked="checked" <?php endif; ?> >
+                <span></span><span><?php _e("Yes"); ?></span></label></li>
+
+        <li><label class="mw-ui-check">
+                <input name="stripe_testmode" class="mw_option_field"    data-option-group="payments"  value="n"  type="radio"  <?php if(get_option('stripe_testmode', 'payments') != 'y'): ?> checked="checked" <?php endif; ?> >
+                <span></span><span><?php _e("No"); ?></span></label></li>
+    </ul>
--- userfiles/modules/shop/payments/gateways/omnipay_stripe/index.php
+++ userfiles/modules/shop/payments/gateways/omnipay_stripe/index.php
@@ -1,3 +1,16 @@
-<?php 
+<?php
 
-include(dirname(__DIR__).DS.'lib'.DS.'omnipay'.DS.'cc_form_fields.php'); 
+$stripe_is_checkout = (get_option('stripe_checkout', 'payments')) == 'y';
+
+if($stripe_is_checkout == 'y') {
+?>
+	<div>
+		<p class="alert alert-info"><small><strong> *<?php _e("Note"); ?> </strong>Payment details will be taken via the Stripe Checkout</small> </p>
+	</div>
+
+<?php
+} else {
+
+	include(dirname(__DIR__).DS.'lib'.DS.'omnipay'.DS.'cc_form_fields.php');
+
+}
