bdc2ffdfe982b2ab24f0eafc8ed6b00d8b299d6d
nickwuk <nick@estores.org.uk>
2017-10-12 21:17:08 +0100

    update

--- userfiles/templates/default/header.php
+++ userfiles/templates/default/header.php
@@ -6,6 +6,7 @@
 
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta property="og:title" content="{content_meta_title}">
     <meta name="keywords" content="{content_meta_keywords}">
     <meta name="description" content="{content_meta_description}">
