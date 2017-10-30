9486fc39edfc8fd3246549bf1bdf9dd5b82dbafd
Petko Yovchevski <pyovchevski@gmail.com>
2017-10-30 18:18:50 +0200

    fix

--- userfiles/modules/captcha/templates/default.php
+++ userfiles/modules/captcha/templates/default.php
@@ -27,7 +27,7 @@ description: Default comments template
             <div class="form-group">
                 <div class="captcha-holder">
                     <div class="col-xs-4">
-                        <a href="javascript:;" class="tip" data-tip="Refresh captcha" data-tipposition="top-center"><img onclick="mw.tools.refresh_image(this);" class="mw-captcha-img" id="captcha-<?php print $form_id; ?>" src="<?php print api_link('captcha') ?>"/></a>
+                        <a href="javascript:;" class="tip" data-tip="Refresh captcha" data-tipposition="top-center"><img onclick="mw.tools.refresh_image(this);" class="mw-captcha-img" id="captcha-<?php print $form_id; ?>" src="<?php print api_link('captcha') ?>?w=100&h=20"/></a>
                     </div>
                     <div class="col-xs-6">
                         <input name="captcha" type="text" required class="mw-captcha-input form-control" placeholder="<?php _e("Security code"); ?>"/>
