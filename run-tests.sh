5a2fce3cff3a93cfaf0abd3eef61b14173118c4b
Brightside56 <support@4play.by>
2017-11-02 00:55:34 +0300

    fixes

--- run-tests.sh
+++ run-tests.sh
@@ -2,6 +2,8 @@
 
 set -o errexit
 
+echo ${pwd}
+
 docker-compose build && docker-compose up -d
-docker-compose exec -T fpm composer install
+docker-compose exec -T fpm composer install --dev
 docker-compose exec -T fpm vendor/bin/phpunit
