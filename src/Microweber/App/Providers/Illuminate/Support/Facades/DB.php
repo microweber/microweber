232048f9e906ea2111d6f861eceeb208c0c70aa5
Peter Ivanov <peter@microweber.com>
2017-11-06 22:09:36 +0200

    wrapping laravel classes
    for updates compatibility if there is change of bindings in app.php

---
+++ src/Microweber/App/Providers/Illuminate/Support/Facades/DB.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Microweber\App\Providers\Illuminate\Support\Facades;
+
+class DB extends \Illuminate\Support\Facades\DB
+{
+
+}
---
+++ src/Microweber/App/Providers/Illuminate/Support/Facades/Redis.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Microweber\App\Providers\Illuminate\Support\Facades;
+
+class Redis extends \Illuminate\Support\Facades\Redis
+{
+
+}
