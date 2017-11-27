cbfbda6a358a1ba9c82f018bd20fe0a5264ecd83
Petko Yovchevski <pyovchevski@gmail.com>
2017-11-27 17:30:39 +0200

    updates

--- userfiles/modules/video/index.php
+++ userfiles/modules/video/index.php
@@ -46,37 +46,19 @@ if ($autoplay == '') {
 }
  
 
-if ($prior != '2' or $prior == false) {
-    if ($code != '') {
-        $code = trim($code);
-        if (stristr($code, '<iframe') !== false) {
-            $code = preg_replace('#\<iframe(.*?)\ssrc\=\"(.*?)\"(.*?)\>#i',
-                '<iframe$1 src="$2?wmode=transparent"$3>', $code);
-            //print '<div class="mwembed">' . $code . '</div>';
-        }
 
 
-        if (video_module_is_embed($code) == true) {
-            print '<div class="mwembed">' . $code . '</div>';
-        } else {
-            print video_module_url2embed($code, $w, $h, $autoplay);
-        }
-    } else {
-        print lnotif("<div class='video-module-default-view mw-open-module-settings'><img src='" . $config['url_to_module'] . "video.png' /></div>", true);
-    }
-} else if ($prior == '2') {
-    if ($upload != '') {
-        if ($autoplay == '0') {
-            $autoplay = '';
-        } else {
-            $autoplay = 'autoplay';
-        }
-        print '<div class="mwembed "><video controls width="' . $w . '" height="' . $h . '" ' . $autoplay . ' src="' . $upload . '"></video></div>';
-    } else {
-        print ("<div class='video-module-default-view mw-open-module-settings'><img src='" . $config['url_to_module'] . "video.png' /></div>");
+$module_template = get_option('data-template', $params['id']);
 
-    }
-} else {
-    print "<div class='video-module-default-view mw-open-module-settings'><img src='" . $config['url_to_module'] . "video.png' /></div>";
+
+if ($module_template == false and isset($params['template'])) {
+    $module_template = $params['template'];
 }
- 
+if ($module_template != false) {
+    $template_file = module_templates($config['module'], $module_template);
+} else {
+    $template_file = module_templates($config['module'], 'default');
+}
+if (is_file($template_file)) {
+    include($template_file);
+}
