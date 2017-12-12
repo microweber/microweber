c49b8a34da6698953d3c72fbb022509252a2bd33
Petko Yovchevski <pyovchevski@gmail.com>
2017-12-12 14:38:35 +0200

    updates

--- userfiles/modules/embed/admin.php
+++ userfiles/modules/embed/admin.php
@@ -1,11 +1,20 @@
 <?php
 only_admin_access();
 
-$rand_id = md5(serialize($params)); ?>
+$rand_id = md5(serialize($params));
+$hide_in_live_edit = get_option('hide_in_live_edit', $params['id']);
+if($hide_in_live_edit != '' OR $hide_in_live_edit != false){
+    $hide_in_live_edit == true;
+}
+?>
 
 <div class="module-live-edit-settings">
-  <div id="mw_email_source_code_editor<?php print $rand_id ?>">
-      <label class="mw-ui-label"><?php _e("Insert Embed Code"); ?></label>
-      <textarea name="source_code" class="mw_option_field mw-ui-field w100" style="height:86px;" data-refresh="embed"><?php print get_option('source_code', $params['id']) ?></textarea>
-   </div>
- </div>
+    <div id="mw_email_source_code_editor<?php print $rand_id ?>">
+        <label class="mw-ui-label"><?php _e("Insert Embed Code"); ?></label>
+        <textarea name="source_code" class="mw_option_field mw-ui-field w100" style="height:86px;" data-refresh="embed"><?php print get_option('source_code', $params['id']) ?></textarea>
+
+        <hr/>
+        <label class="mw-ui-label"><?php _e("Hide in Live Edit"); ?></label>
+        <input type="checkbox" name="hide_in_live_edit" class="mw_option_field" value="true" data-refresh="embed" <?php if($hide_in_live_edit): ?>checked<?php endif; ?> /> Hide
+    </div>
+</div>
