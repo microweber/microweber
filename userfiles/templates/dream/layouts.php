a5869737ffe2df17c4085a66dadbecaab5e3bd11
Petko Yovchevski <pyovchevski@gmail.com>
2018-03-06 12:37:47 +0200

    updates

---
+++ userfiles/templates/dream/layouts.php
@@ -0,0 +1,91 @@
+<?php
+
+/*
+
+  type: layout
+  content_type: static
+  name: Layouts
+  position: 11
+  description: Home layout
+
+*/
+
+?>
+<?php include template_dir() . "header.php"; ?>
+
+<div class="edit" rel="content" field="dream_content">
+    <module type="layouts" template="skin-1"/>
+    <module type="layouts" template="skin-2"/>
+    <module type="layouts" template="skin-3"/>
+    <module type="layouts" template="skin-4"/>
+    <module type="layouts" template="skin-5"/>
+    <module type="layouts" template="skin-6"/>
+    <module type="layouts" template="skin-7"/>
+    <module type="layouts" template="skin-8"/>
+    <module type="layouts" template="skin-9"/>
+    <module type="layouts" template="skin-10"/>
+    <module type="layouts" template="skin-11"/>
+    <module type="layouts" template="skin-12"/>
+    <module type="layouts" template="skin-13"/>
+    <module type="layouts" template="skin-14"/>
+    <module type="layouts" template="skin-15"/>
+    <module type="layouts" template="skin-16"/>
+    <module type="layouts" template="skin-17"/>
+    <module type="layouts" template="skin-18"/>
+    <module type="layouts" template="skin-19"/>
+    <module type="layouts" template="skin-20"/>
+    <module type="layouts" template="skin-21"/>
+    <module type="layouts" template="skin-22"/>
+    <module type="layouts" template="skin-23"/>
+    <module type="layouts" template="skin-24"/>
+    <module type="layouts" template="skin-25"/>
+    <module type="layouts" template="skin-26"/>
+    <module type="layouts" template="skin-27"/>
+    <module type="layouts" template="skin-28"/>
+    <module type="layouts" template="skin-29"/>
+    <module type="layouts" template="skin-30"/>
+    <module type="layouts" template="skin-31"/>
+    <module type="layouts" template="skin-32"/>
+    <module type="layouts" template="skin-33"/>
+    <module type="layouts" template="skin-34"/>
+    <module type="layouts" template="skin-35"/>
+    <module type="layouts" template="skin-36"/>
+    <module type="layouts" template="skin-37"/>
+    <module type="layouts" template="skin-38"/>
+    <module type="layouts" template="skin-39"/>
+    <module type="layouts" template="skin-40"/>
+    <module type="layouts" template="skin-41"/>
+    <module type="layouts" template="skin-42"/>
+    <module type="layouts" template="skin-43"/>
+    <module type="layouts" template="skin-44"/>
+    <module type="layouts" template="skin-45"/>
+    <module type="layouts" template="skin-46"/>
+    <module type="layouts" template="skin-47"/>
+    <module type="layouts" template="skin-48"/>
+    <module type="layouts" template="skin-49"/>
+    <module type="layouts" template="skin-50"/>
+    <module type="layouts" template="skin-51"/>
+    <module type="layouts" template="skin-52"/>
+    <module type="layouts" template="skin-53"/>
+    <module type="layouts" template="skin-54"/>
+    <module type="layouts" template="skin-55"/>
+    <module type="layouts" template="skin-56"/>
+    <module type="layouts" template="skin-57"/>
+    <module type="layouts" template="skin-58"/>
+    <module type="layouts" template="skin-59"/>
+    <module type="layouts" template="skin-60"/>
+    <module type="layouts" template="skin-61"/>
+    <module type="layouts" template="skin-62"/>
+    <module type="layouts" template="skin-63"/>
+    <module type="layouts" template="skin-64"/>
+    <module type="layouts" template="skin-65"/>
+    <module type="layouts" template="skin-66"/>
+    <module type="layouts" template="skin-67"/>
+    <module type="layouts" template="skin-68"/>
+    <module type="layouts" template="skin-69"/>
+    <module type="layouts" template="skin-70"/>
+
+
+</div>
+
+<?php include template_dir() . "footer.php"; ?>
