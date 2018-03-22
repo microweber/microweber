fe5729da8f7c63fa9d34d450b55ba2a87b8d229a
Petko Yovchevski <pyovchevski@gmail.com>
2018-03-22 10:00:03 +0200

    updates

--- userfiles/modules/site_stats/src/views/parts/languages.php
+++ userfiles/modules/site_stats/src/views/parts/languages.php
@@ -1,1 +1,20 @@
-languages
+<?php
+if (!$data) {
+    return;
+}
+?>
+
+<?php foreach ($data as $item): ?>
+    <div class="item location">
+        <div class="location-progressbar" style="width: <?php print $item['sessions_percent']; ?>%;"></div>
+        <div class="mw-ui-row">
+            <div class="mw-ui-col">
+                <div class="title"><?php print $item['language']; ?></div>
+            </div>
+
+            <div class=" mw-ui-col" style="width:30px;">
+                <div class="cnt"><?php print $item['sessions_count']; ?></div>
+            </div>
+        </div>
+    </div>
+<?php endforeach; ?>
