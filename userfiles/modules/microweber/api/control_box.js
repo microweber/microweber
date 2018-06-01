fc543a2d8716691591523f11990a43f714f93fdf
alex <raikov@ymail.com>
2018-06-01 15:47:37 +0300

    fonts ready

--- userfiles/modules/microweber/api/control_box.js
+++ userfiles/modules/microweber/api/control_box.js
@@ -2,14 +2,25 @@ mw.controlBox = function(options){
     this.options = options;
     this.defaults = {
         position:'bottom',
-        content:''
+        content:'',
+        skin:'default'
     };
     this.settings = $.extend({}, this.defaults, this.options);
     this.active = false;
 
     this.build = function(){
         this.box = document.createElement('div');
-        this.box.className = 'mw-control-box mw-control-box-' + this.settings.position;
+        this.box.className = 'mw-control-box mw-control-box-' + this.settings.position + ' mw-control-box-' + this.settings.skin;
+        this.boxContent = document.createElement('div');
+        this.boxContent.className = 'mw-control-boxcontent';
+        this.box.appendChild(this.boxContent);
+        this.closeButton = document.createElement('span');
+        this.closeButton.className = 'mw-control-boxclose';
+        this.box.appendChild(this.closeButton);
+        var scope = this;
+        this.closeButton.onclick = function(){
+            scope.hide()
+        }
         document.body.appendChild(this.box)
     }
 
@@ -17,23 +28,29 @@ mw.controlBox = function(options){
         var cont = this.settings.content.trim();
         var scope = this;
         return $.get(cont, function(data){
-            scope.box.innerHTML = data;
+            scope.boxContent.innerHTML = data;
+            this.settings.content = data;
         });
     }
-    this.setContent = function(){
-        var cont = this.settings.content.trim();
+    this.setContent = function(c){
+        var cont = c||this.settings.content.trim();
+        this.settings.content = cont;
         if(cont.indexOf('http://') === 0 || cont.indexOf('https://') === 0){
             return this.setContentByUrl()
         }
-        this.box.innerHTML = cont;
+        this.boxContent.innerHTML = cont;
     }
 
-    this.activate = function(){
+    this.show = function(){
         this.active = true;
         $(this.box).addClass('active')
     }
 
-    this.deactivate = function(){
+    this.init = function(){
+        this.build();
+        this.setContent()
+    }
+    this.hide = function(){
         this.active = false;
         $(this.box).removeClass('active')
     }
@@ -41,6 +58,7 @@ mw.controlBox = function(options){
 
     this.toggle = function(){
         this.active = !this.active;
-        this[this.active?'activate':'deactivate']();
+        this[this.active?'hide':'show']();
     }
+    this.init()
 }
