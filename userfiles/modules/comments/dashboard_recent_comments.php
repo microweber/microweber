fe6ca83819cfcd519243a2afcd170c6c93f87068
Petko Yovchevski <pyovchevski@gmail.com>
2018-06-08 14:29:24 +0300

    updates

--- userfiles/modules/comments/dashboard_recent_comments.php
+++ userfiles/modules/comments/dashboard_recent_comments.php
@@ -17,7 +17,7 @@ $ccount = get_comments($comments_data);
 <div class="dashboard-recent">
     <div class="dr-head">
         <span class="drh-activity-name"><i class="mai-comment"></i> <?php _e("Last comments") ?></span>
-        <a href="<?php print admin_url('view:content/action:posts'); ?>" class="mw-ui-btn mw-ui-btn-medium mw-ui-btn-info"><strong><?php print $ccount; ?></strong> <?php print _e('New comments'); ?></a>
+        <a href="<?php print admin_url('view:modules/load_module:comments'); ?>" class="mw-ui-btn mw-ui-btn-medium mw-ui-btn-info"><strong><?php print $ccount; ?></strong> <?php print _e('New comments'); ?></a>
     </div>
     <div class="dr-list">
         <module type="comments/manage"  id="dashboard-recent-comments" limit="5" >
