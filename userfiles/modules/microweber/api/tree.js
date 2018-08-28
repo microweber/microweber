5d73e5570b3260cc1d58d0a912f47803e746d655
alex <raikov@ymail.com>
2018-08-28 14:54:28 +0300

    tree fixes

--- userfiles/modules/microweber/api/tree.js
+++ userfiles/modules/microweber/api/tree.js
@@ -28,6 +28,7 @@ mw.lib.require('nestedsortable');
         this.options.multiPageSelect =  this.options.multiPageSelect === undefined ? true : this.options.multiPageSelect;
         this.options.saveState = this.options.saveState === undefined ? true : this.options.saveState;
         this.options.sortable = this.options.sortable === undefined ? false : this.options.sortable;
+        this.options.nestedSortable = this.options.nestedSortable === undefined ? false : this.options.nestedSortable;
         this.options.singleSelect = this.options.singleSelect === undefined ? false : this.options.singleSelect;
 
 
@@ -39,9 +40,7 @@ mw.lib.require('nestedsortable');
             this.list.id = this.options.id;
             this.list.className = 'mw-defaults mw-tree-nav mw-tree-nav-skin-'+this.options.skin;
             this.list._id = 0;
-            console.log(this.options.data)
             this.options.data.forEach(function(item){
-                console.log(item)
                 var list = scope.getParent(item);
                 if(list){
                     list.appendChild(scope.createItem(item));
@@ -161,7 +160,6 @@ mw.lib.require('nestedsortable');
 
         this.openAll = function(){
             var all = this.list.querySelectorAll('li');
-            console.log(all)
             $(all).each(function(){
                 scope.open(this, undefined, true);
             });
@@ -255,6 +253,9 @@ mw.lib.require('nestedsortable');
             if(this.options.sortable){
                 this.sortable()
             }
+            if(this.options.nestedSortable){
+                this.nestedSortable()
+            }
 
         }
 
@@ -265,25 +266,28 @@ mw.lib.require('nestedsortable');
                 listType:'ul',
                 handle:'.mw-tree-item-title',
                 update:function(e, ui){
-                    var old = $.extend({},ui.item[0]._data);
-                    var obj = ui.item[0]._data;
-                    var objParent = ui.item[0].parentNode.parentNode._data;
-                    ui.item[0].dataset.parent_id = objParent.id
-                    console.log(ui.item[0].parentNode.parentNode)
-                    obj.parent_id = objParent.id
-                    obj.parent_type = objParent.type;
-                    var newdata = [];
-                    $('li', scope.list).each(function(){
-                        if(this._data) newdata.push(this._data)
-                    });
-                    scope.options.data = newdata;
-                    var local = [];
-                    $(ui.item[0].parentNode.querySelectorAll('li')).each(function(){
-                        local.push(this._data.id);
-                    });
-                    $(scope.list).remove();
-                    scope.init();
-                    $(scope).trigger('orderChange', [obj, scope.options.data, old, local])
+                    setTimeout(function(){
+                        var old = $.extend({},ui.item[0]._data);
+                        var obj = ui.item[0]._data;
+                        var objParent = ui.item[0].parentNode.parentNode._data;
+                        ui.item[0].dataset.parent_id = objParent.id
+
+                        obj.parent_id = objParent.id
+                        obj.parent_type = objParent.type;
+                        var newdata = [];
+                        $('li', scope.list).each(function(){
+                            if(this._data) newdata.push(this._data)
+                        });
+                        scope.options.data = newdata;
+                        var local = [];
+                        $(ui.item[0].parentNode.querySelectorAll('li')).each(function(){
+                            local.push(this._data.id);
+                        });
+                        $(scope.list).remove();
+                        scope.init();
+                        $(scope).trigger('orderChange', [obj, scope.options.data, old, local])
+                    }, 10);
+
                 }
             })
         };
@@ -297,7 +301,6 @@ mw.lib.require('nestedsortable');
                     var obj = ui.item[0]._data;
                     var objParent = ui.item[0].parentNode.parentNode._data;
                     ui.item[0].dataset.parent_id = objParent.id
-                    console.log(ui.item[0].parentNode.parentNode)
                     obj.parent_id = objParent.id
                     obj.parent_type = objParent.type;
                     var newdata = [];
@@ -436,7 +439,7 @@ mw.lib.require('nestedsortable');
         };
 
         this.prepend = function(){
-            if(this.options.append){
+            if(this.options.prepend){
                 $.each(this.options.append, function(){
                     $(scope.list).prepend(scope.additional(this))
                 })
