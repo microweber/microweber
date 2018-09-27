70c5795af7df1baa3333dfca0206a6326a26781a
b.slaveykov <selfworksbg@gmail.com>
2018-09-27 09:53:02 +0300

    Remove reccur from lib

--- userfiles/modules/calendar/lib/recurr/tests/bootstrap.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-require_once __DIR__.'/../vendor/autoload.php';
-
-$classLoader = new \Composer\Autoload\ClassLoader();
-$classLoader->add('Recurr\\Test', __DIR__);
-$classLoader->register(true);
