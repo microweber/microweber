278cbcfa90ed254afc69dfa0a2308ca35c389a28
Peter Ivanov <peter@microweber.com>
2018-10-05 11:01:22 +0300

    updates

--- userfiles/modules/calendar/templates/events_list/index.php
+++ userfiles/modules/calendar/templates/events_list/index.php
@@ -37,7 +37,7 @@ $mod_suffix = md5($params['id']);
         <div class="mw-events-calendar"></div>
     </div>
     <div class="col-sm-9">
-        <module class="mw-events-list-selected-date" type="calendar" template="events_list/list"/>
+        <module class="mw-events-list-selected-date" type="calendar/templates/events_list/list" template="events_list/list" id="inner-calendar-<?php print $params['id'] ?>" />
     </div>
 </div>
 
