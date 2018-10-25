4f15f258d1ec872f723098f385d2b33edc0a922f
Petko Yovchevski <pyovchevski@gmail.com>
2018-10-25 16:22:21 +0300

    updates

--- userfiles/modules/google_maps/admin.php
+++ userfiles/modules/google_maps/admin.php
@@ -4,52 +4,17 @@
         mw.$("#zoom_level").val(val).trigger("change");
     }
 </script>
-<style type="text/css">
-    #val_slider {
-        width: 200px;
-    }
 
-    #addr {
-        width: 368px;
-    }
-</style>
-
-<div class="module-live-edit-settings">
+<div class="module-live-edit-settings  module-google-maps-settings">
     <div class="mw-ui-field-holder">
-        <label class="mw-ui-label">
-            <?php _e("Address"); ?>
-        </label>
-        <input
-                name="data-address"
-                class="mw-ui-field mw_option_field"
-                id="addr"
-                type="text"
-                value="<?php print get_option('data-address', $params['id']) ?>"/>
+        <label class="mw-ui-label"><?php _e("Enter Your Address"); ?></label>
+        <input name="data-address" class="mw-ui-field mw_option_field mw-full-width" id="addr" type="text" value="<?php print get_option('data-address', $params['id']) ?>" placeholder="<?php print _e('Example: Bulgaria, Sofia, bul. Cherni Vrah 47'); ?>"/>
     </div>
+
     <div class="mw-ui-field-holder">
-        <label class="mw-ui-label" style="padding-bottom: 20px;">
-            <?php _e("Zoom Level"); ?>
-        </label>
-        <?php /* <div
-    data-type=""
-    data-custom="zoommap"
-    data-value="<?php print get_option('data-zoom', $params['id']) ?>"
-    data-max="19"
-    data-min="0"
-    id="val_slider"
-    class="ed_slider val-slider es_item left ui-slider ui-slider-horizontal ui-widget ui-widget-content ui-corner-all"
-    aria-disabled="false">
-    <a href="#" class="ui-slider-handle ui-state-default ui-corner-all" style="left: 0%;"></a>
-</div>
-*/ ?>
+        <label class="mw-ui-label"><?php _e("Zoom Level"); ?></label>
         <div class="range-slider">
-            <input
-                    name="data-zoom"
-                    class="mw-ui-field-range mw_option_field"
-                    max="21"
-                    min="0"
-                    type="range" id="zoom_level"
-                    value="<?php print get_option('data-zoom', $params['id']) ?>"/>
+            <input name="data-zoom" class="mw-ui-field-range mw_option_field mw-full-width" max="21" min="0" type="range" id="zoom_level" value="<?php print get_option('data-zoom', $params['id']) ?>"/>
         </div>
     </div>
 </div>
--- userfiles/modules/popup/admin.php
+++ userfiles/modules/popup/admin.php
@@ -8,22 +8,57 @@ if (!$time_delay) {
 }
 ?>
 
-<div class="module-live-edit-settings">
+<script>
+    function showHideDelay() {
+        var selectedType = $('input[name="type"]:checked').val();
+        if (selectedType == 'on_time') {
+            $('.js-time-delay').show();
+        } else {
+            $('.js-time-delay').hide();
+        }
+    }
+    $(document).ready(function () {
+        showHideDelay();
+
+        $('input[name="type"]').on('change', function () {
+            showHideDelay();
+        });
+    });
+</script>
+
+<div class="module-live-edit-settings module-popup-settings">
+    <div class="mw-ui-field-holder">
+        <label class="mw-ui-label"><?php _e("Show Pop-Up event"); ?></label>
+
+        <ul class="mw-ui-inline-list">
+            <li>
+                <label class="mw-ui-check">
+                    <input type="radio" name="type" class="mw_option_field" value="on_click" data-refresh="popup"
+                           <?php if ($type == 'on_click'): ?>checked<?php endif; ?>><span></span><span>On click</span>
+                </label>
+            </li>
+
+            <li>
+                <label class="mw-ui-check">
+                    <input type="radio" name="type" class="mw_option_field" value="on_time" data-refresh="popup"
+                           <?php if ($type == 'on_time'): ?>checked<?php endif; ?>><span></span><span>On time (<span class="tip" data-tipposition="top-center" title="Only first time (if accept) with cookies">?</span>)</span>
+                </label>
+            </li>
+
+            <li>
+                <label class="mw-ui-check">
+                    <input type="radio" name="type" class="mw_option_field" value="on_leave_window" data-refresh="popup"
+                           <?php if ($type == 'on_leave_window'): ?>checked<?php endif; ?>><span></span><span>On Leave window</span>
+                </label>
+            </li>
+        </ul>
+    </div>
+
+    <div class="mw-ui-field-holder js-time-delay" style="display: none;">
+        <label class="mw-ui-label"><?php _e("Time delay in MS"); ?></label>
+        <input type="text" name="time_delay" class="mw_option_field mw-ui-field mw-full-width" value="<?php print $time_delay; ?>" data-refresh="popup"/>
+        <small><?php _e("Only if Show Pop-Up Event is On time"); ?></small>
+    </div>
+
     <module type="admin/modules/templates" simple="true"/>
-
-    <div class="mw-ui-field-holder">
-        <label class="mw-ui-label"><?php _e("Type of work"); ?></label>
-        <input type="radio" name="type" class="mw_option_field" value="on_time" data-refresh="popup"
-               <?php if ($type == 'on_time'): ?>checked<?php endif; ?> > On time - only first time (if accept) with cookies<br>
-        <input type="radio" name="type" class="mw_option_field" value="on_click" data-refresh="popup"
-               <?php if ($type == 'on_click'): ?>checked<?php endif; ?> > On click<br>
-        <input type="radio" name="type" class="mw_option_field" value="on_leave_window" data-refresh="popup"
-               <?php if ($type == 'on_leave_window'): ?>checked<?php endif; ?> > On Leave window<br>
-    </div>
-
-
-    <div class="mw-ui-field-holder">
-        <label class="mw-ui-label"><?php _e("Time delay in ms (only if type of work is On time)"); ?></label>
-        <input type="text" name="time_delay" class="mw_option_field" value="<?php print $time_delay; ?>" data-refresh="popup"/>
-    </div>
 </div>
