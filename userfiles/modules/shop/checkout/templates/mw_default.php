9813d9c351cc262ccf77c31d092c4b13d63efd54
Petko Yovchevski <pyovchevski@gmail.com>
2018-12-05 13:52:05 +0200

    updates

--- userfiles/modules/shop/checkout/templates/mw_default.php
+++ userfiles/modules/shop/checkout/templates/mw_default.php
@@ -14,28 +14,28 @@ $cart_show_payments = 'n';
 
             <div class="mw-ui-field-holder">
                 <label class="mw-ui-label"><?php _e("First Name"); ?></label>
-                <input name="first_name" class="mw-ui-field element-block" type="text" value="<?php if (isset($user['first_name'])) {
+                <input name="first_name" class="mw-ui-field mw-full-width" type="text" value="<?php if (isset($user['first_name'])) {
                     print $user['first_name'];
                 } ?>"/>
             </div>
 
             <div class="mw-ui-field-holder">
                 <label class="mw-ui-label"><?php _e("Last Name"); ?></label>
-                <input name="last_name" class="mw-ui-field element-block" type="text" value="<?php if (isset($user['last_name'])) {
+                <input name="last_name" class="mw-ui-field mw-full-width" type="text" value="<?php if (isset($user['last_name'])) {
                     print $user['last_name'];
                 } ?>"/>
             </div>
 
             <div class="mw-ui-field-holder">
                 <label class="mw-ui-label"><?php _e("Email"); ?></label>
-                <input name="email" class="mw-ui-field element-block" type="text" value="<?php if (isset($user['email'])) {
+                <input name="email" class="mw-ui-field mw-full-width" type="text" value="<?php if (isset($user['email'])) {
                     print $user['email'];
                 } ?>"/>
             </div>
 
             <div class="mw-ui-field-holder">
                 <label class="mw-ui-label"><?php _e("Phone"); ?></label>
-                <input name="phone" class="mw-ui-field element-block" type="text" value="<?php if (isset($user['phone'])) {
+                <input name="phone" class="mw-ui-field mw-full-width" type="text" value="<?php if (isset($user['phone'])) {
                     print $user['phone'];
                 } ?>"/>
             </div>
