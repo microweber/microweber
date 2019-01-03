e11da98d3b0b4ef1fd747cd2d9da37f4614f8bb6
Peter Ivanov <peter@microweber.com>
2019-01-03 16:30:15 +0200

    updates

--- userfiles/modules/newsletter/functions.php
+++ userfiles/modules/newsletter/functions.php
@@ -1,15 +1,17 @@
 <?php
 
-include 'functions/sender_functions.php';
-include 'functions/template_functions.php';
-include 'functions/subscriber_functions.php';
-include 'functions/campaign_functions.php';
-include 'functions/list_functions.php';
+include __DIR__ . '/functions/sender_functions.php';
+include __DIR__ . '/functions/template_functions.php';
+include __DIR__ . '/functions/subscriber_functions.php';
+include __DIR__ . '/functions/campaign_functions.php';
+include __DIR__ . '/functions/list_functions.php';
 
-function array_search_multidimensional($array, $column, $key){
+function array_search_multidimensional($array, $column, $key)
+{
     return (array_search($key, array_column($array, $column)));
 }
 
 event_bind('website.privacy_settings', function () {
-	print '<h2>Newsletter settings</h2><module type="newsletter/privacy_settings" />';
+    print '<h2>Newsletter settings</h2><module type="newsletter/privacy_settings" />';
 });
+
--- userfiles/modules/newsletter/functions/sender_functions.php
+++ userfiles/modules/newsletter/functions/sender_functions.php
@@ -1,4 +1,3 @@
-
 <?php
 /* 
  * EMAIL SENDER FUNCTIONS
