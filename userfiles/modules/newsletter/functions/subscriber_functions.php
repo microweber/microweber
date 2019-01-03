ac43a5f769f30f649fe1a8e5f8738b7826abc6c8
Peter Ivanov <peter@microweber.com>
2019-01-03 17:22:59 +0200

    updates

--- userfiles/modules/newsletter/functions/subscriber_functions.php
+++ userfiles/modules/newsletter/functions/subscriber_functions.php
@@ -1,16 +1,18 @@
 <?php
 /* SUBSCRIBER FUNCTIONS */
 
-function newsletter_unsubscribe($params) {
+function newsletter_unsubscribe($params)
+{
     // code...
 }
 
 api_expose('newsletter_subscribe');
-function newsletter_subscribe($params) {
+function newsletter_subscribe($params)
+{
     $adm = mw()->user_manager->is_admin();
     if (defined('MW_API_CALL')) {
         $validate_token = mw()->user_manager->csrf_validate($params);
-        if (! $adm) {
+        if (!$adm) {
             if ($validate_token == false) {
                 return array(
                     'error' => 'Invalid token!'
@@ -21,39 +23,39 @@ function newsletter_subscribe($params) {
     $rules = [
         'email' => 'required|email|unique:newsletter_subscribers'
     ];
-    
+
     $input = Input::only('name', 'email', 'list_id', 'terms', 'captcha');
     $messages = array(
         'unique' => 'This email is already subscribed!'
     );
-    
+
     $validator = Validator::make($input, $rules, $messages);
     if ($validator->fails()) {
         return array(
             'error' => $validator->messages()
         );
     }
-    
+
     $needs_terms = get_option('require_terms', 'newsletter') == 'y';
     $enable_captcha = get_option('enable_captcha', 'newsletter') == 'y';
-    
+
     if ($needs_terms) {
         $user_id_or_email = mw()->user_manager->id();
-        if (! $user_id_or_email) {
+        if (!$user_id_or_email) {
             if (isset($input ['email'])) {
                 $user_id_or_email = $input ['email'];
             }
         }
-        
-        if (! $user_id_or_email) {
+
+        if (!$user_id_or_email) {
             return array(
                 'error' => _e('You must provide email address', true)
             );
         } else {
             $terms_and_conditions_name = 'terms_newsletter';
-            
+
             $check_term = mw()->user_manager->terms_check($terms_and_conditions_name, $user_id_or_email);
-            if (! $check_term) {
+            if (!$check_term) {
                 if (isset($input ['terms']) and $input ['terms']) {
                     mw()->user_manager->terms_accept($terms_and_conditions_name, $user_id_or_email);
                 } else {
@@ -66,9 +68,9 @@ function newsletter_subscribe($params) {
             }
         }
     }
-    
+
     if ($enable_captcha) {
-        if (! isset($input ['captcha'])) {
+        if (!isset($input ['captcha'])) {
             return array(
                 'error' => _e('Invalid captcha answer!', true),
                 'captcha_error' => true,
@@ -77,7 +79,7 @@ function newsletter_subscribe($params) {
             );
         } else {
             $validate_captcha = mw()->captcha->validate($input ['captcha']);
-            if (! $validate_captcha) {
+            if (!$validate_captcha) {
                 return array(
                     'error' => _e('Invalid captcha answer!', true),
                     'captcha_error' => true,
@@ -87,23 +89,26 @@ function newsletter_subscribe($params) {
             }
         }
     }
-    
+    newsletter_set_cookie_for_subscribed_user();
+
     $confirmation_code = str_random(30);
-    
+
     newsletter_save_subscriber([
         'email' => Input::get('email'),
-    	'name' => Input::get('name'),
-    	'list_id' => Input::get('list_id'),
+        'name' => Input::get('name'),
+        'list_id' => Input::get('list_id'),
         'confirmation_code' => $confirmation_code
     ]);
-    
+
     $msg = 'Thanks for your subscription!';
-    
+
     return array(
         'success' => $msg
     );
 }
-function newsletter_get_subscribers($params) {
+
+function newsletter_get_subscribers($params)
+{
     if (is_string($params)) {
         $params = parse_params($params);
     }
@@ -117,92 +122,119 @@ function newsletter_get_subscriber($subscriber_id)
 {
     $data = ['id' => $subscriber_id, 'single' => true];
     $table = "newsletter_subscribers";
-    
+
     return db_get($table, $data);
 }
 
 api_expose_admin('newsletter_save_subscriber');
-function newsletter_save_subscriber($data) {
-	
+function newsletter_save_subscriber($data)
+{
+
+    // mw()->user_manager->ses
+
+
     $table = "newsletter_subscribers";
-    
-    if (! isset($data ['is_subscribed']) and ! isset($data ['id'])) {
+
+    if (!isset($data ['is_subscribed']) and !isset($data ['id'])) {
         $data ['is_subscribed'] = 1;
     }
-    
-    if (! isset($data['subscribed_for'])) {
+
+    if (!isset($data['subscribed_for'])) {
         // Default list
-        
+
     } else {
         newsletter_delete_subscriber_lists($data['id']);
         foreach ($data['subscribed_for'] as $list_id) {
-        	newsletter_save_subscriber_list($data['id'], $list_id);
+            newsletter_save_subscriber_list($data['id'], $list_id);
         }
     }
-    
+
     $save_id = db_save($table, $data);
-    
+
     if (isset($data['list_id'])) {
-    	newsletter_save_subscriber_list($save_id, $data['list_id']);
+        newsletter_save_subscriber_list($save_id, $data['list_id']);
     }
-    
+
     return $save_id;
 }
 
-function newsletter_delete_subscriber_lists($subscriber_id) {
-    
+function newsletter_delete_subscriber_lists($subscriber_id)
+{
+
     $table = "newsletter_subscribers_lists";
-    
+
     // Delete subscriber from old lists
     db_delete($table, $subscriber_id, 'subscriber_id');
 }
 
-function newsletter_save_subscriber_list($subscriber_id, $list_id) {
-    
+function newsletter_save_subscriber_list($subscriber_id, $list_id)
+{
+
     $table = "newsletter_subscribers_lists";
-    
+
     // Add subscriber to list
     $data = array();
     $data['subscriber_id'] = $subscriber_id;
     $data['list_id'] = $list_id;
-    
+
     return db_save($table, $data);
 }
 
-function newsletter_get_subscriber_lists($subscriber_id) {
+function newsletter_get_subscriber_lists($subscriber_id)
+{
     $data = ['subscriber_id' => $subscriber_id];
     $data ['table'] = "newsletter_subscribers_lists";
     return db_get($data);
 }
 
-function newsletter_get_subscribers_for_list($list_id) {
-	
-	$subscribers =  DB::table('newsletter_subscribers_lists')
-	->join('newsletter_subscribers', 'newsletter_subscribers.id', '=', 'newsletter_subscribers_lists.subscriber_id')
-	->select('newsletter_subscribers_lists.*', 'newsletter_subscribers.name', 'newsletter_subscribers.email', 'newsletter_subscribers.is_subscribed')
-	->where('newsletter_subscribers.is_subscribed', 1)
-	->get();
-	
-	$readySubscribers = array();
-	
-	foreach($subscribers as $subscriber) {
-		$readySubscribers[] = array(
-			'id'=>$subscriber->id,
-			'subscriber_id'=>$subscriber->subscriber_id,
-			'list_id'=>$subscriber->list_id,
-			'name'=>$subscriber->name,
-			'email'=>$subscriber->email,
-		);
-	}
-	
-	return $readySubscribers;
+function newsletter_get_subscribers_for_list($list_id)
+{
+
+    $subscribers = DB::table('newsletter_subscribers_lists')
+        ->join('newsletter_subscribers', 'newsletter_subscribers.id', '=', 'newsletter_subscribers_lists.subscriber_id')
+        ->select('newsletter_subscribers_lists.*', 'newsletter_subscribers.name', 'newsletter_subscribers.email', 'newsletter_subscribers.is_subscribed')
+        ->where('newsletter_subscribers.is_subscribed', 1)
+        ->get();
+
+    $readySubscribers = array();
+
+    foreach ($subscribers as $subscriber) {
+        $readySubscribers[] = array(
+            'id' => $subscriber->id,
+            'subscriber_id' => $subscriber->subscriber_id,
+            'list_id' => $subscriber->list_id,
+            'name' => $subscriber->name,
+            'email' => $subscriber->email,
+        );
+    }
+
+    return $readySubscribers;
 }
 
 api_expose_admin('newsletter_delete_subscriber');
-function newsletter_delete_subscriber($params) {
+function newsletter_delete_subscriber($params)
+{
     if (isset($params ['id'])) {
         $table = "newsletter_subscribers";
         $id = $params ['id'];
         return db_delete($table, $id);
     }
+}
+
+
+function newsletter_check_cookie_for_subscribed_user()
+{
+    $hash = user_ip();
+    $hash = md5($hash);
+    if (isset($_COOKIE['mw-newsletter_is_subscribed']) and $_COOKIE['mw-newsletter_is_subscribed'] == $hash) {
+        return true;
+    }
+}
+
+function newsletter_set_cookie_for_subscribed_user()
+{
+    $hash = user_ip();
+    $hash = md5($hash);
+
+    setcookie('mw-newsletter_is_subscribed', $hash, 45000);
 }
