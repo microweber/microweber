b219144b75d9a246917a3ba0260234693f8c51e6
Petko Yovchevski <pyovchevski@gmail.com>
2019-01-10 10:46:12 +0200

    updates

--- userfiles/modules/cookie_notice/scwCookie/output/popup.php
+++ userfiles/modules/cookie_notice/scwCookie/output/popup.php
@@ -1,13 +1,13 @@
 <?php
 $scw_cookie_style = '';
-if(!empty($this->config['backgroundColor'])) {
-	$scw_cookie_style = ' style="background:' . $this->config['backgroundColor'] . '"';
+if (!empty($this->config['backgroundColor'])) {
+    $scw_cookie_style = ' style="background:' . $this->config['backgroundColor'] . '"';
 }
 ?>
-<link href="userfiles/modules/cookie_notice/scwCookie/output/assets/scwCookie.min.css" rel="stylesheet" type="text/css">
-<div class="scw-cookie<?php print ($this->decisionMade ? ' scw-cookie-out' : ''); ?>"<?php print $scw_cookie_style;?>>
+<link href="<?php print modules_url(); ?>/cookie_notice/scwCookie/output/assets/scwCookie.min.css" rel="stylesheet" type="text/css"/>
+<div class="scw-cookie<?php print ($this->decisionMade ? ' scw-cookie-out' : ''); ?>"<?php print $scw_cookie_style; ?>>
     <div class="scw-cookie-panel-toggle scw-cookie-panel-toggle-<?php print $this->config['panelTogglePosition']; ?>"
-        onclick="scwCookiePanelToggle()"
+         onclick="scwCookiePanelToggle()"
     >
         <span class="icon icon-cookie"></span>
     </div>
@@ -17,10 +17,10 @@ if(!empty($this->config['backgroundColor'])) {
             We use cookies to personalise content<?php print $liveChatMessage; ?> and to analyse our web traffic.
         </div>
         <div class="scw-cookie-decision">
-            <div class="scw-cookie-btn" onclick="scwCookieHide('<?php print $this->mod_id;?>')">OK</div>
+            <div class="scw-cookie-btn" onclick="scwCookieHide('<?php print $this->mod_id; ?>')">OK</div>
             <div class="scw-cookie-settings scw-cookie-tooltip-trigger"
-                onclick="scwCookieDetails()"
-                data-label="Cookie settings"
+                 onclick="scwCookieDetails()"
+                 data-label="Cookie settings"
             >
                 <span class="icon icon-settings"></span>
             </div>
@@ -39,21 +39,23 @@ if(!empty($this->config['backgroundColor'])) {
                     <div></div>
                 </label>
             </div>
-            <?php foreach ($this->enabledCookies() as $name => $label) { ?>
-                <div class="scw-cookie-toggle">
-                    <div class="scw-cookie-name" onclick="scwCookieToggle(this)"><?php print $label; ?></div>
-                    <label class="scw-cookie-switch<?php print ($this->isAllowed($name) ? ' checked' : ''); ?>">
-                        <input type="checkbox"
-                        name="<?php print $name; ?>"
-                        <?php print ($this->isAllowed($name) ? 'checked="checked"' : ''); ?>
-                        />
-                        <div></div>
-                    </label>
-                </div>
+            <?php if ($this->enabledCookies()): ?>
+                <?php foreach ($this->enabledCookies() as $name => $label) { ?>
+                    <div class="scw-cookie-toggle">
+                        <div class="scw-cookie-name" onclick="scwCookieToggle(this)"><?php print $label; ?></div>
+                        <label class="scw-cookie-switch<?php print ($this->isAllowed($name) ? ' checked' : ''); ?>">
+                            <input type="checkbox"
+                                   name="<?php print $name; ?>"
+                                <?php print ($this->isAllowed($name) ? 'checked="checked"' : ''); ?>
+                            />
+                            <div></div>
+                        </label>
+                    </div>
 
                 <?php } ?>
-            </div>
+            <?php endif; ?>
         </div>
     </div>
-    <script src="userfiles/modules/cookie_notice/scwCookie/output/assets/js-cookie.js" type="text/javascript"></script>
-    <script src="userfiles/modules/cookie_notice/scwCookie/output/assets/scwCookie.min.js" type="text/javascript"></script>
+</div>
+<script src="<?php print modules_url(); ?>/cookie_notice/scwCookie/output/assets/js-cookie.js" type="text/javascript"></script>
+<script src="<?php print modules_url(); ?>/cookie_notice/scwCookie/output/assets/scwCookie.min.js" type="text/javascript"></script>
