0f2da32b333ac72be0842f6aa672415d27566446
Petko Yovchevski <pyovchevski@gmail.com>
2019-01-30 16:59:43 +0200

    updates

--- userfiles/modules/admin/packages.php
+++ userfiles/modules/admin/packages.php
@@ -4,15 +4,20 @@
 
 $search_packages = mw()->update->composer_search_packages('cache=true');
 
-d($search_packages);
+//dd($search_packages);
 ?>
-<div class="mw-ui-row admin-section-bar">
-    <div class="mw-ui-col">
-        <h2 class="mw-side-main-title relative"><span class="mw-icon-updates"></span>
-            <?php _e("Updates"); ?>
-        </h2>
+
+
+<div class="section-header">
+    <h2 class="pull-left"><span class="mw-icon-updates"></span> <?php _e("Packages"); ?></h2>
+    <div class="pull-right">
+        <div class="top-search">
+            <input value="" name="module_keyword" placeholder="Search" type="text" autocomplete="off" onkeyup="event.keyCode==13?mw.url.windowHashParam('search',this.value):false">
+            <span class="top-form-submit" onclick="mw.url.windowHashParam('search',$(this).prev().val())"><span class="mw-icon-search"></span></span>
+        </div>
     </div>
 </div>
+
 
 <script>
     $(document).ready(function () {
@@ -23,103 +28,172 @@ d($search_packages);
     });
 </script>
 
-<div class="admin-side-content">
+<div class="admin-side-content" style="max-width: 90%">
     <div id="nav" class="mw-ui-row">
         <div class="mw-ui-col" style="width: 20%;">
             <div class="mw-ui-col-container">
                 <ul class="mw-ui-box mw-ui-navigation" id="nav">
                     <li><a href="javascript:;" class="active">Themes</a></li>
                     <li><a href="javascript:;">Plugins</a></li>
-                    <li><a href="javascript:;">Core</a></li>
+                    <li><a href="javascript:;">Others</a></li>
                 </ul>
             </div>
         </div>
 
         <div class="mw-ui-col" style="width: 80%;">
             <div class="mw-ui-col-container">
-                <div class="mw-ui-box" style="width: 100%;">
+                <div class="mw-ui-box" style="width: 100%; padding: 12px 0;">
                     <div class="mw-ui-box-content tab">
-
-
-                        <?php if ($search_packages) { ?>
-
-
+                        <?php if ($search_packages) : ?>
                             <?php foreach ($search_packages as $key => $item): ?>
+                                <?php if ($item['type'] == 'microweber-template'): ?>
+                                    <div class="mw-ui-col" style="width: 33%;">
+                                        <div class="mw-ui-col-container">
+                                            <div class="mw-ui-box" style="min-height: 300px;">
+                                                <div class="mw-ui-box-header">
+                                                    <span class="mw-icon-gear"></span><span> <?php print $item['name'] ?></span>
+                                                </div>
+                                                <div class="mw-ui-box-content">
 
-                                <div class="mw-ui-col" style="width: 33.3%;">
-                                    <div class="mw-ui-col-container">
-                                        <div class="mw-ui-box">
-                                            <div class="mw-ui-box-header">
-                                                <span class="mw-icon-gear"></span><span> <?php print $item['name'] ?></span>
-                                            </div>
-                                            <div class="mw-ui-box-content">
+                                                    <p class="m-b-20"><?php print $item['description'] ?></p>
 
-                                                <?php print $item['description'] ?>
+                                                    <table cellspacing="0" cellpadding="0" class="mw-ui-table" width="100%">
+                                                        <tbody>
+                                                        <tr>
+                                                            <td>Release date</td>
+                                                            <td><?php print $item['latest_version']['release_date'] ?></td>
 
-                                                <ul>
-                                                   <li>release_date: <?php print $item['latest_version']['release_date'] ?></li>
-                                                   <li>version: <?php print $item['latest_version']['version'] ?></li>
-                                                   <li>author: Petko</li>
-                                                   <li>website: example.com</li>
-                                                </ul>
+                                                        </tr>
+                                                        <tr>
+                                                            <td>Version</td>
+                                                            <td><?php print $item['latest_version']['version'] ?></td>
 
-                                                <a class="mw-ui-btn">Read more</a>
-                                                <a class="mw-ui-btn">Install</a>
+                                                        </tr>
+                                                        <tr>
+                                                            <td>Author</td>
+                                                            <td>Petko</td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td>Website</td>
+                                                            <td><a href="#" class="mw-blue">plumtex.com</a></td>
+                                                        </tr>
+                                                        </tbody>
+                                                    </table>
+
+                                                    <div class="text-center m-t-20">
+                                                        <a href="javascript:;" class="mw-ui-btn mw-ui-btn-medium mw-ui-btn-info">Read more</a>
+                                                        <a href="javascript:;" class="mw-ui-btn mw-ui-btn-medium mw-ui-btn-notification">Install</a>
+                                                        <a href="javascript:;" class="mw-ui-btn mw-ui-btn-medium mw-ui-btn-important mw-ui-btn-outline"><i class="mw-icon-trash-a"></i></a>
+                                                    </div>
+                                                </div>
                                             </div>
                                         </div>
                                     </div>
-                                </div>
+                                <?php endif; ?>
                             <?php endforeach; ?>
-
-
-                        <?php } ?>
-
-                        <div class="mw-ui-col" style="width: 33.3%;">
-                            <div class="mw-ui-col-container">
-                                <div class="mw-ui-box">
-                                    <div class="mw-ui-box-header">
-                                        <span class="mw-icon-gear"></span><span> Module title</span>
-                                    </div>
-                                    <div class="mw-ui-box-content">
-
-                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc quis justo et
-                                        sapien varius gravida. Fusce
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="mw-ui-col" style="width: 33.3%;">
-                            <div class="mw-ui-col-container">
-                                <div class="mw-ui-box">
-                                    <div class="mw-ui-box-header">
-                                        <span class="mw-icon-gear"></span><span> Module title</span>
-                                    </div>
-                                    <div class="mw-ui-box-content">
-                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc quis justo et
-                                        sapien varius gravida. Fusce
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="mw-ui-col" style="width: 33.3%;">
-                            <div class="mw-ui-col-container">
-                                <div class="mw-ui-box">
-                                    <div class="mw-ui-box-header">
-                                        <span class="mw-icon-gear"></span><span> Module title</span>
-                                    </div>
-                                    <div class="mw-ui-box-content">
-                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc quis justo et
-                                        sapien varius gravida. Fusce
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+                        <?php endif; ?>
                     </div>
 
-                    <div class="mw-ui-box-content tab" style="display: none">About - Lorem Ipsum</div>
-                    <div class="mw-ui-box-content tab" style="display: none">About - Lorem Ipsum 2</div>
+                    <div class="mw-ui-box-content tab" style="display: none">
+                        <?php if ($search_packages) : ?>
+                            <?php foreach ($search_packages as $key => $item): ?>
+                                <?php if ($item['type'] == 'microweber-module'): ?>
+                                    <div class="mw-ui-col" style="width: 33%;">
+                                        <div class="mw-ui-col-container">
+                                            <div class="mw-ui-box" style="min-height: 300px;">
+                                                <div class="mw-ui-box-header">
+                                                    <span class="mw-icon-gear"></span><span> <?php print $item['name'] ?></span>
+                                                </div>
+                                                <div class="mw-ui-box-content">
+
+                                                    <p class="m-b-20"><?php print $item['description'] ?></p>
+
+                                                    <table cellspacing="0" cellpadding="0" class="mw-ui-table" width="100%">
+                                                        <tbody>
+                                                        <tr>
+                                                            <td>Release date</td>
+                                                            <td><?php print $item['latest_version']['release_date'] ?></td>
+
+                                                        </tr>
+                                                        <tr>
+                                                            <td>Version</td>
+                                                            <td><?php print $item['latest_version']['version'] ?></td>
+
+                                                        </tr>
+                                                        <tr>
+                                                            <td>Author</td>
+                                                            <td>Petko</td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td>Website</td>
+                                                            <td><a href="#" class="mw-blue">plumtex.com</a></td>
+                                                        </tr>
+                                                        </tbody>
+                                                    </table>
+
+                                                    <div class="text-center m-t-20">
+                                                        <a href="javascript:;" class="mw-ui-btn mw-ui-btn-medium mw-ui-btn-info">Read more</a>
+                                                        <a href="javascript:;" class="mw-ui-btn mw-ui-btn-medium mw-ui-btn-notification">Install</a>
+                                                        <a href="javascript:;" class="mw-ui-btn mw-ui-btn-medium mw-ui-btn-important mw-ui-btn-outline"><i class="mw-icon-trash-a"></i></a>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                <?php endif; ?>
+                            <?php endforeach; ?>
+                        <?php endif; ?>
+                    </div>
+                    <div class="mw-ui-box-content tab" style="display: none">
+                        <?php if ($search_packages) : ?>
+                            <?php foreach ($search_packages as $key => $item): ?>
+                                <?php if ($item['type'] != 'microweber-template' AND $item['type'] != 'microweber-module'): ?>
+                                    <div class="mw-ui-col" style="width: 33%;">
+                                        <div class="mw-ui-col-container">
+                                            <div class="mw-ui-box" style="min-height: 300px;">
+                                                <div class="mw-ui-box-header">
+                                                    <span class="mw-icon-gear"></span><span> <?php print $item['name'] ?></span>
+                                                </div>
+                                                <div class="mw-ui-box-content">
+
+                                                    <p class="m-b-20"><?php print $item['description'] ?></p>
+
+                                                    <table cellspacing="0" cellpadding="0" class="mw-ui-table" width="100%">
+                                                        <tbody>
+                                                        <tr>
+                                                            <td>Release date</td>
+                                                            <td><?php print $item['latest_version']['release_date'] ?></td>
+
+                                                        </tr>
+                                                        <tr>
+                                                            <td>Version</td>
+                                                            <td><?php print $item['latest_version']['version'] ?></td>
+
+                                                        </tr>
+                                                        <tr>
+                                                            <td>Author</td>
+                                                            <td>Petko</td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td>Website</td>
+                                                            <td><a href="#" class="mw-blue">plumtex.com</a></td>
+                                                        </tr>
+                                                        </tbody>
+                                                    </table>
+
+                                                    <div class="text-center m-t-20">
+                                                        <a href="javascript:;" class="mw-ui-btn mw-ui-btn-medium mw-ui-btn-info">Read more</a>
+                                                        <a href="javascript:;" class="mw-ui-btn mw-ui-btn-medium mw-ui-btn-notification">Install</a>
+                                                        <a href="javascript:;" class="mw-ui-btn mw-ui-btn-medium mw-ui-btn-important mw-ui-btn-outline"><i class="mw-icon-trash-a"></i></a>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                <?php endif; ?>
+                            <?php endforeach; ?>
+                        <?php endif; ?>
+                    </div>
                 </div>
             </div>
         </div>
