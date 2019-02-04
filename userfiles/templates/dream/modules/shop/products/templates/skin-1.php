9bfa5a499914bcd92ffc3d7832358f6dab3f3dcc
Petko Yovchevski <pyovchevski@gmail.com>
2019-02-04 14:48:54 +0200

    updates

--- userfiles/templates/dream/modules/shop/products/templates/skin-1.php
+++ userfiles/templates/dream/modules/shop/products/templates/skin-1.php
@@ -84,6 +84,9 @@ if (!isset($tn[1])) {
                                 <a href="<?php print $item['link'] ?>">
                                     <div class="card__price">
                                         <span><?php print currency_format($val1); ?></span>
+                                        <?php if ($item['original_price']): ?>
+                                            <del><?php print currency_format($item['original_price']); ?></del>
+                                        <?php endif; ?>
                                     </div>
                                 </a>
                             <?php } ?>
