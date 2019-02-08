3a8d13b01f1a42b6f12bdbb1bba3e77c0fab9cfc
Petko Yovchevski <pyovchevski@gmail.com>
2019-02-08 14:31:05 +0200

    updates

---
+++ userfiles/modules/layouts/default_layout_classes.php
@@ -0,0 +1,23 @@
+<?php
+$css_classes = [];
+$css_classes['padding-top'][1] = 'mw-p-t-10';
+$css_classes['padding-top'][2] = 'mw-p-t-20';
+$css_classes['padding-top'][3] = 'mw-p-t-30';
+$css_classes['padding-top'][4] = 'mw-p-t-40';
+$css_classes['padding-top'][5] = 'mw-p-t-50';
+$css_classes['padding-top'][6] = 'mw-p-t-60';
+$css_classes['padding-top'][7] = 'mw-p-t-70';
+$css_classes['padding-top'][8] = 'mw-p-t-80';
+$css_classes['padding-top'][9] = 'mw-p-t-90';
+$css_classes['padding-top'][10] = 'mw-p-t-100';
+
+$css_classes['padding-bottom'][1] = 'mw-b-t-10';
+$css_classes['padding-bottom'][2] = 'mw-b-t-20';
+$css_classes['padding-bottom'][3] = 'mw-b-t-30';
+$css_classes['padding-bottom'][4] = 'mw-b-t-40';
+$css_classes['padding-bottom'][5] = 'mw-b-t-50';
+$css_classes['padding-bottom'][6] = 'mw-b-t-60';
+$css_classes['padding-bottom'][7] = 'mw-b-t-70';
+$css_classes['padding-bottom'][8] = 'mw-b-t-80';
+$css_classes['padding-bottom'][9] = 'mw-b-t-90';
+$css_classes['padding-bottom'][10] = 'mw-b-t-100';
