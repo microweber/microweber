c91a71d7541c34ab4f2ea2b3f851f7467f222a7c
Petko Yovchevski <pyovchevski@gmail.com>
2019-02-11 13:56:29 +0200

    updates

--- userfiles/templates/liteness/modules/layouts/templates/skin-3.php
+++ userfiles/templates/liteness/modules/layouts/templates/skin-3.php
@@ -12,7 +12,18 @@ position: 2
 
 ?>
 
-<div class="nodrop safe-mode edit" field="layout-skin-3-<?php print $params['id'] ?>" rel="module">
+<?php
+if (!$classes['padding_top']) {
+    $classes['padding_top'] = '';
+}
+if (!$classes['padding_bottom']) {
+    $classes['padding_bottom'] = '';
+}
+
+$layout_classes = ' ' . $classes['padding_top'] . ' ' . $classes['padding_bottom'] . ' ';
+?>
+
+<div class="nodrop safe-mode edit <?php print $layout_classes; ?>" field="layout-skin-3-<?php print $params['id'] ?>" rel="module">
     <div class="container">
         <div class="box-container latest-items">
             <h2 class="section-title">
--- userfiles/templates/liteness/modules/layouts/templates/skin-6.php
+++ userfiles/templates/liteness/modules/layouts/templates/skin-6.php
@@ -12,7 +12,18 @@ position: 5
 
 ?>
 
-<div class="nodrop safe-mode edit" field="layout-skin-6-<?php print $params['id'] ?>" rel="module">
+<?php
+if (!$classes['padding_top']) {
+    $classes['padding_top'] = '';
+}
+if (!$classes['padding_bottom']) {
+    $classes['padding_bottom'] = '';
+}
+
+$layout_classes = ' ' . $classes['padding_top'] . ' ' . $classes['padding_bottom'] . ' ';
+?>
+
+<div class="nodrop safe-mode edit <?php print $layout_classes; ?>" field="layout-skin-6-<?php print $params['id'] ?>" rel="module">
     <div class="container">
         <div class="box-container">
             <div class="mw-row contacts-block">
