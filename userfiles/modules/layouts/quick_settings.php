aaeee36bad78d0699d48e1a3a957c037f2e697b1
Petko Yovchevski <pyovchevski@gmail.com>
2019-02-15 12:17:32 +0200

    updates

--- userfiles/modules/layouts/quick_settings.php
+++ userfiles/modules/layouts/quick_settings.php
@@ -4,7 +4,7 @@ $template_config = mw()->template->get_config();
 if (isset($template_config['layouts_css_classes'])) {
     $css_classes = $template_config['layouts_css_classes'];
 } else {
-    include ('default_layout_classes.php');
+    include('default_layout_classes.php');
 }
 
 $padding_top = get_option('padding-top', $params['id']);
@@ -18,26 +18,32 @@ if ($padding_bottom === null OR $padding_bottom === false OR $padding_bottom == 
 }
 ?>
 
-<div class="padding-select">
-    <label class="mw-ui-label">Padding Top</label>
-    <select name="padding-top" class="mw-ui-field mw_option_field" data-option-group="<?php print $params['id']; ?>">
-        <option value="" <?php if (!$padding_top) {echo 'selected';} ?>><?php _e("No Selected"); ?></option>
-        <?php if(isset($css_classes['padding-top'])): ?>
-            <?php foreach($css_classes['padding-top'] as $key => $option): ?>
-                <option value="<?php print $key; ?>"<?php if ($padding_top == $key) {echo 'selected';} ?>><?php _e("Padding Top"); ?> <?php print $key; ?></option>
-            <?php endforeach; ?>
-        <?php endif; ?>
-    </select>
-</div>
+<div class="mw-flex-row">
+    <div class="mw-flex-col-xs-12 ">
+        <div class="mw-ui-field-holder">
+            <label class="mw-ui-label">Padding Top</label>
+            <select name="padding-top" class="mw-ui-field mw_option_field mw-full-width" data-option-group="<?php print $params['id']; ?>">
+                <option value="" <?php if (!$padding_top) {echo 'selected';} ?>><?php _e("No Selected"); ?></option>
+                <?php if (isset($css_classes['padding-top'])): ?>
+                    <?php foreach ($css_classes['padding-top'] as $key => $option): ?>
+                        <option value="<?php print $key; ?>"<?php if ($padding_top == $key) {echo 'selected';} ?>><?php print _e("Padding Top") . ' ' . $key; ?></option>
+                    <?php endforeach; ?>
+                <?php endif; ?>
+            </select>
+        </div>
+    </div>
 
-<div class="padding-select">
-    <label class="mw-ui-label">Padding Bottom</label>
-    <select name="padding-bottom" class="mw-ui-field mw_option_field" data-option-group="<?php print $params['id']; ?>">
-        <option value="" <?php if (!$padding_bottom) {echo 'selected';} ?>><?php _e("No Selected"); ?></option>
-        <?php if(isset($css_classes['padding-bottom'])): ?>
-            <?php foreach($css_classes['padding-bottom'] as $key => $option): ?>
-                <option value="<?php print $key; ?>"<?php if ($padding_bottom == $key) {echo 'selected';} ?>><?php _e("Padding Bottom"); ?> <?php print $key; ?></option>
-            <?php endforeach; ?>
-        <?php endif; ?>
-    </select>
+    <div class="mw-flex-col-xs-12 ">
+        <div class="mw-ui-field-holder">
+            <label class="mw-ui-label">Padding Bottom</label>
+            <select name="padding-bottom" class="mw-ui-field mw_option_field mw-full-width" data-option-group="<?php print $params['id']; ?>">
+                <option value="" <?php if (!$padding_bottom) {echo 'selected';} ?>><?php _e("No Selected"); ?></option>
+                <?php if (isset($css_classes['padding-bottom'])): ?>
+                    <?php foreach ($css_classes['padding-bottom'] as $key => $option): ?>
+                        <option value="<?php print $key; ?>"<?php if ($padding_bottom == $key) {echo 'selected';} ?>><?php print _e("Padding Bottom") . ' ' . $key; ?></option>
+                    <?php endforeach; ?>
+                <?php endif; ?>
+            </select>
+        </div>
+    </div>
 </div>
