89e411e6687445f7d111c62faa0758301375e749
Petko Yovchevski <pyovchevski@gmail.com>
2019-03-11 14:54:19 +0200

    updates

--- userfiles/modules/slider/templates/bxslider-skin-2.php
+++ userfiles/modules/slider/templates/bxslider-skin-2.php
@@ -4,9 +4,9 @@
 
   type: layout
 
-  name: bXSlider - Four slides
+  name: bxSlider - Background overlay
 
-  description: bXSlider - Four slides
+  description: bxSlider - Background overlay
 
 
 */
@@ -14,41 +14,15 @@
 ?>
 <link rel="stylesheet" type="text/css" href="<?php print $config['url_to_module'] ?>/templates/bxslider-skin-2/style.css"/>
 
-<?php
+<div class="bxSlider-skin-2">
+    <ul class="bxSlider">
+        <?php foreach ($data as $slide) { ?>
+            <li>
+                <?php if (isset($slide['skin_file'])) { ?>
+                    <?php include $slide['skin_file'] ?>
 
-if (isset($params['pager_custom'])) {
-    $pagerCustom = $params['pager_custom'];
-} else {
-    $pagerCustom = '';
-}
-?>
-
-
-<div class="template-four-slides">
-    <div class="slider-images">
-        <ul class="bxSlider">
-            <?php foreach ($data as $slide) { ?>
-                <li>
-                    <?php if (isset($slide['skin_file'])) { ?>
-                        <?php include $slide['skin_file'] ?>
-                    <?php } ?>
-                </li>
-            <?php } ?>
-        </ul>
-    </div>
-
-    <?php if (isset($pagerCustom) and $pagerCustom != ''): ?>
-        <div class="slider-pagers">
-            <div class="<?php print $pagerCustom; ?>">
-                <?php //d($data); ?>
-                <?php foreach ($data as $key => $slide) { ?>
-                    <a data-slide-index="<?php print $key ?>" href="javascript:;" class="<?php if ($key == 0): ?>active<?php endif; ?>">
-                        <span><?php print $slide['primaryText']; ?></span>
-                        <p><?php print $slide['secondaryText']; ?></p>
-                    </a>
                 <?php } ?>
-            </div>
-        </div>
-    <?php endif; ?>
+            </li>
+        <?php } ?>
+    </ul>
 </div>
-
--- userfiles/modules/slider/templates/slickslider-skin-2.php
+++ userfiles/modules/slider/templates/slickslider-skin-2.php
@@ -14,14 +14,12 @@
 ?>
 <link rel="stylesheet" type="text/css" href="<?php print $config['url_to_module'] ?>/templates/slickslider-skin-2/style.css"/>
 
-<div class="slickslideWrapper template-second">
-    <ul class="slickSlider">
+<div class="slickSlider-skin-2">
+    <div class="slickSlider">
         <?php foreach ($data as $slide) { ?>
-            <div>
-                <?php if (isset($slide['skin_file'])) { ?>
-                    <?php include $slide['skin_file'] ?>
-                <?php } ?>
-            </div>
+            <?php if (isset($slide['skin_file'])) { ?>
+                <?php include $slide['skin_file'] ?>
+            <?php } ?>
         <?php } ?>
-    </ul>
+    </div>
 </div>
