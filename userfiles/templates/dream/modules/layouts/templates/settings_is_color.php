ea65b747b1b520bb63d5453481abc2bc05c3a2ed
Peter Ivanov <peter@microweber.com>
2019-04-01 14:36:19 +0300

    updates

--- userfiles/templates/dream/modules/layouts/templates/settings_is_color.php
+++ userfiles/templates/dream/modules/layouts/templates/settings_is_color.php
@@ -8,11 +8,38 @@ if ($is_color === null OR $is_color === false OR $is_color == '') {
 <script>
 
     $(window).load(function(){
+
+        var timer_color_pick;
+
+
         mw.colorPicker({
             element:'.color_picker_for_sidebar_settings',
             position:'bottom-left',
+            value:'<?php print $is_color; ?>',
+
             onchange:function(color){
-                $('.color_picker_for_sidebar_settings').trigger('change');
+
+
+                     window.clearTimeout(timer_color_pick);
+
+                    $('#color_change_val').val(color);
+
+
+                     timer_color_pick = window.setTimeout(function(){
+
+                         $('#color_change_val').trigger('change');
+                       //  $('.color_picker_for_sidebar_settings').trigger('change');
+
+
+
+
+                     },500);
+
+
+
+
+
+
             },
             method: 'inline'
         });
@@ -24,6 +51,12 @@ if ($is_color === null OR $is_color === false OR $is_color == '') {
 <div class="is-color-select" style="padding-top: 15px;">
     <label class="mw-ui-label"><?php _lang("Color as overlay", "templates/dream"); ?> ?</label>
     <div class="color_picker_for_sidebar_settings"></div>
+
+
+    <input id="color_change_val" type="hidden" name="is_color" placeholder="<?php _lang("Color name Or HEX", "templates/dream"); ?>" value="<?php print $is_color; ?>" class="mw-ui-field mw_option_field"/>
+
+
+
     <?php /*
     <input type="text" name="is_color" placeholder="<?php _lang("Color name Or HEX", "templates/dream"); ?>" value="<?php print $is_color; ?>" class="mw-ui-field mw_option_field color_picker_for_sidebar_settings"/>
 
