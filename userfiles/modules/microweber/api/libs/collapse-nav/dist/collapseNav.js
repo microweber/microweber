98d33f87d2df698121e4897a3fb3b45d74d7f134
Petko Yovchevski <pyovchevski@gmail.com>
2019-04-03 14:20:26 +0300

    updates

--- userfiles/modules/microweber/api/libs/collapse-nav/dist/collapseNav.js
+++ userfiles/modules/microweber/api/libs/collapse-nav/dist/collapseNav.js
@@ -56,58 +56,110 @@ function collapseNav(selector, config) {
         }
 
         var ul_width = $(navigation).outerWidth();
-        var li_width = 0;
-        var possible_buttons = -1;
-        var li_count = 0;
+        var li_width;
+        var possible_buttons;
+        var li_count;
 
-        //The New More Button Width
-        $(navigation).children().first().clone().appendTo(navigation);
-        $(navigation).children().last().find('a').html(more_text + ' ' + caret).css({'vissibility': 'hidden'});
-        var the_more_button_width = $(navigation).children().last();
-        li_width = li_width + the_more_button_width.outerWidth(true) + 50;
-        the_more_button_width.remove();
+        //console.log('ul: ' + ul_width);
+
+        /*---------------------------------------
+         --- Check base buttons to navigation  ---
+         --------------------------------------*/
+        li_width = 0;
+        possible_buttons = 0;
+        li_count = 0;
 
         $(navigation).children("li").each(function (i) {
             li_count = li_count + 1;
             li_width = li_width + $(this).outerWidth(true);
+
+            //console.log(li_count + ' li ' + $(this).text() + ': ' + li_width);
 
             if (ul_width >= li_width) {
                 possible_buttons = possible_buttons + 1;
             }
         });
 
+        // The navigation does not need a More menu, then stop the script
+        if (li_count <= possible_buttons) {
+            return;
+        }
+
+        //console.log('Possible buttons: ' + possible_buttons);
+
+
+        /*-------------------------------------------
+         --- Check the more buttons to navigation ---
+         ------------------------------------------*/
+        li_width = 0;
+        possible_buttons = 0;
+        li_count = 0;
+
+        //The More Button Width
+        $(navigation).children().first().clone().appendTo(navigation);
+        $(navigation).children().last().find('a').html(more_text + ' ' + caret).css({'vissibility': 'hidden'});
+        var the_more_button_width = $(navigation).children().last();
+
+        li_count = li_count + 1;
+        li_width = li_width + the_more_button_width.outerWidth(true);
+        //console.log(li_count + ' li More: ' + li_width);
+        the_more_button_width.remove();
+
+        $(navigation).children("li").each(function (i) {
+            li_count = li_count + 1;
+            li_width = li_width + $(this).outerWidth(true);
+
+            //console.log(li_count + ' li ' + $(this).text() + ': ' + li_width);
+
+            if (ul_width >= li_width) {
+                possible_buttons = possible_buttons + 1;
+            }
+        });
+
+        //console.log('Possible buttons: ' + (possible_buttons + 1) + ' + More button');
+
+        /*------------------
+         --- Some checks ---
+         -----------------*/
+
         if (responsive == 1) {
             number_of_buttons = possible_buttons;
         } else {
-            var number_of_buttons = config.number_of_buttons - 1;
+            var number_of_buttons = config.number_of_buttons;
             if (!number_of_buttons) {
-                number_of_buttons = 4 - 1;
+                number_of_buttons = 4;
             }
         }
+        //console.log('Number of buttons: ' + number_of_buttons);
 
         if ($(window).width() < mobile_break) {
             return;
         }
 
-        if (li_count <= possible_buttons + 3) {
-            return;
-        }
-
+        /*----------------------------------------
+         --- Convert the navigation to the new ---
+         ----------------------------------------*/
+        var btn_n = 0;
         var ul = '<ul class="' + li_ul_class + '">'
         $(navigation).children("li").each(function (i) {
-            if (i > number_of_buttons) {
+            btn_n = btn_n + 1;
+
+            if (btn_n > number_of_buttons) {
+                //console.log(btn_n + ' > ' + number_of_buttons);
+
                 ul += this.outerHTML;
             }
         })
         ul += '</ul>';
 
+        number_of_buttons = number_of_buttons - 1;
         $(navigation).children("li:gt(" + number_of_buttons + ")").remove();
         $(navigation).append('<li class="' + li_class + '"><a href="javascript:;" class="' + li_a_class + '" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">' + more_text + caret + '</a>' + ul + '</li>')
     }
 
     init(selector, config);
 
-    $(window).on('resize', function () {
+    $(window).on('resize collapseNavReInit', function () {
         $(navigation).html(original_navigation);
         init(navigation, configuration);
     });
