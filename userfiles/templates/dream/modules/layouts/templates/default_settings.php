a4d55be11c2a9098608aadd07cf57cef130be4bf
Peter Ivanov <peter@microweber.com>
2019-05-08 14:50:37 +0300

    updates

--- userfiles/templates/dream/modules/layouts/templates/default_settings.php
+++
@@ -1,6 +0,0 @@
-<div id="settings-holder">
-    <br/><br/>
-    <h5 style="font-weight: bold; display: block;"><?php _lang("Skin Settings", "templates/dream"); ?></h5>
-
-    <?php  include __DIR__.'/settings_padding.php'; ?>
-</div>
--- userfiles/templates/dream/modules/layouts/templates/skin-68_settings.php
+++
@@ -1,6 +0,0 @@
-<div id="settings-holder">
-    <br/><br/>
-    <h5 style="font-weight: bold; display: block;"><?php _e("Skin Settings") ?></h5>
-
-    <?php include('settings_height.php'); ?>
-</div>
