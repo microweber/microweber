c0fa3661b070e700e370618fe7a65708172520a9
b.slaveykov <b.slaveykov@radioactive.bg>
2019-05-14 10:48:36 +0300

    remove export path

--- userfiles/modules/admin/export/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-$config = array();
-$config['name'] = "Export";
-$config['author'] = "Microweber";
- 
-$config['categories'] = "admin"; 
-$config['version'] = 0.3;
-$config['ui_admin'] = true;
-$config['position'] = 99;
