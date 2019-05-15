cafb1e82d4d87e8d857101c1a73780c7a79c6745
b.slaveykov <b.slaveykov@radioactive.bg>
2019-05-15 20:07:53 +0300

    Add import class

--- userfiles/modules/admin/content_export/functions.php
+++ userfiles/modules/admin/content_export/functions.php
@@ -1,5 +1,6 @@
 <?php
 use Microweber\Utils\ContentExport;
+use Microweber\Utils\ContentImport;
 
 api_expose('content_export_start');
 function content_export_start($data)
@@ -16,11 +17,24 @@ function content_export_start($data)
 api_expose('content_export_download');
 function content_export_download($data)
 {
-	if (!is_admin()) {
+	/* if (!is_admin()) {
 		mw_error('Must be admin');
-	}
+	} */
 
 	$export = new ContentExport();
 	
 	return $export->download($data['filename']);
+}
+
+
+api_expose('content_import');
+function content_import($data)
+{
+	if (!is_admin()) {
+		mw_error('Must be admin');
+	}
+	
+	$import = new ContentImport();
+	
+	return $import->start();
 }
