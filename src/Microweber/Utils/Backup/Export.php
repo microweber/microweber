38c854732935feb116426d4c4da4bc4193096402
b.slaveykov <b.slaveykov@radioactive.bg>
2019-05-30 19:01:06 +0300

    update

--- src/Microweber/Utils/Backup/Export.php
+++ src/Microweber/Utils/Backup/Export.php
@@ -81,6 +81,27 @@ class Export
 			$tableContent = $this->_getTableContent($table, $ids);
 			
 			if (!empty($tableContent)) {
+				
+				$relations = array();
+				foreach($tableContent as $content) {
+					if (isset($content['rel_type']) && isset($content['rel_id'])) {
+						$relations[$content['rel_type']][$content['rel_id']] = $content['rel_id'];
+					}
+				}
+				
+				if (!empty($relations)) {
+					foreach($relations as $relationTable=>$relationIds) {
+						$relationTableContent = $this->_getTableContent($relationTable, $relationIds);
+						
+						
+						var_dump($relationTableContent);
+						die();
+					}
+				}
+				
+				var_dump($relations);
+				die();
+				
 				$readyContent[$table] = $tableContent;
 			}
 		}
@@ -177,137 +198,5 @@ class Export
 		}
 		
 		return $readyTableList;
-	}
-	
-	public function getContentX()
-	{
-		$export_only_ids = array();
-		$content_ids = array();
-		$categories_ids = array();
-		$export_items = array();
-
-
-		$skip_tables = array(
-			"modules",
-			"elements",
-			"users",
-			"log",
-			"notifications",
-			"content_revisions_history",
-			'module_templates',
-			"stats_users_online",
-			"stats_browser_agents",
-			"stats_referrers_paths",
-			"stats_referrers_domains",
-			"stats_referrers",
-			"stats_visits_log",
-			"stats_urls",
-			"system_licenses",
-			"users_oauth",
-			"sessions"
-		);
-
-		$all_tables = array();
-		$all_tables_with_rel = array();
-
-		$all_tables_raw = mw()->database_manager->get_tables_list();
-		$local_prefix = mw()->database_manager->get_prefix();
-
-		foreach ($all_tables_raw as $k => $v) {
-			if ($local_prefix) {
-				$v = str_replace_first($local_prefix, '', $v);
-				$all_tables[] = $v;
-			} else {
-				$all_tables[] = $v;
-			}
-		}
-		$exported_tables_data = array();
-		if ($all_tables) {
-
-			foreach ($all_tables as $table) {
-				if (! in_array($table, $skip_tables)) {
-					$table_exists = mw()->database_manager->table_exists($table);
-					if ($table_exists) {
-						$db_export_params = array();
-						$db_export_params['no_limit'] = 1;
-						$db_export_params['do_not_replace_site_url'] = 1;
-
-						$has_rel_field = false;
-
-						$table_fields = mw()->database_manager->get_fields($table);
-
-						if ($table_fields) {
-							foreach ($table_fields as $table_field) {
-
-								if (strtolower($table_field) == 'rel_type') {
-									$has_rel_field = true;
-								}
-							}
-						}
-
-						if ($table == 'content') {
-							if ($content_ids) {
-								$rel_ids_implode = implode(',', $content_ids);
-
-								$db_export_params['id'] = '[in]' . $rel_ids_implode;
-							}
-						} elseif ($table == 'categories') {
-							if ($categories_ids) {
-								$rel_ids_implode = implode(',', $categories_ids);
-
-								$db_export_params['id'] = '[in]' . $rel_ids_implode;
-							}
-						} elseif ($has_rel_field) {
-
-							$all_tables_with_rel[] = $table;
-						}
-
-						$table_content = array();
-						$table_content2 = array();
-
-						$table_content = db_get($table, $db_export_params);
-
-						if (! $export_only_ids) {} else {
-
-							if ($has_rel_field) {
-								foreach ($export_only_ids as $rel_key => $rel_ids) {
-									$rel_ids_implode = implode(',', $rel_ids);
-
-									$db_export_params['rel_type'] = $rel_key;
-									$db_export_params['rel_id'] = '[in]' . $rel_ids_implode;
-
-									$table_content_rel = db_get($table, $db_export_params);
-
-									if ($table_content_rel) {
-										$table_content2 = array_merge($table_content2, $table_content_rel);
-									}
-								}
-								// /$export_only_ids
-							} else {
-								// $table_conent = db_get($table, $db_export_params);
-							}
-						}
-
-						if ($table_content) {
-							if (isset($exported_tables_data[$table])) {
-								$exported_tables_data[$table] = array_merge($exported_tables_data[$table], $table_content);
-							} else {
-								$exported_tables_data[$table] = $table_content;
-							}
-						}
-						if ($table_content2) {
-							$exported_tables_data[$table] = array_merge($exported_tables_data[$table], $table_content2);
-						}
-					}
-				}
-			}
-		}
-		array_walk_recursive($exported_tables_data, function (&$item) {
-			if (is_string($item)) {
-				$item = utf8_encode($item);
-			}
-		});
-
-		return $this->exportAsType($exported_tables_data);
 	}
 }
