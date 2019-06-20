8cfa2f59b4c391e1ec15f084decc4d39417ddcc3
b.slaveykov <b.slaveykov@radioactive.bg>
2019-06-20 15:27:43 +0300

    update

--- src/Microweber/Utils/Backup/DatabaseSave.php
+++ src/Microweber/Utils/Backup/DatabaseSave.php
@@ -16,18 +16,49 @@ class DatabaseSave
 		$tableData['skip_cache'] = true;
 		$tableData['allow_html'] = true;
 		$tableData['allow_scripts'] = true;
-		$tableData['extended_save'] = true;
 		
 		$tableData = self::_fixContentEncoding($tableData);
 
 		if ($table == 'content') {
-			return save_content($tableData);
+			return self::_saveContent($tableData);
 		} else {
 			return db_save($table, $tableData);
 		}
 		
 	}
+	
+	private static function _saveContent($tableData) 
+	{
+		$tableData['extended_save'] = true;
+		
+		if (isset($tableData['categories'])) {
+			//$mainCategory = explode(',', $tableData['category'])[0];
+			$tableData['parent'] = self::_getParentPageId();;
+		}
+		
+		return save_content($tableData);
+	}
 
+	private static function _getParentPageId() {
+		
+		$pages = get_content('content_type=page&subtype=dynamic&limit=1000');
+		
+		if (empty($pages)) {
+				
+			$params = array(
+				'title' => 'My categories page',
+				'content_type' => 'page',
+				'subtype' => 'dynamic',
+				
+				'is_active' => 1,);
+			
+			//saving
+			return save_content($params);
+		} else {
+			return $pages[0]['id'];
+		}
+	}
+	
 	/**
 	 * Fix wrong encoding on database
 	 *
