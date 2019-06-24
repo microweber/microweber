3f903103eef356d572c6ea2badeae1bb61b8a9b7
b.slaveykov <b.slaveykov@radioactive.bg>
2019-06-24 11:59:14 +0300

    update

--- userfiles/modules/contact_form/integrations.php
+++ userfiles/modules/contact_form/integrations.php
@@ -1,6 +1,21 @@
-<?php 
+<?php
+only_admin_access();
 include 'mail_providers.php';
 ?>
+<script type="text/javascript">
+    $(document).ready(function () {
+        mw.options.form('.<?php print $config['module_class'] ?>', function () {
+            mw.notification.success("<?php _e("All changes are saved"); ?>.");
+        });
+    });
+</script>
+<?php
+$mod_id = $params['id'];
+if(isset($params['for_module_id'])){
+    $mod_id = $params['for_module_id'];
+}
+?>
+
 <div class="mw-accordion">
 
 	<?php foreach(get_mail_providers() as $mailProvider): ?>
@@ -15,7 +30,7 @@ include 'mail_providers.php';
 			<?php foreach ($mailProvider['fields'] as $field): ?>
 			<div class="demobox">
                 <label class="mw-ui-label"><?php echo $field['title']; ?></label>
-                <input type="text" name="<?php echo $field['name']; ?>" class="mw-ui-field" style="width:100%;">
+                <input type="text" option-group="<?php print $mod_id ?>" value="<?php print get_option('mail_field_' . $field['name'], $mod_id); ?>" name="<?php echo $field['name']; ?>" class="mw-ui-field w100 mw_option_field">
             </div>
             <br />
             <?php endforeach; ?>
