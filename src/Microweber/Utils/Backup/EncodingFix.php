46391eca8579b53db1f00dc6a46ab8ef9f6696c0
b.slaveykov <b.slaveykov@radioactive.bg>
2019-07-01 19:48:59 +0300

    update

--- src/Microweber/Utils/Backup/EncodingFix.php
+++ src/Microweber/Utils/Backup/EncodingFix.php
@@ -22,44 +22,10 @@ class EncodingFix
 		// Fix content encoding
 		array_walk_recursive($content, function (&$item) {
 			if (is_string($item)) {
-				$item = self::fixLetters($item);
+				$item = utf8_decode($item);
 			}
 		});
 		
-		//var_dump($content);
-		
 		return $content;
-	}
-	
-	public static function fixLetters($string) {
-		
-		$readyString = '';
-		
-		$letters = self::utf8Split($string);
-		
-		if (is_array($letters)) {
-			foreach($letters as $letter) {
-				
-				//$letter = utf8_encode($letter);
-				//$letter = mb_substr($letter, 0, 1);
-				
-				//$letter = str_replace('Â ', ' ', $letter);
-				//$letter = str_replace("Â ", ' ', $letter);
-				
-				$readyString .= $letter;
-			}
-		}
-		
-		return $readyString;
-	}
-	
-	public static function utf8Split($str, $len = 1)
-	{
-		$arr = array();
-		$strLen = mb_strlen($str);
-		for ($i = 0; $i < $strLen; $i ++) {
-			$arr[] = mb_substr($str, $i, $len);
-		}
-		return $arr;
 	}
 }
