9237b36b4227135f1d6a2ddf653f9b7c6aa2c7ae
b.slaveykov <b.slaveykov@radioactive.bg>
2019-07-15 16:18:26 +0300

    update

--- src/Microweber/Utils/Backup/Exporters/CsvExport.php
+++ src/Microweber/Utils/Backup/Exporters/CsvExport.php
@@ -1,41 +1,68 @@
 <?php
 namespace Microweber\Utils\Backup\Exporters;
 
+use Microweber\Utils\Backup\Loggers\BackupExportLogger;
+
 class CsvExport extends DefaultExport
 {
 
 	public function start()
 	{
 		
-		$contents = array();
+		$export = array();
 		
-		$readyContent = array();
-		$readyContent['id'] = 'id';
-		$readyContent['content_type'] = 'content_type';
-		$readyContent['title'] = 'title';
-		$readyContent['url'] = 'url';
-		$readyContent['content_body'] = 'content_body';
-		$contents[] = $readyContent;
-		
-		if (isset($this->data['content'])) {
-			foreach ($this->data['content'] as $content) {
+		if (!empty($this->data)) {
+			foreach ($this->data as $tableName=>$tableData) {
 				
-				$readyContent = array();
-				$readyContent['id'] = $content['id'];
-				$readyContent['content_type'] = $content['content_type'];
-				$readyContent['title'] = $content['title'];
-				$readyContent['url'] = $content['url'];
-				$readyContent['content_body'] = trim($content['content']);
-				
-				$contents[] = $readyContent;
-				
+				foreach($tableData as $item) {
+					
+					if ($tableName == 'content') {
+						$readyItem = array();
+						$readyItem['id'] = $item['id'];
+						$readyItem['content_type'] = $item['content_type'];
+						$readyItem['title'] = $item['title'];
+						$readyItem['url'] = $item['url'];
+						$readyItem['content_body'] = trim($item['content']);
+						
+						
+						$export[$tableName][] = $readyItem;
+					} else {
+						$export[$tableName][] = $item;
+					}
+				}
 			}
 		}
 		
-		echo $this->array2csv($contents);
+		$exportedFiles = array();
 		
-		//var_dump($contents);
-		die();
+		if (!empty($export)) {
+			
+			foreach($export as $tableName=>$exportData) {
+			
+				$generateFileName = $this->_generateFilename($tableName);
+		 		
+				$csv = \League\Csv\Writer::createFromPath($generateFileName['filepath'], 'w');
+				$csv->setEncodingFrom('UTF-8');
+				$csv->setOutputBOM(\League\Csv\Writer::BOM_UTF8);
+				$csv->insertOne(array_keys(reset($exportData)));
+				$csv->insertAll($exportData);
+				
+				$exportedFiles[] = $generateFileName;
+			}
+		}
+		
+		$zip = new CsvZipExport();
+		foreach ($exportedFiles as $file) {
+			$zip->addFile($file);
+		}
+		$zipFile = $zip->start();
+		
+		// Remove files
+		foreach ($exportedFiles as $file) {
+			unlink($file['filepath']);
+		}
+		
+		return $zipFile;
 	}
 
 	public function array2csv($data, $delimiter = ',', $enclosure = '"', $escape_char = "\\")
@@ -48,5 +75,54 @@ class CsvExport extends DefaultExport
 		rewind($f);
 		return stream_get_contents($f);
 		
+	}
+}
+
+class CsvZipExport extends ZipExport {
+	
+	protected $_files = array();
+	
+	public function addFile($file) {
+		$this->_files[] = $file;
+	}
+	
+	public function start() {
+		
+		// Clear cache
+		mw()->cache_manager->clear();
+		
+		// Get zip filename
+		$zipFileName = $this->_getZipFileName();
+		
+		// Generate zip file
+		$zip = new \ZipArchive();
+		$zip->open($zipFileName['filepath'], \ZipArchive::CREATE);
+		$zip->setArchiveComment("Microweber backup of the userfiles folder and db.
+                \nThe Microweber version at the time of backup was ".MW_VERSION."
+                \nCreated on " . date('l jS \of F Y h:i:s A'));
+		
+		foreach ($this->_files as $file) {
+			BackupExportLogger::setLogInfo('Archiving file <b>'. $file['filename'] . '</b>');
+			$zip->addFile($file['filepath'], $file['filename']);
+		}
+		
+		
+		$userFiles = $this->_getUserFilesPaths();
+		
+		if (!empty($userFiles)) {
+			foreach($userFiles as $file) {
+				BackupExportLogger::setLogInfo('Archiving file <b>'. $file['dataFile'] . '</b>');
+				$zip->addFile($file['filePath'], $file['dataFile']);
+			}
+		}
+		
+		$zip->setCompressionIndex(0, \ZipArchive::CM_STORE);
+		$zip->close();
+
+		BackupExportLogger::setLogInfo('All files are ziped.');
+		
+		$this->_finishUp();
+		
+		return $zipFileName;
 	}
 }
