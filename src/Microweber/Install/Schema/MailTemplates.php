81346df9c3b18d08177545872679e23a211f9937
b.slaveykov <b.slaveykov@radioactive.bg>
2019-07-17 16:34:07 +0300

    update

---
+++ src/Microweber/Install/Schema/MailTemplates.php
@@ -0,0 +1,24 @@
+<?php
+namespace Microweber\Install\Schema;
+
+class MailTemlates
+{
+	public function get()
+	{
+		return [
+			'mail_templates' => [
+				'type' => 'string',
+				'name' => 'string',
+				'subject' => 'string',
+				'message' => 'string',
+				'attachments' => 'string',
+				'from_name' => 'string',
+				'from_email' => 'string',
+				'custom' => 'string',
+				'copy_to' => 'string',
+				'plain_text' => 'integer',
+				'is_active' => 'integer'
+			]
+		];
+	}
+}
