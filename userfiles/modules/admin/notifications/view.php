12805e9fdf1796fa9e52f25f22a8e5431c9aaa73
b.slaveykov <b.slaveykov@radioactive.bg>
2019-07-29 14:11:29 +0300

    update

--- userfiles/modules/admin/notifications/view.php
+++ userfiles/modules/admin/notifications/view.php
@@ -2,11 +2,13 @@
 only_admin_access();
 
 $notification_id = (int) $params['notification_id'];
-$notification_module = (int) $params['notification_module'];
+$notification_module = $params['notification_module'];
 ?>
 
-<?php 
-if ($notification_module == 'contact_form') :
+<?php
+if ($notification_module === 'contact_form') :
 ?>
-<module type
+<module type="contact_form/view" notification_id="<?php echo $notification_id; ?>" />
+<?php else: ?>
+
 <?php endif; ?>
