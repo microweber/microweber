4765fbe05997aea877e7147c9e8256c01d73b19c
b.slaveykov <b.slaveykov@radioactive.bg>
2019-07-31 18:00:52 +0300

    update

--- userfiles/modules/comments/src/Jobs/JobSendMailNotificationOnComment.php
+++ userfiles/modules/comments/src/Jobs/JobSendMailNotificationOnComment.php
@@ -1,48 +1,88 @@
 <?php
-
 namespace Microweber\Comments\Jobs;
 
-
-use \Illuminate\Bus\Queueable;
-use \Illuminate\Queue\SerializesModels;
-use \Illuminate\Queue\InteractsWithQueue;
-use \Illuminate\Contracts\Queue\ShouldQueue;
-
-use \Illuminate\Foundation\Bus\Dispatchable;
-use \Illuminate\Contracts\Queue\Job;
-use \Microweber\Comments\Models\Comment as Comment;
-
-use \Illuminate\Contracts\Mail\Mailer;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Bus\Queueable;
+use Microweber\Comments\Models\Comment as Comment;
+use Microweber\App\Jobs\JobSendEmail;
 
 
-class SendEmailJob implements ShouldQueue
+class JobSendMailNotificationOnComment implements ShouldQueue
 {
-    // use Dispatchable, InteractsWithQueue, Queueable;
-    use Dispatchable, InteractsWithQueue, Queueable;
+	use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
-    public $timeout = 120;
+	public $timeout = 120;
 
+	private $commentId;
 
-    private $comment;
+	/**
+	 * Create a new job instance.
+	 *
+	 * @return void
+	 */
+	public function __construct($commentId)
+	{
+		$this->commentId = $commentId;
+	}
 
-    /**
-     * Create a new job instance.
-     *
-     * @return void
-     */
-    public function __construct(Comment $comment)
-    {
-        $this->comment = $comment;
-    }
-
-    /**
-     * Execute the job.
-     *
-     * @return void
-     */
-    public function handle(Mailer $mailer)
-    {
-
-
-    }
+	/**
+	 * Execute the job.
+	 *
+	 * @return void
+	 */
+	public function handle()
+	{	
+		$newComment = get_comments('single=1&id=' . $this->commentId);
+		if (empty($newComment)) {
+			echo 'No comment found.';
+			return;
+		}
+		
+		$newCommentMailTemplateId = mw()->option_manager->get('new_comment_reply_email_template', 'comments');
+		$mailTemplate = get_mail_template_by_id($newCommentMailTemplateId, 'new_comment_reply');
+		
+		$comments = get_comments('content_id=' . $newComment['rel_id']);
+		
+		$commentsMailMap = array();
+		foreach ($comments as $comment) {
+			
+			$emailTo = $comment['comment_email'];
+			
+			if (array_key_exists($emailTo, $commentsMailMap)) {
+				continue;
+			}
+			
+			$commentsMailMap[$emailTo] = $comment;
+			
+			try {
+				$twig = new \Twig_Environment(new \Twig_Loader_String());
+				
+				$commentEmailContent = $twig->render(
+					$mailTemplate['message'],
+					array('comment_author' => $comment['comment_name'], 'comment_reply_author' => $newComment['comment_name'], 'post_url'=>$comment['from_url'])
+				);
+				
+				if (isset($emailTo) and (filter_var($emailTo, FILTER_VALIDATE_EMAIL))) {
+					
+					$jobSendEmail = new JobSendEmail();
+					$jobSendEmail->setEmailTo($emailTo);
+					$jobSendEmail->setEmailSubject($mailTemplate['subject']);
+					$jobSendEmail->setEmailMessage($commentEmailContent);
+					$jobSendEmail->setEmailFrom($mailTemplate['from_email']);
+					$jobSendEmail->setEmailFromName($mailTemplate['from_name']);
+					$jobSendEmail->onQueue('processing');
+					
+					\Queue::later(3, $jobSendEmail);
+					
+					return true;
+				}
+			} catch (\Exception $e) {
+				echo $e->getMessage();
+			}
+		}
+		
+	}
 }
