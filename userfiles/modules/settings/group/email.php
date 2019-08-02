c984f3171afbeee6c7e1dd4c6aaf39521fd946ce
b.slaveykov <b.slaveykov@radioactive.bg>
2019-08-02 15:22:56 +0300

    update

--- userfiles/modules/settings/group/email.php
+++ userfiles/modules/settings/group/email.php
@@ -1,8 +1,5 @@
 <?php only_admin_access(); ?>
 <script  type="text/javascript">
-/*
-
-<button onClick="saveEmailOptions()" class="mw-ui-btn mw-ui-btn-medium mw-ui-btn-notification">Save email settings</button>
 
 function saveEmailOptions() {
 	$("input, select, textarea", $('.<?php print $config['module_class'] ?>')).each(function () {
@@ -18,17 +15,17 @@ $(document).ready(function(){
 	$('.js-email-transport-select').change(function() {
 		saveEmailOptions();
 	});
-});*/
+});
 
 
-
+/*
 $(document).ready(function(){
     mw.options.form(".<?php print $config['module_class'] ?>", function () {
         mw.notification.success("<?php _e("Email settings are saved"); ?>.");
         mw.reload_module("<?php print $config['module'] ?>");
     });
 });
-
+*/
 mw.email_send_test = function(){
 
 	var email_to = {}
@@ -55,6 +52,14 @@ mw.email_send_test = function(){
 
 <div class="<?php print $config['module_class'] ?> mw-ui-box">
 	<div class="mw-ui-box-content">
+	
+		<div class="mw-flex-row">
+		
+		<div class="mw-flex-col-xs-12">
+			<h2>Email Settings</h2><p>Set the email transport settings to your website.</p><br />
+		</div>
+		
+		<div class="mw-flex-col-xs-6">
 		<div class="mw-ui-field-holder">
 			<label class="mw-ui-label">
 				<?php _e("Your email address"); ?>
@@ -66,6 +71,9 @@ mw.email_send_test = function(){
 				)</small> </label>
 			<input name="email_from" class="mw_option_field mw-ui-field" type="email" type="text" option-group="email"   value="<?php print get_option('email_from','email'); ?>" />
 		</div>
+		</div>
+		
+		<div class="mw-flex-col-xs-6">
 		<div class="mw-ui-field-holder">
 			<label class="mw-ui-label">
 				<?php _e("From name"); ?>
@@ -77,44 +85,50 @@ mw.email_send_test = function(){
 				)</small> </label>
 			<input name="email_from_name" class="mw_option_field mw-ui-field"    type="text" option-group="email"   value="<?php print get_option('email_from_name','email'); ?>" />
 		</div>
+		</div>
+		</div>
 		
 		<hr>
 		
 		<div class="mw-ui-box">
 			<div class="mw-ui-box-header"><?php _e("Mail Send Settings"); ?></div>
 			<div class="mw-ui-box-content">
-
-<div class="mw-ui-field-holder">
-    <label class="mw-ui-label">
-        <?php _e("Send email function"); ?>
-    </label>
-    <?php
-        $email_transport = get_option('email_transport','email');
-        if($email_transport == false){
-            $email_transport = 'php';
-        }
-    ?>
-    <select name="email_transport" class="mw-ui-field mw_option_field js-email-transport-select"   type="text" option-group="email" data-refresh="settings/group/email">
-        <option value="php" <?php if($email_transport == 'php'): ?> selected="selected" <?php endif; ?>>
-        <?php _e("PHP mail function"); ?>
-        </option>
-        <option value="gmail" <?php if($email_transport == 'gmail'): ?> selected="selected" <?php endif; ?>>
-        <?php _e("GMail"); ?>
-        </option>
-      <?php  /* <option value="yahoo" <?php if($email_transport == 'yahoo'): ?> selected="selected" <?php endif; ?>>
-        <?php _e("Yahoo"); ?>
-        </option>
-        <option value="hotmail" <?php if($email_transport == 'hotmail'): ?> selected="selected" <?php endif; ?>>
-        <?php _e("HotMail"); ?>
-        </option>*/  ?>
-        <option value="smtp" <?php if($email_transport == 'smtp'): ?> selected="selected" <?php endif; ?>>
-        <?php _e("SMTP server"); ?>
-        </option>
-    </select>
-</div>
-
+			<div class="mw-flex-row">
+				
+				<div class="mw-flex-col-xs-12">
+				<div class="mw-ui-field-holder">
+				    <label class="mw-ui-label">
+				        <?php _e("Send email function"); ?>
+				    </label>
+				    <?php
+				        $email_transport = get_option('email_transport','email');
+				        if($email_transport == false){
+				            $email_transport = 'php';
+				        }
+				    ?>
+				    <select name="email_transport" class="mw-ui-field mw_option_field js-email-transport-select"   type="text" option-group="email" data-refresh="settings/group/email">
+				        <option value="php" <?php if($email_transport == 'php'): ?> selected="selected" <?php endif; ?>>
+				        <?php _e("PHP mail function"); ?>
+				        </option>
+				        <option value="gmail" <?php if($email_transport == 'gmail'): ?> selected="selected" <?php endif; ?>>
+				        <?php _e("GMail"); ?>
+				        </option>
+				      <?php  /* <option value="yahoo" <?php if($email_transport == 'yahoo'): ?> selected="selected" <?php endif; ?>>
+				        <?php _e("Yahoo"); ?>
+				        </option>
+				        <option value="hotmail" <?php if($email_transport == 'hotmail'): ?> selected="selected" <?php endif; ?>>
+				        <?php _e("HotMail"); ?>
+				        </option>*/  ?>
+				        <option value="smtp" <?php if($email_transport == 'smtp'): ?> selected="selected" <?php endif; ?>>
+				        <?php _e("SMTP server"); ?>
+				        </option>
+				    </select>
+				</div>
+				</div>
 				
 				<?php if($email_transport == 'smtp' or $email_transport == 'gmail' or $email_transport == 'yahoo' or $email_transport == 'hotmail' or $email_transport == 'smtp'): ?>
+                
+                <div class="mw-flex-col-xs-6">
                 <div class="mw-ui-field-holder">
 				<label class="mw-ui-label"><?php print ucfirst($email_transport); ?>
 					<?php _e("Username"); ?>
@@ -126,14 +140,23 @@ mw.email_send_test = function(){
 					</small></label>
 				<input name="smtp_username" class="mw_option_field mw-ui-field"   type="text" option-group="email"  value="<?php print get_option('smtp_username','email'); ?>" />
                 </div>
+                </div>
+                
+                <div class="mw-flex-col-xs-6">
                <div class="mw-ui-field-holder">
 				<label class="mw-ui-label"><?php print ucfirst($email_transport); ?>
 					<?php _e("Password"); ?>
+					<br />
+					<small>&nbsp;</small>
 				</label>
 				<input name="smtp_password" class="mw_option_field mw-ui-field"   type="password" option-group="email"  value="<?php print get_option('smtp_password','email'); ?>" />
                 </div>
+                </div>
+                
 				<?php endif; ?>
+				
 				<?php if($email_transport == 'smtp'): ?>
+				 <div class="mw-flex-col-xs-6">
                 <div class="mw-ui-field-holder">
 				<label class="mw-ui-label">
 					<?php _e("Smtp Email Host"); ?>
@@ -143,6 +166,9 @@ mw.email_send_test = function(){
 					: smtp.gmail.com</small></label>
 				<input name="smtp_host" class="mw_option_field mw-ui-field"   type="text" option-group="email"  value="<?php print get_option('smtp_host','email'); ?>" />
 				</div>
+				</div>
+				
+				 <div class="mw-flex-col-xs-6">
                 <div class="mw-ui-field-holder">
 				<label class="mw-ui-label">
 					<?php _e("Smtp Email Port"); ?>
@@ -152,6 +178,9 @@ mw.email_send_test = function(){
 					: 587 or 995, 465, 110, 25</small></label>
 				<input name="smtp_port" class="mw_option_field mw-ui-field"   type="text" option-group="email"  value="<?php print get_option('smtp_port','email'); ?>" />
 				 </div>
+				 </div>
+				 
+				  <div class="mw-flex-col-xs-6">
                  <div class="mw-ui-field-holder">
 				<label class="mw-ui-label">
 					<?php _e("Enable SMTP authentication"); ?>
@@ -170,6 +199,9 @@ mw.email_send_test = function(){
 						</option>
 					</select>
 			   </div>
+			   </div>
+			   
+			    <div class="mw-flex-col-xs-6">
 				<div class="mw-ui-field-holder">
 				<label class="mw-ui-label">
 					<?php _e("Enable SMTP Secure Method"); ?>
@@ -188,32 +220,50 @@ mw.email_send_test = function(){
 						</option>
 					</select>
                  </div>
+                 </div>
 				<?php endif; ?>
-
+				
+				<div class="mw-flex-col-xs-12">
 				<table width=" 100%" border="0" id="test_eml_toggle"  class="mw-ui-box mw-ui-box-content" style="display:none;background: white;">
 					<tr>
-						<td><label class="mw-ui-label">
+						<td>
+							<div class="mw-flex-row">
+							<div class="mw-flex-col-xs-12"><h3>Make a test email</h3><p>Send test email to check settings are they work correctly.</p><br /></div>
+							<div class="mw-flex-col-xs-5">
+							<label class="mw-ui-label">
 								<?php _e("Send test email to"); ?>
 							</label>
 							<input name="test_email_to" id="test_email_to" class="mw_option_field mw-ui-field"   type="text" option-group="email" value="<?php print get_option('test_email_to','email'); ?>"  />
-
+							</div>
+							<div class="mw-flex-col-xs-5">
 							<label class="mw-ui-label">
 								<?php _e("Test mail subject"); ?>
 							</label>
 							<input name="test_email_subject" id="test_email_subject" class="mw_option_field mw-ui-field"   type="text" option-group="email"   value="<?php print get_option('test_email_subject','email'); ?>"  />
-							
-							<span onclick="mw.email_send_test();" class="mw-ui-btn mw-ui-btn-green" id="email_send_test_btn">
-							<?php _e("Send test email"); ?>
-							</span></td>
+							</div>
+							<div class="mw-flex-col-xs-2">
+							<label class="mw-ui-label">&nbsp;</label>
+							<span onclick="mw.email_send_test();" class="mw-ui-btn mw-ui-btn-outline mw-ui-btn-notification" style="width:100%;" id="email_send_test_btn">
+							<span class="mw-icon-forward"></span> <?php _e("Send Test Email"); ?>
+							</span>
+							</div>
+							</div>
+						</td>
 						<td><pre id="email_send_test_btn_output"></pre></td>
 					</tr>
 				</table>
+				<br />
+				<a class="mw-ui-btn mw-ui-btn-outline mw-ui-btn-info" href="javascript:$('#test_eml_toggle').toggle(); void(0);"><span class="mw-icon-beaker"></span> <?php _e("Test Mail Sending Method"); ?></a> 
+				</div>
 				
-				<a class="mw-ui-btn " href="javascript:$('#test_eml_toggle').toggle(); void(0);"><?php _e("Test Method"); ?></a> </div>
-		</div>
+				</div>
+				</div>
+				</div>
+			<br />
 		
+			<button onClick="saveEmailOptions()" class="mw-ui-btn mw-ui-btn-outline mw-ui-btn-notification"><span class="mw-icon-check"></span> <?php _e("Save email settings"); ?></button>
 
-
+		<br />
 	</div>
 </div>
 </div>
