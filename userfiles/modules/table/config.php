d0bd240e6ee29134780fd6703ad8b182dbf7aaa8
Nick Woolley <nick@estores.org.uk>
2019-08-02 09:10:49 +0100

    table module updates (#593)

--- userfiles/modules/table/config.php
+++ userfiles/modules/table/config.php
@@ -7,4 +7,4 @@ $config['ui'] = true; //if set to true, module will be visible in the toolbar
 $config['ui_admin'] = false; //if set to true, module will be visible in the admin panel
 $config['categories'] = "content";
 $config['position'] = 99;
-$config['version'] = 0.1;
+$config['version'] = 0.2;
