fe2072112ebfa42494d3c91387071053c056694c
b.slaveykov <b.slaveykov@radioactive.bg>
2019-08-05 12:52:37 +0300

    update

--- userfiles/modules/microweber/custom_fields/hidden.php
+++ userfiles/modules/microweber/custom_fields/hidden.php
@@ -1,5 +1,1 @@
-<div class="control-group form-group">
-        <div class="controls">
-			<input type="hidden" data-custom-field-id="<?php print $data["id"]; ?>" value="<?php print $data["value"]; ?>" name="<?php print $data["name"]; ?>" />
-        </div>
-</div>
+<input type="hidden" data-custom-field-id="<?php print $data["id"]; ?>" value="<?php print $data["value"]; ?>" name="<?php print $data["name"]; ?>" />
