0a6f131adb7ecad051f48314e27af6a0003cefcf
Petko Yovchevski <pyovchevski@gmail.com>
2019-08-07 14:03:13 +0300

    updates

---
+++ userfiles/modules/editor/stylesheet_compiler/index.php
@@ -0,0 +1,136 @@
+<?php
+$template_settings = mw()->template->get_config();
+$stylesheet_settings = false;
+if (isset($template_settings['stylesheet_compiler']) AND isset($template_settings['stylesheet_compiler']['settings'])) {
+    $stylesheet_settings = $template_settings['stylesheet_compiler']['settings'];
+}
+
+
+if (!$stylesheet_settings) {
+    return;
+}
+
+$option_group = 'mw-template-' . mw()->template->folder_name();
+
+if ($stylesheet_settings) {
+    foreach ($stylesheet_settings as $key => $setting) {
+
+        $$key = get_option($key, $option_group);
+        if ($$key == '') {
+            $$key = $setting['default'];
+        }
+
+    }
+}
+
+//dd($template_settings);
+?>
+
+
+<style>
+    #color-scheme {
+        display: none;
+    }
+
+    .theme-color-selector {
+        width: 100%;
+        display: block;
+        line-height: 34px;
+    }
+
+    .theme-color-selector:after {
+        width: 100%;
+        display: block;
+        clear: both;
+        content: '';
+    }
+
+    .theme-color-selector button {
+        border: 1px solid transparent;
+        width: 30px;
+        height: 30px;
+        background: #425cbb;
+        margin: 3px;
+        outline: none !important;
+        border: 1px solid #000000;
+        float: left;
+        margin-right: 10px;
+    }
+
+    .bootstrap3ns .checkbox label, .bootstrap3ns .radio label {
+        padding-left: 0;
+    }
+</style>
+
+<script>mw.lib.require('bootstrap3ns');</script>
+
+<script>
+    $(document).ready(function () {
+        mw.options.form("#settings-holder", function () {
+            reloadTemplate();
+        });
+    });
+
+    function reloadTemplate() {
+        parent.mw.notification.success("<?php _e("Template settings are saved"); ?>.");
+        parent.$("#theme-style").attr('href', '<?php print mw()->template->get_stylesheet($template_settings['stylesheet_compiler']['source_file'], false, false); ?>&t=' + mw.random());
+        mw.tools.refresh(parent.$("#theme-style"));
+    }
+
+    function deleteCompiledCSS() {
+        $.get(mw.settings.api_url + "template/delete_compiled_css?path=assets/less/theme.less&option_group=mw-template-dream", function () {
+            // Delete
+            reloadTemplate();
+            window.parent.mw.drag.save();
+            window.parent.location.reload(false);
+        });
+    }
+</script>
+
+
+<div id="settings-holder">
+    <div class="col-12">
+        <h5 style="font-weight: bold;"><?php _e("Stylesheet Settings"); ?></h5>
+    </div>
+
+    <div class="bootstrap3ns">
+        <?php if ($stylesheet_settings): ?>
+            <?php foreach ($stylesheet_settings as $key => $setting): ?>
+                <?php if ($setting['type'] == 'color'): ?>
+                    <div class="form-group" style="margin-bottom:5px;">
+                        <div class="theme-color-selector">
+                            <button style="background: <?php echo $$key ?>;" id="<?php echo $key; ?>"></button>
+                            <input class="mw-ui-field mw_option_field hidden" name="<?php echo $key; ?>" value="<?php echo $$key ?>" data-option-group="<?php echo $option_group; ?>" placeholder="Default color: <?php echo $setting['default']; ?>">
+                            <?php echo $setting['label']; ?>
+                        </div>
+
+                        <script>
+                            $(window).load(function () {
+                                mw.colorPicker({
+                                    element: '#<?php echo $key; ?>',
+                                    position: 'bottom-left',
+                                    onchange: function (color) {
+                                        $('#<?php echo $key; ?>').css('background', color);
+                                        $('input[name="<?php echo $key; ?>"]').val(color);
+                                        $('input[name="<?php echo $key; ?>"]').trigger('change');
+                                    }
+                                });
+                            });
+                        </script>
+                    </div>
+                <?php endif; ?>
+
+                <?php if ($setting['type'] == 'text'): ?>
+                    <div class="form-group">
+                        <label class="control-label mw-ui-label"><?php echo $setting['label']; ?></label>
+                        <input class="form-control mw-ui-field mw_option_field" name="<?php echo $key; ?>" value="<?php echo $$key ?>" data-option-group="<?php echo $option_group; ?>" placeholder="Default: <?php echo $setting['default']; ?>">
+                    </div>
+                <?php endif; ?>
+            <?php endforeach; ?>
+        <?php endif; ?>
+
+        <div class="form-group">
+            <span class="mw-ui-btn mw-ui-btn-medium right" onclick="deleteCompiledCSS();" style="margin-top: 4px;"><?php _e("Reset Template Settings"); ?></span>
+        </div>
+    </div>
+</div>
