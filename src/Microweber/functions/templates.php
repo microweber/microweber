98c0ce206a84ceea567e4fc4e777539038ee7aa7
Bozhidar Slaveykov <50577633+bobimicroweber@users.noreply.github.com>
2019-08-07 13:56:33 +0300

    Update templates.php

--- src/Microweber/functions/templates.php
+++ src/Microweber/functions/templates.php
@@ -1,4 +1,9 @@
 <?php
+function get_template_option_group()
+{
+	return 'mw-template-'. mw()->template->folder_name();
+}
+
 function get_template_stylesheet()
 {
   $template_settings = mw()->template->get_config();
@@ -13,4 +18,3 @@ function get_template_stylesheet()
 
   return '<link href="' . mw()->template->get_stylesheet($template_settings['stylesheet_compiler']['source_file'], $template_settings['stylesheet_compiler']['css_file'], true) . '" id="theme-style" rel="stylesheet" type="text/css" media="all"/>';
 }
-
