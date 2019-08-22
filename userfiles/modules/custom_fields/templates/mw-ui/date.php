d64dbfdfe56211151ee77ad090e4172a1e207302
b.slaveykov <b.slaveykov@radioactive.bg>
2019-08-22 15:42:03 +0300

    update

---
+++ userfiles/modules/custom_fields/templates/mw-ui/date.php
@@ -0,0 +1,24 @@
+<?php
+$rand = uniqid();
+?>
+<div class="control-group form-group">
+  <label class="mw-ui-label">
+  <?php echo $data["name"]; ?>
+  <?php if ($settings['required']): ?>  
+  <span style="color:red;">*</span>
+  <?php endif; ?>
+  </label>
+  <div class="mw-custom-field-form-controls">
+    <input type="text" <?php if ($settings['required']): ?> required="true"  <?php endif; ?>  data-custom-field-id="<?php echo $data["id"]; ?>"  name="<?php print $data["name"]; ?>" id="date_<?php echo $rand; ?>" placeholder="<?php echo $data["placeholder"]; ?>" class="mw-ui-field <?php echo $settings['class']; ?>" />
+  </div>
+</div>
+
+ <script>
+    mw.require("datepicker.css", true);
+    mw.require("datepicker.js", true);
+ </script>
+ <script>
+    $(document).ready(function(){
+      mw.$( "#date_<?php print $rand; ?>" ).datepicker();
+    });
+ </script>
