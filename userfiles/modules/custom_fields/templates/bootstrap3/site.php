5ca37d36042af83183d1772c3299d2a2f5b0cf1e
b.slaveykov <b.slaveykov@radioactive.bg>
2019-08-26 17:00:45 +0300

    update

--- userfiles/modules/custom_fields/templates/bootstrap3/site.php
+++ userfiles/modules/custom_fields/templates/bootstrap3/site.php
@@ -12,8 +12,6 @@ $rand = uniqid();
 	 <?php if ($data['help']): ?>
         <small class="mw-custom-field-help"><?php echo $data['help']; ?></small>
     <?php endif; ?>
-	<div class="mw-ui-controls">
-		<input type="url" class="mw-ui-field form-control" id="custom_field_help_text<?php print $rand; ?>" <?php if ($settings['required']): ?>required="true"<?php endif; ?> data-custom-field-id="<?php echo $data['id']; ?>" name="<?php echo $data['name']; ?>" placeholder="<?php echo $data['placeholder']; ?>" />
-	</div>
+	<input type="url" class="mw-ui-field form-control" id="custom_field_help_text<?php print $rand; ?>" <?php if ($settings['required']): ?>required="true"<?php endif; ?> data-custom-field-id="<?php echo $data['id']; ?>" name="<?php echo $data['name']; ?>" placeholder="<?php echo $data['placeholder']; ?>" />
 </div>
 </div>
--- userfiles/modules/custom_fields/templates/bootstrap3/text.php
+++ userfiles/modules/custom_fields/templates/bootstrap3/text.php
@@ -9,12 +9,12 @@
 	 <?php if ($data['help']): ?>
         <small class="mw-custom-field-help"><?php echo $data['help']; ?></small>
     <?php endif; ?>
-	<div class="mw-ui-controls">
-		<?php if ($settings['as_text_area']): ?>
-			<textarea type="text" class="mw-ui-field form-control" <?php if ($settings['required']): ?>required="true"<?php endif; ?> data-custom-field-id="<?php echo $data['id']; ?>" name="<?php echo $data['name']; ?>" placeholder="<?php echo $data['placeholder']; ?>"></textarea>
-		<?php else: ?>
-			<input type="text" class="mw-ui-field form-control" <?php if ($settings['required']): ?>required="true"<?php endif; ?> data-custom-field-id="<?php echo $data['id']; ?>" name="<?php echo $data['name']; ?>" placeholder="<?php echo $data['placeholder']; ?>" />
-		<?php endif; ?>
-	</div>
+
+	<?php if ($settings['as_text_area']): ?>
+		<textarea type="text" class="mw-ui-field form-control" <?php if ($settings['required']): ?>required="true"<?php endif; ?> data-custom-field-id="<?php echo $data['id']; ?>" name="<?php echo $data['name']; ?>" placeholder="<?php echo $data['placeholder']; ?>"></textarea>
+	<?php else: ?>
+		<input type="text" class="mw-ui-field form-control" <?php if ($settings['required']): ?>required="true"<?php endif; ?> data-custom-field-id="<?php echo $data['id']; ?>" name="<?php echo $data['name']; ?>" placeholder="<?php echo $data['placeholder']; ?>" />
+	<?php endif; ?>
+
 </div>
 </div>
