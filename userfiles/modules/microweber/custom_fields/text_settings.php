e78595f301d0d0017e0068d78f43fed6e8669034
b.slaveykov <b.slaveykov@radioactive.bg>
2019-08-27 17:18:48 +0300

    update

--- userfiles/modules/microweber/custom_fields/text_settings.php
+++ userfiles/modules/microweber/custom_fields/text_settings.php
@@ -27,7 +27,6 @@
     #mw-custom-fields-text-holder textarea{
       resize:both;
     }
-
 </style>
  <div class="custom-field-settings-name">
                
@@ -39,15 +38,13 @@
     <input type="text" onkeyup="" class="mw-ui-field mw-full-width" value="<?php print ($data['name']) ?>" name="name" id="input_field_label<?php print $rand; ?>">
 
     <br />
-       <br />
-
-    <label class="mw-ui-check"><input type="checkbox"  class="mw-custom-field-option mw-full-width" name="options[as_text_area]"  <?php if(isset($data['options']) == true and isset($data['options']["as_text_area"]) == true): ?> checked="checked" <?php endif; ?> value="1" id="mw-custom-fields-text-switch"><span></span><span><?php _e("Use as Text Area"); ?></span></label>
     <br />
-     <br />
-     
-    <label class="mw-ui-check"><input type="checkbox"  class="mw-custom-field-option mw-full-width" name="options[required]"  <?php if(isset($data['options']) == true and isset($data['options']["required"]) == true): ?> checked="checked" <?php endif; ?> value="1"><span></span><span><?php _e("Required"); ?>?</span></label>
 
-    
+    <label class="mw-ui-check"><input type="checkbox"  class="mw-custom-field-option mw-full-width" name="options[as_text_area]"  <?php if($settings["as_text_area"]): ?> checked="checked" <?php endif; ?> value="1" id="mw-custom-fields-text-switch"><span></span><span><?php _e("Use as Text Area"); ?></span></label>
+    <br />
+    <br />
+     
+    <label class="mw-ui-check"><input type="checkbox"  class="mw-custom-field-option mw-full-width" name="options[required]"  <?php if ($settings["required"]) : ?> checked="checked" <?php endif; ?> value="1"><span></span><span><?php _e("Required"); ?>?</span></label>
 
 </div>
 
@@ -58,13 +55,11 @@
       <label class="mw-ui-label" for="value<?php print $rand; ?>"><?php _e("Value"); ?></label>
         <div id="mw-custom-fields-text-holder">
          <textarea class="mw-ui-field mw-full-width" name="value"><?php print ($data['value']) ?></textarea>
-                
- 
-        
         </div>
     </div>
 
     <?php print $savebtn; ?>
+    
     </div>
 
     <?php include('settings_footer.php'); ?>
