71c659b5c7aa60ee2d41fa6506e93a1f7a734608
Nick Woolley <nick@estores.org.uk>
2019-08-28 09:44:48 +0100

    privacy settings update (#603)

--- userfiles/modules/contact_form/functions.php
+++ userfiles/modules/contact_form/functions.php
@@ -1,9 +1,11 @@
 <?php
 
-
-
 event_bind(
     'mw.admin.dashboard.content.3', function ($item) {
     print '<div type="contact_form/dashboard_recent_messages" class="mw-lazy-load-module" id="admin-dashboard-contact-form"></div>';
 }
 );
+
+event_bind('website.privacy_settings', function () {
+    print '<h2>Contact Form settings</h2><module type="contact_form/privacy_settings" />';
+});
--- userfiles/modules/contact_form/templates/default.php
+++ userfiles/modules/contact_form/templates/default.php
@@ -2,13 +2,13 @@
 
 /*
 
-type: layout
+  type: layout
 
-name: Default
+  name: Default
 
-description: Default template for Contact form
+  description: Default template for Contact form
 
-icon: dream.png
+  icon: dream.png
 
 
 */
@@ -22,38 +22,39 @@ icon: dream.png
 
 <div class="contact-form-container contact-form-template-dream">
     <div class="contact-form">
-        <div class="edit" data-field="contact_form_title" rel="contact_form_module" data-id="<?php print $params['id'] ?>">
-            <h3 class="element contact-form-title"><?php _e("Leave a Message"); ?></h3>
-        </div>
         <form class="mw_form" data-form-id="<?php print $form_id ?>" name="<?php print $form_id ?>" method="post">
 
 
             <module type="custom_fields" for-id="<?php print $params['id'] ?>" data-for="module" default-fields="name,email,message"/>
-            
+
+		<?php if($require_terms && $require_terms_when == 'b'): ?>
+            <module type="users/terms" data-for="contact_form_default" />
+        <?php endif; ?>
+
             <div class="mw-flex-row">
-            
+
             	<div class="mw-flex-col-md-9 mw-flex-col-sm-12 mw-flex-col-xs-12">
-		            <div class="control-group form-group">
-		                <?php if (get_option('disable_captcha', $params['id']) != 'y'): ?>
-		                    <label class="custom-field-title"><b><?php _e("Enter Security code"); ?></b></label>
-		                    <div class="mw-ui-row captcha-holder" style="width: 220px;">
-		                        <div class="mw-ui-col">
-		                            <module type="captcha"/>
-		                        </div>
-		                    </div>
-		                <?php endif; ?>
+                    <div class="control-group form-group">
+                      <?php if (get_option('disable_captcha', $params['id']) != 'y'): ?>
+                        <label class="custom-field-title"><?php _e("Enter Security code"); ?></label>
+                        <div class="mw-ui-row captcha-holder" style="width: 262px;">
+                            <div class="mw-ui-col">
+                                <module type="captcha"/>
+                            </div>
+                        </div>
+		              <?php endif; ?>
 		            </div>
 	            </div>
-	            
+
 	            <div class="mw-flex-col-md-3 mw-flex-col-sm-12 mw-flex-col-xs-12">
 		            <label>&nbsp;</label>
-		             <div class="control-group form-group">
+		            <div class="control-group form-group">
 		                   <module type="btn" button_action="submit" button_style="mw-ui-btn pull-right" button_text="<?php _e("Send Message"); ?>"  />
-		             </div>
-	             </div>
-	             
-	         </div>
-	         
+		            </div>
+	            </div>
+
+	        </div>
+
         </form>
     </div>
     <div class="message-sent" id="msg<?php print $form_id ?>">
