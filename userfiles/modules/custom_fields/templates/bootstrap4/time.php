c9054c5f3890f22ecac57d5b0b4bb905d8aa56c6
b.slaveykov <b.slaveykov@radioactive.bg>
2019-08-29 15:39:46 +0300

    update

---
+++ userfiles/modules/custom_fields/templates/bootstrap4/time.php
@@ -0,0 +1,28 @@
+<div class="<?php echo $settings['class']; ?>">
+<div class="mw-ui-field-holder">
+	<label class="mw-ui-label"> 
+	<?php echo $data['name']; ?>
+	<?php if ($settings['required']): ?>
+	<span style="color: red;">*</span>
+	<?php endif; ?>
+	</label>
+	 <?php if ($data['help']): ?>
+        <small class="mw-custom-field-help"><?php echo $data['help']; ?></small>
+    <?php endif; ?>
+	<div class="mw-ui-controls" id="datetimepicker3">
+		<input type="time" class="mw-ui-field" <?php if ($settings['required']): ?>required="true"<?php endif; ?> data-custom-field-id="<?php echo $data['id']; ?>" name="<?php echo $data['name']; ?>" placeholder="<?php echo $data['placeholder']; ?>" />
+	</div>
+</div>
+</div>
+
+<script>
+	mw.lib.require("datetimepicker");
+</script>
+
+<script type="text/javascript">
+$(function () {
+	$('#datetimepicker3').datetimepicker({
+		format: 'LT'
+	});
+});
+</script>
