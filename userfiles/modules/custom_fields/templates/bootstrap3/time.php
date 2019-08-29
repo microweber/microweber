35e90248e58a18e4b41c16e53a98e249830eeffc
b.slaveykov <b.slaveykov@radioactive.bg>
2019-08-29 16:43:01 +0300

    update

--- userfiles/modules/custom_fields/templates/bootstrap3/time.php
+++ userfiles/modules/custom_fields/templates/bootstrap3/time.php
@@ -1,5 +1,5 @@
 <div class="col-md-<?php echo $settings['field_size']; ?>">
-<div class="mw-ui-field-holder">
+<div class="form-group">
 	<label class="mw-ui-label"> 
 	<?php echo $data['name']; ?>
 	<?php if ($settings['required']): ?>
@@ -10,7 +10,7 @@
         <small class="mw-custom-field-help"><?php echo $data['help']; ?></small>
     <?php endif; ?>
 	<div class="mw-ui-controls" id="datetimepicker3">
-		<input type="time" class="mw-ui-field" <?php if ($settings['required']): ?>required="true"<?php endif; ?> data-custom-field-id="<?php echo $data['id']; ?>" name="<?php echo $data['name']; ?>" placeholder="<?php echo $data['placeholder']; ?>" />
+		<input type="text" class="mw-ui-field form-control" <?php if ($settings['required']): ?>required="true"<?php endif; ?> data-custom-field-id="<?php echo $data['id']; ?>" name="<?php echo $data['name']; ?>" placeholder="<?php echo $data['placeholder']; ?>" />
 	</div>
 </div>
 
