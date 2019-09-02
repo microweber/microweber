0ac1db9cea85f991154ef8894f78feb593394877
Bozhidar Slaveykov <50577633+bobimicroweber@users.noreply.github.com>
2019-09-02 18:34:27 +0300

    Update functions.php

--- userfiles/modules/contact_form/functions.php
+++ userfiles/modules/contact_form/functions.php
@@ -9,3 +9,13 @@ event_bind(
 event_bind('website.privacy_settings', function () {
     print '<h2>Contact Form settings</h2><module type="contact_form/privacy_settings" />';
 });
+
+api_expose('get_contact_entry_by_id');
+function get_contact_entry_by_id($params)
+{
+	only_admin_access();
+	
+	$form_data = mw()->forms_manager->get_entires('single=1&id=' . $params['id']);
+	
+	return $form_data;
+}
