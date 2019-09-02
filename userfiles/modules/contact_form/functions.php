c8e074c68f3b71558a514a29914e17444f3a0333
Bozhidar Slaveykov <50577633+bobimicroweber@users.noreply.github.com>
2019-09-02 18:25:15 +0300

    Update functions.php

--- userfiles/modules/contact_form/functions.php
+++ userfiles/modules/contact_form/functions.php
@@ -1,9 +1,14 @@
 <?php
+event_bind('mw.admin.dashboard.content.3', function ($item) {
+	print '<div type="contact_form/dashboard_recent_messages" class="mw-lazy-load-module" id="admin-dashboard-contact-form"></div>';
+});
 
-
-
-event_bind(
-    'mw.admin.dashboard.content.3', function ($item) {
-    print '<div type="contact_form/dashboard_recent_messages" class="mw-lazy-load-module" id="admin-dashboard-contact-form"></div>';
+api_expose('get_contact_entry_by_id');
+function get_contact_entry_by_id($params)
+{
+	only_admin_access();
+	
+	$form_data = mw()->forms_manager->get_entires('single=1&id=' . $params['id']);
+	
+	return $form_data;
 }
-);
