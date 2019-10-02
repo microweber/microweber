964982f3338c696660e6c510a337afae039a8843
b.slaveykov <b.slaveykov@radioactive.bg>
2019-10-02 13:47:08 +0300

    update

---
+++ userfiles/modules/users/login_attempts.php
@@ -0,0 +1,50 @@
+<?php
+
+$user_id = $params['user_id'];
+$loginAttempts = \Microweber\App\LoginAttempt::where('id', $user_id)
+    ->orderBy('time', 'desc')
+    ->take(40)
+    ->get();
+?>
+
+<div class="mw-ui-box mw-ui-box-content" data-view="">
+    <table class="mw-ui-table mw-full-width mw-ui-table-basic">
+        <thead>
+        <tr>
+            <th><?php echo _e('Username'); ?></th>
+            <th><?php echo _e('E-mail'); ?></th>
+            <th><?php echo _e('IP'); ?></th>
+            <th><?php echo _e('Date'); ?></th>
+            <th><?php echo _e('Status'); ?></th>
+        </tr>
+        </thead>
+        <tfoot>
+        <tr>
+            <td><?php echo _e('Username'); ?></td>
+            <td><?php echo _e('E-mail'); ?></td>
+            <td><?php echo _e('IP'); ?></td>
+            <td><?php echo _e('Date'); ?></td>
+            <td><?php echo _e('Status'); ?></td>
+        </tr>
+        </tfoot>
+        <tbody>
+        <?php
+        foreach($loginAttempts as $attempt):
+        ?>
+        <tr>
+            <td><?php echo $attempt->username; ?></td>
+            <td><?php echo $attempt->email; ?></td>
+            <td><?php echo $attempt->ip; ?></td>
+            <td><?php echo date("Y-m-d H:i:s", $attempt->time); ?></td>
+            <td>
+                <?php if($attempt->success):?>
+                    <?php echo _e('Success login'); ?>
+                <?php else: ?>
+                    <?php echo _e('Failed login'); ?>
+                <?php endif; ?>
+            </td>
+        </tr>
+        <?php endforeach; ?>
+        </tbody>
+    </table>
+</div>
