717da62ccb1b5145256092edb88f2ce734852a8b
b.slaveykov <b.slaveykov@radioactive.bg>
2019-10-03 16:52:02 +0300

    update

--- src/Microweber/Views/emails/new_order.blade.php
+++ src/Microweber/Views/emails/new_order.blade.php
@@ -1,1 +1,26 @@
-NEW ORDER
+<p><strong>Order Information</strong></p>
+
+<br>
+
+<p> Order ID:{{order_id}}</p>
+<p>Transaction ID: {{transaction_id}}</p>
+<p>Currency: {{currency}}</p>
+<p>Order Status: {{order_status}}</p>
+<p>Amount: {{amount}}</p>
+<p>Cart items: {{cart_items}}</p>
+<p>Date: {{date}}</p>
+
+<br>
+
+<p><strong>Customer Information</strong></p>
+<br />
+
+<p>First Name: {{first_name}}</p>
+<p>Last Name: {{last_name}}</p>
+<p>Email: {{email}}</p>
+<p>Phone: {{phone}}</p>
+<p>Address: {{address}}</p>
+<p> Zip: {{zip}}</p>
+<p>State: {{state}}</p>
+<p>City: {{city}}</p>
+<p>Country: {{country}}</p>
