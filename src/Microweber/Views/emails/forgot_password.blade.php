e70cec59f9c9a64ec718b00979d4e2259e0a2a1d
b.slaveykov <b.slaveykov@radioactive.bg>
2019-10-08 13:25:12 +0300

    update

--- src/Microweber/Views/emails/forgot_password.blade.php
+++ src/Microweber/Views/emails/forgot_password.blade.php
@@ -1,16 +1,16 @@
-<p>Dear {{username}},</p>
+<p>Dear {username},</p>
 <br/>
 <p>
-    Recently a request was submitted to reset your password for our client area from ip {{ip}}. If you did not request
+    Recently a request was submitted to reset your password for our client area from ip {ip}. If you did not request
     this, please ignore this email. It will expire and become useless in 2 hours time.
 </p>
 <br/>
 <p>
     To reset your password, please visit the url below:
     <br/>
-    {{reset_password_link}}
+    {reset_password_link}
 </p>
 <br/>
 <p>When you visit the link above, you will have the opportunity to choose a new password.</p>
 
-<p>{{created_at}}</p>
+<p>{created_at}</p>
