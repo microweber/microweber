dad2da4be67f33919aa6927062d4f16a7a2290b7
b.slaveykov <b.slaveykov@radioactive.bg>
2019-10-11 15:37:43 +0300

    update

--- src/Microweber/tests/CustomFieldsTemplatesTest.php
+++ src/Microweber/tests/CustomFieldsTemplatesTest.php
@@ -4,7 +4,6 @@ namespace Microweber\tests;
 
 class CustomFieldsTemplatesTest extends TestCase
 {
-
     public function setUp()
     {
         parent::setUp();
@@ -13,15 +12,15 @@ class CustomFieldsTemplatesTest extends TestCase
         mw()->database_manager->extended_save_set_permission(true);
     }
 
-    public function testTempalte()
+    public function testBootstrapTempalte()
     {
         $rel = 'module';
-        $rel_id = 'layouts-'.rand(1111,9999).'-contact-form';
-        $fields_csv_str = 'text, email, message';
+        $rel_id = 'layouts-' . rand(1111, 9999) . '-contact-form';
+        $fields_csv_str = 'text, select, number, phone, website, email, fileupload, message';
         $fields_csv_array = explode(',', $fields_csv_str);
 
         $fields = mw()->fields_manager->make_default($rel, $rel_id, $fields_csv_str);
-        foreach ($fields as $key=>$field_id) {
+        foreach ($fields as $key => $field_id) {
 
             $option = array();
             $option['option_value'] = 'bootstrap3/index.php';
@@ -30,26 +29,87 @@ class CustomFieldsTemplatesTest extends TestCase
             $save = save_option($option);
 
             $output = mw()->fields_manager->make($field_id);
+            $field = mw()->fields_manager->get_by_id($field_id);
 
             $checkRow = false;
             if (strpos($output, 'class="col-md-12"') !== false) {
                 $checkRow = true;
             }
-            $this->assertEquals($checkRow, true);
-
+            if (!$checkRow) {
+                //   echo $field['type'] . PHP_EOL;
+            }
+            $this->assertEquals(true, $checkRow);
 
             $checkInputClass = false;
             if (strpos($output, 'class="form-control"') !== false) {
                 $checkInputClass = true;
             }
-            $this->assertEquals($checkInputClass, true);
+            if (!$checkInputClass) {
+                //  echo $field['type'] . PHP_EOL;
+            }
+
+            $this->assertEquals(true, $checkInputClass);
 
             $checkFormGroup = false;
             if (strpos($output, 'class="form-group"') !== false) {
                 $checkFormGroup = true;
             }
-            $this->assertEquals($checkFormGroup, true);
+            if (!$checkFormGroup) {
+                // echo $field['type'] . PHP_EOL;
+            }
+            $this->assertEquals(true, $checkFormGroup);
 
+        }
+    }
+
+    public function testMwUiTempalte()
+    {
+        $rel = 'module';
+        $rel_id = 'layouts-' . rand(1111, 9999) . '-contact-form';
+        $fields_csv_str = 'text, select, number, phone, website, email, fileupload, message';
+        $fields_csv_array = explode(',', $fields_csv_str);
+
+        $fields = mw()->fields_manager->make_default($rel, $rel_id, $fields_csv_str);
+        foreach ($fields as $key => $field_id) {
+
+            $option = array();
+            $option['option_value'] = 'mw-ui/index.php';
+            $option['option_key'] = 'data-template';
+            $option['option_group'] = $field_id;
+            $save = save_option($option);
+
+            $output = mw()->fields_manager->make($field_id);
+            $field = mw()->fields_manager->get_by_id($field_id);
+
+            $checkRow = false;
+            if (strpos($output, 'class="mw-flex-col-md-12"') !== false) {
+                $checkRow = true;
+            }
+            if (!$checkRow) {
+                // echo $field['type'] . PHP_EOL;
+            }
+
+            $this->assertEquals(true, $checkRow);
+
+
+            $checkInputClass = false;
+            if (strpos($output, 'class="mw-ui-field"') !== false) {
+                $checkInputClass = true;
+            }
+            if (!$checkInputClass) {
+                //   echo $field['type'] . PHP_EOL;
+            }
+            $this->assertEquals(true, $checkInputClass);
+
+            $checkFormGroup = false;
+            if (strpos($output, 'class="mw-ui-controls"') !== false) {
+                $checkFormGroup = true;
+            }
+            if (!$checkFormGroup) {
+                // echo $field['type'] . PHP_EOL;
+            }
+
+            $this->assertEquals(true, $checkFormGroup);
         }
     }
 }
--- userfiles/modules/custom_fields/templates/bootstrap3/phone.php
+++ userfiles/modules/custom_fields/templates/bootstrap3/phone.php
@@ -9,6 +9,6 @@
         <?php if ($data['help']): ?>
             <small class="mw-custom-field-help"><?php echo $data['help']; ?></small>
         <?php endif; ?>
-        <input type="text" class="mw-ui-field form-control" <?php if ($settings['required']): ?>required="true"<?php endif; ?> data-custom-field-id="<?php echo $data['id']; ?>" name="<?php echo $data['name']; ?>" placeholder="<?php echo $data['placeholder']; ?>"/>
+        <input type="text" class="form-control" <?php if ($settings['required']): ?>required="true"<?php endif; ?> data-custom-field-id="<?php echo $data['id']; ?>" name="<?php echo $data['name']; ?>" placeholder="<?php echo $data['placeholder']; ?>"/>
     </div>
 </div>
