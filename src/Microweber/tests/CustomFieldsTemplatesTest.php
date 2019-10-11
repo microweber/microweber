5a820b62cea78203eb3bbde145f06b52a2654888
b.slaveykov <b.slaveykov@radioactive.bg>
2019-10-11 15:43:18 +0300

    update

--- src/Microweber/tests/CustomFieldsTemplatesTest.php
+++ src/Microweber/tests/CustomFieldsTemplatesTest.php
@@ -62,6 +62,56 @@ class CustomFieldsTemplatesTest extends TestCase
         }
     }
 
+    public function testBootstrapNewTempalte()
+    {
+        $rel = 'module';
+        $rel_id = 'layouts-' . rand(1111, 9999) . '-contact-form';
+        $fields_csv_str = 'text, select, number, phone, website, email, fileupload, message';
+        $fields_csv_array = explode(',', $fields_csv_str);
+
+        $fields = mw()->fields_manager->make_default($rel, $rel_id, $fields_csv_str);
+        foreach ($fields as $key => $field_id) {
+
+            $option = array();
+            $option['option_value'] = 'bootstrap4/index.php';
+            $option['option_key'] = 'data-template';
+            $option['option_group'] = $field_id;
+            $save = save_option($option);
+
+            $output = mw()->fields_manager->make($field_id);
+            $field = mw()->fields_manager->get_by_id($field_id);
+
+            $checkRow = false;
+            if (strpos($output, 'class="col-12"') !== false) {
+                $checkRow = true;
+            }
+            if (!$checkRow) {
+                //   echo $field['type'] . PHP_EOL;
+            }
+            $this->assertEquals(true, $checkRow);
+
+            $checkInputClass = false;
+            if (strpos($output, 'class="form-control"') !== false) {
+                $checkInputClass = true;
+            }
+            if (!$checkInputClass) {
+                //  echo $field['type'] . PHP_EOL;
+            }
+
+            $this->assertEquals(true, $checkInputClass);
+
+            $checkFormGroup = false;
+            if (strpos($output, 'class="form-group"') !== false) {
+                $checkFormGroup = true;
+            }
+            if (!$checkFormGroup) {
+                // echo $field['type'] . PHP_EOL;
+            }
+            $this->assertEquals(true, $checkFormGroup);
+
+        }
+    }
+
     public function testMwUiTempalte()
     {
         $rel = 'module';
