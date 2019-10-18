cfa876e3a0dd1cf9b18febcf451e613b8e55767e
b.slaveykov <b.slaveykov@radioactive.bg>
2019-10-18 13:52:45 +0300

    update

--- userfiles/packages/Audio/Http/routes.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-Route::group(['middleware' => 'web', 'prefix' => 'audio', 'namespace' => 'Packages\Audio\Http\Controllers'], function()
-{
-    Route::get('/', 'AudioController@index');
-});
--- userfiles/packages/Audio/Resources/views/index.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('audio::layouts.master')
-
-@section('content')
-    <h1>Hello World</h1>
-
-    <p>
-        This view is loaded from module: {!! config('audio.name') !!}
-    </p>
-@stop
