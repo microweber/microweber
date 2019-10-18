8cc7d81051b593c9d6fbd3d99bebaf58d894e616
b.slaveykov <b.slaveykov@radioactive.bg>
2019-10-18 13:55:34 +0300

    update

--- config/modules.php
+++ config/modules.php
@@ -11,7 +11,7 @@ return [
     |
     */
 
-    'namespace' => 'Packages',
+    'namespace' => 'Microweber\Packages',
 
     /*
     |--------------------------------------------------------------------------
