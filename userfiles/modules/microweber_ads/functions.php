04163f27355eaf83f631211ffa05d380a9453f5a
b.slaveykov <b.slaveykov@radioactive.bg>
2019-10-29 17:05:06 +0200

    update

--- userfiles/modules/microweber_ads/functions.php
+++ userfiles/modules/microweber_ads/functions.php
@@ -1,4 +1,40 @@
 <?php
+
+function showMicroweberAdsBar() {
+
+    $showBar = false;
+    $showBarUrl = false;
+
+    $whmcsSettingsFile = modules_path() . 'whmcs_connector/settings.json';
+    $whmcsSettingsFile = normalize_path($whmcsSettingsFile, false);
+    if (is_file($whmcsSettingsFile)) {
+
+        $whmcsUrl = false;
+        $whmcsSettingsFileContent = file_get_contents($whmcsSettingsFile);
+        $settings = json_decode($whmcsSettingsFileContent, true);
+        if (isset($settings['url'])) {
+            $whmcsUrl = $settings['url'];
+        }
+        if (isset($settings['whmcs_url'])) {
+            $whmcsUrl = $settings['whmcs_url'];
+        }
+
+        if ($whmcsUrl) {
+            $checkDomainUrl = $whmcsUrl . 'index.php?m=microweber_addon&function=check_domain_is_premium&domain=' . $_SERVER['HTTP_HOST'];
+            $checkDomain = file_get_contents($checkDomainUrl);
+            $checkDomain = json_decode($checkDomain, true);
+
+            if (isset($checkDomain['free']) && $checkDomain['free'] == true && isset($checkDomain['ads_bar_url'])) {
+                $showBarUrl = $checkDomain['ads_bar_url'];
+                $showBar = true;
+            }
+        }
+
+    }
+
+    return array('show'=>$showBar, 'iframe_url'=>$showBarUrl);
+}
+
 event_bind('mw.front', function () {
     $css = '
         <style>
@@ -34,10 +70,10 @@ event_bind('mw.front', function () {
         </style>
     ';
 
-    $iframe_content = '<div class="row">
-    <div class="col"><p>Този уебсайт е направен с <strong>Microweber.bg</strong> сайт билдър</p></div>
-    <div class="col"><a href="https://microweber.bg" target="_blank">Направи си сайт</a></div>
-    </div>';
+    $bar = showMicroweberAdsBar();
 
-    mw()->template->foot($css . '<div class="js-microweber-add-iframe" src="">' . $iframe_content . '</div>');
+    if ($bar['show']) {
+       mw()->template->foot($css . '<iframe class="js-microweber-add-iframe" src="'.$bar['iframe_url'].'"></iframe>');
+    }
+
 });
