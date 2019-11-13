b58396eb2b7626f928b4778937e3d468730a6cc5
b.slaveykov <b.slaveykov@radioactive.bg>
2019-11-13 18:21:11 +0200

    упдате

--- userfiles/modules/multilanguage/src/TranslateManager.php
+++ userfiles/modules/multilanguage/src/TranslateManager.php
@@ -6,7 +6,8 @@ require_once __DIR__ . '/TranslateTables/TranslateCategory.php';
 require_once __DIR__ . '/TranslateTables/TranslateContent.php';
 require_once __DIR__ . '/TranslateTables/TranslateContentFields.php';
 
-class TranslateManager {
+class TranslateManager
+{
 
     public $translateProviders = [
         'TranslateMenu',
@@ -16,7 +17,10 @@ class TranslateManager {
         'TranslateContentFields'
     ];
 
-    public function run() {
+    public function run()
+    {
+        $currentLocale = mw()->lang_helper->current_lang();
+        $defaultLocale = mw()->lang_helper->default_lang();
 
         if (!empty($this->translateProviders)) {
             foreach ($this->translateProviders as $provider) {
@@ -25,7 +29,7 @@ class TranslateManager {
                 $providerTable = $providerInstance->getRelType();
 
                 // BIND GET TABLES
-                event_bind('mw.database.'.$providerTable.'.get', function($get) use ($providerInstance) {
+                event_bind('mw.database.'.$providerTable.'.get', function($get) use ($currentLocale, $defaultLocale, $providerInstance) {
                     if (is_array($get) && !empty($get)) {
                         foreach ($get as &$item) {
                             if (isset($item['option_key']) && $item['option_key'] == 'language') {
@@ -38,27 +42,40 @@ class TranslateManager {
                 });
 
                 // BIND SAVE TABLES
-                event_bind('mw.database.'.$providerTable.'.save.params', function($save) use ($providerInstance) {
-
-                    $currentLocale = mw()->lang_helper->current_lang();
-                    $defaultLocale = mw()->lang_helper->default_lang();
-
+                event_bind('mw.database.' . $providerTable . '.save.params', function ($saveData) use ($currentLocale, $defaultLocale, $providerInstance) {
                     if ($currentLocale != $defaultLocale) {
 
-                        if (isset($save['option_key']) && $save['option_key'] == 'language') {
+                        if (isset($saveData['option_key']) && $saveData['option_key'] == 'language') {
                             return false;
                         }
 
-                        if (isset($save['id']) && isset($save['option_value'])) {
-                            $providerInstance->saveOrUpdate($save);
-                        }
+                        if (!empty($providerInstance->getColumns())) {
+                            $translatedData = array();
+                            foreach ($providerInstance->getColumns() as $column) {
 
-                        if (isset($save['option_value'])) {
-                            unset($save['option_value']);
+                                if (!isset($saveData['id'])) {
+                                    continue;
+                                }
+
+                                if (!isset($saveData[$column])) {
+                                    continue;
+                                }
+
+                                $translatedData = $saveData;
+
+                                if (isset($saveData[$column])) {
+                                    unset($saveData[$column]);
+                                }
+                            }
+
+                            if (!empty($translatedData)) {
+                                $providerInstance->saveOrUpdate($translatedData);
+                            }
                         }
                     }
 
-                    return $save;
+                    return $saveData;
+
                 });
 
             }
