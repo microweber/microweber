369e8b335641367079a527a87ea4ed1b209822fc
Jeffry <jeffry.l@bluevinegar.com>
2020-01-31 09:34:49 +0800

    fix incompatible query for psql

--- src/Microweber/Traits/QueryFilter.php
+++ src/Microweber/Traits/QueryFilter.php
@@ -271,7 +271,7 @@ trait QueryFilter
                                                 $query->{$where_or_where}(function ($query) use ($to_search_in_fields, $to_search_keyword, $params, $table) {
                                                     foreach ($to_search_in_fields as $to_search_in_field) {
                                                         $to_search_keyword = str_replace('.', ' ', $to_search_keyword);
-                                                        $query = $query->orWhere($to_search_in_field, 'REGEXP', $to_search_keyword);
+                                                        // $query = $query->orWhere($to_search_in_field, 'REGEXP', $to_search_keyword);
                                                         $query = $query->orWhere($to_search_in_field, 'LIKE', '%'.$to_search_keyword.'%');
                                                         $query = $query->orWhere($to_search_in_field, 'LIKE', '%'.$to_search_keyword);
                                                         $query = $query->orWhere($to_search_in_field, 'LIKE', '.'.$to_search_keyword);
