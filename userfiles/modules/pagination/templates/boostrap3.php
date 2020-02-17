329546fe01b80827cedfaa2e684b96396769c0bd
b.slaveykov <b.slaveykov@radioactive.bg>
2020-02-17 16:15:44 +0200

    update

--- userfiles/modules/pagination/templates/boostrap3.php
+++ userfiles/modules/pagination/templates/boostrap3.php
@@ -5,3 +5,21 @@ name: Bootstrap 3
 description: Bootstrap 3
 */
 ?>
+<nav aria-label="...">
+    <ul class="pagination">
+        <li class="page-item disabled">
+            <span class="page-link">Previous</span>
+        </li>
+        <li class="page-item"><a class="page-link" href="#">1</a></li>
+        <li class="page-item active">
+      <span class="page-link">
+        2
+        <span class="sr-only">(current)</span>
+      </span>
+        </li>
+        <li class="page-item"><a class="page-link" href="#">3</a></li>
+        <li class="page-item">
+            <a class="page-link" href="#">Next</a>
+        </li>
+    </ul>
+</nav>
