db50aaff0c3207cae370e70955618a89d0773747
Bobkata <selfworksbg@gmail.com>
2020-04-03 00:18:12 +0300

    update

--- userfiles/modules/captcha/templates/default.php
+++ userfiles/modules/captcha/templates/default.php
@@ -1,5 +1,4 @@
 <?php
-
 /*
 
 type: layout
@@ -8,65 +7,29 @@ name: Default
 
 description: Default comments template
 
-*/;
-
-
+*/
 ?>
-<!--<script>
-    mw.lib.require('bootstrap3ns');
-</script>
-<script>
-    $(document).ready(function () {
-        $('[data-toggle="tooltip"]').tooltip();
-    });
-</script>-->
-<!--
-<div class="bootstrap3ns">
-    <div class="row">
-        <div class="mw-captcha" style="max-width: 400px; margin: 15px;">
-            <div class="form-group">
-                <div class="captcha-holder">
-                    <div class="col-xs-4">
-                        <a href="javascript:;" class="tip" data-tip="Refresh captcha" data-tipposition="top-center">
-                            <img
-                                onclick="mw.tools.refresh_image(this);"
-                                class="mw-captcha-img"
-                                id="captcha-<?php /*print $form_id; */ ?>"
-                                src="<?php /*print api_link('captcha') */ ?>?w=100&h=60&uid=<?php /*print uniqid($form_id) */ ?>&rand=<?php /*print rand(1,10000) */ ?>&id=<?php /*print $params['id'] */ ?> ?>"  /></a>
-                    </div>
-                    <div class="col-xs-6">
-                        <input name="captcha" type="text" required class="mw-captcha-input form-control" placeholder="<?php /*_e("Security code"); */ ?>"/>
-                    </div>
-                    <div class="clearfix"></div>
-                </div>
-                <div class="clearfix"></div>
-            </div>
-            <div class="clearfix"></div>
-        </div>
-        <div class="clearfix"></div>
-    </div>
-    <div class="clearfix"></div>
-</div>
--->
-<script src="https://www.google.com/recaptcha/api.js?onload=onloadCallback&render=explicit"
-        async defer>
-</script>
-
-<script type="text/javascript">
-    var onloadCallback = function() {
-        grecaptcha.render('js-mw-google-recaptcha-v2-<?php print $params['id'] ?> ?>', {
-            'sitekey' : '<?php echo get_option('recaptcha_v2_site_key', 'captcha'); ?>'
-        });
-    };
-</script>
 
 <div class="mw-ui-row">
     <?php
     $captcha_provider = get_option('provider', 'captcha');
     if ($captcha_provider == 'google_recaptcha_v2'):
     ?>
+    <script src="//www.google.com/recaptcha/api.js?onload=onloadCallback&render=explicit" async defer>
+    </script>
+
+    <script type="text/javascript">
+        var onloadCallback = function() {
+            if (typeof(grecaptcha) !== 'undefined') {
+                grecaptcha.render('js-mw-google-recaptcha-v2-<?php print $params['id'] ?>', {
+                    'sitekey': '<?php echo get_option('recaptcha_v2_site_key', 'captcha'); ?>'
+                });
+            }
+        };
+        onloadCallback();
+    </script>
     <div class="mw-captcha">
-        <div id="js-mw-google-recaptcha-v2-<?php print $params['id'] ?> ?>"></div>
+        <div id="js-mw-google-recaptcha-v2-<?php print $params['id'] ?>"></div>
     </div>
     <?php else: ?>
     <div class="mw-captcha" style="max-width: 400px; margin: 15px;">
@@ -77,7 +40,8 @@ description: Default comments template
                         <img onclick="mw.tools.refresh_image(this);"
                              class="mw-captcha-img"
                              id="captcha-<?php print $form_id; ?>"
-                             src="<?php print api_link('captcha') ?>?w=100&h=60&uid=<?php print uniqid($form_id) ?>&rand=<?php print rand(1, 10000) ?>&id=<?php print $params['id'] ?> ?>"/></a>
+                             src="<?php print api_link('captcha') ?>?w=100&h=60&uid=<?php print uniqid($form_id) ?>&rand=<?php print rand(1, 10000) ?>&id=<?php print $params['id'] ?>" />
+                    </a>
                 </div>
                 <div class="mw-ui-col">
                     <input name="captcha" type="text" required class="mw-captcha-input form-control" placeholder="<?php _e("Security code"); ?>"/>
