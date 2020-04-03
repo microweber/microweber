0297f58ec3d423144c99f13d49c02439709c706b
Bobkata <selfworksbg@gmail.com>
2020-04-03 18:14:33 +0300

    update

--- src/Microweber/functions/paths.php
+++ src/Microweber/functions/paths.php
@@ -20,26 +20,6 @@ function userfiles_url()
     return $folder;
 }
 
-function media_base_url()
-{
-    static $folder;
-    if (!$folder) {
-        $folder = userfiles_url() . (MW_MEDIA_FOLDER_NAME . '/');
-    }
-
-    return $folder;
-}
-
-function media_base_path()
-{
-    static $folder;
-    if (!$folder) {
-        $folder = userfiles_path() . (MW_MEDIA_FOLDER_NAME . DIRECTORY_SEPARATOR);
-    }
-
-    return $folder;
-}
-
 function modules_path()
 {
     static $folder;
@@ -49,22 +29,6 @@ function modules_path()
 
     return $folder;
 }
-
-
-function media_uploads_url()
-{
-    $environment = App::environment();
-    $folder = media_base_url() . ($environment . '/');
-    return $folder;
-}
-
-function media_uploads_path()
-{
-    $environment = App::environment();
-    $folder = media_base_path() . ($environment . DIRECTORY_SEPARATOR);
-    return $folder;
-}
-
 
 function elements_path()
 {
