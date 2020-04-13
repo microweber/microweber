8c8927964af31b1ca2016d0f0aa17198d388ae98
Peter Ivanov <peter@microweber.com>
2020-04-13 12:54:25 +0300

    update

--- userfiles/modules/captcha/templates/recaptcha.php
+++ userfiles/modules/captcha/templates/recaptcha.php
@@ -10,24 +10,25 @@ if ($captcha_provider == 'google_recaptcha_v2'):
     </script>
     <script type="text/javascript">
 
-
-        grecaptchaverifyCallback = function (response) {
-            $('#js-mw-google-recaptcha-v2-<?php print $params['id'] ?>-input').val(response);
-
-        };
-
-        grecaptchaonloadCallback = function () {
-
-            if (typeof(grecaptcha) !== 'undefined') {
-                grecaptcha.render('js-mw-google-recaptcha-v2-<?php print $params['id'] ?>', {
-                    'sitekey': '<?php echo get_option('recaptcha_v2_site_key', 'captcha'); ?>',
-                    'callback': grecaptchaverifyCallback,
-                });
-            }
-        };
         $(document).ready(function () {
             setTimeout(function () {
-                grecaptchaonloadCallback();
+                if (typeof(grecaptcha) !== 'undefined') {
+
+                    try {
+                        grecaptcha.render('js-mw-google-recaptcha-v2-<?php print $params['id'] ?>', {
+                            'sitekey': '<?php echo get_option('recaptcha_v2_site_key', 'captcha'); ?>',
+                            'callback': function (response) {
+                                $('#js-mw-google-recaptcha-v2-<?php print $params['id'] ?>-input').val(response);
+
+                            },
+                        });
+                    }
+                    catch (error) {
+
+                    }
+
+
+                }
             }, 1000);
         });
 
@@ -40,7 +41,7 @@ if ($captcha_provider == 'google_recaptcha_v2'):
 
 <?php elseif ($captcha_provider == 'google_recaptcha_v3'): ?>
     <script type="text/javascript">
-         if (typeof(grecaptcha) === 'undefined') {
+        if (typeof(grecaptcha) === 'undefined') {
             mw.require('//www.google.com/recaptcha/api.js?render=<?php echo get_option('recaptcha_v3_site_key', 'captcha'); ?>', true, 'recaptcha');
         }
     </script>
@@ -49,12 +50,20 @@ if ($captcha_provider == 'google_recaptcha_v2'):
         $(document).ready(function () {
             setTimeout(function () {
                 if (typeof(grecaptcha) !== 'undefined') {
-                    grecaptcha.ready(function () {
-                        grecaptcha.execute('<?php echo get_option('recaptcha_v3_site_key', 'captcha'); ?>', {action: 'contact'}).then(function (token) {
-                            var recaptchaResponse = document.getElementById('js-mw-google-recaptcha-v3-<?php print $params['id'] ?>-input');
-                            recaptchaResponse.value = token;
+
+                    try {
+                        grecaptcha.ready(function () {
+                            grecaptcha.execute('<?php echo get_option('recaptcha_v3_site_key', 'captcha'); ?>', {action: 'contact'}).then(function (token) {
+                                var recaptchaResponse = document.getElementById('js-mw-google-recaptcha-v3-<?php print $params['id'] ?>-input');
+                                recaptchaResponse.value = token;
+                            });
                         });
-                    });
+                    }
+                    catch (error) {
+
+                    }
+
+
                 }
             }, 1000);
         });
