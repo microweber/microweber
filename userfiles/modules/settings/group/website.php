cb060f72db3a5afd8d541c1a0e2036c7251a7b6b
Bobkata <selfworksbg@gmail.com>
2020-04-28 17:00:31 +0300

    update

--- userfiles/modules/settings/group/website.php
+++ userfiles/modules/settings/group/website.php
@@ -102,20 +102,19 @@
             </label>
 
             <?php
-            $permalink_structures = array(
-                '/sample-post',
-                '/sample-category/sample-post',
-                '/sample-category/sample-sub-category/sample-post',
-                '/sample-page/sample-post',
-                '/sample-page/sample-category/sample-post',
-                '/sample-page/sample-category/sample-sub-category/sample-post',
+            $permalinkStructures = array(
+                'post'=> 'sample-post',
+                'category_post'=> 'sample-category/sample-post',
+                'page_category_post'=> 'sample-page/sample-category/sample-post',
+                'page_category_sub_categories_post'=> 'sample-page/sample-category/sub-category/sample-post'
             );
             ?>
-            <?php $curent_val = get_option('permalink_structure', 'website'); ?>
-            <b><?php echo mw()->url_manager->site_url(); ?></b> <select name="permalink_structure" class="mw-ui-field mw_option_field" option-group="website">
-                <?php if (is_array($permalink_structures)): ?>
-                    <?php foreach ($permalink_structures as $item): ?>
-                        <option value="<?php print $item ?>" <?php if ($curent_val == $item): ?> selected="selected" <?php endif; ?>><?php print $item ?></option>
+            <?php $currentPremalinkStructure = get_option('permalink_structure', 'website'); ?>
+            <b><?php echo mw()->url_manager->site_url(); ?></b>
+            <select name="permalink_structure" class="mw-ui-field mw_option_field" option-group="website">
+                <?php if (is_array($permalinkStructures)): ?>
+                    <?php foreach ($permalinkStructures as $structureKey=>$structureVal): ?>
+                        <option value="<?php print $structureKey ?>" <?php if ($currentPremalinkStructure == $structureVal): ?> selected="selected" <?php endif; ?>><?php print $structureVal ?></option>
                     <?php endforeach; ?>
                 <?php endif; ?>
             </select>
