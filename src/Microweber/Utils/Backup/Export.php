5c0b93136ca3ef767cb7b27447dd5fc561f3a491
b.slaveykov <b.slaveykov@radioactive.bg>
2020-06-04 17:29:11 +0300

    update

--- src/Microweber/Utils/Backup/Export.php
+++ src/Microweber/Utils/Backup/Export.php
@@ -67,7 +67,7 @@ class Export
         }
 
         if ($this->includeTemplates) {
-            $exportWithZip = true;
+            $exportWithZip = true;  
         }
 
 		if ($exportWithZip || $exportMediaUserFiles) {
