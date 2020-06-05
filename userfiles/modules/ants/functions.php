7b00377442f405555766d02ff05fc31fb76c3e49
Peter Ivanov <peter@microweber.com>
2020-06-05 10:49:45 +0300

    update

--- userfiles/modules/ants/functions.php
+++ userfiles/modules/ants/functions.php
@@ -5,10 +5,3 @@ function antzz()
     exit('antzz is here!!!');
 }
 
-
-
-
-event_bind('mw.cart.checkout.order_paid', function ($order) {
-
-    file_put_contents(__DIR__.'/debug.txt', print_r($order,1) . PHP_EOL . PHP_EOL, FILE_APPEND);
- });
