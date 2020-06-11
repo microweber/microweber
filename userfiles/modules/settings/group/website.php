b6c19b4e53e11699877b99ec24bc211249632e66
alex <raikov@ymail.com>
2020-06-11 11:24:47 +0300

    website classes

--- userfiles/modules/settings/group/website.php
+++ userfiles/modules/settings/group/website.php
@@ -27,31 +27,31 @@
 
 <div class="admin-side-content">
     <div class="<?php print $config['module_class'] ?>">
-        <div class="mw-ui-field-holder">
-            <label class="mw-ui-label">
+        <div class="form-group">
+            <label class="control-label">
                 <?php _e("Website Name"); ?><br>
                 <small>
                     <?php _e("This is very important for search engines"); ?>.
                     <?php _e("Your website will be categorized by many criteria and its name is one of them"); ?>.
                 </small>
             </label>
-            <input name="website_title" class="mw_option_field mw-ui-field" type="text" option-group="website" value="<?php print get_option('website_title', 'website'); ?>"/>
+            <input name="website_title" class="mw_option_field form-control" type="text" option-group="website" value="<?php print get_option('website_title', 'website'); ?>"/>
         </div>
 
-        <div class="mw-ui-field-holder">
-            <label class="mw-ui-label">
+        <div class="form-group">
+            <label class="control-label">
                 <?php _e("Website Description"); ?><br>
                 <small><?php _e("Describe what your website is about"); ?>.</small>
             </label>
-            <textarea name="website_description" class="mw_option_field mw-ui-field" type="text" option-group="website"><?php print get_option('website_description', 'website'); ?></textarea>
+            <textarea name="website_description" class="mw_option_field form-control" type="text" option-group="website"><?php print get_option('website_description', 'website'); ?></textarea>
         </div>
 
 
         <?php
 
 
-        /*        <div class="mw-ui-field-holder">
-                    <label class="mw-ui-label">
+        /*        <div class="form-group">
+                    <label class="control-label">
                         <?php _e("Shop Enable/Disable"); ?>
                     </label>
 
@@ -70,24 +70,24 @@
 
         ?>
 
-        <div class="mw-ui-field-holder">
-            <label class="mw-ui-label">
+        <div class="form-group">
+            <label class="control-label">
                 <?php _e("Website Keywords"); ?>
                 <br>
                 <small>
                     <?php _e("Ex.: Cat, Videos of Cats, Funny Cats, Cat Pictures, Cat for Sale, Cat Products and Food"); ?>
                 </small>
             </label>
-            <input name="website_keywords" class="mw_option_field mw-ui-field" type="text" option-group="website" value="<?php print get_option('website_keywords', 'website'); ?>"/>
+            <input name="website_keywords" class="mw_option_field form-control" type="text" option-group="website" value="<?php print get_option('website_keywords', 'website'); ?>"/>
         </div>
 
-        <div class="mw-ui-field-holder">
-            <label class="mw-ui-label">
+        <div class="form-group">
+            <label class="control-label">
                 <?php _e("Posts per Page"); ?><br>
                 <small><?php _e("Select how many posts or products you want to be shown per page"); ?>?</small>
             </label>
 
-            <select name="items_per_page" class="mw-ui-field mw_option_field" type="range" option-group="website">
+            <select name="items_per_page" class="form-control mw_option_field" type="range" option-group="website">
                 <?php
                 $per_page = get_option('items_per_page', 'website');
                 $found = false;
@@ -111,8 +111,8 @@
 
 
 
-        <div class="mw-ui-field-holder js-permalink-edit-option-hook"  >
-            <label class="mw-ui-label">
+        <div class="form-group js-permalink-edit-option-hook"  >
+            <label class="control-label">
                 <?php _e("Permalink Settings"); ?>
                 <br />
                 <small>Choose the URL posts & page format.</small>
@@ -122,7 +122,7 @@
 
             <?php $currentPremalinkStructure = get_option('permalink_structure', 'website'); ?>
             <b><?php echo mw()->url_manager->site_url(); ?></b>
-            <select name="permalink_structure" class="mw-ui-field mw_option_field" option-group="website">
+            <select name="permalink_structure" class="form-control mw_option_field" option-group="website">
                 <?php if (is_array($permalinkStructures)): ?>
                     <?php foreach ($permalinkStructures as $structureKey=>$structureVal): ?>
                         <option value="<?php print $structureKey ?>" <?php if ($currentPremalinkStructure == $structureKey): ?> selected="selected" <?php endif; ?>><?php print $structureVal ?></option>
@@ -131,13 +131,13 @@
             </select>
         </div>
 
-        <div class="mw-ui-field-holder">
-            <label class="mw-ui-label">
+        <div class="form-group">
+            <label class="control-label">
                 <?php _e("Date Format"); ?>
             </label>
             <?php $date_formats = array("Y-m-d H:i:s", "Y-m-d H:i", "d-m-Y H:i:s", "d-m-Y H:i", "m/d/y", "m/d/Y", "d/m/Y", "F j, Y g:i a", "F j, Y", "F, Y", "l, F jS, Y", "M j, Y @ G:i", "Y/m/d \a\t g:i A", "Y/m/d \a\t g:ia", "Y/m/d g:i:s A", "Y/m/d", "g:i a", "g:i:s a", 'D-M-Y', 'D-M-Y H:i'); ?>
             <?php $curent_val = get_option('date_format', 'website'); ?>
-            <select name="date_format" class="mw-ui-field mw_option_field" option-group="website" style="width:300px;">
+            <select name="date_format" class="form-control mw_option_field" option-group="website" style="width:300px;">
                 <?php if (is_array($date_formats)): ?>
                     <?php foreach ($date_formats as $item): ?>
                         <option value="<?php print $item ?>" <?php if ($curent_val == $item): ?> selected="selected" <?php endif; ?>><?php print date($item, time()) ?> - (<?php print $item ?>)</option>
@@ -146,8 +146,8 @@
             </select>
         </div>
 
-        <div class="mw-ui-field-holder">
-            <label class="mw-ui-label">
+        <div class="form-group">
+            <label class="control-label">
                 <?php _e("Time Zone"); ?>
             </label>
             <?php $curent_time_zone = get_option('time_zone', 'website'); ?>
@@ -157,7 +157,7 @@
             }
 
             $timezones = timezone_identifiers_list(); ?>
-            <select name="time_zone" class="mw-ui-field mw_option_field" option-group="website" style="width:300px;">
+            <select name="time_zone" class="form-control mw_option_field" option-group="website" style="width:300px;">
                 <?php foreach ($timezones as $timezone) {
                     echo '<option';
                     if ($timezone == $curent_time_zone) echo ' selected="selected"';
@@ -167,8 +167,8 @@
         </div>
     </div>
 
-    <div class="mw-ui-field-holder" style="display: none">
-        <label class="mw-ui-label">
+    <div class="form-group" style="display: none">
+        <label class="control-label">
             <?php _e("Fonts"); ?>
         </label>
         <div class="mw-ui-box mw-ui-box-content">
@@ -224,8 +224,8 @@
     ?>
 
 
-    <div class="mw-ui-field-holder">
-        <label class="mw-ui-label">
+    <div class="form-group">
+        <label class="control-label">
             <?php _e("Change Favicon"); ?>
         </label>
 
