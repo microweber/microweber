53aedf703eb348109f56043345f40b797517a053
b.slaveykov <b.slaveykov@radioactive.bg>
2020-07-03 13:43:28 +0300

    update

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -20,7 +20,7 @@ class Kernel extends HttpKernel
         \App\Http\Middleware\CheckForMaintenanceMode::class,
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
-        \Illuminate\Session\Middleware\StartSession::class,
+        \Illuminate\Session\Middleware\StartSession::class, //TODO our routers must be added on web middleware
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
     ];
 
