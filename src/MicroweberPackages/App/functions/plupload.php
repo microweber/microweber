8865b88badde5b3b7cc1393fbd79dc67519a2b99
b.slaveykov <b.slaveykov@radioactive.bg>
2020-07-08 17:57:09 +0300

    update

--- src/MicroweberPackages/App/functions/plupload.php
+++ src/MicroweberPackages/App/functions/plupload.php
@@ -1,6 +1,6 @@
 <?php
 
-$files_utils = new Microweber\Utils\Files();
+$files_utils = new \MicroweberPackages\Utils\System\Files();
 $dangerous = $files_utils->get_dangerous_files_extentions();
 
 
@@ -287,7 +287,7 @@ $fileName = isset($_REQUEST['name']) ? $_REQUEST['name'] : '';
 
 // Clean the fileName for security reasons
 $fileNameExtension = get_file_extension($fileName);
-$fileName = \MicroweberPackages\Utils\URLify::filter($fileName);
+$fileName = \MicroweberPackages\Helpers\URLify::filter($fileName);
 //$fileName = url_title($fileName);
 //$fileName = preg_replace('/[\p{P}\p{Zs}\w\._]+/u', "", $fileName);
 // $fileName = preg_replace('/[^\w\._]+/', '_', $fileName);
