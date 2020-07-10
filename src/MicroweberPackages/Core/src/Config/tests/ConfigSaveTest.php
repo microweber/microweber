ea8409bb97cbc3ad2b6673b5f815690ffb3b2ee4
b.slaveykov <b.slaveykov@radioactive.bg>
2020-07-10 16:24:26 +0300

    update

--- src/MicroweberPackages/Core/src/Config/tests/ConfigSaveTest.php
+++ src/MicroweberPackages/Core/src/Config/tests/ConfigSaveTest.php
@@ -1,7 +1,11 @@
 <?php
 namespace MicroweberPackages\Config\tests;
 
-class ConfigSaveTest extends BaseTest
+use Illuminate\Support\Facades\Config;
+use MicroweberPackages\Config\ConfigSave;
+use MicroweberPackages\Core\tests\TestCase;
+
+class ConfigSaveTest extends TestCase
 {
     public function testSimple()
     {
@@ -13,8 +17,9 @@ class ConfigSaveTest extends BaseTest
         $this->assertEquals('Bozhidar', Config::get('microweber.firstName'));
         $this->assertEquals('Slaveykov', Config::get('microweber.lastName'));
 
-        $configFile = config_path('microweber.php');
-
+        $defaultDir = $this->app->configPath();
+        $configFile = $defaultDir . DIRECTORY_SEPARATOR . $this->app->environment(). DIRECTORY_SEPARATOR . 'microweber.php';
+        
         $this->assertTrue(is_file($configFile));
 
         $configFileContent = include($configFile);
