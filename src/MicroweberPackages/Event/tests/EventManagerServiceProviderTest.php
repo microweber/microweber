d7a95457f1183c25c0cd35a449cc98d213f77750
b.slaveykov <b.slaveykov@radioactive.bg>
2020-07-15 14:31:05 +0300

    update

--- src/MicroweberPackages/Event/tests/EventManagerServiceProviderTest.php
+++ src/MicroweberPackages/Event/tests/EventManagerServiceProviderTest.php
@@ -4,6 +4,6 @@ namespace MicroweberPackages\Event\EventManager\tests;
 class EventManagerServiceProviderTest extends BaseTest
 {
 	public function testEventManagerWhenUsing() {
-		$this->assertInstanceOf(\MicroweberPackages\Event\EventManager\Event::class, app('event_manager'));
+		$this->assertInstanceOf(\MicroweberPackages\Event\Event::class, app('event_manager'));
 	}
 }
