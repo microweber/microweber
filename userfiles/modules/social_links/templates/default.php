29614c855c1a37bf69e7bd9ac3a2150abc904ff0
nickwuk <nick@estores.org.uk>
2020-07-17 11:57:45 +0100

    GooglePlus removed, template added

--- userfiles/modules/social_links/templates/default.php
+++ userfiles/modules/social_links/templates/default.php
@@ -22,68 +22,62 @@ description: Default
     <?php if ($facebook_enabled) { ?>
 
 
-        <a href="//facebook.com/<?php print $facebook_url; ?>" target="_blank"><span class="mw-icon-facebook"></span></a>
+        <a href="//facebook.com/<?php print $facebook_url; ?>" target="_blank" aria-label="Facebook"><span class="mw-icon-facebook"></span></a>
 
     <?php } ?>
 
     <?php if ($twitter_enabled) { ?>
 
-        <a href="//twitter.com/<?php print $twitter_url; ?>" target="_blank"><span class="mw-icon-twitter"></span></a>
+        <a href="//twitter.com/<?php print $twitter_url; ?>" target="_blank" aria-label="Twitter"><span class="mw-icon-twitter"></span></a>
 
     <?php } ?>
 
-
-    <?php if ($googleplus_enabled) { ?>
-
-        <a href="//plus.google.com/<?php print $googleplus_url; ?>" target="_blank"><span class="mw-icon-googleplus"></span></a>
-
-    <?php } ?>
 
     <?php if ($pinterest_enabled) { ?>
 
-        <a href="//pinterest.com/<?php print $pinterest_url; ?>" target="_blank"><span class="mw-icon-social-pinterest"></span></a>
+        <a href="//pinterest.com/<?php print $pinterest_url; ?>" target="_blank" aria-label="Pinterest"><span class="mw-icon-social-pinterest"></span></a>
 
     <?php } ?>
 
     <?php if ($youtube_enabled) { ?>
 
-        <a href="//youtube.com/<?php print $youtube_url; ?>" target="_blank"><span class="mw-icon-social-youtube"></span></a>
+        <a href="//youtube.com/<?php print $youtube_url; ?>" target="_blank" aria-label="YouTube"><span class="mw-icon-social-youtube"></span></a>
 
     <?php } ?>
 
     <?php if ($instagram_enabled) { ?>
 
-        <a href="https://instagram.com/<?php print $instagram_url; ?>" target="_blank"><span class="mw-icon-social-instagram"></span></a>
+        <a href="https://instagram.com/<?php print $instagram_url; ?>" target="_blank" aria-label="Instagram"><span class="mw-icon-social-instagram"></span></a>
 
     <?php } ?>
 
     <?php if ($linkedin_enabled) { ?>
 
-        <a href="//linkedin.com/<?php print $linkedin_url; ?>" target="_blank"><span class="mw-icon-social-linkedin"></span></a>
+        <a href="//linkedin.com/<?php print $linkedin_url; ?>" target="_blank" aria-label="LinkedIn"><span class="mw-icon-social-linkedin"></span></a>
 
     <?php } ?>
 
     <?php if ($github_enabled) { ?>
 
-        <a href="//github.com/<?php print $github_url; ?>" target="_blank"><span class="mw-icon-social-github"></span></a>
+        <a href="//github.com/<?php print $github_url; ?>" target="_blank" aria-label="GitHub"><span class="mw-icon-social-github"></span></a>
 
     <?php } ?>
 
     <?php if ($soundcloud_enabled) { ?>
 
-        <a href="//soundcloud.com/<?php print $soundcloud_url; ?>" target="_blank"><span class="fab fa-soundcloud mw-icon-social-soundcloud"></span></a>
+        <a href="//soundcloud.com/<?php print $soundcloud_url; ?>" target="_blank" aria-label="SoundCloud"><span class="fab fa-soundcloud mw-icon-social-soundcloud"></span></a>
 
     <?php } ?>
 
     <?php if ($mixcloud_enabled) { ?>
 
-        <a href="//mixcloud.com/<?php print $mixcloud_url; ?>" target="_blank"><span class="fab fa-mixcloud mw-icon-social-mixcloud"></span></a>
+        <a href="//mixcloud.com/<?php print $mixcloud_url; ?>" target="_blank" aria-label="Mixcloud"><span class="fab fa-mixcloud mw-icon-social-mixcloud"></span></a>
 
     <?php } ?>
 
     <?php if ($medium_enabled) { ?>
 
-        <a href="//medium.com/<?php print $medium_url; ?>" target="_blank"><span class="fab fa-medium mw-icon-social-medium"></span></a>
+        <a href="//medium.com/<?php print $medium_url; ?>" target="_blank" aria-label="Medium"><span class="fab fa-medium mw-icon-social-medium"></span></a>
 
     <?php } ?>
 
