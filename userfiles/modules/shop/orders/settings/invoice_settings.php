5e5233bd2cbf217bd939f39a96a2e22db7ffa44b
b.slaveykov <b.slaveykov@radioactive.bg>
2020-07-31 13:28:28 +0300

    update

--- userfiles/modules/shop/orders/settings/invoice_settings.php
+++ userfiles/modules/shop/orders/settings/invoice_settings.php
@@ -1,3 +1,12 @@
+<script type="text/javascript">
+    $(document).ready(function () {
+        mw.options.form('.<?php print $config['module_class'] ?>', function () {
+            mw.notification.success("<?php _ejs("Saved"); ?>.");
+        });
+    });
+</script>
+
+<div class="mw-ui-box mw-ui-settings-box mw-ui-box-content">
 <div class="mw-ui-row m-b-10">
     <div class="">
         <p class="bold">Enable invoicing</p>
@@ -15,13 +24,13 @@
 
 <div class="m-b-10">
     <label class="mw-ui-label bold p-b-10">Company Logo: </label>
-    <input type="file" class="mw-ui-field mw_option_field block-field" data-option-group="shop" name="invoice_company_logo" placeholder="" value="">
+    <input type="file" class="mw-ui-field mw_option_field block-field" data-option-group="shop" name="invoice_company_logo" placeholder="" value="<?php echo get_option('invoice_company_logo', 'shop'); ?>">
 </div>
 
 <div class="mw-ui-row">
     <div class="m-b-10">
         <label class="mw-ui-label bold p-b-10">Company Name: </label>
-        <input type="text" class="mw-ui-field mw_option_field block-field" data-option-group="shop" name="invoice_company_name" placeholder="" value="">
+        <input type="text" class="mw-ui-field mw_option_field block-field" data-option-group="shop" name="invoice_company_name" placeholder="" value="<?php echo get_option('invoice_company_name', 'shop'); ?>">
     </div>
 </div>
 
@@ -44,31 +53,37 @@
 <div class="mw-ui-row">
     <div class="m-b-10">
         <label class="mw-ui-label bold p-b-10">Company City: </label>
-        <input type="text" class="mw-ui-field mw_option_field block-field" data-option-group="shop" name="invoice_company_city" placeholder="" value="">
+        <input type="text" class="mw-ui-field mw_option_field block-field" data-option-group="shop" name="invoice_company_city" placeholder="" value="<?php echo get_option('invoice_company_city', 'shop'); ?>">
     </div>
 
     <div class="m-b-10">
         <label class="mw-ui-label bold p-b-10">Company Address: </label>
-        <input type="text" class="mw-ui-field mw_option_field block-field" data-option-group="shop" name="invoice_company_address" placeholder="" value="">
+        <input type="text" class="mw-ui-field mw_option_field block-field" data-option-group="shop" name="invoice_company_address" placeholder="" value="<?php echo get_option('invoice_company_address', 'shop'); ?>">
     </div>
 
     <div class="m-b-10">
         <label class="mw-ui-label bold p-b-10">Company VAT Number: </label>
-        <input type="text" class="mw-ui-field mw_option_field block-field" data-option-group="shop" name="invoice_company_vat_number" placeholder="" value="">
+        <input type="text" class="mw-ui-field mw_option_field block-field" data-option-group="shop" name="invoice_company_vat_number" placeholder="" value="<?php echo get_option('invoice_company_vat_number', 'shop'); ?>">
     </div>
 
     <div class="m-b-10">
         <label class="mw-ui-label bold p-b-10">ID Company Number: </label>
-        <input type="text" class="mw-ui-field mw_option_field block-field" data-option-group="shop" name="invoice_id_company_number" placeholder="" value="">
+        <input type="text" class="mw-ui-field mw_option_field block-field" data-option-group="shop" name="invoice_id_company_number" placeholder="" value="<?php echo get_option('invoice_id_company_number', 'shop'); ?>">
+    </div>
+
+    <div class="m-b-10">
+        <label class="mw-ui-label bold p-b-10">Invoice Prefix: </label>
+        <input type="text" class="mw-ui-field mw_option_field block-field" data-option-group="shop" name="invoice_prefix" placeholder="" value="<?php echo get_option('invoice_prefix', 'shop'); ?>">
     </div>
 
     <div class="m-b-10">
         <label class="mw-ui-label bold p-b-10">Additional information: </label>
-        <textarea class="mw-ui-field mw_option_field block-field" data-option-group="shop" name="invoice_company_bank_details" placeholder="For example: reason for taxes"></textarea>
+        <textarea class="mw-ui-field mw_option_field block-field" data-option-group="shop" name="invoice_company_bank_details" placeholder="For example: reason for taxes"><?php echo get_option('invoice_company_bank_details', 'shop'); ?></textarea>
     </div>
 
     <div class="m-b-10">
         <label class="mw-ui-label bold p-b-10">Bank transfer details: </label>
-        <textarea class="mw-ui-field mw_option_field block-field" data-option-group="shop" name="invoice_company_bank_details" placeholder="Enter your bank details here"></textarea>
+        <textarea class="mw-ui-field mw_option_field block-field" data-option-group="shop" name="invoice_company_bank_details" placeholder="Enter your bank details here"><?php echo get_option('invoice_company_bank_details', 'shop'); ?></textarea>
     </div>
+</div>
 </div>
