c96417388df6d34c82b1bdc819c0b73e63caff0c
b.slaveykov <b.slaveykov@radioactive.bg>
2020-08-05 16:18:48 +0300

    update

---
+++ src/Microweber/tests/TestCase.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Bojidar
+ * Date: 8/5/2020
+ * Time: 4:17 PM
+ */
+namespace Microweber\tests;
+
+class TestCase extends \MicroweberPackages\Core\tests\TestCase {
+
+}
