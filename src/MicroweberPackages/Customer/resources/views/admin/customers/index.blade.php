2226b17836428bd9322f7383992a8cc396cc406c
b.slaveykov <b.slaveykov@radioactive.bg>
2020-08-06 13:44:53 +0300

    update

--- src/MicroweberPackages/Customer/resources/views/admin/customers/index.blade.php
+++ src/MicroweberPackages/Customer/resources/views/admin/customers/index.blade.php
@@ -1,5 +1,11 @@
 @extends('invoice::admin.layout')
 
+@section('icon')
+    <i class="fa fa-user module-icon-svg-fill"></i>
+@endsection
+
+@section('title', _e('Clients', true))
+
 @section('content')
 
     <script type="text/javascript">
@@ -33,19 +39,19 @@
     <form method="get">
         <div class="row well">
             <div class="col-md-3">
-                <b>Name</b>
+                <b><?php _e('Name'); ?></b>
                 <input type="text" class="form-control" value="@if(request()->get('name')){{request()->get('name')}}@endif" name="name">
             </div>
             <div class="col-md-3">
-                <b>Contact Name</b>
+                <b><?php _e('Contact Name'); ?></b>
                 <input type="text" class="form-control" value="@if(request()->get('contact_name')){{request()->get('contact_name')}}@endif" name="contact_name">
             </div>
             <div class="col-md-3">
-                <b>Phone</b>
+                <b><?php _e('Phone'); ?></b>
                 <input type="text" class="form-control" value="@if(request()->get('phone')){{request()->get('phone')}}@endif" name="phone">
             </div>
-            <div class="col-md-2">
-                <button type="submit" style="margin-top: 17px" class="btn btn-success btn-block"><i class="fa fa-filter"></i> Filter results</button>
+            <div class="col-md-3">
+                <button type="submit" style="margin-top: 17px" class="btn btn-success btn-block"><i class="fa fa-filter"></i> <?php _e('Filter results'); ?></button>
             </div>
         </div>
     </form>
@@ -55,7 +61,7 @@
     <div class="actions">
         <form method="POST" class="js-delete-selected-form" action="{{ route('customers.delete') }}">
             {{csrf_field()}}
-            <button class="btn btn-danger js-delete-all"><i class="fa fa-times"></i> Delete all</button>
+            <button class="btn btn-danger js-delete-all"><i class="fa fa-times"></i> <?php _e('Delete all'); ?></button>
         </form>
     </div>
 
@@ -63,11 +69,11 @@
         <thead>
         <tr>
             <th><input type="checkbox" class="js-select-all"></th>
-            <th>Customer Name</th>
-            <th>Email</th>
-            <th>Phone</th>
-            <th>Amount Due</th>
-            <th>Added on</th>
+            <th><?php _e('Customer Name'); ?></th>
+            <th><?php _e('Email'); ?></th>
+            <th><?php _e('Phone'); ?></th>
+            <th><?php _e('Amount Due'); ?></th>
+            <th><?php _e('Added on'); ?></th>
             <th></th>
         </tr>
         </thead>
@@ -86,9 +92,9 @@
                      Action
                     </a>
                     <div class="dropdown-menu dropdown-menu-right">
-                        <a class="dropdown-item" href="{{ route('customers.edit', $customer->id) }}"><i class="fa fa-pen"></i> &nbsp; Edit</a>
-                        <a class="dropdown-item" href="{{ route('customers.show', $customer->id) }}"><i class="fa fa-eye"></i> &nbsp; View</a>
-                        <a class="dropdown-item" href="{{ route('customers.edit', $customer->id) }}"><i class="fa fa-times"></i> &nbsp; Delete</a>
+                        <a class="dropdown-item" href="{{ route('customers.edit', $customer->id) }}"><i class="fa fa-pen"></i> &nbsp; <?php _e('Edit'); ?></a>
+                        <a class="dropdown-item" href="{{ route('customers.show', $customer->id) }}"><i class="fa fa-eye"></i> &nbsp; <?php _e('View'); ?></a>
+                        <a class="dropdown-item" href="{{ route('customers.edit', $customer->id) }}"><i class="fa fa-times"></i> &nbsp; <?php _e('Delete'); ?></a>
                     </div>
                 </div>
             </td>
