ff01f3c147650c926e264579c6ac1d8e68ef6e71
Petko Yovchevski <petko@microweber.com>
2020-08-06 14:04:54 +0300

    updates

--- src/MicroweberPackages/Customer/resources/views/admin/customers/index.blade.php
+++ src/MicroweberPackages/Customer/resources/views/admin/customers/index.blade.php
@@ -93,34 +93,32 @@
         </thead>
         <tbody>
         @foreach($customers as $customer)
-        <tr>
-            <th><input type="checkbox" name="id" class="js-selected-customer" value="{{$customer->id}}"></th>
-            <td>{{ $customer->first_name }} {{ $customer->last_name }}</td>
-            <td>{{ $customer->email }}</td>
-            <td>{{ $customer->phone }}</td>
-            <td>{{ number_format($customer->due_amount, 2) }}</td>
-            <td>{{ $customer->created_at }}</td>
-            <td>
-                <div class="btn-group">
-                    <a type="button" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true"
-                       aria-expanded="false">
-                        Action
-                    </a>
-                    <div class="dropdown-menu dropdown-menu-right">
-                        <a class="dropdown-item" href="{{ route('customers.edit', $customer->id) }}"><i
-                                    class="fa fa-pen"></i> &nbsp; <?php _e('Edit'); ?></a>
-                        <a class="dropdown-item" href="{{ route('customers.show', $customer->id) }}"><i
-                                    class="fa fa-eye"></i> &nbsp; <?php _e('View'); ?></a>
-                        <a class="dropdown-item" href="{{ route('customers.edit', $customer->id) }}"><i
-                                    class="fa fa-times"></i> &nbsp; <?php _e('Delete'); ?></a>
+            <tr>
+                <th><input type="checkbox" name="id" class="js-selected-customer" value="{{$customer->id}}"></th>
+                <td>{{ $customer->first_name }} {{ $customer->last_name }}</td>
+                <td>{{ $customer->email }}</td>
+                <td>{{ $customer->phone }}</td>
+                <td>{{ number_format($customer->due_amount, 2) }}</td>
+                <td>{{ $customer->created_at }}</td>
+                <td>
+                    <div class="btn-group">
+                        <a type="button" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true"
+                           aria-expanded="false">
+                            Action
+                        </a>
+                        <div class="dropdown-menu dropdown-menu-right">
+                            <a class="dropdown-item" href="{{ route('customers.edit', $customer->id) }}"><i
+                                        class="fa fa-pen"></i> &nbsp; <?php _e('Edit'); ?></a>
+                            <a class="dropdown-item" href="{{ route('customers.show', $customer->id) }}"><i
+                                        class="fa fa-eye"></i> &nbsp; <?php _e('View'); ?></a>
+                            <a class="dropdown-item" href="{{ route('customers.edit', $customer->id) }}"><i
+                                        class="fa fa-times"></i> &nbsp; <?php _e('Delete'); ?></a>
+                        </div>
                     </div>
-                </div>
-            </td>
-        </tr>
+                </td>
+            </tr>
         @endforeach
         </tbody>
     </table>
 
-
-    </div>
 @endsection
