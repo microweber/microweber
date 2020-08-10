16f00e7dc94f0b7765ad463527ead730806f64e2
Petko Yovchevski <petko@microweber.com>
2020-08-10 17:45:42 +0300

    updates

--- userfiles/modules/popup/admin.php
+++ userfiles/modules/popup/admin.php
@@ -32,6 +32,7 @@ if (isset($params["live_edit"]) and $params["live_edit"]) {
 
         // TODO: fix module refresh on closing settings
         ?>
+        <script>mw.lib.require('mwui_init');</script>
 
         <script type="text/javascript">
             is_searching = false;
@@ -117,90 +118,73 @@ if (isset($params["live_edit"]) and $params["live_edit"]) {
             });
         </script>
 
-        <style>
-            .module-popup-settings .mw-ui-inline-list li {
-                list-style: none;
-                margin-bottom: 10px;
-            }
 
-            #insert_link_list .mw-dropdown-content {
-                position: relative;
-            }
+        <nav class="nav nav-pills nav-justified btn-group btn-group-toggle btn-hover-style-3">
+            <a class="btn btn-outline-secondary justify-content-center active" data-toggle="tab" href="#settings"><i class="mdi mdi-cog-outline mr-1"></i> <?php print _e('Settings'); ?></a>
+            <a class="btn btn-outline-secondary justify-content-center" data-toggle="tab" href="#templates"><i class="mdi mdi-pencil-ruler mr-1"></i> <?php print _e('Templates'); ?></a>
+        </nav>
 
-            #insert_link_list {
-                width: 100%;
-            }
-        </style>
-        <div class="mw-modules-tabs">
-            <div class="mw-accordion-item">
-                <div class="mw-ui-box-header mw-accordion-title">
-                    <div class="header-holder">
-                        <i class="mw-icon-gear"></i> <?php print _e('Settings'); ?>
+        <div class="tab-content py-3">
+            <div class="tab-pane fade show active" id="settings">
+                <div class="module-live-edit-settings module-popup-settings">
+                    <div class="form-group">
+                        <label class="control-label d-block"><?php _e("Show Pop-Up event"); ?></label>
+
+                        <div class="custom-control custom-radio d-inline-block mr-3">
+                            <input type="radio" id="type1" name="type" class="mw_option_field custom-control-input" value="on_click" data-refresh="popup" <?php if ($type == 'on_click'): ?>checked<?php endif; ?>>
+                            <label class="custom-control-label" for="type1">On button click</label>
+                        </div>
+
+                        <div class="custom-control custom-radio d-inline-block mr-3">
+                            <input type="radio" id="type2" name="type" class="mw_option_field custom-control-input" value="on_click_host" data-refresh="popup" <?php if ($type == 'on_click_host'): ?>checked<?php endif; ?>>
+                            <label class="custom-control-label" for="type2">
+                                On host link click
+                                <span data-toggle="tooltip" title="Use nearest preceding link"><i class="mdi mdi-help-circle"></i></span>
+                            </label>
+                        </div>
+
+                        <div class="custom-control custom-radio d-inline-block mr-3">
+                            <input type="radio" id="type3" name="type" class="mw_option_field custom-control-input" value="on_time" data-refresh="popup" <?php if ($type == 'on_time'): ?>checked<?php endif; ?>>
+                            <label class="custom-control-label" for="type3">
+                                On time
+                                <span data-toggle="tooltip" title="Only first time (if accept) with cookies"><i class="mdi mdi-help-circle"></i></span>
+                            </label>
+                        </div>
+
+                        <div class="custom-control custom-radio d-inline-block mr-3">
+                            <input type="radio" id="type4" name="type" class="mw_option_field custom-control-input" value="on_leave_window" data-refresh="popup" <?php if ($type == 'on_leave_window'): ?>checked<?php endif; ?>>
+                            <label class="custom-control-label" for="type4">On Leave window</label>
+                        </div>
                     </div>
-                </div>
-                <div class="mw-accordion-content mw-ui-box mw-ui-box-content">
-                    <div class="module-live-edit-settings module-popup-settings">
-                        <div class="mw-ui-field-holder">
-                            <label class="mw-ui-label"><?php _e("Show Pop-Up event"); ?></label>
-                            <ul class="mw-ui-inline-list">
-                                <li>
-                                    <label class="mw-ui-check">
-                                        <input type="radio" name="type" class="mw_option_field" value="on_click" data-refresh="popup"
-                                               <?php if ($type == 'on_click'): ?>checked<?php endif; ?>><span></span><span>On click link</span>
-                                    </label>
-                                </li>
-                                <li>
-                                    <label class="mw-ui-check">
-                                        <input type="radio" name="type" class="mw_option_field" value="on_click_host" data-refresh="popup"
-                                               <?php if ($type == 'on_click_host'): ?>checked<?php endif; ?>><span></span><span>On click host link (<span class="tip" data-tipposition="top-center" title="Use nearest preceding link">?</span>)</span>
-                                    </label>
-                                </li>
-                                <li>
-                                    <label class="mw-ui-check">
-                                        <input type="radio" name="type" class="mw_option_field" value="on_time" data-refresh="popup"
-                                               <?php if ($type == 'on_time'): ?>checked<?php endif; ?>><span></span><span>On time (<span class="tip" data-tipposition="top-center" title="Only first time (if accept) with cookies">?</span>)</span>
-                                    </label>
-                                </li>
-                                <li>
-                                    <label class="mw-ui-check">
-                                        <input type="radio" name="type" class="mw_option_field" value="on_leave_window" data-refresh="popup"
-                                               <?php if ($type == 'on_leave_window'): ?>checked<?php endif; ?>><span></span><span>On Leave window</span>
-                                    </label>
-                                </li>
-                            </ul>
+
+                    <div class="form-group link_text">
+                        <label class="control-label"><?php _e("Button text"); ?></label>
+                        <input type="text" name="link_text" class="mw_option_field form-control" value="<?php print $link_text; ?>" data-refresh="popup"/>
+                    </div>
+
+                    <div class="form-group js-time-delay" style="display: none;">
+                        <label class="control-label"><?php _e("Time delay in MS"); ?></label>
+                        <small class="text-muted d-block mb-2"><?php _e("Only if Show Pop-Up Event is On time"); ?></small>
+                        <input type="text" name="time_delay" class="mw_option_field form-control" value="<?php print $time_delay; ?>" data-refresh="popup"/>
+                    </div>
+
+                    <div class="form-group">
+                        <label class="control-label d-block"><?php _e("Content source"); ?></label>
+
+                        <div class="custom-control custom-radio d-inline-block mr-3">
+                            <input type="radio" id="source1" name="source" class="mw_option_field custom-control-input" value="edited_text" data-refresh="popup" <?php if ($source == 'edited_text'): ?>checked<?php endif; ?>>
+                            <label class="custom-control-label" for="source1">Edited text (<span class="tip" data-tipposition="top-center" title="Open the popup to edit the text">?</span>)</label>
                         </div>
 
-                        <div class="mw-ui-field-holder link_text">
-                            <label class="mw-ui-label"><?php _e("Link text"); ?></label>
-                            <input type="text" name="link_text" class="mw_option_field mw-ui-field mw-full-width" value="<?php print $link_text; ?>" data-refresh="popup"/>
+                        <div class="custom-control custom-radio d-inline-block mr-3">
+                            <input type="radio" id="source2" name="source" class="mw_option_field custom-control-input" value="existing_page" data-refresh="popup" <?php if ($source == 'existing_page'): ?>checked<?php endif; ?>>
+                            <label class="custom-control-label" for="source2">Existing page content</label>
                         </div>
+                    </div>
 
-                        <div class="mw-ui-field-holder js-time-delay" style="display: none;">
-                            <label class="mw-ui-label"><?php _e("Time delay in MS"); ?></label>
-                            <input type="text" name="time_delay" class="mw_option_field mw-ui-field mw-full-width" value="<?php print $time_delay; ?>" data-refresh="popup"/>
-                            <small><?php _e("Only if Show Pop-Up Event is On time"); ?></small>
-                        </div>
-
-                        <div class="mw-ui-field-holder">
-                            <label class="mw-ui-label"><?php _e("Content source"); ?></label>
-                            <ul class="mw-ui-inline-list">
-                                <li>
-                                    <label class="mw-ui-check">
-                                        <input type="radio" name="source" class="mw_option_field" value="edited_text" data-refresh="popup"
-                                               <?php if ($source == 'edited_text'): ?>checked<?php endif; ?>><span></span><span>Edited text (<span class="tip" data-tipposition="top-center" title="Open the popup to edit the text">?</span>)</span>
-                                    </label>
-                                </li>
-                                <li>
-                                    <label class="mw-ui-check">
-                                        <input type="radio" name="source" class="mw_option_field" value="existing_page" data-refresh="popup"
-                                               <?php if ($source == 'existing_page'): ?>checked<?php endif; ?>><span></span><span>Existing page content</span>
-                                    </label>
-                                </li>
-                            </ul>
-                        </div>
-
-                        <div id="popup_url_holder" class="mw-ui-field-holder popup_page_id">
-                            <label class="mw-ui-label"><?php _e("Select existing page for popup content"); ?></label>
+                    <div id="popup_url_holder" class="form-group popup_page_id">
+                        <label class="control-label"><?php _e("Select existing page for popup content"); ?></label>
+                        <div>
                             <div data-value="<?php print site_url(); ?>" id="insert_link_list" class="mw-dropdown mw-dropdown-default active">
                                 <input type="hidden" class="mw_option_field" name="page_id" id="page_id_field"/>
                                 <input type="text" class="mw-ui-field inactive" id="dd_pages_search" autocomplete="off" placeholder="<?php _e("Click to select"); ?>"/>
@@ -212,21 +196,13 @@ if (isset($params["live_edit"]) and $params["live_edit"]) {
                                 </div>
                             </div>
                         </div>
-
                     </div>
                 </div>
             </div>
-            <div class="mw-accordion-item">
-                <div class="mw-ui-box-header mw-accordion-title">
-                    <div class="header-holder">
-                        <i class="mw-icon-beaker"></i> <?php print _e('Templates'); ?>
-                    </div>
-                </div>
-                <div class="mw-accordion-content mw-ui-box mw-ui-box-content">
-                    <module type="admin/modules/templates"/>
-                </div>
+
+            <div class="tab-pane fade" id="templates">
+                <module type="admin/modules/templates"/>
             </div>
         </div>
-
     </div>
 </div>
