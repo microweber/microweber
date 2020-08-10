f2356094f0a19a315ccbfb88f711e0365e766bf2
Petko Yovchevski <petko@microweber.com>
2020-08-10 16:48:28 +0300

    updates

--- userfiles/modules/tags/admin_backend.php
+++ userfiles/modules/tags/admin_backend.php
@@ -18,34 +18,8 @@ if (isset($params["live_edit"]) and $params["live_edit"]) {
     </div>
 
     <div class="card-body pt-3">
-
-        <style>
-            .mw-select {
-                width: 100%;
-            }
-
-            .select-posts {
-                width: 500px;
-            }
-
-            .mw-ui-btn-nav-tabs > .mw-ui-btn:last-child {
-                float: right;
-                border-left-width: 1px;
-                display: none;
-            }
-
-            .mw-ui-btn-nav-tabs {
-                width: 100%;
-            }
-
-        </style>
-
         <script>
-            mw.lib.require('bootstrap4');
-
-
             function editTaggingTag(tagging_tag_id, taggable_ids) {
-
                 var modal_title = 'Add new tag';
                 if (tagging_tag_id) {
                     modal_title = 'Edit tag';
@@ -62,7 +36,6 @@ if (isset($params["live_edit"]) and $params["live_edit"]) {
                 params.taggable_ids = taggable_ids;
 
                 mw.load_module('tags/edit_tagging_tag', '#mw_admin_edit_tagging_tag_item_module', null, params);
-
             }
 
             function deleteTaggingTag(tagging_tag_id) {
@@ -91,7 +64,6 @@ if (isset($params["live_edit"]) and $params["live_edit"]) {
             }
 
             function addTaggingTagged(taggable_id = false, taggable_ids = false) {
-
                 var modal_title = 'Add new tag';
 
                 mw_admin_edit_tag_modal = mw.dialog({
@@ -105,7 +77,6 @@ if (isset($params["live_edit"]) and $params["live_edit"]) {
                 params.taggable_ids = taggable_ids;
 
                 mw.load_module('tags/add_tagging_tagged', '#mw_admin_add_tagging_tagged_item_module', null, params);
-
             }
 
             function deleteTaggingTagged(tagging_tagged_id) {
@@ -135,7 +106,6 @@ if (isset($params["live_edit"]) and $params["live_edit"]) {
                 });
 
                 var params = {}
-                //      params.tag_id = $id;
                 params.tags = $(instance).attr('data-slug');
                 params.no_toolbar = 1;
 
@@ -143,89 +113,49 @@ if (isset($params["live_edit"]) and $params["live_edit"]) {
             }
         </script>
 
-        <?php
-        sync_tags();
-        ?>
+        <?php sync_tags(); ?>
 
-        <div id="mw-admin-content" class="admin-side-content">
+        <nav class="nav nav-pills nav-justified btn-group btn-group-toggle btn-hover-style-3">
+            <a class="btn btn-outline-secondary justify-content-center active" data-toggle="tab" href="#list"><i class="mdi mdi-format-list-bulleted-square mr-1"></i> Posts</a>
+            <a class="btn btn-outline-secondary justify-content-center" data-toggle="tab" href="#global-tags"><i class="mdi mdi-format-list-bulleted-square mr-1"></i> <?php _e('Global Tags'); ?></a>
+        </nav>
 
-
-            <div class="mw-modules-tabs">
-
-                <div class="mw-accordion-item">
-                    <div class="mw-ui-box-header mw-accordion-title">
-                        <div class="header-holder">
-                            <i class="mw-icon-navicon-round"></i> <?php _e('Posts'); ?>
-                        </div>
-                    </div>
-                    <div class="mw-accordion-content mw-ui-box mw-ui-box-content" style="min-height: 500px">
-
-                        <module type="tags/manage_tagging_tagged"/>
-
-                    </div>
-                </div>
-
-
-                <div class="mw-accordion-item">
-                    <div class="mw-ui-box-header mw-accordion-title">
-                        <div class="header-holder">
-                            <i class="mw-icon-navicon-round"></i> <?php _e('Global Tags'); ?>
-                        </div>
-                    </div>
-                    <div class="mw-accordion-content mw-ui-box mw-ui-box-content">
-
-                        <div class="mw-flex-col-xs-12">
-                            <div class="row">
-                                <div class="col-md-6">
-                                    <div style="font-weight: bold;">Search tags</div>
-                                    <div class="input-group">
-                                        <input type="text" class="form-control js-search-tags-keyword" placeholder="Keyword...">
-                                        <div class="input-group-append">
-                                            <button class="btn btn-success js-search-posts-submit" type="button">Search</button>
-                                        </div>
-                                    </div>
-                                    <span class="mb-3">You can search multiple tags seperated by coma.</span>
-                                </div>
-                                <div class="col-md-6" style="padding-top: 17px">
-                                    <button class="btn btn-success pull-right" onclick="editTaggingTag(false);"><i class="fa fa-plus"></i> Create new global tag</button>
-
-                                </div>
-
-                                <div class="col-md-12">
-                                    <div class="card" style="margin-top:15px">
-                                        <div class="card-header">
-                                            Global tags
-                                        </div>
-                                        <div class="card-body">
-                                            <div class="js-all-tags"></div>
-                                        </div>
-                                    </div>
-                                </div>
-
-                            </div>
-                        </div>
-
-                    </div>
-                </div>
-
-                <div class="mw-accordion-item">
-                    <div class="mw-ui-box-header mw-accordion-title">
-                        <a href="#"><i class="fa fa-info-circle"></i> <?php _e('How to use this module?'); ?></a>
-                    </div>
-                    <div class="mw-accordion-content mw-ui-box mw-ui-box-content">
-
-                        How to ?
-
-                    </div>
-                </div>
-
+        <div class="tab-content py-3">
+            <div class="tab-pane fade show active" id="list">
+                <module type="tags/manage_tagging_tagged"/>
             </div>
 
-            <br/>
+            <div class="tab-pane fade" id="global-tags">
 
-            <module type="help/modal_with_button" for_module="tags"/>
+                <div class="row">
+                    <div class="col-md-6">
+                        <div style="font-weight: bold;">Search tags</div>
+                        <div class="input-group">
+                            <input type="text" class="form-control js-search-tags-keyword" placeholder="Keyword...">
+                            <div class="input-group-append">
+                                <button class="btn btn-success js-search-posts-submit" type="button">Search</button>
+                            </div>
+                        </div>
+                        <span class="mb-3">You can search multiple tags seperated by coma.</span>
+                    </div>
 
+                    <div class="col-md-6">
+                        <button class="btn btn-success pull-right" onclick="editTaggingTag(false);"><i class="fa fa-plus"></i> Create new global tag</button>
+                    </div>
+
+                    <div class="col-md-12">
+                        <div class="card style-1">
+                            <div class="card-header">Global tags</div>
+                            <div class="card-body">
+                                <div class="js-all-tags"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
+
+        <module type="help/modal_with_button" for_module="tags"/>
     </div>
 </div>
 
