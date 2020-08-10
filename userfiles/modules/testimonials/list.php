19aca9d94efc04613193aef19ca349e5a6ca63f7
Petko Yovchevski <petko@microweber.com>
2020-08-10 11:09:31 +0300

    updates

--- userfiles/modules/testimonials/list.php
+++ userfiles/modules/testimonials/list.php
@@ -83,47 +83,47 @@
         margin: 0 auto 10px auto;
     }
 
-    .text-danger.position-absolute{
+    .text-danger.position-absolute {
         right: -10px;
         top: -20px;
     }
 
-    .testimonial-holder .text-danger.position-absolute{
+    .testimonial-holder .text-danger.position-absolute {
         display: none;
     }
 
-    .testimonial-holder:hover .text-danger.position-absolute{
+    .testimonial-holder:hover .text-danger.position-absolute {
         display: block;
     }
 </style>
 
 <?php $data = get_testimonials(); ?>
 <?php if ($data): ?>
+    <div class="muted-cards-3">
+        <?php foreach ($data as $item): ?>
+            <div class="card style-1 testimonial-holder mb-3" data-id="<?php print $item['id'] ?>">
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-auto d-flex flex-column align-items-center">
+                            <div class="img-circle-holder img-absolute">
+                                <img src="<?php print thumbnail($item['client_picture'], 75, 75) ?>"/>
+                            </div>
 
-    <?php foreach ($data as $item): ?>
-        <div class="card style-1 testimonial-holder mb-3" data-id="<?php print $item['id'] ?>">
-            <div class="card-body">
-                <div class="row">
-                    <div class="col-auto d-flex flex-column align-items-center">
-                        <div class="img-circle-holder img-absolute">
-                            <img src="<?php print thumbnail($item['client_picture'], 75, 75) ?>"/>
+                            <a class="btn btn-outline-primary btn-sm mt-2" href="javascript:;" onclick="edit_testimonial('<?php print $item['id'] ?>');">Edit</a>
                         </div>
 
-                        <a class="btn btn-outline-primary btn-sm mt-2" href="javascript:;" onclick="edit_testimonial('<?php print $item['id'] ?>');">Edit</a>
-                    </div>
+                        <div class="col">
+                            <a href="javascript:delete_testimonial('<?php print $item['id'] ?>');" class="btn btn-link text-danger btn-sm position-absolute" data-toggle="tooltip" data-title="Delete item"><i class="mdi mdi-close-thick"></i></a>
 
-                    <div class="col">
-                        <a href="javascript:delete_testimonial('<?php print $item['id'] ?>');" class="btn btn-link text-danger btn-sm position-absolute" data-toggle="tooltip" data-title="Delete item"><i class="mdi mdi-close-thick"></i></a>
+                            <h6 class="font-weight-bold"><?php print $item['name'] ?> </h6>
 
-                        <h6 class="font-weight-bold"><?php print $item['name'] ?> </h6>
-
-                        <p><?php print character_limiter($item['content'], 400); ?></p>
+                            <p><?php print character_limiter($item['content'], 400); ?></p>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    <?php endforeach; ?>
-
+        <?php endforeach; ?>
+    </div>
 <?php else: ?>
     <h2 class="text-center"><?php _e('You have no testimonials'); ?></h2>
     <div class="text-center"><a href="javascript:;" onclick="add_new_testimonial()" class="mw-ui-btn"><?php _e('Create new'); ?></a></div>
--- userfiles/modules/twitter_feed/admin.php
+++ userfiles/modules/twitter_feed/admin.php
@@ -33,68 +33,57 @@ if (isset($params["live_edit"]) and $params["live_edit"]) {
             });
         </script>
 
+        <nav class="nav nav-pills nav-justified btn-group btn-group-toggle btn-hover-style-3">
+            <a class="btn btn-outline-secondary justify-content-center active" data-toggle="tab" href="#settings"><i class="mdi mdi-cog-outline mr-1"></i> <?php print _e('Settings'); ?></a>
+            <a class="btn btn-outline-secondary justify-content-center" data-toggle="tab" href="#templates"><i class="mdi mdi-pencil-ruler mr-1"></i> <?php print _e('Templates'); ?></a>
+        </nav>
 
-        <div class="mw-modules-tabs">
-            <div class="mw-accordion-item">
-                <div class="mw-ui-box-header mw-accordion-title">
-                    <div class="header-holder">
-                        <i class="mw-icon-gear"></i> <?php print _e('Settings'); ?>
+        <div class="tab-content py-3">
+            <div class="tab-pane fade show active" id="settings">
+                <!-- Settings Content -->
+                <div class="module-live-edit-settings module-twitter-feed-settings">
+                    <div class="form-group">
+                        <label class="control-label"><?php _e('Search string'); ?> <a href="https://dev.twitter.com/rest/public/search" target="_blank">[?]</a></label>
+                        <input type="text" class="mw_option_field form-control" placeholder="Example: microweber" name="search_string" value="<?php print get_option('search_string', $params['id']); ?>">
                     </div>
-                </div>
-                <div class="mw-accordion-content mw-ui-box mw-ui-box-content">
-                    <!-- Settings Content -->
-                    <div class="module-live-edit-settings module-twitter-feed-settings">
-                        <div class="mw-ui-field-holder">
-                            <label class="mw-ui-label"><?php _e('Search string'); ?> <a href="https://dev.twitter.com/rest/public/search" target="_blank">[?]</a></label>
-                            <input type="text" class="mw_option_field mw-ui-field mw-full-width" placeholder="Example: microweber" name="search_string" value="<?php print get_option('search_string', $params['id']); ?>">
-                        </div>
 
-                        <div class="mw-ui-field-holder">
-                            <label class="mw-ui-label"><?php _e('Number of items'); ?></label>
-                            <input type="number" class="mw_option_field mw-ui-field mw-full-width" name="number_of_items" value="<?php print get_option('number_of_items', $params['id']); ?>">
-                        </div>
-
-                        <hr/>
-
-                        <h5>Access Token Settings</h5>
-
-                        <div class="mw-ui-field-holder">
-                            <label class="mw-ui-label"><?php _e('Consumer Key'); ?></label>
-                            <input type="text" class="mw_option_field mw-ui-field mw-full-width" name="consumer_key" option-group="twitter_feed" value="<?php print get_option('consumer_key', 'twitter_feed'); ?>">
-                        </div>
-
-                        <div class="mw-ui-field-holder">
-                            <label class="mw-ui-label"><?php _e('Consumer Secret'); ?></label>
-                            <input type="text" class="mw_option_field mw-ui-field mw-full-width" name="consumer_secret" option-group="twitter_feed" value="<?php print get_option('consumer_secret', 'twitter_feed'); ?>">
-                        </div>
-
-                        <div class="mw-ui-field-holder">
-                            <label class="mw-ui-label"><?php _e('Access Token'); ?></label>
-                            <input type="text" class="mw_option_field mw-ui-field mw-full-width" name="access_token" option-group="twitter_feed" value="<?php print get_option('access_token', 'twitter_feed'); ?>">
-                        </div>
-
-                        <div class="mw-ui-field-holder">
-                            <label class="mw-ui-label"><?php _e('Access Token Secret'); ?></label>
-                            <input type="text" class="mw_option_field mw-ui-field mw-full-width" name="access_token_secret" option-group="twitter_feed" value="<?php print get_option('access_token_secret', 'twitter_feed'); ?>">
-                        </div>
-
-                        <p><?php _e('Get your Twitter access keys'); ?> <a href="https://apps.twitter.com/app" target="_blank" class="mw-ui-link"><?php _e('from here'); ?></a></p>
+                    <div class="form-group">
+                        <label class="control-label"><?php _e('Number of items'); ?></label>
+                        <input type="number" class="mw_option_field form-control" name="number_of_items" value="<?php print get_option('number_of_items', $params['id']); ?>">
                     </div>
-                    <!-- Settings Content - End -->
+
+                    <hr class="thin"/>
+
+                    <h5 class="font-weight-bold mb-3">Access Token Settings</h5>
+
+                    <div class="form-group">
+                        <label class="control-label"><?php _e('Consumer Key'); ?></label>
+                        <input type="text" class="mw_option_field form-control" name="consumer_key" option-group="twitter_feed" value="<?php print get_option('consumer_key', 'twitter_feed'); ?>">
+                    </div>
+
+                    <div class="form-group">
+                        <label class="control-label"><?php _e('Consumer Secret'); ?></label>
+                        <input type="text" class="mw_option_field form-control" name="consumer_secret" option-group="twitter_feed" value="<?php print get_option('consumer_secret', 'twitter_feed'); ?>">
+                    </div>
+
+                    <div class="form-group">
+                        <label class="control-label"><?php _e('Access Token'); ?></label>
+                        <input type="text" class="mw_option_field form-control" name="access_token" option-group="twitter_feed" value="<?php print get_option('access_token', 'twitter_feed'); ?>">
+                    </div>
+
+                    <div class="form-group">
+                        <label class="control-label"><?php _e('Access Token Secret'); ?></label>
+                        <input type="text" class="mw_option_field form-control" name="access_token_secret" option-group="twitter_feed" value="<?php print get_option('access_token_secret', 'twitter_feed'); ?>">
+                    </div>
+
+                    <p><?php _e('Get your Twitter access keys'); ?> <a href="https://apps.twitter.com/app" target="_blank" class="mw-ui-link"><?php _e('from here'); ?></a></p>
                 </div>
+                <!-- Settings Content - End -->
             </div>
 
-            <div class="mw-accordion-item">
-                <div class="mw-ui-box-header mw-accordion-title">
-                    <div class="header-holder">
-                        <i class="mw-icon-beaker"></i> <?php print _e('Templates'); ?>
-                    </div>
-                </div>
-                <div class="mw-accordion-content mw-ui-box mw-ui-box-content">
-                    <module type="admin/modules/templates"/>
-                </div>
+            <div class="tab-pane fade" id="templates">
+                <module type="admin/modules/templates"/>
             </div>
         </div>
-
     </div>
 </div>
