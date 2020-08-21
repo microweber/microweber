6f3dca26652871527bda7535bc860ae95aae0bdd
Petko Yovchevski <petko@microweber.com>
2020-08-21 10:40:32 +0300

    updates

--- userfiles/modules/custom_fields/values_preview.php
+++ userfiles/modules/custom_fields/values_preview.php
@@ -50,50 +50,52 @@ if (isset($params['field-id'])) {
     });
 </script>
 
-<?php if (isset($field['type']) and ($field['type'] == 'select' or $field['type'] == 'dropdown' or $field['type'] == 'checkbox' or $field['type'] == 'radio')): ?>
-    <?php
-    if (isset($field['values']) and is_array($field['values'])) {
-        $vals = $field['values'];
-    } elseif (isset($field['value'])) {
-        $vals = $field['value'];
-    } else {
-        $vals = '';
-    }
-    if (is_string($vals)) {
-        $vals = array($vals);
-    }
-    ?>
+<div class="">
+    <?php if (isset($field['type']) and ($field['type'] == 'select' or $field['type'] == 'dropdown' or $field['type'] == 'checkbox' or $field['type'] == 'radio')): ?>
+        <?php
+        if (isset($field['values']) and is_array($field['values'])) {
+            $vals = $field['values'];
+        } elseif (isset($field['value'])) {
+            $vals = $field['value'];
+        } else {
+            $vals = '';
+        }
+        if (is_string($vals)) {
+            $vals = array($vals);
+        }
+        ?>
 
-    <span class="custom-fields-values-holder d-flex flex-wrap">
+        <span class="custom-fields-values-holder d-flex flex-wrap">
         <?php $i = 0; ?>
-        <?php foreach ($vals as $val): ?>
-            <?php $i++; ?>
-            <span class="mw-admin-custom-field-value-edit-inline-holder mw-admin-custom-field-checkbox bg-primary-opacity-1 d-inline-flex mr-2 my-1 p-0">
+            <?php foreach ($vals as $val): ?>
+                <?php $i++; ?>
+                <span class="mw-admin-custom-field-value-edit-inline-holder mw-admin-custom-field-checkbox bg-primary-opacity-1 d-inline-flex mr-2 my-1 p-0">
                 <small class="mw-admin-custom-field-value-edit-inline p-1 text-dark" data-id="<?php print $field['id']; ?>"><?php print $val; ?></small>
                 <small class="delete-custom-fields bg-danger text-white p-1" onclick="mw.admin.custom_fields.deleteFieldValue(this);"><i class="mdi mdi-close"></i></small>
             </span>
-        <?php endforeach; ?>
+            <?php endforeach; ?>
     </span>
 
-    <span class="btn-create-custom-field-value btn btn-primary btn-sm py-2 px-0 d-inline-flex align-items-center justify-content-center show-on-hover" data-id="<?php print $field['id']; ?>">
+        <span class="btn-create-custom-field-value btn btn-primary btn-sm py-2 px-0 d-inline-flex align-items-center justify-content-center show-on-hover" data-id="<?php print $field['id']; ?>">
         <i class="mdi mdi-plus mdi-16px"></i>
     </span>
-<?php elseif (isset($field['type']) and ($field['type'] == 'text' or $field['type'] == 'message' or $field['type'] == 'textarea' or $field['type'] == 'title')): ?>
-    <textarea class="mw-admin-custom-field-value-edit-text form-control bg-primary-opacity-1 border-0 border-radius-0" style=" width:100%; overflow:hidden;" data-id="<?php print $field['id']; ?>"><?php print $field['value']; ?></textarea>
-<?php elseif (isset($field['type']) and (($field['type'] == 'address') or $field['type'] == 'upload')): ?>
-    <div style="width:100%; display:block; min-height:20px;" onclick="mw.admin.custom_fields.edit_custom_field_item('#mw-custom-fields-list-settings-<?php print $field['id']; ?>',<?php print $field['id']; ?>);"><?php print $field['values_plain']; ?></div>
+    <?php elseif (isset($field['type']) and ($field['type'] == 'text' or $field['type'] == 'message' or $field['type'] == 'textarea' or $field['type'] == 'title')): ?>
+        <textarea class="mw-admin-custom-field-value-edit-text form-control bg-primary-opacity-1 border-0 border-radius-0" style=" width:100%; overflow:hidden;" data-id="<?php print $field['id']; ?>"><?php print $field['value']; ?></textarea>
+    <?php elseif (isset($field['type']) and (($field['type'] == 'address') or $field['type'] == 'upload')): ?>
+        <div style="width:100%; display:block; min-height:20px;" onclick="mw.admin.custom_fields.edit_custom_field_item('#mw-custom-fields-list-settings-<?php print $field['id']; ?>',<?php print $field['id']; ?>);"><?php print $field['values_plain']; ?></div>
 
-<?php else: ?>
-    <?php $vals = '';
-    if ($field['values_plain'] != '') {
-        $vals = $field['values_plain'];
-    } elseif (is_string($field['value'])) {
-        $vals = $field['value'];
-    }
-    ?>
-    <span class="custom-fields-values-holder">
+    <?php else: ?>
+        <?php $vals = '';
+        if ($field['values_plain'] != '') {
+            $vals = $field['values_plain'];
+        } elseif (is_string($field['value'])) {
+            $vals = $field['value'];
+        }
+        ?>
+        <span class="custom-fields-values-holder">
         <span class="mw-admin-custom-field-value-edit-inline-holder bg-primary-opacity-1 d-inline-block px-3 py-1">
             <small class="mw-admin-custom-field-value-edit-inline px-1 py-1" data-id="<?php print $field['id']; ?>"><?php print $vals; ?></small>
         </span>
     </span>
-<?php endif; ?>
+    <?php endif; ?>
+</div>
