2c9a29fcda98273b02bcdeb93bd9b900e590f1a1
Petko Yovchevski <petko@microweber.com>
2020-08-24 15:28:07 +0300

    updates

--- userfiles/modules/shop/taxes/admin_list_taxes.php
+++ userfiles/modules/shop/taxes/admin_list_taxes.php
@@ -6,19 +6,21 @@ $defined_taxes = mw()->tax_manager->get();
 ?>
 
 <?php if (!empty($defined_taxes)) : ?>
+    <h6 class="font-weight-bold"><?php _e("Taxes list"); ?></h6>
+
     <div class="table-responsive">
-        <table cellspacing="0" cellpadding="0" class="table-style-3 mw-ui-table layout-auto">
+        <table cellspacing="0" cellpadding="0" class="table">
             <thead>
             <tr>
                 <th><?php _e('Tax name'); ?></th>
                 <th><?php _e('Tax rate'); ?></th>
                 <th><?php _e('Tax type'); ?></th>
-                <th class="center" style="width: 200px;"><?php _e('Actions'); ?></th>
+                <th class="text-center" style="width: 200px;"><?php _e('Actions'); ?></th>
             </tr>
             </thead>
             <tbody>
             <?php foreach ($defined_taxes as $item) : ?>
-                <tr>
+                <tr class="small">
                     <td><?php print $item['name']; ?></td>
                     <td>
                         <?php if ($item['type'] == 'percent'): ?>
@@ -32,10 +34,10 @@ $defined_taxes = mw()->tax_manager->get();
 
                     <td><?php print $item['type']; ?></td>
 
-                    <td class="center" style="padding-left: 0; padding-right: 0;">
-                        <button onclick="mw_admin_edit_tax_item_popup('<?php print $item['id']; ?>')" class="mw-ui-btn mw-ui-btn-info mw-ui-btn-medium" title="Edit"><?php print _e('Edit'); ?></button>
+                    <td class="text-center" style="padding-left: 0; padding-right: 0;">
+                        <button onclick="mw_admin_edit_tax_item_popup('<?php print $item['id']; ?>')" class="btn btn-outline-primary btn-sm" title="Edit"><?php print _e('Edit'); ?></button>
                         &nbsp;
-                        <button onclick="mw_admin_delete_tax_item_confirm('<?php print $item['id']; ?>')" class="mw-ui-btn mw-ui-btn-important mw-ui-btn-outline mw-ui-btn-medium" title="Delete"><?php print _e('Delete'); ?></button>
+                        <button onclick="mw_admin_delete_tax_item_confirm('<?php print $item['id']; ?>')" class="btn btn-outline-danger btn-sm" title="Delete"><?php print _e('Delete'); ?></button>
                     </td>
                 </tr>
             <?php endforeach; ?>
@@ -43,25 +45,11 @@ $defined_taxes = mw()->tax_manager->get();
         </table>
     </div>
 <?php else: ?>
-    <div class="table-responsive">
-        <table cellspacing="0" cellpadding="0" class="table-style-3 mw-ui-table layout-auto">
-            <thead>
-            <tr>
-                <th class="text-center">
-                    <br/>
-                    <h3><?php _e('You don\'t have any defined taxes'); ?></h3>
-                    <br/>
-                    <a class="mw-ui-btn mw-ui-btn-normal mw-ui-btn-info mw-ui-btn-outline" href="javascript:mw_admin_edit_tax_item_popup(0)"><span> <?php _e('Add new tax'); ?> </span></a>
-                    <br/> <br/> <br/>
-                </th>
-            </tr>
-            </thead>
-        </table>
+    <div class="text-center py-3">
+        <h4 class="m-0"><?php _e('You don\'t have any defined taxes'); ?></h4>
     </div>
 <?php endif; ?>
 
-
-<br/><br/>
 <?php if (!empty($defined_taxes)) : ?>
-    <span class="mw-ui-label-help"><?php _e('Example tax for 1000.00 is'); ?><?php print mw()->tax_manager->calculate(1000) ?></span>
+    <!--<span class="text-muted"><?php _e('Example tax for 1000.00 is'); ?><?php print ' ' . mw()->tax_manager->calculate(1000) ?></span>-->
 <?php endif; ?>
