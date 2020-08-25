435acdad58fb7e8388629ba2e5b9bac762f4ee30
Petko Yovchevski <petko@microweber.com>
2020-08-25 15:27:11 +0300

    updates

---
+++ userfiles/modules/admin/admin_nav.php
@@ -0,0 +1,7 @@
+<div class="position-relative">
+    <div class="main-toolbar" id="mw-modules-toolbar">
+        <div>&nbsp;</div>
+
+        <module type="admin/settings_search"/>
+    </div>
+</div>
