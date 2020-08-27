353c18ad15b5312cbabfff8c1f2375faa8b9bc9d
b.slaveykov <b.slaveykov@radioactive.bg>
2020-08-27 17:35:42 +0300

    update

--- userfiles/modules/content/views/product/variants.php
+++ userfiles/modules/content/views/product/variants.php
@@ -18,46 +18,46 @@ $productVariantOptions[] = [
 </style>
 
 <script>
-    function addProductVariantValues() {
-        var variantHtml = '   <tr>\n' +
-            '                        <th scope="row" style="vertical-align: middle;">\n' +
-            '                            <span>L / Red</span>\n' +
-            '                        </th>\n' +
-            '                        <td>\n' +
-            '                            <div class="input-group prepend-transparent m-0">\n' +
-            '                                <div class="input-group-prepend">\n' +
-            '                                    <span class="input-group-text text-muted">BGN</span>\n' +
-            '                                </div>\n' +
-            '                                <input type="text" class="form-control" value="0.00">\n' +
-            '                            </div>\n' +
-            '                        </td>\n' +
-            '                        <td>\n' +
-            '                            <div class="input-group append-transparent input-group-quantity m-0">\n' +
-            '                                <input type="text" class="form-control" value="0">\n' +
-            '                                <div class="input-group-append">\n' +
-            '                                    <div class="input-group-text plus-minus-holder">\n' +
-            '                                        <button type="button" class="plus"><i class="mdi mdi-menu-up"></i></button>\n' +
-            '                                        <button type="button" class="minus"><i class="mdi mdi-menu-down"></i></button>\n' +
-            '                                    </div>\n' +
-            '                                </div>\n' +
-            '                            </div>\n' +
-            '                        </td>\n' +
-            '                        <td>\n' +
-            '                            <div class="form-group m-0">\n' +
-            '                                <input type="text" class="form-control" value="">\n' +
-            '                            </div>\n' +
-            '                        </td>\n' +
-            '                        <td style="vertical-align: middle;">\n' +
-            '                            <div class="btn-group">\n' +
-            '                                <button class="btn btn-outline-secondary btn-sm">Edit</button>\n' +
-            '                                <button class="btn btn-outline-secondary btn-sm"><i class="mdi mdi-trash-can-outline"></i></button>\n' +
-            '                            </div>\n' +
-            '                        </td>\n' +
-            '                    </tr>';
+    function addProductVariantValues(variant_name) {
+        var variantHtml = '<tr>\n' +
+            '<th scope="row" style="vertical-align: middle;">\n' +
+            '    <span>'+variant_name+'</span>\n' +
+            '</th>\n' +
+            '<td>\n' +
+            '    <div class="input-group prepend-transparent m-0">\n' +
+            '        <div class="input-group-prepend">\n' +
+            '            <span class="input-group-text text-muted">BGN</span>\n' +
+            '        </div>\n' +
+            '        <input type="text" class="form-control" value="0.00">\n' +
+            '    </div>\n' +
+            '</td>\n' +
+            '<td>\n' +
+            '    <div class="input-group append-transparent input-group-quantity m-0">\n' +
+            '        <input type="text" class="form-control" value="0">\n' +
+            '        <div class="input-group-append">\n' +
+            '            <div class="input-group-text plus-minus-holder">\n' +
+            '                <button type="button" class="plus"><i class="mdi mdi-menu-up"></i></button>\n' +
+            '                <button type="button" class="minus"><i class="mdi mdi-menu-down"></i></button>\n' +
+            '            </div>\n' +
+            '        </div>\n' +
+            '    </div>\n' +
+            '</td>\n' +
+            '<td>\n' +
+            '    <div class="form-group m-0">\n' +
+            '        <input type="text" class="form-control" value="">\n' +
+            '    </div>\n' +
+            '</td>\n' +
+            '<td style="vertical-align: middle;">\n' +
+            '    <div class="btn-group">\n' +
+            '        <button class="btn btn-outline-secondary btn-sm">Edit</button>\n' +
+            '        <button class="btn btn-outline-secondary btn-sm"><i class="mdi mdi-trash-can-outline"></i></button>\n' +
+            '    </div>\n' +
+            '</td>\n' +
+            '</tr>';
 
-        
-
+        $('.js-product-variants-fields').append(variantHtml);
     }
+
     function addProductVariantOption(option_id = 0, option_name = '', option_values = '')
     {
         var optionHtml = '<div class="row js-product-variant-option-box js-product-variant-option-'+option_id+'">\n' +
@@ -65,7 +65,7 @@ $productVariantOptions[] = [
             '<div class="form-group">\n' +
             '     <h6 class="pb-1"><strong>Option</strong></h6>\n' +
             '      <div>\n' +
-            '           <input type="text" name="product_variant_option['+option_id+'][name]" value="'+option_name+'" class="form-control">\n' +
+            '           <input type="text" name="product_variant_option['+option_id+'][name]" value="'+option_name+'" class="form-control js-option-name">\n' +
             '     </div>\n' +
             '</div>\n' +
             '</div>\n' +
@@ -86,6 +86,49 @@ $productVariantOptions[] = [
     }
 
 
+    const moveThreadForwardAt = (t, tCursor) => {
+        if (tCursor < 0)
+            return true; // reached end of first array
+
+        const newIndex = (t[tCursor][0] + 1) % t[tCursor][1];
+        t[tCursor][0] = newIndex;
+
+        if (newIndex == 0)
+            return moveThreadForwardAt(t, tCursor - 1);
+
+        return false;
+    }
+
+    const cartesianMult = (...args) => {
+        let result = [];
+        const t = Array.from(Array(args.length)).map((x, i) => [0, args[i].length]);
+        let reachedEndOfFirstArray = false;
+
+        while (false == reachedEndOfFirstArray) {
+            result.push(t.map((v, i) => args[i][v[0]]));
+
+            reachedEndOfFirstArray = moveThreadForwardAt(t, args.length - 1);
+        }
+
+        return result;
+    }
+
+    function refreshProductVariantValues()
+    {
+        const productVariantCombinations = [];
+
+        $(".js-product-variant-option-box").each(function() {
+            //var productVariantOptionName = $(this).find('.js-option-name').val();
+            var productVariantOptionValues = $(this).find('.js-tags-input').val().split(",");
+            productVariantCombinations.push(productVariantOptionValues);
+        });
+
+        for (let item of cartesianMult(productVariantCombinations)) {
+            console.log('[' + item.join(', ') + ']');
+            addProductVariantValues(item.join('/'));
+        }
+    }
+
     <?php
     foreach ($productVariantOptions as $productVariantOptionKey=>$productVariantOption):
     ?>
@@ -98,8 +141,6 @@ $productVariantOptions[] = [
         $('.js-product-variant-option-' + option_id).remove();
     }
 
-    function
-
     $(document).ready(function () {
 
        $('.js-product-has-variants').click(function () {
@@ -111,6 +152,7 @@ $productVariantOptions[] = [
                 alert('Maximum product variants are 3');
                return;
            }
+           refreshProductVariantValues();
            addProductVariantOption(Math.floor(Math.random() * 1000));
 
        });
@@ -163,9 +205,7 @@ $productVariantOptions[] = [
                         <th scope="col" class="border-0">Actions</th>
                     </tr>
                     </thead>
-                    <tbody>
-
-                    </tbody>
+                    <tbody class="js-product-variants-fields"></tbody>
                 </table>
             </div>
         </div>
