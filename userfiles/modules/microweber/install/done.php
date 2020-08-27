02c56033859e8487bc27b2d328653c5af164b8c3
Petko Yovchevski <petko@microweber.com>
2020-08-27 17:29:02 +0300

    updates

--- userfiles/modules/microweber/install/done.php
+++ userfiles/modules/microweber/install/done.php
@@ -1,3 +1,2 @@
-
 <h2><?php _e("Done"); ?>, </h2>
 <a href="<?php print site_url('admin') ?>"><?php _e("click here to to to admin"); ?></a> <a href="<?php print site_url() ?>"><?php _e("click here to to to site"); ?></a>
