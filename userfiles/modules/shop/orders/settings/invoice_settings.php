2d5de5c82b2ad2bf6349fe4cd5682d722669effe
Petko Yovchevski <petko@microweber.com>
2020-08-28 12:04:47 +0300

    updates

--- userfiles/modules/shop/orders/settings/invoice_settings.php
+++ userfiles/modules/shop/orders/settings/invoice_settings.php
@@ -1,36 +1,50 @@
-<div class="mw-ui-row m-b-10">
-    <div class="">
-        <p class="bold">Enable invoicing</p>
+<?php
+$from_live_edit = false;
+if (isset($params["live_edit"]) and $params["live_edit"]) {
+    $from_live_edit = $params["live_edit"];
+}
+?>
+
+<?php if (isset($params['backend'])): ?>
+    <module type="admin/modules/info"/>
+<?php endif; ?>
+
+<div class="card style-1 mb-3 <?php if ($from_live_edit): ?>card-in-live-edit<?php endif; ?>">
+    <div class="card-header">
+        <?php $module_info = module_info($params['module']); ?>
+        <h5>
+            <i class="module-icon-svg-fill"></i> <strong>Invoicing</strong>
+        </h5>
     </div>
 
-    <div class="">
-        <label class="mw-switch inline-switch m-0 m-t-10 m-b-10">
-            <input type="checkbox" name="enable_invoices" data-option-group="shop" data-value-checked="1" data-value-unchecked="0" class="mw_option_field" <?php if (true): ?> checked="checked" <?php endif; ?>>
-            <span class="mw-switch-off">OFF</span>
-            <span class="mw-switch-on">ON</span>
-            <span class="mw-switcher"></span>
-        </label>
-    </div>
-</div>
+    <div class="card-body pt-3">
+        <div class="form-group">
+            <div class="custom-control custom-switch">
+                <input type="checkbox" name="enable_invoices" id="enable_invoices" data-option-group="shop" data-value-checked="1" data-value-unchecked="0" class="mw_option_field custom-control-input" <?php if (true): ?>checked<?php endif; ?>>
+                <label class="custom-control-label" for="enable_invoices">Enable invoicing</label>
+            </div>
+        </div>
 
-<div class="m-b-10">
-    <label class="mw-ui-label bold p-b-10">Company Logo: </label>
-    <input type="file" class="mw-ui-field mw_option_field block-field" data-option-group="shop" name="invoice_company_logo" placeholder="" value="">
-</div>
+        <div class="form-group">
+            <label class="control-label">Company Logo: </label>
 
-<div class="mw-ui-row">
-    <div class="m-b-10">
-        <label class="mw-ui-label bold p-b-10">Company Name: </label>
-        <input type="text" class="mw-ui-field mw_option_field block-field" data-option-group="shop" name="invoice_company_name" placeholder="" value="">
-    </div>
-</div>
+            <div class="input-group mb-3">
+                <div class="custom-file">
+                    <input type="file" class="mw_option_field custom-file-input" id="invoice_company_logo" data-option-group="shop" name="invoice_company_logo" />
+                    <label class="custom-file-label" for="invoice_company_logo">Choose file</label>
+                </div>
+            </div>
+        </div>
 
-<div class="mw-ui-row m-b-10">
-    <div class="mw-ui-col">
-        <div class="mw-ui-col-container">
-            <label class="mw-ui-label bold p-b-10">Company Country:</label>
+        <div class="form-group">
+            <label class="control-label">Company Name: </label>
+            <input type="text" class="mw_option_field form-control" data-option-group="shop" name="invoice_company_name" placeholder="" value="">
+        </div>
 
-            <select name="invoice_company_country" class="mw-ui-field mw_option_field w100 silver-field" data-option-group="shop">
+        <div class="form-group">
+            <label class="control-label">Company Country:</label>
+
+            <select name="invoice_company_country" class="mw_option_field selectpicker" data-size="5" data-width="100%" data-option-group="shop">
                 <?php if (countries_list()): ?>
                     <?php foreach (countries_list() as $country): ?>
                         <option value="<?php print $country; ?>" selected="selected"><?php print $country; ?></option>
@@ -38,37 +52,37 @@
                 <?php endif; ?>
             </select>
         </div>
-    </div>
-</div>
 
-<div class="mw-ui-row">
-    <div class="m-b-10">
-        <label class="mw-ui-label bold p-b-10">Company City: </label>
-        <input type="text" class="mw-ui-field mw_option_field block-field" data-option-group="shop" name="invoice_company_city" placeholder="" value="">
-    </div>
+        <div>
+            <div class="form-group">
+                <label class="control-label">Company City: </label>
+                <input type="text" class="mw_option_field form-control" data-option-group="shop" name="invoice_company_city" placeholder="" value="">
+            </div>
 
-    <div class="m-b-10">
-        <label class="mw-ui-label bold p-b-10">Company Address: </label>
-        <input type="text" class="mw-ui-field mw_option_field block-field" data-option-group="shop" name="invoice_company_address" placeholder="" value="">
-    </div>
+            <div class="form-group">
+                <label class="control-label">Company Address: </label>
+                <input type="text" class="mw_option_field form-control" data-option-group="shop" name="invoice_company_address" placeholder="" value="">
+            </div>
 
-    <div class="m-b-10">
-        <label class="mw-ui-label bold p-b-10">Company VAT Number: </label>
-        <input type="text" class="mw-ui-field mw_option_field block-field" data-option-group="shop" name="invoice_company_vat_number" placeholder="" value="">
-    </div>
+            <div class="form-group">
+                <label class="control-label">Company VAT Number: </label>
+                <input type="text" class="mw_option_field form-control" data-option-group="shop" name="invoice_company_vat_number" placeholder="" value="">
+            </div>
 
-    <div class="m-b-10">
-        <label class="mw-ui-label bold p-b-10">ID Company Number: </label>
-        <input type="text" class="mw-ui-field mw_option_field block-field" data-option-group="shop" name="invoice_id_company_number" placeholder="" value="">
-    </div>
+            <div class="form-group">
+                <label class="control-label">ID Company Number: </label>
+                <input type="text" class="mw_option_field form-control" data-option-group="shop" name="invoice_id_company_number" placeholder="" value="">
+            </div>
 
-    <div class="m-b-10">
-        <label class="mw-ui-label bold p-b-10">Additional information: </label>
-        <textarea class="mw-ui-field mw_option_field block-field" data-option-group="shop" name="invoice_company_bank_details" placeholder="For example: reason for taxes"></textarea>
-    </div>
+            <div class="form-group">
+                <label class="control-label">Additional information: </label>
+                <textarea class="mw_option_field form-control" data-option-group="shop" name="invoice_company_bank_details" placeholder="For example: reason for taxes"></textarea>
+            </div>
 
-    <div class="m-b-10">
-        <label class="mw-ui-label bold p-b-10">Bank transfer details: </label>
-        <textarea class="mw-ui-field mw_option_field block-field" data-option-group="shop" name="invoice_company_bank_details" placeholder="Enter your bank details here"></textarea>
+            <div class="form-group">
+                <label class="control-label">Bank transfer details: </label>
+                <textarea class="mw_option_field form-control" data-option-group="shop" name="invoice_company_bank_details" placeholder="Enter your bank details here"></textarea>
+            </div>
+        </div>
     </div>
 </div>
--- userfiles/modules/shop/shipping/set_units.php
+++ userfiles/modules/shop/shipping/set_units.php
@@ -1,18 +1,13 @@
 <script type="text/javascript">
     mw.require('forms.js');
     mw.require('options.js');
-
 </script>
 <script type="text/javascript">
     $(document).ready(function () {
-
         mw.options.form('#shipping-units-setup', function () {
             mw.notification.success("<?php _ejs("Shipping units are saved!"); ?>");
         });
-    });
-</script>
-<script type="text/javascript">
-    $(document).ready(function () {
+
         if (self !== parent && typeof _binded === 'undefined') {
             _binded = true;
             $(mwd.body).ajaxStop(function () {
@@ -24,51 +19,38 @@
     });
 </script>
 
+<div id="shipping-units-setup">
+    <h5 class="font-weight-bold"><?php print _e('Shipping units'); ?></h5>
 
-<div id="shipping-units-setup-accordion" class="mw-ui-box mw-ui-box-silver-blue == m-t-20">
-    <div class="mw-ui-box-header" onclick="mw.accordion('#shipping-units-setup-accordion');">
-        <div class="header-holder">
-            <i class="mai-shipping"></i> <?php print _e('Shipping units'); ?>
+    <div class="form-group">
+        <label class="control-label d-block"><?php _e("Units for weight"); ?></label>
+
+        <div class="custom-control custom-radio d-inline-block mr-2">
+            <input name="shipping_weight_units" class="mw_option_field custom-control-input" id="shipping_weight_units_1" data-option-group="orders" value="kg" type="radio" <?php if (get_option('shipping_weight_units', 'orders') == 'kg'): ?> checked="checked" <?php endif; ?> >
+
+            <label class="custom-control-label" for="shipping_weight_units_1"><?php _e("kilograms (kg)"); ?></label>
+        </div>
+
+        <div class="custom-control custom-radio d-inline-block mr-2">
+            <input name="shipping_weight_units" class="mw_option_field custom-control-input" id="shipping_weight_units_2" data-option-group="orders" value="lb" type="radio" <?php if (get_option('shipping_weight_units', 'orders') == 'lb'): ?> checked="checked" <?php endif; ?> >
+
+            <label class="custom-control-label" for="shipping_weight_units_2"><?php _e("pound (lb)"); ?></label>
         </div>
     </div>
 
-    <div class="mw-accordion-content mw-ui-box-content" style="display: none;">
-        <div class="mw-ui-row">
-            <div class="mw-ui-col">
-                <div class="m-b-10">
-                    <label class="mw-ui-label bold p-b-10"><?php _e("Units for weight"); ?></label>
 
-                    <div class="mw-ui-check-selector">
-                        <label class="mw-ui-check">
-                            <input name="shipping_weight_units" class="mw_option_field" data-option-group="orders" value="kg" type="radio" <?php if (get_option('shipping_weight_units', 'orders') == 'kg'): ?> checked="checked" <?php endif; ?> >
-                            <span></span><span><?php _e("kilograms (kg)"); ?></span>
-                        </label>
-                        <label class="mw-ui-check">
-                            <input name="shipping_weight_units" class="mw_option_field" data-option-group="orders" value="lb" type="radio" <?php if (get_option('shipping_weight_units', 'orders') == 'lb'): ?> checked="checked" <?php endif; ?> >
-                            <span></span><span><?php _e("pound (lb)"); ?></span>
-                        </label>
-                    </div>
-                </div>
-            </div>
+    <div class="form-group">
+        <label class="control-label d-block"><?php _e("Units for size"); ?></label>
 
-            <div class="mw-ui-col">
-                <div class="m-b-10">
-                    <label class="mw-ui-label bold p-b-10"><?php _e("Units for size"); ?></label>
+        <div class="custom-control custom-radio d-inline-block mr-2">
+            <input name="shipping_size_units" class="mw_option_field custom-control-input" id="shipping_size_units_1" data-option-group="orders" value="cm" type="radio" <?php if (get_option('shipping_size_units', 'orders') == 'cm'): ?> checked="checked" <?php endif; ?> >
+            <label class="custom-control-label" for="shipping_size_units_1"><?php _e("centimeters (cm)"); ?></label>
+        </div>
 
-                    <div class="mw-ui-check-selector">
-                        <label class="mw-ui-check">
-                            <input name="shipping_size_units" class="mw_option_field" data-option-group="orders" value="cm" type="radio" <?php if (get_option('shipping_size_units', 'orders') == 'cm'): ?> checked="checked" <?php endif; ?> >
-                            <span></span><span><?php _e("centimeters (cm)"); ?></span>
-                        </label>
-                        <label class="mw-ui-check">
-                            <input name="shipping_size_units" class="mw_option_field" data-option-group="orders" value="in" type="radio" <?php if (get_option('shipping_size_units', 'orders') == 'in'): ?> checked="checked" <?php endif; ?> >
-                            <span></span><span><?php _e("inches (in)"); ?></span>
-                        </label>
-                    </div>
-                </div>
-            </div>
+        <div class="custom-control custom-radio d-inline-block mr-2">
+            <input name="shipping_size_units" class="mw_option_field custom-control-input" id="shipping_size_units_2" data-option-group="orders" value="in" type="radio" <?php if (get_option('shipping_size_units', 'orders') == 'in'): ?> checked="checked" <?php endif; ?> >
+
+            <label class="custom-control-label" for="shipping_size_units_2"><?php _e("inches (in)"); ?></label>
         </div>
     </div>
 </div>
-
-
