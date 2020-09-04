a8abb369dfcb3fb43464ecce20d614cc2e6dd6ce
petko6 <petko@papakaya.com>
2020-09-04 18:32:34 +0300

    update

--- config/app.php
+++ config/app.php
@@ -112,8 +112,28 @@ return [
         /*
                 * Laravel Framework Service Providers...
 //                */
-        Illuminate\Session\SessionServiceProvider::class,
+        Illuminate\Auth\AuthServiceProvider::class,
+        Illuminate\Broadcasting\BroadcastServiceProvider::class,
+        Illuminate\Bus\BusServiceProvider::class,
+        Illuminate\Cache\CacheServiceProvider::class,
+        Illuminate\Foundation\Providers\ConsoleSupportServiceProvider::class,
+        Illuminate\Cookie\CookieServiceProvider::class,
+        Illuminate\Database\DatabaseServiceProvider::class,
+        Illuminate\Encryption\EncryptionServiceProvider::class,
         Illuminate\Filesystem\FilesystemServiceProvider::class,
+        Illuminate\Foundation\Providers\FoundationServiceProvider::class,
+        Illuminate\Hashing\HashServiceProvider::class,
+        Illuminate\Mail\MailServiceProvider::class,
+        Illuminate\Notifications\NotificationServiceProvider::class,
+        Illuminate\Pagination\PaginationServiceProvider::class,
+        Illuminate\Pipeline\PipelineServiceProvider::class,
+        Illuminate\Queue\QueueServiceProvider::class,
+        Illuminate\Redis\RedisServiceProvider::class,
+        Illuminate\Auth\Passwords\PasswordResetServiceProvider::class,
+        Illuminate\Session\SessionServiceProvider::class,
+        Illuminate\Translation\TranslationServiceProvider::class,
+        Illuminate\Validation\ValidationServiceProvider::class,
+        Illuminate\View\ViewServiceProvider::class,
 
         /*
          * Application Service Providers...
--- src/MicroweberPackages/App/Providers/AppServiceProvider.php
+++ src/MicroweberPackages/App/Providers/AppServiceProvider.php
@@ -69,6 +69,8 @@ class AppServiceProvider extends ServiceProvider {
         /*
           * Laravel Framework Service Providers...
           */
+      //  \Illuminate\Session\SessionServiceProvider::class,
+      //  \Illuminate\Filesystem\FilesystemServiceProvider::class,
         \Illuminate\Auth\AuthServiceProvider::class,
         \Illuminate\Broadcasting\BroadcastServiceProvider::class,
         \Illuminate\Bus\BusServiceProvider::class,
@@ -77,7 +79,6 @@ class AppServiceProvider extends ServiceProvider {
         \Illuminate\Cookie\CookieServiceProvider::class,
         \Illuminate\Database\DatabaseServiceProvider::class,
         \Illuminate\Encryption\EncryptionServiceProvider::class,
-        \Illuminate\Filesystem\FilesystemServiceProvider::class,
         \Illuminate\Foundation\Providers\FoundationServiceProvider::class,
         \Illuminate\Hashing\HashServiceProvider::class,
         \Illuminate\Mail\MailServiceProvider::class,
@@ -87,7 +88,6 @@ class AppServiceProvider extends ServiceProvider {
         \Illuminate\Queue\QueueServiceProvider::class,
         \Illuminate\Redis\RedisServiceProvider::class,
         \Illuminate\Auth\Passwords\PasswordResetServiceProvider::class,
-        \Illuminate\Session\SessionServiceProvider::class,
         \Illuminate\Translation\TranslationServiceProvider::class,
         \Illuminate\Validation\ValidationServiceProvider::class,
         \Illuminate\View\ViewServiceProvider::class
@@ -120,7 +120,7 @@ class AppServiceProvider extends ServiceProvider {
         'DB' => \Illuminate\Support\Facades\DB::class,
         'Eloquent' => \Illuminate\Database\Eloquent\Model::class,
         'Event' => \Illuminate\Support\Facades\Event::class,
-        'File' => \Illuminate\Support\Facades\File::class,
+      //  'File' => \Illuminate\Support\Facades\File::class,
         'Gate' => \Illuminate\Support\Facades\Gate::class,
         'Hash' => \Illuminate\Support\Facades\Hash::class,
         'Http' => \Illuminate\Support\Facades\Http::class,
@@ -136,7 +136,7 @@ class AppServiceProvider extends ServiceProvider {
         'Response' => \Illuminate\Support\Facades\Response::class,
         'Route' => \Illuminate\Support\Facades\Route::class,
         'Schema' => \Illuminate\Support\Facades\Schema::class,
-        'Session' => \Illuminate\Support\Facades\Session::class,
+      //  'Session' => \Illuminate\Support\Facades\Session::class,
         'Storage' => \Illuminate\Support\Facades\Storage::class,
         'Str' => \Illuminate\Support\Str::class,
         'URL' => \Illuminate\Support\Facades\URL::class,
@@ -222,7 +222,6 @@ class AppServiceProvider extends ServiceProvider {
 
     protected function registerLaravelProviders()
     {
-
         $this->app->singleton('mw_migrator', function ($app) {
             $repository = $app['migration.repository'];
             return new MicroweberMigrator($repository, $app['db'], $app['files'], $app['events'], $app);
