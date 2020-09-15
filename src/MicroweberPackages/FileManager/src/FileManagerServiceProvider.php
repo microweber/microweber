9006e51802b74baf692a65bfae45d394fa96382d
b.slaveykov <b.slaveykov@radioactive.bg>
2020-09-15 15:00:03 +0300

    update

---
+++ src/MicroweberPackages/FileManager/src/FileManagerServiceProvider.php
@@ -0,0 +1,27 @@
+<?php
+/*
+ * This file is part of the Microweber framework.
+ *
+ * (c) Microweber CMS LTD
+ *
+ * For full license information see
+ * https://github.com/microweber/microweber/blob/master/LICENSE
+ *
+ */
+
+namespace MicroweberPackages\FileManager;
+
+use Illuminate\Support\ServiceProvider;
+
+class FileManagerServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+
+    }
+}
