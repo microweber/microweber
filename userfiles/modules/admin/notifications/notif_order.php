4222a35cf7aa9a1e29cc6c2e9b5e788b19f5b3d0
Petko Yovchevski <petko@microweber.com>
2020-09-23 17:07:41 +0300

    updates

--- userfiles/modules/admin/notifications/notif_order.php
+++ userfiles/modules/admin/notifications/notif_order.php
@@ -26,7 +26,7 @@ if (isset($item['created_by'])) {
 }
 ?>
 
-<div class="card mb-3 not-collapsed-border collapsed <?php if (!isset($is_order)): ?>card-bubble<?php endif; ?> card-order-holder bg-silver" data-toggle="collapse" data-target="#notif-order-item-<?php print $item_id; ?>" aria-expanded="false" aria-controls="collapseExample">
+<div class="card mb-3 not-collapsed-border collapsed <?php if (!isset($is_order)): ?>card-bubble<?php endif; ?> card-order-holder <?php if ($item['is_read'] == 0): ?>active<?php endif; ?> bg-silver" data-toggle="collapse" data-target="#notif-order-item-<?php print $item_id; ?>" aria-expanded="false" aria-controls="collapseExample">
     <div class="card-body py-2">
         <div class="row">
             <div class="col-12 col-md-6">
