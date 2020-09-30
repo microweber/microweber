178b2c63cce151bdf69a4679a284e961490e0c69
Petko Yovchevski <petko@microweber.com>
2020-09-30 15:49:33 +0300

    update

--- userfiles/modules/admin/footer.php
+++ userfiles/modules/admin/footer.php
@@ -3,16 +3,6 @@
         </div>
         <!--  /#mw-admin-main-block -->
 
-        <style>
-            .theme-color-picker-iframe {
-                width: 300px;
-                height: 400px;
-                position: fixed;
-                right: 0;
-                bottom: 0;
-                border: 1px solid silver;
-            }
-        </style>
-        <iframe src="<?php echo modules_url() . 'microweber/api/libs/mw-ui/picker.php?full_width'; ?>" class="theme-color-picker-iframe"></iframe>
+        <module type="admin_colors" />
     </body>
 </html>
