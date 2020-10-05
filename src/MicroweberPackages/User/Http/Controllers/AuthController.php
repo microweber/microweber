18dcf627fe4aa8c9861d8c37b4752bc0e8ac5e72
bobimicroweber <bobi@microweber.com>
2020-10-05 14:37:45 +0300

    update

--- src/MicroweberPackages/User/Http/Controllers/AuthController.php
+++ src/MicroweberPackages/User/Http/Controllers/AuthController.php
@@ -2,6 +2,7 @@
 
 namespace MicroweberPackages\User\Http\Controllers;
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Request;
 
 class AuthController
@@ -13,9 +14,14 @@ class AuthController
      */
     public function index(Request $request)
     {
+
+        if (Auth::check() && Auth::user()->is_admin == 1) {
+            return redirect(admin_url());
+        }
+
         $parsed = view('user::admin.auth.index');
 
-        return app()->parser->process($parsed);;
+        return app()->parser->process($parsed);
     }
 
 }
