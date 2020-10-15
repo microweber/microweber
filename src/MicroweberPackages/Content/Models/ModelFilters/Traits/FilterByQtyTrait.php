1399d0f0e7eae73211a99abc799764a800c21077
bobimicroweber <bobi@microweber.com>
2020-10-15 17:04:55 +0300

    update

---
+++ src/MicroweberPackages/Content/Models/ModelFilters/Traits/FilterByQtyTrait.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Bojidar
+ * Date: 10/15/2020
+ * Time: 3:42 PM
+ */
+
+namespace MicroweberPackages\Content\Models\ModelFilters\Traits;
+use Illuminate\Database\Eloquent\Builder;
+
+trait FilterByQtyTrait {
+
+
+    public function qty($qty)
+    {
+        return $this->whereHas('contentData', function (Builder $query) use ($qty) {
+            $query->where('field_name', '=', 'qty');
+            $query->where('field_value', '=', $qty);
+        });
+    }
+
+}
--- src/MicroweberPackages/Product/Models/ModelFilters/ProductFilter.php
+++ src/MicroweberPackages/Product/Models/ModelFilters/ProductFilter.php
@@ -10,24 +10,16 @@ namespace MicroweberPackages\Product\Models\ModelFilters;
 
 use EloquentFilter\ModelFilter;
 use Illuminate\Database\Eloquent\Builder;
+use MicroweberPackages\Content\Models\ModelFilters\Traits\FilterByPriceTrait;
+use MicroweberPackages\Content\Models\ModelFilters\Traits\FilterByTitleTrait;
+use MicroweberPackages\Content\Models\ModelFilters\Traits\FilterByUrlTrait;
+use MicroweberPackages\Content\Models\ModelFilters\Traits\FilterByQtyTrait;
 
 class ProductFilter extends ModelFilter
 {
-    public function url($url)
-    {
-        return $this->where('url', $url);
-    }
+    use FilterByTitleTrait;
+    use FilterByQtyTrait;
+    use FilterByUrlTrait;
+    use FilterByPriceTrait;
 
-    public function title($title)
-    {
-        return $this->where('title', 'LIKE', "%$title%");
-    }
-
-    public function qty($qty)
-    {
-       return $this->whereHas('contentData', function (Builder $query) use ($qty) {
-            $query->where('field_name', '=', 'qty');
-            $query->where('field_value', '=', $qty);
-        });
-    }
 }
