7810d288531093b478519473fbcd5a4486bc8bee
Peter Ivanov <peter@microweber.com>
2020-10-16 14:26:23 +0300

    update

--- src/MicroweberPackages/User/migrations/2014_10_12_000001_update_users_table.php
+++ src/MicroweberPackages/User/migrations/2014_10_12_000001_update_users_table.php
@@ -17,19 +17,25 @@ class UpdateUsersTable extends Migration
             $table->string('name')->nullable();
             $table->string('phone')->nullable();
 
-            $table->string('facebook_id')->nullable();
-            $table->string('google_id')->nullable();
-            $table->string('github_id')->nullable();
-            $table->string('contact_name')->nullable();
-            $table->string('company_name')->nullable();
 
-            $table->boolean('enable_portal')->nullable();
+            $table->string('username')->unique();
+            $table->string('email')->unique();
 
-            $table->integer('currency_id')->unsigned()->nullable();
-            $table->foreign('currency_id')->references('id')->on('currencies')->onDelete('cascade');
 
-            $table->integer('company_id')->unsigned()->nullable();
-            $table->foreign('company_id')->references('id')->on('companies')->onDelete('cascade');
+
+//            $table->string('facebook_id')->nullable();
+//            $table->string('google_id')->nullable();
+//            $table->string('github_id')->nullable();
+       //     $table->string('contact_name')->nullable();
+        //    $table->string('company_name')->nullable();
+
+   //         $table->boolean('enable_portal')->nullable();
+
+       //     $table->integer('currency_id')->unsigned()->nullable();
+       //     $table->foreign('currency_id')->references('id')->on('currencies')->onDelete('cascade');
+
+          //  $table->integer('company_id')->unsigned()->nullable();
+          //  $table->foreign('company_id')->references('id')->on('companies')->onDelete('cascade');
 
         });
     }
