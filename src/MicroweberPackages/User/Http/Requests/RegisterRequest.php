1244098af7900a65b0b8365371ce41ae2cd008e4
bobimicroweber <bobi@microweber.com>
2020-10-20 14:23:08 +0300

    update

--- src/MicroweberPackages/User/Http/Requests/RegisterRequest.php
+++ src/MicroweberPackages/User/Http/Requests/RegisterRequest.php
@@ -5,6 +5,8 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class RegisterRequest extends FormRequest
 {
+    private $_registerRules = [];
+
     public function authorize()
     {
         return true;
@@ -17,15 +19,31 @@ class RegisterRequest extends FormRequest
      */
     public function rules()
     {
-        $rules = [
-           // 'terms' => 'terms:terms_user,terms_newsletter',
-           // 'name' => 'max:255',
-          //  'email' => 'string|max:255|unique:users',
-            'username' => 'string',
-            'password' => 'required|min:1',
-            'confirm_password' => 'required|min:1|same:password',
-        ];
+        $this->_registerRules['password'] = 'required|min:1' ;
+        $this->_registerRules['confirm_password'] = 'required|min:1|same:password' ;
 
-        return $rules;
+        return $this->_registerRules;
+    }
+
+    public function validationData()
+    {
+        $inputs = $this->query->all();
+
+        if (!isset($inputs['username']) || !isset($inputs['email'])) {
+            $inputs['username'] = '';
+        }
+
+        if (isset($inputs['email'])) {
+            $this->_registerRules['email'] = 'required|string|max:255|unique:users';
+        }
+
+        if (isset($inputs['username'])) {
+            $this->_registerRules['username'] = 'required|string|max:255|unique:users';
+        }
+
+        // 'terms' => 'terms:terms_user,terms_newsletter',
+        /// 'email' => 'string|max:255|unique:users',
+
+        return parent::validationData();
     }
 }
