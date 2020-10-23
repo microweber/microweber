5e7e4236c0c7eb1e22a7c70d32c4ab7f0f95e40f
bobimicroweber <bobi@microweber.com>
2020-10-23 13:58:10 +0300

    update

--- config/auth.php
+++ config/auth.php
@@ -61,7 +61,7 @@ return [
     'providers' => [
         'users' => [
             'driver' => 'eloquent',
-            'model' => \MicroweberPackages\User\User::class,
+            'model' => \MicroweberPackages\User\Models\User::class,
         ],
         // 'users' => [
         //     'driver' => 'database',
