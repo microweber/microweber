98de25f109570c73abfb4f7d22c5ae693e5ef1a1
bobimicroweber <bobi@microweber.com>
2020-10-23 10:52:40 +0300

    update

--- src/MicroweberPackages/User/Http/Controllers/UserRegisterController.php
+++ src/MicroweberPackages/User/Http/Controllers/UserRegisterController.php
@@ -2,16 +2,11 @@
 
 namespace MicroweberPackages\User\Http\Controllers;
 
-use _HumbugBox58fd4d9e2a25\Exception;
 use Illuminate\Http\Request;
-use Illuminate\Http\Response;
 use Illuminate\Routing\Controller;
-use Illuminate\Support\Facades\Auth;
 use MicroweberPackages\User\Events\UserWasRegistered;
-use MicroweberPackages\User\Http\Requests\LoginRequest;
 use MicroweberPackages\User\Http\Requests\RegisterRequest;
 use MicroweberPackages\User\User;
-use MicroweberPackages\Utils\ThirdPartyLibs\DisposableEmailChecker;
 
 class UserRegisterController extends Controller
 {
@@ -22,8 +17,21 @@ class UserRegisterController extends Controller
         ]
     ];
 
-    public $fillable = ['username', 'password', 'email', 'basic_mode', 'first_name', 'last_name', 'thumbnail',
-        'parent_id', 'user_information', 'subscr_id', 'profile_url', 'website_url', 'phone'];
+    public $fillable = [
+        'username',
+        'password',
+        'email',
+        'basic_mode',
+        'first_name',
+        'last_name',
+        'thumbnail',
+        'parent_id',
+        'user_information',
+        'subscr_id',
+        'profile_url',
+        'website_url',
+        'phone'
+    ];
 
 
     /**
