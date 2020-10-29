fd208b76bc7c272b36a2c2e9430531138cd080b8
petko6 <petko@papakaya.com>
2020-10-29 17:56:50 +0200

    Skip reserved on queue processing

--- src/MicroweberPackages/Queue/Http/Controllers/ProcessQueueController.php
+++ src/MicroweberPackages/Queue/Http/Controllers/ProcessQueueController.php
@@ -10,23 +10,38 @@ class ProcessQueueController extends Controller
 {
     public function handle()
     {
-        $allQueue = Jobs::all();
+        $limit = 10;
+        $allQueue = Jobs::whereNull('reserved')->where(function($query)
+        {
+            $query->where('attempts', '<', 10);
+            $query->orWhereNull('attempts');
+        })->limit($limit)->get();
 
         if ($allQueue) {
-            $i = 1;
+            //>>> Make reserved
+            foreach($allQueue as $queue) {
+                $queue->reserved = true;
+                $queue->save();
+            }
+            //<<< Make reserved
+
             foreach ($allQueue as $queueItem) {
-                if($i >= 10) {
-                    break;
-                }
                 $payload = $queueItem->payload;
-                $queueItem->delete();
-                $i++;
                 if ($payload) {
                     $payload = @json_decode($payload, true);
                     $command = @unserialize($payload['data']['command']);
 
                     if (is_object($command)) {
-                        dispatch_now($command);
+                        try {
+                            dispatch_now($command);
+                            $queueItem->delete();
+                        } catch (\Exception $e) {
+                            $queueItem->reserved = null;
+                            $queueItem->attempts = (int)$queueItem->attempts + 1;
+
+                            $queueItem->save();
+                            \Log::error($e);
+                        }
                     }
                 }
             }
