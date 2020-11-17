4f91c8d82c043d9e21a584d91ed83961c33e81db
bobimicroweber <bobi@microweber.com>
2020-11-17 12:50:43 +0200

    update

--- userfiles/modules/content/views/product/inventory.php
+++ userfiles/modules/content/views/product/inventory.php
@@ -24,28 +24,28 @@
             <div class="col-md-6">
                 <div class="form-group mb-3">
                     <label>SKU (Stock Keeping Unit)</label>
-                    <input type="text" name="sku" class="form-control" value="<?php echo $contentData['sku']; ?>">
+                    <input type="text" name="content_data[sku]" class="form-control" value="<?php echo $contentData['sku']; ?>">
                 </div>
             </div>
 
             <div class="col-md-6">
                 <div class="form-group">
                     <label>Barcode (ISBN, UPC, GTIN, etc.)</label>
-                    <input type="text" name="barcode" class="form-control" value="<?php echo $contentData['barcode']; ?>">
+                    <input type="text" name="content_data[barcode]" class="form-control" value="<?php echo $contentData['barcode']; ?>">
                 </div>
             </div>
 
             <div class="col-md-12">
                 <div class="form-group">
                     <div class="custom-control custom-checkbox">
-                        <input type="checkbox" name="track_quantity" class="custom-control-input js-track-quantity-check" value="1" <?php if ($contentData['track_quantity']==1):?>checked="checked"<?php endif; ?> id="customCheck2">
+                        <input type="checkbox" name="content_data[track_quantity]" class="custom-control-input js-track-quantity-check" value="1" <?php if ($contentData['track_quantity']==1):?>checked="checked"<?php endif; ?> id="customCheck2">
                         <label class="custom-control-label" for="customCheck2">Track quantity</label>
                     </div>
                 </div>
 
                 <div class="form-group">
                     <div class="custom-control custom-checkbox">
-                        <input type="checkbox" class="custom-control-input" id="customCheck3" name="sell_oos" value="1" <?php if ($contentData['sell_oos']==1):?>checked="checked"<?php endif; ?>>
+                        <input type="checkbox" class="custom-control-input" id="customCheck3" name="content_data[sell_oos]" value="1" <?php if ($contentData['sell_oos']==1):?>checked="checked"<?php endif; ?>>
                         <label class="custom-control-label" for="customCheck3">Continue selling when out of stock</label>
                     </div>
                 </div>
@@ -63,7 +63,7 @@
                     <div class="form-group">
                         <label>Available</label>
                         <div class="input-group mb-1 append-transparent input-group-quantity">
-                            <input type="text" class="form-control" name="quantity" value="<?php echo $contentData['quantity']; ?>" />
+                            <input type="text" class="form-control" name="content_data[quantity]" value="<?php echo $contentData['quantity']; ?>" />
                             <div class="input-group-append">
                                 <div class="input-group-text plus-minus-holder">
                                     <button type="button" class="plus"><i class="mdi mdi-menu-up"></i></button>
@@ -80,7 +80,7 @@
                     <div class="form-group">
                         <label>Max quantity per order</label>
                         <div class="input-group mb-1 append-transparent input-group-quantity">
-                            <input type="text" class="form-control" name="max_quantity_per_order" value="<?php echo $contentData['max_quantity_per_order']; ?>" placeholder="No limit" />
+                            <input type="text" class="form-control" name="content_data[max_quantity_per_order]" value="<?php echo $contentData['max_quantity_per_order']; ?>" placeholder="No limit" />
                             <div class="input-group-append">
                                 <div class="input-group-text plus-minus-holder">
                                     <button type="button" class="plus"><i class="mdi mdi-menu-up"></i></button>
