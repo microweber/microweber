efa98401bc71ea68cd6dcc721e4e0b1b02741392
petko6 <petko@papakaya.com>
2020-11-17 16:56:45 +0200

    Add check when MW is not installed

---
+++ src/MicroweberPackages/App/Http/Middleware/AuthenticateSessionForUser.php
@@ -0,0 +1,29 @@
+<?php
+namespace MicroweberPackages\App\Http\Middleware;
+
+use Illuminate\Session\Middleware\AuthenticateSession;
+use phpDocumentor\Reflection\Types\Parent_;
+
+
+class AuthenticateSessionForUser extends AuthenticateSession
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, \Closure $next)
+    {
+         if (!mw_is_installed()) {
+            return $next($request);
+        }
+
+
+        return parent::handle($request,$next);
+
+    }
+}
+
+
