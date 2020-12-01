0eeb60254c55fcd667503519cefb80fdc50d086f
Peter Ivanov <peter@microweber.com>
2020-12-01 15:18:35 +0200

    update

--- userfiles/modules/admin_colors/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$config = array();
-$config['name'] = "Color Schemes";
-$config['ui'] = false;
-$config['ui_admin'] = true;
-$config['categories'] = "other";
-$config['position'] = "999";
-$config['version'] = 0.1;
