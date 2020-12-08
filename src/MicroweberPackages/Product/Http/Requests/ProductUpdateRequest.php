c2850515a5f554089ff78d5cb3f2eef94f984d19
petko6 <petko@papakaya.com>
2020-12-08 10:26:43 +0200

    Add price request validation

--- src/MicroweberPackages/Product/Http/Requests/ProductUpdateRequest.php
+++ src/MicroweberPackages/Product/Http/Requests/ProductUpdateRequest.php
@@ -16,8 +16,21 @@ class ProductUpdateRequest extends FormRequest
         $rules = [
 
              //'title' => 'required',
+             'price' => 'sometimes|regex:/^\d+(\.\d{1,2})?$/', //The regex will hold for quantities like '12' or '12.5' or '12.05' '. If you want more decimal points than two, replace the "2" with the allowed decimals you need.
         ];
 
         return $rules;
+    }
+
+    /**
+     * Get the error messages for the defined validation rules.
+     *
+     * @return array
+     */
+    public function messages()
+    {
+        return [
+            'price.regex' => _e("The price must be in format 12 or 12.3 or 12.03", true)
+        ];
     }
 }
