9d30ea69ebfea02ae867d051cc25cb541fe730a6
Peter Ivanov <peter@microweber.com>
2020-12-14 15:30:48 +0200

    update

--- src/MicroweberPackages/App/Console/Kernel.php
+++ src/MicroweberPackages/App/Console/Kernel.php
@@ -36,6 +36,6 @@ class Kernel extends ConsoleKernel
     {
         $this->load(__DIR__.'/Commands');
 
-        require base_path('routes/console.php');
+      //  require base_path('routes/console.php');
     }
 }
