054a3cd38945c28f89cbc6dedf1480ab996f4b6f
Peter Ivanov <peter@microweber.com>
2021-01-07 13:50:33 +0200

    update

--- userfiles/modules/settings/group/template.php
+++ userfiles/modules/settings/group/template.php
@@ -30,7 +30,7 @@
 
     <script type="text/javascript">
         function mw_set_default_template() {
-            var el1 = mw.$('.mw-site-theme-selector').find("[name='<?php print  $data['option_key']; ?>']")[0];
+            var el1 = mw.$('.mw-site-theme-selector').find("[name='<?php print  $data['option_key']; ?>'] option:selected")[0];
             mw.options.save(el1, function () {
                 mw.notification.success("<?php _ejs("Template settings are saved"); ?>.");
             });
@@ -38,7 +38,7 @@
 
         $(document).ready(function () {
             $(window).on('templateSelected', function () {
-                $(".mw-site-theme-selector").find("[name='active_site_template']").each(function (index) {
+                $(".mw-site-theme-selector").find("[name='active_site_template'] option:selected").each(function (index) {
                     $("#mw_curr_theme_val").val($(this).val());
                 });
             });
@@ -46,7 +46,7 @@
     </script>
 
     <div class="mw-site-theme-selector">
-        <input id="mw_curr_theme_val" name="current_template" class="mw_option_field mw-ui-field" type="hidden" option-group="template" value="<?php print  $data['option_value']; ?>" data-id="<?php print  $data['id']; ?>"/>
+        <input id="mw_curr_theme_val" name="current_template" class="mw_option_field mw-ui-field" type="text" option-group="template" value="<?php print  $data['option_value']; ?>" data-id="<?php print  $data['id']; ?>"/>
         <module type="content/views/layout_selector" show_save_changes_buttons="true" show_full="true" data-active-site-template="<?php print $data['option_value'] ?>" autoload="1" xxlive_edit_styles_check="1" no-default-name="true"/>
     </div>
 </div>
