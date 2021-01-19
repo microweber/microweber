a7300343d584d01318c00e90cf623dc3c0d8ed78
Bobi <bobi@cloudvision.bg>
2021-01-19 18:46:05 +0200

    update

--- userfiles/modules/users/profile/address/templates/default.php
+++ userfiles/modules/users/profile/address/templates/default.php
@@ -57,28 +57,6 @@
         </div>
 
         <div class="form-group">
-            <label class="control-label">Company Name:</label>
-            <input type="text" class="form-control" value="<?php echo $billing_address['company_name'];?>" name="addresses[1][company_name]">
-        </div>
-
-        <div class="form-group">
-            <label class="control-label">Company ID:</label>
-            <input type="text" class="form-control" value="<?php echo $billing_address['company_id'];?>" name="addresses[1][company_id]">
-        </div>
-
-        <div class="form-group">
-            <label class="control-label">VAT number:</label>
-            <input type="text" class="form-control" value="<?php echo $billing_address['company_vat'];?>" name="addresses[1][company_vat]">
-        </div>
-
-        <div class="form-group">
-            <div class="custom-control custom-switch">
-                <input type="checkbox" class="custom-control-input" id="company_vat_registered" value="<?php echo $billing_address['company_vat_registered'];?>" name="addresses[1][company_vat_registered]">
-                <label class="custom-control-label" for="company_vat_registered">VAT registered</label>
-            </div>
-        </div>
-
-        <div class="form-group">
             <label class="control-label">Address:</label>
             <textarea class="form-control" placeholder="Street 1" name="addresses[1][address_street_1]"><?php echo $billing_address['address_street_1'];?></textarea>
         </div>
