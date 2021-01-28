fe141decb9d077b2e6f500e470ee3f7401211c4c
Bozhidar Slaveykov <selfworksbg@gmail.com>
2021-01-28 18:07:25 +0200

    add trans laravel work with mw translations

--- src/MicroweberPackages/Translation/TranslationManager.php
+++ src/MicroweberPackages/Translation/TranslationManager.php
@@ -24,5 +24,37 @@ class TranslationManager extends FileLoader
             return $fileTranslations;
         }
 
+        $lines = $this->_loadLanguageFiles($locale, $group, $namespace);
+
+        if (isset($lines[$locale][$group])) {
+            return $lines[$locale][$group];
+        }
+    }
+
+    private function _loadLanguageFiles($locale, $group, $namespace)
+    {
+        $languageFiles = [];
+        $languageFiles[] = userfiles_path() . 'language' . DIRECTORY_SEPARATOR . $locale . '.json';
+
+        if (empty($locale) || $locale == 'en') {
+            $languageFiles[] = mw_includes_path() . 'language' . DIRECTORY_SEPARATOR . 'en.json';
+        } else {
+            $languageFiles[] =  normalize_path(mw_includes_path() . 'language' . DIRECTORY_SEPARATOR . $locale . '.json', false);
+        }
+
+        $variablesWithContent = [];
+        foreach ($languageFiles as $languageFile) {
+            if (is_file($languageFile)) {
+                $languageContent = file_get_contents($languageFile);
+                $languageVariables = json_decode($languageContent, true);
+                if (isset($languageVariables) and is_array($languageVariables)) {
+                    foreach ($languageVariables as $languageVariableKey=> $languageVariableValue) {
+                        $variablesWithContent[$locale][$languageVariableKey] = $languageVariableValue;
+                    }
+                }
+            }
+        }
+
+        return $variablesWithContent;
     }
 }
