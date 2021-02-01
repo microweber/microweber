8971e5db2fc3687e067dd2d5de66f21d5219d109
Bozhidar Slaveykov <bobi@microweber.com>
2021-02-01 16:08:06 +0200

    update

--- src/MicroweberPackages/App/routes/web.php
+++ src/MicroweberPackages/App/routes/web.php
@@ -1,4 +1,107 @@
 <?php
+
+use MicroweberPackages\Translation\Models\Translation;
+
+function loadLanguageByLocale($locale) {
+
+    $translatedLanguageLines = [];
+
+    $languageFiles = [];
+    $languageFiles[] = userfiles_path() . 'language' . DIRECTORY_SEPARATOR . $locale . '.json';
+
+    if (empty($locale) || $locale == 'en') {
+        $languageFiles[] = mw_includes_path() . 'language' . DIRECTORY_SEPARATOR . 'en.json';
+    } else {
+        $languageFiles[] = normalize_path(mw_includes_path() . 'language' . DIRECTORY_SEPARATOR . $locale . '.json', false);
+    }
+
+    foreach ($languageFiles as $languageFile) {
+        if (is_file($languageFile)) {
+            $languageContent = file_get_contents($languageFile);
+            $languageVariables = json_decode($languageContent, true);
+            if (isset($languageVariables) and is_array($languageVariables)) {
+                foreach ($languageVariables as $languageVariableKey => $languageVariableValue) {
+                    $translatedLanguageLines[$languageVariableKey] = $languageVariableValue;
+                }
+            }
+        }
+    }
+
+    return $translatedLanguageLines;
+}
+
+function migrateLanguages()
+{
+    $enTranslations = loadLanguageByLocale('en');
+
+    $locales = [];
+    $files = glob(normalize_path(mw_includes_path() . 'language') . '/*');
+    foreach ($files as $file) {
+        if (strpos($file, 'json') !== false) {
+            $locale = basename($file);
+            $locale = str_replace('.json', '', $locale);
+            $locales[] = $locale;
+        }
+    }
+
+    $saveTranslations = [];
+    foreach ($locales as $locale) {
+
+        $getCurrentLocale = loadLanguageByLocale($locale);
+
+        foreach ($enTranslations as $enTranslationKey => $enTranslationValue) {
+            foreach ($getCurrentLocale as $currentLocaleKey => $currentLocaleValue) {
+                if ($enTranslationKey == $currentLocaleKey) {
+                    $saveTranslations[$enTranslationKey][$locale] = $currentLocaleValue;
+                }
+            }
+        }
+    }
+
+    foreach ($saveTranslations as $translation) {
+
+        $key = $translation['en'];
+
+        foreach ($translation as $locale => $text) {
+            $saveText = [
+                'key' => $key,
+                'text' => $text,
+                'group' => '*',
+                'namespace' => '*',
+                'locale' => $locale,
+            ];
+
+            $findTranslation = Translation::where('namespace', $saveText['namespace'])
+                ->where('group', $saveText['group'])
+                ->where('key', $saveText['key'])
+                ->where('locale', $saveText['locale'])->first();
+            if ($findTranslation == null) {
+                Translation::insert($saveText);
+            }
+        }
+    }
+}
+
+Route::get('migrateLanguages', function () {
+    migrateLanguages();
+
+});
+
+Route::get('test-lang', function () {
+
+    //  _e('Website');
+    // echo '---';
+
+      app()->setLocale('bg');
+   //   _e('Login URL');
+     // app()->setLocale('en');
+   //  _e('Login URL');
+
+    // echo _e('translation::all.fwawafwaf');
+    echo _e('Optional thumbnail. image for use with uploaded. or embedded videos. Required . Lazy Loading. selected.');
+
+});
+
 Route::group(['middleware' => \MicroweberPackages\App\Http\Middleware\SessionlessMiddleware::class, 'namespace' => '\MicroweberPackages\App\Http\Controllers'], function () {
     Route::any('/apijs', 'JsCompileController@apijs');
     Route::any('apijs/{all}', array('as' => 'apijs', 'uses' => 'JsCompileController@apijs'))->where('all', '.*');
