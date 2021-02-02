d0af7e941e5b7a3cf5515308194a0773c0c49e0b
Peter Ivanov <peter@microweber.com>
2021-02-02 17:15:40 +0200

    update

--- userfiles/modules/shop/shipping/templates/default.php
+++ userfiles/modules/shop/shipping/templates/default.php
@@ -14,7 +14,11 @@ description: Default
 
 
 ?>
-
+    <h2 style="margin-top:0 " class="edit nodrop" field="checkout_shipping_information_title" rel="global"
+        rel_id="<?php print $params['id'] ?>">
+        <?php _e("Shipping Information"); ?>
+    </h2>
+    <hr/>
 
 <?php
 
