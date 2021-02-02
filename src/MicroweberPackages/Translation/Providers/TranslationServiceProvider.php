d164ef86f44b29589618931719a36404c45f6394
bobimicroweber <bobi@microweber.com>
2021-02-02 11:05:55 +0200

    update

--- src/MicroweberPackages/Translation/Providers/TranslationServiceProvider.php
+++ src/MicroweberPackages/Translation/Providers/TranslationServiceProvider.php
@@ -43,7 +43,11 @@ class TranslationServiceProvider extends IlluminateTranslationServiceProvider
                             Translation::insert($text);
                         }
                     }
-                    clearcache();
+                    try {
+                        clearcache();
+                    } catch (\Exception $e) {
+                        //
+                    }
                 }
             });
         }
