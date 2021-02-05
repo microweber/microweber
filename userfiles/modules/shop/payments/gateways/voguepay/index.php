e5a63f95da2695b11d64b3fbeb7db094a7dd45da
bobimicroweber <bobi@microweber.com>
2021-02-05 15:36:46 +0200

    update

--- userfiles/modules/shop/payments/gateways/voguepay/index.php
+++ userfiles/modules/shop/payments/gateways/voguepay/index.php
@@ -1,5 +1,10 @@
 <div>
-    <p class="alert alert-info"><small><strong> *<?php _e("Note"); ?> </strong>You will be redirected to the Voguepay.com website to complete your payment.</small> </p>
+    <p class="alert alert-info">
+        <small>
+            <strong> *<?php _e("Note"); ?></strong>
+            <?php _e("You will be redirected to the Voguepay.com website to complete your payment."); ?>
+        </small>
+    </p>
 </div>
 
 
