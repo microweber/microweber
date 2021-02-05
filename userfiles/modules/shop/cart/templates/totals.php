9e4e7846f2eb0357b1290c9d01bd468d1d74618f
Peter Ivanov <peter@microweber.com>
2021-02-05 13:50:28 +0200

    update

--- userfiles/modules/shop/cart/templates/totals.php
+++ userfiles/modules/shop/cart/templates/totals.php
@@ -16,13 +16,13 @@ description:  Show cart totals
        <?php if ($cart_totals): ?>
            <table class="table table-responsive">
                <div class="form-row">
-                   <h5><?php _lang("Total amount", "templates/big"); ?></h5>
+                   <h5><?php _lang("Total amount" ); ?></h5>
 
                    <?php foreach ($cart_totals as $cart_total_key => $cart_total): ?>
                        <?php if ($cart_total_key != 'total' and $cart_total and is_array($cart_total) and !empty($cart_total) and isset($cart_total['value']) and $cart_total['value']): ?>
                            <thead>
                            <tr>
-                               <th class="pl-0"><?php _lang($cart_total['label'], "templates/big"); ?>: <?php print currency_format($cart_total['value']); ?></th>
+                               <th class="pl-0"><?php _lang($cart_total['label']); ?>: <?php print currency_format($cart_total['value']); ?></th>
                            </tr>
                            </thead>
                        <?php endif; ?>
@@ -35,7 +35,7 @@ description:  Show cart totals
        <?php if ($cart_totals): ?>
            <div class="form-row">
                <?php $print_total = cart_total(); ?>
-               <h5 class="col-xs-6 checkout-modal-total-label mr-1 font-weight-bold"><?php _lang("Total", "templates/big"); ?>:</h5>
+               <h5 class="col-xs-6 checkout-modal-total-label mr-1 font-weight-bold"><?php _lang("Total"); ?>:</h5>
                <h5 class="col-xs-6 checkout-modal-total-price pl-0">
                    <?php print currency_format($print_total); ?>
                </h5>
