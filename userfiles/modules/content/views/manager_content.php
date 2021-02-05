ea4e9c8c841800b8e2657f245e7d7f21b6661456
bobimicroweber <bobi@microweber.com>
2021-02-05 15:23:36 +0200

    update

--- userfiles/modules/content/views/manager_content.php
+++ userfiles/modules/content/views/manager_content.php
@@ -109,7 +109,7 @@
                                             <?php
                                             if (user_can_access('module.content.edit')):
                                                 ?>
-                                                <a target="_top" class="btn btn-outline-success btn-sm" href="<?php print $edit_link ?>"> 
+                                                <a target="_top" class="btn btn-outline-success btn-sm" href="<?php print $edit_link ?>">
                                                     <?php echo $edit_text; ?>
                                                 </a>
 
