546e8083c064f0413b6fe8b5bb45bd86f6cd8fbf
bobimicroweber <bobi@microweber.com>
2021-02-12 18:48:54 +0200

    update

--- src/MicroweberPackages/Translation/Http/Controllers/TranslationController.php
+++ src/MicroweberPackages/Translation/Http/Controllers/TranslationController.php
@@ -12,6 +12,7 @@ use Illuminate\Http\Request;
 use MicroweberPackages\Backup\Exporters\JsonExport;
 use MicroweberPackages\Backup\Exporters\XlsxExport;
 use MicroweberPackages\Translation\Models\Translation;
+use MicroweberPackages\Translation\Models\TranslationKey;
 use MicroweberPackages\Translation\TranslationXlsxImport;
 
 class TranslationController {
@@ -35,64 +36,59 @@ class TranslationController {
     public function export(Request $request) {
 
         $namespace = $request->post('namespace','*');
-        $exportLocale = $request->post('locale', mw()->lang_helper->default_lang());
-
+        $locale = $request->post('locale', mw()->lang_helper->default_lang());
         $format = $request->post('format', 'json');
-        
+
+
         $exportFileName = 'translation-global';
+        if ($namespace !== '*') {
+            $exportFileName = 'translation-' . $namespace;
+        }
+        $exportFileName = $exportFileName . '-' . $locale;
 
-        $query = Translation::query();
-        $query->groupBy(\DB::raw("MD5(translation_key)"));
-
-        if (!empty($namespace)) {
-            $query->where('translation_namespace', $namespace);
-            if ($namespace !== '*') {
-                $exportFileName = 'translation-' . $namespace;
-            }
+        $getTranslations = [];
+        $getTranslationsQuery = TranslationKey::
+            join('translation_texts', 'translation_keys.id', '=', 'translation_texts.translation_key_id')
+            ->where('translation_texts.translation_locale', $locale)
+            ->where('translation_namespace', $namespace)
+            ->get();
+        if ($getTranslationsQuery !== null) {
+            $getTranslations = $getTranslationsQuery->toArray();
         }
 
-        if (!empty($exportLocale)) {
-            $exportFileName = $exportFileName . '-' . $exportLocale;
+        $getTranslationsWithoutTexts = TranslationKey::
+            whereNotIn('translation_keys.id', function ($query) use($locale) {
+                $query->select('translation_texts.translation_key_id')->from('translation_texts')->where('translation_texts.translation_locale', $locale);
+            })
+            ->get();
+
+        if ($getTranslationsWithoutTexts !== null) {
+            $getTranslations = array_merge($getTranslations, $getTranslationsWithoutTexts->toArray());
         }
 
-        $query->orderBy('id');
+        $readyTranslations = [];
 
-        $getTranslatable = $query->get(); // Get original english fields
+        foreach ($getTranslations as $translation) {
 
-        $readyExportData = [];
-        foreach ($getTranslatable as $translation) {
-
-            $translationText = $translation->translation_text;
-
-            // Get the current locale lang translatable fields
-            $getTranslationByLocale = Translation::
-                where(\DB::raw('md5(translation_key)'), md5($translation->translation_key))
-                ->where('translation_namespace', $translation->translation_namespace)
-                ->where('translation_group', $translation->translation_group)
-                ->where('translation_locale', $exportLocale)
-                ->first();
-
-            if ($getTranslationByLocale != null) {
-                $translationText = $getTranslationByLocale->translation_text;
+            if (!isset($translation['translation_text'])) {
+                $translation['translation_text'] = '';
+                $translation['translation_locale'] = '';
             }
 
-            $readyTranslate = [];
-            $readyTranslate['id'] = $translation->id;
-            $readyTranslate['translation_namespace'] = $translation->translation_namespace;
-            $readyTranslate['translation_group'] = $translation->translation_group;
-            $readyTranslate['translation_key'] = $translation->translation_key;
-            $readyTranslate['translation_text'] = $translationText;
-            $readyTranslate['translation_locale'] = $exportLocale;
-
-            $readyExportData[] = $readyTranslate;
-
+            $readyTranslations[] = [
+              'translation_group'=>$translation['translation_group'],
+              'translation_namespace'=>$translation['translation_namespace'],
+              'translation_key'=>$translation['translation_key'],
+              'translation_text'=>$translation['translation_text'],
+              'translation_locale'=>$translation['translation_locale'],
+            ];
         }
 
         $exportFileName = $exportFileName . '-' . date('Y-m-d-H-i-s');
 
         if ($format == 'json') {
 
-            $export = new JsonExport($readyExportData);
+            $export = new JsonExport($readyTranslations);
             $export->setFilename($exportFileName);
             $export->useEncodeFix = false;
 
@@ -100,7 +96,7 @@ class TranslationController {
 
         } else {
             $export = new XlsxExport();
-            $export->data[$exportFileName] = $readyExportData;
+            $export->data[$exportFileName] = $readyTranslations;
 
             return $export->start();
         }
