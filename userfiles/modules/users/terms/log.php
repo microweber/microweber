422c871e790db69381a84a4a42b8b441e9a41462
bobimicroweber <bobi@microweber.com>
2021-02-17 17:54:45 +0200

    update

--- userfiles/modules/users/terms/log.php
+++ userfiles/modules/users/terms/log.php
@@ -9,35 +9,22 @@ if ($uid) {
     $terms_params = array();
     $terms_params['user_id'] = $uid;
 
-    $terms = new \Microweber\Providers\Users\TosManager();
+    $terms = new \MicroweberPackages\User\TosManager();
     $terms = $terms->get($terms_params);
-
-
 }
-
-
 ?>
 
 <?php if (!$terms) { ?>
-
-    You don't have any accepted terms and conditions
+    <?php _e('You don\'t have any accepted terms and conditions'); ?>
 <?php } else { ?>
-
     <table class="mw-ui-table mw-ui-table-basic">
         <?php foreach ($terms as $term): ?>
             <tr>
                 <td>
-
-
-
-<strong title="<?php print ($term['tos_name']); ?>"><?php print titlelize($term['tos_name']); ?></strong>
-                    is accepted on <?php print ($term['created_at']); ?> from ip <?php print ($term['user_ip']); ?>
+                <strong title="<?php print ($term['tos_name']); ?>"><?php print titlelize($term['tos_name']); ?></strong>
+                   <?php _e('is accepted on'); ?> <?php print ($term['created_at']); ?>  <?php _e('from ip'); ?> <?php print ($term['user_ip']); ?>
                 </td>
             </tr>
         <?php endforeach; ?>
-
     </table>
-
-
 <?php } ?>
-
