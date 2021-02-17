223535fb0a981185a9c530250dcc80cb68068eb0
Peter Ivanov <peter@microweber.com>
2021-02-17 15:25:52 +0200

    update

--- userfiles/modules/categories/manage.php
+++ userfiles/modules/categories/manage.php
@@ -148,7 +148,26 @@
 
                 mw.quick_cat_edit_create = mw.quick_cat_edit_create || function (id) {
 
-                    mw.url.windowHashParam('action', 'editcategory:' + id)
+
+                    if(self !== top && mw.top().settings.liveEdit){
+
+
+                        var opts = {};
+                        opts.width = '800';
+                        opts.height =  '600';
+
+                        opts.liveedit = true;
+                        opts.mode = 'modal';
+
+                        var additional_params = {};
+                        additional_params.category_id = id;
+                        return mw.tools.open_global_module_settings_modal('categories/edit_category',  '#mw_edit_category_admin_holder_modal', opts,additional_params);
+
+
+                    } else {
+                        mw.url.windowHashParam('action', 'editcategory:' + id)
+
+                    }
 
                 }
 
