7faa695ab6ca268e26c3ad29b7d7e05fc0931296
Nikolay <rachkuw@gmail.com>
2021-02-26 13:08:47 +0200

    tranaslation word

--- userfiles/modules/settings/group/maintenance_mode.php
+++ userfiles/modules/settings/group/maintenance_mode.php
@@ -15,20 +15,20 @@ $maintenance_mode_text = get_option('maintenance_mode_text', 'website');
 
 <div class="form-group">
     <label class="control-label d-block"><?php _e("Maintenance mode"); ?></label>
-    <small class="text-muted d-block mb-2"><?php _e("Turn on 'Under construction mode' of your site"); ?></small>
+    <small class="text-muted d-block mb-2"><?php _e("Turn on Under construction mode of your site"); ?></small>
 
     <div class="custom-control custom-radio d-inline-block mr-2">
         <input name="maintenance_mode" class="mw_option_field custom-control-input" id="maintenance_mode_1"
                data-option-group="website" value="n"
                type="radio" <?php if (get_option('maintenance_mode', 'website') !== "y"): ?> checked="checked" <?php endif; ?> >
-        <label class="custom-control-label" for="maintenance_mode_1"><?php _e("Disabled"); ?></label>
+        <label class="custom-control-label" for="maintenance_mode_1"><?php _e("Disable"); ?></label>
     </div>
 
     <div class="custom-control custom-radio d-inline-block mr-2">
         <input name="maintenance_mode" class="mw_option_field custom-control-input" id="maintenance_mode_0"
                data-option-group="website" value="y"
                type="radio" <?php if (get_option('maintenance_mode', 'website') === "y"): ?> checked="checked" <?php endif; ?> >
-        <label class="custom-control-label" for="maintenance_mode_0"><?php _e("Enabled"); ?></label>
+        <label class="custom-control-label" for="maintenance_mode_0"><?php _e("Enable"); ?></label>
     </div>
 
 
