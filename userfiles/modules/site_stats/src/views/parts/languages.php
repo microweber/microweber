64ddf1e53fe0e3f812676ac1450c66b46208d6bf
Nikolay <rachkuw@gmail.com>
2021-03-10 14:55:55 +0200

    cols

--- userfiles/modules/site_stats/src/views/parts/languages.php
+++ userfiles/modules/site_stats/src/views/parts/languages.php
@@ -7,12 +7,12 @@ if (!$data) {
 <?php foreach ($data as $item): ?>
     <div class="item location">
         <div class="location-progressbar" style="width: <?php print $item['sessions_percent']; ?>%;"></div>
-        <div class="mw-ui-row">
-            <div class="mw-ui-col">
+        <div class="mw-ui-row d-flex">
+            <div class="col-6">
                 <div class="title"><?php print $item['language']; ?></div>
             </div>
 
-            <div class=" mw-ui-col">
+            <div class=" col-6">
                 <div class="cnt"><?php print $item['sessions_count']; ?></div>
             </div>
         </div>
--- userfiles/modules/site_stats/src/views/parts/locations.php
+++ userfiles/modules/site_stats/src/views/parts/locations.php
@@ -7,13 +7,13 @@ if (!$data) {
 <?php foreach ($data as $item): ?>
     <div class="item location">
         <div class="location-progressbar" style="width: <?php print $item['sessions_percent']; ?>%;"></div>
-        <div class="mw-ui-row">
-            <div class="mw-ui-col">
+        <div class="mw-ui-row d-flex">
+            <div class="col-6">
                 <div class="flag"><span class="flag-icon flag-icon-<?php print strtolower($item['country_code']); ?>"></span></div>
                 <div class="title"><?php print $item['country_name']; ?></div>
             </div>
 
-            <div class=" mw-ui-col">
+            <div class="col-6">
                 <div class="cnt"><?php print $item['sessions_count']; ?></div>
             </div>
         </div>
