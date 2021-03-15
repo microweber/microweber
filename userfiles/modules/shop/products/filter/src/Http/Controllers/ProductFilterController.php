429d92342dcb8eb40410ba3a9649cb885c54fc4e
Bozhidar Slaveykov <bobi@microweber.com>
2021-03-15 19:04:13 +0200

    update

--- userfiles/modules/shop/products/filter/src/Http/Controllers/ProductFilterController.php
+++ userfiles/modules/shop/products/filter/src/Http/Controllers/ProductFilterController.php
@@ -13,9 +13,78 @@ class ProductFilterController
      */
     public function index(Request $request)
     {
+        $pageId = $request->get('content-id');
 
-        
-        return view('productFilter::index', []);
+        $limit = '';
+        if (isset($_GET['limit'])) {
+            $limit = (int) $_GET['limit'];
+        }
+
+        $orderBy = '';
+        if (isset($_GET['orderBy'])) {
+            $orderBy = $_GET['orderBy'];
+        }
+
+        $filters = [];
+        $productPrices = [];
+        $getProducts = \MicroweberPackages\Product\Models\Product::where('parent', $pageId)->get();
+        if (!empty($getProducts)) {
+            foreach ($getProducts as $product) {
+                $productPrices[] = $product->price;
+                $customFields = $product->customField()->with('fieldValue')->get();
+                foreach ($customFields as $customField) {
+                    $customFieldValues = $customField->fieldValue()->get();
+                    if (empty($customFieldValues)) {
+                        continue;
+                    }
+                    $filterOptions = [];
+                    foreach ($customFieldValues as $customFieldValue) {
+                        $filterOptions[] = [
+                            'id'=>$customFieldValue->id,
+                            'value'=>$customFieldValue->value,
+                        ];
+                    }
+                    $filters[$customField->name_key] = [
+                        'type'=>$customField->type,
+                        'name'=>$customField->name,
+                        'options'=>$filterOptions
+                    ];
+                }
+            }
+        }
+
+        asort($productPrices, SORT_STRING | SORT_FLAG_CASE | SORT_NATURAL);
+        $productsMinPrice = $productPrices[0];
+        $productsMaxPrice = end($productPrices);
+
+        $priceBetween = '';
+        if (isset($_GET['priceBetween'])) {
+            $priceBetween = $_GET['priceBetween'];
+        }
+
+        $getMinPrice = $priceBetween;
+        $getMaxPrice = false;
+
+        if (strpos($priceBetween, ',') !== false) {
+            $priceRange = explode(',', $priceBetween);
+            $getMinPrice = $priceRange[0];
+            $getMaxPrice = $priceRange[1];
+        }
+
+        $getMinPrice = intval($getMinPrice);
+        $getMaxPrice = intval($getMaxPrice);
+
+        return view('productFilter::index', [
+            'currencySymbol'=>mw()->shop_manager->currency_symbol(),
+            'getMinPrice'=>$getMinPrice,
+            'getMaxPrice'=>$getMaxPrice,
+            'priceBetween'=>$priceBetween,
+            'productsMinPriceRounded'=>round($productsMinPrice),
+            'productsMaxPriceRounded'=>round($productsMaxPrice),
+            'filters'=>$filters,
+            'orderBy'=>$orderBy,
+            'limit'=>$limit,
+        ]);
     }
 
 }
