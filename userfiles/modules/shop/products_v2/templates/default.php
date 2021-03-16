bf7d39d8f39795440e20e70c0a97d4f6916b9917
Bozhidar Slaveykov <bobi@microweber.com>
2021-03-16 13:22:39 +0200

    update

--- userfiles/modules/shop/products_v2/templates/default.php
+++ userfiles/modules/shop/products_v2/templates/default.php
@@ -6,114 +6,115 @@ type: layout
 
 name: Default
 
-description: Default - 3 Columns
+description: Default
 
 */
 ?>
+
 <?php
-
-
 $tn = $tn_size;
-if(!isset($tn[0]) or ($tn[0]) == 150){
-     $tn[0] = 350;
+if (!isset($tn[0]) or ($tn[0]) == 150) {
+    $tn[0] = 350;
 }
-if(!isset($tn[1])){
-     $tn[1] = $tn[0];
+if (!isset($tn[1])) {
+    $tn[1] = $tn[0];
 }
-
-
 ?>
+
 <?php if (!empty($data)): ?>
-<?php
+    <div class="row shop-products">
+        <?php foreach ($data as $item): ?>
+            <?php $categories = content_categories($item['id']); ?>
 
-
-    $count = 0;
-    $len =  count($data);
-
-    $helpclass = '';
-
-    if($len%3 !=0 ){
-        if((($len-1)%3)==0 or $len==1){
-             $helpclass = 'last-row-single';
-        }
-        elseif((($len-2)%3)== 0  or $len==2){
-            $helpclass = 'last-row-twoitems';
-        }
-    } ?>
-
-<div class="clearfix module-products-template-columns-3 <?php print $helpclass; ?>">
-  <?php
-    foreach ($data as $item):
-
-    $count++;
-    ?>
-  <?php if($count == 1 or ($count-1) % 3 == 0) { ?>
-  <div class="mw-ui-row">
-    <?php } ?>
-    <div class="mw-ui-col">
-      <div class="mw-ui-col-container">
-        <div class="mw-module-products-default-item product-item-single" itemscope itemtype="<?php print $schema_org_item_type_tag ?>">
-          <?php if($show_fields == false or in_array('thumbnail', $show_fields)): ?>
-          <a class="img-polaroid img-rounded" href="<?php print $item['link'] ?>"> <span class="valign"> <span class="valign-cell"> <img <?php if($item['image']==false){ ?>class="pixum"<?php } ?>  i src="<?php print thumbnail($item['image'], $tn[0], $tn[1]); ?>" alt="<?php print $item['title'] ?>" title="<?php print $item['title'] ?>" temprop="image"  /> </span> </span> </a>
-          <?php endif; ?>
-          <?php if($show_fields == false or in_array('title', $show_fields)): ?>
-          <h3 itemprop="name"><a itemprop="url" class="lead" href="<?php print $item['link'] ?>"><?php print $item['title'] ?></a></h3>
-          <?php endif; ?>
-          <?php if($show_fields != false and in_array('created_at', $show_fields)): ?>
-          <span class="date"  itemprop="dateCreated"><?php print $item['created_at'] ?></span>
-          <?php endif; ?>
-          <?php if($show_fields == false or ($show_fields != false and  is_array($show_fields) and  in_array('description', $show_fields))): ?>
-          <p class="description" itemprop="description"> <?php print $item['description']; ?> </p>
-          <?php endif; ?>
-          <?php if($show_fields != false and ($show_fields != false and  in_array('read_more', $show_fields))): ?>
-          <a href="<?php print $item['link'] ?>" itemprop="url" class="mw-more">
-          <?php $read_more_text ? print $read_more_text : print _e('Read More', true); ?>
-          </a>
-          <?php endif; ?>
-          <div class="product-price-holder clearfix">
-            <?php if($show_fields == false or in_array('price', $show_fields)): ?>
-            <?php if(isset($item['prices']) and is_array($item['prices'])){  ?>
-            <?php 
-                $vals2 = array_values($item['prices']);
-                $val1 = array_shift($vals2); ?>
-            <span class="price"><?php print currency_format($val1); ?></span>
-            <?php } else{ ?>
-            <?php } ?>
-            <?php endif; ?>
-            <?php if($show_fields == false or in_array('add_to_cart', $show_fields)): ?>
             <?php
-                $add_cart_text = get_option('data-add-to-cart-text', $params['id']);
-			  if( $add_cart_text == false or $add_cart_text == "Add to cart"){ 
-			    $add_cart_text =  _e("Add to cart", true);
-			   }
+            $itemData = content_data($item['id']);
+            $itemTags = content_tags($item['id']);
 
-         ?>
-            <?php if(is_array( $item['prices'])): ?>
-            <?php 
-		  
-	/*	  <button class="mw-ui-btn" type="button" onclick="mw.cart.add('.mw-add-to-cart-<?php print $item['id'].$count ?>');"><i class="icon-shopping-cart glyphicon glyphicon-shopping-cart"></i>&nbsp;<?php print $add_cart_text ?></button> */
-		  ?>
-            <button class="mw-ui-btn" type="button" onclick="mw.cart.add_and_checkout('<?php print $item['id'] ?>');"><i class="icon-shopping-cart glyphicon glyphicon-shopping-cart"></i>&nbsp;<?php print $add_cart_text ?></button>
-            <?php endif; ?>
-            <?php endif; ?>
-          </div>
-          <?php if(is_array( $item['prices']) ): ?>
-          <?php foreach( $item['prices']  as $k  => $v): ?>
-          <div class="clear products-list-proceholder mw-add-to-cart-<?php print $item['id'].$count ?>">
-            <input type="hidden"  name="price" value="<?php print $v ?>" />
-            <input type="hidden"  name="content_id" value="<?php print $item['id'] ?>" />
-          </div>
-          <?php break; endforeach ; ?>
-          <?php  endif; ?>
-        </div>
-      </div>
+            if (!isset($itemData['label'])) {
+                $itemData['label'] = '';
+            }
+            if (!isset($itemData['label-color'])) {
+                $itemData['label-color'] = '';
+            }
+
+            $itemCats = '';
+            if ($categories) {
+                foreach ($categories as $category) {
+                    $itemCats .= $category['title'] . ', ';
+                }
+            }
+            ?>
+
+            <div class="col-12 col-md-6 col-lg-4 col-xl-4 item-<?php print $item['id'] ?>" data-masonry-filter="<?php print $itemCats; ?>" itemscope
+                 itemtype="<?php print $schema_org_item_type_tag ?>">
+                <div class="product">
+                    <?php if (is_array($item['prices'])): ?>
+                        <?php foreach ($item['prices'] as $k => $v): ?>
+                            <input type="hidden" name="price" value="<?php print $v ?>"/>
+                            <input type="hidden" name="content_id" value="<?php print $item['id'] ?>"/>
+                            <?php break; endforeach; ?>
+                    <?php endif; ?>
+
+                    <?php if ($itemData['label'] != ''): ?>
+                        <div class="product-label" style="background-color: <?php echo $itemData['label-color']; ?>;"><?php echo $itemData['label']; ?></div>
+                    <?php endif; ?>
+
+                    <?php if ($show_fields == false or in_array('thumbnail', $show_fields)): ?>
+                        <div class="image" style="background-image: url('<?php print thumbnail($item['image'], 450, 450); ?>');">
+                            <a href="<?php print $item['link'] ?>" class="d-flex h-100 w-100"></a>
+                            <?php /*<div class="hover">
+                                <?php if ($show_fields == false or ($show_fields != false and in_array('add_to_cart', $show_fields))): ?>
+                                    <a href="javascript:;" onclick="mw.cart.add('.shop-products .item-<?php print $item['id'] ?>');" class="btn btn-primary"><i class="material-icons">shopping_cart</i></a>
+                                <?php endif; ?>
+                                <?php if ($show_fields == false or ($show_fields != false and in_array('read_more', $show_fields))): ?>
+                                    <a href="<?php print $item['link'] ?>" class="btn btn-default"><i class="material-icons">remove_red_eye</i></a>
+                                <?php endif; ?>
+                            </div>*/ ?>
+                        </div>
+                    <?php endif; ?>
+
+
+                    <div class="m-t-20">
+                        <?php if ($show_fields == false or in_array('title', $show_fields)): ?>
+                            <a href="<?php print $item['link'] ?>">
+                                <div class="heading-holder">
+                                    <h5><?php print $item['title'] ?></h5>
+                                </div>
+                            </a>
+                        <?php endif; ?>
+
+
+                        <div class="row">
+                            <div class="col-6 price-holder">
+                                <?php if ($show_fields == false or in_array('price', $show_fields)): ?>
+                                    <?php if (isset($item['prices']) and is_array($item['prices'])): ?>
+                                        <?php
+                                        $vals2 = array_values($item['prices']);
+                                        $val1 = array_shift($vals2);
+                                        ?>
+                                        <p>
+                                            <?php if (isset($item['original_price']) AND $item['original_price'] != ''): ?>
+                                                <span class="price-old"><?php print currency_format($item['original_price']); ?></span>
+                                            <?php endif; ?>
+                                            <span class="price"><?php print currency_format($val1); ?></span>
+                                        </p>
+                                    <?php endif; ?>
+                                <?php endif; ?>
+                            </div>
+
+                            <div class="col-6 text-right">
+                                <?php if ($show_fields == false or ($show_fields != false and in_array('add_to_cart', $show_fields))): ?>
+                                    <a href="javascript:;" onclick="mw.cart.add('.shop-products .item-<?php print $item['id'] ?>');" class="btn btn-primary"><i class="material-icons">shopping_cart</i> Add to cart</a>
+                                <?php endif; ?>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        <?php endforeach; ?>
     </div>
-    <?php if($count % 3 == 0 or $count == $len){ ?>
-  </div>
-  <?php  } ?>
-  <?php  endforeach; ?>
-</div>
 <?php endif; ?>
+
 <?php if (isset($pages_count) and $pages_count > 1 and isset($paging_param)): ?>
-<?php print paging("num={$pages_count}&paging_param={$paging_param}&current_page={$current_page}") ?>
+    <module type="pagination" pages_count="<?php echo $pages_count; ?>" paging_param="<?php echo $paging_param; ?>"/>
 <?php endif; ?>
