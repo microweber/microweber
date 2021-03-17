c0332b73d9c7785b469da820243d65504e4bdb04
Bozhidar Slaveykov <bobi@microweber.com>
2021-03-17 13:25:10 +0200

    update

--- src/MicroweberPackages/Content/Content.php
+++ src/MicroweberPackages/Content/Content.php
@@ -17,7 +17,7 @@ use MicroweberPackages\Tag\Traits\TaggableTrait;
 
 class Content extends Model
 {
-    /*use Taggable;
+    use Taggable;
     use TaggableTrait;
     use ContentDataTrait;
     use CustomFieldsTrait;
@@ -25,8 +25,8 @@ class Content extends Model
     use HasSlugTrait;
     use MediaTrait;
     use Filterable;
-    use HasCreatedByFieldsTrait;*/
-   // use CacheableQueryBuilderTrait;
+    use HasCreatedByFieldsTrait;
+    use CacheableQueryBuilderTrait;
 
 
     protected $table = 'content';
