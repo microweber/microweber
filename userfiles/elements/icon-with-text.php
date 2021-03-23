3db353399d328c494f3ca98de2ab4d8f78c6ae8b
Nikolay <rachkuw@gmail.com>
2021-03-23 17:07:15 +0200

    Adding new static layouts in core

---
+++ userfiles/elements/icon-with-text.php
@@ -0,0 +1,9 @@
+<div class="container">
+    <div class="row d-flex justify-content-center text-center">
+        <div class="col-md-5 col">
+            <div class="mb-3"><i class="mdi mdi-earth " style="font-size: 48px;"></i></div>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        </div>
+    </div>
+</div>
+
