2d328119f8f5f3ade4554d959016564abb70c2b2
Nikolay <rachkuw@gmail.com>
2021-03-23 17:16:16 +0200

    update

--- userfiles/elements/image-with-title-and-text.php
+++ userfiles/elements/image-with-title-and-text.php
@@ -1,6 +1,6 @@
 <div class="container">
     <div class="row d-flex justify-content-center text-center">
-        <div class="col-md-6 col">
+        <div class="col-md-5 col">
             <img class="rounded-xl pb-4" src="<?php print template_url(); ?>assets/img/image-for-layouts.png" >
             <h1 class="mb-3 font-weight-bold">Short Title</h1>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt <a href="">ut labore et</a> dolore magna aliqua.</p>
