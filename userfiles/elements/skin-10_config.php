821d1389778213f04472c57fd41828a53d68ae91
Nikolay <rachkuw@gmail.com>
2021-03-24 09:41:56 +0200

    liveedit blog elements reordering

--- userfiles/elements/skin-10_config.php
+++ userfiles/elements/skin-10_config.php
@@ -8,5 +8,5 @@ $config['website'] = "http://microweber.com";
 $config['no_cache'] = true;
 $config['categories'] = "custom";
 $config['version'] = 0.1;
-$config['position'] = 17;
+$config['position'] = 29;
 $config['as_element'] = true;
