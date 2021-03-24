db875eeab83f13c64ad559d43fae23154d00e7f2
Nikolay <rachkuw@gmail.com>
2021-03-24 11:04:48 +0200

    two-text-columns-with-icon-and-title.php

--- userfiles/elements/two-text-columns-with-icon-and-title.php
+++ userfiles/elements/two-text-columns-with-icon-and-title.php
@@ -1,13 +1,13 @@
 <div class="container">
-    <div class="row d-flex justify-content-center">
-       <div class="col-md-5 col d-flex">
-           <div class="col-md-6 col">
+    <div class="row">
+       <div class="col d-flex justify-content-center">
+           <div class="col">
                <div class="mb-3"><i class="mdi mdi-earth" style="font-size: 25px;"></i></div>
                <h6 class="mb-3 font-weight-bold">Awesome title</h6>
                <p>Contrary to popular belief, Lorem Ipsum is not simply random text.</p>
            </div>
 
-           <div class="col-md-6 col">
+           <div class="col">
                <div class="mb-3"><i class="mdi mdi-earth" style="font-size: 25px;"></i></div>
                <h6 class="mb-3 font-weight-bold">Awesome title</h6>
                <p>Contrary to popular belief, Lorem Ipsum is not simply random text.</p>
@@ -15,4 +15,3 @@
        </div>
     </div>
 </div>
-
