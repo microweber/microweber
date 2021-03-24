88b6b1860cc074cd00e12e0b2c3944a142d99738
Nikolay <rachkuw@gmail.com>
2021-03-24 11:05:04 +0200

    two text columns

--- userfiles/elements/two-text-columns.php
+++ userfiles/elements/two-text-columns.php
@@ -1,11 +1,11 @@
 <div class="container">
-    <div class="row d-flex justify-content-center">
-        <div class="col-md-5 col d-flex">
-            <div class="col-md-6 col">
+    <div class="row">
+        <div class="col d-flex justify-content-center">
+            <div class="col">
                 <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from from 45 BC, making it over 2000 years old.</p>
             </div>
 
-            <div class="col-md-6 col">
+            <div class="col">
                 <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from from 45 BC, making it over 2000 years old.</p>
             </div>
         </div>
