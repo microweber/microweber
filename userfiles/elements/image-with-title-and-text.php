d0bdcc413f9212e4c7ddcc1b9308c8452963727d
Nikolay <rachkuw@gmail.com>
2021-03-24 10:39:38 +0200

    Columns reorganize

--- userfiles/elements/image-with-title-and-text.php
+++ userfiles/elements/image-with-title-and-text.php
@@ -1,9 +1,11 @@
 <div class="container">
     <div class="row d-flex justify-content-center text-center">
-        <div class="col-md-5 col">
-            <img class="rounded-xl pb-4" src="<?php print template_url(); ?>assets/img/image-for-layouts.png" >
-            <h1 class="mb-3 font-weight-bold">Short Title</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt <a href="">ut labore et</a> dolore magna aliqua.</p>
+        <div class="col-auto">
+            <img class="rounded-xl pb-4 w-100" src="<?php print template_url(); ?>assets/img/image-for-layouts.png" >
+            <div class="col mx-auto">
+                <h1 class="mb-3 font-weight-bold">Short Title</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt <a href="">ut labore et</a> dolore magna aliqua.</p>
+            </div>
         </div>
     </div>
 </div>
