7908f8dc29bf0efbccfe162e9ab91639f70b1af6
Nikolay <rachkuw@gmail.com>
2021-03-25 15:27:58 +0200

    Live edit Layouts skin-3 redesign

--- userfiles/elements/skin-3.php
+++ userfiles/elements/skin-3.php
@@ -1,5 +1,5 @@
 <div class="container">
-    <div class="background-image-holder bg-image text-center rounded text-white pt-5"
+    <div class="background-image-holder bg-image text-center rounded text-white"
          style="
         background-image: url('<?php print elements_url() ?>images/image-for-layouts.png');
         background-repeat: no-repeat;
@@ -10,10 +10,12 @@
         right: 0;
         height: 400px;
         display: block;">
-        <div class="pt-5">
-            <h1 class="h1 pt-3 mb-3">Our Services</h1>
-            <p class="mb-3">Be assured, our services and team members will support your business growth</p>
-            <module type="btn" text="Button" />
+        <div class="h-100 d-flex align-items-center element">
+            <div class="col-12">
+                <h1 class="pt-3 mb-3">Our Services</h1>
+                <p class="mb-3">Be assured, our services and team members will support your business growth</p>
+                <module type="btn" text="Button" />
+            </div>
         </div>
     </div>
 </div>
