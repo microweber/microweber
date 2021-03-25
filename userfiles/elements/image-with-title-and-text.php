52755db9cba3250fb117c143e98a6c68aa559a00
Nikolay <rachkuw@gmail.com>
2021-03-25 14:14:06 +0200

    Small changes

--- userfiles/elements/image-with-title-and-text.php
+++ userfiles/elements/image-with-title-and-text.php
@@ -1,7 +1,7 @@
 <div class="container">
     <div class="row d-flex justify-content-center text-center">
         <div class="col-auto">
-            <img class="rounded-xl pb-4 w-100" src="<?php print template_url(); ?>assets/img/image-for-layouts.png" >
+            <img class="rounded-xl pb-4 w-100" src="<?php print elements_url(); ?>images/image-for-layouts.png">
             <div class="col mx-auto">
                 <h1 class="mb-3 font-weight-bold">Short Title</h1>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt <a href="">ut labore et</a> dolore magna aliqua.</p>
