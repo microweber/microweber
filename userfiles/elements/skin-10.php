b3d7808b59ba7b305e6868175bacfb4d6a1129c1
Nikolay <rachkuw@gmail.com>
2021-03-25 15:29:35 +0200

    Live edit Layouts skin-10 redesign

--- userfiles/elements/skin-10.php
+++ userfiles/elements/skin-10.php
@@ -1,56 +1,35 @@
-<div class="mw-static-element mw-features-boxes" id="head-image-<?php print CONTENT_ID; ?>">
-    <h2>How it Works?</h2>
-    <br/>
-    <div class="mw-ui-row">
-        <div class="mw-ui-col cloneable">
-            <div class="mw-ui-row">
-                <div class="mw-ui-col" style="width: 35px;">
-                    <div class="number text-primary">1.</div>
-                </div>
-                <div class="mw-ui-col">
-                    <div class="text">
-                        <h4>We love our customers</h4>
-                        <p>Template layout is ready for edit in ream time with Microweber.
-                            How to Be Creative. Creativity is a skill that you can work on with time, training, and effort. There are many areas you can focus on to improve your overall
-                            creativity.
-                        </p>
-                    </div>
-                </div>
+<style>
+    div.number {
+        font-size: 24px;
+    }
+</style>
+
+<div class="container">
+    <h2 class="text-center mb-3">How it Works?</h2>
+    <div class="row">
+        <div class="col-md col-12 text-md-left text-center cloneable my-4">
+            <div class="d-flex">
+                <div class="number mr-3 align-self-center text-primary">1.</div>
+                <h4 class="mb-2">Let’s go to the space</h4>
             </div>
+            <p>Template layout is ready for edit in ream time with Microweber.</p>
         </div>
 
-        <div class="mw-ui-col cloneable">
-            <div class="mw-ui-row">
-                <div class="mw-ui-col" style="width: 35px;">
-                    <div class="number text-primary">2.</div>
-                </div>
-                <div class="mw-ui-col">
-                    <div class="text">
-                        <h4>Your website your way</h4>
-                        <p>Template layout is ready for edit in ream time with Microweber.
-                            How to Be Creative. Creativity is a skill that you can work on with time, training, and effort. There are many areas you can focus on to improve your overall
-                            creativity.
-                        </p>
-                    </div>
-                </div>
+        <div class="col-md col-12 text-md-left text-center cloneable my-4">
+            <div class="d-flex">
+                <div class="number mr-3 align-self-center text-primary">2.</div>
+                <h4 class="mb-2">Let’s go to the space</h4>
             </div>
+            <p>Template layout is ready for edit in ream time with Microweber.</p>
         </div>
 
-        <div class="mw-ui-col cloneable">
-            <div class="mw-ui-row">
-                <div class="mw-ui-col" style="width: 35px;">
-                    <div class="number text-primary">3.</div>
-                </div>
-                <div class="mw-ui-col">
-                    <div class="text">
-                        <h4>Let’s go to the space</h4>
-                        <p>Template layout is ready for edit in ream time with Microweber.
-                            How to Be Creative. Creativity is a skill that you can work on with time, training, and effort. There are many areas you can focus on to improve your overall
-                            creativity.
-                        </p>
-                    </div>
-                </div>
+        <div class="col-md col-12 text-md-left text-center cloneable my-4">
+            <div class="d-flex">
+                <div class="number mr-3 align-self-center text-primary">3.</div>
+                <h4 class="mb-2">Let’s go to the space</h4>
             </div>
+            <p>Template layout is ready for edit in ream time with Microweber.</p>
         </div>
     </div>
 </div>
+
