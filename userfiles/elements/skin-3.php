da1a658228e64ce73c7c0cfc29295cbb0d870524
Nikolay <rachkuw@gmail.com>
2021-03-26 12:29:41 +0200

    fixes

--- userfiles/elements/skin-3.php
+++ userfiles/elements/skin-3.php
@@ -1,5 +1,5 @@
-<div class="container" data-parallax="" data-background-position="center top"">
-    <div class="row background-image-holder parallax text-center rounded text-white d-flex w-100"
+<div data-parallax="" data-background-position="center"">
+    <div class="row background-image-holder parallax text-center rounded text-white d-flex"
          style="background-image: url('<?php print elements_url() ?>images/image-for-layouts.png');">
         <div class="w-100 align-self-center text-center info-holder allow-drop"></div>
     </div>
