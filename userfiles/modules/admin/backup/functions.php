eea03ef218d3611f05c0d8e047b6608922fa1214
Bobi <69676022+bobicloudvision@users.noreply.github.com>
2021-03-31 14:08:15 +0300

    fix

--- userfiles/modules/admin/backup/functions.php
+++ userfiles/modules/admin/backup/functions.php
@@ -1,26 +1,39 @@
 <?php
 
-autoload_add_namespace(__DIR__.'/src/', 'MicroweberPackages\\LegacyBackup\\');
+autoload_add_namespace(__DIR__ . '/src/', 'MicroweberPackages\\LegacyBackup\\');
 
 
-$backup = new \MicroweberPackages\LegacyBackup\Backup();
+api_expose_admin('MicroweberPackages\LegacyBackup\Backup\create', function ($params) {
+    $backup = new \MicroweberPackages\LegacyBackup\Backup();
+    return $backup->create($params);
+});
 
-api_expose_admin('MicroweberPackages\LegacyBackup\Backup\create', $backup->create());
-api_expose_admin('MicroweberPackages\LegacyBackup\Backup\create_full', $backup->create_full());
-api_expose_admin('MicroweberPackages\LegacyBackup\Backup\cronjob', $backup->cronjob());
+api_expose_admin('MicroweberPackages\LegacyBackup\Backup\create_full', function ($params) {
+    $backup = new \MicroweberPackages\LegacyBackup\Backup();
+    return $backup->create_full($params);
+});
 
-api_expose_admin('MicroweberPackages\LegacyBackup\Backup\delete', function($params) use ($backup) {
+api_expose_admin('MicroweberPackages\LegacyBackup\Backup\cronjob', function ($params) {
+    $backup = new \MicroweberPackages\LegacyBackup\Backup();
+    return $backup->cronjob($params);
+});
+
+api_expose_admin('MicroweberPackages\LegacyBackup\Backup\delete', function ($params) {
+    $backup = new \MicroweberPackages\LegacyBackup\Backup();
     return $backup->delete($params);
 });
 
-api_expose_admin('MicroweberPackages\LegacyBackup\Backup\download', function($params) use ($backup) {
+api_expose_admin('MicroweberPackages\LegacyBackup\Backup\download', function ($params) {
+    $backup = new \MicroweberPackages\LegacyBackup\Backup();
     return $backup->download($params);
 });
 
-api_expose_admin('MicroweberPackages\LegacyBackup\Backup\restore', function($params) use ($backup) {
+api_expose_admin('MicroweberPackages\LegacyBackup\Backup\restore', function ($params) {
+    $backup = new \MicroweberPackages\LegacyBackup\Backup();
     return $backup->restore($params);
 });
 
-api_expose_admin('MicroweberPackages\LegacyBackup\Backup\move_uploaded_file_to_backup', function($params) use ($backup) {
+api_expose_admin('MicroweberPackages\LegacyBackup\Backup\move_uploaded_file_to_backup', function ($params) {
+    $backup = new \MicroweberPackages\LegacyBackup\Backup();
     return $backup->move_uploaded_file_to_backup($params);
 });
