ce661cce49f531754322076952f32b2f52641849
Bozhidar Slaveykov <bobi@microweber.com>
2021-04-06 15:48:29 +0300

    update

--- src/MicroweberPackages/Checkout/resources/views/contact_information.blade.php
+++ src/MicroweberPackages/Checkout/resources/views/contact_information.blade.php
@@ -7,7 +7,6 @@
     <div class="col-8">
         <form method="post" action="{{ route('checkout.contact_information_save') }}">
 
-        <a href="{{ site_url() }}" class="btn btn-outline-primary"><i class="mdi mdi-arrow-left"></i> {{ _e('Back to shopping') }}</a>
         <a href="{{ route('checkout.cart') }}" class="btn btn-outline-primary"><i class="mdi mdi-cart"></i> {{ _e('Back to cart') }}</a>
         <div class="shop-cart" style="margin-top:25px;">
 
