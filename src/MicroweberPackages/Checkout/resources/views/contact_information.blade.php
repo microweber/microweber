e7507e104b15da27fb9c33ae8f3b36cb0167830b
Bozhidar Slaveykov <bobi@microweber.com>
2021-04-06 17:01:12 +0300

    update

--- src/MicroweberPackages/Checkout/resources/views/contact_information.blade.php
+++ src/MicroweberPackages/Checkout/resources/views/contact_information.blade.php
@@ -19,14 +19,14 @@
                 <div class="col-6">
                     <div class="form-group">
                         <label for="exampleInputFirstName"><?php _e("First Name"); ?></label>
-                        <input required name="first_name" type="text" value="<?php if (!empty($checkout_session['first_name'])) echo $checkout_session['first_name']; ?>" class="form-control"
+                        <input name="first_name" type="text" value="<?php if (!empty($checkout_session['first_name'])) echo $checkout_session['first_name']; ?>" class="form-control"
                                placeholder="<?php _e("First Name"); ?>">
                     </div>
                 </div>
                 <div class="col-6">
                     <div class="form-group">
                         <label for="exampleInputLastName"><?php _e("Last Name"); ?></label>
-                        <input required name="last_name" type="text" value="<?php if (!empty($checkout_session['last_name'])) echo $checkout_session['last_name']; ?>" class="form-control"
+                        <input name="last_name" type="text" value="<?php if (!empty($checkout_session['last_name'])) echo $checkout_session['last_name']; ?>" class="form-control"
                                placeholder="<?php _e("Last Name"); ?>">
                     </div>
                 </div>
@@ -36,14 +36,14 @@
                 <div class="col-6">
                     <div class="form-group m-0">
                         <label for="exampleInputEmail1"><?php _e("Email"); ?></label>
-                        <input required name="email" type="email" value="<?php if (!empty($checkout_session['email'])) echo $checkout_session['email']; ?>" class="form-control"
+                        <input name="email" type="email" value="<?php if (!empty($checkout_session['email'])) echo $checkout_session['email']; ?>" class="form-control"
                                placeholder="<?php _e("Enter email"); ?>">
                     </div>
                 </div>
                 <div class="col-6">
                     <div class="form-group">
                         <label for="exampleInputPhone"><?php _e("Phone"); ?></label>
-                        <input required name="phone" type="text" value="<?php if (!empty($checkout_session['phone'])) echo $checkout_session['phone']; ?>" class="form-control"
+                        <input name="phone" type="text" value="<?php if (!empty($checkout_session['phone'])) echo $checkout_session['phone']; ?>" class="form-control"
                                placeholder="<?php _e("Enter phone"); ?>">
                     </div>
                 </div>
@@ -54,6 +54,7 @@
             <button type="submit" class="btn btn-info">{{ _e('Continue') }}</button>
         </form>
     </div>
+    
     <div class="col-4">
         <module type="shop/cart" template="checkout_v2_sidebar" data-checkout-link-enabled="n" />
     </div>
