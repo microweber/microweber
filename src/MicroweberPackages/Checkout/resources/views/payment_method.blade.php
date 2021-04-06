84e75ea3db860e606d076e0b703a2f16f9ec1563
Bozhidar Slaveykov <bobi@microweber.com>
2021-04-06 15:48:52 +0300

    update

--- src/MicroweberPackages/Checkout/resources/views/payment_method.blade.php
+++ src/MicroweberPackages/Checkout/resources/views/payment_method.blade.php
@@ -7,7 +7,6 @@
     <div class="col-8">
         <form method="post" action="{{ route('checkout.payment_method_save') }}">
 
-            <a href="{{ site_url() }}" class="btn btn-outline-primary"><i class="mdi mdi-arrow-left"></i> {{ _e('Back to shopping') }}</a>
             <a href="{{ route('checkout.cart') }}" class="btn btn-outline-primary"><i class="mdi mdi-cart"></i> {{ _e('Back to cart') }}</a>
             <a href="{{ route('checkout.contact_information') }}" class="btn btn-outline-primary"><i class="mdi mdi-phone"></i> {{ _e('Back to contact information') }}</a>
             <a href="{{ route('checkout.shipping_method') }}" class="btn btn-outline-primary"><i class="mdi mdi-shipping"></i> {{ _e('Back to shipping information') }}</a>
--- src/MicroweberPackages/Checkout/resources/views/shipping_method.blade.php
+++ src/MicroweberPackages/Checkout/resources/views/shipping_method.blade.php
@@ -8,7 +8,6 @@
 
         <form method="post" action="{{ route('checkout.shipping_method_save') }}">
 
-            <a href="{{ site_url() }}" class="btn btn-outline-primary"><i class="mdi mdi-arrow-left"></i> {{ _e('Back to shopping') }}</a>
             <a href="{{ route('checkout.cart') }}" class="btn btn-outline-primary"><i class="mdi mdi-cart"></i> {{ _e('Back to cart') }}</a>
             <a href="{{ route('checkout.contact_information') }}" class="btn btn-outline-primary"><i class="mdi mdi-phone"></i> {{ _e('Back to contact information') }}</a>
             <div class="shop-cart" style="margin-top:25px;">
