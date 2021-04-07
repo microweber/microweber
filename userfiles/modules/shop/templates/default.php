f4c702f8394d9d097fa8e1a364b46ec20e8d7d51
Bozhidar Slaveykov <bobi@microweber.com>
2021-04-07 14:18:59 +0300

    update

--- userfiles/modules/shop/templates/default.php
+++ userfiles/modules/shop/templates/default.php
@@ -8,13 +8,13 @@ description: Full width shop template with filters
 
 */
 ?>
-
 <div class="row justify-content-between">
     <div class="col-sm-12 col-md-8 col-lg-9 col-xl-9">
         <module type="shop/products_v2" content-id="<?php print PAGE_ID; ?>" />
     </div
 
     <div class="col-sm-12 col-md-4 col-lg-3 col-xl-2">
+
         <div class="edit allow-drop" field="default_shop_sidebar" rel="inherit">
             <div class="sidebar">
 
@@ -24,7 +24,7 @@ description: Full width shop template with filters
                 </div>
 
                 <div class="sidebar__widget m-b-40">
-                    <h4 class="m-b-20"><?php _lang("Categories"); ?> - <?php print PAGE_ID; ?></h4>
+                    <h4 class="m-b-20"><?php _lang("Categories"); ?></h4>
 
                     <module type="categories" content-id="<?php print PAGE_ID; ?>" />
                 </div>
@@ -35,6 +35,6 @@ description: Full width shop template with filters
                 </div>
 
             </div>
-        </div>
+        </div class="edit allow-drop">
     </div>
 </div>
