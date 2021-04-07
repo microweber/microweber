b4309c330d1f9f63913fca32fbfcb7e94189f677
Nikolay <rachkuw@gmail.com>
2021-04-07 14:04:31 +0300

    Payment method step

--- src/MicroweberPackages/Checkout/resources/views/payment_method.blade.php
+++ src/MicroweberPackages/Checkout/resources/views/payment_method.blade.php
@@ -1,49 +1,56 @@
 @extends('checkout::layout')
 
 @section('content')
+    <form method="post" action="{{ route('checkout.payment_method_save') }}">
 
-    faila se namira v: /src/MicroweberPackages/Checkout/resources/views/payment_method.blade.php
+        <a href="{{ route('checkout.shipping_method') }}" class="btn btn-outline-primary"><i class="mdi mdi-arrow-left"></i> {{ _e('Back') }}</a>
+        <div class="shop-cart mt-3">
+            <label class="font-weight-bold control-label mb-0"><?php _e("Personal information"); ?></label>
+            <small class="text-muted d-block mb-2"> <?php _e("Your information"); ?></small>
 
-    <div class="col-8">
-        <form method="post" action="{{ route('checkout.payment_method_save') }}">
+            <div class="card mb-3">
+                <div class="card-body d-flex p-3">
 
-            <a href="{{ route('checkout.shipping_method') }}" class="btn btn-outline-primary"><i class="mdi mdi-arrow-left"></i> {{ _e('Back') }}</a>
-            <div class="shop-cart" style="margin-top:25px;margin-bottom:25px;">
-
-                <div class="card" style="margin-bottom:15px;">
-                    <div class="card-body">
-                        Contact info:
-                        <br />
-                        Name: <?php if (!empty($checkout_session['first_name'])) echo $checkout_session['first_name']; ?>
+                    <div class="col-10">
+                        <?php if (!empty($checkout_session['first_name'])) echo $checkout_session['first_name']; ?>
                         <?php if (!empty($checkout_session['last_name'])) echo $checkout_session['last_name']; ?>
                         <br/>
-                        Phone: <?php if (!empty($checkout_session['phone'])) echo $checkout_session['phone']; ?>
+                        <?php if (!empty($checkout_session['phone'])) echo $checkout_session['phone']; ?>
                         <br />
-                        E-mail: <?php if (!empty($checkout_session['email'])) echo $checkout_session['email']; ?>
+                        <?php if (!empty($checkout_session['email'])) echo $checkout_session['email']; ?>
                         <br />
-                        <a href="{{ route('checkout.contact_information') }}" class="btn btn-outline-primary"><i class="mdi mdi-pencil"></i> {{ _e('Edit contact information') }}</a>
+                    </div>
+
+                    <div class="col-2 justify-content-end text-right align-self-top px-0">
+                        <a href="{{ route('checkout.contact_information') }}" class="btn btn-link text-right">{{ _e('Edit') }}</a>
                     </div>
                 </div>
-
-                <div class="card" style="margin-bottom:15px;">
-                    <div class="card-body">
-                        Shipping Method:
-                        <?php if (!empty($checkout_session['shipping_gw'])) echo $checkout_session['shipping_gw']; ?>
-                        <br />
-                        <br />
-                        <a href="{{ route('checkout.shipping_method') }}" class="btn btn-outline-primary"><i class="mdi mdi-pencil"></i> {{ _e('Edit shipping method') }}</a>
-                    </div>
-                </div>
-
-                <module type="shop/payments" template="checkout_v2" />
-
-                <module type="shop/checkout/terms" />
-
             </div>
 
-            <button type="submit" class="btn btn-info">{{ _e('Continue') }}</button>
-        </form>
-    </div>
+            <label class="font-weight-bold control-label mb-0 pt-2"><?php _e("Shipping method"); ?></label>
+            <small class="text-muted d-block mb-2"> <?php _e("Your choice"); ?></small>
+
+            <div class="card mb-3">
+                <div class="card-body d-flex p-3">
+                    <div class="col-10">
+                        <?php if (!empty($checkout_session['shipping_gw'])) echo $checkout_session['shipping_gw']; ?>
+                    </div>
+
+                    <div class="col-2 justify-content-end text-right align-self-top px-0">
+                        <a href="{{ route('checkout.shipping_method') }}" class="btn btn-link text-right">{{ _e('Edit') }}</a>
+                    </div>
+                </div>
+            </div>
+
+            <module type="shop/payments" template="checkout_v2" />
+
+            <module type="shop/checkout/terms" />
+
+        </div>
+
+        <button type="submit" class="btn btn-info">{{ _e('Continue') }}</button>
+    </form>
+
 
     <div class="col-4">
         <module type="shop/cart" template="checkout_v2_sidebar" data-checkout-link-enabled="n" />
--- userfiles/modules/shop/shipping/templates/checkout_v2.php
+++ userfiles/modules/shop/shipping/templates/checkout_v2.php
@@ -1,19 +1,22 @@
-
-faila se namira v: userfiles\modules\shop\shipping\templates\checkout_v2.php
-
+<style>
+    input[type=radio] {
+        width: 20px;
+        height: 20px;
+    }
+</style>
 <div class="mw-shipping-select">
     <div  class="my-4" <?php if(count($shipping_options) == 1): ?>style="display: none" <?php endif; ?>>
-        <label class="control-label"><?php _e("Choose shipping method"); ?></label>
-        <small class="text-muted d-block mb-2"> <?php _e("Please choose the right method for deliver your order."); ?></small>
-        <select autocomplete="off" onchange="Gateway(this);" name="shipping_gw"
-                class="col-6 pl-0 selectpicker mw-shipping-gateway mw-shipping-gateway-<?php print $params['id']; ?> <?php if (count($shipping_options) == 1): ?> semi_hidden <?php endif; ?>">
+        <h4><?php _e("How you prefer to receive your order ?"); ?></h4>
+        <small class="text-muted d-block mb-2"> <?php _e("Choose the right method for deliver your order."); ?></small>
             <?php foreach ($shipping_options as $item) : ?>
-                <option value="<?php print  $item['module_base']; ?>" <?php if ($selected_shipping_gateway == $item['module_base']): ?> selected <?php endif; ?>><?php print  $item['name']; ?></option>
+                <div class="form-group">
+                    <div class="custom-control custom-radio pl-0 pt-3">
+                        <input type="radio"  onchange="Gateway(this);" name="shipping_gw" value="<?php print  $item['module_base']; ?>" <?php if ($selected_shipping_gateway == $item['module_base']): ?> <?php endif; ?>">
+                        <label class="control-label"> <i class="mdi mdi-truck-check-outline" style="font-size: 36px;"></i> <i class="mdi mdi-walk" style="font-size: 36px;"></i>  <?php print  $item['name']; ?></label>
+                    </div>
+                </div>
             <?php endforeach; ?>
-        </select>
     </div>
-
-
 
     <?php if (isset($selected_shipping_gateway) and is_module($selected_shipping_gateway)): ?>
         <div id="mw-shipping-gateway-selected-wrapper-<?php print $params['id']; ?>">
