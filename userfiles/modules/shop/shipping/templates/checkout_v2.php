a7e4efeace0ae08f29e3d0675d079670a1addfb7
Bozhidar Slaveykov <bobi@microweber.com>
2021-04-08 15:11:04 +0300

    update

--- userfiles/modules/shop/shipping/templates/checkout_v2.php
+++ userfiles/modules/shop/shipping/templates/checkout_v2.php
@@ -1,3 +1,24 @@
+<script type="text/javascript">
+    showShippingModule = function (shippingModule,shippingModulePath) {
+        $('.js-shipping-gateway-box').html('');
+        $.ajax({
+            url: "<?php print route('shop.shipping.set_provider') ?>",
+            data: {"provider":shippingModule},
+            method: 'POST',
+        }).done(function() {
+            mw.reload_module('shop/cart');
+
+            var newShippingModuleElement  = $('<div/>').appendTo('#mw-shipping-gateway-module-'+shippingModule);
+            
+            newShippingModuleElement.attr('id', 'mw-shipping-gateway-module-render-'+shippingModule);
+            newShippingModuleElement.attr('data-type',shippingModulePath);
+            newShippingModuleElement.attr('class','js-shipping-gateway-module-box');
+
+            mw.reload_module(newShippingModuleElement);
+        });
+    }
+</script>
+
 <div class="mw-shipping-select">
     <div  class="my-3" <?php if(count($shipping_options) == 1): ?>style="display: none" <?php endif; ?>>
         <h4 class="mt-5"><?php _e("How you prefer to receive your order ?"); ?></h4>
@@ -7,7 +28,7 @@
                 <div class="form-group">
                     <div class="custom-control custom-radio checkout-v2-radio pl-0 pt-3">
                         <label class="control-label">
-                        <input type="radio" onchange="Gateway(this);" <?php if ($count == 1): ?> checked="checked" <?php endif; ?> name="shipping_gw" value="<?php print  $item['module_base']; ?>" <?php if ($selected_shipping_gateway == $item['module_base']): ?> <?php endif; ?>">
+                        <input type="radio" onchange="showShippingModule('<?php echo md5($item['module_base']); ?>','<?php echo $item['module_base']; ?>');" <?php if ($count == 1): ?> checked="checked" <?php endif; ?> name="shipping_gw" value="<?php print  $item['module_base']; ?>" <?php if ($selected_shipping_gateway == $item['module_base']): ?> <?php endif; ?>">
 
                             <?php
                             if (isset($item['settings']['icon_class'])):
@@ -20,13 +41,14 @@
                         </label>
                     </div>
                 </div>
+
+             <div id="mw-shipping-gateway-module-<?php echo md5($item['module_base']); ?>" class="js-shipping-gateway-box"></div>
+
             <?php endforeach; ?>
     </div>
 
     <?php if (isset($selected_shipping_gateway) and is_module($selected_shipping_gateway)): ?>
-        <div id="mw-shipping-gateway-selected-wrapper-<?php print $params['id']; ?>">
-            <module id="mw-shipping-gateway-selected-<?php print $params['id']; ?>" type="<?php print $selected_shipping_gateway ?>" template="checkout_v2" />
-        </div>
+        <?php print $selected_shipping_gateway ?>
     <?php endif; ?>
 </div>
 
