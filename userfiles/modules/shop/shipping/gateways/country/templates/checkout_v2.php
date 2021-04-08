a66b681e979e9e3adf29ec8b091e0e503c24d731
Bozhidar Slaveykov <bobi@microweber.com>
2021-04-08 18:41:43 +0300

    update

--- userfiles/modules/shop/shipping/gateways/country/templates/checkout_v2.php
+++ userfiles/modules/shop/shipping/gateways/country/templates/checkout_v2.php
@@ -30,7 +30,7 @@ description: Default
                        <div class="form-group">
                            <label><?php _e("Country"); ?></label>
                            <select required name="country" class="form-control shipping-country-select w-100">
-                               <option value=""><?php _e("Country"); ?></option>
+                               <option value=""><?php _e("Select your country"); ?></option>
                                <?php foreach ($data as $item): ?>
                                    <option value="<?php print $item['shipping_country'] ?>" <?php if (isset($selected_country) and $selected_country == $item['shipping_country']): ?> selected="selected" <?php endif; ?>><?php print $item['shipping_country'] ?></option>
                                <?php endforeach; ?>
