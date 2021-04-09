c18695c8d3ae7362d7842da2ffd4acb2ddcc8d67
Nikolay <rachkuw@gmail.com>
2021-04-09 10:22:28 +0300

    Register redesign

--- userfiles/modules/users/register/templates/checkout_v2.php
+++ userfiles/modules/users/register/templates/checkout_v2.php
@@ -30,67 +30,68 @@ description: Default register template
         });
     </script>
 
-<div class="card mt-5">
-    <div id="register_form_holder" class="card-body">
-        <h5><?php _e("Register new account"); ?></h5>
-
+    <div class=" mt-5" id="register_form_holder">
+        <div class="d-flex pb-4">
+            <h4><?php _e("Registration"); ?></h4>
+            <a class="ml-auto align-self-center" href="<?php print route('checkout.login'); ?>">
+                <?php _e("Already have account?"); ?>
+            </a>
+        </div>
         <br />
+
         <form class="p-t-10" action="#" id="user_registration_form_holder" method="post">
             <?php print csrf_form(); ?>
             <?php if ($form_show_first_name): ?>
                 <div class="form-group">
-                    <input class="form-control input-lg" type="text" name="first_name" placeholder="<?php _e('First Name'); ?>">
+                    <label class="control-label"><?php _e("Firs name"); ?></label>
+                    <input class="form-control input-lg" type="text" name="first_name">
                 </div>
             <?php endif; ?>
 
             <?php if ($form_show_last_name): ?>
                 <div class="form-group">
-                    <input class="form-control input-lg" type="text" name="last_name" placeholder="<?php _e('Last Name'); ?>">
+                    <label class="control-label"><?php _e("Last name"); ?></label>
+                    <input class="form-control input-lg" type="text" name="last_name">
                 </div>
             <?php endif; ?>
 
             <div class="form-group">
-                <input class="form-control input-lg" type="email" name="email" placeholder="E-mail">
+                <label class="control-label"><?php _e("Email"); ?></label>
+                <input class="form-control input-lg" type="email" name="email">
             </div>
 
             <div class="form-group m-t-20">
-                <input class="form-control input-lg" type="password" name="password" placeholder="Password">
+                <label class="control-label"><?php _e("Password"); ?></label>
+                <input class="form-control input-lg" type="password" name="password">
             </div>
 
             <?php if ($form_show_password_confirmation): ?>
                 <div class="form-group m-t-20">
-                    <input class="form-control input-lg" type="password" name="password2" placeholder="<?php _e("Confirm Password"); ?>">
+                    <label class="control-label"><?php _e("Confirm password"); ?></label>
+                    <input class="form-control input-lg" type="password" name="password2">
                 </div>
             <?php endif; ?>
 
-            <?php if (!$captcha_disabled): ?>
-                <module type="captcha" template="skin-1"/>
-            <?php endif; ?>
 
 
             <div class="row">
                 <div class="col-12">
-                    <p class="personal-data">Your personal data will be used to support your expirience
+                    <p class="personal-data"><?php _e("Your personal data will be used to support your expirience
                         throughout this website, to manage access to your account
-                        and for other purposes described in our <a href="#">privacy policy</a>.</p>
+                        and for other purposes described in our"); ?> <a href="#"><?php _e("privacy policy"); ?></a>.</p>
                 </div>
             </div>
 
-            <button type="submit" class="btn btn-success btn-lg btn-block m-t-30 m-b-20"><?php print $form_btn_title ?></button>
-
-            <input type="hidden" name="login" value="<?php echo route('checkout.contact_information'); ?>" />
-
-            <div class="text-center">
-            <b> <?php _e("OR"); ?></b>
-                <br />
-                <br />
-            <a class="btn btn-default m-t-3 m-b-20" href="<?php echo route('checkout.login'); ?>">
-              <i class="mdi mdi-key"></i>  <?php _e("Already have account? Login"); ?>
-            </a>
+            <div class="d-flex align-items-center mt-3">
+                <?php if (!$captcha_disabled): ?>
+                    <module type="captcha" template="checkout_v2"/>
+                <?php endif; ?>
+                <button type="submit" class="btn btn-outline-primary ml-auto px-5"><?php print $form_btn_title ?></button>
             </div>
 
+            <input type="hidden" name="login" value="<?php echo route('checkout.contact_information'); ?>"/>
+
         </form>
-    </div>
     </div>
 <?php else: ?>
     <p class="text-center">
