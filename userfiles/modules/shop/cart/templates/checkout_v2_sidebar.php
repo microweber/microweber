dcf5bcdb79bb014c42bc9c915aa0954f1674f6cd
Nikolay <rachkuw@gmail.com>
2021-04-09 13:21:14 +0300

    update

--- userfiles/modules/shop/cart/templates/checkout_v2_sidebar.php
+++ userfiles/modules/shop/cart/templates/checkout_v2_sidebar.php
@@ -61,13 +61,13 @@ $total = cart_total();
                            <div class="col-xl-2 col-3 ml-xl-0 ml-5">
                                <h6><?php print currency_format($item['price']); ?></h6>
                            </div>
-                           <div class="col-xl-1 col-3 mw-qty-field">
+                           <div class="col-xl-2 col-3 mw-qty-field">
                                <input min=1 type="number" class="form-control input-sm" name="qty" value="<?php print $item['qty'] ?>"  oninput="check_qty(this)" onchange=" mw.cart.qty('<?php print $item['id'] ?>', this.value)" style="width: 70px;"/>
                            </div>
 
 
-                           <div class="col-xl-2 col-4 checkout-v2-remove-icon">
-                               <a data-toggle="tooltip" title="<?php _e("Remove"); ?>" onclick="return confirm(mw.lang('Are you sure you want yo delete this?'))" href="javascript:mw.cart.remove('<?php print $item['id'] ?>');"><i class="checkout-v2-remove-icon mdi mdi-delete-outline text-secondary d-flex justify-content-center justify-content-xl-end" style="font-size: 24px"></i></a>
+                           <div class="col-xl-1 col-4 checkout-v2-remove-icon">
+                               <a data-toggle="tooltip" title="<?php _e("Remove"); ?>" onclick="return confirm(mw.lang('Are you sure you want yo delete this?'))" href="javascript:mw.cart.remove('<?php print $item['id'] ?>');"><i class="checkout-v2-remove-icon mdi mdi-delete-outline text-secondary" style="font-size: 24px"></i></a>
                            </div>
                        </div>
                    <?php endforeach; ?>
