d17b99f716c5726ae1a8e3eeac58532d63cb1863
Nikolay <rachkuw@gmail.com>
2021-04-13 15:57:25 +0300

    update

--- src/MicroweberPackages/Checkout/resources/views/layout.blade.php
+++ src/MicroweberPackages/Checkout/resources/views/layout.blade.php
@@ -9,6 +9,7 @@
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link rel="stylesheet" type="text/css"
           href="https://fonts.googleapis.com/css?family=Lato:300,400,700,900&display=swap">
+
     <script>
         mw.require('icon_selector.js');
         mw.lib.require('bootstrap4');
@@ -116,34 +117,34 @@
 
 <nav class="navbar-expand-lg checkout-v2-navbar navbar-light d-lg-none d-block">
     <div class="row">
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon float-left ml-3"></span>
-        <?php $cart_totals = mw()->cart_manager->totals(); ?>
-        <?php if ($cart_totals): ?>
-        <?php $print_total = cart_total(); ?>
-        <h4 class="checkout-modal-total-label float-right mr-3"><?php _lang("Total"); ?>:<?php print currency_format($print_total); ?></h4>
-        <?php endif; ?>
-    </button>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon float-left ml-3"></span>
+            <?php $cart_totals = mw()->cart_manager->totals(); ?>
+            <?php if ($cart_totals): ?>
+            <?php $print_total = cart_total(); ?>
+            <h4 class="checkout-modal-total-label float-right mr-3"><?php _lang("Total"); ?>:<?php print currency_format($print_total); ?></h4>
+            <?php endif; ?>
+        </button>
 
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
 
-        @hasSection('checkout_sidebar')
-            @yield('checkout_sidebar')
-        @else
-            <div class="checkout-v2-sidebar right-column col-12">
-                <div class="col-lg-10 col-12 checkout-v2-right-column float-lg-left p-xl-5 p-md-3 p-3">
-                    <div class="text-left">
-                        <h6 class="m-t-100"><?php _e("Your order"); ?></h6>
-                        <small class="text-muted d-block mb-2"> <?php _e("List with products"); ?></small>
-                    </div>
+            @hasSection('checkout_sidebar')
+                @yield('checkout_sidebar')
+            @else
+                <div class="checkout-v2-sidebar right-column col-12">
+                    <div class="col-lg-10 col-12 checkout-v2-right-column float-lg-left p-xl-5 p-md-3 p-3">
+                        <div class="text-left">
+                            <h6 class="m-t-100"><?php _e("Your order"); ?></h6>
+                            <small class="text-muted d-block mb-2"> <?php _e("List with products"); ?></small>
+                        </div>
 
-                    <div class="mt-3">
-                        <module type="shop/cart" template="checkout_v2_sidebar" data-checkout-link-enabled="n"/>
+                        <div class="mt-3">
+                            <module type="shop/cart" template="checkout_v2_sidebar" data-checkout-link-enabled="n"/>
+                        </div>
                     </div>
                 </div>
-            </div>
-        @endif
-    </div>
+            @endif
+        </div>
     </div>
 </nav>
 
