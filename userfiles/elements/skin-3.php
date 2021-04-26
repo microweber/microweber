c487d6af6f674180771c93683a23a172f40527a2
Nikolay <rachkuw@gmail.com>
2021-04-26 12:48:29 +0300

    update

--- userfiles/elements/skin-3.php
+++ userfiles/elements/skin-3.php
@@ -1,5 +1,5 @@
 <style>
-    .background-image-holder.parallax {
+    .background-image-holder.mw-elements-parallax {
         position: relative;
         height: 600px;
         max-height: 100vh;
@@ -10,7 +10,7 @@
 </style>
 
 <div data-parallax="" data-background-position="center"">
-    <div class="row background-image-holder parallax text-center rounded text-white d-flex"
+    <div class="row background-image-holder mw-elements-parallax text-center rounded text-white d-flex"
          style="background-image: url('<?php print elements_url() ?>images/image-for-layouts.png');">
         <div class="w-100 align-self-center text-center info-holder allow-drop"></div>
     </div>
