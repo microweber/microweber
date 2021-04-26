04d96eb41080d5e0ef09d7c111bfabb82c2c1baf
Nikolay <rachkuw@gmail.com>
2021-04-26 13:38:47 +0300

    checkout modal css

--- userfiles/modules/shop/cart/templates/modal.php
+++ userfiles/modules/shop/cart/templates/modal.php
@@ -37,7 +37,7 @@ $total = cart_total();
     <?php if (is_array($data) and $data) : ?>
         <?php foreach ($data as $item) :?>
             <div class="form-row checkout-modal-product-list-item align-items-center py-4 pl-4">
-                <div class="col-md-1 col-4">
+                <div class="col-md-2 col-4">
                     <?php if (isset($item['item_image']) and $item['item_image'] != false): ?>
                         <?php $p = $item['item_image']; ?>
                     <?php else: ?>
@@ -48,7 +48,7 @@ $total = cart_total();
                     <?php endif; ?>
                 </div>
 
-                <div class="col-md-6 col-8 ml-md-3">
+                <div class="col-md-5 col-8">
                     <h6 class="mb-1"><?php _e($item['title']) ?></h6>
                     <small class="text-muted mw-order-custom-fields">
                         <?php if (isset($item['custom_fields']) and $item['custom_fields'] != false): ?>
@@ -60,8 +60,8 @@ $total = cart_total();
                 <div class="col-md-2 col-3 ml-md-0 ml-5">
                     <h6><?php print currency_format($item['price']); ?></h6>
                 </div>
-                <div class="col-md-1 col-3 mw-qty-field">
-                    <input min=1 type="number" class="form-control input-sm" name="qty" value="<?php print $item['qty'] ?>"  oninput="check_qty(this)" onchange=" mw.cart.qty('<?php print $item['id'] ?>', this.value)" style="width: 70px;"/>
+                <div class="col-md-2 col-3 mw-qty-field">
+                    <input min=1 type="number" class="form-control input-sm" name="qty" value="<?php print $item['qty'] ?>"  oninput="check_qty(this)" onchange=" mw.cart.qty('<?php print $item['id'] ?>', this.value)"/>
                 </div>
 
 
