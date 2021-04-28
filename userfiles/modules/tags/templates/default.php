14163b7242b1a087bdd9bc89bff6c22c03f7aa71
Nikolay <rachkuw@gmail.com>
2021-04-28 17:39:40 +0300

    tags skins update

--- userfiles/modules/tags/templates/default.php
+++ userfiles/modules/tags/templates/default.php
@@ -11,10 +11,19 @@ description: Default
 */
 ?>
 
+<?php include modules_path() . 'editor' . DS . 'template_settings' . DS . 'generated_vars.php'; ?>
+
 <style>
-    .mw-active-tag {
-        background-color: #5c98ff;
-        border-color: #5c98ff;
+    .mw-tags-button.btn-outline-primary {
+        background-color: <?php echo $less_primary; ?>;
+        border: 1px solid <?php echo $less_primary; ?>;
+        color: <?php echo $less_textLight; ?>;
+    }
+
+    .mw-active-tag-default {
+        background-color: <?php echo $less_dark; ?>!important;
+        border: 1px solid <?php echo $less_dark; ?>!important;
+        color: <?php echo $less_textLight; ?>!important;
     }
 </style>
 
@@ -23,7 +32,7 @@ description: Default
 <?php foreach ($content_tags_data as $tag_item): ?>
     <div class="btn-group tag tag-xs m-1">
         <a href="<?php print $tags_url_base ?><?php print $current_tag == $tag_item['tag_slug'] ? '' : '/tags:'. $tag_item['tag_slug'] ?>">
-            <span class="btn btn-primary btn-sm icon-left no-hover <?php print $current_tag == $tag_item['tag_slug'] ? 'mw-active-tag' : '' ?>"><i class="mdi mdi-tag"></i> <?php print $tag_item['tag_name']; ?></span>
+            <span class="btn mw-tags-button btn-outline-primary rounded-pill btn-sm icon-left no-hover px-3 <?php print $current_tag == $tag_item['tag_slug'] ? 'mw-active-tag-default' : '' ?>"> <?php print $tag_item['tag_name']; ?></span>
         </a>
     </div>
 <?php endforeach; ?>
--- userfiles/modules/tags/templates/skin-1.php
+++ userfiles/modules/tags/templates/skin-1.php
@@ -12,22 +12,18 @@ description: Skin-1
 ?>
 
 
+<style>
+    .mw-active-tag-skin-1 {
+        text-decoration: underline;
+    }
 
+</style>
+<?php $current_tag = url_param('tags'); ?>
 
-
-
-
-<div class="shop-tags">
-    <?php foreach ($content_tags_data as $tag_item): ?>
-        <a href="<?php print $tags_url_base ?>/tags:<?php print $tag_item['tag_slug']; ?>">
-
-
-            #<?php print $tag_item['tag_name']; ?>
-
+<?php foreach ($content_tags_data as $tag_item): ?>
+    <div class="btn-group tag tag-xs m-1">
+        <a href="<?php print $tags_url_base ?><?php print $current_tag == $tag_item['tag_slug'] ? '' : '/tags:'. $tag_item['tag_slug'] ?>">
+            <button class="btn btn-link px-0 <?php print $current_tag == $tag_item['tag_slug'] ? 'mw-active-tag-skin-1' : '' ?>"> #<?php print $tag_item['tag_name']; ?></button>
         </a>
-    <?php endforeach; ?>
-</div>
-
-
-
-
+    </div>
+<?php endforeach; ?>
