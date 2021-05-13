f39bbf300ea6980c9b441559704df1e194e8f6dc
Bozhidar Slaveykov <bobi@microweber.com>
2021-05-13 17:14:29 +0300

    update

--- userfiles/modules/white_label/config.php
+++ userfiles/modules/white_label/config.php
@@ -8,5 +8,7 @@ $config['position'] = "500";
 $config['version'] = 0.3;
 $config['ui_admin'] = true;
 
+$config['settings'] = [];
 
-
+// This will hide this module when its a whitelabel website run
+$config['settings']['hide_on_whitelabel_run'] = true;
