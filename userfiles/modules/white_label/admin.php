0be8a41c1e1240ed964033cd8090bec726e4efdc
Peter Ivanov <peter@microweber.com>
2021-05-14 22:13:13 +0300

    update

--- userfiles/modules/white_label/admin.php
+++ userfiles/modules/white_label/admin.php
@@ -127,7 +127,6 @@ if (isset($params["live_edit"]) and $params["live_edit"]) {
                     form.submit(function () {
                         var url = "<?php print api_url() ?>save_white_label_config"; // the script where you handle the form input.
 
-                        console.log(mw.serializeFields(this));
 
                         $.ajax({
                             type: "POST",
@@ -135,7 +134,8 @@ if (isset($params["live_edit"]) and $params["live_edit"]) {
                             data: mw.serializeFields(this), // serializes the form's elements.
                             success: function (data) {
                                 mw.notification.success("White label saved");
-                            }
+                                mw.trigger('whiteLabelModuleSettingsUpdated', data);
+                             }
                         });
 
                         return false; // avoid to execute the actual submit of the form.
@@ -320,25 +320,49 @@ if (isset($params["live_edit"]) and $params["live_edit"]) {
                             </div>
                         </div>
                     </div>
-
-                    <module type="white_label/whmcs" />
+                    <div id="white_label-whmcs-admin-module-holder">
+                    </div>
 
                 </form>
             </div>
         </div>
 
+      <div>
+          <div class="text-left">
+              <a href="javascript:;" id="white_label-whmcs-admin-module-link" onclick="mw.show_white_label_whmcs_settings();">WHMCS settings</a>
+              <script>
+                  mw.show_white_label_whmcs_settings = function () {
+
+                      mw.load_module( 'white_label/whmcs/admin',"#white_label-whmcs-admin-module-holder");
+                      $("#white_label-whmcs-admin-module-link").hide();
+                  }
+              </script>
+          </div>
+
+          <div class="text-left">
+              <a href="javascript:;" onclick="mw.show_licenses_modal();">My Licenses</a>
+              <script>
+                  mw.show_licenses_modal = function () {
+                      var data = {}
+                      licensesModal = mw.tools.open_module_modal('settings/group/licenses', data, {
+                          title: 'Licenses',
+                          skin: 'simple'
+                      })
+                  }
+              </script>
+          </div>
+      </div>
+
+
         <div class="text-right">
-            <a href="javascript:;" onclick="mw.show_licenses_modal();">My Licenses</a>
-            <script>
-                mw.show_licenses_modal = function () {
-                    var data = {}
-                    licensesModal = mw.tools.open_module_modal('settings/group/licenses', data, {
-                        title: 'Licenses',
-                        skin: 'simple'
-                    })
-                }
-            </script>
+
+            <button form="white_label_settings_holder" type="submit" class="btn btn-lg btn-success btn-save " ><span>Save</span></button>
+
+
         </div>
+
+
+
     </div>
 </div>
 
