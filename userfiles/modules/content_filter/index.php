a09c41d0e4cb0f13c22e17cb6e28eef699c2ab59
Bozhidar Slaveykov <bobi@microweber.com>
2021-05-17 18:24:07 +0300

    update

--- userfiles/modules/content_filter/index.php
+++ userfiles/modules/content_filter/index.php
@@ -1,1 +1,9 @@
-OUAAAAAAAAU!
+<?php
+
+$controller = \Illuminate\Support\Facades\App::make(\MicroweberPackages\ContentFilter\Http\Controllers\ContentFilterController::class);
+
+$request = new \Illuminate\Http\Request();
+$request->merge($params);
+$request->merge($_REQUEST);
+
+echo $controller->index($request);
