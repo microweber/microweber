043a90d1eb5057a3825cd6c120a627f63fa776bf
Bozhidar Slaveykov <bobi@microweber.com>
2021-05-20 10:47:17 +0300

    update

--- userfiles/modules/blog/src/resources/views/index.blade.php
+++ userfiles/modules/blog/src/resources/views/index.blade.php
@@ -3,11 +3,8 @@
     <div class="row">
 
         <div class="col-md-4">
-
-
             {!! $posts->limit(); !!}
             {!! $posts->sort(); !!}
-
         </div>
 
         <div class="col-md-8">
