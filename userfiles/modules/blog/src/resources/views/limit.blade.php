dcacbf235dd36d6227758382417f7d80e330c24d
Bozhidar Slaveykov <bobi@microweber.com>
2021-05-20 10:45:12 +0300

    update

--- userfiles/modules/blog/src/resources/views/limit.blade.php
+++ userfiles/modules/blog/src/resources/views/limit.blade.php
@@ -1,5 +1,5 @@
 <select class="form-control" onchange="if (this.value != '') window.location.href=this.value">
-    <option value="">Select</option>
+    <option value=""><?php _e('Select');?></option>
     @foreach($options as $option)
     <option value="{{$option->link}}" @if($option->active) selected="selected" @endif>{{$option->name}}</option>
     @endforeach
