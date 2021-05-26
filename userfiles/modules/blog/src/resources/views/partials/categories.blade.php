472f5602a89a2f4817722f069092443e4f6c6a1e
Bozhidar Slaveykov <bobi@microweber.com>
2021-05-26 15:17:35 +0300

    update

--- userfiles/modules/blog/src/resources/views/partials/categories.blade.php
+++ userfiles/modules/blog/src/resources/views/partials/categories.blade.php
@@ -1,10 +1,27 @@
-@foreach($categories as $category)
-    <ul style="margin-top:15px;">
-        <li>
-            <a href="?category={{$category->id}}" @if(\Request::get('category', false) == $category->id) style="font-weight:bold;text-decoration:underline;" @endif >{{$category->title}}</a>
-        </li>
-        @if($category->children()->count() > 0)
-            @include('blog::partials.categories_children', ['categories' => $category->children])
-        @endif
-    </ul>
-@endforeach
+<?php
+$randomId = uniqid();
+?>
+
+<div class="card-header">
+    <a href="#" data-toggle="collapse" data-target="#collapse_{{$randomId}}" aria-expanded="true" class="">
+        <h6 class="title"><?php _e('Categories') ?></h6>
+        <i class="fa fa-chevron-down" style="float:right;margin-top:-18px;"></i>
+    </a>
+</div>
+
+<div class="collapse show" id="collapse_{{$randomId}}">
+    <div class="card-body">
+
+        @foreach($categories as $category)
+            <ul style="margin-top:15px;">
+                <li>
+                    <a href="?category={{$category->id}}" @if(\Request::get('category', false) == $category->id) style="font-weight:bold;text-decoration:underline;" @endif >{{$category->title}}</a>
+                </li>
+                @if($category->children()->count() > 0)
+                    @include('blog::partials.categories_children', ['categories' => $category->children])
+                @endif
+            </ul>
+        @endforeach
+        
+    </div>
+</div>
--- userfiles/modules/blog/src/resources/views/partials/tags.blade.php
+++ userfiles/modules/blog/src/resources/views/partials/tags.blade.php
@@ -1,8 +1,20 @@
-<div class="form-group">
-<label><?php _e('Tags'); ?></label>
-    <div class="tags">
-@foreach($tags as $tag)
-    <a href="{{$tag->url}}" @if(\Request::get('tags', false) == $tag->slug) style="border:3px solid #43a90c;" @endif>{{$tag->name}}</a>
-@endforeach
+<?php
+$randomId = uniqid();
+?>
+
+<div class="card-header">
+    <a href="#" data-toggle="collapse" data-target="#collapse_{{$randomId}}" aria-expanded="true" class="">
+        <h6 class="title"><?php _e('Tags') ?></h6>
+        <i class="fa fa-chevron-down" style="float:right;margin-top:-18px;"></i>
+    </a>
 </div>
+
+<div class="collapse show" id="collapse_{{$randomId}}">
+    <div class="card-body">
+        <div class="tags">
+        @foreach($tags as $tag)
+            <a href="{{$tag->url}}" @if(\Request::get('tags', false) == $tag->slug) style="border:3px solid #43a90c;" @endif>{{$tag->name}}</a>
+        @endforeach
+        </div>
+    </div>
 </div>
