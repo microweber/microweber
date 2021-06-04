6602160f01f08b8a6855a75b1e27728e8bf9bf54
Bozhidar Slaveykov <bobi@microweber.com>
2021-06-04 15:45:29 +0300

    update

---
+++ src/MicroweberPackages/Shop/tests/ShopFilterTest.php
@@ -0,0 +1,13 @@
+<?php
+namespace MicroweberPackages\Shop\tests;
+
+use MicroweberPackages\Core\tests\TestCase;
+
+class ShopFilterTest extends TestCase
+{
+    public function testGetProducts()
+    {
+
+        echo 1;
+    }
+}
