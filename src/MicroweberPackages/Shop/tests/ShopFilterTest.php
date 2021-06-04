2865098bf9c83e53e0f01309f012a07db749151b
Bozhidar Slaveykov <bobi@microweber.com>
2021-06-04 16:15:24 +0300

    update

--- src/MicroweberPackages/Shop/tests/ShopFilterTest.php
+++ src/MicroweberPackages/Shop/tests/ShopFilterTest.php
@@ -3,22 +3,31 @@ namespace MicroweberPackages\Shop\tests;
 
 use Illuminate\Support\Facades\App;
 use Illuminate\Http\Request;
+use MicroweberPackages\Content\Content;
 use MicroweberPackages\Core\tests\TestCase;
+use MicroweberPackages\Page\Models\Page;
 use MicroweberPackages\Shop\Http\Controllers\ShopController;
 
 class ShopFilterTest extends TestCase
 {
-    public function testCreateShopPage()
-    {
-
-    }
-
     public function testGetProducts()
     {
+        $newPage = new Page();
+        $newPage->title = uniqid();
+        $newPage->is_shop = 1;
+        $newPage->content_type = 'page';
+        $newPage->subtype = 'dynamic';
+        $newPage->save();
+
+        $shopPage = Page::where('content_type', 'page')
+            ->where('subtype','dynamic')
+            ->where('is_shop', 1)
+            ->first();
+        
         $controller = App::make(ShopController::class);
 
         $request = new Request();
-        $request->merge([]);
+        $request->merge(['id'=>$shopPage->id]);
 
         $html = $controller->index($request);
 
