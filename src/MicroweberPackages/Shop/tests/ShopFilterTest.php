570b2186c63d6555711532a0cf0c7d94ede14c81
Bozhidar Slaveykov <bobi@microweber.com>
2021-06-04 15:52:01 +0300

    add unit test to shop fitlers

--- src/MicroweberPackages/Shop/tests/ShopFilterTest.php
+++ src/MicroweberPackages/Shop/tests/ShopFilterTest.php
@@ -1,13 +1,27 @@
 <?php
 namespace MicroweberPackages\Shop\tests;
 
+use Illuminate\Support\Facades\App;
+use Illuminate\Http\Request;
 use MicroweberPackages\Core\tests\TestCase;
+use MicroweberPackages\Shop\Http\Controllers\ShopController;
 
 class ShopFilterTest extends TestCase
 {
     public function testGetProducts()
     {
+        $controller = App::make(ShopController::class);
 
-        echo 1;
+        $request = new Request();
+        $request->merge([]);
+
+        $html = $controller->index($request);
+
+        $findJs = (strpos($html, 'filter.js') !== false);
+        $this->assertTrue($findJs);
+        
+        $findCss = (strpos($html, 'filter.css') !== false);
+        $this->assertTrue($findCss);
+
     }
 }
