0958916d9979c2992b9bf37aa3e402f485abadf4
Bozhidar Slaveykov <selfworksbg@gmail.com>
2021-06-09 17:41:18 +0300

    seperate diffrent custom fields from shop settings

--- src/MicroweberPackages/Shop/routes/web.php
+++ src/MicroweberPackages/Shop/routes/web.php
@@ -1,2 +1,11 @@
 <?php
 
+Route::name('admin.shop.filter.')
+    ->prefix(ADMIN_PREFIX)
+    ->middleware(['admin'])
+    ->namespace('\MicroweberPackages\Shop\Http\Controllers')
+    ->group(function () {
+
+        Route::post('get-custom-fields-table', 'LiveEditAdminController@getCustomFieldsTableFromPage')->name('get-custom-fields-table');
+
+    });
