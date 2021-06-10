07dcc1f8775352db11a07c0221c243a07594c239
Bozhidar Slaveykov <selfworksbg@gmail.com>
2021-06-10 18:23:11 +0300

    add predefined colors

--- userfiles/modules/microweber/custom_fields/color_settings.php
+++ userfiles/modules/microweber/custom_fields/color_settings.php
@@ -1,25 +1,73 @@
+
 <?php include('settings_header.php'); ?>
 
 <script type="text/javascript">
 
    mw.lib.require('colorpicker');
 
-    color = mw.colorPicker({
-        element: '#value<?php print $rand; ?>',
+  /*  color = mw.colorPicker({
+        element: '#color<?php print $rand; ?>',
         position: 'bottom-left',
         onchange: function (color) {
-            $('#value<?php print $rand; ?>').val(color).css('background', color);
-            $('#value<?php print $rand; ?>').trigger('change');
+            $('#color<?php print $rand; ?>').val(color).css('background', color);
+            $('#color<?php print $rand; ?>').trigger('change');
         }
+    });
+*/
+    $(document).ready(function () {
+
+        $('.predefined-colors .color').click(function () {
+           $('#value<?php print $rand; ?>').val($(this).data('name'));
+           $('#value<?php print $rand; ?>').css('background', $(this).data('hex')); 
+        });
+
     });
 
 </script>
 
-<div class="custom-field-settings-values">
-  <label class="control-label" for="value<?php print $rand; ?>"><?php _e("Color"); ?></label>
-   <small class="text-muted d-block mb-2"><?php _e('This attribute specifies the color');?></small>
+<style>
+    .predefined-colors {
+        display: block;
+        width: 100%;
+        clear: both;
+    }
+    .predefined-colors .color {
+        height: 50px;
+        width: 50px;
+        font-size:9px;
+        padding: 2px;
+        float:left;
+        margin-right: 2px;
+        opacity: 0.9;
+        cursor: pointer;
+        border: 2px solid #fff;
+    }
+    .predefined-colors .color:hover {
+        opacity: 1;
+        border: 2px solid #00000052;
+    }
+</style>
 
-   <input type="text" class="form-control" name="value" autocomplete="off" value="<?php print ($data['value']) ?>" id="value<?php print $rand; ?>">
+<div class="custom-field-settings-values">
+
+    <div class="form-group">
+      <label class="control-label" for="value<?php print $rand; ?>"><?php _e("Color"); ?></label>
+       <small class="text-muted d-block mb-2"><?php _e('This attribute specifies the color');?></small>
+       <input type="text" class="form-control" name="value" autocomplete="off" value="<?php print ($data['value']) ?>" id="value<?php print $rand; ?>">
+    </div>
+
+    <div class="predefined-colors">
+        <?php
+        foreach (mw()->ui->predefined_colors() as $color) {
+        ?>
+        <div class="color" style="background: #<?php echo $color['hex']; ?>" data-hex="<?php echo $color['hex']; ?>" data-name="<?php echo $color['name']; ?>">
+           <!-- <?php echo $color['name']; ?> -->
+        </div>
+        <?php
+        }
+        ?>
+    </div>
+
    <?php print $savebtn; ?>
 </div>
 
