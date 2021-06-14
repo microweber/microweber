1dfe8b03cfcfb0873355b8af91255043a8a78319
Bozhidar Slaveykov <bobi@microweber.com>
2021-06-14 11:26:51 +0300

    update

--- userfiles/modules/microweber/custom_fields/color_settings.php
+++ userfiles/modules/microweber/custom_fields/color_settings.php
@@ -31,6 +31,30 @@
             $('#value<?php print $rand; ?>').trigger('change');
         });
 
+        $('.js-color-input-method-picker').hide();
+        $('.js-color-input-method-image-upload').hide();
+        $('.js-color-input-method-color-palette').hide();
+
+        $('.js-color-input-method-type').change(function() {
+
+            var methodType = $(this).val();
+            if (methodType == 'picker') {
+                $('.js-color-input-method-picker').show();
+                $('.js-color-input-method-image-upload').hide();
+                $('.js-color-input-method-color-palette').hide();
+            }
+            if (methodType == 'palette') {
+                $('.js-color-input-method-picker').hide();
+                $('.js-color-input-method-image-upload').hide();
+                $('.js-color-input-method-color-palette').show();
+            }
+            if (methodType == 'image') {
+                $('.js-color-input-method-picker').hide();
+                $('.js-color-input-method-image-upload').show();
+                $('.js-color-input-method-color-palette').hide();
+            }
+        });
+
     });
 
 </script>
@@ -40,20 +64,37 @@
       <label class="control-label" for="value<?php print $rand; ?>"><?php _e("Color"); ?></label>
        <small class="text-muted d-block mb-2"><?php _e('This attribute specifies the color');?></small>
 
-       <input type="text" class="form-control" name="value" autocomplete="off" value="<?php print ($data['value']) ?>" id="value<?php print $rand; ?>">
-<!--
-        <select class="selectpicker" data-live-search="true" style="width:100%;">
-            <?php
-/*            foreach (mw()->ui->predefined_colors() as $color) {
-                */?> 
-                 <option data-hex="<?php /*echo $color['hex']; */?>" data-name="<?php /*echo $color['name']; */?>" data-tokens="<?php /*echo $color['name']; */?>" style="background: #<?php /*echo $color['hex']; */?>;">
-                     <?php /*echo $color['name']; */?>
-                 </option>
-                <?php
-/*            }
-            */?>
-        </select>
--->
+        <div class="row">
+            <div class="col-4">
+            <select class="form-control js-color-input-method-type">
+                <option value="picker">Picker</option>
+                <option value="palette">Palette</option>
+                <option value="image">Image</option>
+            </select>
+            </div>
+              <div class="col-8">
+                  <div class="js-color-input-method-picker">
+                      <input type="text" class="form-control" name="value" autocomplete="off" value="<?php print ($data['value']) ?>" id="value<?php print $rand; ?>">
+                  </div>
+                  <div class="js-color-input-method-color-palette">
+                      <select class="selectpicker" data-live-search="true" style="width:100%;">
+                          <?php
+                          foreach (mw()->ui->predefined_colors() as $color) {
+                         ?>
+                          <option data-hex="<?php echo $color['hex']; ?>" data-name="<?php echo $color['name']; ?>" data-tokens="<?php echo $color['name']; ?>" style="background: #<?php echo $color['hex']; ?>;">
+                              <?php echo $color['name']; ?>
+                          </option>
+                          <?php
+                          }
+                          ?>
+                      </select>
+                  </div>
+                  <div class="js-color-input-method-image-upload">
+                    
+                  </div>
+            </div>
+        </div>
+
        <input type="hidden" name="options[hex]" value="<?php print ($data['value']) ?>" id="color<?php print $rand; ?>">
     </div>
     <div class="custom-field-settings-values">
