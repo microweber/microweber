48877447f68b25da73b917e762a7ec6b447e9595
Nikolay <rachkuw@gmail.com>
2021-06-16 17:23:09 +0300

    update

--- src/MicroweberPackages/Blog/resources/views/partials/sort.blade.php
+++ src/MicroweberPackages/Blog/resources/views/partials/sort.blade.php
@@ -1,6 +1,6 @@
 <div class="form-group d-flex">
     <label class="control-label align-self-center mr-2"><?php _e('Sort');?></label>
-    <select class="selectpicker form-control js-filter-change-sort">
+    <select class="form-control js-filter-change-sort">
         <option disabled="disabled"><?php _e('Select');?></option>
         @foreach($options as $option)
             <option data-sort="{{$option->sort}}" data-order="{{$option->order}}" @if($option->active) selected="selected" @endif>{{$option->name}}</option>
