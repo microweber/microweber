98e038bdb9bd6c1fb035e46c88dc171ef3a68ce9
Bozhidar Slaveykov <bobi@microweber.com>
2021-06-18 10:27:10 +0300

    update

---
+++ src/MicroweberPackages/App/Utils/Parser/Parser.php
@@ -0,0 +1,42 @@
+<?php
+namespace MicroweberPackages\App\Utils\Parser;
+
+
+use Illuminate\View\View;
+
+final class Parser extends ParserModule
+{
+    use ParserHelperTrait;
+
+    public function process($layout, $options = false, $coming_from_parent = false, $coming_from_parent_id = false, $previous_attrs = false)
+    {
+        echo $this->recursive_parse_modules($layout);
+    }
+
+    public function replace_url_placeholders($layout)
+    {
+        if (defined('TEMPLATE_URL')) {
+            $replaces = array(
+                '{TEMPLATE_URL}',
+                '{THIS_TEMPLATE_URL}',
+                '{DEFAULT_TEMPLATE_URL}',
+                '%7BTEMPLATE_URL%7D',
+                '%7BTHIS_TEMPLATE_URL%7D',
+                '%7BDEFAULT_TEMPLATE_URL%7D',
+            );
+
+            $replaces_vals = array(
+                TEMPLATE_URL,
+                THIS_TEMPLATE_URL,
+                DEFAULT_TEMPLATE_URL,
+                TEMPLATE_URL,
+                THIS_TEMPLATE_URL,
+                DEFAULT_TEMPLATE_URL
+            );
+
+            $layout = str_replace_bulk($replaces, $replaces_vals, $layout);
+        }
+
+        return $layout;
+    }
+}
---
+++ src/MicroweberPackages/App/Utils/Parser/ParserModule.php
@@ -0,0 +1,74 @@
+<?php
+namespace MicroweberPackages\App\Utils\Parser;
+
+class ParserModule {
+
+    public function recursive_parse_modules($layout)
+    {
+        // Check first block to parse
+        $module_tags = $this->_preg_match_module_tags($layout);
+        if (!$module_tags) {
+            return false;
+        }
+
+        // Execute modules
+        foreach($module_tags as $module_tag) {
+            $module_attributes = $this->_extract_tag_attributes($module_tag);
+            $render_module = $this->execute_module($module_tag, $module_attributes);
+            $layout = $this->_str_replace_first($module_tag, $render_module, $layout);
+        }
+
+        // Check next block to parse
+        $module_tags = $this->_preg_match_module_tags($layout);
+        if ($module_tags) {
+            $layout = $this->recursive_parse_modules($layout);
+        }
+
+        return $layout;
+    }
+
+    public function execute_module($module_tag, $module_attributes)
+    {
+        if (!isset($module_attributes['type'])) {
+            // If you want to run this module, you must set the attribute type
+            return false;
+        }
+
+        // Find the index of the module file
+        $module_index_file = modules_path() . $module_attributes['type'] . DS . 'index.php';
+        if (!is_file($module_index_file)) {
+            return false;
+        }
+
+        // Execute module in ob start
+        ob_start();
+
+        $config = [];
+        $config['module'] = $module_attributes['type'];
+
+        $params = $module_attributes;
+        $params['id'] = uniqid();
+        $params['module'] = $module_attributes['type'];
+
+        if (isset($module_attributes['id'])) {
+            $params['id'] = $module_attributes['id'];
+        }
+
+        include($module_index_file);
+
+        $module_rendered = ob_get_clean();
+        // Module is rendered
+
+        // Append executed module in html
+        $attributes = '';
+        foreach($module_attributes as $attribute_key=>$attribute_value) {
+            $attributes .= " ".$attribute_key . '="'.$attribute_value.'"';
+        }
+
+        $html_output = '<div'.$attributes.' mw_module="true">';
+        $html_output .= trim($module_rendered);
+        $html_output .= '</div>';
+
+        return $html_output;
+    }
+}
