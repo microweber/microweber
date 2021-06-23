fbf0c5f4e9465820ea3bded755cf0c172d97dabb
Bozhidar Slaveykov <bobi@microweber.com>
2021-06-23 17:23:51 +0300

    update

--- src/MicroweberPackages/Option/Traits/ModuleOptionTrait.php
+++ src/MicroweberPackages/Option/Traits/ModuleOptionTrait.php
@@ -7,7 +7,6 @@ use MicroweberPackages\Option\Models\ModuleOption;
 trait ModuleOptionTrait {
 
     public $memoryModuleOptionGroup = [];
-    public $memoryModuleOptionGroupPrefix = [];
 
     public function getModuleOptions($optionGroup)
     {
@@ -16,42 +15,23 @@ trait ModuleOptionTrait {
         }
 
         if ($optionGroup) {
-
-            $this->memoryModuleOptionGroup[$optionGroup] = [];
-
-            $optionGroupShared = $optionGroup;
-
-            // This will be explode the first symbols of the modules option group
-            $optionGroupPrefix = explode('--', $optionGroup);
-            if (isset($optionGroupPrefix[0]) && !empty($optionGroupPrefix[0])) {
-                $optionGroupShared = $optionGroupPrefix[0];
-            }
-
-            if (isset($this->memoryModuleOptionGroupPrefix[$optionGroupShared])) {
-                $allOptions = $this->memoryModuleOptionGroupPrefix[$optionGroupShared];
-            } else {
-                $allOptions = ModuleOption::where('option_group','LIKE', '%' . $optionGroupShared. '%')->get()->toArray();
-                $this->memoryModuleOptionGroupPrefix[$optionGroupShared] = $allOptions;
-            }
-
-            foreach ($allOptions as $option) {
-                $this->memoryModuleOptionGroup[$option['option_group']][] = $option;
-            }
-
-            return $this->memoryModuleOptionGroup[$optionGroup];
+            $allOptions = ModuleOption::select(['id','option_key','option_group','option_value'])->where('option_group', $optionGroup)->get()->toArray();
+            $this->memoryModuleOptionGroup[$optionGroup] = $allOptions;
+            return $allOptions;
         }
 
         return false;
     }
 
-    public function getModuleOption($optionKey, $optionGroup, $returnFull)
+    public function getModuleOption($optionKey, $optionGroup, $returnFull = false)
     {
         if (isset($this->memoryModuleOptionGroup[$optionGroup])) {
             return $this->getOptionFromOptionsArray($optionKey, $this->memoryModuleOptionGroup[$optionGroup], $returnFull);
         }
 
-        if ($optionGroup) {
-            $allOptions = $this->getModuleOptions($optionGroup);
+        if ($optionGroup) { 
+            $allOptions = ModuleOption::select(['id','option_key','option_group','option_value'])->where('option_group', $optionGroup)->get()->toArray();
+            $this->memoryModuleOptionGroup[$optionGroup] = $allOptions;
             return $this->getOptionFromOptionsArray($optionKey, $allOptions, $returnFull);
         }
 
