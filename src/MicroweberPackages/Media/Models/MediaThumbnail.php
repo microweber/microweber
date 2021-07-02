8e51a0495cec51a1bbcb38670461b8fab291101c
Bozhidar Slaveykov <50577633+bobimicroweber@users.noreply.github.com>
2021-07-02 14:08:40 +0300

    update

--- src/MicroweberPackages/Media/Models/MediaThumbnail.php
+++ src/MicroweberPackages/Media/Models/MediaThumbnail.php
@@ -5,20 +5,16 @@ use Illuminate\Database\Eloquent\Model;
 use MicroweberPackages\Database\Casts\ReplaceSiteUrlCast;
 use MicroweberPackages\Database\Traits\CacheableQueryBuilderTrait;
 
-class ThumbnailTemp extends Model
+class MediaThumbnail extends Model
 {
     use CacheableQueryBuilderTrait;
 
-    public $table = 'media';
+    public $table = 'media_thumbnails';
 
     protected $guarded = ['id'];
 
     protected $casts = [
         'image_options' => 'json',
         'filename' => ReplaceSiteUrlCast::class, //Casts like that: http://lorempixel.com/400/200/ =>  {SITE_URL}400/200/
-    ];
-
-    protected $attributes = [
-        'media_type' => 'media_tn_temp',
     ];
 }
