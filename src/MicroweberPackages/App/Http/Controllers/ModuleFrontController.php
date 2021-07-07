8a3093efe81948e0e77f15117b50afd085c42afa
Bozhidar Slaveykov <50577633+bobimicroweber@users.noreply.github.com>
2021-07-07 16:28:35 +0300

    update

--- src/MicroweberPackages/App/Http/Controllers/ModuleFrontController.php
+++ src/MicroweberPackages/App/Http/Controllers/ModuleFrontController.php
@@ -8,12 +8,14 @@ use MicroweberPackages\Option\Models\Option;
 class ModuleFrontController
 {
     public $viewData = [];
+    public $viewInstance = [];
     public $moduleParams = [];
     public $moduleOptions = [];
     public $moduleConfig = [];
 
     public function setModuleParams($params)
     {
+        $this->viewInstance = view();
         $this->moduleParams = $params;
         $this->moduleOptions = Option::where('option_group', $this->moduleParams['id'])->get();
     }
@@ -27,14 +29,13 @@ class ModuleFrontController
     {
         $moduleTemplate = get_option('data-template', $this->moduleParams['id']);
         // if (isset($this->moduleParams['default-template'])) {
-      //       $defaultTemplate = $this->moduleParams['default-template'];
-       //  }
-        view()->getFinder()->flush();
+        //       $defaultTemplate = $this->moduleParams['default-template'];
+        //  }
 
         if ($moduleTemplate != false) {
             $templateFile = module_templates($this->moduleConfig['module'], $moduleTemplate);
         } else {
-            $templateFile = module_templates($this->moduleConfig['module'], template_name());
+            $templateFile = module_templates($this->moduleConfig['module'], 'default');
         }
 
         if ($templateFile) {
@@ -42,38 +43,38 @@ class ModuleFrontController
             if (is_dir($templateDir)) {
 
                 $defaultDir = dirname($templateDir) . DS . 'default';
-                $defaultDir2 = mw_root_path().'/src/MicroweberPackages/Shop/resources/views';
                 if (is_dir($defaultDir)) {
+                    $this->viewInstance->addNamespace($this->moduleConfig['module'], $defaultDir);
                     view()->addNamespace($this->moduleConfig['module'], $defaultDir);
-                    view()->addNamespace($this->moduleConfig['module'], $defaultDir2);
                 }
 
-                if (is_dir($templateDir)) {
-                    view()->replaceNamespace($this->moduleConfig['module'], $templateDir);
-                    view()->addNamespace($this->moduleConfig['module'], $defaultDir);
-                    view()->addNamespace($this->moduleConfig['module'], $defaultDir2);
-                }
-
+                $this->viewInstance->replaceNamespace($this->moduleConfig['module'], $templateDir);
+                view()->replaceNamespace($this->moduleConfig['module'], $templateDir);
             }
         }
     }
 
     public function view($view = false, $data = [], $return = false)
     {
+        $this->viewInstance->getFinder()->flush();
         view()->getFinder()->flush();
 
         $this->viewData = array_merge($this->viewData, $data);
 
         $this->viewData['params'] = $this->moduleParams;
         $this->viewData['config'] = $this->moduleConfig;
-//dd($view);
+
         if (strpos($view, '::') !== false) {
+            return $this->viewInstance->make($view, $this->viewData);
             return view()->make($view, $this->viewData);
         } else {
             if ($view) {
-               return view()->make($this->moduleConfig['module'] . '::' . $view, $this->viewData);
+                return $this->viewInstance->make($this->moduleConfig['module'] . '::' . $view, $this->viewData);
+                return view()->make($this->moduleConfig['module'] . '::' . $view, $this->viewData);
             }
             // return view($this->moduleConfig['module'] . '::' . no_ext(basename($templateFile)), $this->viewData);
         }
     }
 }
+
+
