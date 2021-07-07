b3c72ab3d157c58d97de69a1224c41f30689c7b7
Bozhidar Slaveykov <50577633+bobimicroweber@users.noreply.github.com>
2021-07-07 11:32:44 +0300

    update

--- userfiles/modules/microweber/custom_fields/placeholder_settings.php
+++ userfiles/modules/microweber/custom_fields/placeholder_settings.php
@@ -6,3 +6,22 @@
         <input type="text" class="form-control" name="placeholder" value="<?php echo $data['placeholder']; ?>" />
     </div>
 </div>
+
+
+<?php if (isset($settings['show_placeholder'])): ?>
+<div class="custom-field-settings-show-placeholder">
+    <div class="d-flex">
+        <div class="mw-custom-field-form-controls p-0">
+            <label class="mw-ui-check">
+                <input type="hidden" value="false" name="options[show_placeholder]">
+                <input type="checkbox" class="custom-control-input" name="options[show_placeholder]" id="custom_field_show_placeholder<?php print $rand; ?>" value="true" <?php if ($settings['show_placeholder'] == 'true'): ?> checked="checked"  <?php endif; ?> >
+                <span></span>
+                <span></span>
+            </label>
+            <span class="align-self-center col-6 pl-0"><?php _e('Show placeholder'); ?></span>
+        </div>
+    </div>
+    <small class="text-muted d-block mb-2"><?php _e('Display the placeholder inside in field');?></small>
+</div>
+<br />
+<?php endif; ?>
