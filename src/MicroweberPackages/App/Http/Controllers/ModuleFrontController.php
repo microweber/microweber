287a846c0f03b8162d07c5d0973cd859d01d38d9
Bozhidar Slaveykov <50577633+bobimicroweber@users.noreply.github.com>
2021-07-07 16:29:55 +0300

    update

--- src/MicroweberPackages/App/Http/Controllers/ModuleFrontController.php
+++ src/MicroweberPackages/App/Http/Controllers/ModuleFrontController.php
@@ -8,14 +8,12 @@ use MicroweberPackages\Option\Models\Option;
 class ModuleFrontController
 {
     public $viewData = [];
-    public $viewInstance = [];
     public $moduleParams = [];
     public $moduleOptions = [];
     public $moduleConfig = [];
 
     public function setModuleParams($params)
     {
-        $this->viewInstance = view();
         $this->moduleParams = $params;
         $this->moduleOptions = Option::where('option_group', $this->moduleParams['id'])->get();
     }
@@ -44,11 +42,9 @@ class ModuleFrontController
 
                 $defaultDir = dirname($templateDir) . DS . 'default';
                 if (is_dir($defaultDir)) {
-                    $this->viewInstance->addNamespace($this->moduleConfig['module'], $defaultDir);
                     view()->addNamespace($this->moduleConfig['module'], $defaultDir);
                 }
 
-                $this->viewInstance->replaceNamespace($this->moduleConfig['module'], $templateDir);
                 view()->replaceNamespace($this->moduleConfig['module'], $templateDir);
             }
         }
@@ -56,7 +52,6 @@ class ModuleFrontController
 
     public function view($view = false, $data = [], $return = false)
     {
-        $this->viewInstance->getFinder()->flush();
         view()->getFinder()->flush();
 
         $this->viewData = array_merge($this->viewData, $data);
@@ -65,16 +60,12 @@ class ModuleFrontController
         $this->viewData['config'] = $this->moduleConfig;
 
         if (strpos($view, '::') !== false) {
-            return $this->viewInstance->make($view, $this->viewData);
             return view()->make($view, $this->viewData);
         } else {
             if ($view) {
-                return $this->viewInstance->make($this->moduleConfig['module'] . '::' . $view, $this->viewData);
                 return view()->make($this->moduleConfig['module'] . '::' . $view, $this->viewData);
             }
             // return view($this->moduleConfig['module'] . '::' . no_ext(basename($templateFile)), $this->viewData);
         }
     }
 }
-
-
