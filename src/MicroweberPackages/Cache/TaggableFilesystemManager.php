48c00e1ba0b9a8ce754afe04a1eaf8b6ccad2933
Peter Ivanov <peter@microweber.com>
2021-07-07 12:46:30 +0300

    update

--- src/MicroweberPackages/Cache/TaggableFilesystemManager.php
+++ src/MicroweberPackages/Cache/TaggableFilesystemManager.php
@@ -3,6 +3,7 @@
 namespace MicroweberPackages\Cache;
 
 use Illuminate\Filesystem\Filesystem;
+use MicroweberPackages\Cache\CacheFileHandler\MemoryCacheFileHandler;
 
 class TaggableFilesystemManager extends Filesystem
 {
@@ -10,5 +11,13 @@ class TaggableFilesystemManager extends Filesystem
     public $cachedDataMemory = [];
     public $tagMapCacheMemory = [];
     public $tagMapPathsCacheMemory = [];
+    public $cacheHandler = null;
+    public function __construct()
+    {
+
+        //$this->cacheHandler = new CacheFileHandler();
+        $this->cacheHandler = new MemoryCacheFileHandler();
+
+    }
 
 }
