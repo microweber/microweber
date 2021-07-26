2706041633e0af003689095415f32104ddb2d6d3
Peter Ivanov <peter@microweber.com>
2021-07-26 18:27:32 +0300

    update

--- userfiles/modules/admin/notifications/notif_form_entry.php
+++ userfiles/modules/admin/notifications/notif_form_entry.php
@@ -40,12 +40,6 @@ if (isset($item['created_by'])) {
 
 <script>
     $( document ).ready(function() {
-
-        $('.collapse', '.js-form-entry-<?php print $item_id ?>').collapse({
-                toggle: false
-        });
-
-
         $('.collapse', '.js-form-entry-<?php print $item_id ?>').on('shown.bs.collapse', function () {
             $('.js-form-entry-<?php print $item_id ?>').prop('disabled',true).removeAttr('data-toggle');
         });
