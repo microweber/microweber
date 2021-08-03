474387cc2cfb169202a5c968f43a9cbe53304dc3
Nikolay <rachkuw@gmail.com>
2021-08-03 11:51:59 +0300

    update

---
+++ userfiles/modules/custom_fields/templates/bootstrap5_flex/time.php
@@ -0,0 +1,40 @@
+<div class="col-<?php echo $settings['field_size']; ?>">
+	<div class="mb-3 d-flex">
+
+        <?php if($settings['show_label']): ?>
+			<label class="control-label me-2 align-self-center mb-0">
+				<?php echo $data['name']; ?>
+				<?php if ($settings['required']): ?>
+					<span style="color: red;">*</span>
+				<?php endif; ?>
+			</label>
+        <?php endif; ?>
+
+        <input type="text" class="form-control js-bootstrap4-timepicker" <?php if ($settings['required']): ?>required="true"<?php endif; ?> data-custom-field-id="<?php echo $data['id']; ?>" name="<?php echo $data['name']; ?>" value="<?php echo $data['value']; ?>" placeholder="<?php echo $data['placeholder']; ?>" autocomplete="off"/>
+        <div class="valid-feedback"><?php _e('Success! You\'ve done it.'); ?></div>
+        <div class="invalid-feedback"><?php _e('Error! The value is not valid.'); ?></div>
+
+        <?php if ($data['help']): ?>
+            <small class="form-text text-muted"><?php echo $data['help']; ?></small>
+        <?php endif; ?>
+	</div>
+</div>
+
+<script>
+    mw.lib.require("bootstrap_datetimepicker");
+</script>
+
+<script type="text/javascript">
+    $(document).ready(function () {
+        $('.js-bootstrap4-timepicker').datetimepicker({
+            pickDate: false,
+            minuteStep: 15,
+            pickerPosition: 'bottom-right',
+            format: 'HH:ii p',
+            autoclose: true,
+            showMeridian: true,
+            startView: 1,
+            maxView: 1,
+        });
+    });
+</script>
