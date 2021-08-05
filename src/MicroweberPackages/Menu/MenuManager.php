d869a2581788caa9eff4f8d83cf5affac4e1aec5
Bozhidar Slaveykov <selfworksbg@gmail.com>
2021-08-05 14:12:21 +0300

    update

--- src/MicroweberPackages/Menu/MenuManager.php
+++ src/MicroweberPackages/Menu/MenuManager.php
@@ -185,46 +185,7 @@ class MenuManager
 
     public function get_menus($params = false)
     {
-        $table = $this->tables['menus'];
-
-        $params2 = array();
-        if ($params == false) {
-            $params = array();
-        }
-        if (is_string($params)) {
-            $params = parse_str($params, $params2);
-            $params = $params2;
-        }
-
-        //$table = MODULE_DB_SHOP_ORDERS;
-        $params['table'] = $table;
-        $params['item_type'] = 'menu';
-    if(is_live_edit()){
-       $params['no_cache'] = 1; // If remove this we mess up menu auto creating
-        //dd($params);
-    }
-
-        //$params['debug'] = 'menu';
-        $menus = $this->app->database_manager->get($params);
-
-        if (!empty($menus)) {
-            return $menus;
-        } else {
-            if (!defined('MW_MENU_IS_ALREADY_MADE_ONCE')) {
-                if (isset($params['make_on_not_found']) and ($params['make_on_not_found']) == true and isset($params['title'])) {
-                    $check  = $this->app->database_manager->get('no_cache=1&title=' . $params['title']);
-                     if(!$check){
-                        $new_menu = $this->menu_create('title=' . $params['title']);
-                        $params['id'] = $new_menu;
-                        $menus = $this->app->database_manager->get($params);
-                    }
-                }
-                define('MW_MENU_IS_ALREADY_MADE_ONCE', true);
-            }
-        }
-        if (!empty($menus)) {
-            return $menus;
-        }
+        return app()->menu_repository->getMenus($params);
     }
 
     public function menu_tree($menu_id, $maxdepth = false, $show_images = false)
