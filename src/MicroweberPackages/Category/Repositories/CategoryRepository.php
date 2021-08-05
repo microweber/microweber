b0930e5b0cc33ed54e52f99a2490485fb669c1da
Bozhidar Slaveykov <selfworksbg@gmail.com>
2021-08-05 14:39:45 +0300

    update

---
+++ src/MicroweberPackages/Category/Repositories/CategoryRepository.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace MicroweberPackages\Category\Repositories;
+
+use MicroweberPackages\Category\Models\Category;
+use MicroweberPackages\Repository\MicroweberQuery;
+use MicroweberPackages\Repository\Repositories\AbstractRepository;
+
+class CategoryRepository extends AbstractRepository
+{
+
+    /**
+     * Specify Model class name
+     *
+     * @return string
+     */
+    public $model = Category::class;
+
+
+
+    
+}
