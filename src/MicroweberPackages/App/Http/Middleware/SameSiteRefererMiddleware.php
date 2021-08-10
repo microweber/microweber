f071bd097edcb99401bc9cf94fde996f9efe2b8c
Peter Ivanov <peter@microweber.com>
2021-08-10 15:49:49 +0300

    update

---
+++ src/MicroweberPackages/App/Http/Middleware/SameSiteRefererMiddleware.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace MicroweberPackages\App\Http\Middleware;
+
+use Illuminate\Support\Str;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class SameSiteRefererMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @param  \Closure $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next, $guard = null)
+    {
+        $full_url = $request->headers->get('referer');
+
+        $result = Str::startsWith($full_url, site_url());
+
+        if(!$result){
+            $error = 'You are not allowed to make requests from this address';
+            return abort(403, $error);
+
+
+        }
+        return $next($request);
+
+    }
+
+
+}
+
