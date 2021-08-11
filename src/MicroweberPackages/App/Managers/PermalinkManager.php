cda9822c0ba8c6b644a5764a15e390f2c382bcc2
Peter Ivanov <peter@microweber.com>
2021-08-11 14:10:05 +0300

    update

--- src/MicroweberPackages/App/Managers/PermalinkManager.php
+++ src/MicroweberPackages/App/Managers/PermalinkManager.php
@@ -248,7 +248,9 @@ class PermalinkManager
             }
 
             $selected_cat = get_category_by_id($selected_cat['id']);
+            if($selected_cat and isset($selected_cat['url'])){
             $slug = $selected_cat['url'];
+            }
         }
 
         return $slug;
