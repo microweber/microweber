348bd587c9efcc1468a1be168ee5c2d99b72c2e2
Bozhidar Slaveykov <50577633+bobimicroweber@users.noreply.github.com>
2021-08-13 14:28:22 +0300

    update

--- routes/web.php
+++ routes/web.php
@@ -37,7 +37,7 @@ Route::get('mw-test-api', function () {
 
     $x =  get_content('id='. $saved_id);*/
 
-    $get = \MicroweberPackages\Content\Content::filter(['tags'=>'FELICHITA'])->get();
+    $get = \MicroweberPackages\Content\Content::filter(['tags'=>'skype'])->get();
     //$get = get_content('keyword=kw61151fb832261');
 
     dump($get->toArray());
