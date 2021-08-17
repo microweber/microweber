dab42f624737cd64aa018521e03182572cfe08f0
raikov <raikov@ymail.com>
2021-08-17 14:47:36 +0300

    module: before-after improvements

--- userfiles/modules/editor/stylesheet_compiler/index.php
+++ userfiles/modules/editor/stylesheet_compiler/index.php
@@ -81,7 +81,7 @@ if ($stylesheet_settings) {
     function reloadTemplate() {
         $.get(mw.settings.api_url + "template/delete_compiled_css?path=<?php print $template_settings['stylesheet_compiler']['source_file']; ?>&option_group=<?php print $option_group; ?>", function () {
             mw.parent().notification.success("<?php _ejs("Template settings are saved"); ?>.");
-            mw.parent().$("#theme-style").attr(', '<?php print mw()->template->get_stylesheet($template_settings['stylesheet_compiler']['source_file'], false, false); ?>&t=' + mw.random());
+            mw.parent().$("#theme-style").attr('href', '<?php print mw()->template->get_stylesheet($template_settings['stylesheet_compiler']['source_file'], false, false); ?>&t=' + mw.random());
             mw.tools.refresh(parent.$("#theme-style"));
         });
     }
