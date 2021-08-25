a43da3e51d0f74c554aed1363587c93a8c51feaa
Nikolay <rachkuw@gmail.com>
2021-08-25 13:32:11 +0300

    update

--- src/MicroweberPackages/Blog/resources/views/index.blade.php
+++ src/MicroweberPackages/Blog/resources/views/index.blade.php
@@ -1,7 +1,7 @@
 {!! $posts->scripts() !!}
 
 <div class="container-fluid">
-    <div class="row">
+    <div class="row pt-5">
         <div class="col-md-3">
             <div class="card border-0 text-dark bg-white">
 
