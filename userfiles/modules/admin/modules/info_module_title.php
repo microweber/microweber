fa012365e39005ca7ce4964823ef23ab21781c39
Peter Ivanov <peter@microweber.com>
2021-08-27 14:43:07 +0300

    update

--- userfiles/modules/admin/modules/info_module_title.php
+++ userfiles/modules/admin/modules/info_module_title.php
@@ -4,21 +4,22 @@ $mod = false;
 if (isset($params['for-module'])) {
     $mod = $params['for-module'];
 }
-
-
 if (!$mod) {
     return;
 }
 
-
 $module_info = module_info($mod);
- if(!$module_info){
+if (!$module_info) {
     return;
 }
 ?>
 <h5>
-    <img src="<?php echo $module_info['icon']; ?>" class="module-icon-svg-fill"/>
-    <strong><?php _e($module_info['name']); ?></strong>
+    <?php if (isset($module_info['icon'])) { ?>
+        <img src="<?php echo $module_info['icon']; ?>" class="module-icon-svg-fill"/>
+    <?php } ?>
+    <?php if (isset($module_info['name'])) { ?>
+        <strong><?php _e($module_info['name']); ?></strong>
+    <?php } ?>
 </h5>
 
 
