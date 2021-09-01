9638431f88eb3d5a62b7348775b708a9adeccb5d
Bozhidar Slaveykov <bobi@microweber.com>
2021-09-01 18:27:56 +0300

    update

--- src/MicroweberPackages/Multilanguage/Models/Traits/HasMultilanguageTrait.php
+++ src/MicroweberPackages/Multilanguage/Models/Traits/HasMultilanguageTrait.php
@@ -3,24 +3,82 @@
 namespace MicroweberPackages\Multilanguage\Models\Traits;
 
 
+use Illuminate\Support\Facades\DB;
+use MicroweberPackages\Multilanguage\Models\MultilanguageTranslations;
+
 trait HasMultilanguageTrait
 {
 
-    private $_addCustomFields = [];
+    private $_addMultilanguage = [];
 
     public function initializeHasMultilanguageTrait()
     {
         $this->fillable[] = 'multilanguage';
     }
 
+    protected function __getDefaultLocale()
+    {
+        return mw()->lang_helper->default_lang();
+    }
+
+    protected function __getLocale()
+    {
+        return mw()->lang_helper->current_lang();
+    }
+
     public static function bootHasMultilanguageTrait()
     {
         static::saving(function ($model) {
-
+            if (isset($model->attributes['multilanguage'])) {
+                $model->_addMultilanguage = $model->attributes['multilanguage'];
+                unset($model->attributes['multilanguage']);
+            }
         });
 
-        static::creating(function ($model) {
+        static::created(function ($model) {
+            $model->_saveMultilanguageTranslation();
+        });
 
+        static::saved(function ($model) {
+            $model->_saveMultilanguageTranslation();
+        });
+    }
+
+    public function _saveMultilanguageTranslation()
+    {
+        if (empty($model->_addMultilanguage)) {
+            return;
+        }
+
+        $model = $this;
+        $defaultLocale = $this->__getDefaultLocale();
+
+        DB::transaction(function () use ($model, $defaultLocale) {
+            foreach ($this->_addMultilanguage as $fieldName => $field) {
+                foreach ($field as $fieldLocale => $fieldValue) {
+
+                    if ($fieldLocale == $defaultLocale) {
+                        continue;
+                    }
+
+                    $findTranslate = MultilanguageTranslations::where('field_name', $fieldName)
+                        ->where('rel_type', $model->getTable())
+                        ->where('rel_id', $model->id)
+                        ->where('locale', $fieldLocale)
+                        ->first();
+
+                    if ($findTranslate == null) {
+                        $findTranslate = new MultilanguageTranslations();
+                        $findTranslate->field_name = $fieldName;
+                        $findTranslate->rel_type = $model->getTable();
+                        $findTranslate->rel_id = $model->id;
+                        $findTranslate->locale = $fieldLocale;
+                    }
+
+                    $findTranslate->field_value = $fieldValue;
+                    $findTranslate->save();
+                }
+            }
         });
     }
 }
