9a001ab6bf25da4b6728e93fd8ebac9df5c8268f
Peter Ivanov <peter@microweber.com>
2021-09-03 16:41:05 +0300

    update

---
+++ src/MicroweberPackages/Multilanguage/migrations/2021_09_03_133600_change_en_uk_to_en_gb_locale.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class ChangeEnUkToEnGbLocale extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        if (Schema::hasTable('multilanguage_supported_locales')) {
+
+            Schema::table('multilanguage_supported_locales', function (Blueprint $table) {
+                DB::table('multilanguage_supported_locales')
+                    ->where('locale', 'LIKE', "%en_uk%")
+                    ->update(['locale' => 'en_GB']);
+            });
+            Schema::table('multilanguage_translations', function (Blueprint $table) {
+                DB::table('multilanguage_translations')
+                    ->where('locale', 'LIKE', "%en_uk%")
+                    ->update(['locale' => 'en_GB']);
+            });
+        }
+
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('en_gb_locale', function (Blueprint $table) {
+            //
+        });
+    }
+}
