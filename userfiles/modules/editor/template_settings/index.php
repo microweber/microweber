34f1a1e12e67a3ce46bec5bc4534f777f2b45b18
Nikolay <rachkuw@gmail.com>
2021-09-14 17:12:34 +0300

    Update index.php

--- userfiles/modules/editor/template_settings/index.php
+++ userfiles/modules/editor/template_settings/index.php
@@ -177,6 +177,6 @@
     </div>
     <br>
 
-    <span class="mw-ui-btn mw-ui-btn-medium mw-full-width" onclick="resetToDefault();" style="margin-top: 4px;"><?php _e("Reset Template Settings"); ?></span>
+    <span class="btn btn-danger justify-content-center d-block" onclick="resetToDefault();" style="margin-top: 4px;"><?php _e("Reset Template Settings"); ?></span>
 </div>
 <!-- /#settings-holder -->
