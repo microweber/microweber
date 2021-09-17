024c009b05ce81693641b266bd090e485cac20d5
Bozhidar Slaveykov <b.slaveykov@radioactive.bg>
2021-09-17 11:26:57 +0300

    remove content filter module

--- userfiles/modules/content_filter/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$controller = \Illuminate\Support\Facades\App::make(\MicroweberPackages\ContentFilter\Http\Controllers\ContentFilterController::class);
-
-$request = new \Illuminate\Http\Request();
-$request->merge($params);
-$request->merge($_REQUEST);
-
-echo $controller->index($request);
