a26f4213a24585c22d23d6f0c94d1ae3d4d41eea
Bozhidar Slaveykov <b.slaveykov@radioactive.bg>
2021-09-27 12:12:10 +0300

    update

--- src/MicroweberPackages/Order/resources/views/admin/orders/partials/order_filtering.blade.php
+++ src/MicroweberPackages/Order/resources/views/admin/orders/partials/order_filtering.blade.php
@@ -21,29 +21,6 @@
                class="btn btn-link btn-sm <?php if (isset($abandoned)): ?>font-weight-bold text-dark active<?php else: ?>text-muted<?php endif; ?>"><?php _e("Abandoned carts"); ?></a>
         </div>
 
-        <?php if ($orders->count() > 0) { ?>
-        <div
-            class="js-table-sorting text-end my-1 d-flex justify-content-center justify-content-sm-end align-items-center">
-            <small><?php _e("Sort By"); ?>: &nbsp;</small>
-            <div class="d-inline-block mx-1">
-                <select class="form-control" onchange="location = this.value;">
-                    <option <?php if($orderBy == 'created_at' && $orderDirection == 'asc'): ?>selected="selected"
-                            <?php endif;?> value="{{route('admin.order.index')}}?orderBy=created_at&orderDirection=asc"><?php _e("Date"); ?> <?php _e("[ASC]"); ?></option>
-                    <option <?php if($orderBy == 'created_at' && $orderDirection == 'desc'): ?>selected="selected"
-                            <?php endif;?> value="{{route('admin.order.index')}}?orderBy=created_at&orderDirection=desc"><?php _e("Date"); ?> <?php _e("[DESC]"); ?></option>
-                    <option <?php if($orderBy == 'order_status' && $orderDirection == 'asc'): ?>selected="selected"
-                            <?php endif;?> value="{{route('admin.order.index')}}?orderBy=order_status&orderDirection=asc"><?php _e("Status"); ?> <?php _e("[ASC]"); ?></option>
-                    <option <?php if($orderBy == 'order_status' && $orderDirection == 'desc'): ?>selected="selected"
-                            <?php endif;?> value="{{route('admin.order.index')}}?orderBy=order_status&orderDirection=desc"><?php _e("Status"); ?> <?php _e("[DESC]"); ?></option>
-                    <option <?php if($orderBy == 'amount' && $orderDirection == 'asc'): ?>selected="selected"
-                            <?php endif;?> value="{{route('admin.order.index')}}?orderBy=amount&orderDirection=asc"><?php _e("Amount"); ?> <?php _e("[ASC]"); ?></option>
-                    <option <?php if($orderBy == 'amount' && $orderDirection == 'desc'): ?>selected="selected"
-                            <?php endif;?> value="{{route('admin.order.index')}}?orderBy=amount&orderDirection=desc"><?php _e("Amount"); ?> <?php _e("[DESC]"); ?></option>
-                </select>
-            </div>
-        </div>
-        <?php } ?>
-
     </div>
 
     <?php if ($orders->count() > 0 || $filteringResults) { ?>
@@ -68,7 +45,7 @@
             </div>
         </div>
 
-        <div class="col-md-3">
+        <div class="col-md-4">
             <div class="d-inline-block mx-1">
                 <button type="submit" name="filteringResults" value="true" class="btn btn-success btn-block">
                 <i class="mdi mdi-filter"></i> <?php _e("Filter"); ?></button>
@@ -79,6 +56,29 @@
                 </a>
             </div>
         </div>
+
+        <div class="col-md-3">
+            <div class="js-table-sorting text-end my-1 d-flex justify-content-center justify-content-sm-end align-items-center">
+                <small><?php _e("Sort By"); ?>: &nbsp;</small>
+                <div class="d-inline-block mx-1">
+                    <select class="form-control" onchange="location = this.value;">
+                        <option <?php if($orderBy == 'created_at' && $orderDirection == 'asc'): ?>selected="selected"
+                                <?php endif;?> value="{{route('admin.order.index')}}?orderBy=created_at&orderDirection=asc"><?php _e("Date"); ?> <?php _e("[ASC]"); ?></option>
+                        <option <?php if($orderBy == 'created_at' && $orderDirection == 'desc'): ?>selected="selected"
+                                <?php endif;?> value="{{route('admin.order.index')}}?orderBy=created_at&orderDirection=desc"><?php _e("Date"); ?> <?php _e("[DESC]"); ?></option>
+                        <option <?php if($orderBy == 'order_status' && $orderDirection == 'asc'): ?>selected="selected"
+                                <?php endif;?> value="{{route('admin.order.index')}}?orderBy=order_status&orderDirection=asc"><?php _e("Status"); ?> <?php _e("[ASC]"); ?></option>
+                        <option <?php if($orderBy == 'order_status' && $orderDirection == 'desc'): ?>selected="selected"
+                                <?php endif;?> value="{{route('admin.order.index')}}?orderBy=order_status&orderDirection=desc"><?php _e("Status"); ?> <?php _e("[DESC]"); ?></option>
+                        <option <?php if($orderBy == 'amount' && $orderDirection == 'asc'): ?>selected="selected"
+                                <?php endif;?> value="{{route('admin.order.index')}}?orderBy=amount&orderDirection=asc"><?php _e("Amount"); ?> <?php _e("[ASC]"); ?></option>
+                        <option <?php if($orderBy == 'amount' && $orderDirection == 'desc'): ?>selected="selected"
+                                <?php endif;?> value="{{route('admin.order.index')}}?orderBy=amount&orderDirection=desc"><?php _e("Amount"); ?> <?php _e("[DESC]"); ?></option>
+                    </select>
+                </div>
+            </div>
+        </div>
+
     </div>
 
     <?php } ?>
