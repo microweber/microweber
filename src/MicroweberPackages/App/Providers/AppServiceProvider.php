b019eec7909df70ed8088854f22ab3ab733cef1b
Bozhidar Slaveykov <b.slaveykov@radioactive.bg>
2021-10-01 15:04:41 +0300

    update

--- src/MicroweberPackages/App/Providers/AppServiceProvider.php
+++ src/MicroweberPackages/App/Providers/AppServiceProvider.php
@@ -441,45 +441,7 @@ class AppServiceProvider extends ServiceProvider
                 DB::connection('sqlite')->getPdo()->sqliteCreateFunction('md5', 'md5');
             }
 
-            $isLocaleChangedFromLink = false;
-            //  Change language if user request language with LINK has lang abr
-            if (MultilanguageHelpers::multilanguageIsEnabled()) {
-                $currentUri = request()->path();
-                $linkSegments = url_segment(-1, $currentUri);
-                $linkSegments = array_filter($linkSegments, 'trim');
-                if (!empty($linkSegments)) {
-                    if (isset($linkSegments[0]) and $linkSegments[0]) {
-                        $localeSettings = app()->multilanguage_repository->getSupportedLocaleByDisplayLocale($linkSegments[0]);
-                        if (!$localeSettings) {
-                            $localeSettings = app()->multilanguage_repository->getSupportedLocaleByLocale($linkSegments[0]);
-                        }
-                        if ($localeSettings and isset($localeSettings['locale'])) {
-                            $isLocaleChangedFromLink = true;
-                            change_language_by_locale($localeSettings['locale'], true);
-                        }
-                    }
-                }
-            }
-
-            // If locale is not changed from link
-            if (!$isLocaleChangedFromLink) {
-
-                // If we have a lang cookie read from theere
-                if (isset($_COOKIE['lang']) && !empty($_COOKIE['lang'])) {
-                    $setCurrentLangTo = $_COOKIE['lang'];
-                } else {
-                    if (MultilanguageHelpers::multilanguageIsEnabled()) {
-                        // Set from default homepage lang settings
-                        $setCurrentLangTo = get_option('homepage_language', 'website');
-                    } else {
-                        // Set from default language language settings
-                        $setCurrentLangTo = get_option('language', 'website');
-                    }
-                }
-                if ($setCurrentLangTo) {
-                    set_current_lang($setCurrentLangTo);
-                }
-            }
+            $this->_localeSetup();
 
             load_all_functions_files_for_modules($this->app);
 
@@ -638,4 +600,46 @@ class AppServiceProvider extends ServiceProvider
         }
     }
 
+    private function _localeSetup()
+    {
+        $isLocaleChangedFromLink = false;
+        //  Change language if user request language with LINK has lang abr
+        if (MultilanguageHelpers::multilanguageIsEnabled()) {
+            $currentUri = request()->path();
+            $linkSegments = url_segment(-1, $currentUri);
+            $linkSegments = array_filter($linkSegments, 'trim');
+            if (!empty($linkSegments)) {
+                if (isset($linkSegments[0]) and $linkSegments[0]) {
+                    $localeSettings = app()->multilanguage_repository->getSupportedLocaleByDisplayLocale($linkSegments[0]);
+                    if (!$localeSettings) {
+                        $localeSettings = app()->multilanguage_repository->getSupportedLocaleByLocale($linkSegments[0]);
+                    }
+                    if ($localeSettings and isset($localeSettings['locale'])) {
+                        $isLocaleChangedFromLink = true;
+                        change_language_by_locale($localeSettings['locale'], true);
+                    }
+                }
+            }
+        }
+
+        // If locale is not changed from link
+        if (!$isLocaleChangedFromLink) {
+
+            // If we have a lang cookie read from theere
+            if (isset($_COOKIE['lang']) && !empty($_COOKIE['lang'])) {
+                $setCurrentLangTo = $_COOKIE['lang'];
+            } else {
+                if (MultilanguageHelpers::multilanguageIsEnabled()) {
+                    // Set from default homepage lang settings
+                    $setCurrentLangTo = get_option('homepage_language', 'website');
+                } else {
+                    // Set from default language language settings
+                    $setCurrentLangTo = get_option('language', 'website');
+                }
+            }
+            if ($setCurrentLangTo) {
+                set_current_lang($setCurrentLangTo);
+            }
+        }
+    }
 }
