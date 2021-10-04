930f16e63aaafd85e8817f47a494e45e3ca042ed
Bozhidar Slaveykov <b.slaveykov@radioactive.bg>
2021-10-04 17:59:45 +0300

    update

--- src/MicroweberPackages/Module/tests/ModuleListTest.php
+++ src/MicroweberPackages/Module/tests/ModuleListTest.php
@@ -5,38 +5,38 @@ namespace MicroweberPackages\Module\tests;
 use Illuminate\Support\Facades\Auth;
 use MicroweberPackages\Core\tests\TestCase;
 use MicroweberPackages\User\Models\User;
-//
-//class ModuleListTest extends TestCase
-//{
-//    public function testModuleIndex()
-//    {
-//        $getModules = app()->module_repository->getAllModules();
-//
-//        $user = User::where('is_admin', '=', '1')->first();
-//        Auth::login($user);
-//
-//        // Test modules index
-//        foreach ($getModules as $module) {
-//            $moduleOutput = app()->parser->process('<module type="' . $module['module'] . '" />');
-//            $this->assertNotEmpty($moduleOutput);
-//        }
-//    }
-//
-//
-//    public function testModuleAdmin()
-//    {
-//        $getModules = app()->module_repository->getAllModules();
-//
-//        $user = User::where('is_admin', '=', '1')->first();
-//        Auth::login($user);
-//
-//        // Test modules admin
-//        foreach ($getModules as $module) {
-//            if(isset($module['ui_admin']) and $module['ui_admin']){
-//            $moduleOutput = app()->parser->process('<module type="' . $module['module'] . '/admin" />');
-//            $this->assertNotEmpty($moduleOutput);
-//            }
-//        }
-//
-//    }
-//}
+
+class ModuleListTest extends TestCase
+{
+    public function testModuleIndex()
+    {
+        $getModules = app()->module_repository->getAllModules();
+
+        $user = User::where('is_admin', '=', '1')->first();
+        Auth::login($user);
+
+        // Test modules index
+        foreach ($getModules as $module) {
+            $moduleOutput = app()->parser->process('<module type="' . $module['module'] . '" />');
+            $this->assertNotEmpty($moduleOutput);
+        }
+    }
+
+
+    public function testModuleAdmin()
+    {
+        $getModules = app()->module_repository->getAllModules();
+
+        $user = User::where('is_admin', '=', '1')->first();
+        Auth::login($user);
+
+        // Test modules admin
+        foreach ($getModules as $module) {
+            if(isset($module['ui_admin']) and $module['ui_admin']){
+            $moduleOutput = app()->parser->process('<module type="' . $module['module'] . '/admin" />');
+            $this->assertNotEmpty($moduleOutput);
+            }
+        }
+
+    }
+}
