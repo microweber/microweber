2bf3d42a312c48f8f390b6d5fd5f6ae5c97eaef3
Bozhidar Slaveykov <b.slaveykov@radioactive.bg>
2021-10-04 18:27:34 +0300

    update

--- tests/Browser/ExampleTest.php
+++ tests/Browser/ExampleTest.php
@@ -16,8 +16,7 @@ class ExampleTest extends DuskTestCase
     public function testBasicExample()
     {
         $this->browse(function (Browser $browser) {
-            $browser->visit('/')
-                    ->assertSee('Laravel');
+            $browser->visit('/')->dump();
         });
     }
 }
