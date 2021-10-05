630c3c840b32d2f07bd4b6994d6f0250cab6f4cc
Bozhidar Slaveykov <b.slaveykov@radioactive.bg>
2021-10-05 17:44:13 +0300

    update

--- src/MicroweberPackages/Option/Models/Traits/HasMultilanguageModuleOptionTrait.php
+++ src/MicroweberPackages/Option/Models/Traits/HasMultilanguageModuleOptionTrait.php
@@ -1,6 +1,11 @@
 <?php
 namespace MicroweberPackages\Option\Models\Traits;
 
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
+use MicroweberPackages\Multilanguage\Models\MultilanguageTranslations;
+use MicroweberPackages\Multilanguage\MultilanguageHelpers;
+
 trait HasMultilanguageModuleOptionTrait {
 
     private $_addMultilanguage = [];
@@ -24,6 +29,49 @@ trait HasMultilanguageModuleOptionTrait {
                 unset($model->attributes['lang']);
             }
         });
+
+        static::saved(function ($model) {
+            if (MultilanguageHelpers::multilanguageIsEnabled()) {
+                $model->_saveMultilanguageTranslation();
+            }
+        });
+    }
+
+    public function _saveMultilanguageTranslation()
+    {
+        if (empty($this->_addMultilanguage)) {
+            return;
+        }
+
+        $model = $this;
+        $defaultLocale = $this->__getDefaultLocale();
+
+        DB::transaction(function () use ($model, $defaultLocale) {
+            foreach ($this->_addMultilanguage as $fieldName => $field) {
+                foreach ($field as $fieldLocale => $fieldValue) {
+                    if ($fieldLocale == $defaultLocale) {
+                        continue;
+                    }
+
+                    $findTranslate = MultilanguageTranslations::where('field_name', $fieldName)
+                        ->where('rel_type', $model->getTable())
+                        ->where('rel_id', $model->id)
+                        ->where('locale', $fieldLocale)
+                        ->first();
+                    
+                    if ($findTranslate == null) {
+                        $findTranslate = new MultilanguageTranslations();
+                        $findTranslate->field_name = $fieldName;
+                        $findTranslate->rel_type = $model->getTable();
+                        $findTranslate->rel_id = $model->id;
+                        $findTranslate->locale = $fieldLocale;
+                    }
+
+                    $findTranslate->field_value = $fieldValue;
+                    $findTranslate->save();
+                }
+            }
+        });
     }
 
     public static function getTranslatableModuleOptions() {
@@ -34,5 +82,27 @@ trait HasMultilanguageModuleOptionTrait {
             }
         }
         return $translatableModuleOptions;
+    }
+
+    private static $__getDefaultLocale = false;
+    protected function __getDefaultLocale()
+    {
+        if (self::$__getDefaultLocale) {
+            return self::$__getDefaultLocale;
+        }
+
+        self::$__getDefaultLocale = mw()->lang_helper->default_lang();
+
+        return self::$__getDefaultLocale;
+    }
+
+    private static $__getLocale = false;
+    protected function __getLocale()
+    {
+        if (self::$__getLocale) {
+            return self::$__getLocale;
+        }
+        self::$__getLocale = mw()->lang_helper->current_lang();
+        return self::$__getLocale;
     }
 }
