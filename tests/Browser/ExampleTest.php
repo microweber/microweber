c10e687e09f953473b75232dd3d5b74246a537cd
Bozhidar Slaveykov <b.slaveykov@radioactive.bg>
2021-10-12 15:53:43 +0300

    update

--- tests/Browser/ExampleTest.php
+++ tests/Browser/ExampleTest.php
@@ -14,15 +14,16 @@ class ExampleTest extends DuskTestCase
      */
     public function testBasicExample()
     {
-        define('MW_SITE_URL', 'http://127.0.0.1:8000/');
+        $siteUrl = 'http://127.0.0.1:8000/';
 
-        $siteUrl = site_url();
+        $this->browse(function (Browser $browser) use ($siteUrl) {
 
-        $this->browse(function (Browser $browser) use($siteUrl) {
             $browser->visit($siteUrl);
             $browser->pause(2000);
+            $browser->assertSee('login');
 
-            dump($browser->dump());
+        //    dump($browser->dump());
+
         });
     }
 }
