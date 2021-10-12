e99a175bf1c1cbc70ebe9035c0b1beb1581a40d8
Bozhidar Slaveykov <b.slaveykov@radioactive.bg>
2021-10-12 11:08:06 +0300

    update

--- src/MicroweberPackages/Form/Elements/MwModuleSettings.php
+++ src/MicroweberPackages/Form/Elements/MwModuleSettings.php
@@ -22,17 +22,17 @@ class MwModuleSettings extends TextArea
 <script>
 $(window).on(\'load\', function () {
 
-    var data = '.$data.';
-    $.each(data, function (key) {
-        if (typeof data[key].images === \'string\') {
-            data[key].images = data[key].images.split(\',\');
+    var data'.$mwModuleSettingsId.' = '.$data.';
+    $.each(data'.$mwModuleSettingsId.', function (key) {
+        if (typeof data'.$mwModuleSettingsId.'[key].images === \'string\') {
+            data'.$mwModuleSettingsId.'[key].images = data'.$mwModuleSettingsId.'[key].images.split(\',\');
         }
     });
 
     this.bxSettings_'.$mwModuleSettingsId.' = new mw.moduleSettings({
         element: \'#settings-box\',
         header: \'<i class="mw-icon-drag"></i> Content #{count} <a class="pull-right" data-action="remove"><i class="mw-icon-close"></i></a>\',
-        data: data,
+        data: data'.$mwModuleSettingsId.',
         key: \'settings\',
         group: \'id\',
         autoSave: true,
@@ -42,9 +42,9 @@ $(window).on(\'load\', function () {
     $(bxSettings_'.$mwModuleSettingsId.').on(\'change\', function (e, val) {
         var final = [];
         $.each(val, function () {
-            var curr = $.extend({}, this);
-            curr.images = curr.images.join(\',\');
-            final.push(curr)
+            var current = $.extend({}, this);
+            current.images = (current.images||[]).join(\',\');
+            final.push(current)
         });
         $(\'#settingsfield'.$mwModuleSettingsId.'\').val(JSON.stringify(final)).trigger(\'change\')
     });
