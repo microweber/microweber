cdf26584c8de68f5594dee1d12b41f9bf767d489
Bozhidar Slaveykov <b.slaveykov@radioactive.bg>
2021-10-13 17:47:41 +0300

    update

--- userfiles/modules/microweber/api/libs/multilanguage/mlInput.js
+++ userfiles/modules/microweber/api/libs/multilanguage/mlInput.js
@@ -23,7 +23,6 @@
             }
 
             $(obj).css('opacity', 0.2).css('display','none');
-          //  $(obj).attr('value', translations[currentLocale]);
 
             var outputHtml = '<div class="input-group">';
 
@@ -34,10 +33,6 @@
 
                     // If ml input is changed change and the value attr
                     $('body').on('keyup','#' + mlInputLocaleId, function () {
-                        if (currentLocale == $(this).attr('lang')) {
-                            // Change original field to this current lang value
-                            //$(obj).attr('value', $(this).val());
-                        }
                         $(this).attr('value', $(this).val());
                     });
 
