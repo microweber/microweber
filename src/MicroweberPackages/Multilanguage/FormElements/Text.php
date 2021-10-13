3b66cf430bd91481a3409812fc69841acc1fb3c9
raikov <raikov@ymail.com>
2021-10-13 13:28:50 +0300

    textarea switch

--- src/MicroweberPackages/Multilanguage/FormElements/Text.php
+++ src/MicroweberPackages/Multilanguage/FormElements/Text.php
@@ -125,6 +125,7 @@ class Text extends \MicroweberPackages\Form\Elements\Text
                          mw.on("mlChangedLanguage", function (e, data) {
                             var applyChangedLang = document.getElementById("js-multilanguage-select-lang-' . $this->randId . '");
                             $(applyChangedLang).selectpicker("val", data);
+                             $(\'a[data-toggle="tab"][href*="\'+data+\'"]\').tab(\'show\')
                          });
                         runMlTextField' . $this->randId . '();
                     });
