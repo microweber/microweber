d6f625c728de2e12e9447eecec90800b4fe3b8f7
Bozhidar Slaveykov <b.slaveykov@radioactive.bg>
2021-10-13 16:05:46 +0300

    update

--- userfiles/modules/multilanguage/admin.php
+++ userfiles/modules/multilanguage/admin.php
@@ -1,7 +1,7 @@
 <?php if (!isset($params['live_edit'])): ?>
-    <?php // include($config['path_to_module'] . 'admin_backend.php'); ?>
+    <?php include($config['path_to_module'] . 'admin_backend.php'); ?>
 <?php else: ?>
-    <?php // include($config['path_to_module'] . 'admin_live_edit.php'); ?>
+    <?php include($config['path_to_module'] . 'admin_live_edit.php'); ?>
 <?php endif; ?>
 
 <script>
