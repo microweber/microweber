f498337b73a6f1ee490527537b25c52a4cd0574c
Bozhidar Slaveykov <b.slaveykov@radioactive.bg>
2021-10-15 12:10:46 +0300

    update

--- src/MicroweberPackages/Multilanguage/tests/MultilanguageModuleOptionTest.php
+++ src/MicroweberPackages/Multilanguage/tests/MultilanguageModuleOptionTest.php
@@ -11,13 +11,37 @@ class MultilanguageOptionTest extends MultilanguageTestBase
 
     public function testSaveModuleOptionApi()
     {
+
+        $defaultLang = app()->lang_helper->default_lang();
+        $currentLang = app()->lang_helper->current_lang();
+
+        $optionKey = 'settings';
+        $module = 'slider';
+        $optionGroup = 'slider-20211014142244';
+
+        // Save the option on default lang
+        $dataDefaultLang = [];
+        $dataDefaultLang['option_key'] = $optionKey;;
+        $dataDefaultLang['module'] = $module;
+        $dataDefaultLang['option_value'] = 'ti si manqk-' . $defaultLang;
+        $dataDefaultLang['option_group'] = $optionGroup;
+        mw()->option_manager->save($dataDefaultLang);
+
+        // Check the value is the same
+        $findModuleOption = ModuleOption::where('option_key', $dataDefaultLang['option_key'])
+            ->where('module', $dataDefaultLang['module'])
+            ->where('option_key', $dataDefaultLang['option_key'])
+            ->where('option_group', $dataDefaultLang['option_group'])->first();
+        $this->assertEquals($findModuleOption->option_value, $dataDefaultLang['option_value']);
+
+
         // Save mutilang module option
         $data = [];
         $data['lang'] = 'bg_BG';
-        $data['option_key'] = 'settings';
-        $data['module'] = 'slider';
+        $data['option_key'] = $optionKey;;
+        $data['module'] = $module;
         $data['option_value'] = 'ti si manqk';
-        $data['option_group'] = 'slider-20211014142244';
+        $data['option_group'] = $optionGroup;
         mw()->option_manager->save($data);
 
         $findModuleOption = ModuleOption::where('option_key', $data['option_key'])
@@ -36,10 +60,10 @@ class MultilanguageOptionTest extends MultilanguageTestBase
         /////////////////////////
         $data = [];
         $data['lang'] = 'ar_SA';
-        $data['option_key'] = 'settings';
-        $data['module'] = 'slider';
+        $data['option_key'] = $optionKey;;
+        $data['module'] = $module;
         $data['option_value'] = 'ti si arabski manqk';
-        $data['option_group'] = 'slider-20211014142244';
+        $data['option_group'] = $optionGroup;
         mw()->option_manager->save($data);
 
         $findModuleOption = ModuleOption::where('option_key', $data['option_key'])
@@ -56,6 +80,12 @@ class MultilanguageOptionTest extends MultilanguageTestBase
         $this->assertEquals($findTranslation->field_value, $data['option_value']);
 
 
+        // Check the value is the same after many antoher multilanguage saves
+        $findModuleOption = ModuleOption::where('option_key', $dataDefaultLang['option_key'])
+            ->where('module', $dataDefaultLang['module'])
+            ->where('option_key', $dataDefaultLang['option_key'])
+            ->where('option_group', $dataDefaultLang['option_group'])->first();
+        $this->assertEquals($findModuleOption->option_value, $dataDefaultLang['option_value']);
 
 
     }
