d7c06b8b25c0bd9a9830f7d38db4141089003e5f
Bozhidar Slaveykov <b.slaveykov@radioactive.bg>
2021-10-19 16:36:57 +0300

    update

---
+++ src/MicroweberPackages/App/tests/LiveEditTest.php
@@ -0,0 +1,74 @@
+<?php
+namespace MicroweberPackages\App\tests;
+
+use Illuminate\Support\Facades\Auth;
+use MicroweberPackages\App\Http\Controllers\FrontendController;
+use MicroweberPackages\Core\tests\TestCase;
+use MicroweberPackages\Page\Models\Page;
+use MicroweberPackages\User\Models\User;
+
+class LiveEditTest extends TestCase
+{
+    public function testSaveContentOnPage()
+    {
+
+        $user = User::where('is_admin', '=', '1')->first();
+        Auth::login($user);
+
+        $newCleanPage = save_content([
+            'subtype' => 'static',
+            'content_type' => 'page',
+            'layout_file' => 'clean.php',
+            'title' => 'LiveEditPage',
+            'url' => 'liveedit-url',
+            'preview_layout_file' => 'clean.php',
+            'active_site_template'=> 'default',
+            'is_active' => 1,
+        ]);
+
+        $fingPage = Page::whereId($newCleanPage)->first();
+        $this->assertEquals($fingPage->id, $newCleanPage);
+
+        // Save on default lang
+        $contentFieldHtml = 'Example content saved from live edit api'. uniqid('_unit');
+        $fieldsData = [
+            'field_data_0'=>[
+                'attributes'=>[
+                    'class'=>'container edit',
+                    'rel'=>'content',
+                    'field'=>'content',
+                ],
+                'html'=>$contentFieldHtml
+            ]
+        ];
+
+        $encoded = base64_encode(json_encode($fieldsData));
+
+        $_SERVER['HTTP_REFERER'] = content_link($fingPage->id);
+
+        $response = $this->call(
+            'POST',
+            route('api.content.save_edit'),
+            [
+                'data_base64' => $encoded,
+            ],
+            [],//params
+            $_COOKIE,//cookie
+            [],//files
+            $_SERVER //server
+        );
+        $fieldSaved = $response->decodeResponseJson();
+
+        $this->assertEquals($fieldSaved[0]['rel_type'], 'content');
+        $this->assertEquals($fieldSaved[0]['field'], 'content');
+
+        $_REQUEST['content_id'] = $fingPage->id;
+
+        $frontRender = new FrontendController();
+        $html = $frontRender->index();
+
+        $this->assertTrue(str_contains($html, $contentFieldHtml));
+
+
+    }
+}
