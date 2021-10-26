d14074bed255cd18c2348bf24469983c15e3e117
Bozhidar Slaveykov <b.slaveykov@radioactive.bg>
2021-10-26 14:20:29 +0300

    update

--- src/MicroweberPackages/Form/Models/FormData.php
+++ src/MicroweberPackages/Form/Models/FormData.php
@@ -11,25 +11,4 @@ class FormData extends Model
     {
         return $this->hasMany(FormDataValue::class);
     }
-
-    /**
-     * This functions is backward compatibility with old format data values
-     * @return array
-     */
-    public function getFormValuesAttribute()
-    {
-        $values = [];
-        $formDataValues = $this->formDataValues()->get();
-
-        if ($formDataValues->count() > 0) {
-            foreach ($formDataValues as $formDataValue) {
-                if (is_array($formDataValue->field_value_json) && !empty($formDataValue->field_value_json)) {
-                    $values[$formDataValue->field_name] = $formDataValue->field_value_json;
-                } else {
-                    $values[$formDataValue->field_name] = $formDataValue->field_value;
-                }
-            }
-        }
-        return $values;
-    }
 }
