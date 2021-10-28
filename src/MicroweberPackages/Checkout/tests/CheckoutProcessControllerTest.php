1dc34ba60838bfaf45ae9769a50b66d7ecb970c8
Bozhidar Slaveykov <b.slaveykov@radioactive.bg>
2021-10-28 18:36:52 +0300

    Update CheckoutProcessControllerTest.php

--- src/MicroweberPackages/Checkout/tests/CheckoutProcessControllerTest.php
+++ src/MicroweberPackages/Checkout/tests/CheckoutProcessControllerTest.php
@@ -39,11 +39,42 @@ class CheckoutProcessControllerTest extends TestCase
         $this->assertEquals(isset($cart_add['product']), true);
         $this->assertEquals($cart_add['product']['price'], 4444);
 
+        // go to checkout cart
         $response = $this->call('GET', route('checkout.cart', []));
-
+        $response->assertStatus(302);
         $response->assertRedirect(route('checkout.contact_information'));
 
+        /// goto contact info
+        $response = $this->call('GET', route('checkout.contact_information', []));
+        $response->assertStatus(200);
 
+        // save contact ifno
+        $response = $this->call('POST', route('checkout.contact_information_save', [
+            'first_name'=>'Bozhidar',
+            'last_name'=>'Slaveykov',
+            'email'=>'bobi@microweber.com',
+            'phone'=>'08812345678',
+        ]));
+        $response->assertStatus(302);
+        $response->assertRedirect(route('checkout.shipping_method'));
+
+        // goto shipping method
+        $response = $this->call('GET', route('checkout.shipping_method', []));
+        $response->assertStatus(200);
+
+        // save shipping method
+        $response = $this->call('POST', route('checkout.shipping_method_save', [
+            'shipping_gw'=>'shop/shipping/gateways/pickup',
+        ]));
+        $response->assertStatus(302);
+        $response->assertRedirect(route('checkout.payment_method'));
+
+        // goto payment method
+        $response = $this->call('GET', route('checkout.payment_method', []));
+        $response->assertStatus(200);
+
+
+        
 
     }
 
