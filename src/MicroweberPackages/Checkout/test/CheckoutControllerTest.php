0289499f915fe556f3108384385a3299392882d6
Bozhidar Slaveykov <b.slaveykov@radioactive.bg>
2021-10-28 18:14:50 +0300

    Create CheckoutControllerTest.php

---
+++ src/MicroweberPackages/Checkout/test/CheckoutControllerTest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace MicroweberPackages\Checkout\test;
+
+use MicroweberPackages\Core\tests\TestCase;
+
+class CheckoutControllerTest extends TestCase
+{
+
+    public function testProcess()
+    {
+        app()->database_manager->extended_save_set_permission(true);
+
+        $params = array(
+            'title' => 'My new product',
+            'content_type' => 'product',
+            'subtype' => 'product',
+            'custom_fields_advanced' => array(
+                array('type' => 'dropdown', 'name' => 'Color', 'value' => array('Purple', 'Blue')),
+                array('type' => 'price', 'name' => 'Price', 'value' => '30'),
+
+            ),
+            'is_active' => 1,);
+
+
+        $saved_id = save_content($params);
+        $get = get_content_by_id($saved_id);
+
+        $this->assertEquals($saved_id, ($get['id']));
+        self::$content_id = $saved_id;
+
+        $add_to_cart = array(
+            'content_id' => self::$content_id,
+            // 'price' => 30,
+        );
+        $cart_add = update_cart($add_to_cart);
+
+        $this->assertEquals(isset($cart_add['success']), true);
+        $this->assertEquals(isset($cart_add['product']), true);
+        $this->assertEquals($cart_add['product']['price'], 30);
+
+
+
+
+
+    }
+
+}
