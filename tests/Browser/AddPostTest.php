905240fe97a3842d3b1cfde44c2cf9165bb66a64
Bozhidar Slaveykov <b.slaveykov@radioactive.bg>
2021-11-03 11:52:23 +0200

    update

--- tests/Browser/AddPostTest.php
+++ tests/Browser/AddPostTest.php
@@ -16,6 +16,9 @@ class AddPostTest extends DuskTestCase
 
         $this->browse(function (Browser $browser) use($siteUrl) {
 
+            $postTitle = 'This is the post title'.time();
+            $postDescription = 'This is the post description'.time();
+
             $browser->visit($siteUrl . 'admin/login')->assertSee('Login');
 
             $browser->waitFor('@login-button');
@@ -34,16 +37,31 @@ class AddPostTest extends DuskTestCase
             $browser->visit($siteUrl.'admin/view:content#action=new:post?dusk=1');
 
             $browser->pause(3000);
-            $browser->value('#slug-field-holder input', 'This is the post title');
+            $browser->value('#slug-field-holder input', $postTitle);
+
+            $browser->pause(3000);
+            $browser->script('$(".mw-editor-area").html("'.$postDescription.'")');
+            $browser->pause(1000);
+
+            $browser->scrollTo('.mw-uploader-input');
+            $browser->attach('input.mw-uploader-input', userfiles_path() . '/templates/default/img/patterns/img1.jpg');
+            $browser->pause(4000);
+            $browser->attach('input.mw-uploader-input', userfiles_path() . '/templates/default/img/patterns/img2.jpg');
+            $browser->pause(4000);
+            $browser->attach('input.mw-uploader-input', userfiles_path() . '/templates/default/img/patterns/img3.jpg');
+
+            $browser->pause(11500);
 
             $browser->pause(1000);
             $browser->click('#js-admin-save-content-main-btn');
 
-
             $browser->pause(1500);
             $browser->assertSee('Content saved');
 
+            $browser->pause(4000);
+            $browser->assertValue('#slug-field-holder input', $postTitle);
 
+            $browser->pause(11500);
         });
 
     }
