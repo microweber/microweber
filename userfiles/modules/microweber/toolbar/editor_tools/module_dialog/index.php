50dc295238244de5145d761460b5c478fa32a869
Nikolay <rachkuw@gmail.com>
2021-11-03 16:43:54 +0200

    Update index.php

--- userfiles/modules/microweber/toolbar/editor_tools/module_dialog/index.php
+++ userfiles/modules/microweber/toolbar/editor_tools/module_dialog/index.php
@@ -41,7 +41,7 @@
             overflow: hidden;
             position: relative;
             min-height: 300px;
-            padding: 25px;
+            padding: 25px 0px;
         }
 
         #settings-container:after {
