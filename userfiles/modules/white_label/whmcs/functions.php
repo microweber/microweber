36efeef5e2d1dd852279ff4fef6b814d7493a400
Bozhidar Slaveykov <b.slaveykov@radioactive.bg>
2021-11-08 13:45:05 +0200

    Update functions.php

--- userfiles/modules/white_label/whmcs/functions.php
+++ userfiles/modules/white_label/whmcs/functions.php
@@ -88,14 +88,14 @@ if (!function_exists('get_whitelabel_whmcs_settings')) {
     }
 
     $whitelabelSettings = get_whitelabel_whmcs_settings();
-
+    
     if (isset($whitelabelSettings['whmcs_url']) && !empty($whitelabelSettings['whmcs_url'])) {
 
-        event_bind('mw.admin.header.last', function () use ($whitelabelSettings) {
-            echo '<div style="bottom:44px;position:absolute;">
-                    <a class="btn btn-outline-primary" href="' . $whitelabelSettings['whmcs_url'] . 'clientarea.php?action=services" target="_blank">
+        event_bind('mw.admin.sidebar.li.last', function () use ($whitelabelSettings) {
+            echo '<li class="nav-item dropdown" style="margin-top:15px;">
+                    <a class="nav-link" href="' . $whitelabelSettings['whmcs_url'] . 'clientarea.php?action=services" target="_blank">
                         <i class="mdi mdi-account-box-multiple"></i> ' . _e('Members area', true) . '</a>
-                </div>';
+                </li>';
         });
 
     }
