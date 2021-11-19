623cb2ddfa6650eb98def259ebc978d35fb963fe
Bozhidar Slaveykov <bobi@microweber.com>
2021-11-19 11:42:52 +0200

    Update AdminEditProfileTest.php

--- tests/Browser/AdminEditProfileTest.php
+++ tests/Browser/AdminEditProfileTest.php
@@ -39,13 +39,21 @@ class AdminEditProfileTest extends DuskTestCase
             $browser->waitForText('First Name');
             $browser->pause(3000);
             $browser->scrollTo('#advanced-settings');
-            
+
             $browser->type('first_name', 'Visual');
             $browser->type('last_name', 'Test');
             $browser->type('phone', '08812345678');
             $browser->type('email', 'visualtest@microweber.com');
 
             $browser->press('Save');
+            $browser->pause(3000);
+
+            $findUser = User::where('email', 'visualtest@microweber.com')->first();
+
+            $this->assertEquals('Visual', $findUser->first_name);
+            $this->assertEquals('Test', $findUser->last_name);
+            $this->assertEquals('08812345678', $findUser->phone);
+
         });
     }
 }
