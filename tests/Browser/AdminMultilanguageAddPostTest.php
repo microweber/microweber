4b15ba95e1f6b3287fa25335c0bb25497279ff73
Bozhidar Slaveykov <bobi@microweber.com>
2021-11-26 13:37:54 +0200

    Update AdminMultilanguageAddPostTest.php

--- tests/Browser/AdminMultilanguageAddPostTest.php
+++ tests/Browser/AdminMultilanguageAddPostTest.php
@@ -46,9 +46,9 @@ class AdminMultilanguageAddPostTest extends DuskTestCase
             $bgDescription = 'Bulgarian description'.time();
             $arDescription = 'Arabic description'.time();
             $browser->within(new AdminContentMultilanguage, function ($browser) use ($bgDescription, $enDescription, $arDescription) {
-                $browser->fillDescription($bgDescription, 'BG_BG');
-                $browser->fillDescription($enDescription, 'EN_US');
-                $browser->fillDescription($arDescription, 'AR_SA');
+                $browser->fillDescription($bgDescription, 'bg_BG');
+                $browser->fillDescription($enDescription, 'en_US');
+                $browser->fillDescription($arDescription, 'ar_SA');
             });
 
             $browser->pause(1000);
