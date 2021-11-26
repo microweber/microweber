efa47957b5f3817bed31093ec8fb150c92c8b471
Bozhidar Slaveykov <bobi@microweber.com>
2021-11-26 17:36:36 +0200

    Update AdminMultilanguageAddProductTest.php

--- tests/Browser/AdminMultilanguageAddProductTest.php
+++ tests/Browser/AdminMultilanguageAddProductTest.php
@@ -20,6 +20,8 @@ class AdminMultilanguageAddProductTest extends DuskTestCase
     {
         $this->browse(function (Browser $browser) {
 
+            $quickTest = true;
+
             $browser->within(new AdminLogin, function ($browser) {
                 $browser->fillForm();
             });
@@ -41,11 +43,11 @@ class AdminMultilanguageAddProductTest extends DuskTestCase
 
                 $productDataMultilanguage[$locale] = [
                   'title'=> $productTitle,
-                  'url'=> str_slug($productTitle),
+               //   'url'=> str_slug($productTitle),
                   'content_body'=> 'Product content body ' . $locale . ' on lang' . time().rand(1111,9999),
-                  'content_meta_title'=> 'Product content meta title ' . $locale . ' on lang' . time().rand(1111,9999),
-                  'description'=> 'Product description ' . $locale . ' on lang' . time().rand(1111,9999),
-                  'content_meta_keywords'=> 'Product, content, meta, keywords, ' . $locale . ', on lang, ' . time().rand(1111,9999),
+                 // 'content_meta_title'=> 'Product content meta title ' . $locale . ' on lang' . time().rand(1111,9999),
+                 // 'description'=> 'Product description ' . $locale . ' on lang' . time().rand(1111,9999),
+                 // 'content_meta_keywords'=> 'Product, content, meta, keywords, ' . $locale . ', on lang, ' . time().rand(1111,9999),
                 ];
             }
 
@@ -58,72 +60,74 @@ class AdminMultilanguageAddProductTest extends DuskTestCase
                 }
             });
 
-            $productPrice = rand(1111, 9999);
-            $productSpecialPrice = $productPrice - rand(1, 9);
-            $productSku = rand(1111, 9999);
+            if ($quickTest == false) {
+                $productPrice = rand(1111, 9999);
+                $productSpecialPrice = $productPrice - rand(1, 9);
+                $productSku = rand(1111, 9999);
 
-            $productBarcode = rand(1111, 9999);
-            $productQuantity = rand(11, 99);
+                $productBarcode = rand(1111, 9999);
+                $productQuantity = rand(11, 99);
 
-            $browser->value('.js-product-price', $productPrice);
-            $browser->pause(1000);
+                $browser->value('.js-product-price', $productPrice);
+                $browser->pause(1000);
 
-            $browser->script("$('html, body').animate({ scrollTop: $('.js-product-pricing-card').offset().top - 30 }, 0);");
-            $browser->pause(3000);
-            $browser->script("$('.js-toggle-offer-price-button').click()");
-            $browser->pause(2000);
-            $browser->value('.js-product-special-price', $productSpecialPrice);
-            $browser->pause(1000);
+                $browser->script("$('html, body').animate({ scrollTop: $('.js-product-pricing-card').offset().top - 30 }, 0);");
+                $browser->pause(3000);
+                $browser->script("$('.js-toggle-offer-price-button').click()");
+                $browser->pause(2000);
+                $browser->value('.js-product-special-price', $productSpecialPrice);
+                $browser->pause(1000);
 
-            $browser->script("$('.js-invertory-sell-oos').click()");
-            $browser->script("$('.js-track-quantity-check').click()");
-            $browser->pause(1000);
-            $browser->select('.js-track-quantity-select-qty', $productQuantity);
-
-
-            $browser->value('.js-invertory-sku', $productSku);
-            $browser->pause(1000);
-
-            $browser->value('.js-invertory-barcode', $productBarcode);
-            $browser->pause(1000);
+                $browser->script("$('.js-invertory-sell-oos').click()");
+                $browser->script("$('.js-track-quantity-check').click()");
+                $browser->pause(1000);
+                $browser->select('.js-track-quantity-select-qty', $productQuantity);
 
 
-            $category4 = 'Shop';
-            $category4_1 = 'Clothes';
-            $category4_2 = 'T-shirts';
-            $category4_3 = 'Decor';
+                $browser->value('.js-invertory-sku', $productSku);
+                $browser->pause(1000);
 
-            $browser->within(new AdminContentCategorySelect, function ($browser) use ($category4, $category4_1, $category4_2, $category4_3) {
-                $browser->selectCategory($category4);
-                $browser->selectSubCategory($category4, $category4_1);
-                $browser->selectSubCategory($category4, $category4_2);
-                $browser->selectSubCategory($category4, $category4_3);
-            });
-
-            $tag1 = 'Tagdusk-' . time() . rand(100, 200);
-            $tag2 = 'Tagdusk-' . time() . rand(200, 300);
-            $tag3 = 'Tagdusk-' . time() . rand(300, 400);
-            $tag4 = 'Tagdusk-' . time() . rand(400, 500);
-
-            $browser->within(new AdminContentTagAdd, function ($browser) use ($tag1, $tag2, $tag3, $tag4) {
-                $browser->addTag($tag1);
-                $browser->addTag($tag2);
-                $browser->addTag($tag3);
-                $browser->addTag($tag4);
-            });
-
-            // add images to gallery
-            $browser->within(new AdminContentImageAdd, function ($browser) {
-                $browser->addImage(userfiles_path() . '/templates/default/img/patterns/img1.jpg');
-                $browser->addImage(userfiles_path() . '/templates/default/img/patterns/img2.jpg');
-                $browser->addImage(userfiles_path() . '/templates/default/img/patterns/img3.jpg');
-            });
+                $browser->value('.js-invertory-barcode', $productBarcode);
+                $browser->pause(1000);
 
 
-            $browser->within(new AdminContentCustomFieldAdd, function ($browser) {
-                $browser->addCustomField('dropdown', 'Dropdown');
-                $browser->addCustomField('text', 'Text Field');
-            });
+                $category4 = 'Shop';
+                $category4_1 = 'Clothes';
+                $category4_2 = 'T-shirts';
+                $category4_3 = 'Decor';
+
+                $browser->within(new AdminContentCategorySelect, function ($browser) use ($category4, $category4_1, $category4_2, $category4_3) {
+                    $browser->selectCategory($category4);
+                    $browser->selectSubCategory($category4, $category4_1);
+                    $browser->selectSubCategory($category4, $category4_2);
+                    $browser->selectSubCategory($category4, $category4_3);
+                });
+
+                $tag1 = 'Tagdusk-' . time() . rand(100, 200);
+                $tag2 = 'Tagdusk-' . time() . rand(200, 300);
+                $tag3 = 'Tagdusk-' . time() . rand(300, 400);
+                $tag4 = 'Tagdusk-' . time() . rand(400, 500);
+
+                $browser->within(new AdminContentTagAdd, function ($browser) use ($tag1, $tag2, $tag3, $tag4) {
+                    $browser->addTag($tag1);
+                    $browser->addTag($tag2);
+                    $browser->addTag($tag3);
+                    $browser->addTag($tag4);
+                });
+
+                // add images to gallery
+                $browser->within(new AdminContentImageAdd, function ($browser) {
+                    $browser->addImage(userfiles_path() . '/templates/default/img/patterns/img1.jpg');
+                    $browser->addImage(userfiles_path() . '/templates/default/img/patterns/img2.jpg');
+                    $browser->addImage(userfiles_path() . '/templates/default/img/patterns/img3.jpg');
+                });
+
+
+                $browser->within(new AdminContentCustomFieldAdd, function ($browser) {
+                    $browser->addCustomField('dropdown', 'Dropdown');
+                    $browser->addCustomField('text', 'Text Field');
+                });
+            }
 
             $browser->pause(1000);
             $browser->click('#js-admin-save-content-main-btn');
@@ -131,7 +135,7 @@ class AdminMultilanguageAddProductTest extends DuskTestCase
             $browser->pause(3000);
             $browser->waitForText('Editing product');
 
-            $findProduct = Product::where('title', $enTitle)->first();
+            $findProduct = Product::where('title', $productDataMultilanguage['en_US']['title'])->first();
 
             $browser->waitForLocation(route('admin.product.edit', $findProduct->id));
 
@@ -145,25 +149,22 @@ class AdminMultilanguageAddProductTest extends DuskTestCase
                 });
 
             }
+            $this->assertEquals($productDataMultilanguage['en_US']['title'], $findProduct->title);
+
+            foreach($productDataMultilanguage as $locale=>$productData) {
+                foreach($productData as $dataKey=>$dataValue) {
+                    $this->assertEquals($dataValue, $findProduct->multilanguage[$locale][$dataKey]);
+                }
+            }
 
             return;
-
-            $this->assertEquals($enTitle, $findProduct->title);
-            $this->assertEquals($enTitle, $findProduct->multilanguage['en_US']['title']);
-            $this->assertEquals($enDescription, $findProduct->multilanguage['en_US']['content_body']);
-
-            $this->assertEquals($bgTitle, $findProduct->multilanguage['bg_BG']['title']);
-            $this->assertEquals($bgDescription, $findProduct->multilanguage['bg_BG']['content_body']);
-
-            $this->assertEquals($arTitle, $findProduct->multilanguage['ar_SA']['title']);
-            $this->assertEquals($arDescription, $findProduct->multilanguage['ar_SA']['content_body']);
 
             $this->assertEquals($productPrice, $findProduct->price);
             $this->assertEquals($productSpecialPrice, $findProduct->special_price);
             $this->assertEquals($productQuantity, $findProduct->qty);
             $this->assertEquals($productSku, $findProduct->sku);
 
-            $this->assertEquals($findProduct->content_body, $enDescription);
+            $this->assertEquals($findProduct->content_body, $productDataMultilanguage['en_US']['content_body']);
             $this->assertEquals($findProduct->content_type, 'product');
             $this->assertEquals($findProduct->subtype, 'product');
 
