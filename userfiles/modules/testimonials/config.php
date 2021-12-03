b96d7ba9133bd6338996569eeedbce64c7b22274
Bozhidar Slaveykov <bobi@microweber.com>
2021-12-03 12:54:55 +0200

    Update config.php

--- userfiles/modules/testimonials/config.php
+++ userfiles/modules/testimonials/config.php
@@ -12,14 +12,11 @@ $config['tables'] = array(
     "testimonials" => array(
         'id' => "integer",
         'name' => "text",
-
-
         'content' => "text",
         'read_more_url' => "text",
         'created_on' => "datetime",
 		'project_name' => "text",
 	    'client_company' => "text",
-
 		'client_role' => "text",
 		'client_picture' => "text",
 		'client_website' => "text",
