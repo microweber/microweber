9ee1b01118509751d896967bb22302e291df0309
Bozhidar Slaveykov <bobi@microweber.com>
2021-12-06 13:27:09 +0200

    Update 2021_01_14_000003_add_new_fields_on_jobs_table.php

--- src/MicroweberPackages/Queue/migrations/2021_01_14_000003_add_new_fields_on_jobs_table.php
+++ src/MicroweberPackages/Queue/migrations/2021_01_14_000003_add_new_fields_on_jobs_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class UpdateJobsTable2 extends Migration
+class AddNewFieldsOnJobsTable extends Migration
 {
     /**
      * Run the migrations.
