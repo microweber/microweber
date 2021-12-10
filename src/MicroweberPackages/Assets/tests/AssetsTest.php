53fdc58ac1cc77f5aa9e4ae904c2cf1a1b1b2357
Bozhidar Slaveykov <bobi@microweber.com>
2021-12-10 14:16:30 +0200

    Create AssetsTest.php

---
+++ src/MicroweberPackages/Assets/tests/AssetsTest.php
@@ -0,0 +1,14 @@
+<?php
+namespace MicroweberPackages\Assets\tests;
+
+use MicroweberPackages\Core\tests\TestCase;
+
+class AssetsTest extends TestCase
+{
+    public function testAddGrup()
+    {
+
+        echo 1;
+
+    }
+}
