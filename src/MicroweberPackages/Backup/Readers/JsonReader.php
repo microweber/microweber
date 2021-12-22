5fc26fff8f8b3f8a5de4d16a2010fb54cecf604a
Peter Ivanov <peter@microweber.com>
2021-12-22 16:24:47 +0200

    update

--- src/MicroweberPackages/Backup/Readers/JsonReader.php
+++ src/MicroweberPackages/Backup/Readers/JsonReader.php
@@ -5,8 +5,6 @@ $dir = __DIR__;
 $dir = str_replace('Backup\Readers', '', $dir);
 $dir = str_replace('Backup/Readers', '', $dir);
 
-use \JsonMachine\Items;
-use JsonMachine\JsonDecoder\ExtJsonDecoder;
 use MicroweberPackages\Backup\EncodingFix;
 
 class JsonReader extends DefaultReader
@@ -15,12 +13,15 @@ class JsonReader extends DefaultReader
 	public function readData()
 	{
 		$readyJson = array();
-        $json = Items::fromFile($this->file,'', new ExtJsonDecoder(true));
+
+        $items = [];
+        $parser = new \JsonCollectionParser\Parser();
+        $parser->parse($this->file, function (array $item) use (&$items) {
+            $items[] = $item;
+        },true);
 
 
-		foreach ($json as $jsonKey => $jsonValue) {
-			$readyJson[$jsonKey] = $jsonValue;
-		}
+        $readyJson = $items;
 
 		if (isset($readyJson[0]['id'])) {
 		   return EncodingFix::decode(array("content"=>$readyJson));
