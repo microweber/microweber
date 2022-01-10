2b7cb22da6f7cb052c942de7d1876b798005b0bf
Nikolay <rachkuw@gmail.com>
2022-01-10 17:41:17 +0200

    update

--- userfiles/elements/ordered-list.php
+++ userfiles/elements/ordered-list.php
@@ -1,9 +1,9 @@
 <div class="col mw-default-layout-typography-1">
     <div class="element mb-1 ml-2" >
         <ol class="mw-default-layout-ordered-t1">
-            <li>Ordered list item</li>
-            <li>Ordered list item test</li>
-            <li>Ordered list item</li>
+            <li class="my-1">Ordered list item</li>
+            <li class="my-1">Ordered list item test</li>
+            <li class="my-1">Ordered list item</li>
         </ol>
     </div>
 </div>
