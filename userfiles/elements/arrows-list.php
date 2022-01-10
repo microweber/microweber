b9b925f0d1e39e4143e86127bdc450b93e0f69fd
Nikolay <rachkuw@gmail.com>
2022-01-10 17:51:04 +0200

    update

--- userfiles/elements/arrows-list.php
+++ userfiles/elements/arrows-list.php
@@ -1,9 +1,9 @@
 <div class="col">
     <div class="element">
         <ul class="mw-default-layout-un-ordered-arrows">
-            <li class="my-1">Unordered list with arrows</li>
-            <li class="my-1">Unordered list with arrows</li>
-            <li class="my-1">Unordered list with arrows</li>
+            <li class="my-2">Unordered list with arrows</li>
+            <li class="my-2">Unordered list with arrows</li>
+            <li class="my-2">Unordered list with arrows</li>
         </ul>
     </div>
 </div>
