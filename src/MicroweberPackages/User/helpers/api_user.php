e680e134a4215c979bfd2eaf58336be34c8fc6e6
Bozhidar Slaveykov <bobi@microweber.com>
2022-01-19 11:35:10 +0200

    search_authors only admins

--- src/MicroweberPackages/User/helpers/api_user.php
+++ src/MicroweberPackages/User/helpers/api_user.php
@@ -63,9 +63,9 @@ api_expose_admin('users/forgot_password_email_send_test', function () {
 
 });
 
-api_expose('users/search_authors', function ($params = false) {
+api_expose_admin('users/search_authors', function ($params = false) {
 
-    $return = array();
+    $return = array(); 
 
     $kw = false;
     if (isset($params['kw'])) {
