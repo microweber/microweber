45510f586e49f37d149de7498d0e3bda9f026b59
Bozhidar Slaveykov <bobi@microweber.com>
2022-01-26 14:38:45 +0200

    update

---
+++ userfiles/modules/admin/export_tool/config.php
@@ -0,0 +1,16 @@
+<?php
+$config = array();
+$config['name'] = "Export Tool";
+$config['author'] = "Microweber";
+
+$config['categories'] = "admin";
+$config['version'] = 0.3;
+$config['ui_admin'] = true;
+$config['position'] = 99;
+
+$config['permissions'] = [
+    'index' => 'module.admin.export_tool.index',
+    'create' => 'module.admin.export_tool.create',
+    'edit' => 'module.admin.export_tool.edit',
+    'destroy' => 'module.admin.export_tool.destroy'
+];
