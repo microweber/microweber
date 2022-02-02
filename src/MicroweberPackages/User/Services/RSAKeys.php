d61ad9db07ef09652a3deb24c26274da2ded1493
Bozhidar Slaveykov <bobi@microweber.com>
2022-02-02 12:42:48 +0200

    Update RSAKeys.php

--- src/MicroweberPackages/User/Services/RSAKeys.php
+++ src/MicroweberPackages/User/Services/RSAKeys.php
@@ -1,13 +1,13 @@
 <?php
 
-
-
 namespace MicroweberPackages\User\Services;
 
 use phpseclib3\Crypt\RSA;
 
-
-class  RSAKeys  extends RSA
+class RSAKeys extends RSA
 {
-
+    public function toString($type, array $options = [])
+    {
+        // TODO: Implement toString() method.
+    }
 }
