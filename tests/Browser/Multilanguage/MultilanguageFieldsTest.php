38d8f23888c0d4fd3032fb2e35f3e32abac83058
Bozhidar Slaveykov <bobi@microweber.com>
2022-02-07 15:52:32 +0200

    Update MultilanguageFieldsTest.php

--- tests/Browser/Multilanguage/MultilanguageFieldsTest.php
+++ tests/Browser/Multilanguage/MultilanguageFieldsTest.php
@@ -12,12 +12,11 @@ class MultilanguageFieldsTest extends DuskTestCaseMultilanguage
 {
     public $siteUrl = 'http://127.0.0.1:8000/';
 
-    public function testSwitchLanguageFields()
+    public function testLiveEdit()
     {
         $siteUrl = $this->siteUrl;
 
         $this->browse(function (Browser $browser) use($siteUrl) {
-
 
             $browser->within(new AdminLogin, function ($browser) {
                 $browser->fillForm();
@@ -27,45 +26,8 @@ class MultilanguageFieldsTest extends DuskTestCaseMultilanguage
             $browser->visit($siteUrl.'admin/view:modules/load_module:multilanguage?dusk=1');
             $browser->pause(1000);
 
-            // Test bulgarian switch
-            $browser->select('#js-field-box-4 .input-group-append select', 'bg_BG');
-            $browser->pause(1000);
-
-            $browser->script("
-                var jsFieldBox4 = $('#js-field-box-4 .input-group input:visible');
-                alert(jsFieldBox4.val());
-            ");
-            $browser->pause(1000);
-            $browser->assertDialogOpened('Текст на български');
-            $browser->acceptDialog();
-            $browser->pause(1000);
-
-            // Test arabian switch
-            $browser->select('#js-field-box-4 .input-group-append select', 'ar_SA');
-            $browser->pause(1000);
-
-            $browser->script("
-                var jsFieldBox4 = $('#js-field-box-4 .input-group input:visible');
-                alert(jsFieldBox4.val());
-            ");
-            $browser->pause(1000);
-            $browser->assertDialogOpened('ARABSKI BRAT');
-            $browser->acceptDialog();
-            $browser->pause(1000);
 
 
-            // Test english switch
-            $browser->select('#js-field-box-4 .input-group-append select', 'en_US');
-            $browser->pause(1000);
-
-            $browser->script("
-                var jsFieldBox4 = $('#js-field-box-4 .input-group input:visible');
-                alert(jsFieldBox4.val());
-            ");
-            $browser->pause(1000);
-            $browser->assertDialogOpened('Text on English');
-            $browser->acceptDialog();
-            $browser->pause(1000);
 
         });
 
