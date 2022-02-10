69fc8def8f69a6f64d2139cbb2669a3188d6df04
Bozhidar Slaveykov <bobi@microweber.com>
2022-02-10 14:45:35 +0200

    Update web.php

--- src/MicroweberPackages/User/routes/web.php
+++ src/MicroweberPackages/User/routes/web.php
@@ -24,7 +24,7 @@ Route::name('admin.')
     ])
     ->namespace('\MicroweberPackages\User\Http\Controllers\Admin')
     ->group(function () {
-        Route::resource('user', 'UserController',['except' => ['index', 'show']]);
+        Route::resource('user', 'UserController',['except' => ['show','edit', 'create']]);
     });
 
 Route::namespace('\MicroweberPackages\User\Http\Controllers')->middleware(['web'])->group(function () {
