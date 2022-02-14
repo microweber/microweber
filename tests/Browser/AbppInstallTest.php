0c72386d073cf756d390ec4723b762abef1db338
Peter Ivanov <peter@microweber.com>
2022-02-14 17:36:23 +0200

    update

--- tests/Browser/AbppInstallTest.php
+++ tests/Browser/AbppInstallTest.php
@@ -20,6 +20,11 @@ class AbppInstallTest extends DuskTestCase
 
         $this->browse(function (Browser $browser) use ($siteUrl) {
 
+            $browser->within(new AdminMakeInstall(), function ($browser) {
+                $browser->makeInstallation();
+            });
+
+
             $browser->within(new AdminLogin(), function ($browser) {
                 $browser->fillForm();
             });
