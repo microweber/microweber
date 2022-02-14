d072872a6f40d090f322c41a2ad55f405f6b1f48
Peter Ivanov <peter@microweber.com>
2022-02-14 14:10:03 +0200

    update

--- tests/DuskTestCase.php
+++ tests/DuskTestCase.php
@@ -5,8 +5,12 @@ namespace Tests;
 use Facebook\WebDriver\Chrome\ChromeOptions;
 use Facebook\WebDriver\Remote\DesiredCapabilities;
 use Facebook\WebDriver\Remote\RemoteWebDriver;
+use Illuminate\Contracts\Console\Kernel;
+use Illuminate\Support\Facades\Auth;
 use Laravel\Dusk\Browser;
 use Laravel\Dusk\TestCase as BaseTestCase;
+use MicroweberPackages\User\Models\User;
+use Tests\Browser\Components\ChekForJavascriptErrors;
 
 abstract class DuskTestCase extends BaseTestCase
 {
@@ -25,6 +29,8 @@ abstract class DuskTestCase extends BaseTestCase
         if (! static::runningInSail()) {
             static::startChromeDriver();
         }
+
+
     }
 
     /**
@@ -65,6 +71,64 @@ abstract class DuskTestCase extends BaseTestCase
         );
     }
 
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->testInstallation();
+    }
+
+    public function testInstallation()
+    {
+        $siteUrl = $this->siteUrl;
+
+        $this->browse(function (Browser $browser) use($siteUrl) {
+
+            if (mw_is_installed()) {
+                $this->assertTrue(true);
+                return true;
+            }
+
+            /* $deleteDbFiles = [];
+             $deleteDbFiles[] = dirname(dirname(__DIR__)) . DS . 'config/microweber.php';
+             $deleteDbFiles[] = dirname(dirname(__DIR__)) . DS . 'storage/127_0_0_1.sqlite';
+             foreach ($deleteDbFiles as $file) {
+                 if (is_file($file)) {
+                     unlink($file);
+                 }
+             }*/
+
+            $browser->visit($siteUrl)->assertSee('install');
+
+            $browser->within(new ChekForJavascriptErrors(), function ($browser) {
+                $browser->validate();
+            });
+
+
+            // Fill the install fields
+            $browser->type('admin_username', '1');
+            $browser->type('admin_password', '1');
+            $browser->type('admin_password2', '1');
+            $browser->type('admin_email', 'bobi@microweber.com');
+
+            $browser->pause(300);
+            $browser->select('#default_template', 'new-world');
+
+            $browser->pause(100);
+            $browser->click('@install-button');
+
+            $browser->pause(20000);
+
+            clearcache();
+
+            $user = User::where('is_admin', '=', '1')->first();
+            Auth::login($user);
+
+        });
+    }
+
+
+
+
     /**
      * Determine whether the Dusk command has disabled headless mode.
      *
@@ -89,5 +153,7 @@ abstract class DuskTestCase extends BaseTestCase
         }
 
     }
+
+
 
 }
