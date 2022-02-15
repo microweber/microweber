e4b365c1f95383d97a5ebeabf6fe3a91fc99a523
Nikolay <rachkuw@gmail.com>
2022-02-15 15:08:44 +0200

    Update index.blade.php

--- src/MicroweberPackages/Shop/resources/views/index.blade.php
+++ src/MicroweberPackages/Shop/resources/views/index.blade.php
@@ -30,7 +30,7 @@
             </div>
             <div class="row">
                 @foreach($products->results() as $product)
-                    <div class="col-4 mb-5">
+                    <div class="col-xl-4 col-lg-6 col-sm-12 mb-5">
                         <a href="{{content_link($product->id)}}">
                             <img src="{{$product->thumbnail(800,800, true)}}" alt="">
 
