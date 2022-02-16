7b9bfbba70a3a18f0642921db08dd7ec5ab1d9c6
Peter Ivanov <peter@microweber.com>
2022-02-16 11:42:51 +0200

    update

--- tests/Browser/Components/EnvCheck.php
+++ tests/Browser/Components/EnvCheck.php
@@ -5,6 +5,7 @@ namespace Tests\Browser\Components;
 use Facebook\WebDriver\WebDriverBy;
 use Laravel\Dusk\Browser;
 use Laravel\Dusk\Component as BaseComponent;
+use MicroweberPackages\User\Models\User;
 use PHPUnit\Framework\Assert as PHPUnit;
 
 class EnvCheck extends BaseComponent
@@ -42,15 +43,19 @@ class EnvCheck extends BaseComponent
 
     public function checkEnvName(Browser $browser)
     {
-//        if (mw_is_installed()) {
-//            $environment = app()->environment();
-//            $browserEnvironment = $browser->visit(route('l5-swagger.dusk.env'))
-//                ->element('')->getText();
-//
-//            PHPUnit::assertEquals($environment, $browserEnvironment,
-//                "Browser environment [{$browserEnvironment}]
-//            diverge from the given environment [{$environment}]");
-//        }
+        if (mw_is_installed()) {
+
+            $user = User::where('username', 1)->first();
+
+
+            $environment = app()->environment();
+            $browserEnvironment = $browser->loginAs($user)->visit(route('l5-swagger.dusk.env'))
+                ->element('')->getText();
+
+            PHPUnit::assertEquals($environment, $browserEnvironment,
+                "Browser environment [{$browserEnvironment}]
+            diverge from the given environment [{$environment}]");
+        }
 
     }
 
