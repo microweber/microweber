1e1f5e41be6f71572025c0794c2deb0d1182fa36
Bozhidar Slaveykov <bobi@microweber.com>
2022-02-17 16:33:56 +0200

    Update ZipReader.php

--- src/MicroweberPackages/Import/Formats/ZipReader.php
+++ src/MicroweberPackages/Import/Formats/ZipReader.php
@@ -18,6 +18,11 @@ class ZipReader extends DefaultReader
         if (is_dir($backupLocation)) {
             rmdir_recursive($backupLocation, false);
         }
+
+        $cacheLocation = backup_location() . 'cache_backup_zip';
+        if (is_dir($cacheLocation)) {
+            rmdir_recursive($cacheLocation, false);
+        }
     }
 
 	/**
