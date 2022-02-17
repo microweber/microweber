198595542df2f1a4dc1f044cee789ea81b0727b0
Bozhidar Slaveykov <bobi@microweber.com>
2022-02-17 11:58:17 +0200

    Create BrowsePagesForBrokenTagsTest.php

---
+++ tests/Browser/SlowTests/BrowsePagesForBrokenTagsTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tests\Browser\SlowTests;
+
+use Laravel\Dusk\Browser;
+use Tests\Browser\Components\AdminLogin;
+use Tests\DuskTestCase;
+
+class BrowsePagesForBrokenTagsTest extends DuskTestCase
+{
+    public function testPages()
+    {
+        $this->browse(function (Browser $browser) {
+
+
+
+
+            
+
+        });
+    }
+}
