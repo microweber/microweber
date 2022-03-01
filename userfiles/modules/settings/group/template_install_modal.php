7a077dc26415aa7d2470e06efa72b904ed2f06c1
Bozhidar Slaveykov <bobi@microweber.com>
2022-03-01 15:48:48 +0200

    update

---
+++ userfiles/modules/settings/group/template_install_modal.php
@@ -0,0 +1,52 @@
+<?php must_have_access(); ?>
+
+<link rel="stylesheet" href="<?php echo modules_url() . '/admin/backup/css/style.css?v=' .time(); ?>" type="text/css"/>
+
+<div class="mw-backup-restore">
+
+    <div class="mw-backup-restore-options">
+
+        <h2 style="font-weight: bold">How do you like to apply this template?</h2>
+        <br/>
+
+        <label class="mw-ui-check mw-backup-restore-option">
+            <div class="option-radio">
+                <input type="radio" name="import_by_type" value="1" checked="checked "/>
+                <span></span>
+            </div>
+            <h3>Use template with current content</h3>
+            <p>Change the website template without any content changes</p>
+        </label>
+
+        <label class="mw-ui-check mw-backup-restore-option active">
+            <div class="option-radio">
+                <input type="radio" name="import_by_type" value="2"/>
+                <span></span>
+            </div>
+            <h3>Import content, media and css files</h3>
+            <p>Import the default content, media and css files from template</p>
+        </label>
+
+
+        <label class="mw-ui-check mw-backup-restore-option">
+            <div class="option-radio">
+                <input type="radio" name="import_by_type" value="3"/>
+                <span></span>
+            </div>
+            <h3>Import only media and css</h3>
+            <p>This option will import only the media and css files</p>
+        </label>
+
+    </div>
+
+    <div style="margin-bottom:20px;" class="js-backup-restore-installation-language-wrapper"></div>
+    <div class="backup-restore-modal-log-progress"></div>
+
+    <div class="mw-backup-restore-buttons">
+        <a class="btn btn-link button-cancel">Close</a>
+        <button class="btn btn-primary btn-rounded button-start" onclick="mw.change_template()" type="submit">
+            Change Template
+        </button>
+    </div>
+
+</div>
