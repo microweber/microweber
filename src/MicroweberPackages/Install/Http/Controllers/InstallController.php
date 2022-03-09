6ed0334ded1ef85b52962a2ae821b42e08506551
Bozhidar Slaveykov <bobi@microweber.com>
2022-03-09 12:03:23 +0200

    Update InstallController.php

--- src/MicroweberPackages/Install/Http/Controllers/InstallController.php
+++ src/MicroweberPackages/Install/Http/Controllers/InstallController.php
@@ -545,10 +545,10 @@ class InstallController extends Controller
                 if ($latestVersion['type'] !== 'microweber-template') {
                     continue;
                 }
-               /* if (isset($latestVersion['dist']['type']) && $latestVersion['dist']['type'] == 'zip') {
+                if (isset($latestVersion['dist']['type']) && $latestVersion['dist']['type'] == 'zip') {
                     $ready[] = MicroweberComposerPackage::format($latestVersion);
-                }*/
-                $ready[] = MicroweberComposerPackage::format($latestVersion);
+                }
+               // $ready[] = MicroweberComposerPackage::format($latestVersion);
             }
         }
 
@@ -559,8 +559,7 @@ class InstallController extends Controller
     {
         $runner = new MicroweberComposerClient();
         $results = $runner->requestInstall(['require_name' => $package_name]);
-
-       // $runner->requestInstall($results['form_data_module_params']);
+        $runner->requestInstall($results['form_data_module_params']);
 
         return $results;
     }
