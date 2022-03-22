6be49dd5d06a38cc37fddfc1727798bbb815b42c
Bozhidar Slaveykov <bobi@microweber.com>
2022-03-22 17:22:51 +0200

    Create CartTest.php

---
+++ src/MicroweberPackages/Cart/tests/CartTest.php
@@ -0,0 +1,12 @@
+<?php
+namespace MicroweberPackages\Admin\tests;
+
+use MicroweberPackages\Core\tests\TestCase;
+
+class CartTest extends TestCase
+{
+    public function testCart()
+    {
+        $this->assertTrue(true);
+    }
+}
