60a48f44416962d514b33f4fad3e359b6c2b5ac4
Bozhidar Slaveykov <bobi@microweber.com>
2022-03-30 21:43:00 +0300

    Update import.php

--- userfiles/modules/admin/import_export_tool/modals/import.php
+++ userfiles/modules/admin/import_export_tool/modals/import.php
@@ -6,24 +6,24 @@
         <div class="import-modal-vendor-btn-image">
             <img src="<?php echo modules_url(); ?>/admin/import_export_tool/images/wordpress.svg"/>
         </div>
-        <div class="import-modal-vendor-btn-title">Wordpress</div>
+        <div class="import-modal-vendor-btn-title"><?php _e("Wordpress"); ?></div>
     </div>
     <div class="import-modal-vendor-btn">
         <div class="import-modal-vendor-btn-image">
             <img src="<?php echo modules_url(); ?>/admin/import_export_tool/images/woocommerce.svg"/>
         </div>
-        <div class="import-modal-vendor-btn-title">WooCommerce</div>
+        <div class="import-modal-vendor-btn-title"><?php _e("WooCommerce"); ?></div>
     </div>
     <div class="import-modal-vendor-btn">
         <div class="import-modal-vendor-btn-image">
             <img src="<?php echo modules_url(); ?>/admin/import_export_tool/images/shopify.svg"/>
         </div>
-        <div class="import-modal-vendor-btn-title">Shopify</div>
+        <div class="import-modal-vendor-btn-title"><?php _e("Shopify"); ?></div>
     </div>
     <div class="import-modal-vendor-btn">
         <div class="import-modal-vendor-btn-image">
             <img src="<?php echo modules_url(); ?>/admin/import_export_tool/images/feed.svg"/>
         </div>
-        <div class="import-modal-vendor-btn-title">Feed</div>
+        <div class="import-modal-vendor-btn-title"><?php _e("Feed"); ?></div>
     </div>
 </div>
