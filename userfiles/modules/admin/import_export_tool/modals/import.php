24f3c6c1c71759b3290c890228cd6ccc99c43c1b
Bozhidar Slaveykov <bobi@microweber.com>
2022-03-30 22:06:31 +0300

    Update import.php

--- userfiles/modules/admin/import_export_tool/modals/import.php
+++ userfiles/modules/admin/import_export_tool/modals/import.php
@@ -1,26 +1,36 @@
-<h4>
+<script>
+function importSelectType(type)
+{
+    if (type == 'wordpress') {
+
+    }
+
+    $('.import-modal-vendors').fadeOut();
+}
+</script>
+<h5>
     <?php _e("Select type of import"); ?>
-</h4>
+</h5>
 <div class="import-modal-vendors">
-    <div class="import-modal-vendor-btn">
+    <div class="import-modal-vendor-btn" onclick="importSelectType('wordpress')">
         <div class="import-modal-vendor-btn-image">
             <img src="<?php echo modules_url(); ?>/admin/import_export_tool/images/wordpress.svg"/>
         </div>
         <div class="import-modal-vendor-btn-title"><?php _e("Wordpress"); ?></div>
     </div>
-    <div class="import-modal-vendor-btn">
+    <div class="import-modal-vendor-btn" onclick="importSelectType('woocommerce')">
         <div class="import-modal-vendor-btn-image">
             <img src="<?php echo modules_url(); ?>/admin/import_export_tool/images/woocommerce.svg"/>
         </div>
         <div class="import-modal-vendor-btn-title"><?php _e("WooCommerce"); ?></div>
     </div>
-    <div class="import-modal-vendor-btn">
+    <div class="import-modal-vendor-btn" onclick="importSelectType('shopify')">
         <div class="import-modal-vendor-btn-image">
             <img src="<?php echo modules_url(); ?>/admin/import_export_tool/images/shopify.svg"/>
         </div>
         <div class="import-modal-vendor-btn-title"><?php _e("Shopify"); ?></div>
     </div>
-    <div class="import-modal-vendor-btn">
+    <div class="import-modal-vendor-btn" onclick="importSelectType('Feed')">
         <div class="import-modal-vendor-btn-image">
             <img src="<?php echo modules_url(); ?>/admin/import_export_tool/images/feed.svg"/>
         </div>
