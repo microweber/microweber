f090c362852ef1eb870fc01dbf870aa34173e8ce
Bozhidar Slaveykov <bobi@microweber.com>
2022-04-12 17:26:12 +0300

    Update index.blade.php

--- userfiles/modules/admin/import_export_tool/src/resources/views/admin/index.blade.php
+++ userfiles/modules/admin/import_export_tool/src/resources/views/admin/index.blade.php
@@ -173,27 +173,6 @@
                                                    id="feed_download_image_0" value="0"> <label
                                                 for="feed_download_image_0">No</label>
 
-
-
-
-                                       {{--     <div class="form-check form-switch">
-                                                <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault">
-                                                <label class="form-check-label" for="flexSwitchCheckDefault">Default switch checkbox input</label>
-                                            </div>
-                                            <div class="form-check form-switch">
-                                                <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckChecked" checked>
-                                                <label class="form-check-label" for="flexSwitchCheckChecked">Checked switch checkbox input</label>
-                                            </div>
-                                            <div class="form-check form-switch">
-                                                <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDisabled" disabled>
-                                                <label class="form-check-label" for="flexSwitchCheckDisabled">Disabled switch checkbox input</label>
-                                            </div>
-                                            <div class="form-check form-switch">
-                                                <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckCheckedDisabled" checked disabled>
-                                                <label class="form-check-label" for="flexSwitchCheckCheckedDisabled">Disabled checked switch checkbox input</label>
-                                            </div>
---}}
-
                                         </td>
                                     </tr>
                                     <tr>
