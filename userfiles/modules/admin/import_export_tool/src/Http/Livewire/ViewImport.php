019df776ab017d8a519645874b2b6d61e6b376d8
Bozhidar Slaveykov <bobi@microweber.com>
2022-04-12 16:52:48 +0300

    update

---
+++ userfiles/modules/admin/import_export_tool/src/Http/Livewire/ViewImport.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace MicroweberPackages\Modules\Admin\ImportExportTool\Http\Livewire;
+
+use Livewire\Component;
+
+class ViewImport extends Component
+{
+    public function save()
+    {
+
+    }
+
+    public function download()
+    {
+
+    }
+
+    public function upload()
+    {
+
+    }
+
+    public function render()
+    {
+        return view('import_export_tool::admin.livewire-viewimport');
+    }
+
+}
