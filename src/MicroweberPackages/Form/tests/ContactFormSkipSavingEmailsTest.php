ea936c97a107adc231efc0432b27d4adaaccc250
Bozhidar Slaveykov <bobi@microweber.com>
2022-04-15 16:41:03 +0300

    Update ContactFormSkipSavingEmailsTest.php

--- src/MicroweberPackages/Form/tests/ContactFormSkipSavingEmailsTest.php
+++ src/MicroweberPackages/Form/tests/ContactFormSkipSavingEmailsTest.php
@@ -7,11 +7,42 @@ use MicroweberPackages\Core\tests\TestCase;
 class ContactFormSkipSavingEmailsTest extends TestCase
 {
 
-    public function testSkipSavingEmails()
+    public function testQuickEmail()
     {
         $params = array();
         $params['for_id'] = '1234-skip-saving';
         $params['for'] = 'test-skip-saving';
+
+        $params['message'] = 'HELLO CONTACT FORM GLBOAL! THIS IS MY GLOBAL MESSAGE';
+        $params['email'] = 'unit.b.slaveykov@unittest-global.com';
+        $params['Company'] = 'CloudVisionLtd-Global';
+        $params['Phone'] = '0885451012-Global';
+        $params['Your Name'] = 'Bozhidar Veselinov Slaveykov';
+        $params['module_name'] = 'contact_form_global_settings';
+
+        // Disable captcha
+        save_option(array(
+            'option_group' => $params['for_id'],
+            'option_key' => 'disable_captcha',
+            'option_value' => 'y'
+        ));
+
+        $formResponse = mw()->forms_manager->post($params);
+        $this->assertArrayHasKey('success', $formResponse);
+
+        $params = array();
+        $params['id'] = $formResponse['id'];
+        $response = mw()->forms_manager->get_entires($params);
+
+        $this->assertEquals($response[0]['id'], $formResponse['id']);
+
+    }
+
+    public function testSkipSavingEmails()
+    {
+        $params = array();
+        $params['for_id'] = '1234567-skip-saving';
+        $params['for'] = '1234567-test-skip-saving';
 
         $params['message'] = 'HELLO CONTACT FORM GLBOAL! THIS IS MY GLOBAL MESSAGE';
         $params['email'] = 'unit.b.slaveykov@unittest-global.com';
@@ -33,10 +64,14 @@ class ContactFormSkipSavingEmailsTest extends TestCase
             'option_value' => 'y'
         ));
 
-        $response = mw()->forms_manager->post($params);
-        $this->assertArrayHasKey('success', $response);
-        
+        $formResponse = mw()->forms_manager->post($params);
+        $this->assertArrayHasKey('success', $formResponse);
 
+        $params = array();
+        $params['id'] = $formResponse['id'];
+        $response = mw()->forms_manager->get_entires($params);
+
+        $this->assertFalse($response);
 
     }
 
