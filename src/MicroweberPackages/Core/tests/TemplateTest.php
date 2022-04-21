3d9b8984d2df7a24adc42025fc137294273b328c
Peter Ivanov <peter@microweber.com>
2022-04-21 14:54:50 +0300

    update

--- src/MicroweberPackages/Core/tests/TemplateTest.php
+++ src/MicroweberPackages/Core/tests/TemplateTest.php
@@ -5,10 +5,14 @@ namespace MicroweberPackages\Core\tests;
 
 class TemplateTest extends TestCase
 {
+    public $template_name = 'default';
+
     public function testGetTemplateName()
     {
+        save_option('current_template', $this->template_name,'template');
+
         $current_template = mw()->template->name();
-        $this->assertEquals('default', $current_template);
+        $this->assertEquals($this->template_name, $current_template);
     }
 
     public function testGetAllTemplates()
