4775e926694a55e50ff2754dc48abe456396dd87
Bozhidar Slaveykov <bobi@microweber.com>
2022-04-26 21:19:29 +0300

    update

--- userfiles/modules/admin/import_export_tool/src/tests/ImportExportToolTest.php
+++ userfiles/modules/admin/import_export_tool/src/tests/ImportExportToolTest.php
@@ -2,6 +2,10 @@
 namespace MicroweberPackages\Modules\Admin\ImportExportTool\tests;
 
 use MicroweberPackages\Core\tests\TestCase;
+use MicroweberPackages\Import\Import;
+use MicroweberPackages\Modules\Admin\ImportExportTool\ImportMapping\FeedMapToArray;
+use MicroweberPackages\Modules\Admin\ImportExportTool\ImportMapping\HtmlDropdownMappingRecursiveTable;
+use MicroweberPackages\Modules\Admin\ImportExportTool\Models\ImportFeed;
 
 class ImportExportToolTest extends TestCase
 {
@@ -11,9 +15,28 @@ class ImportExportToolTest extends TestCase
         $zip = new \ZipArchive();
         $zip->open(__DIR__.'/simple-data.zip');
         $content = $zip->getFromName('data-example-1.csv');
-        //$zip->close();
+        $zip->close();
 
-        dump($zip);
+        $this->assertNotEmpty($content);
+
+        $importFeed = new ImportFeed();
+        $importFeed->name = rand(111,999) . 'simple-feed';
+        $importFeed->save();
+
+
+      /*  $feedMapToArray = new FeedMapToArray();
+        $feedMapToArray->setImportFeedId($this->import_feed->id);
+        $preparedData = $feedMapToArray->toArray();
+
+        $dropdownMapping = new HtmlDropdownMappingRecursiveTable();
+        $dropdownMapping->setContent(['Data'=>$content]);
+        $dropdownMapping->setContentParentTags('Data');
+
+        $html = $dropdownMapping->render();
+
+
+        dd($html);*/
+
 
     }
 }
