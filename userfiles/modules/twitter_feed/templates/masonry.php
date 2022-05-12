b76c279cc178719971817c6d9741f843b93a7b04
Alex <raikov@ymail.com>
2022-05-12 16:48:38 +0300

    masonry twitter

--- userfiles/modules/twitter_feed/templates/masonry.php
+++ userfiles/modules/twitter_feed/templates/masonry.php
@@ -12,6 +12,9 @@ description: Masonry skin for Twitter Feed
 
 ?>
 <script>
+    mw.require("<?php print modules_url(); ?>pictures/js/masonry.pkgd.min.js");
+</script>
+<script>
 
     $( document ).ready(function() {
         window.twttr = (function(d, s, id) {
@@ -31,7 +34,7 @@ description: Masonry skin for Twitter Feed
             return t;
         }(document, "script", "twitter-wjs"));
 
-        twttr.widgets.load()
+        // twttr.widgets.load()
 
 
         // When widget is ready, run masonry
@@ -39,25 +42,59 @@ description: Masonry skin for Twitter Feed
             twttr.events.bind('loaded', function (event) {
                 $('.grid-twitter-masonry').masonry({
                     itemSelector : '.grid-item-masonry-twitter',
-                    columnWidth : 300,
-                    gutter: 20
+                    columnWidth : '.grid-item-masonry-twitter-sizer',
+                    gutter: 20,
+                    fitWidth: true
                 });
+                var time = null;
+                window.addEventListener('resize', function () {
+                    clearTimeout(time);
+                    time = setTimeout(function (){
+                        $('.grid-twitter-masonry').masonry({
+                            itemSelector : '.grid-item-masonry-twitter',
+                            columnWidth : '.grid-item-masonry-twitter-sizer',
+                            gutter: 20,
+                            fitWidth: true
+                        });
+                    }, 700)
+                })
             });
         });
-        mw.require("<?php print modules_url(); ?>pictures/js/masonry.pkgd.min.js", true);
+
     });
 </script>
 
 <style>
+    .grid-twitter-masonry .twitter-tweet{
+        max-width: 100% !important;
+    }
+    .grid-twitter-masonry iframe{
+        width: 100% !important;
+    }
     .grid-twitter-masonry {
         width:100%;
-        max-width: 940px;
+        /*max-width: 940px;*/
         margin: 50px auto;
+        max-width: 100%;
+
     }
-    .grid-item-masonry-twitter {
-        float:left;
-        padding: 0px 3px;
-        width: 300px;
+
+    .grid-item-masonry-twitter,
+    .grid-item-masonry-twitter-sizer{
+        width: 30%;
+    }
+
+    @media (max-width: 1400px) {
+        .grid-item-masonry-twitter,
+        .grid-item-masonry-twitter-sizer{
+            width: 48%;
+        }
+    }
+    @media (max-width: 700px) {
+        .grid-item-masonry-twitter,
+        .grid-item-masonry-twitter-sizer{
+            width: 100%;
+        }
     }
 
     .twitter-tweet.twitter-tweet-rendered {
@@ -74,13 +111,25 @@ description: Masonry skin for Twitter Feed
 
 
 <?php if ($items): ?>
+<div class="grid-twitter-masonry">
+    <div class="grid-item-masonry-twitter-sizer"></div>
     <?php foreach ($items as $tweet): ?>
-        <div class="grid-twitter-masonry" data-masonry='{ "itemSelector": ".grid-item", "columnWidth": 300, "gutter":20 }'>
-            <div class="grid-item-masonry-twitter">
-
-                <blockquote data-id="<?php print $tweet['id']; ?>" data-align="<?php print $twitter_feed_align ?>" data-cards="<?php print $twitter_feed_cards ?>" data-conversation="<?php print $twitter_feed_conversation ?>" data-width="<?php print $twitter_feed_width ?>" data-theme="<?php print $twitter_feed_theme ?>" class="twitter-tweet"><p lang="en" dir="ltr"><a href="<?php print $tweet['url']; ?>"><span class="d-none"><?php print $tweet['text']; ?></span></a></p></blockquote>
-
-            </div>
+        <div class="grid-item-masonry-twitter">
+            <blockquote
+                data-id="<?php print $tweet['id']; ?>"
+                data-align="<?php print $twitter_feed_align ?>"
+                data-cards="<?php print $twitter_feed_cards ?>"
+                data-conversation="<?php print $twitter_feed_conversation ?>"
+                data-width="<?php print $twitter_feed_width ?>"
+                data-theme="<?php print $twitter_feed_theme ?>"
+                class="twitter-tweet">
+                    <p lang="en" dir="ltr">
+                        <a href="<?php print $tweet['url']; ?>">
+                            <span class="d-none"><?php print $tweet['text']; ?></span>
+                        </a>
+                    </p>
+            </blockquote>
         </div>
     <?php endforeach; ?>
+</div>
 <?php endif; ?>
