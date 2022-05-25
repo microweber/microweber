6f35dbe7722ecfbc946fc9d3bc08eae43e335538
Alex <raikov@ymail.com>
2022-05-25 17:52:55 +0300

    title, text placeholders

--- userfiles/modules/text/index.php
+++ userfiles/modules/text/index.php
@@ -1,1 +1,1 @@
-<p class="element lipsum"><?php _e('This text is set by default and is suitable for edit in real time. By default the drag and drop core feature will allow you to position it anywhere on the site. Get creative & Make Web.'); ?></p>
+<p class="element lipsum" data-mwplaceholder="<?php _e('This text is set by default and is suitable for edit in real time. By default the drag and drop core feature will allow you to position it anywhere on the site. Get creative & Make Web.'); ?>"></p>
