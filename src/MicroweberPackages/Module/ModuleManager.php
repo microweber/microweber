78b9c9d4edf6141a2836c8a1e5fc9a374cd43e33
Bozhidar Slaveykov <bobi@microweber.com>
2022-06-16 12:29:18 +0300

    Update ModuleManager.php

--- src/MicroweberPackages/Module/ModuleManager.php
+++ src/MicroweberPackages/Module/ModuleManager.php
@@ -844,8 +844,6 @@ class ModuleManager
      */
     public function templates($module_name, $template_name = false, $get_settings_file = false)
     {
-
-
         $module_name = str_replace('admin', '', $module_name);
         $module_name_l = $this->locate($module_name);
         $replace_paths = array();
