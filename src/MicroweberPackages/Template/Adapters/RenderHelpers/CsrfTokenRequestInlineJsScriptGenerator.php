4a35f28fd663137b0f022878e757b081de5d62bb
Peter Ivanov <peter@microweber.com>
2022-06-28 12:00:46 +0300

    update

--- src/MicroweberPackages/Template/Adapters/RenderHelpers/CsrfTokenRequestInlineJsScriptGenerator.php
+++ src/MicroweberPackages/Template/Adapters/RenderHelpers/CsrfTokenRequestInlineJsScriptGenerator.php
@@ -6,6 +6,11 @@ class CsrfTokenRequestInlineJsScriptGenerator
 {
     public function generate()
     {
+        if(!mw_is_installed()){
+            return;
+        }
+
+
         $content =  file_get_contents(__DIR__.'/CsrfTokenRequestInlineJsScriptGenerator.js');
         return <<<HTML
        $content
