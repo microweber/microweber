88949392ee8e7797d2a43196b6676f50ca3e3853
Bozhidar Slaveykov <bobi@microweber.com>
2022-09-04 20:28:58 +0300

    Update price-range.blade.php

--- src/MicroweberPackages/Livewire/resources/views/livewire/mw-livewire-tables/components/tools/filters/price-range.blade.php
+++ src/MicroweberPackages/Livewire/resources/views/livewire/mw-livewire-tables/components/tools/filters/price-range.blade.php
@@ -7,22 +7,15 @@
 @elseif ($theme === 'bootstrap-4' || $theme === 'bootstrap-5')
 
     <div class="mb-3 mb-md-0 input-group">
-        <input
-            wire:model.stop="{{ $component->getTableName() }}.filters.{{ $filter->getKey() }}"
-            wire:key="{{ $component->getTableName() }}-filter-{{ $filter->getKey() }}"
-            id="{{ $component->getTableName() }}-filter-{{ $filter->getKey() }}"
-            type="number"
-            class="form-control"
-            placeholder="Min price"
-        />
-        <input
-            wire:model.stop="{{ $component->getTableName() }}.filters.{{ $filter->getKey() }}"
-            wire:key="{{ $component->getTableName() }}-filter-{{ $filter->getKey() }}"
-            id="{{ $component->getTableName() }}-filter-{{ $filter->getKey() }}"
-            type="number"
-            class="form-control"
-            placeholder="Max Price"
-        />
+
+        <input type="text"
+               wire:model.stop="{{ $component->getTableName() }}.filters.{{ $filter->getKey() }}[min]"
+               wire:key="{{ $component->getTableName() }}-filter-{{ $filter->getKey() }}-min"
+               id="{{ $component->getTableName() }}-filter-{{ $filter->getKey() }}-min" >
+
+        <input type="number" class="form-control" placeholder="Min price" />
+        <input type="number" class="form-control" placeholder="Max Price" />
+
     </div>
 
 @endif
