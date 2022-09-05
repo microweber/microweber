61e2011828f1d67357a3edb4de492fd670eff21d
Bozhidar Slaveykov <bobi@microweber.com>
2022-09-05 15:57:09 +0300

    Update price-range.blade.php

--- src/MicroweberPackages/Livewire/resources/views/livewire/mw-livewire-tables/components/tools/filters/price-range.blade.php
+++ src/MicroweberPackages/Livewire/resources/views/livewire/mw-livewire-tables/components/tools/filters/price-range.blade.php
@@ -14,7 +14,9 @@
     />
 
     <div class="mb-3 mb-md-0 input-group">
-        <input type="number" class="form-control" id="js-{{ $filter->getKey() }}-min-price" placeholder="Min price" />
+        <span class="input-group-text">From</span>
+         <input type="number" class="form-control" id="js-{{ $filter->getKey() }}-min-price" placeholder="Min price" />
+        <span class="input-group-text">To</span>
         <input type="number" class="form-control" id="js-{{ $filter->getKey() }}-max-price" placeholder="Max Price" />
     </div>
 
