6bc54776d1e9ea64a51b5b817e0e47c2edc9d1ac
Peter Ivanov <peter@microweber.com>
2022-09-08 17:53:50 +0300

    update

--- src/MicroweberPackages/Livewire/resources/views/livewire/mw-livewire-tables/components/tools/filters/category.blade.php
+++ src/MicroweberPackages/Livewire/resources/views/livewire/mw-livewire-tables/components/tools/filters/category.blade.php
@@ -1,4 +1,48 @@
+<?php
+
+$data = [];
+$data['parent'] = 0;
+$categories_active_ids = "0,1";
+
+?>
+<script type="text/javascript">
+    categoryFilterSelectTree = function (){
+        var ok = mw.element('<button class="btn btn-primary">Apply</button>');
+        var btn = ok.get(0);
+
+        var dialog = mw.dialog({
+            footer: btn,
+            onResult: function(result){
+                console.log(result)
+            }
+        });
+
+        var tree;
+
+        mw.admin.tree(dialog.dialogContainer, {
+            options: {
+                sortable: false,
+                selectable: true,
+                multiPageSelect: true
+            }
+        }, 'tree').then(function (res){
+            tree = res.tree;
+            $(tree).on("selectionChange", function () {
+                btn.disabled = tree.getSelected().length === 0;
+            });
+            $(tree).on("ready", function () {
+                dialog.center();
+            });
+        });
+
+        ok.on('click', function(){ dialog.result(tree.getSelected(), true) })
+    }
 
 
+    $(document).ready(function () {
 
-<button class="btn btn-outline-primary btn-block"><i class="fa fa-list"></i> Select Categories</button>
+    });
+</script>
+
+
+<button class="btn btn-outline-primary btn-block" onclick="categoryFilterSelectTree()"><i class="fa fa-list"></i> Select Categories</button>
