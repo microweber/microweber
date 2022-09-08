c03d37c2d0574b9324374025b44cbc28a648448d
Peter Ivanov <peter@microweber.com>
2022-09-08 18:11:22 +0300

    update

--- src/MicroweberPackages/Livewire/resources/views/livewire/mw-livewire-tables/components/tools/filters/category.blade.php
+++ src/MicroweberPackages/Livewire/resources/views/livewire/mw-livewire-tables/components/tools/filters/category.blade.php
@@ -1,16 +1,25 @@
 <?php
 
-$data = [];
-$data['parent'] = 0;
-$categories_active_ids = "0,1";
+
+$selected_page_id = "6";
+$categories_active_ids = "281,282,283";
 
 ?>
 <script type="text/javascript">
     categoryFilterSelectTree = function (){
+
+
+        var selectedPages = [ <?php print $selected_page_id; ?>];
+        var selectedCategories = [ <?php print $categories_active_ids; ?>];
+
+
+
+
         var ok = mw.element('<button class="btn btn-primary">Apply</button>');
         var btn = ok.get(0);
 
         var dialog = mw.dialog({
+            title: '<?php _ejs('Select categories'); ?>',
             footer: btn,
             onResult: function(result){
                 console.log(result)
@@ -22,15 +31,28 @@ $categories_active_ids = "0,1";
         mw.admin.tree(dialog.dialogContainer, {
             options: {
                 sortable: false,
+                singleSelect:false,
                 selectable: true,
-                multiPageSelect: true
+                multiPageSelect: false
             }
-        }, 'tree').then(function (res){
+        }, 'treeTags').then(function (res){
             tree = res.tree;
             $(tree).on("selectionChange", function () {
                 btn.disabled = tree.getSelected().length === 0;
             });
             $(tree).on("ready", function () {
+
+                if (selectedPages.length) {
+                    $.each(selectedPages, function () {
+                        tree.select(this, 'page')
+                    });
+                }
+                if (selectedCategories.length) {
+                    $.each(selectedCategories, function () {
+                        tree.select(this, 'category')
+                    });
+                }
+
                 dialog.center();
             });
         });
@@ -46,3 +68,5 @@ $categories_active_ids = "0,1";
 
 
 <button class="btn btn-outline-primary btn-block" onclick="categoryFilterSelectTree()"><i class="fa fa-list"></i> Select Categories</button>
+
+
