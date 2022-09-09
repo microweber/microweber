2c8c06a1d4701121fc6e4d39c91c7b2e96c3d7c7
Peter Ivanov <peter@microweber.com>
2022-09-09 14:56:58 +0300

    update

--- src/MicroweberPackages/Livewire/resources/views/livewire/mw-livewire-tables/components/tools/filters/tags.blade.php
+++ src/MicroweberPackages/Livewire/resources/views/livewire/mw-livewire-tables/components/tools/filters/tags.blade.php
@@ -5,46 +5,50 @@
     type="hidden"
 />
 
-<input type="text" value="" id="{{ $component->getTableName() }}-filter-{{ $filter->getKey() }}-tags-autocomplete"
-       class="form-control">
 
-<script id="js-tags-filter-<?php echo md5(rand(111,999));?>">
-    function initTagsFilter() {
+<div wire:ignore>
 
-        let tagsSelected_{{ $filter->getKey() }} = [];
-        let tagsElement_{{ $filter->getKey() }} = document.getElementById('{{ $component->getTableName() }}-filter-{{ $filter->getKey() }}');
+    <input type="text" value="" id="{{ $component->getTableName() }}-filter-{{ $filter->getKey() }}-tags-autocomplete"
+           class="form-control">
 
-        if (tagsElement_{{ $filter->getKey() }}.value != '') {
-            tagsSelected_{{ $filter->getKey() }} = tagsElement_{{ $filter->getKey() }}.value.split(",");
+    <script id="js-tags-filter-<?php echo md5(rand(111, 999));?>">
+        function initTagsFilter() {
+
+            let tagsSelected_{{ $filter->getKey() }} = [];
+            let tagsElement_{{ $filter->getKey() }} = document.getElementById('{{ $component->getTableName() }}-filter-{{ $filter->getKey() }}');
+
+            if (tagsElement_{{ $filter->getKey() }}.value != '') {
+                tagsSelected_{{ $filter->getKey() }} = tagsElement_{{ $filter->getKey() }}.value.split(",");
+            }
+
+            mw.log($('#{{ $component->getTableName() }}-filter-{{ $filter->getKey() }}-tags-autocomplete'));
+
+            var tagsSelect_{{ $filter->getKey() }} = new mw.select({
+                element: '#{{ $component->getTableName() }}-filter-{{ $filter->getKey() }}-tags-autocomplete',
+                multiple: false,
+                autocomplete: true,
+                tags: false,
+                placeholder: '',
+                ajaxMode: {
+                    paginationParam: 'page',
+                    searchParam: 'keyword',
+                    endpoint: mw.settings.api_url + 'tagging_tag/autocomplete',
+                    method: 'get'
+                }
+            });
+
+            $(tagsSelect_{{ $filter->getKey() }}).on("change", function (event, tag) {
+
+                tagsSelected_{{ $filter->getKey() }}.push(tag.title);
+                let tagsSelectedSeperated_{{ $filter->getKey() }} = tagsSelected_{{ $filter->getKey() }}.join(",");
+
+                $(tagsElement_{{ $filter->getKey() }}).val(tagsSelectedSeperated_{{ $filter->getKey() }});
+
+                tagsElement_{{ $filter->getKey() }}.dispatchEvent(new Event('input'));
+            });
+
         }
 
-        mw.log($('#{{ $component->getTableName() }}-filter-{{ $filter->getKey() }}-tags-autocomplete'));
-
-        var tagsSelect_{{ $filter->getKey() }} = new mw.select({
-            element: '#{{ $component->getTableName() }}-filter-{{ $filter->getKey() }}-tags-autocomplete',
-            multiple: false,
-            autocomplete: true,
-            tags: false,
-            placeholder: '',
-            ajaxMode: {
-                paginationParam: 'page',
-                searchParam: 'keyword',
-                endpoint: mw.settings.api_url + 'tagging_tag/autocomplete',
-                method: 'get'
-            }
-        });
-
-        $(tagsSelect_{{ $filter->getKey() }}).on("change", function (event, tag) {
-
-            tagsSelected_{{ $filter->getKey() }}.push(tag.title);
-            let tagsSelectedSeperated_{{ $filter->getKey() }} = tagsSelected_{{ $filter->getKey() }}.join(",");
-
-            $(tagsElement_{{ $filter->getKey() }}).val(tagsSelectedSeperated_{{ $filter->getKey() }});
-
-            tagsElement_{{ $filter->getKey() }}.dispatchEvent(new Event('input'));
-        });
-
-    }
-
-    initTagsFilter();
-</script>
+        initTagsFilter();
+    </script>
+</div>
