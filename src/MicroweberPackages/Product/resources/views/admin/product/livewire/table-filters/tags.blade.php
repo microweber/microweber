9d7119f3334fb1a51075a9425e932c810e074817
Bozhidar Slaveykov <bobi@microweber.com>
2022-09-12 14:30:58 +0300

    Update tags.blade.php

--- src/MicroweberPackages/Product/resources/views/admin/product/livewire/table-filters/tags.blade.php
+++ src/MicroweberPackages/Product/resources/views/admin/product/livewire/table-filters/tags.blade.php
@@ -3,6 +3,49 @@
         Tags
     </label>
 
+    <input  wire:model.stop="filters.tags" id="js-filter-tags" type="text" />
+
+        <input type="text" value="" id="js-tags-autocomplete" class="form-control">
+
+        <script>
+            function initTagsFilter() {
+
+                let tagsSelected = [];
+                let tagsElement = document.getElementById('js-filter-tags');
+
+                if (tagsElement.value != '') {
+                    tagsSelected = tagsElement.value.split(",");
+                }
+
+                mw.log($('#js-tags-autocomplete'));
+
+                var tagsSelect = new mw.select({
+                    element: '#js-tags-autocomplete',
+                    multiple: false,
+                    autocomplete: true,
+                    tags: false,
+                    placeholder: '',
+                    ajaxMode: {
+                        paginationParam: 'page',
+                        searchParam: 'keyword',
+                        endpoint: mw.settings.api_url + 'tagging_tag/autocomplete',
+                        method: 'get'
+                    }
+                });
+
+                $(tagsSelect).on("change", function (event, tag) {
+
+                    tagsSelected.push(tag.title);
+                    let tagsSelectedSeperated = tagsSelected.join(",");
+                    tagsElement.value = tagsSelectedSeperated;
+                    tagsElement.dispatchEvent(new Event('input'));
+
+                });
+
+            }
+
+            initTagsFilter();
+        </script>
 
 
 </div>
