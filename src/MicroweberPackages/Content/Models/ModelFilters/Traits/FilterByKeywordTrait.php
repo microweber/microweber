593c1388629c22601a390f4e152093106b36a43e
Peter Ivanov <peter@microweber.com>
2022-09-13 14:34:43 +0300

    update

--- src/MicroweberPackages/Content/Models/ModelFilters/Traits/FilterByKeywordTrait.php
+++ src/MicroweberPackages/Content/Models/ModelFilters/Traits/FilterByKeywordTrait.php
@@ -35,6 +35,15 @@ trait FilterByKeywordTrait
 
 
         if ($keywordToSearch) {
+            $antiXss = new \MicroweberPackages\Helper\HTMLClean();
+            $keywordToSearch = $antiXss->clean($keywordToSearch);
+            $keywordToSearch = mb_trim($keywordToSearch);
+            if ($keywordToSearch != '') {
+                if (mb_strlen($keywordToSearch) > 1000) {
+                    $keywordToSearch = mb_substr($keywordToSearch, 0, 1000);
+                }
+
+            }
             if (isset($this->input['searchInFields'])) {
                 $searchInFieldsNew = [];
                 $searchInFieldsInput = $this->input['searchInFields'];
@@ -62,35 +71,32 @@ trait FilterByKeywordTrait
 
             $this->query->where(function ($subQuerySearch) use ($table, $searchInFields, $keywordToSearch) {
                 if ($searchInFields) {
-                    $antiXss = new \MicroweberPackages\Helper\HTMLClean();
-                    $keywordToSearch = $antiXss->clean($keywordToSearch);
-                    $keywordToSearch = mb_trim($keywordToSearch);
-                    if ($keywordToSearch != '') {
-                        if (mb_strlen($keywordToSearch) > 1000) {
-                            $keywordToSearch = mb_substr($keywordToSearch, 0, 1000);
-                        }
-                        foreach ($searchInFields as $field) {
-                           // $subQuerySearch->orWhere($table . '.' . $field, '=',  $keywordToSearch);
-                            $subQuerySearch->orWhere($table . '.' . $field, 'LIKE', '%' . $keywordToSearch . '%');
-                           // $subQuerySearch->whereRaw("LOWERCASE(  $field') LIKE '%'". strtolower($keywordToSearch)."'%'");
-
-                        }
+                    foreach ($searchInFields as $field) {
+                        // $subQuerySearch->orWhere($table . '.' . $field, '=',  $keywordToSearch);
+                        $subQuerySearch->orWhere($table . '.' . $field, 'LIKE', '%' . $keywordToSearch . '%');
+                        // $subQuerySearch->whereRaw("LOWERCASE(  $field') LIKE '%'". strtolower($keywordToSearch)."'%'");
                     }
+
                 }
                 return $subQuerySearch;
             });
 
 
-            if (MultilanguageHelpers::multilanguageIsEnabled()) {
+            if (MultilanguageHelpers::multilanguageIsEnabled() and $keywordToSearch) {
                 $this->query->orWhere(function ($subQuerySearch) use ($table, $searchInFields, $keywordToSearch) {
                     $multilanguageTranslationsQuery = MultilanguageTranslations::query();
+
                     $multilanguageTranslationsQuery->where('rel_type', $table);
                     $multilanguageTranslationsQuery->whereIn('field_name', ['url', 'description', 'title']);
                     $multilanguageTranslationsQuery->where('field_value', 'LIKE', '%' . $keywordToSearch . '%');
+                    $multilanguageTranslationsQuery->leftJoin($table, $table.'.id', '=', 'rel_id');
+
                     $multilanguageTranslationsQuery->limit(3000); // MYSQL LIMIT FOR WHERE IN
+
                     $multilanguageTranslations = $multilanguageTranslationsQuery->get();
                     $relIds = $multilanguageTranslations->pluck('rel_id');
-                    if (!empty($relIds)) {
+
+                     if (!empty($relIds)) {
                       return  $subQuerySearch->orWhereIn($table.'.id', $relIds);
                     }
                 });
