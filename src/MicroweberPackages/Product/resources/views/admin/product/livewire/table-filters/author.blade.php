bfc8ccbb2347c2625ee9d529e5fb9d2b78b60275
Bozhidar Slaveykov <bobi@microweber.com>
2022-09-14 12:22:29 +0300

    Update author.blade.php

--- src/MicroweberPackages/Product/resources/views/admin/product/livewire/table-filters/author.blade.php
+++ src/MicroweberPackages/Product/resources/views/admin/product/livewire/table-filters/author.blade.php
@@ -3,8 +3,40 @@
         Author
     </label>
 
-    <div class="mb-3 mb-md-0 input-group">
-        <input wire:model.stop="filters.author" type="text" class="form-control">
-    </div>
+    <input wire:model.stop="filters.author" id="js-filter-author" type="hidden" class="form-control">
 
+    <div class="mb-3 mb-md-0 input-group">
+
+        <div id="js-filter-author-select"></div>
+
+        <script>mw.require('autocomplete.js')</script>
+
+        <script>
+            $(document).ready(function () {
+                var filterAuthorFiled = new mw.autoComplete({
+                    element: "#js-filter-author-select",
+                    ajaxConfig: {
+                        method: 'get',
+                        url: mw.settings.api_url + 'users/search_authors?kw=${val}',
+                        cache: true
+                    },
+                    map: {
+                        value: 'id',
+                        title: 'display_name',
+                        image: 'picture'
+                    },
+                    selected: [
+                        {
+                            id: '',
+                            display_name: ''
+                        }
+                    ]
+                });
+                $(filterAuthorFiled).on("change", function (e, val) {
+                    $("#js-filter-author").val(val[0].id).trigger('change')
+                    document.getElementById('js-filter-author').dispatchEvent(new Event('input'));
+                })
+            });
+        </script>
+    </div>
 </div>
