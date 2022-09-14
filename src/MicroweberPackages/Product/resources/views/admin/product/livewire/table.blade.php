d96360515d1e0618b6c1b2d3c8181aff433e977c
Bozhidar Slaveykov <bobi@microweber.com>
2022-09-14 11:05:20 +0300

    fix bugs

--- src/MicroweberPackages/Product/resources/views/admin/product/livewire/table.blade.php
+++ src/MicroweberPackages/Product/resources/views/admin/product/livewire/table.blade.php
@@ -1,43 +1,7 @@
 <div>
-    <style>
-        #content-results-table tr .mw_admin_posts_sortable_handle {
-            visibility: hidden;
-        }
-        #content-results-table tr:hover .mw_admin_posts_sortable_handle {
-            visibility: visible;
-        }
-    </style>
-    <script>
-        mw.manage_content_sort = function () {
-            if (!mw.$("#content-results-table").hasClass("ui-sortable")) {
-                mw.$("#content-results-table").sortable({
-                    items: '.manage-post-item',
-                    axis: 'y',
-                    handle: '.mw_admin_posts_sortable_handle',
-                    update: function () {
-                        var obj = {ids: []}
-                        $(this).find('.js-select-posts-for-action').each(function () {
-                            var id = this.attributes['value'].nodeValue;
-                            obj.ids.push(id);
-                        });
-                        $.post("<?php print api_link('content/reorder'); ?>", obj, function () {
-                            mw.reload_module('#mw_page_layout_preview');
-                            mw.reload_module_parent('posts');
-                            mw.reload_module_parent('content');
-                            mw.reload_module_parent('shop/products');
-                            mw.notification.success('<?php _e("All changes are saved"); ?>.');
-                        });
-                    },
-                    start: function (a, ui) {
-                        $(this).height($(this).outerHeight());
-                        $(ui.placeholder).height($(ui.item).outerHeight())
-                        $(ui.placeholder).width($(ui.item).outerWidth())
-                    },
-                    scroll: false
-                });
-            }
-        }
-    </script>
+
+    @include('product::admin.product.livewire.table-includes.category-tree-js')
+    @include('product::admin.product.livewire.table-includes.table-tr-reoder-js')
 
     @if(!empty($appliedFiltersFriendlyNames))
         <div class="mb-4">
