cd29fac327bff32e85fe53efe1bb861f3b3a4fbc
Bozhidar Slaveykov <bobi@microweber.com>
2022-09-15 17:11:57 +0300

    Update date_range.blade.php

--- src/MicroweberPackages/Order/resources/views/admin/orders/livewire/table-filters/date_range.blade.php
+++ src/MicroweberPackages/Order/resources/views/admin/orders/livewire/table-filters/date_range.blade.php
@@ -3,13 +3,14 @@
 
 <div class=" col-12 col-sm-6 col-md-4 col-lg-4 mb-4">
 
-    <input type="text" id="js-date-range" wire:model.stop="filters.dateBetween">
+    <input type="hidden" id="js-date-range" wire:model.stop="filters.dateBetween">
 
     <label class="d-block">
         Order Date Range
     </label>
+
     <div class="mb-3 mb-md-0 input-group">
-        <input id="js-date-range-picker" class="form-control" type="text"/>
+        <input id="js-date-range-picker" class="form-control" type="text" />
     </div>
 
 </div>
@@ -17,23 +18,29 @@
 <script>
     mw.lib.require("air_datepicker");
 
-    var myDataPicker = $('#js-date-range-picker').datepicker({
+    let dateRangeElement = document.getElementById('js-date-range');
+    var dateRangePickerInstance = $('#js-date-range-picker').datepicker({
         language: 'en',
         timepicker: false,
         range: true,
         multipleDates: true,
         dateFormat: '',
         multipleDatesSeparator: " - ",
-        selectedDates: [new Date()],
         onSelect: function (fd, d, picker) {
 
             var dateRange = fd;
             dateRange = dateRange.replace(' - ', ',');
 
-            let dateRangeElement = document.getElementById('js-date-range');
             dateRangeElement.value = dateRange;
             dateRangeElement.dispatchEvent(new Event('input'));
 
+        }
+    }).data('datepicker');
+
+    document.addEventListener('livewire:load', function () {
+        if (dateRangeElement) {
+            const dateRangeExp = dateRangeElement.value.split(",");
+            dateRangePickerInstance.selectDate([new Date(dateRangeExp[0]), new Date([dateRangeExp[1]])]);
         }
     });
 </script>
