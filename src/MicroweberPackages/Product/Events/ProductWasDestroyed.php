44798696db20f81cbf56cbb19b618a419db8923d
Peter Ivanov <peter@microweber.com>
2022-09-19 16:59:17 +0300

    update

---
+++ src/MicroweberPackages/Product/Events/ProductWasDestroyed.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace MicroweberPackages\Product\Events;
+
+use MicroweberPackages\Core\Events\AbstractResourceWasUpdated;
+
+class ProductWasDestroyed extends AbstractResourceWasUpdated
+{
+
+}
