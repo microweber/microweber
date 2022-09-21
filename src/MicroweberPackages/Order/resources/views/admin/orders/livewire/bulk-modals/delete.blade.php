d2d0c71ab03e112a0e9b04f2c5cd05da82bf7d20
Bozhidar Slaveykov <bobi@microweber.com>
2022-09-21 12:27:41 +0300

    update

--- src/MicroweberPackages/Order/resources/views/admin/orders/livewire/bulk-modals/delete.blade.php
+++ src/MicroweberPackages/Order/resources/views/admin/orders/livewire/bulk-modals/delete.blade.php
@@ -1,19 +1,17 @@
 <div>
-    <form wire:submit.prevent="delete">
-        <div class="modal-header">
-            <h5 class="modal-title">Bulk Delete</h5>
-            <button type="button" class="btn btn-link" wire:click="$emit('closeModal')">
-                <i class="fa fa-times text-muted"></i>
-            </button>
+    <div class="modal-header">
+        <h5 class="modal-title">Bulk Delete</h5>
+        <button type="button" class="btn btn-link" wire:click="$emit('closeModal')">
+            <i class="fa fa-times text-muted"></i>
+        </button>
+    </div>
+    <div class="modal-body">
+        <div>
+            Are you sure you want to delete the selected data? <br/>
         </div>
-        <div class="modal-body">
-            <div>
-                Are you sure you want to delete the selected data? <br/>
-            </div>
-        </div>
-        <div class="modal-footer">
-            <button type="submit" class="btn btn-outline-dark" wire:click="$emit('closeModal')">Cancel</button>
-            <button type="submit" class="btn btn-success">Yes</button>
-        </div>
-    </form>
+    </div>
+    <div class="modal-footer">
+        <button type="button" class="btn btn-outline-dark" wire:click="$emit('closeModal')">Cancel</button>
+        <button type="button" class="btn btn-success" wire:click="delete">Yes</button>
+    </div>
 </div>
