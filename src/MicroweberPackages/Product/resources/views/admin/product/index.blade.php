8d5595550b469cf5a6f0790e01b50c218ee3b132
Peter Ivanov <peter@microweber.com>
2022-09-30 17:16:12 +0300

    update

--- src/MicroweberPackages/Product/resources/views/admin/product/index.blade.php
+++ src/MicroweberPackages/Product/resources/views/admin/product/index.blade.php
@@ -1,23 +1,31 @@
-<livewire:admin-products-list />
+<livewire:admin-products-list/>
 <script>
 
-    document.body.addEventListener("click", function (e) {
-        if (mw.tools.hasAnyOfClassesOnNodeOrParent(e.target, ['js-filter-item-dropdown'])) {
+    // document.body.addEventListener("click", function (e) {
+    //     if (mw.tools.hasAnyOfClassesOnNodeOrParent(e.target, ['js-filter-item-dropdown'])) {
+    //
+    //         var elementTarget = mw.tools.firstParentOrCurrentWithAnyOfClasses(e.target, ['js-filter-item-dropdown']);
+    //     }
+    // });
 
-            setTimeout(function() {
-                var elementTarget = mw.tools.firstParentOrCurrentWithAnyOfClasses(e.target, ['js-filter-item-dropdown']);
-                var clickedDropdownId = $(elementTarget).attr('wire:id');
-                console.log(elementTarget);
+</script>
+<script>
+    document.addEventListener("DOMContentLoaded", () => {
 
-                /* $('.js-filter-item-dropdown').each(function (item, element) {
-                     if ($(element).attr('data-dropdown-show') == '1') {
-                         console.log(clickedDropdownId);
-                         //  window.livewire.emit('closeDropdown','1');
-                     }
-                 });*/
-            }, 1000);
+        Livewire.hook('element.updated', (el, component) => {
+            var elementTargetId = $(el).attr('wire:id');
+            if (typeof elementTargetId != 'undefined') {
+                var elementTargetIdOpenendDropdown = $(el).attr('data-dropdown-show');
+                $(".js-filter-item-dropdown[data-dropdown-show='1']").each(function () {
+                    var elementTargetIdOther = $(this).attr('wire:id');
+                    if (elementTargetIdOther) {
+                        if (elementTargetId != elementTargetIdOther) {
+                            window.livewire.emit('closeDropdown', elementTargetIdOther);
+                        }
+                    }
+                });
+            }
+        })
 
-        }
     });
-
 </script>
