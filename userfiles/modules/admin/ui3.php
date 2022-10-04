6e35e1e4b1c236521b1bdb2743a8c37ed26e28ab
Peter Ivanov <peter@microweber.com>
2022-10-04 11:28:47 +0300

    update

--- userfiles/modules/admin/ui3.php
+++ userfiles/modules/admin/ui3.php
@@ -119,3 +119,19 @@
     <input type="checkbox" class="btn-check" id="btncheck3" autocomplete="off">
     <label class="btn btn-outline-info" for="btncheck3">Checkbox 3</label>
 </div>
+<div class="btn-toolbar" role="toolbar" aria-label="Toolbar with button groups">
+    <div class="btn-group me-2" role="group" aria-label="First group">
+        <button type="button" class="btn btn-primary">1</button>
+        <button type="button" class="btn btn-primary">2</button>
+        <button type="button" class="btn btn-primary">3</button>
+        <button type="button" class="btn btn-primary">4</button>
+    </div>
+    <div class="btn-group me-2" role="group" aria-label="Second group">
+        <button type="button" class="btn btn-secondary">5</button>
+        <button type="button" class="btn btn-secondary">6</button>
+        <button type="button" class="btn btn-secondary">7</button>
+    </div>
+    <div class="btn-group" role="group" aria-label="Third group">
+        <button type="button" class="btn btn-info">8</button>
+    </div>
+</div>
