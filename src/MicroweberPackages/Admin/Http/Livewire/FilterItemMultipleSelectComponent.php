8a39dd83f796cb615b3e15dcf0f957c47384f6ee
Bozhidar Slaveykov <bobi@microweber.com>
2022-10-06 16:27:23 +0300

    update

--- src/MicroweberPackages/Admin/Http/Livewire/FilterItemMultipleSelectComponent.php
+++ src/MicroweberPackages/Admin/Http/Livewire/FilterItemMultipleSelectComponent.php
@@ -11,14 +11,12 @@ class FilterItemMultipleSelectComponent extends AutoCompleteMultipleSelectCompon
     public $name = 'Component';
     public $perPage = 10;
     public $total = 0;
-    public $hideFilterItem = 0;
 
     public string $view = 'admin::livewire.filters.filter-item-mulitple-select';
 
     public function hideFilterItem($id)
     {
         if ($this->id == $id) {
-            $this->hideFilterItem = 1;
             $this->closeDropdown($id);
             $this->emit('hideFilterItem', $this->selectedItemKey);
             $this->resetProperties();
