846c82c12f693e4d7a63fd6ebb3cb679be58132a
Peter Ivanov <peter@microweber.com>
2022-10-12 14:53:18 +0300

    update

--- userfiles/modules/categories/category_images/index.php
+++ userfiles/modules/categories/category_images/index.php
@@ -45,8 +45,8 @@ $cache_ttl = 3600;
 $cache_id = __CLASS__ . __FUNCTION__ .'category_images'. crc32(json_encode($params) . $hide_pages . $show_subcats . $cfg_filter_in_stock.$show_category_header . $show_only_for_parent . $selected_page . $parent . current_lang());
 $cache_group = 'categories';
 
-//$results = cache_get($cache_id, $cache_group, $cache_ttl);
- $results = false;
+ $results = cache_get($cache_id, $cache_group, $cache_ttl);
+// $results = false;
 if ($results) {
     $cats = $results;
 } else {
