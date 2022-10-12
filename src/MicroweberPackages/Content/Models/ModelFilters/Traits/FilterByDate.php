db95855531951635e994fcce44eab726adf20f39
Bozhidar Slaveykov <bobi@microweber.com>
2022-10-12 11:59:09 +0300

    update

--- src/MicroweberPackages/Content/Models/ModelFilters/Traits/FilterByDate.php
+++ src/MicroweberPackages/Content/Models/ModelFilters/Traits/FilterByDate.php
@@ -16,6 +16,29 @@ use MicroweberPackages\Multilanguage\MultilanguageHelpers;
 
 trait FilterByDate
 {
+    public function dateBetween($date)
+    {
+        $minDate = $date;
+        $maxDate = false;
+
+        if (strpos($date, ',') !== false) {
+            $date = explode(',', $date);
+            $minDate = $date[0];
+            $maxDate = $date[1];
+        }
+
+        $table = $this->getModel()->getTable();
+
+        if (!empty($minDate)) {
+            $this->query->where($table . '.created_at', '>', $minDate);
+        }
+
+
+        if (!empty($maxDate)) {
+            $this->query->where($table . '.created_at', '<', $maxDate);
+        }
+    }
+
     public function createdAt($createdAt = false)
     {
         if ($createdAt) {
