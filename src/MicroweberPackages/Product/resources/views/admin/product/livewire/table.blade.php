d4da21c389b893d4bbdbe107e459d83ce111febe
Bozhidar Slaveykov <bobi@microweber.com>
2022-10-20 17:47:44 +0300

    Update table.blade.php

--- src/MicroweberPackages/Product/resources/views/admin/product/livewire/table.blade.php
+++ src/MicroweberPackages/Product/resources/views/admin/product/livewire/table.blade.php
@@ -1,7 +1,7 @@
 <div class="card style-1 mb-3">
 
     <div class="card-header d-flex align-items-center justify-content-between px-md-4">
-        <div class="col d-flex justify-content-md-start justify-content-center align-items-center px-0">
+        <div class="col d-flex justify-content-md-between justify-content-center align-items-center px-0">
             <h5 class="mb-0 d-flex">
                 <i class="mdi mdi-shopping text-primary mr-md-3 mr-1 justify-contetn-center"></i>
                 <strong class="d-xl-flex d-none">
@@ -18,7 +18,7 @@
 
                 </strong>
             </h5>
-            <div class="justify-content-end">
+            <div>
             <a href="{{route('admin.product.create')}}" class="btn btn-outline-success btn-sm js-hide-when-no-items ms-md-4 ms-1">{{_e('Add Product')}}</a>
             </div>
         </div>
