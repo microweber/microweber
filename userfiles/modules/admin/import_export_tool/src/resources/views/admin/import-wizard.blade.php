c0da3952b0a0c928906ce2d3b2dd41502e887df4
Bozhidar Slaveykov <bobi@microweber.com>
2022-10-26 14:46:19 +0300

    Update import-wizard.blade.php

--- userfiles/modules/admin/import_export_tool/src/resources/views/admin/import-wizard.blade.php
+++ userfiles/modules/admin/import_export_tool/src/resources/views/admin/import-wizard.blade.php
@@ -88,75 +88,64 @@
 
             <ul class="import-wizard justify-content-center">
                 <li class="import-wizard__item active">
-                    <a class="import-wizard__link" href="#tab1" data-toggle="tab">
+                    <a class="import-wizard__link" href="#tab1">
                         <span class="step">1</span>
                         <span class="desc">Import Type</span>
                     </a>
                 </li>
                 <li class="import-wizard__item">
-                    <a class="import-wizard__link" href="#tab2" data-toggle="tab">
+                    <div class="import-wizard__link">
                         <span class="step">2</span>
                         <span class="desc">Upload File</span>
-                    </a>
+                    </div>
                 </li>
                 <li class="import-wizard__item">
-                    <a class="import-wizard__link" href="#tab3" data-toggle="tab">
+                    <div class="import-wizard__link">
                         <span class="step">3</span>
                         <span class="desc">Map Fields</span>
-                    </a>
+                    </div>
                 </li>
                 <li class="import-wizard__item">
-                    <a class="import-wizard__link" href="#tab4" data-toggle="tab">
+                    <div class="import-wizard__link">
                         <span class="step">4</span>
                         <span class="desc">Import</span>
-                    </a>
+                    </div>
                 </li>
             </ul>
-            <div class="tab-content mt-5 mb-5">
-                <div class="tab-pane active" id="tab1">
-                    <div class="d-flex justify-content-center">
-                        <div class="import-wizard-select-type">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" style="width:50px;">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m2.25 0H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z" />
-                            </svg>
-                            <div class="mt-2">
-                                <b>Pages</b>
-                            </div>
-                        </div>
-                        <div class="import-wizard-select-type">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" style="width:50px;">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M12 7.5h1.5m-1.5 3h1.5m-7.5 3h7.5m-7.5 3h7.5m3-9h3.375c.621 0 1.125.504 1.125 1.125V18a2.25 2.25 0 01-2.25 2.25M16.5 7.5V18a2.25 2.25 0 002.25 2.25M16.5 7.5V4.875c0-.621-.504-1.125-1.125-1.125H4.125C3.504 3.75 3 4.254 3 4.875V18a2.25 2.25 0 002.25 2.25h13.5M6 7.5h3v3H6v-3z" />
-                            </svg>
-                            <div class="mt-2">
-                                <b>Posts</b>
-                            </div>
-                        </div>
-                        <div class="import-wizard-select-type">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" style="width:50px;">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 10.5V6a3.75 3.75 0 10-7.5 0v4.5m11.356-1.993l1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 01-1.12-1.243l1.264-12A1.125 1.125 0 015.513 7.5h12.974c.576 0 1.059.435 1.119 1.007zM8.625 10.5a.375.375 0 11-.75 0 .375.375 0 01.75 0zm7.5 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
-                            </svg>
-                            <div class="mt-2">
-                             <b>Products</b>
-                            </div>
-                        </div>
-                        <div class="import-wizard-select-type">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" style="width:50px;">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 16.875h3.375m0 0h3.375m-3.375 0V13.5m0 3.375v3.375M6 10.5h2.25a2.25 2.25 0 002.25-2.25V6a2.25 2.25 0 00-2.25-2.25H6A2.25 2.25 0 003.75 6v2.25A2.25 2.25 0 006 10.5zm0 9.75h2.25A2.25 2.25 0 0010.5 18v-2.25a2.25 2.25 0 00-2.25-2.25H6a2.25 2.25 0 00-2.25 2.25V18A2.25 2.25 0 006 20.25zm9.75-9.75H18a2.25 2.25 0 002.25-2.25V6A2.25 2.25 0 0018 3.75h-2.25A2.25 2.25 0 0013.5 6v2.25a2.25 2.25 0 002.25 2.25z" />
-                            </svg>
-                            <div class="mt-2">
-                                <b>Categories</b>
-                            </div>
+            <div class="mt-5 mb-5">
+                <div class="d-flex justify-content-center">
+                    <div class="import-wizard-select-type">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" style="width:50px;">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m2.25 0H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z" />
+                        </svg>
+                        <div class="mt-2">
+                            <b>Pages</b>
                         </div>
                     </div>
-                </div>
-                <div class="tab-pane" id="tab2">
-
-                </div>
-                <div class="tab-pane" id="tab3">
-
-                </div>
-                <div class="tab-pane" id="tab4">
-
+                    <div class="import-wizard-select-type">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" style="width:50px;">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M12 7.5h1.5m-1.5 3h1.5m-7.5 3h7.5m-7.5 3h7.5m3-9h3.375c.621 0 1.125.504 1.125 1.125V18a2.25 2.25 0 01-2.25 2.25M16.5 7.5V18a2.25 2.25 0 002.25 2.25M16.5 7.5V4.875c0-.621-.504-1.125-1.125-1.125H4.125C3.504 3.75 3 4.254 3 4.875V18a2.25 2.25 0 002.25 2.25h13.5M6 7.5h3v3H6v-3z" />
+                        </svg>
+                        <div class="mt-2">
+                            <b>Posts</b>
+                        </div>
+                    </div>
+                    <div class="import-wizard-select-type">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" style="width:50px;">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 10.5V6a3.75 3.75 0 10-7.5 0v4.5m11.356-1.993l1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 01-1.12-1.243l1.264-12A1.125 1.125 0 015.513 7.5h12.974c.576 0 1.059.435 1.119 1.007zM8.625 10.5a.375.375 0 11-.75 0 .375.375 0 01.75 0zm7.5 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
+                        </svg>
+                        <div class="mt-2">
+                         <b>Products</b>
+                        </div>
+                    </div>
+                    <div class="import-wizard-select-type">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" style="width:50px;">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 16.875h3.375m0 0h3.375m-3.375 0V13.5m0 3.375v3.375M6 10.5h2.25a2.25 2.25 0 002.25-2.25V6a2.25 2.25 0 00-2.25-2.25H6A2.25 2.25 0 003.75 6v2.25A2.25 2.25 0 006 10.5zm0 9.75h2.25A2.25 2.25 0 0010.5 18v-2.25a2.25 2.25 0 00-2.25-2.25H6a2.25 2.25 0 00-2.25 2.25V18A2.25 2.25 0 006 20.25zm9.75-9.75H18a2.25 2.25 0 002.25-2.25V6A2.25 2.25 0 0018 3.75h-2.25A2.25 2.25 0 0013.5 6v2.25a2.25 2.25 0 002.25 2.25z" />
+                        </svg>
+                        <div class="mt-2">
+                            <b>Categories</b>
+                        </div>
+                    </div>
                 </div>
             </div>
         </form>
