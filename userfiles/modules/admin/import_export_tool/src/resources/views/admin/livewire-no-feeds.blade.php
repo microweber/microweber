83bbbd0f643b3f51c31b7164863eecd947ad008d
Bozhidar Slaveykov <bobi@microweber.com>
2022-11-09 16:29:30 +0200

    Update livewire-no-feeds.blade.php

--- userfiles/modules/admin/import_export_tool/src/resources/views/admin/livewire-no-feeds.blade.php
+++ userfiles/modules/admin/import_export_tool/src/resources/views/admin/livewire-no-feeds.blade.php
@@ -4,5 +4,7 @@
     <p>Create your first import feed right now.<br>
         You are able to do that in very easy way!</p>
     <br>
-    <button type="button" wire:loading.attr="disabled" wire:click="$emit('openModal', 'import_export_tool::new_import_modal')" class="btn btn-primary btn-rounded">Create new import feed</button>
+    <a href="{{route('admin.import-export-tool.import-wizard')}}" class="btn btn-primary btn-rounded">
+        Create new import feed
+    </a>
 </div>
