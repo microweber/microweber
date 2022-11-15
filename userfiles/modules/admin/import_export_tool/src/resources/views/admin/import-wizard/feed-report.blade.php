8de607b2789630fafbb049b7e69adb4687767757
Bozhidar Slaveykov <bobi@microweber.com>
2022-11-15 16:42:36 +0200

    update

--- userfiles/modules/admin/import_export_tool/src/resources/views/admin/import-wizard/feed-report.blade.php
+++ userfiles/modules/admin/import_export_tool/src/resources/views/admin/import-wizard/feed-report.blade.php
@@ -7,25 +7,39 @@
             <b>{{count($importFeed['mapped_content'])}} products are imported</b>
         </div>
 
-
-        @foreach($importFeed['mapped_content'] as $content)
-        <table class="table table-bordered">
-            <tbody>
+        @foreach($mappedContent->items() as $content)
+            <table class="table table-bordered">
+                <tbody>
                 @foreach($content as $columnKey=>$columnValue)
-                <tr>
-                    <td>
-                        {{$columnKey}}
-                    </td>
-                    <td>
-                        @if(is_string($columnValue))
-                            {{$columnValue}}
-                        @endif
-                    </td>
-                </tr>
+                    <tr>
+                        <td>
+                            {{$columnKey}}
+                        </td>
+                        <td>
+                            @if(is_string($columnValue))
+                                {{$columnValue}}
+                            @endif
+
+                            @if($columnKey == 'media_urls')
+                                @if(is_array($columnValue))
+                                    @foreach($columnValue as $picture) <img src="{{$picture}}" style="width:120px;" /> @endforeach
+                                @endif
+                            @else
+
+                                @if(is_array($columnValue))
+                                        {!! json_encode($columnValue, JSON_PRETTY_PRINT) !!}
+                                @endif
+
+                            @endif
+
+                        </td>
+                    </tr>
                 @endforeach
-            </tbody>
-        </table>
+                </tbody>
+            </table>
         @endforeach
+
+     {{$mappedContent->links('livewire-tables::specific.bootstrap-4.pagination')}}
 
     @endif
 </div>
