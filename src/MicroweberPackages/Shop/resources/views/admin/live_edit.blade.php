0d233408b25670618102d3f868a532fbf4ac724b
Bozhidar Slaveykov <bobi@microweber.com>
2022-11-17 11:53:02 +0200

    Update live_edit.blade.php

--- src/MicroweberPackages/Shop/resources/views/admin/live_edit.blade.php
+++ src/MicroweberPackages/Shop/resources/views/admin/live_edit.blade.php
@@ -123,17 +123,6 @@
                                 </div>
                             </div>
 
-                            <div class="form-group col-4 js-filtering-allow-search">
-                                <label class="control-label d-block"><?php echo _e("Allow search", true); ?></label>
-                                <span class="text-muted"><?php echo _e("Show search on the shop page", true); ?></span>
-                                <div class="custom-control custom-switch pl-0">
-                                    <label class="d-inline-block mr-5" for="disable_search"><?php _e('No'); ?></label>
-                                    <input class="mw_option_field custom-control-input" id="disable_search" type="checkbox"
-                                           autocomplete="off" name="disable_search" <?php if (!get_option('disable_search', $moduleId)): ?>checked<?php endif; ?> option-group="<?php echo $moduleId;?>" data-value-checked="0" data-value-unchecked="1">
-                                    <label class="custom-control-label" for="disable_search"><?php _e('Yes'); ?></label>
-                                </div>
-                            </div>
-
                             <div class="form-group col-4">
                                 <label class="control-label d-block"><?php echo _e("Filtering the results", true); ?></label>
                                 <span class="text-muted"><?php echo _e("Show filters on the shop page", true); ?></span>
@@ -173,7 +162,12 @@
                                     <div class="card-body">
                                         <strong><?php _e("Filtering the results"); ?></strong>
 
-                                        <div class="custom-control custom-checkbox mt-3 js-filtering-by-tags">
+                                        <div class="custom-control custom-checkbox mt-3 js-disable-search">
+                                            <input type="checkbox" <?php if (!get_option('disable_search', $moduleId)): ?>checked="checked"<?php endif; ?> class="mw_option_field custom-control-input" data-value-checked="0" data-value-unchecked="1" name="disable_search" value="1" id="disable_search">
+                                            <label class="custom-control-label" for="disable_search"><?php _e("Search"); ?></label>
+                                        </div>
+
+                                        <div class="custom-control custom-checkbox js-filtering-by-tags">
                                             <input type="checkbox" <?php if ('1'== get_option('filtering_by_tags', $moduleId)): ?>checked="checked"<?php endif; ?> class="mw_option_field custom-control-input" name="filtering_by_tags" value="1" id="filter_by_tags">
                                             <label class="custom-control-label" for="filter_by_tags"><?php _e("Tags"); ?></label>
                                         </div>
