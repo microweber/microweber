461aad2f0968fb17dcef4f54d6403b1089bba7bb
Bozhidar Slaveykov <bobi@microweber.com>
2022-11-28 11:53:51 +0200

    Create ExportFeedTest.php

---
+++ userfiles/modules/admin/import_export_tool/src/tests/ExportFeedTest.php
@@ -0,0 +1,29 @@
+<?php
+namespace MicroweberPackages\Modules\Admin\ImportExportTool\tests;
+
+use Livewire\Livewire;
+use MicroweberPackages\Core\tests\TestCase;
+use MicroweberPackages\Modules\Admin\ImportExportTool\Http\Livewire\ExportWizard;
+use MicroweberPackages\Modules\Admin\ImportExportTool\Http\Livewire\Install;
+use MicroweberPackages\Modules\Admin\ImportExportTool\Http\Livewire\StartExportingModal;
+use MicroweberPackages\Modules\Admin\ImportExportTool\Models\ExportFeed;
+
+class ExportFeedTest extends TestCase
+{
+    public function testInstall()
+    {
+        Livewire::test(Install::class)->call('startInstalling');
+
+    }
+
+    public function testExportWizard()
+    {
+        Livewire::test(ExportWizard::class)
+            ->call('selectExportType', 'products')
+            ->call('selectExportFormat', 'xlsx');
+
+        $this->assertTrue(ExportFeed::alL()->count() > 0);
+
+        
+    }
+}
