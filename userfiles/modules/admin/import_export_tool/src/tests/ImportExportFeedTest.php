98d6a9f958990e30a452d675b1ef1f406d0e9cbe
Bozhidar Slaveykov <bobi@microweber.com>
2022-11-30 17:06:52 +0200

    update

--- userfiles/modules/admin/import_export_tool/src/tests/ImportExportFeedTest.php
+++ userfiles/modules/admin/import_export_tool/src/tests/ImportExportFeedTest.php
@@ -1,6 +1,9 @@
 <?php
 namespace MicroweberPackages\Modules\Admin\ImportExportTool\tests;
 
+use Illuminate\Http\File;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Storage;
 use Livewire\Livewire;
 use MicroweberPackages\Core\tests\TestCase;
 use MicroweberPackages\Import\Formats\XlsxReader;
@@ -9,6 +12,8 @@ use MicroweberPackages\Modules\Admin\ImportExportTool\Http\Livewire\ImportWizard
 use MicroweberPackages\Modules\Admin\ImportExportTool\Http\Livewire\Install;
 use MicroweberPackages\Modules\Admin\ImportExportTool\Http\Livewire\StartExportingModal;
 use MicroweberPackages\Modules\Admin\ImportExportTool\Models\ExportFeed;
+use MicroweberPackages\Modules\Admin\ImportExportTool\Models\ImportFeed;
+use MicroweberPackages\Page\Models\Page;
 use MicroweberPackages\Product\Models\Product;
 
 class ImportExportFeedTest extends TestCase
@@ -25,13 +30,45 @@ class ImportExportFeedTest extends TestCase
         $content = $zip->getFromName('mw-export-format-products.xlsx');
         $zip->close();
 
-        $tempName = tempnam(storage_path(),'xlsx');
-        file_put_contents($tempName, $content);
+        if (!is_dir(storage_path().'/import-export-tool/')) {
+            mkdir_recursive(storage_path().'/import-export-tool/');
+        }
+
+        $page = new Page();
+        $page->title = 'Shop';
+        $page->is_shop = 1;
+        $page->save();
+
+        $fakerFile = UploadedFile::fake()
+            ->createWithContent('mw-export-format-products.xlsx', $content);
 
         $instance = Livewire::test(ImportWizard::class)
-                ->call('selectImportTo', 'products');
+                ->call('selectImportTo', 'products')
+                ->set('import_feed.source_type', 'upload_file')
+                ->set('upload_file', $fakerFile)
+                ->call('upload')
+                ->assertDispatchedBrowserEvent('read-feed-from-file')
+                ->assertSuccessful()
+                ->assertSee('Feed is uploaded successfully');
+
+        $importFeed = ImportFeed::where('id', $instance->importFeedId)->first()->toArray();
+        mkdir_recursive(dirname($importFeed['source_file_realpath']));
+        file_put_contents($importFeed['source_file_realpath'], $content);
+
+        $instance->set('import_feed.content_tag', 'Worksheet')
+                ->call('changeContentTag')
+                ->assertEmitted('dropdownMappingPreviewRefresh')
+                ->assertSee('Feed is read successfully')
+                ->call('saveMapping')
+                ->set('import_feed.primary_key', 'id')
+                ->set('import_feed.parent_page', Page::where('is_shop',1)->first()->id);
+
+        // Lets import with modal
 
 
+        
+        $importFeed = ImportFeed::where('id', $instance->importFeedId)->first()->toArray();
+        dd($importFeed);
         die();
 
         $instance = Livewire::test(ExportWizard::class)
