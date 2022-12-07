97eeddf7717f92ad48fc9246e6c7e51b75d9c089
Peter Ivanov <peter@microweber.com>
2022-12-07 18:05:09 +0200

    update

--- src/MicroweberPackages/Form/resources/views/admin/notifications/uploads_listing_partial.blade.php
+++ src/MicroweberPackages/Form/resources/views/admin/notifications/uploads_listing_partial.blade.php
@@ -5,10 +5,12 @@
     @foreach($valInner as $k => $v)
         <div>
             <small class="text-muted"><?php echo $k; ?></small> <br />
+            @if(isset($v['url']))
             <a href="<?php echo $v['url']; ?>" target="_blank">
                 <i class="mdi {{ pathinfo($v['file_name'], PATHINFO_EXTENSION) == 'pdf' ? 'mdi-pdf-box' : 'mdi-file-check' }} text-primary mdi-18px"></i> <?php echo str_limit(basename($v['url']),30); ?>
                 (<?php echo app()->format->human_filesize($v['file_size']); ?>)
             </a>
+            @endif
         </div>
     @endforeach
 @endforeach
