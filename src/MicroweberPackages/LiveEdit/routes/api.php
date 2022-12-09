b4150198eec409c2222876fba941b6ed5c7a34d4
Bozhidar Slaveykov <bobi@microweber.com>
2022-12-09 12:18:34 +0200

    update

--- src/MicroweberPackages/LiveEdit/routes/api.php
+++ src/MicroweberPackages/LiveEdit/routes/api.php
@@ -4,13 +4,9 @@ use Illuminate\Support\Facades\Route;
 
 \Route::name('api.live_edit.')
 
-    ->prefix('api.live_edit')
+    ->prefix('api/live-edit')
     ->middleware(['api', 'admin', 'xss'])
     ->namespace('\MicroweberPackages\LiveEdit\Http\Controllers\Api')
     ->group(function () {
-
-        Route::get('', function () {
-
-        });
-
+        Route::get('modules-list', 'ModulesList@index');
     });
