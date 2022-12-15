de5df3416406d65145d317663100a514b5c16aa4
Bozhidar Slaveykov <bobi@microweber.com>
2022-12-15 12:07:07 +0200

    Update ModulesList.php

--- src/MicroweberPackages/LiveEdit/Http/Controllers/Api/ModulesList.php
+++ src/MicroweberPackages/LiveEdit/Http/Controllers/Api/ModulesList.php
@@ -412,6 +412,14 @@ class ModulesList extends Controller
                             $module_item['categories'] = [];
                         }
 
+                        if (!isset($module_item['settings'])) {
+                            $module_item['settings'] = [];
+                        }
+                        
+                        if (!isset($module_item['as_element'])) {
+                            $module_item['as_element'] = '';
+                        }
+
                         $moduleDataItem = [
                             'module_id' => $module_id,
                             'hidden' => false,
