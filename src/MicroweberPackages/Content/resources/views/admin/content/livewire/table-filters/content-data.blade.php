8f6ead03cc854e8026e277aa419089d0eee8e965
Bozhidar Slaveykov <bobi@microweber.com>
2023-01-09 11:15:08 +0200

    update

---
+++ src/MicroweberPackages/Content/resources/views/admin/content/livewire/table-filters/content-data.blade.php
@@ -0,0 +1,20 @@
+<div class="ms-0 ms-md-2 mb-3 mb-md-0 mt-2">
+
+    @php
+        $itemValue = '';
+        if (isset($filters['contentData'][$fieldKey])) {
+            $itemValue = $filters['contentData'][$fieldKey];
+        }
+    @endphp
+
+    @livewire('admin-filter-item-value', [
+        'name'=> $fieldName,
+        'itemValue'=>$itemValue,
+        'itemValueKey'=>['contentData'=>$fieldKey],
+        'showDropdown'=> session()->get('showFilterContentFields'),
+        'onChangedEmitEvents' => [
+             'setFirstPageContentList'
+        ]
+    ], key($fieldKey)) 
+
+</div>
---
+++ src/MicroweberPackages/Content/resources/views/admin/content/livewire/table-includes/table-th.blade.php
@@ -0,0 +1,18 @@
+<th scope="col">
+    <div>
+        {{$name}}
+        <span style="font-size: 15px;cursor: pointer">
+            @if(isset($filters['orderBy']) && !empty($filters['orderBy']))
+                @if ($filters['orderBy'] == $key . ',desc')
+                    <i class="fa fa-arrow-down" wire:click="orderBy('{{$key}},asc')"></i>
+                @elseif ($filters['orderBy'] == $key . ',asc')
+                    <i class="fa fa-arrow-up" wire:click="orderBy('{{$key}},desc')"></i>
+                @else
+                <i class="fa fa-sort" wire:click="orderBy('{{$key}},desc')"></i>
+                @endif
+            @else
+                <i class="fa fa-sort " wire:click="orderBy('{{$key}},desc')"></i>
+            @endif
+        </span>
+    </div>
+</th>
