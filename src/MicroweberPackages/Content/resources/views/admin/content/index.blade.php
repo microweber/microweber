d5cf82f0c58fb1d8cc228b860b13eda540dc85fa
Bozhidar Slaveykov <bobi@microweber.com>
2023-01-09 11:04:06 +0200

    update

--- src/MicroweberPackages/Content/resources/views/admin/content/index.blade.php
+++ src/MicroweberPackages/Content/resources/views/admin/content/index.blade.php
@@ -1,3 +1,15 @@
-<div>
+<div class="pt-0">
 
+    @include('content::admin.content.index-page-category-tree', [
+       'is_shop'=>1,
+   ])
+
+    <div class="module-content">
+
+        <livewire:admin-content-list />
+        <livewire:admin-content-bulk-options />
+
+    </div>
 </div>
+
+@include('content::admin.content.index-scripts')
