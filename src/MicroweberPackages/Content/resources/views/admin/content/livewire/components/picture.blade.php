e5b597b8a12cb341ae5780b45a1a8591f4dbc890
Peter Ivanov <peter@microweber.com>
2023-01-10 18:10:02 +0200

    update

--- src/MicroweberPackages/Content/resources/views/admin/content/livewire/components/picture.blade.php
+++ src/MicroweberPackages/Content/resources/views/admin/content/livewire/components/picture.blade.php
@@ -1,6 +1,6 @@
 @if($content->media()->first())
     <div class="img-as-background border-radius-0 border-0">
-        <img src="{{$content->thumbnail(480, 600, true)}}" class="rounded-full">
+        <img src="{{$content->thumbnail(640,480, true)}}" class="rounded-full">
     </div>
 @else
     @include('content::admin.content.livewire.components.icon', ['content'=>$content])
