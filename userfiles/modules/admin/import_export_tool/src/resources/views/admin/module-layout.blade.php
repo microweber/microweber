80cea4e793527e4ea920cce8aeb15c5a6b5c1565
Bozhidar Slaveykov <bobi@microweber.com>
2023-01-18 15:27:38 +0200

    Update module-layout.blade.php

--- userfiles/modules/admin/import_export_tool/src/resources/views/admin/module-layout.blade.php
+++ userfiles/modules/admin/import_export_tool/src/resources/views/admin/module-layout.blade.php
@@ -1,5 +1,4 @@
-@extends('admin::layouts.app')
-@section('content')
+
 <div class="row">
     <div class="col-md-12">
         <div>
@@ -36,4 +35,3 @@
         </div>
     </div>
 </div>
-@endsection
