261ccba920e603b2f6ac1a5ef1694c3a88a516b3
Bozhidar Slaveykov <bobi@microweber.com>
2023-01-19 15:30:48 +0200

    update

--- src/MicroweberPackages/App/Http/Middleware/Admin.php
+++ src/MicroweberPackages/App/Http/Middleware/Admin.php
@@ -2,73 +2,10 @@
 
 namespace MicroweberPackages\App\Http\Middleware;
 
-use Closure;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
-use MicroweberPackages\User\Models\User;
-
-class Admin
+/**
+ * @deprecated
+ */
+class Admin extends \MicroweberPackages\Admin\Http\Middleware\Admin
 {
-    /**
-     * The routes that should be excluded from verification.
-     *
-     * @var array
-     */
-    protected $except = [
-        'admin.login.*',
-        'admin.reset.*',
-    ];
 
-    /**
-     * Handle an incoming request.
-     *
-     * @param \Illuminate\Http\Request $request
-     * @param \Closure $next
-     * @return \Illuminate\Http\Response
-     */
-    public function handle(Request $request, Closure $next)
-    {
-        if (!mw_is_installed()) {
-            return redirect(site_url());
-        }
-
-
-        if (Auth::check() && intval(Auth::user()->is_admin) === 1) {
-            return $next($request);
-        }
-
-
-
-        if ($this->inExceptArray($request) || (Auth::check() && intval(Auth::user()->is_admin) === 1)) {
-             return $next($request);
-        }
-
-        $hasNoAdmin = User::where('is_admin', 1)->limit(1)->count();
-
-        if (!$hasNoAdmin) {
-            return $next($request);
-        }
-
-        return redirect()->guest(route('admin.login'));
-    }
-
-
-    /**
-     * Determine if the request URI is in except array.
-     *
-     * @param \Illuminate\Http\Request $request
-     * @return bool
-     */
-    protected function inExceptArray($request)
-    {
-        foreach ($this->except as $except) {
-            $routeName = optional($request->route())->getName();
-
-            if (preg_match("/{$except}/", $routeName)) {
-                return true;
-            }
-        }
-
-        return false;
-    }
 }
