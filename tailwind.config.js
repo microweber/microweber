916c3ec35e0124f58e9a335511dda601de02780c
Peter Ivanov <peter@microweber.com>
2023-02-21 17:08:19 +0200

    update

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+const colors = require("tailwindcss/colors");
+
+module.exports = {
+    content: ["./resources/**/*.blade.php", "./vendor/filament/**/*.blade.php"],
+    darkMode: "class",
+    theme: {
+        extend: {
+            colors: {
+                danger: colors.rose,
+                primary: colors.purple,
+                success: colors.green,
+                warning: colors.yellow,
+            },
+        },
+    },
+    plugins: [
+        require("@tailwindcss/forms"),
+        require("@tailwindcss/typography"),
+    ],
+};
