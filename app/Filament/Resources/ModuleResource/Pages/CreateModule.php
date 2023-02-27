554e3b0ab11912f7680c64b9a767eddd3b00c899
Bozhidar Slaveykov <bobi@microweber.com>
2023-02-27 17:38:04 +0200

    update

---
+++ app/Filament/Resources/ModuleResource/Pages/CreateModule.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Resources\ModuleResource\Pages;
+
+use App\Filament\Resources\ModuleResource;
+use Filament\Pages\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateModule extends CreateRecord
+{
+    protected static string $resource = ModuleResource::class;
+}
