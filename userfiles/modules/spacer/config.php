0eccda11303c06784a9263be37e463ab3efbfd70
Alex <raikov@ymail.com>
2023-03-08 12:43:32 +0200

    spacer

---
+++ userfiles/modules/spacer/config.php
@@ -0,0 +1,15 @@
+<?php
+
+$config = array();
+$config['name'] = "Spacer";
+$config['author'] = "Microweber";
+$config['description'] = "Microweber";
+$config['website'] = "http://microweber.com/";
+$config['help'] = "http://microweber.info/modules/spacer";
+$config['version'] = 1.0;
+$config['ui'] = true;
+$config['position'] = 3;
+$config['as_element'] = 1;
+$config['categories'] = "essentials";
+
+
