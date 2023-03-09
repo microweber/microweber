9a692b6ee05666f0ee76bbfeef3542212d0628c1
Peter Ivanov <peter@microweber.com>
2023-03-09 14:27:00 +0200

    update

--- src/MicroweberPackages/Translation/Providers/TranslationServiceProvider.php
+++ src/MicroweberPackages/Translation/Providers/TranslationServiceProvider.php
@@ -128,26 +128,7 @@ class TranslationServiceProvider extends IlluminateTranslationServiceProvider
                 }
             });
 
-            if (mw_is_installed()) {
-                $this->app->singleton('translation.loader', function ($app) {
-                    return new TranslationLoader($app['files'], $app['path.lang']);
-                });
-            }
 
-            app()->singleton('translator', function ($app) {
-                $loader = $app['translation.loader'];
-
-                // When registering the translator component, we'll need to set the default
-                // locale as well as the fallback locale. So, we'll grab the application
-                // configuration so we can easily get both of these values from there.
-                $locale = $app['config']['app.locale'];
-
-                $trans = new Translator($loader, $locale);
-
-                $trans->setFallback($app['config']['app.fallback_locale']);
-
-                return $trans;
-            });
         }
     }
 
@@ -164,7 +145,26 @@ class TranslationServiceProvider extends IlluminateTranslationServiceProvider
         }
 
 
+        if (mw_is_installed()) {
+            $this->app->singleton('translation.loader', function ($app) {
+                return new TranslationLoader($app['files'], $app['path.lang']);
+            });
+        }
 
+        app()->singleton('translator', function ($app) {
+            $loader = $app['translation.loader'];
+
+            // When registering the translator component, we'll need to set the default
+            // locale as well as the fallback locale. So, we'll grab the application
+            // configuration so we can easily get both of these values from there.
+            $locale = $app['config']['app.locale'];
+
+            $trans = new Translator($loader, $locale);
+
+            $trans->setFallback($app['config']['app.fallback_locale']);
+
+            return $trans;
+        });
 
 
 
