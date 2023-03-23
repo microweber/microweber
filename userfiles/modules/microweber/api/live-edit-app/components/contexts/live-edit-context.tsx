47b52223888e740d2d4b7b26a64d5062ce34ec9d
Peter Ivanov <peter@microweber.com>
2023-03-23 15:53:58 +0200

    update

--- userfiles/modules/microweber/api/live-edit-app/components/contexts/live-edit-context.tsx
+++
@@ -1,5 +0,0 @@
-import React, { createContext} from 'react';
-
-export const LiveEditContext = {
-    bojkata:123,
-};
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import react from '@vitejs/plugin-react';
+
 
 export default defineConfig({
     plugins: [
@@ -9,12 +9,10 @@ export default defineConfig({
                // 'resources/css/app.css',
                // 'resources/js/app.js'
 
-              //  'userfiles/modules/microweber/api/live-edit-app/app.tsx',
                 'resources/css/microweber-admin-filament.css',
             ],
             publicDirectory: "public",
             refresh: true,
-        }),
-        react( )
-    ],
+        })
+    ]
 });
