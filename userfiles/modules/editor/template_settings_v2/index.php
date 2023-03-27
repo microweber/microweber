728329cc93c52b8e36524ea3eab4720256aa5bf8
Bozhidar Slaveykov <bobi@microweber.com>
2023-03-27 17:34:10 +0300

    update

--- userfiles/modules/editor/template_settings_v2/index.php
+++ userfiles/modules/editor/template_settings_v2/index.php
@@ -1,8 +1,20 @@
-
-
 <div>
 
+    <script src="//unpkg.com/vue@2"></script>
+    <script src="<?php echo userfiles_url(); ?>modules/editor/template_settings_v2/dist/App.umd.js"></script>
 
- 
-    test
+    <div id="js-template-settings-v2">
+        <demo></demo>
+    </div>
+
+    <script>
+
+        new Vue({
+            components: {
+                demo: App
+            }
+        }).$mount('#js-template-settings-v2')
+
+    </script>
+
 </div>
---
+++ userfiles/modules/editor/template_settings_v2/src/App.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+    data() {
+        return {
+            greeting: 'Hello World!'
+        }
+    }
+}
+</script>
+
+<template>
+    <p class="greeting">{{ greeting }}</p>
+</template>
+<style>
+.greeting {
+    color: red;
+}
+</style>
