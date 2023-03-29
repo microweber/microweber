c69681db17c29ead3101944e19957a49ee180551
Bozhidar Slaveykov <bobi@microweber.com>
2023-03-29 13:10:47 +0300

    Update iframe.blade.php

--- src/MicroweberPackages/Admin/resources/views/live_edit/iframe.blade.php
+++ src/MicroweberPackages/Admin/resources/views/live_edit/iframe.blade.php
@@ -13,6 +13,8 @@
 
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
     <script src="//unpkg.com/vue@2"></script>
 
 
