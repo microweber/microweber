21a46374ae28361a616b6514d2c346bdf4bde3de
Bozhidar Slaveykov <bobi@microweber.com>
2023-03-30 17:39:11 +0300

    Update ListLayouts.vue

--- src/MicroweberPackages/LiveEdit/resources/js/ui/components/Layouts/ListLayouts.vue
+++ src/MicroweberPackages/LiveEdit/resources/js/ui/components/Layouts/ListLayouts.vue
@@ -3,6 +3,10 @@
     <div v-if="showModal" style="visibility: hidden; position: absolute; width: 1px; height: 1px;"></div>
     <div v-if="showModal" class="mw-le-overlay active"></div>
 
+    <Transition
+        enter-active-class="animate__animated animate__jello"
+        leave-active-class="animate__animated animate__bounceOutLeft"
+    >
     <div v-if="showModal" class="mw-le-dialog-block mw-le-layouts-dialog active">
         <div class="modules-list modules-list-defaultModules">
             <div class="mw-le-layouts-dialog-row">
@@ -22,7 +26,6 @@
                     <div v-if="keyword" class="pl-4 mb-3 mt-3">
                         Looking for {{keyword}}
                     </div>
-
 
                     <div class="modules-list-block">
 
@@ -48,10 +51,15 @@
             </div>
         </div>
     </div>
+    </Transition>
 
     <div v-if="showModal" v-on:click="showModal = false" class="mw-le-dialog-close active"></div>
 
 </template>
+
+<style>
+@import "//cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css";
+</style>
 
 <script>
 export default {
