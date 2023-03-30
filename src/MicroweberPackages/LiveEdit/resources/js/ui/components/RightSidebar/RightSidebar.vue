168fa87b2724af789f23acda7209af75782a6454
Bozhidar Slaveykov <bobi@microweber.com>
2023-03-30 18:42:20 +0300

    Update RightSidebar.vue

--- src/MicroweberPackages/LiveEdit/resources/js/ui/components/RightSidebar/RightSidebar.vue
+++ src/MicroweberPackages/LiveEdit/resources/js/ui/components/RightSidebar/RightSidebar.vue
@@ -1,3 +1,26 @@
+<template>
+    <div>
+
+        <div id="general-theme-settings" :class="[showSidebar == true ? 'active' : '']">
+
+            <div v-if="showSidebar">
+                <button v-on:click="showSidebar = false" type="button" class="btn btn-danger">
+                    Close Sidebar
+                </button>
+            </div>
+
+            <TemplateSettings></TemplateSettings>
+        </div>
+
+    </div>
+</template>
+
+<style>
+.general-theme-settings {
+    background:#000;
+}
+</style>
+
 <script>
 import TemplateSettings from "./TemplateSettings/TemplateSettings.vue";
 export default {
@@ -23,10 +46,3 @@ export default {
 }
 </script>
 
-<template>
-    <div>
-        <div id="general-theme-settings" :class="[showSidebar == true ? 'active' : '']">
-            <TemplateSettings></TemplateSettings>
-        </div>
-    </div>
-</template>
