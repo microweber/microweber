cbde6fe89ef6f714437324173dedd0929908785a
Bozhidar Slaveykov <bobi@microweber.com>
2023-03-30 14:12:02 +0300

    Update App.vue

--- src/MicroweberPackages/LiveEdit/resources/js/ui/App.vue
+++ src/MicroweberPackages/LiveEdit/resources/js/ui/App.vue
@@ -1,8 +1,13 @@
 <template>
 
     <div>
+        Vueto zahapa
         <div id="live-edit-frame-holder"></div>
-        wwowow
     </div>
 
 </template>
+<style>
+    #live-edit-frame-holder {
+        border:3px solid #0a53be;
+    }
+</style>
