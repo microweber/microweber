8958f7d01747db506b58083d9c9dfb88bf7f6518
Bozhidar Slaveykov <bobi@microweber.com>
2023-03-30 15:10:05 +0300

    Update App.vue

--- src/MicroweberPackages/LiveEdit/resources/js/ui/App.vue
+++ src/MicroweberPackages/LiveEdit/resources/js/ui/App.vue
@@ -3,15 +3,20 @@
     <div>
         <Toolbar></Toolbar>
         <LeftSidebar></LeftSidebar>
+        <RightSidebar></RightSidebar>
         <div id="live-edit-frame-holder"></div>
     </div>
 
 </template>
+
 <style>
 #live-edit-frame-holder {
     border: 3px solid #0a53be;
 }
 </style>
+
 <script setup>
 import Toolbar from "./components/Toolbar/Toolbar.vue";
-import LeftSidebar from "./components/LeftSidebar/LeftSidebar.vue";</script>
+import LeftSidebar from "./components/LeftSidebar/LeftSidebar.vue";
+import RightSidebar from "./components/RightSidebar/RightSidebar.vue";
+</script>
