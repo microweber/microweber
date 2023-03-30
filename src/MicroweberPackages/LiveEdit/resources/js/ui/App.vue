356b64153bbc4b12ebb70aba21d9283fc96673bf
Bozhidar Slaveykov <bobi@microweber.com>
2023-03-30 16:42:51 +0300

    Update App.vue

--- src/MicroweberPackages/LiveEdit/resources/js/ui/App.vue
+++ src/MicroweberPackages/LiveEdit/resources/js/ui/App.vue
@@ -6,8 +6,8 @@
         <div id="live-edit-frame-holder"></div>
         <RightSidebar></RightSidebar>
 
-        <ListLayouts></ListLayouts>
-        <ListModules></ListModules>
+<!--        <ListLayouts></ListLayouts>
+        <ListModules></ListModules>-->
 
     </div>
 
@@ -23,6 +23,6 @@
 import Toolbar from "./components/Toolbar/Toolbar.vue";
 import LeftSidebar from "./components/LeftSidebar/LeftSidebar.vue";
 import RightSidebar from "./components/RightSidebar/RightSidebar.vue";
-import ListLayouts from "./components/Layouts/ListLayouts";
-import ListModules from "./components/Modules/ListModules";
+import ListLayouts from "./components/Layouts/ListLayouts.vue";
+import ListModules from "./components/Modules/ListModules.vue";
 </script>
