1cca3514b8df468ba58bf5a1ef79ede95ef0d7f5
Bozhidar Slaveykov <bobi@microweber.com>
2023-03-30 19:18:23 +0300

    update

--- src/MicroweberPackages/LiveEdit/resources/js/ui/components/LeftSidebar/BubbleNav.vue
+++ src/MicroweberPackages/LiveEdit/resources/js/ui/components/LeftSidebar/BubbleNav.vue
@@ -3,18 +3,21 @@
 
     <div id="bubble-nav" class="active">
         <span v-on:click="show('template-settings')">
-           TS
+            <CogIcon class="h-6 w-6 text-blue-500"/>
         </span>
         <span v-on:click="show('show-modules')">
-           Mod
+            <PlayIcon class="h-6 w-6 text-blue-500"/>
         </span>
         <span v-on:click="show('show-layouts')">
-           Layo
+           <PencilIcon class="h-6 w-6 text-blue-500"/>
         </span>
     </div>
 
 </template>
 <script>
+import { ViewListIcon } from '@heroicons/vue/solid'
+import { PencilIcon, PlayIcon, CogIcon } from '@heroicons/vue/outline'
+
 export default {
     methods: {
         show: function (name) {
@@ -22,7 +25,9 @@ export default {
         }
     },
     components: {
-
+        PencilIcon,
+        PlayIcon,
+        CogIcon
     },
     data() {
         return {
