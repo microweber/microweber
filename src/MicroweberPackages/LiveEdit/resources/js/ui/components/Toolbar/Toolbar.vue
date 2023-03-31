dcda98e20c32a5fe4906b5e0b83af1d64107fee2
Peter Ivanov <peter@microweber.com>
2023-03-31 16:22:07 +0300

    update

--- src/MicroweberPackages/LiveEdit/resources/js/ui/components/Toolbar/Toolbar.vue
+++ src/MicroweberPackages/LiveEdit/resources/js/ui/components/Toolbar/Toolbar.vue
@@ -1,9 +1,10 @@
 <script>
 import ResolutionSwitch from "./ResolutionSwitch.vue";
 import Editor from "./Editor.vue";
+import UndoRedo from "./UndoRedo.vue";
 
 export default {
-    components: {Editor, ResolutionSwitch},
+    components: {UndoRedo, Editor, ResolutionSwitch},
     data() {
 
     }
@@ -26,18 +27,7 @@ export default {
 
         <div class="toolbar-col">
             <div class="toolbar-nav toolbar-nav-hover">
-                <button class="mw-le-btn mw-le-btn-icon" id="toolbar-undo" disabled>
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                        <path
-                            d="M12.5,8C9.85,8 7.45,9 5.6,10.6L2,7V16H11L7.38,12.38C8.77,11.22 10.54,10.5 12.5,10.5C16.04,10.5 19.05,12.81 20.1,16L22.47,15.22C21.08,11.03 17.15,8 12.5,8Z"/>
-                    </svg>
-                </button>
-                <button class="mw-le-btn mw-le-btn-icon" id="toolbar-redo" disabled>
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                        <path
-                            d="M18.4,10.6C16.55,9 14.15,8 11.5,8C6.85,8 2.92,11.03 1.54,15.22L3.9,16C4.95,12.81 7.95,10.5 11.5,10.5C13.45,10.5 15.23,11.22 16.62,12.38L13,16H22V7L18.4,10.6Z"/>
-                    </svg>
-                </button>
+                <UndoRedo></UndoRedo>
                 <span class="mw-le-btn mw-le-btn-primary" id="save-button">
                     Save
                 </span>
