9f56283fb1d48bae1fd6a12e8d10d07b9dbf8a91
Alex <raikov@ymail.com>
2023-04-04 12:40:07 +0300

    module handle target

--- src/MicroweberPackages/LiveEdit/resources/js/api-core/core/handles-content/module.js
+++ src/MicroweberPackages/LiveEdit/resources/js/api-core/core/handles-content/module.js
@@ -2,6 +2,7 @@ import {HandleMenu} from "../handle-menu.js";
 import {ElementManager} from "../classes/element.js";
 
 export const ModuleHandleContent = function (rootScope) {
+    var scope = this;
     this.root = ElementManager({
         props: {
             id: 'mw-handle-item-module-root',
@@ -20,7 +21,8 @@ export const ModuleHandleContent = function (rootScope) {
             {
                 "title": "Settings",
                 "icon": '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path d="M20.71,7.04C21.1,6.65 21.1,6 20.71,5.63L18.37,3.29C18,2.9 17.35,2.9 16.96,3.29L15.12,5.12L18.87,8.87M3,17.25V21H6.75L17.81,9.93L14.06,6.18L3,17.25Z" /></svg>',
-                action: (target) => {
+                action: () => {
+                    const target = mw.app.get('liveEdit').handles.get('module').getTarget();
                     mw.app.editor.dispatch('onModuleSettingsRequest', target);
                     var type = target.dataset.type || target.getAttribute('type');
                     type = type.trim();
--- src/MicroweberPackages/LiveEdit/resources/js/api-core/services/components/live-edit/live-edit.js
+++ src/MicroweberPackages/LiveEdit/resources/js/api-core/services/components/live-edit/live-edit.js
@@ -10,15 +10,7 @@ import {ModuleSettings} from "../../services/module-settings";
 
 
 
-import 'https://cdn.interactjs.io/v1.9.20/auto-start/index.js'
-import 'https://cdn.interactjs.io/v1.9.20/actions/drag/index.js'
-import 'https://cdn.interactjs.io/v1.9.20/actions/resize/index.js'
-import 'https://cdn.interactjs.io/v1.9.20/modifiers/index.js'
-import 'https://cdn.interactjs.io/v1.9.20/dev-tools/index.js'
-import interact from 'https://cdn.interactjs.io/v1.9.20/interactjs/index.js'
-
-
-
+ 
 
 
 export const liveEditComponent = () => {
@@ -56,26 +48,7 @@ export const liveEditComponent = () => {
 
 
 
-interact('.mw-handle-item')
-  .resizable({
-    edges: { top: true, left: true, bottom: true, right: true },
-    listeners: {
-      move: function (event) {
-        let { x, y } = event.target.dataset
-
-        x = (parseFloat(x) || 0) + event.deltaRect.left
-        y = (parseFloat(y) || 0) + event.deltaRect.top
-
-        Object.assign(event.target.style, {
-          width: `${event.rect.width}px`,
-          height: `${event.rect.height}px`,
-          transform: `translate(${x}px, ${y}px)`
-        })
-
-        Object.assign(event.target.dataset, { x, y })
-      }
-    }
-  });
+    
 
 
 }
