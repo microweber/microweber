9058090e1eb226305d4788f0a44ff9fed72c7194
Peter Ivanov <peter@microweber.com>
2023-04-04 15:34:45 +0300

    update

---
+++ userfiles/modules/google_maps/src/GoogleMapsServiceProvider.php
@@ -0,0 +1,46 @@
+<?php
+
+
+namespace MicroweberPackages\Modules\GoogleMaps;
+
+
+use Illuminate\Support\Facades\Event;
+use Illuminate\Support\Facades\View;
+use Livewire\Livewire;
+use MicroweberPackages\LiveEdit\Events\ServingLiveEdit;
+use MicroweberPackages\LiveEdit\Events\ServingModuleSettings;
+use MicroweberPackages\Modules\GoogleMaps\Http\Livewire\GoogleMapsSettingsComponent;
+use Spatie\LaravelPackageTools\Package;
+use Spatie\LaravelPackageTools\PackageServiceProvider;
+
+
+class GoogleMapsServiceProvider extends PackageServiceProvider
+{
+    public function configurePackage(Package $package): void
+    {
+        $package->name('microweber-module-google-maps');
+    }
+
+    public function register(): void
+    {
+        parent::register();
+        Event::listen(ServingLiveEdit::class, [$this, 'registerLiveEditAssets']);
+        Event::listen(ServingModuleSettings::class, [$this, 'registerLivewireComponents']);
+
+        Livewire::component('live-edit::google_maps', GoogleMapsSettingsComponent::class);
+
+        // $this->loadRoutesFrom(__DIR__ . '/routes/live_edit.php');
+        View::addNamespace('modules.google_maps', __DIR__ . '/resources/views');
+    }
+
+    public function registerLiveEditAssets(ServingLiveEdit $event): void
+    {
+
+    }
+
+    public function registerLivewireComponents(ServingModuleSettings $event): void
+    {
+
+    }
+
+}
---
+++ userfiles/modules/google_maps/src/resources/views/livewire/index.blade.php
@@ -0,0 +1,10 @@
+<div>
+    <div>
+        <input type="text" wire:model.debounce.100ms="settings.data-address"/>
+        <input type="range" min="1" max="10" wire:model.debounce.100ms="settings.data-zoom"/>
+
+
+    </div>
+
+
+</div>
