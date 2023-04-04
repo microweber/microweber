eb3e17e3ac1544183e7ab3b1783396c8052efc19
Bozhidar Slaveykov <bobi@microweber.com>
2023-04-04 19:14:06 +0300

    Update template-settings.js

--- src/MicroweberPackages/LiveEdit/resources/js/api-core/services/services/template-settings.js
+++ src/MicroweberPackages/LiveEdit/resources/js/api-core/services/services/template-settings.js
@@ -8,8 +8,8 @@ export class TemplateSettings extends MicroweberBaseClass {
         $.get(mw.settings.api_url + "template/delete_compiled_css?path="+source_file+"&option_group=" + option_group, function () {
             mw.top().notification.success("Reloading styles", 7000);
 
-            mw.parent().$("#theme-style").attr('href', source_file + '&t=' + mw.random());
-            mw.tools.refresh(parent.$("#theme-style"));
+            $(document.getElementById('live-editor-frame').contentWindow.document).find("#theme-style").attr('href', source_file + '&t=' + mw.random());
+            mw.tools.refresh($(document.getElementById('live-editor-frame').contentWindow.document).find("#theme-style"));
 
         });
     }
