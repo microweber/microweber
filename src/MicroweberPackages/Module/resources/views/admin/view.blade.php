9bd0529658a8343d8fc3b44960329f2f0f485350
Bozhidar Slaveykov <bobi@microweber.com>
2023-04-06 17:51:24 +0300

    Update view.blade.php

--- src/MicroweberPackages/Module/resources/views/admin/view.blade.php
+++ src/MicroweberPackages/Module/resources/views/admin/view.blade.php
@@ -2,6 +2,8 @@
 
 @section('content')
 
-    <module type="{{$type}}" />
+
+   <div type="{{$type}}" view="admin" class="mw-lazy-load-module" id="id-module-@php echo md5($type); @endphp">
+   </div>
 
 @endsection
