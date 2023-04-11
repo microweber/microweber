51614b690fd62d820449a7ecca9fc499a8bda08a
Nikolay <rachkuw@gmail.com>
2023-04-11 18:36:30 +0300

    update

--- userfiles/modules/settings/src/resources/views/admin/index.blade.php
+++ userfiles/modules/settings/src/resources/views/admin/index.blade.php
@@ -1,7 +1,7 @@
 @extends('admin::layouts.app')
 
 @section('content')
-<div class="col-lg-8 px-md-0 px-2 mx-auto">
+<div class="col-xl-9 col-md-10 col-12 px-md-0 px-2 mx-auto">
     <h1 class="main-pages-title">Settings</h1>
 
     <module type="settings/group/website_group" />
