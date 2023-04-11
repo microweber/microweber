40998d75b6d3c78c3b18b7b943361d682028947f
Bozhidar Slaveykov <bobi@microweber.com>
2023-04-11 16:15:33 +0300

    update

--- src/MicroweberPackages/Marketplace/resources/views/admin/marketplace/livewire/index.blade.php
+++ src/MicroweberPackages/Marketplace/resources/views/admin/marketplace/livewire/index.blade.php
@@ -1,17 +1,19 @@
 <div>
     <div class="container px-5">
-
-
+        
         <div class="card mb-3 ">
             <div class="card-header justify-content-between">
                 <h5 class="card-title mb-0">
                     <i class="mdi mdi-fruit-cherries fs-2 text-primary mr-3"></i> <strong>Marketplace</strong>
                 </h5>
                 <div class="my-2 my-md-0 flex-grow-1 flex-md-grow-0">
-                    <label>Keyword</label>
-                    <input type="text" class="form-control" wire:keydown.enter="filter" wire:model.lazy="keyword" />
-                    <div wire:loading wire:target="keyword" class="spinner-border spinner-border-sm" role="status">
-                        <span class="visually-hidden">Searching...</span>
+                    <div class="input-icon">
+                              <span class="input-icon-addon">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><path d="M10 10m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0"></path><path d="M21 21l-6 -6"></path></svg></span>
+                        <input type="text" class="form-control" placeholder="Search..." wire:keydown.enter="filter" wire:model.lazy="keyword" />
+                        <div wire:loading wire:target="keyword" class="spinner-border spinner-border-sm" role="status">
+                            <span class="visually-hidden">Searching...</span>
+                        </div>
                     </div>
                 </div>
                 <div>
@@ -28,25 +30,30 @@
               <div class="row g-4">
 
                 <div class="col-12">
-                    <button type="button" class="btn @if($category == 'all') btn-primary @else btn-outline-primary @endif" wire:click="filterCategory('all')">
-                        <div wire:loading wire:target="filterCategory('all')" class="spinner-border spinner-border-sm" role="status">
-                            <span class="visually-hidden">Loading...</span>
-                        </div>  All
-                    </button>
-                    <button type="button" class="btn @if($category == 'microweber-template') btn-primary @else btn-outline-primary @endif" wire:click="filterCategory('microweber-template')">
-                        <div wire:loading wire:target="filterCategory('microweber-template')" class="spinner-border spinner-border-sm" role="status">
-                            <span class="visually-hidden">Loading...</span>
-                        </div>
-                        <i class="mdi mr-1 mdi-pencil-ruler"></i> Templates
-                    </button>
-                    <button type="button" class="btn @if($category == 'microweber-module') btn-primary @else btn-outline-primary @endif" wire:click="filterCategory('microweber-module')">
-                        <div wire:loading wire:target="filterCategory('microweber-module')" class="spinner-border spinner-border-sm" role="status">
-                            <span class="visually-hidden">Loading...</span>
-                        </div>
-                        <i class="mdi mr-1 mdi-view-grid-plus"></i> Modules
-                    </button>
+                    <div>
+                        <p>Welcome to the marketplace Here you will find new modules, templates and updates</p>
+                    </div>
+                    <div class="btn-group d-flex">
+                        <button type="button" class="btn @if($category == 'all') btn-primary @else btn-outline-primary @endif" wire:click="filterCategory('all')">
+                            <div wire:loading wire:target="filterCategory('all')" class="spinner-border spinner-border-sm" role="status">
+                                <span class="visually-hidden">Loading...</span>
+                            </div>  All
+                        </button>
+                        <button type="button" class="btn @if($category == 'microweber-template') btn-primary @else btn-outline-primary @endif" wire:click="filterCategory('microweber-template')">
+                            <div wire:loading wire:target="filterCategory('microweber-template')" class="spinner-border spinner-border-sm" role="status">
+                                <span class="visually-hidden">Loading...</span>
+                            </div>
+                            <i class="mdi mr-1 mdi-pencil-ruler"></i> Templates
+                        </button>
+                        <button type="button" class="btn @if($category == 'microweber-module') btn-primary @else btn-outline-primary @endif" wire:click="filterCategory('microweber-module')">
+                            <div wire:loading wire:target="filterCategory('microweber-module')" class="spinner-border spinner-border-sm" role="status">
+                                <span class="visually-hidden">Loading...</span>
+                            </div>
+                            <i class="mdi mr-1 mdi-view-grid-plus"></i> Modules
+                        </button>
+                    </div>
                 </div>
-                  
+
                 <div class="col-12">
                     <div class="row row-cards">
 
