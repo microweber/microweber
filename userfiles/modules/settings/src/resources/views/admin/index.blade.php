881343252b9da58289c2f532d18e4c4adfaa4993
Nikolay <rachkuw@gmail.com>
2023-04-11 16:51:44 +0300

    update

--- userfiles/modules/settings/src/resources/views/admin/index.blade.php
+++ userfiles/modules/settings/src/resources/views/admin/index.blade.php
@@ -1,7 +1,9 @@
 @extends('admin::layouts.app')
 
 @section('content')
-<div class="col-lg-8 mx-auto">
+<div class="col-lg-8 px-md-0 px-2 mx-auto">
+    <h1 class="main-pages-title">Settings</h1>
+
     <module type="settings/group/website_group" />
 
     <?php  if (get_option('shop_disabled', 'website') != 'y') { ?>
