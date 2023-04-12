aef23fbbbb0a348d355691ae56d37be3f26c860e
Bozhidar Slaveykov <bobi@microweber.com>
2023-04-12 15:10:36 +0300

    update

--- src/MicroweberPackages/Marketplace/Http/Livewire/Admin/MarketplaceItemModal.php
+++ src/MicroweberPackages/Marketplace/Http/Livewire/Admin/MarketplaceItemModal.php
@@ -2,12 +2,29 @@
 
 namespace MicroweberPackages\Marketplace\Http\Livewire\Admin;
 
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Cache;
 use LivewireUI\Modal\ModalComponent;
+use MicroweberPackages\ComposerClient\Client;
+use MicroweberPackages\Package\MicroweberComposerClient;
 
 class MarketplaceItemModal extends ModalComponent
 {
-
     public $name;
+    public $package = [];
+
+    public function mount()
+    {
+        $name = $this->name;
+        $package = Cache::remember('livewire-marketplace-' . $name, Carbon::now()->addHours(12), function () use($name) {
+            $marketplace = new MicroweberComposerClient();
+            return $marketplace->search([
+                'require_name'=>$name
+            ]);
+        });
+
+        $this->package = $package;
+    }
 
     public function render()
     {
