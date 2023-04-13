e02f4ddf93571280b938e73f08da281149b84283
Bozhidar Slaveykov <bobi@microweber.com>
2023-04-13 13:52:49 +0300

    fix parser module loading on blade tag compiler

--- src/MicroweberPackages/Category/resources/views/admin/category/index.blade.php
+++ src/MicroweberPackages/Category/resources/views/admin/category/index.blade.php
@@ -3,16 +3,7 @@
 @section('content')
 
     <div class="px-5">
-
-        @php
-        $enableShop = 0;
-        if(isset($isShop)) {
-            $enableShop = 1;
-        }
-        @endphp
-
-        <module type="categories/manage" is_shop="{{$enableShop}}" id="category-manage" />
-
+        <module type="categories/manage" id="category-manage" @if(isset($isShop)) is_shop="1" @endif />
     </div>
 
 @endsection
