13713ac18a75bf3f236a6f8410ff697889e61bc3
Nikolay <rachkuw@gmail.com>
2023-04-19 13:04:44 +0300

    update

--- userfiles/modules/updates/index.php
+++ userfiles/modules/updates/index.php
@@ -5,51 +5,35 @@ if (mw()->ui->disable_marketplace) {
 }
 ?>
 
-<?php if (MW_VERSION < '1.2.0'): ?>
-    <script>
-        mw.lib.require('bootstrap4');
-    </script>
-<?php endif; ?>
 
 <?php if (isset($params['backend'])): ?>
     <module type="admin/modules/info"/>
 <?php endif; ?>
 
-<div class="d-flex justify-content-between mt-3">
-    <div class="card-header">
-        <?php $module_info = module_info($params['module']); ?>
-        <h5 class="card-title">
-            <?php if (isset($module_info['icon'])):?>
-                <img height="auto" width="25px" src="<?php echo $module_info['icon']; ?>" class="module-icon-svg-fill"/>
-                <strong><?php _e('System updates'); ?></strong>
-            <?php endif; ?>
-        </h5>
-    </div>
+<div class="d-flex justify-content-between">
+        <strong class="main-pages-title"><?php _e('System updates'); ?></strong>
+        <div>
+            <?php
+            if (is_module('standalone-updater')) {
+                ?>
+                <a href="<?php echo admin_url('module/view?type=standalone-updater');?>" class="btn btn-primary"><i class="fa fa-clock"></i> <?php _e('Check for system updates'); ?></a>
+            <?php
+            } else {
+            ?>
+                <button class="btn btn-primary" onclick="mw.admin.admin_package_manager.install_composer_package_by_package_name('microweber-modules/standalone-updater');"><i class="fa fa-download"></i> <?php _e('Install Standalone Updater'); ?></button>
 
-
-        <?php
-        if (is_module('standalone-updater')) {
-        ?>
-            <a href="<?php echo admin_url('module/view?type=standalone-updater');?>" class="btn btn-primary"><i class="fa fa-clock"></i> <?php _e('Check for system updates'); ?></a>
-        <?php
-        } else {
-        ?>
-            <button class="btn btn-primary" onclick="mw.admin.admin_package_manager.install_composer_package_by_package_name('microweber-modules/standalone-updater');"><i class="fa fa-download"></i> <?php _e('Install Standalone Updater'); ?></button>
-
-            <script>
-                $(document).ready(function () {
-                    mw.on('install_composer_package_success', function (e, data) {
-                        window.location.href = '<?php echo admin_url('module/view?type=standalone-updater');?>';
+                <script>
+                    $(document).ready(function () {
+                        mw.on('install_composer_package_success', function (e, data) {
+                            window.location.href = '<?php echo admin_url('module/view?type=standalone-updater');?>';
+                        });
                     });
-                });
-            </script>
-        <?php
-        }
-        ?>
-
+                </script>
+                <?php
+            }
+            ?>
+        </div>
 </div>
-<br />
-<br />
 
 <module type="admin/packages" show_only_updates="true" />
 
