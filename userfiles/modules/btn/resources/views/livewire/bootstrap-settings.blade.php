69c8b6f3e12f9de3915a99b1e979d10a4a35b7b7
Peter Ivanov <peter@microweber.com>
2023-04-19 15:07:30 +0300

    update

--- userfiles/modules/btn/resources/views/livewire/bootstrap-settings.blade.php
+++ userfiles/modules/btn/resources/views/livewire/bootstrap-settings.blade.php
@@ -1,29 +1,42 @@
 <div>
     <div>
+        <div class="mb-3">
 
-        <label class="form-label"><?php _e("Style"); ?></label>
-        <select class="form-select" wire:model="settings.button_style">
-            <option value="btn-default"><?php _e("Default"); ?></option>
-            <option value="btn-primary"><?php _e("Primary"); ?></option>
-            <option value="btn-secondary"><?php _e("Secondary"); ?></option>
-            <option value="btn-success"><?php _e("Success"); ?></option>
-            <option value="btn-danger"><?php _e("Danger"); ?></option>
-            <option value="btn-warning"><?php _e("Warning"); ?></option>
-            <option value="btn-info"><?php _e("Info"); ?></option>
-            <option value="btn-light"><?php _e("Light"); ?></option>
-            <option value="btn-dark"><?php _e("Dark"); ?></option>
-        </select>
-        <label class="form-label"><?php _e("Size"); ?></label>
-        <select class="form-select" wire:model="settings.button_size">
-            <option value=""><?php _e("Default"); ?></option>
-            <option value="btn-lg"><?php _e("Large"); ?></option>
-            <option value="btn-md"><?php _e("Medium"); ?></option>
-            <option value="btn-sm"><?php _e("Small"); ?></option>
-            <option value="btn-xs"><?php _e("Mini"); ?></option>
-        </select>
+            <div class="form-floating">
+                <select class="form-select" id="floatingSelectButtonStyle" aria-label="<?php _ejs("Style"); ?>"
+                        wire:model="settings.button_style">
+                    <option value="btn-default"><?php _e("Default"); ?></option>
+                    <option value="btn-primary"><?php _e("Primary"); ?></option>
+                    <option value="btn-secondary"><?php _e("Secondary"); ?></option>
+                    <option value="btn-success"><?php _e("Success"); ?></option>
+                    <option value="btn-danger"><?php _e("Danger"); ?></option>
+                    <option value="btn-warning"><?php _e("Warning"); ?></option>
+                    <option value="btn-info"><?php _e("Info"); ?></option>
+                    <option value="btn-light"><?php _e("Light"); ?></option>
+                    <option value="btn-dark"><?php _e("Dark"); ?></option>
+                </select>
+                <label for="floatingSelectButtonStyle"><?php _e("Style"); ?></label>
+            </div>
+        </div>
+
+        <div class="mb-3">
+            <div class="form-floating">
+                <select class="form-select" id="floatingSelectButtonSize" aria-label="<?php _ejs("Size"); ?>"
+                        wire:model="settings.button_size">
+                    <option value=""><?php _e("Default"); ?></option>
+                    <option value="btn-lg"><?php _e("Large"); ?></option>
+                    <option value="btn-md"><?php _e("Medium"); ?></option>
+                    <option value="btn-sm"><?php _e("Small"); ?></option>
+                    <option value="btn-xs"><?php _e("Mini"); ?></option>
+                </select>
+                <label for="floatingSelectButtonSize"><?php _e("Size"); ?></label>
+            </div>
+
+
+        </div>
+
 
     </div>
-
 
 
 </div>
--- userfiles/modules/btn/resources/views/livewire/settings.blade.php
+++ userfiles/modules/btn/resources/views/livewire/settings.blade.php
@@ -13,32 +13,25 @@
         <div class="tab-pane fade show active" wire:ignore.self id="text">
 
 
-            <div class="mb-3">
-                <label class="form-label">Button Text</label>
-                <input type="text" class="form-control mb-2" wire:model.debounce.100ms="settings.text"
-                       placeholder="Text..">
-
-            </div>
-
+            <x-microweber-module-btn::btn-text :settings="$settings"/>
 
             <x-microweber-module-btn::btn-url-picker :url="$url" :settings="$settings"/>
 
 
-
         </div>
-
 
 
         <div class="tab-pane fade" wire:ignore.self id="design">
 
             <div class="mt-4">
                 <div>
-                    <livewire:microweber-live-edit::module-select-template :moduleId="$moduleId" :moduleType="$moduleType" />
+                    <livewire:microweber-live-edit::module-select-template :moduleId="$moduleId"
+                                                                           :moduleType="$moduleType"/>
                 </div>
                 <div>
-                <x-microweber-module-btn::btn-align :settings="$settings"/>
+                    <x-microweber-module-btn::btn-align :settings="$settings"/>
 
-                <x-microweber-ui::icon-picker wire:model="settings.icon" :value="$settings['icon']"/>
+                    <x-microweber-ui::icon-picker wire:model="settings.icon" :value="$settings['icon']"/>
                 </div>
             </div>
         </div>
