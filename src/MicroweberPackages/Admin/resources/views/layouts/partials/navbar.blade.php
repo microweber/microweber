f631479f7cd27f97f7904a3e5eba8d8a40de4e81
Nikolay <rachkuw@gmail.com>
2023-04-21 18:25:44 +0300

    update

--- src/MicroweberPackages/Admin/resources/views/layouts/partials/navbar.blade.php
+++ src/MicroweberPackages/Admin/resources/views/layouts/partials/navbar.blade.php
@@ -377,8 +377,8 @@
             <ul class="navbar-nav-padding nav-item-profile-wrapper">
                 <li class="nav-item nav-item-profile">
                     <a href="{{admin_url('user/profile')}}" class="nav-link fs-3">
-                        <div class="mw-admin-sidebar-profile" style="background:url('<?php echo user_picture(); ?>');background-size: cover;background-position: top;">
-
+                        <div class="mw-admin-sidebar-profile">
+                                <span style="font-size: 14px;">SB</span>
                         </div>
                         <div>
                             <?php echo get_username_short(); ?>
