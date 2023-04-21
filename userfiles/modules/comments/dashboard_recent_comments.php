f4b125e213852369b2378bf17651ade44d34f567
Nikolay <rachkuw@gmail.com>
2023-04-21 17:58:26 +0300

    update

--- userfiles/modules/comments/dashboard_recent_comments.php
+++ userfiles/modules/comments/dashboard_recent_comments.php
@@ -17,8 +17,8 @@ $ccount = get_comments($comments_data);
 <div class="card mb-4 dashboard-admin-cards">
     <div class="card-body px-xxl-4 d-flex justify-content-between align-items-center">
         <div class="d-flex align-items-center">
-            <div class="dashboard-icons-wrapper wrapper-icon-emails">
-                <img src="<?php print modules_url()?>/microweber/api/libs/mw-ui/assets/img/admin-dashboard-comments.png" alt="messages">
+            <div class="dashboard-icons-wrapper wrapper-icon-comments">
+                <svg xmlns="http://www.w3.org/2000/svg" height="40" viewBox="0 96 960 960" width="40"><path d="M80 976V242.666q0-26.333 19.833-46.499Q119.666 176 146.666 176h666.668q26.333 0 46.499 20.167Q880 216.333 880 242.666v506.668q0 26.333-20.167 46.499Q839.667 816 813.334 816H240L80 976Zm66.666-160.999 65.667-65.667h601.001V242.666H146.666v572.335Zm0-572.335v572.335-572.335Z"/></svg>
             </div>
 
             <div class="row">
--- userfiles/modules/site_stats/src/views/visits_graph.php
+++ userfiles/modules/site_stats/src/views/visits_graph.php
@@ -244,10 +244,10 @@ if ($params['period']) {
 
                <div class="dashboard-admin-cards d-flex align-items-center">
                    <div class="dashboard-icons-wrapper wrapper-icon-statistics">
-                       <img src="<?php print modules_url()?>/microweber/api/libs/mw-ui/assets/img/admin-dashboard-statistics.png" alt="messages">
+                       <img src="<?php print modules_url()?>/microweber/api/libs/mw-ui/assets/img/admin-dashboard-statistics.svg" alt="messages">
                    </div>
 
-                   <div class="row">
+                   <div class="row" style="padding-top: 10px !important; padding-bottom: 10px !important;">
                        <p> <?php _e("Statistics") ?></p>
 
                        <div class="d-flex">
@@ -301,13 +301,15 @@ if ($params['period']) {
 
 
            <div class="stats_box_footer">
-               <div class="px-4 d-flex justify-content-between">
+               <div class="d-flex justify-content-between">
                    <div class="stats-box-colorscheme col-6 col-sm-2 d-flex align-items-center justify-content-center justify-content-sm-start">
-                       <i class="mdi mdi-eye mdi-24px text-muted text-opacity-6"></i> <span class="text-primary mx-2"><?php print $views_count; ?></span> <span><?php _e('Views'); ?></span>
+                       <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 96 960 960" width="24"><path d="m438 630 198-198-57-57-141 141-56-56-57 57 113 113Zm42 240q122-112 181-203.5T720 504q0-109-69.5-178.5T480 256q-101 0-170.5 69.5T240 504q0 71 59 162.5T480 870Zm0 106Q319 839 239.5 721.5T160 504q0-150 96.5-239T480 176q127 0 223.5 89T800 504q0 100-79.5 217.5T480 976Zm0-472Z"/></svg>
+                       <span class="text-primary mx-2"><?php print $views_count; ?></span> <span><?php _e('Views'); ?></span>
                    </div>
 
                    <div class="stats-box-colorscheme col-6 col-sm-2 d-flex align-items-center justify-content-center">
-                       <i class="mdi mdi-account-multiple mdi-24px text-muted text-opacity-6"></i> <span class="text-primary mx-2"><?php print $visits_count; ?></span> <span><?php _e('Visitors') ?></span>
+                       <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 96 960 960" width="24"><path d="M480 576q-66 0-113-47t-47-113q0-66 47-113t113-47q66 0 113 47t47 113q0 66-47 113t-113 47ZM160 896V784q0-34 17.5-62.5T224 678q62-31 126-46.5T480 616q66 0 130 15.5T736 678q29 15 46.5 43.5T800 784v112H160Zm80-80h480v-32q0-11-5.5-20T700 750q-54-27-109-40.5T480 696q-56 0-111 13.5T260 750q-9 5-14.5 14t-5.5 20v32Zm240-320q33 0 56.5-23.5T560 416q0-33-23.5-56.5T480 336q-33 0-56.5 23.5T400 416q0 33 23.5 56.5T480 496Zm0-80Zm0 400Z"/></svg>
+                       <span class="text-primary mx-2"><?php print $visits_count; ?></span> <span><?php _e('Visitors') ?></span>
                    </div>
 
 <!--                   <div class="stats-box-colorscheme col-6 col-sm d-flex align-items-center justify-content-center">-->
@@ -319,7 +321,7 @@ if ($params['period']) {
 <!--                   </div>-->
 
                    <div class="stats-box-colorscheme col-12 col-sm d-flex align-items-center  justify-content-center justify-content-sm-end">
-                       <a class="btn btn-link show-more-stats text-capitalize"><?php _e('Show more'); ?></a>
+                       <a class="btn btn-link show-more-stats text-capitalize" style="padding: 0 !important;"><?php _e('Show more'); ?></a>
                    </div>
                </div>
 
