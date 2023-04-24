7ad844568e1952cb0aa570b5c9ecebebb367e31f
Bozhidar Slaveykov <bobi@microweber.com>
2023-04-24 15:19:49 +0300

    update

--- src/MicroweberPackages/User/resources/views/admin/users/edit.blade.php
+++ src/MicroweberPackages/User/resources/views/admin/users/edit.blade.php
@@ -3,14 +3,14 @@
 @section('content')
     <div class="mx-5">
 
-        @livewire('admin::edit-user.update-profile-information-form')
+        @livewire('admin::edit-user.update-profile-information-form', ['userId' => $user->id])
         <x-microweber-ui::section-border/>
 
-        @livewire('admin::edit-user.update-password-form')
+        @livewire('admin::edit-user.update-password-form', ['userId' => $user->id])
 
         <x-microweber-ui::section-border/>
 
-        @livewire('admin::edit-user.delete-user-form')
+        @livewire('admin::edit-user.delete-user-form', ['userId' => $user->id])
 
     </div>
 @endsection
