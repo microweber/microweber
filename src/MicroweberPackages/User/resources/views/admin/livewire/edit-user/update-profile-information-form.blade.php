70dfcb561bd6ca27fd57956bf87966fe970e7bf9
Bozhidar Slaveykov <bobi@microweber.com>
2023-04-25 12:42:47 +0300

    Update update-profile-information-form.blade.php

--- src/MicroweberPackages/User/resources/views/admin/livewire/edit-user/update-profile-information-form.blade.php
+++ src/MicroweberPackages/User/resources/views/admin/livewire/edit-user/update-profile-information-form.blade.php
@@ -88,6 +88,28 @@
             <x-microweber-ui::input-error for="phone" class="mt-2" />
         </div>
 
+        <!-- Role -->
+        <div class="col-span-6 sm:col-span-4 mt-2">
+            <x-microweber-ui::label for="role" value="Role of the user" />
+            <div class="text-muted">Choose the current role of the user. </div>
+            <select id="role" class="form-control" wire:model.defer="state.is_admin">
+                <option value="0">User</option>
+                <option value="1">Admin</option>
+            </select>
+            <x-microweber-ui::input-error for="role" class="mt-2" />
+        </div>
+
+        <!-- Is Active -->
+        <div class="col-span-6 sm:col-span-4 mt-2">
+            <x-microweber-ui::label for="is_active" value="Is Active?" />
+            <div class="text-muted">Choose the current status of this user</div>
+            <select id="is_active" class="form-control" wire:model.defer="state.is_active">
+                <option value="1">Active</option>
+                <option value="0">Disabled</option>
+            </select>
+            <x-microweber-ui::input-error for="is_active" class="mt-2" />
+        </div>
+
     </x-slot>
 
     <x-slot name="actions">
@@ -100,3 +122,9 @@
         </x-microweber-ui::button>
     </x-slot>
 </x-microweber-ui::form-section>
+<script>
+    import Options from "../../../../../../../../userfiles/modules/microweber/api/libs/chosen/options.html";
+    export default {
+        components: {Options}
+    }
+</script>
