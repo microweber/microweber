a63d0e8eb0e38d1b16b1ef576bd18f91abadf745
Nikolay <rachkuw@gmail.com>
2023-04-25 16:41:25 +0300

    update

--- src/MicroweberPackages/Admin/resources/views/layouts/partials/navbar.blade.php
+++ src/MicroweberPackages/Admin/resources/views/layouts/partials/navbar.blade.php
@@ -119,7 +119,7 @@
 
                 <?php if (user_can_view_module(['module' => 'content'])): ?>
                 <li class="nav-item dropdown">
-                    <a href="<?php echo route('admin.content.index'); ?>" class="nav-link dropdown-toggle py-3 fs-3" data-bs-toggle="dropdown" data-bs-auto-close="false" role="button" aria-expanded="true">
+                    <a  class="nav-link dropdown-toggle py-3 fs-3" data-bs-toggle="dropdown" data-bs-auto-close="false" role="button" aria-expanded="true">
                         <svg style="margin-right: 20px;" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 96 960 960" width="24"><path d="M480 976q-83 0-156-31.5T197 859q-54-54-85.5-127T80 576q0-83 31.5-156T197 293q54-54 127-85.5T480 176q83 0 156 31.5T763 293q54 54 85.5 127T880 576q0 83-31.5 156T763 859q-54 54-127 85.5T480 976Zm-40-82v-78q-33 0-56.5-23.5T360 736v-40L168 504q-3 18-5.5 36t-2.5 36q0 121 79.5 212T440 894Zm276-102q20-22 36-47.5t26.5-53q10.5-27.5 16-56.5t5.5-59q0-98-54.5-179T600 280v16q0 33-23.5 56.5T520 376h-80v80q0 17-11.5 28.5T400 496h-80v80h240q17 0 28.5 11.5T600 616v120h40q26 0 47 15.5t29 40.5Z"/></svg>
                         <span class="badge-holder"><?php _e("Website"); ?></span>
                     </a>
@@ -127,10 +127,18 @@
                     <div class="dropdown-menu <?php echo $website_class; ?>" data-bs-popper="static">
                         <div class="dropdown-menu-columns">
                             <div class="dropdown-menu-column">
-                                <a href="<?php echo route('admin.page.index'); ?>" class="dropdown-item justify-content-between <?php if ($action == 'pages'): ?> active <?php endif; ?>">
+                                <a href="<?php echo route('admin.page.index'); ?>" class="dropdown-item dropdown justify-content-between <?php if ($action == 'pages'): ?> active <?php endif; ?>">
                                         <?php _e("Pages"); ?>
-                                    <span class="add-new" data-bs-toggle="tooltip" title="<?php _e("Add new page") ?>" data-href="<?php print route('admin.page.create'); ?>"><svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 96 960 960" width="24"><path d="M240 656q-33 0-56.5-23.5T160 576q0-33 23.5-56.5T240 496q33 0 56.5 23.5T320 576q0 33-23.5 56.5T240 656Zm240 0q-33 0-56.5-23.5T400 576q0-33 23.5-56.5T480 496q33 0 56.5 23.5T560 576q0 33-23.5 56.5T480 656Zm240 0q-33 0-56.5-23.5T640 576q0-33 23.5-56.5T720 496q33 0 56.5 23.5T800 576q0 33-23.5 56.5T720 656Z"/></svg></svg></span>
+                                    <span onclick="openMenuNavbarDots();" class="add-new" data-bs-toggle="tooltip" title="<?php _e("Add new page") ?>" data-href="<?php print route('admin.page.create'); ?>"><svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 96 960 960" width="24"><path d="M240 656q-33 0-56.5-23.5T160 576q0-33 23.5-56.5T240 496q33 0 56.5 23.5T320 576q0 33-23.5 56.5T240 656Zm240 0q-33 0-56.5-23.5T400 576q0-33 23.5-56.5T480 496q33 0 56.5 23.5T560 576q0 33-23.5 56.5T480 656Zm240 0q-33 0-56.5-23.5T640 576q0-33 23.5-56.5T720 496q33 0 56.5 23.5T800 576q0 33-23.5 56.5T720 656Z"/></svg></svg></span>
                                 </a>
+
+
+                                <div class="dropdown-menu show">
+                                    <a class="dropdown-item" href="#">Action</a>
+                                    <a class="dropdown-item" href="#">Another action</a>
+                                    <a class="dropdown-item" href="#">Third action</a>
+                                </div>
+
 
                                 <a class="dropdown-item justify-content-between <?php if ($action == 'posts'): ?> active <?php endif; ?>" href="<?php echo route('admin.post.index'); ?>">
                                         <?php _e("Posts"); ?>
