e746eca1201d09e0342e98b22746046b639f8cd3
Bozhidar Slaveykov <bobi@microweber.com>
2023-04-27 17:50:40 +0300

    update

--- src/MicroweberPackages/Admin/resources/views/layouts/partials/navbar-link.blade.php
+++ src/MicroweberPackages/Admin/resources/views/layouts/partials/navbar-link.blade.php
@@ -1,12 +1,12 @@
 <li class="nav-item @if(isset($class)) {{$class}} @endif">
-    <a href="{{$item->url()}}" class="nav-link fs-3" @if($item->active) x-init="setTimeout(function() { $el.classList.add('active'); }, 300);" @endif">
-        @if($item->icon)
-            {!! $item->icon !!}
-        @endif
+    <a href="{{$item->getUri()}}" class="nav-link fs-3" @if($item) x-init="setTimeout(function() { $el.classList.add('active'); }, 300);" @endif >
+
+        {!! $item->getAttribute('icon') !!}
         <span>
            <div x-init="setTimeout(function() { $el.classList.remove('placeholder'); $el.classList.remove('placeholder-xs'); }, 300);" class="placeholder placeholder-xs">
-                {{$item->text()}}
+               {{$item->getName()}}
            </div>
         </span>
+
     </a>
 </li>
