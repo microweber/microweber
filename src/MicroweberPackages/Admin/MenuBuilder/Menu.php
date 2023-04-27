1602bfadbb8de8d5e9acff8b10f0d0ca66e4fd48
Bozhidar Slaveykov <bobi@microweber.com>
2023-04-27 17:32:08 +0300

    update

--- src/MicroweberPackages/Admin/MenuBuilder/Menu.php
+++ src/MicroweberPackages/Admin/MenuBuilder/Menu.php
@@ -2,87 +2,32 @@
 
 namespace MicroweberPackages\Admin\MenuBuilder;
 
-use Illuminate\Support\Facades\Route;
-use MicroweberPackages\Admin\MenuBuilder\Traits\HasIcon;
-use MicroweberPackages\Admin\MenuBuilder\Traits\HasId;
-use MicroweberPackages\Admin\MenuBuilder\Traits\HasOrder;
-use Spatie\Menu\Html\Attributes;
-use Spatie\Menu\Item;
+class Menu {
 
-class Menu extends \Spatie\Menu\Menu
-{
-    use HasIcon;
-    use HasOrder;
+    public $menuItems;
+    public $menuFactory;
 
-    public ?string $id = null;
-
-    public string | Item $prepend = '';
-
-
-    protected function __construct(Item ...$items)
+    public function __construct($name = 'default')
     {
-        $this->items = $items;
-
-        $this->htmlAttributes = new Attributes();
-        $this->parentAttributes = new Attributes();
-
-        $this->setActive(function (Link $link) {
-            if ($link->route) {
-                if (Route::currentRouteName() == $link->route) {
-                    return true;
-                }
-            }
-            if ($link->url() == url()->current()) {
-                return true;
-            }
-        });
+        $this->menuFactory = new \Knp\Menu\MenuFactory();
+        $this->menuItems = $this->menuFactory->createItem($name);
     }
 
-    public function reordered()
+    public function addChild($title, $options = [])
     {
-        $items = $this->items();
-
-//        $menu = new Menu();
-//        foreach ($items as $item) {
-//            if (method_exists($item, 'items')) {
-//
-//            }
-//        }
-
-        usort($items, function($item,$secondItem){
-            return $item->order > $secondItem->order;
-        });
-
-        $this->items = $items;
-
+        $this->menuItems->addChild($title, $options);
         return $this;
     }
 
-    public function modify($id)
+    public function getChild($title)
     {
-        $items = $this->items();
-
-        foreach ($items as $item) {
-            if (method_exists($item, 'items')) {
-                foreach ($item->items() as $subItem) {
-                //  dump($item->id());
-                }
-            }
-        }
-
-        $newSubmenu = Menu::new();
-        $newSubmenu->id = $id;
-        $this->submenu(
-            Link::to('', $id),
-            $newSubmenu
-        );
-
-        return $newSubmenu;
+        return $this->menuItems->getChild($title);
     }
 
-    public function items()
+    public function render()
     {
-        return $this->items;
+        $renderer = new \Knp\Menu\Renderer\ListRenderer(new \Knp\Menu\Matcher\Matcher());
+        return $renderer->render($this->menuItems);
     }
 
 }
--- src/MicroweberPackages/Template/Traits/HasMenus.php
+++ src/MicroweberPackages/Template/Traits/HasMenus.php
@@ -31,7 +31,7 @@ trait HasMenus
                 ->icon('<svg style="margin-right: 20px;" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 96 960 960" width="24"><path d="M480 976q-83 0-156-31.5T197 859q-54-54-85.5-127T80 576q0-83 31.5-156T197 293q54-54 127-85.5T480 176q83 0 156 31.5T763 293q54 54 85.5 127T880 576q0 83-31.5 156T763 859q-54 54-127 85.5T480 976Zm-40-82v-78q-33 0-56.5-23.5T360 736v-40L168 504q-3 18-5.5 36t-2.5 36q0 121 79.5 212T440 894Zm276-102q20-22 36-47.5t26.5-53q10.5-27.5 16-56.5t5.5-59q0-98-54.5-179T600 280v16q0 33-23.5 56.5T520 376h-80v80q0 17-11.5 28.5T400 496h-80v80h240q17 0 28.5 11.5T600 616v120h40q26 0 47 15.5t29 40.5Z"/></svg>')
             ,
             Menu::new()
-            ->id('website')
+            ->menuId('website')
             ->add(Link::to(admin_url('page'), 'Pages'))
             ->add(Link::to(admin_url('category'), 'Category'))
             ->add(Link::to(admin_url('post'), 'Post'))
