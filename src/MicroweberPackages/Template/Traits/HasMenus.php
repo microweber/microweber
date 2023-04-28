d3bd588a9990899825d1adc03e00cdb850e929c7
Bozhidar Slaveykov <bobi@microweber.com>
2023-04-28 15:11:36 +0300

    update

--- src/MicroweberPackages/Template/Traits/HasMenus.php
+++ src/MicroweberPackages/Template/Traits/HasMenus.php
@@ -3,6 +3,7 @@
 namespace MicroweberPackages\Template\Traits;
 
 use Illuminate\Support\Facades\Route;
+use MicroweberPackages\Admin\Facades\AdminManager;
 use MicroweberPackages\Admin\MenuBuilder\Link;
 use MicroweberPackages\Admin\MenuBuilder\Menu;
 use Spatie\Menu\Item;
@@ -26,12 +27,18 @@ trait HasMenus
             ]
         ]);
 
+        $this->menus['left_menu_top']->getChild('Dashboard')
+            ->setExtra('orderNumber', 1);
+
         $this->menus['left_menu_top']->addChild('Website', [
             'uri' => admin_url(),
             'attributes'=>[
                 'icon'=>'<svg style="margin-right: 20px;" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 96 960 960" width="24"><path d="M480 976q-83 0-156-31.5T197 859q-54-54-85.5-127T80 576q0-83 31.5-156T197 293q54-54 127-85.5T480 176q83 0 156 31.5T763 293q54 54 85.5 127T880 576q0 83-31.5 156T763 859q-54 54-127 85.5T480 976Zm-40-82v-78q-33 0-56.5-23.5T360 736v-40L168 504q-3 18-5.5 36t-2.5 36q0 121 79.5 212T440 894Zm276-102q20-22 36-47.5t26.5-53q10.5-27.5 16-56.5t5.5-59q0-98-54.5-179T600 280v16q0 33-23.5 56.5T520 376h-80v80q0 17-11.5 28.5T400 496h-80v80h240q17 0 28.5 11.5T600 616v120h40q26 0 47 15.5t29 40.5Z"/></svg>'
             ]
         ]);
+
+        $this->menus['left_menu_top']->getChild('Website')
+            ->setExtra('orderNumber', 2);
 
         $this->menus['left_menu_top']->getChild('Website')
             ->addChild('Pages', [
@@ -64,12 +71,76 @@ trait HasMenus
 
     public function getMenu($menu)
     {
+        $this->reorderMenuItems($this->menus[$menu]->menuItems);
+
         return $this->menus[$menu]->menuItems->getChildren();
     }
 
     public function getMenuInstance($menu)
     {
         return $this->menus[$menu];
+    }
+
+    public function reorderMenuItems($menu)
+    {
+
+        $menuOrderArray = array();
+
+        $addLast = array();
+
+        $alreadyTaken = array();
+
+        foreach ($menu->getChildren() as $key => $menuItem) {
+
+            if ($menuItem->hasChildren()) {
+                $this->reorderMenuItems($menuItem);
+            }
+
+            $orderNumber = $menuItem->getExtra('orderNumber');
+            if ($orderNumber != null) {
+                if (!isset($menuOrderArray[$orderNumber])) {
+                    $menuOrderArray[$orderNumber] = $menuItem->getName();
+                } else {
+                    $alreadyTaken[$orderNumber] = $menuItem->getName();
+                    // $alreadyTaken[] = array('orderNumber' => $orderNumber, 'name' => $menuItem->getName());
+                }
+            } else {
+                $addLast[] = $menuItem->getName();
+            }
+        }
+
+        // sort them after first pass
+        ksort($menuOrderArray);
+
+        // handle position duplicates
+        if (count($alreadyTaken)) {
+            foreach ($alreadyTaken as $key => $value) {
+                // the ever shifting target
+                $keysArray = array_keys($menuOrderArray);
+
+                $position = array_search($key, $keysArray);
+
+                if ($position === false) {
+                    continue;
+                }
+
+                $menuOrderArray = array_merge(array_slice($menuOrderArray, 0, $position), array($value), array_slice($menuOrderArray, $position));
+            }
+        }
+
+        // sort them after second pass
+        ksort($menuOrderArray);
+
+        // add items without ordernumber to the end
+        if (count($addLast)) {
+            foreach ($addLast as $key => $value) {
+                $menuOrderArray[] = $value;
+            }
+        }
+
+        if (count($menuOrderArray)) {
+            $menu->reorderChildren($menuOrderArray);
+        }
     }
 
 }
