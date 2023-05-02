84e8c948c15a0aceb269b0a97d0295caaa4a87a7
Bozhidar Slaveykov <bobi@microweber.com>
2023-05-02 13:40:55 +0300

    Update list-modules.blade.php

--- src/MicroweberPackages/Module/resources/views/livewire/admin/list-modules.blade.php
+++ src/MicroweberPackages/Module/resources/views/livewire/admin/list-modules.blade.php
@@ -4,13 +4,13 @@
 
         @foreach($modules as $module)
             <div class="col-md-3">
-                <div class="card card-stacked" style="min-height:140px">
+                <div class="card card-stacked" style="min-height:170px">
                     <div class="card-body text-center d-flex align-items-center justify-content-center flex-column">
 
 
                         <a href="{{module_admin_url($module->name)}}">
                             <img src="{{$module->icon()}}" style="width:64px" />
-                            <h3 class="card-title">
+                            <h3 class="card-title pt-2 text-muted">
                                 {{str_limit(_e($module->name, true), 30)}}
                             </h3>
                         </a>
