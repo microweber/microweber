fff2e2003420d97fbc85859b61bb8bc8150153f7
Bozhidar Slaveykov <bobi@microweber.com>
2023-05-02 13:39:25 +0300

    Update list-modules.blade.php

--- src/MicroweberPackages/Module/resources/views/livewire/admin/list-modules.blade.php
+++ src/MicroweberPackages/Module/resources/views/livewire/admin/list-modules.blade.php
@@ -6,10 +6,14 @@
             <div class="col-md-3">
                 <div class="card card-stacked" style="min-height:140px">
                     <div class="card-body text-center d-flex align-items-center justify-content-center flex-column">
-                        <img src="{{$module->icon()}}" style="width:64px" />
-                        <h3 class="card-title">
-                            {{str_limit(_e($module->name, true), 30)}}
-                        </h3>
+
+
+                        <a href="{{module_admin_url($module->name)}}">
+                            <img src="{{$module->icon()}}" style="width:64px" />
+                            <h3 class="card-title">
+                                {{str_limit(_e($module->name, true), 30)}}
+                            </h3>
+                        </a>
                     </div>
                 </div>
             </div>
