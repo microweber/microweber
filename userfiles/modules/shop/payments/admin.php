88767f5961588817e245d50380d8e2d4cb6ab652
Nikolay <rachkuw@gmail.com>
2023-05-04 18:26:18 +0300

    update

--- userfiles/modules/shop/payments/admin.php
+++ userfiles/modules/shop/payments/admin.php
@@ -119,15 +119,19 @@ $here = dirname(__FILE__) . DS . 'gateways' . DS;
 $payment_modules = get_modules('type=payment_gateway');
 ?>
 
-<h1 class="main-pages-title"><?php _e('Payment methods'); ?></h1>
 
-<div class="card mb-4">
-    <div class="card-body">
+<div class="card">
+    <div class="card-body mb-3">
       <div class="row">
-          <p class="text-muted m-3"><?php _e("Enable and set up the payment method your customers will use to pay"); ?></p>
+            <div class="card-header d-block px-0">
+                <module type="admin/modules/info_module_title" for-module="<?php print $params['module'] ?>"/>
 
-          <div id="db-payment-methods">
-              <?php if (is_array($payment_modules)): ?>
+                <p class="text-muted m-3 d-block"><?php _e("Enable and set up the payment method your customers will use to pay"); ?></p>
+            </div>
+
+          <div class="card bg-azure-lt " id="db-payment-methods">
+              <div class="card-body col-xl-8 col-12 mx-auto my-4">
+                 <?php if (is_array($payment_modules)): ?>
                   <div id="available_providers">
                       <?php foreach ($payment_modules as $payment_module): ?>
                           <?php
@@ -137,48 +141,46 @@ $payment_modules = get_modules('type=payment_gateway');
                           }
                           ?>
 
-
-
-                          <div class="dragable-item card-body mb-3 <?php if (get_option('payment_gw_' . $payment_module['module'], 'payments') == 1): ?>bg-primary-opacity-1<?php endif; ?>" id="module-db-id-<?php print $module_info['id'] ?>">
-                              <div class="card-body py-3">
-                                  <div class="row d-flex align-items-center">
-                                      <div class="col cursor-move-holder">
-                                          <a href="javascript:;" class="btn btn-link  "><i class="mdi mdi-cursor-move"></i></a>
-                                      </div>
-
-                                      <div class="col pl-0 js-change-method-status" style="max-width: 60px;">
-                                          <div class="form-group m-0">
-                                              <div class="custom-control custom-switch m-0">
-                                                  <input onchange="setActiveProvider('#module-db-id-<?php print $module_info['id'] ?>', this);" type="checkbox" class="mw_option_field form-check-input" id="ccheckbox-payment_gw_<?php print $payment_module['module'] ?>" name="payment_gw_<?php print $payment_module['module'] ?>" data-option-group="payments" data-id="shipping_gw_shop/shipping/gateways/country"<?php if (get_option('payment_gw_' . $payment_module['module'], 'payments') == 1): ?> checked="checked" <?php endif; ?> value="1">
-                                                  <label class="custom-control-label" for="ccheckbox-payment_gw_<?php print $payment_module['module'] ?>"></label>
-                                              </div>
-                                          </div>
-                                      </div>
-
-                                      <div class="col pl-0">
-                                          <img src="<?php print $payment_module['icon']; ?>" alt="" class="d-none"/>
-
-                                          <h4 class="gateway-title font-weight-bold mb-0"><?php _e($payment_module['name']) ?></h4>
-
-                                          <small class="text-muted">
-                                              <?php _e($payment_module['name']) ?> <span class="text-primary js-method-on <?php if (get_option('payment_gw_' . $payment_module['module'], 'payments') != 1): ?>d-none<?php endif; ?>"><?php _e("is ON"); ?></span>
-                                          </small>
-                                      </div>
-
-                                      <div class="col text-end text-right text-right">
-                                          <button type="button" onclick="paymentModal('#module-db-id-<?php print $module_info['id'] ?>');" class="btn btn-outline-primary btn-sm"><?php _e('Settings'); ?></button>
-                                      </div>
+                          <div class="dragable-item align-items-center shadow-sm bg-light mb-3 <?php if (get_option('payment_gw_' . $payment_module['module'], 'payments') == 1): ?>bg-primary-opacity-1<?php endif; ?>" id="module-db-id-<?php print $module_info['id'] ?>">
+                              <div class="row d-flex align-items-center">
+                                  <div class="col-1 cursor-move-holder "
+                                      <a href="javascript:;" class="btn btn-link text-blue-lt ">
+                                          <svg class="mdi-cursor-move" fill="currentColor" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 96 960 960" width="24"><path d="M360 896q-33 0-56.5-23.5T280 816q0-33 23.5-56.5T360 736q33 0 56.5 23.5T440 816q0 33-23.5 56.5T360 896Zm240 0q-33 0-56.5-23.5T520 816q0-33 23.5-56.5T600 736q33 0 56.5 23.5T680 816q0 33-23.5 56.5T600 896ZM360 656q-33 0-56.5-23.5T280 576q0-33 23.5-56.5T360 496q33 0 56.5 23.5T440 576q0 33-23.5 56.5T360 656Zm240 0q-33 0-56.5-23.5T520 576q0-33 23.5-56.5T600 496q33 0 56.5 23.5T680 576q0 33-23.5 56.5T600 656ZM360 416q-33 0-56.5-23.5T280 336q0-33 23.5-56.5T360 256q33 0 56.5 23.5T440 336q0 33-23.5 56.5T360 416Zm240 0q-33 0-56.5-23.5T520 336q0-33 23.5-56.5T600 256q33 0 56.5 23.5T680 336q0 33-23.5 56.5T600 416Z"/></svg>
+                                      </a>
                                   </div>
 
-                                  <template class="js-modal-content" style="display: none;">
-                                      <h5 class="mb-0"><?php _e('Enter your API settings'); ?></h5>
-                                      <small class="text-muted mb-3 d-block"><?php _e("Ask your payment provider for this information and put it below"); ?></small>
+                                  <div class="col pl-0 js-change-method-status" style="max-width: 60px;">
 
-                                      <div class="mw-set-payment-gw-options">
-                                          <module type="<?php print $payment_module['module'] ?>" view="admin"/>
+                                      <div class="form-check form-check-single form-switch p-0">
+                                          <input onchange="setActiveProvider('#module-db-id-<?php print $module_info['id'] ?>', this);" type="checkbox" class="mw_option_field form-check-input" id="ccheckbox-payment_gw_<?php print $payment_module['module'] ?>" name="payment_gw_<?php print $payment_module['module'] ?>" data-option-group="payments" data-id="shipping_gw_shop/shipping/gateways/country"<?php if (get_option('payment_gw_' . $payment_module['module'], 'payments') == 1): ?> checked="checked" <?php endif; ?> value="1">
+                                          <label class="custom-control-label" for="ccheckbox-payment_gw_<?php print $payment_module['module'] ?>"></label>
                                       </div>
-                                  </template>
+
+                                  </div>
+
+                                  <div class="col pl-0">
+                                      <img src="<?php print $payment_module['icon']; ?>" alt="" class="d-none"/>
+
+                                      <h4 class="gateway-title font-weight-bold mb-0"><?php _e($payment_module['name']) ?></h4>
+
+                                      <small class="text-muted">
+                                          <?php _e($payment_module['name']) ?> <span class="text-primary js-method-on <?php if (get_option('payment_gw_' . $payment_module['module'], 'payments') != 1): ?>d-none<?php endif; ?>"><?php _e("is ON"); ?></span>
+                                      </small>
+                                  </div>
+
+                                  <div class="col text-end text-right text-right">
+                                      <button type="button" onclick="paymentModal('#module-db-id-<?php print $module_info['id'] ?>');" class="btn btn-outline-primary btn-sm"><?php _e('Settings'); ?></button>
+                                  </div>
                               </div>
+
+                              <template class="js-modal-content" style="display: none;">
+                                  <h5 class="mb-0"><?php _e('Enter your API settings'); ?></h5>
+                                  <small class="text-muted mb-3 d-block"><?php _e("Ask your payment provider for this information and put it below"); ?></small>
+
+                                  <div class="mw-set-payment-gw-options">
+                                      <module type="<?php print $payment_module['module'] ?>" view="admin"/>
+                                  </div>
+                              </template>
                           </div>
                           <script>
                               $(document).ready(function () {
@@ -190,6 +192,7 @@ $payment_modules = get_modules('type=payment_gateway');
                       <?php endforeach; ?>
                   </div>
               <?php endif; ?>
+              </div>
           </div>
       </div>
     </div>
