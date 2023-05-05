540274f2ea5dae1a26538910feb4547e579ac18f
Nikolay <rachkuw@gmail.com>
2023-05-05 14:15:47 +0300

    update

--- src/MicroweberPackages/Marketplace/resources/views/admin/marketplace/livewire/index.blade.php
+++ src/MicroweberPackages/Marketplace/resources/views/admin/marketplace/livewire/index.blade.php
@@ -1,116 +1,122 @@
 <div>
     <script>mw.require('admin_package_manager.js');</script>
 
-    <div class="container px-5">
 
-        <div class="mb-3">
-            <div class="d-flex justify-content-between">
-                <h1 class="mb-0">
-                    <i class="mdi mdi-fruit-cherries fs-2 text-primary mr-3"></i> <strong>Marketplace</strong>
-                </h1>
-                <div class="my-2 my-md-0 flex-grow-1 flex-md-grow-0">
-                    <div class="input-icon">
-                              <span class="input-icon-addon">
-                                 <svg fill="currentColor"xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><path d="M10 10m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0"></path><path d="M21 21l-6 -6"></path></svg></span>
-                        <input type="text" class="form-control" placeholder="Search..." wire:keydown.enter="filter" wire:model.lazy="keyword" />
-                        <div wire:loading wire:target="keyword" class="spinner-border spinner-border-sm" role="status">
-                            <span class="visually-hidden">Searching...</span>
-                        </div>
-                    </div>
-                </div>
-                <div>
-                    <button type="button" class="btn btn-outline-primary" wire:click="reloadPackages">
-                        <div wire:loading wire:target="reloadPackages" class="spinner-border spinner-border-sm" role="status">
-                            <span class="visually-hidden">Loading...</span>
-                        </div>
-                        Reload Packages
-                    </button>
-                    <button type="button" class="btn btn-outline-success" onclick="mw.admin.admin_package_manager.show_licenses_modal();">Licenses</button>
-                </div>
-            </div>
 
-            <div class="p-3">
-              <div class="row g-4">
 
-                <div class="col-12">
-                    <div>
-                        <p>Welcome to the marketplace Here you will find new modules, templates and updates</p>
-                    </div>
-                    <div class="btn-group d-flex">
-                        <button type="button" class="btn @if($category == 'all') btn-primary @else btn-outline-primary @endif" wire:click="filterCategory('all')">
-                            <div wire:loading wire:target="filterCategory('all')" class="spinner-border spinner-border-sm" role="status">
-                                <span class="visually-hidden">Loading...</span>
-                            </div>  All
-                        </button>
-                        <button type="button" class="btn @if($category == 'microweber-template') btn-primary @else btn-outline-primary @endif" wire:click="filterCategory('microweber-template')">
-                            <div wire:loading wire:target="filterCategory('microweber-template')" class="spinner-border spinner-border-sm" role="status">
-                                <span class="visually-hidden">Loading...</span>
-                            </div>
-                            <i class="mdi mr-1 mdi-pencil-ruler"></i> Templates
-                        </button>
-                        <button type="button" class="btn @if($category == 'microweber-module') btn-primary @else btn-outline-primary @endif" wire:click="filterCategory('microweber-module')">
-                            <div wire:loading wire:target="filterCategory('microweber-module')" class="spinner-border spinner-border-sm" role="status">
-                                <span class="visually-hidden">Loading...</span>
-                            </div>
-                            <i class="mdi mr-1 mdi-view-grid-plus"></i> Modules
-                        </button>
-                    </div>
-                </div>
 
-                <div class="col-12">
-                    <div class="row row-cards">
+    <div class="card px-5 col-xxl-10 mx-auto">
 
-                        @if(!empty($marketplacePagination))
-                        @foreach($marketplacePagination as $marketItem)
-                        <div class="col-sm-6 col-lg-4">
-                            <div class="card my-1 mx-1 card-sm card-link card-stacked">
 
-                                @if(isset($marketItem['extra']['_meta']['screenshot']))
-                                    <button type="button" class="border-0 d-block" onclick="Livewire.emit('openModal', 'admin-marketplace-item-modal', {{ json_encode(['name'=>$marketItem['name']]) }})">
-                                        @if($marketItem['type'] == 'microweber-module')
-                                        <div style="background-image:url({{$marketItem['extra']['_meta']['screenshot']}});width: 100%;height: 180px;background-repeat:no-repeat;background-size: contain;background-position: center;" class="card-img-top">
-                                        </div>
-                                        @else
-                                        <div style="background-image:url({{$marketItem['extra']['_meta']['screenshot']}});width: 100%;height: 180px;background-size: cover;background-position: top;" class="card-img-top">
-                                        </div>
-                                        @endif
-                                    </button>
-                                @else
-                                    <button type="button" class="border-0 d-block" onclick="Livewire.emit('openModal', 'admin-marketplace-item-modal', {{ json_encode(['name'=>$marketItem['name']]) }})">
-                                        <div class="card-img-top text-center">
-                                            <i class="mdi mdi-view-grid-plus text-muted"
-                                               style="opacity:0.5;font-size:126px;margin-left: 15px;"></i>
-                                        </div>
-                                    </button>
-                                @endif
+        <div class="card-body mb-3">
+           <div class="row">
+               <div class="card-header d-flex align-items-center justify-content-between px-2">
+                   <div class="col-lg-5 px-1">
+                       <h1><?php _e("Marketplace"); ?></h1>
+                       <small class="text-muted"><?php _e("Welcome to the marketplace Here you will find new modules, templates and updates"); ?></small>
+                   </div>
 
-                                <div class="card-body">
-                                    <div class="d-flex align-items-center">
-                                        <div>
-                                            <b>
-                                                {{$marketItem['description']}}
-                                            </b>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        @endforeach
-                            <div class="d-flex justify-content-center mt-4">
-                                {!! $marketplacePagination->links('livewire-tables::specific.bootstrap-4.pagination') !!}
-                            </div>
-                        @else
-                            <div class="col-12">
-                            no items
-                            </div>
-                        @endif
 
-                    </div>
-                </div>
-            </div>
-            </div>
+                   <div class="col-lg-4 my-2 my-md-0 flex-grow-1  flex-md-grow-0 px-1">
+
+
+                       <div class="input-icon">
+                          <span class="input-icon-addon">
+                            <svg fill="currentColor" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 96 960 960" width="24"><path d="M796 935 533 672q-30 26-69.959 40.5T378 727q-108.162 0-183.081-75Q120 577 120 471t75-181q75-75 181.5-75t181 75Q632 365 632 471.15 632 514 618 554q-14 40-42 75l264 262-44 44ZM377 667q81.25 0 138.125-57.5T572 471q0-81-56.875-138.5T377 275q-82.083 0-139.542 57.5Q180 390 180 471t57.458 138.5Q294.917 667 377 667Z"/></svg>                          </span>
+                           <input type="text" class="form-control" placeholder="Search..." wire:keydown.enter="filter" wire:model.lazy="keyword" />
+                           <div wire:loading wire:target="keyword" class="spinner-border spinner-border-sm" role="status">
+                               <span class="visually-hidden"><?php _e("Searching"); ?>...</span>
+                           </div>
+                       </div>
+                   </div>
+                   <div class="col-lg-3 px-1 text-end">
+                       <button type="button" class="btn btn-outline-primary" wire:click="reloadPackages">
+                           <div wire:loading wire:target="reloadPackages" class="spinner-border spinner-border-sm" role="status">
+                               <span class="visually-hidden"><?php _e("Loading"); ?>...</span>
+                           </div>
+                           <?php _e("Reload packages"); ?>
+                       </button>
+                       <button type="button" class="btn btn-outline-success" onclick="mw.admin.admin_package_manager.show_licenses_modal();"><?php _e("Licenses"); ?></button>
+                   </div>
+
+               </div>
+
+
+
+
+               <div class="col-12">
+                   <div class="btn-group d-flex">
+                       <button type="button" class="btn @if($category == 'all') btn-primary @else btn-outline-primary @endif" wire:click="filterCategory('all')">
+                           <div wire:loading wire:target="filterCategory('all')" class="spinner-border spinner-border-sm" role="status">
+                               <span class="visually-hidden"><?php _e("Loading"); ?>...</span>
+                           </div> <?php _e("All"); ?>
+                       </button>
+                       <button type="button" class="btn @if($category == 'microweber-template') btn-primary @else btn-outline-primary @endif" wire:click="filterCategory('microweber-template')">
+                           <div wire:loading wire:target="filterCategory('microweber-template')" class="spinner-border spinner-border-sm" role="status">
+                               <span class="visually-hidden"><?php _e("Loading"); ?>...</span>
+                           </div>
+                           <?php _e("Templates"); ?>
+                       </button>
+                       <button type="button" class="btn @if($category == 'microweber-module') btn-primary @else btn-outline-primary @endif" wire:click="filterCategory('microweber-module')">
+                           <div wire:loading wire:target="filterCategory('microweber-module')" class="spinner-border spinner-border-sm" role="status">
+                               <span class="visually-hidden"><?php _e("Loading"); ?>...</span>
+                           </div>
+                           <?php _e("Modules"); ?>
+                       </button>
+                   </div>
+               </div>
+
+               <div class="col-12">
+                   <div class="row row-cards px-0">
+
+                       @if(!empty($marketplacePagination))
+                           @foreach($marketplacePagination as $marketItem)
+                               <div class="col-sm-6 col-lg-4">
+                                   <div class="card my-1 mx-1 card-sm card-link card-stacked">
+
+                                       @if(isset($marketItem['extra']['_meta']['screenshot']))
+                                           <button type="button" class="border-0 d-block" onclick="Livewire.emit('openModal', 'admin-marketplace-item-modal', {{ json_encode(['name'=>$marketItem['name']]) }})">
+                                               @if($marketItem['type'] == 'microweber-module')
+                                                   <div style="background-image:url({{$marketItem['extra']['_meta']['screenshot']}});width: 100%;height: 180px;background-repeat:no-repeat;background-size: contain;background-position: center;" class="card-img-top">
+                                                   </div>
+                                               @else
+                                                   <div style="background-image:url({{$marketItem['extra']['_meta']['screenshot']}});width: 100%;height: 180px;background-size: cover;background-position: top;" class="card-img-top">
+                                                   </div>
+                                               @endif
+                                           </button>
+                                       @else
+                                           <button type="button" class="border-0 d-block" onclick="Livewire.emit('openModal', 'admin-marketplace-item-modal', {{ json_encode(['name'=>$marketItem['name']]) }})">
+                                               <div class="card-img-top text-center">
+                                                   <i class="mdi mdi-view-grid-plus text-muted"
+                                                      style="opacity:0.5;font-size:126px;margin-left: 15px;"></i>
+                                               </div>
+                                           </button>
+                                       @endif
+
+                                       <div class="card-body">
+                                           <div class="d-flex align-items-center">
+                                               <div>
+                                                   <b>
+                                                       {{$marketItem['description']}}
+                                                   </b>
+                                               </div>
+                                           </div>
+                                       </div>
+                                   </div>
+                               </div>
+                           @endforeach
+                           <div class="d-flex justify-content-center mt-4">
+                               {!! $marketplacePagination->links('livewire-tables::specific.bootstrap-4.pagination') !!}
+                           </div>
+                       @else
+                           <div class="col-12">
+                                   <?php _e("no items"); ?>
+                           </div>
+                       @endif
+
+                   </div>
+               </div>
+           </div>
         </div>
-
-
     </div>
 </div>
