6e3fefb65997f922958a3ab6ed4ab78f753a1a5b
Bozhidar Slaveykov <bobi@microweber.com>
2023-05-10 11:15:18 +0300

    update

--- userfiles/modules/shop/offers/edit_offer.php
+++ userfiles/modules/shop/offers/edit_offer.php
@@ -6,7 +6,6 @@
 $date_format = get_date_format();
 //$products = offers_get_products();
 
-$all_products = get_products('nolimit=1');
 
 if (isset($params['offer_id']) && $params['offer_id'] !== 'false') {
     $addNew = false;
@@ -103,34 +102,37 @@ if (isset($params['offer_id']) && $params['offer_id'] !== 'false') {
     <div class="form-group">
         <label class="form-label"><?php _e("Product title | Price"); ?></label>
 
-        <select name="product_id_with_price_id" class="js-product-title  form-select" data-size="5" data-live-search="true" data-width="100%">
-            <?php if ($all_products): ?>
-                <?php foreach ($all_products as $product) {
 
-                    $all_prices = get_product_prices($product['id'], true);
-                    $product_id = $product['id'];
-
-                    if ($all_prices) {
-                        foreach ($all_prices as $a_price) {
-                            if (!isset($a_price['values_plain'])) {
-                                continue;
-                            }
-
-                            $offer_product_price_id = $data['product_id'] . '|' . $data['price_id'];
-                            $option_id = $product_id . '|' . $a_price['id'];
-                            $selected = ($offer_product_price_id == $option_id ? ' selected="selected"' : '');
-                            ?>
-
-                            <option value="<?php print $option_id; ?>"<?php print $selected; ?>><?php print $product['title'] . ' | ' . $a_price['name'] . ' : ' . mw()->shop_manager->currency_symbol() . ' ' . $a_price['values_plain']; ?></option>
-                            <?php
+        <script>
+            $(document).ready(function () {
+                let offerProduct = new mw.autoComplete({
+                    element: "#mw-admin-search-for-products",
+                    placeholder: "Search products",
+                    ajaxConfig: {
+                        method: 'get',
+                        url: mw.settings.api_url + 'offers_search_products?keyword=${val}&product_id=<?php print $data['product_id']; ?>&price_id=<?php print $data['price_id']; ?>',
+                    },
+                    map: {
+                        value: 'id',
+                        title: 'title',
+                        image: 'picture'
+                    },
+                    selected: [
+                        {
+                            id: '<?php print $data['product_id']; ?>|<?php print $data['price_id']; ?>',
+                            title: '<?php print content_title($data['product_id']); ?>',
                         }
-                    }
-                }
-                ?>
-            <?php else: ?>
-                <option value=""><?php _e("No products found"); ?></option>
-            <?php endif; ?>
-        </select>
+                    ]
+                });
+                $(offerProduct).on("change", function (e, val) {
+                    $('#product_id_with_price_id').val(val[0].id);
+                    $('#product_id_with_price_id').trigger('change');
+                })
+            });
+        </script>
+
+        <div id="mw-admin-search-for-products"></div>
+        <input type="hidden" value="<?php print $data['product_id']; ?>|<?php print $data['price_id']; ?>" id="product_id_with_price_id" name="product_id_with_price_id" />
     </div>
 
     <div class="form-group">
