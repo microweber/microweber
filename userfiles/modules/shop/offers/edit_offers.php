ce90389d8a33dae8f5d75bb10f13b1af89bac372
Bozhidar Slaveykov <bobi@microweber.com>
2023-05-11 10:44:32 +0300

    Update edit_offers.php

--- userfiles/modules/shop/offers/edit_offers.php
+++ userfiles/modules/shop/offers/edit_offers.php
@@ -21,8 +21,6 @@ $allOffers = app()->offer_repository->getAll();
         if ($allOffers):
             foreach ($allOffers as $offer):
                 $class_product_deleted = ($offer['is_deleted'] == 1 ? 'class="mw-error"' : '');
-                $class_status = ($offer['is_active'] == 1 ? 'status-active' : 'status-inactive');
-                $class_status_badge = ($offer['is_active'] == 1 ? 'badge bg-success' : 'badge bg-warning');
                 ?>
                 <tr class=" td-valign <?php if ($offer['is_active'] == 1): ?>js-table-active<?php else: ?>js-table-inactive<?php endif; ?> <?php print $class_product_deleted; ?> ">
                     <td><?php print($offer['id']) ?></td>
@@ -39,10 +37,33 @@ $allOffers = app()->offer_repository->getAll();
                     <td><?php print currency_format($offer['price']) ?></td>
                     <td><?php print currency_format($offer['offer_price']) ?></td>
                     <td><?php if ($offer['expires_at'] and $offer['expires_at'] != '0000-00-00 00:00:00' ): ?><?php print date_system_format($offer['expires_at']) ?><?php else: ?>-<?php endif; ?></td>
-                    <td class="<?php print $class_status; ?>">
-                    <span class="<?php print $class_status_badge; ?>"></span>
 
-                    <?php print($offer['is_active'] == 1 ? 'Active' : 'Inactive') ?></td>
+
+                    <?php
+                    $expired = false;
+                    $class_status = ($offer['is_active'] == 1 ? 'status-active' : 'status-inactive');
+                    $class_status_badge = ($offer['is_active'] == 1 ? 'badge bg-success' : 'badge bg-warning');
+                    if (now() > $offer['expires_at']) {
+                        $class_status = 'status-inactive';
+                        $class_status_badge = 'badge bg-warning';
+                        $expired = true;
+                    }
+                    ?>
+
+                    <td class="<?php print $class_status; ?>">
+
+                        <span class="<?php print $class_status_badge; ?>"></span>
+
+                        <?php if ($expired): ?>
+                            <?php _e('Expired'); ?>
+                        <?php else: ?>
+                        
+                           <?php print($offer['is_active'] == 1 ? 'Active' : 'Inactive') ?>
+                        <?php endif; ?>
+
+                    </td>
+
+
                     <td class="action-buttons" style="width: 250px;">
                         <button onclick="editOffer(<?php print($offer['id']) ?>)" class="btn btn-outline-primary btn-sm" title="Edit"><?php _e('Edit'); ?></button>
                         <button onclick="deleteOffer(<?php print($offer['id']) ?>)" class="btn btn-outline-danger btn-sm" title="Delete"><?php _e('Delete'); ?></button>
