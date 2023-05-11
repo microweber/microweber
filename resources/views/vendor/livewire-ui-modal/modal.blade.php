ecd52ca648de4c7349cc9dfcac2b8b2a0c181975
Bozhidar Slaveykov <bobi@microweber.com>
2023-05-11 12:38:01 +0300

    Update modal.blade.php

--- resources/views/vendor/livewire-ui-modal/modal.blade.php
+++ resources/views/vendor/livewire-ui-modal/modal.blade.php
@@ -14,7 +14,7 @@
         x-trap=false
         class="modal modal-blur fade" tabindex="-1" aria-hidden="true">
         <div class="modal-dialog modal-lg modal-dialog-centered modal-dialog-scrollable">
-            <div class="modal-content">
+            <div class="modal-content bg-light">
 
                 @forelse($components as $id => $component)
                     <div x-show.immediate="activeComponent == '{{ $id }}'" x-ref="{{ $id }}" wire:key="{{ $id }}">
