a94cc9159874628356a1664281f2cd624dd9fc29
Bozhidar Slaveykov <bobi@microweber.com>
2023-05-12 13:48:10 +0300

    update

--- src/MicroweberPackages/Admin/resources/views/layouts/partials/navabar-bottom-menu-lang-switch.blade.php
+++ src/MicroweberPackages/Admin/resources/views/layouts/partials/navabar-bottom-menu-lang-switch.blade.php
@@ -17,7 +17,7 @@ if ($supportedLanguages !== null) {
             mw.cookie.set("lang", $(this).val());
             $.ajax({
                 type: "GET",
-                url: mw.settings.api_url + "set_current_lang",
+                url: mw.settings.api_url + "set_admin_lang",
                 data: {lang:$(this).val()}
             }).always(function (){
                 window.location.reload();
