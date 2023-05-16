2ef66632ff239f78f7052455d36bfc1fbdca5e10
Nikolay <rachkuw@gmail.com>
2023-05-16 17:35:51 +0300

    update

--- userfiles/modules/site_stats/src/views/parts/contents.php
+++ userfiles/modules/site_stats/src/views/parts/contents.php
@@ -15,8 +15,8 @@ if (!$data) {
     }
     ?>
     <div class="item content">
-        <div class="mw-ui-row-nodrop info-holder">
-            <div class="mw-ui-col">
+        <div class="d-flex flex-wrap align-items-center justify-content-between info-holder my-3">
+            <div class="col-xxl-10 col-lg-9">
                 <div class="title">
                     <?php
                     if (isset($item['content_title'])) {
@@ -29,11 +29,13 @@ if (!$data) {
                 <div class="slug"><a href="<?php print $url; ?>" target="_blank" rel="noreferrer noopener"><?php print $item['url_slug']; ?></a></div>
             </div>
 
-            <div class=" mw-ui-col w-25";">
+            <div class="col-xxl-2 col-lg-2 text-center">
                 <div class="cnt"><?php print $item['sessions_count']; ?></div>
             </div>
         </div>
 
-        <div class="content-progressbar" style="width: <?php print $item['sessions_percent']; ?>%;"></div>
+        <div class="progress progress-xs my-2" style="width: 80%;">
+            <div class="progress-bar bg-primary" style="width: <?php print $item['sessions_percent']; ?>%;"></div>
+        </div>
     </div>
 <?php endforeach; ?>
