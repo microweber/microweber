80246c3c3c49fc161478d424a8c97261e6b70ebb
Nikolay <rachkuw@gmail.com>
2023-05-17 15:00:02 +0300

    Update referrers.php

--- userfiles/modules/site_stats/src/views/parts/referrers.php
+++ userfiles/modules/site_stats/src/views/parts/referrers.php
@@ -29,8 +29,8 @@ if (!is_array($data) or empty($data)) {
     });
 </script>
 
-<ul class="">
-    <?php foreach ($data as $item): ?>
+
+<?php foreach ($data as $item): ?>
     <?php
     if (isset($item['referrer_domain_id']) and isset($item['referrer_domain']) and $item['referrer_domain_id'] != null) {
         $referrer = $item['referrer_domain'];
@@ -40,57 +40,58 @@ if (!is_array($data) or empty($data)) {
         $referrerIcon = '<i class="fa fa-globe"></i>';
     }
     ?>
-    <li class="nav-item referrers-more-info-show" data-id="referrers-more-<?php print $item['id']; ?>">
-        <a href=" https://templates.microweber.com/mw/admin/customers " class="nav-link fs-3 dropdown-toggle show" data-bs-toggle="dropdown" data-bs-auto-close="false" role="button" aria-expanded="true">
-           Open
-        </a>
-        <div class="referrers-more-info" id="referrers-more-<?php print $item['id']; ?>">
-            <a href=" https://templates.microweber.com/mw/admin/shop/product " class="dropdown-item justify-content-between ">
-                <div class="progress progress-xs my-2" style="width: 80%;">
-                    <div class="progress-bar bg-primary" style="width: <?php print $item['sessions_percent']; ?>%;"></div>
-                </div>
 
-                <span class="arrow"></span>
-                <div class="icon-wrapper"><?php print $referrerIcon; ?></div>
-                <div class="source-name"><span><?php print $referrer; ?></span></div>
-                <div class="cnt"><?php print $item['sessions_count']; ?></div>
-
-            </a>
-
-            <ul class="subsources">
-                <?php if (isset($item['referrer_domain_id'])  and isset($item['referrer_paths'])  and $item['referrer_domain_id'] != null AND $item['referrer_paths']): ?>
-                    <?php foreach ($item['referrer_paths'] as $path): ?>
-
-                        <?php
-                        if(!isset($path['referrer_url'])){
-                            $path['referrer_url'] = '';
-                        }
-                        $ref_url_display =  $path['referrer_url'];
-                        if(isset($path['referrer_path']) and $path['referrer_path']){
-                            $ref_url_display =  $path['referrer_path'];
-                        }
-
-
-                        ?>
-
-
-
-                        <li>
-                            <span class="subsource-url"><a href="<?php print $path['referrer_url']; ?>" target="_blank"><?php print $ref_url_display; ?></a></span>
-                            <span class="subsource-cnt"><?php print $path['path_sessions_count']; ?></span>
-                            <div class="clearfix"></div>
-                        </li>
-                    <?php endforeach; ?>
-                <?php else: ?>
-                    <li>
-                        <span class="subsource-url"><?php _e('No paths'); ?></span>
-                        <span class="subsource-cnt"></span>
-                        <div class="clearfix"></div>
-                    </li>
-                <?php endif; ?>
-            </ul>
+<div>
+    <div class="nav-link fs-3 my-3" data-bs-toggle="collapse" data-bs-target="#refferers-show-more<?php print $item['id'] ?>">
+        <div class="d-flex align-items-center">
+            <div class="icon-wrapper me-1"><?php print $referrerIcon; ?></div>
+            <small class="text-muted ms-1"><a href="<?php print $referrer; ?>"><?php print $referrer; ?></a></small>
         </div>
-    </li>
-    <?php endforeach; ?>
-</ul>
+    </div>
+    <div class="collapse" id="refferers-show-more<?php print $item['id'] ?>">
+
+        <div href="https://templates.microweber.com/mw/admin/shop/product ">
+            <div class="progress progress-xs mb-2" style="width: 80%;">
+                <div class="progress-bar bg-primary" style="width: <?php print $item['sessions_percent']; ?>%;"></div>
+            </div>
+
+            <span class="arrow"></span>
+            <div class="cnt"><?php print $item['sessions_count']; ?></div>
+
+        </div>
+
+        <div class="subsources">
+            <?php if (isset($item['referrer_domain_id'])  and isset($item['referrer_paths'])  and $item['referrer_domain_id'] != null AND $item['referrer_paths']): ?>
+                <?php foreach ($item['referrer_paths'] as $path): ?>
+
+                    <?php
+                    if(!isset($path['referrer_url'])){
+                        $path['referrer_url'] = '';
+                    }
+                    $ref_url_display =  $path['referrer_url'];
+                    if(isset($path['referrer_path']) and $path['referrer_path']){
+                        $ref_url_display =  $path['referrer_path'];
+                    }
+                    ?>
+
+                    <div>
+                        <span class="subsource-url my-2"><a href="<?php print $path['referrer_url']; ?>" target="_blank"><?php print $ref_url_display; ?></a></span>
+                        <span class="subsource-cnt my-2"><?php print $path['path_sessions_count']; ?></span>
+                        <div class="clearfix"></div>
+                    </div>
+                <?php endforeach; ?>
+            <?php else: ?>
+                <div>
+                    <span class="subsource-url my-2"><?php _e('No paths'); ?></span>
+                    <span class="subsource-cnt my-2"></span>
+                    <div class="clearfix"></div>
+                </div>
+            <?php endif; ?>
+        </div>
+    </div>
+</div>
+
+<?php endforeach; ?>
+
+
 
