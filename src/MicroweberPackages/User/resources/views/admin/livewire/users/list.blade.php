e58a41cbd358e094a48be3a0a22be7f09a0d6fa9
Bozhidar Slaveykov <bobi@microweber.com>
2023-05-17 15:04:57 +0300

    Update list.blade.php

--- src/MicroweberPackages/User/resources/views/admin/livewire/users/list.blade.php
+++ src/MicroweberPackages/User/resources/views/admin/livewire/users/list.blade.php
@@ -102,21 +102,26 @@
                                     <td>
                                         <div class="d-flex py-1 align-items-center">
 
+                                            @if(empty($user->thumbnail))
+                                            {{--   Ako nqma avatar --}}
                                             <span class="avatar me-2 border-0 rounded-circle bg-light">
                                                 <img width="24px" height="24px" src="{{$user->avatar}}" alt="">
                                             </span>
-
+                                            @else
+                                            {{--   Ako ima--}}
+                                            <span class="avatar me-2 shadow-none">
+                                                <img class="rounded-circle" src="{{$user->avatar}}" alt="">
+                                            </span>
+                                            @endif
 
                                             <div class="flex-fill">
                                                 <div class="font-weight-medium">{{$user->first_name}} {{$user->last_name}}
 
                                                     @if($user->id == user_id())
-                                                        <span>(You)</span>
+                                                        <span class="badge badge-outline text-azure">{{_e('You')}}</span>
                                                     @endif
-
                                                 </div>
                                                 <div class="text-muted"><a href="{{route('admin.users.edit', $user->id)}}" class="text-reset">{{$user->email}}</a></div>
-
                                             </div>
                                         </div>
                                     </td>
