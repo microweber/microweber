752813512b10167978f5f7567ceb919ce9112e50
Bozhidar Slaveykov <bobi@microweber.com>
2023-05-17 14:35:17 +0300

    Update UserFilter.php

--- src/MicroweberPackages/User/Models/ModelFilters/UserFilter.php
+++ src/MicroweberPackages/User/Models/ModelFilters/UserFilter.php
@@ -12,12 +12,39 @@ use EloquentFilter\ModelFilter;
 use MicroweberPackages\Content\Models\ModelFilters\Traits\FilterByKeywordTrait;
 use MicroweberPackages\Content\Models\ModelFilters\Traits\FilterByUrlTrait;
 use MicroweberPackages\Content\Models\ModelFilters\Traits\OrderByTrait;
+use MicroweberPackages\Helper\XSSClean;
+use MicroweberPackages\Multilanguage\Models\MultilanguageTranslations;
+use MicroweberPackages\Multilanguage\MultilanguageHelpers;
 
 class UserFilter extends ModelFilter
 {
     use OrderByTrait;
     use FilterByUrlTrait;
-    use FilterByKeywordTrait;
+
+    public function keyword($keyword)
+    {
+        $model = $this->getModel();
+        $table = $model->getTable();
+
+        $keyword = trim($keyword);
+
+        $this->query->where('id', intval($keyword));
+        $this->query->orWhere($table.'.username', 'LIKE', '%' . $keyword . '%');
+        $this->query->orWhere($table.'.email', 'LIKE', '%' . $keyword . '%');
+
+        $this->query->orWhere($table.'.first_name', 'LIKE', '%' . $keyword . '%');
+        $this->query->orWhere($table.'.last_name', 'LIKE', '%' . $keyword . '%');
+
+        $keywordExp = explode(' ', $keyword);
+        if (isset($keywordExp[0]) && isset($keywordExp[1])) {
+            $this->query->orWhere($table.'.first_name', 'LIKE', '%' . $keywordExp[0] . '%');
+            $this->query->orWhere($table.'.last_name', 'LIKE', '%' . $keywordExp[1] . '%');
+        }
+
+        $this->query->orWhere($table.'.phone', 'LIKE', '%' . $keyword . '%');
+
+        return $this->query;
+    }
 
     public function isAdmin($isAdmin)
     {
