fa481a1d93b333b78a7993fbe69a0a350cd273a6
Nikolay <rachkuw@gmail.com>
2023-05-17 18:04:24 +0300

    update

--- userfiles/modules/admin/notifications/notif_form_entry.php
+++ userfiles/modules/admin/notifications/notif_form_entry.php
@@ -57,7 +57,7 @@ if (isset($item['created_by'])) {
 
 
 
-<div class="js-form-entry-<?php print $item_id ?> card shadow-sm mb-4 not-collapsed-border collapsed card-message-holder <?php if (!isset($is_entry)): ?>card-bubble<?php endif; ?> <?php if (isset($item['is_read']) AND $item['is_read'] == 0): ?>active<?php endif; ?> bg-silver" data-bs-toggle="collapse" data-bs-target="#notif-entry-item-<?php print $item_id ?>" aria-expanded="false" aria-controls="collapseExample">
+<div class="js-form-entry-<?php print $item_id ?> card shadow-sm hover-bg-light mb-4 not-collapsed-border collapsed card-message-holder <?php if (!isset($is_entry)): ?>card-bubble<?php endif; ?> <?php if (isset($item['is_read']) AND $item['is_read'] == 0): ?>active<?php endif; ?> bg-silver" data-bs-toggle="collapse" data-bs-target="#notif-entry-item-<?php print $item_id ?>" aria-expanded="false" aria-controls="collapseExample">
     <div class="card-body">
         <?php if (isset($params['module']) and $params['module'] == 'admin/notifications'): ?>
             <div class="row align-items-center mb-3">
@@ -75,20 +75,20 @@ if (isset($item['created_by'])) {
 
             <div class="col-6 col-sm">
                 <?php print date('M d, Y', strtotime($item['created_at'])); ?>
-                <small class="text-muted"><?php print date('h:s', strtotime($item['created_at'])); ?>h</small>
+                <small class="text-muted mb-2 font-weight-bold d-block"><?php print date('h:s', strtotime($item['created_at'])); ?>h</small>
             </div>
 
             <div class="col-6 col-sm"><?php print mw('format')->ago($item['created_at']); ?></div>
 
             <div class="col-2 text-end">
-                <a href="#" class="btn" data-bs-toggle="modal" data-bs-target="#notif-entry-item-<?php print $item_id ?>">
+                <a href="#" class="btn btn-link" data-bs-toggle="modal" data-bs-target="#notif-entry-item-<?php print $item_id ?>">
                     <?php _e("View") ?>
                 </a>
             </div>
         </div>
 
         <div class="modal modal-blur fade" id="notif-entry-item-<?php print $item_id ?>" tabindex="-1" style="display: none;" aria-modal="true" role="dialog">
-            <div class="modal-dialog modal-xl modal-dialog-centered" role="document">
+            <div class="modal-dialog modal-lg modal-dialog-centered" role="document">
                 <div class="modal-content">
                     <div class="modal-header">
                         <h5 class="modal-title"><?php _e("Your message") ?></h5>
@@ -101,8 +101,10 @@ if (isset($item['created_by'])) {
                             $iformVr=0;
                             foreach ($form_values_formated as $form_values_row) {
                                 ?>
+                                <?php  if ($iformVr==0){   ?>
+                                    <label class="fs-2 mb-4"><?php _e("Fields"); ?></label>
+                                <?php  } ?>
                                 <div class="col-md-6">
-                                    <?php  if ($iformVr==0){   ?><h6><strong><?php _e("Fields"); ?></strong></h6><?php  } ?>
 
                                     <?php $iformVr++;?>
 
@@ -110,11 +112,11 @@ if (isset($item['created_by'])) {
                                         <?php foreach ($form_values_row as $key => $val1){ ?>
                                             <?php if (!is_array($val1)){ ?>
                                                 <div>
-                                                    <small class="text-muted"><?php echo str_replace('_', ' ', $key); ?>:</small>
+                                                    <small class="text-muted mb-2 font-weight-bold d-block"><?php echo str_replace('_', ' ', $key); ?>:</small>
                                                     <p><?php print htmlentities($val1); ?></p>
                                                 </div>
                                             <?php } else { ?>
-                                                <small class="text-muted"><?php echo str_replace('_', ' ', $key); ?>:</small>
+                                                <small class="text-muted mb-2 font-weight-bold d-block"><?php echo str_replace('_', ' ', $key); ?>:</small>
                                                 <?php foreach ($val1 as $val1_1){ ?>
                                                     <p><?php print htmlentities($val1_1) . '<br />'; ?></p>
                                                 <?php }?>
@@ -135,7 +137,7 @@ if (isset($item['created_by'])) {
 
                                             ?>
                                             <div>
-                                                <small class="text-muted"><?php echo $fileNameKey; ?></small> <br />
+                                                <small class="text-muted mb-2 font-weight-bold d-block"><?php echo $fileNameKey; ?></small> <br />
                                                 <a href="<?php echo $fileSettings['url']; ?>" target="_blank">
                                                     <i class="mdi mdi-file-check text-primary mdi-18px"></i> <?php echo str_limit(basename($fileSettings['url']),20); ?>
                                                     (<?php echo app()->format->human_filesize($fileSettings['file_size']) ?>)
@@ -164,7 +166,7 @@ if (isset($item['created_by'])) {
                                         });
                                     }
                                 </script>
-                                <button type="button" class="btn btn-link pull-right" onclick="deleteFormEntry(event,<?php echo $item_id; ?>)"><i class="mdi mdi-delete-outline"></i> <?php _e('Delete'); ?></button>
+                                <button type="button" class="btn btn-link text-danger pull-right" onclick="deleteFormEntry(event,<?php echo $item_id; ?>)"><i class="mdi mdi-delete-outline"></i> <?php _e('Delete'); ?></button>
                             </div>
                         </div>
                     </div>
