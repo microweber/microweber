56972a0b5c0c01aee1d98dde7e9049e10e359246
Nikolay <rachkuw@gmail.com>
2023-05-19 14:52:22 +0300

    Update index.blade.php

--- userfiles/modules/contact_form/src/resources/views/livewire/admin/contact-form/index.blade.php
+++ userfiles/modules/contact_form/src/resources/views/livewire/admin/contact-form/index.blade.php
@@ -5,21 +5,20 @@
 <div class="mx-5">
 
 <div class="card pb-4 pt-4">
-    <div class="card-header">
-        <div class="d-flex align-items-center justify-content-between">
-            <div>
-                <module type="admin/modules/info_module_title" for-module="contact_form" />
-            </div>
-            <div>
-                <button type="button" onclick="Livewire.emit('openModal', 'contact-form.settings-modal')" class="btn btn-outline-primary mb-3">
-                    <i class="mdi mdi-cogs"></i> {{_e('Settings')}}
-                </button>
-                &nbsp; &nbsp;
-                <button  type="button" onclick="Livewire.emit('openModal', 'contact-form.integrations-modal')" class="btn btn-outline-primary mb-3">
-                    <i class="mdi mdi-cogs"></i> {{_e('Integrations')}}
-                </button>
-            </div>
+    <div class="card-header d-flex align-items-center justify-content-between">
+        <div>
+            <module type="admin/modules/info_module_title" for-module="contact_form" />
         </div>
+        <div>
+            <button type="button" onclick="Livewire.emit('openModal', 'contact-form.settings-modal')" class="btn btn-outline-primary mb-3">
+                <i class="mdi mdi-cogs"></i> {{_e('Settings')}}
+            </button>
+            &nbsp; &nbsp;
+            <button  type="button" onclick="Livewire.emit('openModal', 'contact-form.integrations-modal')" class="btn btn-outline-primary mb-3">
+                <i class="mdi mdi-cogs"></i> {{_e('Integrations')}}
+            </button>
+        </div>
+
     </div>
     <div class="card-body">
 
