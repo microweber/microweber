9aca2ca071ab267c4b99247382afd876479a332d
Nikolay <rachkuw@gmail.com>
2023-05-19 14:47:23 +0300

    update

--- src/MicroweberPackages/Content/resources/views/admin/content/livewire/components/limit.blade.php
+++ src/MicroweberPackages/Content/resources/views/admin/content/livewire/components/limit.blade.php
@@ -1,6 +1,6 @@
 <div class="d-flex align-items-center mx-1">
     <label class="d-xl-block d-none mx-2">{{ _e('Limit') }}</label>
-    <select class="form-select rounded-0" wire:model="paginate">
+    <select class="form-select form-select-lg rounded-0" wire:model="paginate">
         <option value="10">10</option>
         <option value="25">25</option>
         <option value="50">50</option>
