af0fbf828c610203eaf557b62ecd66416ccc919d
Bozhidar Slaveykov <bobi@microweber.com>
2023-05-23 13:14:04 +0300

    update

--- userfiles/modules/comments/src/resources/views/comment_reply.blade.php
+++ userfiles/modules/comments/src/resources/views/comment_reply.blade.php
@@ -1,31 +1,27 @@
-
-
-<form x-data="CommentForm()" @submit.prevent="submitCommentsForm">
+<form>
     <div class="row">
         <div class="col">
             <label>Name:</label>
-            <input type="text" class="form-control" required x-model="commentFormData.comment_name" />
+            <input type="text" class="form-control" required />
         </div>
         <div class="col">
             <label>Email:</label>
-            <input type="email" class="form-control" required x-model="commentFormData.comment_email" />
+            <input type="email" class="form-control" required />
         </div>
     </div>
     <div class="mt-2">
         <label>Comment:</label>
-        <textarea class="form-control" required x-model="commentFormData.comment_body"></textarea>
+        <textarea class="form-control" required></textarea>
     </div>
     <div class="mt-2">
-        <input type="hidden" x-model="commentFormData.rel_id" value="{{ content_id() }}" />
+        <input type="hidden" value="{{ content_id() }}" />
 
         @if(isset($reply_to_comment_id))
-        <input type="hidden" x-model="commentFormData.reply_to_comment_id" value="{{ $reply_to_comment_id }}" />
+        <input type="hidden" value="{{ $reply_to_comment_id }}" />
         @endif
 
-
-        <button type="submit" class="btn btn-outline-primary" :disabled="formLoading">
-            <span x-show="!formLoading">Post comment</span>
-            <span x-show="formLoading">Posting...</span>
+        <button type="submit" class="btn btn-outline-primary">
+            Post comment
         </button>
     </div>
 </form>
