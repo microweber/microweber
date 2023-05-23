947d9712b3b4dd0597da85bf7c815bd44ae1685c
Bozhidar Slaveykov <bobi@microweber.com>
2023-05-23 12:55:58 +0300

    Update comment_preview.blade.php

--- userfiles/modules/comments/src/resources/views/comment_preview.blade.php
+++ userfiles/modules/comments/src/resources/views/comment_preview.blade.php
@@ -1,4 +1,6 @@
-<div class="d-flex flex-start mb-4">
+<div class="d-flex flex-start mb-4" x-data="{
+    showReplyForm: false
+}">
     <div class="shadow-1-strong me-3">
         <i class="fa fa-user-circle-o" style="font-size:42px"></i>
     </div>
@@ -12,11 +14,16 @@
                 {{$comment->comment_body}}
 
                 <div class="d-flex justify-content-end align-items-center mt-4">
-                    <span style="cursor:pointer" class="link-muted"><i class="fa fa-reply me-1"></i> Reply to {{$comment->comment_name}}</span>
+                    <span @click="showReplyForm = ! showReplyForm" style="cursor:pointer" class="link-muted">
+                        <i class="fa fa-reply me-1"></i> Reply to {{$comment->comment_name}}
+                    </span>
                 </div>
             </div>
 
-            <div style="background:#fff;" class="mt-2 mb-4 p-4 d-none">
+            <div x-show="showReplyForm" style="display:none; background:#fff;" class="mt-2 mb-4 p-4">
+                <div class="mb-4">
+                    <b> <i class="fa fa-reply me-1"></i> Reply to {{$comment->comment_name}} </b>
+                </div>
                 @include('comments::comment_reply', [
                     'reply_to_comment_id' => $comment->id,
                 ])
