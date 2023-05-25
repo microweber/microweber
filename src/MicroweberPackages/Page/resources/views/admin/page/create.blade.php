533348dc4094811e3db40f162920b965d3fd1431
Nikolay <rachkuw@gmail.com>
2023-05-25 16:25:58 +0300

    update

--- src/MicroweberPackages/Page/resources/views/admin/page/create.blade.php
+++ src/MicroweberPackages/Page/resources/views/admin/page/create.blade.php
@@ -10,13 +10,13 @@
 
         <div class="row row-cards">
 
-            <div class="col-md-4 pe-4">
+            <div class="col-md-4 pe-5 pb-5">
                 <h3 class="mb-3 font-weight-bold">{{_e("Clean Page")}} </h3>
 
                 <div class="card">
 
                     <div class="card-body" style="padding: 13px; height: calc(50vh - 80px);">
-                        <div class="create-page-clean d-flex justify-content-center align-items-center h-100">
+                        <div class="create-page-clean d-flex justify-content-center align-items-center h-100 bg-azure-lt">
                             <h4 class="font-weight-bold mb-0">{{_e("Empty Page")}}</h4>
                         </div>
                     </div>
@@ -24,19 +24,19 @@
                 </div>
             </div>
         @foreach($layouts as $layout)
-{{--            <div class="col-md-4 pe-3">--}}
-{{--                <h3 class="mb-3 font-weight-bold">{{$layout['name']}} </h3>--}}
+            <div class="col-md-4 pe-5 pb-5">
+                <h3 class="mb-3 font-weight-bold">{{$layout['name']}} </h3>
 
-{{--                <div class="card">--}}
-{{--                        @php--}}
-{{--                            $iframe_start = site_url('new-content-preview-'. uniqid());--}}
-{{--                        @endphp--}}
-{{--                        @include('page::admin.page.iframe', [--}}
-{{--                         'url'=>site_url($iframe_start . '?content_id=0&no_editmode=true&preview_layout=' . $layout['layout_file_preview']--}}
-{{--                    )])--}}
+                <div class="card">
+                        @php
+                            $iframe_start = site_url('new-content-preview-'. uniqid());
+                        @endphp
+                        @include('page::admin.page.iframe', [
+                         'url'=>site_url($iframe_start . '?content_id=0&no_editmode=true&preview_layout=' . $layout['layout_file_preview']
+                    )])
 
-{{--                </div>--}}
-{{--            </div>--}}
+                </div>
+            </div>
 
         @endforeach
 
