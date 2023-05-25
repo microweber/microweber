db6159d05022a9d8568b8ad6f3c9fdb612056797
Bozhidar Slaveykov <bobi@microweber.com>
2023-05-25 17:41:29 +0300

    update

--- src/MicroweberPackages/Page/resources/views/admin/page/create.blade.php
+++ src/MicroweberPackages/Page/resources/views/admin/page/create.blade.php
@@ -13,7 +13,7 @@
         <div class="col-md-4 pe-5 pb-3">
             <h3 class="mb-3 font-weight-bold">{{_e("Clean Page")}} </h3>
 
-            <div class="card">
+            <a href="{{route('admin.page.create')}}?layout=clean" class="card card-link card-link-pop">
 
                 <div class="card-body" style="padding: 13px; height: calc(50vh - 80px);">
                     <div class="create-page-clean d-flex justify-content-center align-items-center h-100">
@@ -21,8 +21,9 @@
                     </div>
                 </div>
 
-            </div>
+            </a>
         </div>
+
         @foreach($layouts as $layout)
 
             @php
@@ -33,15 +34,18 @@
 
             <div class="col-md-4 pe-5 pb-3">
                 <h3 class="mb-3 font-weight-bold">{{$layout['name']}} </h3>
-
                 <div class="card">
-                        @php
+                    @php
                             $iframe_start = site_url('new-content-preview-'. uniqid());
                         @endphp
                         @include('page::admin.page.iframe', [
                          'url'=>site_url($iframe_start . '?content_id=0&no_editmode=true&preview_layout=' . $layout['layout_file_preview']
                     )])
-
+                    <div class="p-2 text-center">
+                        <a href="{{route('admin.page.create')}}?layout={{$layout['layout_file_preview']}}" class="btn btn-outline-primary">
+                            Click to create
+                        </a>
+                    </div>
                 </div>
             </div>
 
