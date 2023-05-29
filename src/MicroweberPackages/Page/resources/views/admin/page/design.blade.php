e1724a1a00f65dc997bc163ebcd943609897c33b
Bozhidar Slaveykov <bobi@microweber.com>
2023-05-29 16:43:09 +0300

    update

--- src/MicroweberPackages/Page/resources/views/admin/page/design.blade.php
+++ src/MicroweberPackages/Page/resources/views/admin/page/design.blade.php
@@ -2,12 +2,44 @@
 
 @section('content')
 
+    <style>
+        .preview_frame_container.preview-in-self {
+            height: calc(80vh - 80px);
+        }
+        .preview_frame_container {
+            position: relative;
+            overflow: hidden;
+        }
+        .preview_frame_container.preview-in-self iframe {
+            height: calc(160vh - 160px) !important;
+        }
+        .preview_frame_container iframe {
+            width: 200%;
+            transform: scale(.5);
+            top: 0;
+            position: absolute;
+            left: 0;
+            transform-origin: 0 0;
+            border: 1px solid silver;
+            transition: .3s;
+        }
+    </style>
+
 
     <div class="mx-5">
-        <module type="settings/group/template"
-                show_save_changes_buttons="false"
-                show_allow_multiple_template="false"
-        />
+
+        <div>
+            <h3 class="font-weight-bold">Design</h3>
+            <p>Change the design of your page</p>
+        </div>
+
+        <div>
+            <div class="preview_frame_container preview-in-self">
+               <iframe clas="preview_frame_small" src="{{site_url()}}"></iframe>
+            </div>
+        </div>
+
+
     </div>
 
 @endsection
