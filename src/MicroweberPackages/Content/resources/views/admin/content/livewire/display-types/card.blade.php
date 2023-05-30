e3885f9f9f87c21490bad9dbf6b61252de497e83
Nikolay <rachkuw@gmail.com>
2023-05-30 18:24:01 +0300

    update

--- src/MicroweberPackages/Content/resources/views/admin/content/livewire/display-types/card.blade.php
+++ src/MicroweberPackages/Content/resources/views/admin/content/livewire/display-types/card.blade.php
@@ -2,23 +2,23 @@
     @foreach ($contents as $content)
 
         <div class="card card-product-holder mb-2 post-has-image-true manage-post-item mb-3">
-            <div class="card-body">
+            <div class="card-body px-4">
                 <div class="d-flex flex-wrap align-items-center flex-lg-box">
 
-                    <div class="col text-center manage-post-item-col-1" style="max-width: 40px;">
-                        <div class="custom-control custom-checkbox ms-1">
-                            <input type="checkbox" value="{{ $content->id }}" id="products-{{ $content->id }}"  class="js-select-posts-for-action form-check-input" wire:model="checked">
-                            <label for="products-{{ $content->id }}" class="custom-control-label"></label>
-                        </div>
-
-                        <div class="cursor-move-holder js-move mw_admin_posts_sortable_handle" onmousedown="mw.manage_content_sort()" style="max-width: 80px;">
+                    <div class="col text-center manage-post-item-col-1 d-flex align-items-center" style="max-width: 40px;">
+                        <div class="cursor-move-holder me-2 js-move mw_admin_posts_sortable_handle" onmousedown="mw.manage_content_sort()" style="max-width: 80px;">
                               <span href="javascript:;" class="btn btn-link text-blue-lt">
                                   <svg class="mdi-cursor-move" fill="currentColor" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 96 960 960" width="24"><path d="M360 896q-33 0-56.5-23.5T280 816q0-33 23.5-56.5T360 736q33 0 56.5 23.5T440 816q0 33-23.5 56.5T360 896Zm240 0q-33 0-56.5-23.5T520 816q0-33 23.5-56.5T600 736q33 0 56.5 23.5T680 816q0 33-23.5 56.5T600 896ZM360 656q-33 0-56.5-23.5T280 576q0-33 23.5-56.5T360 496q33 0 56.5 23.5T440 576q0 33-23.5 56.5T360 656Zm240 0q-33 0-56.5-23.5T520 576q0-33 23.5-56.5T600 496q33 0 56.5 23.5T680 576q0 33-23.5 56.5T600 656ZM360 416q-33 0-56.5-23.5T280 336q0-33 23.5-56.5T360 256q33 0 56.5 23.5T440 336q0 33-23.5 56.5T360 416Zm240 0q-33 0-56.5-23.5T520 336q0-33 23.5-56.5T600 256q33 0 56.5 23.5T680 336q0 33-23.5 56.5T600 416Z"/></svg>
                               </span>
                         </div>
+
+                        <div class="custom-control custom-checkbox mt-4">
+                            <input type="checkbox" value="{{ $content->id }}" id="products-{{ $content->id }}"  class="js-select-posts-for-action form-check-input" wire:model="checked">
+                            <label for="products-{{ $content->id }}" class="custom-control-label"></label>
+                        </div>
                     </div>
 
-                    <div class="col manage-post-item-col-2 mx-md-2" style="max-width: 100px; min-width: 100px;">
+                    <div class="col manage-post-item-col-2 mx-md-4" style="max-width: 80px; min-width: 80px;">
 
 {{--                        @include('page::admin.page.iframe', [--}}
 {{--                            'iframeWidth'=> '600%',--}}
