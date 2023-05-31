7e10786bae4aca93ee1ac8de3c8792d6f6191503
Nikolay <rachkuw@gmail.com>
2023-05-31 12:20:25 +0300

    update

--- src/MicroweberPackages/Page/resources/views/admin/page/design.blade.php
+++ src/MicroweberPackages/Page/resources/views/admin/page/design.blade.php
@@ -46,12 +46,12 @@
                        </div>
 
                         <div class="d-flex align-items-center gap-3">
-                            <a class="tblr-body-color font-weight-bold text-decoration-none" href="{{admin_url('settings?group=general')}}">
+                            <a class="tblr-body-color font-weight-bold text-decoration-none mw-admin-action-links" href="{{admin_url('settings?group=general')}}">
 
                                 <svg fill="currentColor" xmlns="http://www.w3.org/2000/svg" height="20" viewBox="0 -960 960 960" width="20"><path d="M180-180h44l443-443-44-44-443 443v44Zm614-486L666-794l42-42q17-17 42-17t42 17l44 44q17 17 17 42t-17 42l-42 42Zm-42 42L248-120H120v-128l504-504 128 128Zm-107-21-22-22 44 44-22-22Z"/></svg>
                                 <?php _e("Site Details") ?>
                             </a>
-                            <a class="tblr-body-color font-weight-bold text-decoration-none" href="{{admin_url('marketplace?keyword=&category=microweber-template')}}">
+                            <a class="tblr-body-color font-weight-bold text-decoration-none mw-admin-action-links" href="{{admin_url('marketplace?keyword=&category=microweber-template')}}">
                                 <svg fill="currentColor" xmlns="http://www.w3.org/2000/svg" height="20" viewBox="0 -960 960 960" width="20"><path d="M840-519v339q0 24-18 42t-42 18H179q-24 0-42-18t-18-42v-339q-28-24-37-59t2-70l43-135q8-27 28-42t46-15h553q28 0 49 15.5t29 41.5l44 135q11 35 1.5 70T840-519Zm-270-31q29 0 49-19t16-46l-25-165H510v165q0 26 17 45.5t43 19.5Zm-187 0q28 0 47.5-19t19.5-46v-165H350l-25 165q-4 26 14 45.5t44 19.5Zm-182 0q24 0 41.5-16.5T263-607l26-173H189l-46 146q-10 31 8 57.5t50 26.5Zm557 0q32 0 50.5-26t8.5-58l-46-146H671l26 173q3 24 20.5 40.5T758-550ZM179-180h601v-311q1 1-6.5 1H758q-25 0-47.5-10.5T666-533q-16 20-40 31.5T573-490q-30 0-51.5-8.5T480-527q-15 18-38 27.5t-52 9.5q-31 0-55-11t-41-32q-24 21-47 32t-46 11h-13.5q-6.5 0-8.5-1v311Zm601 0H179h601Z"/></svg>
                                 <?php _e("Template Store") ?>
                             </a>
--- userfiles/modules/contact_form/src/resources/views/livewire/admin/contact-form/list.blade.php
+++ userfiles/modules/contact_form/src/resources/views/livewire/admin/contact-form/list.blade.php
@@ -3,7 +3,7 @@
     <div class="d-flex flex-wrap justify-content-between align-items-center">
         <div class="col-md-2 col-12">
             <div class="form-group">
-                <label class="form-label d-block mb-2">
+                <label class="form-label d-block">
                     {{_e('Your form lists')}}
                 </label>
                 <select wire:model="filter.formListId" class="form-select form-control">
@@ -31,13 +31,17 @@
         </div>
 
         <div class="col-md-4 col-12 text-xl-end mt-4">
-            <button type="button" onclick="Livewire.emit('openModal', 'contact-form.settings-modal')" class="btn btn-outline-primary mb-3 btn-sm">
-                <i class="mdi mdi-cogs"></i> {{_e('Settings')}}
+            <button onclick="Livewire.emit('openModal', 'contact-form.settings-modal')" class="btn btn-link tblr-body-color font-weight-bold text-decoration-none me-3 fs-3 mw-admin-action-links" href="{{admin_url('settings?group=general')}}">
+
+                <svg fill="currentColor" class="me-1" xmlns="http://www.w3.org/2000/svg" height="20" viewBox="0 -960 960 960" width="20"><path d="m388-80-20-126q-19-7-40-19t-37-25l-118 54-93-164 108-79q-2-9-2.5-20.5T185-480q0-9 .5-20.5T188-521L80-600l93-164 118 54q16-13 37-25t40-18l20-127h184l20 126q19 7 40.5 18.5T669-710l118-54 93 164-108 77q2 10 2.5 21.5t.5 21.5q0 10-.5 21t-2.5 21l108 78-93 164-118-54q-16 13-36.5 25.5T592-206L572-80H388Zm92-270q54 0 92-38t38-92q0-54-38-92t-92-38q-54 0-92 38t-38 92q0 54 38 92t92 38Zm0-60q-29 0-49.5-20.5T410-480q0-29 20.5-49.5T480-550q29 0 49.5 20.5T550-480q0 29-20.5 49.5T480-410Zm0-70Zm-44 340h88l14-112q33-8 62.5-25t53.5-41l106 46 40-72-94-69q4-17 6.5-33.5T715-480q0-17-2-33.5t-7-33.5l94-69-40-72-106 46q-23-26-52-43.5T538-708l-14-112h-88l-14 112q-34 7-63.5 24T306-642l-106-46-40 72 94 69q-4 17-6.5 33.5T245-480q0 17 2.5 33.5T254-413l-94 69 40 72 106-46q24 24 53.5 41t62.5 25l14 112Z"/></svg>
+                {{_e('Settings')}}
             </button>
-            &nbsp; &nbsp;
-            <button  type="button" onclick="Livewire.emit('openModal', 'contact-form.integrations-modal')" class="btn btn-outline-primary mb-3 btn-sm">
-                <i class="mdi mdi-cogs"></i> {{_e('Integrations')}}
+
+            <button onclick="Livewire.emit('openModal', 'contact-form.integrations-modal')" class="btn btn-link tblr-body-color font-weight-bold text-decoration-none fs-3 mw-admin-action-links" href="{{admin_url('settings?group=general')}}">
+                <svg fill="currentColor" class="me-1" xmlns="http://www.w3.org/2000/svg" height="20" viewBox="0 -960 960 960" width="20"><path d="m384-335 43-43-101-102 101-101-43-43-144 144.5L384-335Zm192 0 145-145-144-144-43 43 101 101-102 102 43 43ZM180-120q-24.75 0-42.375-17.625T120-180v-600q0-24.75 17.625-42.375T180-840h205q5-35 32-57.5t63-22.5q36 0 63 22.5t32 57.5h205q24.75 0 42.375 17.625T840-780v600q0 24.75-17.625 42.375T780-120H180Zm0-60h600v-600H180v600Zm300-617q14 0 24.5-10.5T515-832q0-14-10.5-24.5T480-867q-14 0-24.5 10.5T445-832q0 14 10.5 24.5T480-797ZM180-180v-600 600Z"/></svg>
+                {{_e('Integrations')}}
             </button>
+
         </div>
     </div>
 
