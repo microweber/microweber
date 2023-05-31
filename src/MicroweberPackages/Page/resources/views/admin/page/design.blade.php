7a1b88da601db2661a6e6f7625a7ab8d0a7eeb77
Nikolay <rachkuw@gmail.com>
2023-05-31 15:32:20 +0300

    update

--- src/MicroweberPackages/Page/resources/views/admin/page/design.blade.php
+++ src/MicroweberPackages/Page/resources/views/admin/page/design.blade.php
@@ -30,13 +30,13 @@
     </style>
 <div class="col-xxl-10 col-11 mx-auto">
     <h3 class="main-pages-title"><?php _e("Design") ?>
-        <svg style="opacity: .6; margin-bottom: 5px;" fill="currentColor" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M484-247q16 0 27-11t11-27q0-16-11-27t-27-11q-16 0-27 11t-11 27q0 16 11 27t27 11Zm-35-146h59q0-26 6.5-47.5T555-490q31-26 44-51t13-55q0-53-34.5-85T486-713q-49 0-86.5 24.5T345-621l53 20q11-28 33-43.5t52-15.5q34 0 55 18.5t21 47.5q0 22-13 41.5T508-512q-30 26-44.5 51.5T449-393Zm31 313q-82 0-155-31.5t-127.5-86Q143-252 111.5-325T80-480q0-83 31.5-156t86-127Q252-817 325-848.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 82-31.5 155T763-197.5q-54 54.5-127 86T480-80Zm0-60q142 0 241-99.5T820-480q0-142-99-241t-241-99q-141 0-240.5 99T140-480q0 141 99.5 240.5T480-140Zm0-340Z"/></svg>
+{{--        <svg style="opacity: .6; margin-bottom: 5px;" fill="currentColor" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M484-247q16 0 27-11t11-27q0-16-11-27t-27-11q-16 0-27 11t-11 27q0 16 11 27t27 11Zm-35-146h59q0-26 6.5-47.5T555-490q31-26 44-51t13-55q0-53-34.5-85T486-713q-49 0-86.5 24.5T345-621l53 20q11-28 33-43.5t52-15.5q34 0 55 18.5t21 47.5q0 22-13 41.5T508-512q-30 26-44.5 51.5T449-393Zm31 313q-82 0-155-31.5t-127.5-86Q143-252 111.5-325T80-480q0-83 31.5-156t86-127Q252-817 325-848.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 82-31.5 155T763-197.5q-54 54.5-127 86T480-80Zm0-60q142 0 241-99.5T820-480q0-142-99-241t-241-99q-141 0-240.5 99T140-480q0 141 99.5 240.5T480-140Zm0-340Z"/></svg>--}}
     </h3>
 
     <div class="card">
         <div class="card-body">
             <div class="row">
-                <div class="d-flex align-items-center flex-wrap justify-content-between mb-4">
+                <div class="d-flex align-items-start flex-wrap justify-content-between mb-4">
                     <div class="col-lg-6 col-12 mb-xl-0 mb-2">
 
                        <div class="mb-4">
@@ -66,18 +66,24 @@
                     </div>
 
                     <div class="col-lg-6 col-12 text-end">
-                        <a href="" target="_new" class="js-layout-preview btn btn-outline-primary btn-sm"><?php _e("Preview") ?></a>
-                        <a href="" class="js-layout-customize btn btn-primary btn-sm"><?php _e("Customize") ?></a>
+                        <a href="" target="_new" class="js-layout-preview btn btn-link fs-3 me-2 tblr-body-color" data-bs-toggle="tooltip" aria-label="{{ _e("Preview") }}" data-bs-original-title="{{ _e("Preview") }}">
+                            <svg class="me-2" fill="currentColor" xmlns="http://www.w3.org/2000/svg" height="18px" viewBox="0 0 24 24" width="18px"><path d="M0 0h24v24H0V0z" fill="none"></path><path d="M12 6c3.79 0 7.17 2.13 8.82 5.5C19.17 14.87 15.79 17 12 17s-7.17-2.13-8.82-5.5C4.83 8.13 8.21 6 12 6m0-2C7 4 2.73 7.11 1 11.5 2.73 15.89 7 19 12 19s9.27-3.11 11-7.5C21.27 7.11 17 4 12 4zm0 5c1.38 0 2.5 1.12 2.5 2.5S13.38 14 12 14s-2.5-1.12-2.5-2.5S10.62 9 12 9m0-2c-2.48 0-4.5 2.02-4.5 4.5S9.52 16 12 16s4.5-2.02 4.5-4.5S14.48 7 12 7z"></path></svg>
+
+                        </a>
+
+                        <a href="" class="js-layout-customize btn btn-link fs-3 tblr-body-color" data-bs-toggle="tooltip" aria-label="{{ _e("Customize") }}" data-bs-original-title="{{ _e("Customize") }}">
+
+                        </a>
                     </div>
                 </div>
 
                 <div class="card mb-4">
 
-                   <div class="card-body shadow-sm p-0">
-                       <div class="row">
+                   <div class="card-body p-0">
+                       <div class="row p-0">
                            <div class="card-header d-flex flex-wrap align-items-center justify-content-between shadow-sm p-3">
                               <div class="col-xl-2 col-lg-4 col-md-6 col-12">
-                                  <select class="js-select-layout form-select border-0">
+                                  <select class="js-select-layout form-select">
                                       @foreach($allLayouts as $layout)
                                           @if(isset($layout['pages']) && count($layout['pages']) > 1)
                                           <optgroup label="{{$layout['name']}}">
@@ -131,12 +137,14 @@
                                        $('.preview_frame_small').attr('src', livePreviewUrl);
                                        $('.js-layout-preview').attr('href', livePreviewUrl);
 
+                                        var createIconSvg = '<svg class="me-2" fill="currentColor" xmlns="http://www.w3.org/2000/svg" height="18px" viewBox="0 -960 960 960" width="18px"><path d="M450-200v-250H200v-60h250v-250h60v250h250v60H510v250h-60Z"/></svg>'
+                                       var customizeIconSvg = '<svg class="me-2" fill="currentColor" xmlns="http://www.w3.org/2000/svg" height="18px" viewBox="0 -960 960 960" width="18px"><path d="M480-80q-82 0-155-31.5t-127.5-86Q143-252 111.5-325T80-480q0-85 32-158t87.5-127q55.5-54 130-84.5T489-880q79 0 150 26.5T763.5-780q53.5 47 85 111.5T880-527q0 108-63 170.5T650-294h-75q-18 0-31 14t-13 31q0 27 14.5 46t14.5 44q0 38-21 58.5T480-80Zm0-400Zm-233 26q20 0 35-15t15-35q0-20-15-35t-35-15q-20 0-35 15t-15 35q0 20 15 35t35 15Zm126-170q20 0 35-15t15-35q0-20-15-35t-35-15q-20 0-35 15t-15 35q0 20 15 35t35 15Zm214 0q20 0 35-15t15-35q0-20-15-35t-35-15q-20 0-35 15t-15 35q0 20 15 35t35 15Zm131 170q20 0 35-15t15-35q0-20-15-35t-35-15q-20 0-35 15t-15 35q0 20 15 35t35 15ZM480-140q11 0 15.5-4.5T500-159q0-14-14.5-26T471-238q0-46 30-81t76-35h73q76 0 123-44.5T820-527q0-132-100-212.5T489-820q-146 0-247.5 98.5T140-480q0 141 99.5 240.5T480-140Z"></path></svg>'
                                        if (editUrl.length > 3) {
-                                           $('.js-layout-customize').html('<?php _e("Customize") ?>');
+                                           $('.js-layout-customize').html(customizeIconSvg);
                                            $('.js-layout-customize').attr('href', editUrl);
                                        } else {
                                            $('.js-layout-customize').attr('href', createUrl);
-                                           $('.js-layout-customize').html('<?php _e("Create") ?>');
+                                           $('.js-layout-customize').html(createIconSvg);
                                        }
                                    }
                                    selectLayout();
