8fbfcca715ab913ca98fba5c7eb8c422f3d94c67
Nikolay <rachkuw@gmail.com>
2023-05-31 18:12:47 +0300

    Update index.blade.php

--- src/MicroweberPackages/Marketplace/resources/views/admin/marketplace/livewire/index.blade.php
+++ src/MicroweberPackages/Marketplace/resources/views/admin/marketplace/livewire/index.blade.php
@@ -24,13 +24,18 @@
                        </div>
                    </div>
                    <div class="col-xl-4 col-12 px-1 text-xl-end mt-xl-0 mt-2">
-                       <button type="button" class="btn btn-outline-primary" wire:click="reloadPackages">
+                       <button type="button" class="btn btn-link fs-3 text-decoration-none mw-admin-action-links tblr-body-color font-weight-bold me-3" wire:click="reloadPackages">
                            <div wire:loading wire:target="reloadPackages" class="spinner-border spinner-border-sm" role="status">
                                <span class="visually-hidden"><?php _e("Loading"); ?>...</span>
                            </div>
+                           <svg class="me-2" fill="currentColor" xmlns="http://www.w3.org/2000/svg" height="20" viewBox="0 0 24 24" width="20" ><path d="M0 0h24v24H0V0z" fill="none"/><path d="M17.65 6.35C16.2 4.9 14.21 4 12 4c-4.42 0-7.99 3.58-7.99 8s3.57 8 7.99 8c3.73 0 6.84-2.55 7.73-6h-2.08c-.82 2.33-3.04 4-5.65 4-3.31 0-6-2.69-6-6s2.69-6 6-6c1.66 0 3.14.69 4.22 1.78L13 11h7V4l-2.35 2.35z"/></svg>
                            <?php _e("Reload packages"); ?>
                        </button>
-                       <button type="button" class="btn btn-outline-success" onclick="mw.admin.admin_package_manager.show_licenses_modal();"><?php _e("Licenses"); ?></button>
+                       <button type="button" class="btn btn-link fs-3 text-decoration-none mw-admin-action-links tblr-body-color font-weight-bold" onclick="mw.admin.admin_package_manager.show_licenses_modal();">
+                           <svg class="me-2" fill="currentColor" xmlns="http://www.w3.org/2000/svg" height="20" viewBox="0 -960 960 960" width="20"><path d="m385-412 36-115-95-74h116l38-119 37 119h117l-95 74 35 115-94-71-95 71ZM244-40v-304q-45-47-64.5-103T160-560q0-136 92-228t228-92q136 0 228 92t92 228q0 57-19.5 113T716-344v304l-236-79-236 79Zm236-260q109 0 184.5-75.5T740-560q0-109-75.5-184.5T480-820q-109 0-184.5 75.5T220-560q0 109 75.5 184.5T480-300ZM304-124l176-55 176 55v-171q-40 29-86 42t-90 13q-44 0-90-13t-86-42v171Zm176-86Z"/></svg>
+                           <?php _e("Licenses"); ?>
+
+                       </button>
                    </div>
                </div>
 
