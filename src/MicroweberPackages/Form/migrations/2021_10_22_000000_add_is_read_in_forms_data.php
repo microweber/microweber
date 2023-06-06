bd28e86c3cf2508297b85ae4e9b7f6a2b57fc374
Bozhidar Slaveykov <bobi@microweber.com>
2023-06-06 14:10:38 +0300

    Update 2021_10_22_000000_add_is_read_in_forms_data.php

--- src/MicroweberPackages/Form/migrations/2021_10_22_000000_add_is_read_in_forms_data.php
+++ src/MicroweberPackages/Form/migrations/2021_10_22_000000_add_is_read_in_forms_data.php
@@ -1,7 +1,7 @@
 <?php
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\Schema; 
 
 class AddIsReadInFormsData extends Migration
 {
