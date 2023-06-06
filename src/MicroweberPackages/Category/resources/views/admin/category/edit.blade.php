7cd45904924db08464d5b573ffc5eddb2d41e1d2
Bozhidar Slaveykov <bobi@microweber.com>
2023-06-06 16:45:36 +0300

    update

--- src/MicroweberPackages/Category/resources/views/admin/category/edit.blade.php
+++ src/MicroweberPackages/Category/resources/views/admin/category/edit.blade.php
@@ -1,5 +1,18 @@
 @extends('admin::layouts.app')
 
+@section('topbar2-links-left')
+
+    <div class="mw-toolbar-back-button-wrapper">
+        <div class="main-toolbar mw-modules-toolbar-back-button-holder mb-3 " id="mw-modules-toolbar" style="">
+            <a href="{{route('admin.category.index')}}">
+                <svg fill="currentColor" xmlns="http://www.w3.org/2000/svg" height="28" viewBox="0 96 960 960" width="28"><path d="M480 896 160 576l320-320 47 46.666-240.001 240.001H800v66.666H286.999L527 849.334 480 896Z"></path></svg>
+            </a>
+        </div>
+    </div>
+
+@endsection
+
+
 @section('content')
 
     <div class="d-flex">
