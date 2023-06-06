c2a9a84155e132e83f3520dc7a60a365c5aca0d1
Nikolay <rachkuw@gmail.com>
2023-06-06 16:55:51 +0300

    Update card.blade.php

--- src/MicroweberPackages/Content/resources/views/admin/content/livewire/display-types/card.blade.php
+++ src/MicroweberPackages/Content/resources/views/admin/content/livewire/display-types/card.blade.php
@@ -12,7 +12,7 @@
                               </span>
                         </div>
 
-                        <div class="custom-control custom-checkbox mt-4">
+                        <div class="custom-control custom-checkbox d-flex align-items-center">
                             <input type="checkbox" value="{{ $content->id }}" id="products-{{ $content->id }}"  class="js-select-posts-for-action form-check-input" wire:model="checked">
                             <label for="products-{{ $content->id }}" class="custom-control-label"></label>
                         </div>
