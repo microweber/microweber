735047cf30adfb784777f9803b12e5d875324d5d
Bozhidar Slaveykov <bobi@microweber.com>
2023-06-14 15:41:50 +0300

    update

--- src/MicroweberPackages/MicroweberUI/resources/views/components/captcha.blade.php
+++ src/MicroweberPackages/MicroweberUI/resources/views/components/captcha.blade.php
@@ -3,12 +3,11 @@
         $randId = time().rand(1000,9000);
     @endphp
     <module type="captcha" id="js-captcha-module-{{$randId}}" data-callback="captchaResponse{{$randId}}" />
-    <input type="text" id="js-{{$randId}}" {!! $attributes->merge([]) !!} />
+    <input type="hidden" id="js-{{$randId}}" {!! $attributes->merge([]) !!} />
     <script>
-        function captchaResponse{{$randId}}() {
-            let captcha = document.getElementsByName("captcha")[0];
+        function captchaResponse{{$randId}}(value) {
             let captchaDataElement = document.getElementById('js-{{$randId}}');
-            captchaDataElement.value = captcha.value;
+            captchaDataElement.value = value;
             captchaDataElement.dispatchEvent(new Event('input'));
         }
     </script>
