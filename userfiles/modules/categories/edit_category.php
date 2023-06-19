973388f1a721e59c280a7ee4a73de81523167ddf
Nikolay <rachkuw@gmail.com>
2023-06-19 17:45:00 +0300

    Update edit_category.php

--- userfiles/modules/categories/edit_category.php
+++ userfiles/modules/categories/edit_category.php
@@ -366,7 +366,7 @@ if (isset($_GET['parent'])) {
                             }
                             ?>
 
-                            <a href="<?php print category_link($data['id']) ?>" target="_blank"  data-bs-toggle="tooltip" aria-label="View Category" data-bs-original-title="View Category">
+                            <a class="tblr-body-color" href="<?php print category_link($data['id']) ?>" target="_blank"  data-bs-toggle="tooltip" aria-label="View Category" data-bs-original-title="View Category">
 
                                 <svg class="me-2 " fill="currentColor" xmlns="http://www.w3.org/2000/svg" height="20" viewBox="0 96 960 960" width="20"><path d="M480.078 729.333q72.255 0 122.755-50.578 50.5-50.579 50.5-122.833 0-72.255-50.578-122.755-50.579-50.5-122.833-50.5-72.255 0-122.755 50.578-50.5 50.579-50.5 122.833 0 72.255 50.578 122.755 50.579 50.5 122.833 50.5Zm-.235-62.666q-46.176 0-78.343-32.324-32.167-32.323-32.167-78.5 0-46.176 32.324-78.343 32.323-32.167 78.5-32.167 46.176 0 78.343 32.324 32.167 32.323 32.167 78.5 0 46.176-32.324 78.343-32.323 32.167-78.5 32.167ZM480 856q-146 0-264.667-82.5Q96.667 691 40 556q56.667-135 175.333-217.5Q334 256 480 256q146 0 264.667 82.5Q863.333 421 920 556q-56.667 135-175.333 217.5Q626 856 480 856Zm0-300Zm-.112 233.334q118.445 0 217.612-63.5Q796.667 662.333 848.667 556q-52-106.333-151.054-169.834-99.055-63.5-217.501-63.5-118.445 0-217.612 63.5Q163.333 449.667 110.666 556q52.667 106.333 151.721 169.834 99.055 63.5 217.501 63.5Z"/></svg>
                             </a>
@@ -378,9 +378,9 @@ if (isset($_GET['parent'])) {
                         <?php endif; ?>
                     </div>
                 </div>
-<!--                <div>-->
-<!--                    <button type="button" onclick="save_cat(this);" dusk="category-save" class="btn btn-dark" form="quickform-edit-content"><i class="mdi mdi-content-save me-1"></i> --><?php //_e('Save') ?><!--</button>-->
-<!--                </div>-->
+                <div>
+                    <button type="button" onclick="save_cat(this);" dusk="category-save" class="btn btn-dark" form="quickform-edit-content"><i class="mdi mdi-content-save me-1"></i> <?php _e('Save') ?></button>
+                </div>
             </div>
         <?php endif; ?>
 
