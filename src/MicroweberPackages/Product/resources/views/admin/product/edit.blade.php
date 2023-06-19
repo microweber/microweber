8484dba35328c257610f06f0117d0b2ec0f074d8
Nikolay <rachkuw@gmail.com>
2023-06-19 16:38:15 +0300

    Update edit.blade.php

--- src/MicroweberPackages/Product/resources/views/admin/product/edit.blade.php
+++ src/MicroweberPackages/Product/resources/views/admin/product/edit.blade.php
@@ -4,7 +4,7 @@
 @section('topbar2-links-right')
 
    <div id="content-title-field-buttons" class="mw-page-component-disabled">
-       <button id="js-admin-save-content-main-btn" type="submit" class="btn btn-dark mw-admin-bold-outline-dark btn-save js-bottom-save shadow-none ms-atuo me-3" form="quickform-edit-content"><span>
+       <button id="js-admin-save-content-main-btn" type="submit" class="btn btn-dark mw-admin-bold-outline-dark btn-save js-bottom-save shadow-none border-0 ms-atuo me-3" form="quickform-edit-content"><span>
         <svg fill="currentColor" class="mw-admin-save-btn-svg me-2" xmlns="http://www.w3.org/2000/svg" height="28" viewBox="0 -960 960 960" width="28"><path d="M840-683v503q0 24-18 42t-42 18H180q-24 0-42-18t-18-42v-600q0-24 18-42t42-18h503l157 157Zm-60 27L656-780H180v600h600v-476ZM479.765-245Q523-245 553.5-275.265q30.5-30.264 30.5-73.5Q584-392 553.735-422.5q-30.264-30.5-73.5-30.5Q437-453 406.5-422.735q-30.5 30.264-30.5 73.5Q376-306 406.265-275.5q30.264 30.5 73.5 30.5ZM233-584h358v-143H233v143Zm-53-72v476-600 124Z"/></svg>
 
         <span class="mw-admin-save-btn-loading spinner-border spinner-border-sm me-2" style="display: none;" role="status"></span>
