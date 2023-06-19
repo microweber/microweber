ef4b9f3659d8303ccaa057e1e1171759e95eac9f
Bozhidar Slaveykov <bobi@microweber.com>
2023-06-19 16:06:23 +0300

    update

--- src/MicroweberPackages/Product/resources/views/admin/product/edit.blade.php
+++ src/MicroweberPackages/Product/resources/views/admin/product/edit.blade.php
@@ -1,6 +1,38 @@
 @extends('admin::layouts.app')
 
 
+@section('topbar2-links-right')
+
+   <li>
+       <div id="content-title-field-buttons" class="mw-page-component-disabled">
+           <button id="js-admin-save-content-main-btn" type="submit" class="btn btn-outline-dark mw-admin-bold-outline-dark btn-save js-bottom-save ms-atuo" form="quickform-edit-content"><span>
+            <svg fill="currentColor" class="mw-admin-save-btn-svg me-2" xmlns="http://www.w3.org/2000/svg" height="20" viewBox="0 -960 960 960" width="20"><path d="M840-683v503q0 24-18 42t-42 18H180q-24 0-42-18t-18-42v-600q0-24 18-42t42-18h503l157 157Zm-60 27L656-780H180v600h600v-476ZM479.765-245Q523-245 553.5-275.265q30.5-30.264 30.5-73.5Q584-392 553.735-422.5q-30.264-30.5-73.5-30.5Q437-453 406.5-422.735q-30.5 30.264-30.5 73.5Q376-306 406.265-275.5q30.264 30.5 73.5 30.5ZM233-584h358v-143H233v143Zm-53-72v476-600 124Z"/></svg>
+
+            <span class="mw-admin-save-btn-loading spinner-border spinner-border-sm me-2" style="display: none;" role="status"></span>
+
+            <?php _e('Save'); ?></span>
+           </button>
+       </div>
+
+   </li>
+
+    @php
+        $past_page = site_url();
+    @endphp
+
+    @if (user_can_access('module.content.edit'))
+        <li>
+            <a href="{{$past_page}}?editmode=y"
+               class="btn btn-light border-0 go-live-edit-href-set admin-toolbar-buttons">
+                <img height="28" width="28" src="<?php print modules_url()?>/microweber/api/libs/mw-ui/assets/img/live-edit-button.svg" alt="">
+                <span class="  ms-2" style="font-size: 14px; font-weight: bold;">EDIT</span>
+            </a>
+        </li>
+    @endif
+
+
+@endsection
+
 @section('topbar2-links-left')
 
    <div class="d-flex align-items-center">
