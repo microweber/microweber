f5e336836bdba21819fbe1aee4b3a71b352a6b8a
Nikolay <rachkuw@gmail.com>
2023-06-19 16:30:18 +0300

    update

--- src/MicroweberPackages/Product/resources/views/admin/product/edit.blade.php
+++ src/MicroweberPackages/Product/resources/views/admin/product/edit.blade.php
@@ -3,20 +3,17 @@
 
 @section('topbar2-links-right')
 
-   <li>
-       <div id="content-title-field-buttons" class="mw-page-component-disabled">
-           <button id="js-admin-save-content-main-btn" type="submit" class="btn btn-dark mw-admin-bold-outline-dark btn-save js-bottom-save ms-atuo me-3" form="quickform-edit-content"><span>
-            <svg fill="currentColor" class="mw-admin-save-btn-svg me-2" xmlns="http://www.w3.org/2000/svg" height="28" viewBox="0 -960 960 960" width="28"><path d="M840-683v503q0 24-18 42t-42 18H180q-24 0-42-18t-18-42v-600q0-24 18-42t42-18h503l157 157Zm-60 27L656-780H180v600h600v-476ZM479.765-245Q523-245 553.5-275.265q30.5-30.264 30.5-73.5Q584-392 553.735-422.5q-30.264-30.5-73.5-30.5Q437-453 406.5-422.735q-30.5 30.264-30.5 73.5Q376-306 406.265-275.5q30.264 30.5 73.5 30.5ZM233-584h358v-143H233v143Zm-53-72v476-600 124Z"/></svg>
+   <div id="content-title-field-buttons" class="mw-page-component-disabled">
+       <button id="js-admin-save-content-main-btn" type="submit" class="btn btn-dark mw-admin-bold-outline-dark btn-save js-bottom-save ms-atuo me-3" form="quickform-edit-content"><span>
+        <svg fill="currentColor" class="mw-admin-save-btn-svg me-2" xmlns="http://www.w3.org/2000/svg" height="28" viewBox="0 -960 960 960" width="28"><path d="M840-683v503q0 24-18 42t-42 18H180q-24 0-42-18t-18-42v-600q0-24 18-42t42-18h503l157 157Zm-60 27L656-780H180v600h600v-476ZM479.765-245Q523-245 553.5-275.265q30.5-30.264 30.5-73.5Q584-392 553.735-422.5q-30.264-30.5-73.5-30.5Q437-453 406.5-422.735q-30.5 30.264-30.5 73.5Q376-306 406.265-275.5q30.264 30.5 73.5 30.5ZM233-584h358v-143H233v143Zm-53-72v476-600 124Z"/></svg>
 
-            <span class="mw-admin-save-btn-loading spinner-border spinner-border-sm me-2" style="display: none;" role="status"></span>
+        <span class="mw-admin-save-btn-loading spinner-border spinner-border-sm me-2" style="display: none;" role="status"></span>
 
-             <?php _e('SAVE'); ?>
+         <?php _e('SAVE'); ?>
 
-               </span>
-           </button>
-       </div>
-
-   </li>
+           </span>
+       </button>
+   </div>
 
     @php
         $past_page = site_url();
@@ -24,7 +21,7 @@
 
     @if (user_can_access('module.content.edit'))
         <a href="{{$past_page}}?editmode=y"
-           class="btn btn-light border-0 go-live-edit-href-set admin-toolbar-buttons">
+           class="btn btn-light go-live-edit-href-set admin-toolbar-buttons shadow-none">
             <img height="28" width="28" src="<?php print modules_url()?>/microweber/api/libs/mw-ui/assets/img/live-edit-button.svg" alt="">
             <span class="  ms-2" style="font-size: 14px; font-weight: bold;"><?php _e('EDIT'); ?></span>
         </a>
