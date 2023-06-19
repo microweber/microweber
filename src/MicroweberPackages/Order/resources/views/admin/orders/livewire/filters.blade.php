23ecc75aeee59bb0c9388f4fbd38fca4ea16afc8
Nikolay <rachkuw@gmail.com>
2023-06-19 10:42:31 +0300

    update

--- src/MicroweberPackages/Order/resources/views/admin/orders/livewire/filters.blade.php
+++ src/MicroweberPackages/Order/resources/views/admin/orders/livewire/filters.blade.php
@@ -1,15 +1,13 @@
-<div>
+<div class="col-lg-11 col-12 mx-auto">
 
-    <div class="mx-5">
-        <div class="card-body col-xl-12 mx-auto mb-3">
-            <div class="card-header d-flex col-12 align-items-center justify-content-between px-md-4">
-                <div class="col d-flex justify-content-md-start justify-content-center align-items-center px-0">
-                    <h1 class="main-pages-title">{{_e('Orders')}}</h1>
-                </div>
-                @include('order::admin.orders.livewire.components.keyword')
-                &nbsp;&nbsp;
-                @include('order::admin.orders.action-links-top')
+    <div class="card-body col-xl-12 mx-auto mb-3">
+        <div class="card-header d-flex col-12 align-items-center justify-content-between">
+            <div class="col d-flex justify-content-md-start justify-content-center align-items-center px-0">
+                <h1 class="main-pages-title">{{_e('Orders')}}</h1>
             </div>
+            @include('order::admin.orders.livewire.components.keyword')
+            &nbsp;&nbsp;
+            @include('order::admin.orders.action-links-top')
         </div>
     </div>
 
@@ -26,11 +24,8 @@
         </div>
     </div>
 
-
     <div class="d-flex">
-
         <div class="ms-0 ms-md-2 mb-3 mb-md-0">
-
             <a href="#" class="link-secondary ms-2 " data-bs-toggle="dropdown" aria-expanded="false" ><!-- Download SVG icon from http://tabler-icons.io/i/adjustments -->
                 <svg xmlns="http://www.w3.org/2000/svg" data-bs-toggle="tooltip" aria-label="Search settings" data-bs-original-title="Search settings" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><path d="M4 10a2 2 0 1 0 4 0a2 2 0 0 0 -4 0"></path><path d="M6 4v4"></path><path d="M6 12v8"></path><path d="M10 16a2 2 0 1 0 4 0a2 2 0 0 0 -4 0"></path><path d="M12 4v10"></path><path d="M12 18v2"></path><path d="M16 7a2 2 0 1 0 4 0a2 2 0 0 0 -4 0"></path><path d="M18 4v1"></path><path d="M18 9v11"></path></svg>
                 Show Filters
