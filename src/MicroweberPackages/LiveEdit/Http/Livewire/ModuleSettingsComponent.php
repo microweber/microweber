13c433c70fef45355016fa25caf10dfd9a7a1240
Bozhidar Slaveykov <bobi@microweber.com>
2023-06-23 15:01:06 +0300

    update

--- src/MicroweberPackages/LiveEdit/Http/Livewire/ModuleSettingsComponent.php
+++ src/MicroweberPackages/LiveEdit/Http/Livewire/ModuleSettingsComponent.php
@@ -9,9 +9,8 @@ class ModuleSettingsComponent extends AdminComponent
     public string $view = 'microweber-live-edit::module-settings';
     public string $moduleId = '';
     public string $moduleType = '';
-    public array $settings = [
-
-    ];
+    public array $settings = [];
+    public array $state = [];
 
     public function mount()
     {
@@ -23,17 +22,22 @@ class ModuleSettingsComponent extends AdminComponent
         }
     }
 
-    public function updatedSettings($settings)
+    public function updated()
     {
-
-        if ($this->settings) {
-            foreach ($this->settings as $key => $setting) {
-                save_option($key, $setting, $this->moduleId);
-            }
-        }
-        $this->emit('settingsChanged', ['moduleId' => $this->moduleId, 'settings' => $this->settings]);
-
+        dump($this->state);
     }
+
+//    public function updatedSettings($settings)
+//    {
+//
+//        if ($this->settings) {
+//            foreach ($this->settings as $key => $setting) {
+//                save_option($key, $setting, $this->moduleId);
+//            }
+//        }
+//        $this->emit('settingsChanged', ['moduleId' => $this->moduleId, 'settings' => $this->settings]);
+//
+//    }
 
     public function render()
     {
