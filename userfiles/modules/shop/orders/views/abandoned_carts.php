e37fa2e7ed398f8215d451fe5873a61832030ddb
Nikolay <rachkuw@gmail.com>
2023-06-26 15:34:14 +0300

    update

--- userfiles/modules/shop/orders/views/abandoned_carts.php
+++ userfiles/modules/shop/orders/views/abandoned_carts.php
@@ -25,36 +25,37 @@ if (!isset($orders) or !$orders) {
         });
     </script>
 <?php foreach ($orders as $item) : ?>
-    <table class="table abandoned-cart mt-2 mb-5" id="abandoned-cart-table<?php print $item['id'] ?>" cellpadding="0" cellspacing="0">
+    <div class="table-responsive">
+        <table class="table card-table table-vcenter abandoned-cart mb-5" id="abandoned-cart-table<?php print $item['id'] ?>" cellpadding="0" cellspacing="0">
             <script>
-            $(document).ready(function () {
-                $("#abandoned-cart-table<?php print $item['id'] ?> .mw-order-item-image").bind("mouseenter mouseleave", function (e) {
-                    var index = $(this).dataset('index');
-                    mw.tools.multihover(e, this, "#abandoned-cart-table<?php print $item['id'] ?> .mw-order-item-index-" + index);
+                $(document).ready(function () {
+                    $("#abandoned-cart-table<?php print $item['id'] ?> .mw-order-item-image").bind("mouseenter mouseleave", function (e) {
+                        var index = $(this).dataset('index');
+                        mw.tools.multihover(e, this, "#abandoned-cart-table<?php print $item['id'] ?> .mw-order-item-index-" + index);
+                    });
+                    $("#abandoned-cart-table<?php print $item['id'] ?> tr.mw-order-item").bind("mouseenter mouseleave", function (e) {
+                        var index = $(this).dataset('index');
+                        mw.tools.multihover(e, this, "#abandoned-cart-table<?php print $item['id'] ?> .mw-order-item-image-" + index);
+                    });
                 });
-                $("#abandoned-cart-table<?php print $item['id'] ?> tr.mw-order-item").bind("mouseenter mouseleave", function (e) {
-                    var index = $(this).dataset('index');
-                    mw.tools.multihover(e, this, "#abandoned-cart-table<?php print $item['id'] ?> .mw-order-item-image-" + index);
-                });
-            });
 
-            mw.responsive.table('#shop-orders', {
-                breakPoints: {
-                    768: 4,
-                    600: 2,
-                    400: 1
-                }
-            })
+                mw.responsive.table('#shop-orders', {
+                    breakPoints: {
+                        768: 4,
+                        600: 2,
+                        400: 1
+                    }
+                })
 
-        </script>
+            </script>
 
-        <thead class="table-active">
+            <thead class="table-active">
             <tr>
                 <th class="font-weight-bold"><?php _e("Abandoned Cart ID"); ?>: <?php print $item['id']; ?></th>
                 <th></th>
             </tr>
-        </thead>
-        <tbody>
+            </thead>
+            <tbody>
             <tr class="mw-order-item-<?php print $item['id'] ?> no-hover">
                 <td class="pb-0">
                     <?php $cart_items = get_cart('order_completed=0&session_id=' . $item['session_id']); ?>
@@ -77,32 +78,32 @@ if (!isset($orders) or !$orders) {
                         </div>
                         <table class="table m-0" cellspacing="0" cellpadding="0" width="100%">
                             <thead>
-                                <tr>
-                                    <th class="w-50"><?php _e("Product Name"); ?></th>
-                                    <th><?php _e("Custom fields"); ?></th>
-                                    <th><?php _e("Price"); ?></th>
-                                    <th><?php _e("Qty"); ?></th>
-                                    <th><?php _e("Total"); ?></th>
-                                </tr>
+                            <tr>
+                                <th class="w-50"><?php _e("Product Name"); ?></th>
+                                <th><?php _e("Custom fields"); ?></th>
+                                <th><?php _e("Price"); ?></th>
+                                <th><?php _e("Qty"); ?></th>
+                                <th><?php _e("Total"); ?></th>
+                            </tr>
                             </thead>
                             <tbody>
-                                <?php $subtotal = 0; ?>
-                                <?php $index = -1;
-                                foreach ($cart_items as $item) : ?>
-                                    <?php
-                                    $index++;
-                                    $item_total = floatval($item['qty']) * floatval($item['price']);
-                                    $subtotal = $subtotal + $item_total;
-                                    $grandtotal = $subtotal;
-                                    ?>
-                                    <tr data-index="<?php print $index; ?>" class="mw-order-item mw-order-item-<?php print $item['id'] ?> mw-order-item-index-<?php print $index; ?>">
-                                        <td class="mw-order-item-id"><a href="<?php print content_link($item['rel_id']) ?>" target="_blank"><span><?php print $item['title'] ?></span></a></td>
-                                        <td class="mw-order-item-fields"><?php if (isset($item['custom_fields'])): ?><?php print $item['custom_fields'] ?><?php endif ?></td>
-                                        <td class="mw-order-item-amount"><?php print ($item['price']) ?></td>
-                                        <td class="mw-order-item-count"><?php print $item['qty'] ?></td>
-                                        <td class="nowrap"><?php print currency_format($item_total); ?></td>
-                                    </tr>
-                                <?php endforeach; ?>
+                            <?php $subtotal = 0; ?>
+                            <?php $index = -1;
+                            foreach ($cart_items as $item) : ?>
+                                <?php
+                                $index++;
+                                $item_total = floatval($item['qty']) * floatval($item['price']);
+                                $subtotal = $subtotal + $item_total;
+                                $grandtotal = $subtotal;
+                                ?>
+                                <tr data-index="<?php print $index; ?>" class="mw-order-item mw-order-item-<?php print $item['id'] ?> mw-order-item-index-<?php print $index; ?>">
+                                    <td class="mw-order-item-id"><a href="<?php print content_link($item['rel_id']) ?>" target="_blank"><span><?php print $item['title'] ?></span></a></td>
+                                    <td class="mw-order-item-fields"><?php if (isset($item['custom_fields'])): ?><?php print $item['custom_fields'] ?><?php endif ?></td>
+                                    <td class="mw-order-item-amount"><?php print ($item['price']) ?></td>
+                                    <td class="mw-order-item-count"><?php print $item['qty'] ?></td>
+                                    <td class="nowrap"><?php print currency_format($item_total); ?></td>
+                                </tr>
+                            <?php endforeach; ?>
                             </tbody>
                         </table>
                     <?php else: ?>
@@ -112,21 +113,23 @@ if (!isset($orders) or !$orders) {
             </tr>
             <td class="p-20 d-flex">
                 <?php // event_trigger('mw_admin_quick_stats_by_session', $item['session_id']); ?>
-                   <div class="col-6">
-                       <label class="form-label d-block">
-                           <?php _e("Recover URL"); ?> <span class="mw-icon-help-outline mwahi tip" data-tip="<?php _e("Use this if you need to send it to your clients. They'll be able to restore their Shopping Cart."); ?>"></span>
-                       </label>
-                       <small class="text-muted" onclick="mw.wysiwyg.select_all(this);"><?php print $recart_base . '?recart=' . $item['session_id']; ?></small>
-                   </div>
+                <div class="col-6">
+                    <label class="form-label d-block">
+                        <?php _e("Recover URL"); ?> <span class="mw-icon-help-outline mwahi tip" data-tip="<?php _e("Use this if you need to send it to your clients. They'll be able to restore their Shopping Cart."); ?>"></span>
+                    </label>
+                    <small class="text-muted" onclick="mw.wysiwyg.select_all(this);"><?php print $recart_base . '?recart=' . $item['session_id']; ?></small>
+                </div>
                 <label class="form-label col-2 align-self-center">
                     <span class="mw-icon-lite-clock-outline" style="font-size: 16px;top:-1px;right:2px;"></span>
                     <span class="mw-ui-label-small tip" data-tipposition="top-center" data-tip="<?php _e("Last activity on"); ?>: <?php print $item['updated_at'] ?>"><?php print mw('format')->ago($item['updated_at']); ?></span>
                 </label>
-                   <div class="text-center col-5 align-self-center">
-                       <a class="btn btn-danger btn-sm" href="javascript:mw_delete_shop_order('<?php print ($item['session_id']) ?>',1);"><?php _e("Delete cart"); ?></a>
-                       <a class="btn btn-outline-secondary btn-sm" href="<?php print $recart_base . '?recart=' . $item['session_id'] ?>" target="_blank"><?php _e("Recover"); ?></a>
-                   </div>
+                <div class="text-center col-5 align-self-center">
+                    <a class="btn btn-danger btn-sm" href="javascript:mw_delete_shop_order('<?php print ($item['session_id']) ?>',1);"><?php _e("Delete cart"); ?></a>
+                    <a class="btn btn-outline-secondary btn-sm" href="<?php print $recart_base . '?recart=' . $item['session_id'] ?>" target="_blank"><?php _e("Recover"); ?></a>
+                </div>
             </td>
-        </tbody>
-    </table>
+            </tbody>
+        </table>
+
+    </div>
 <?php endforeach; ?>
