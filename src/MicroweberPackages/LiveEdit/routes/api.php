b8436fb95cf1a515a62790bb1a4018d56411481c
Bozhidar Slaveykov <bobi@microweber.com>
2023-07-03 16:01:13 +0300

    update

--- src/MicroweberPackages/LiveEdit/routes/api.php
+++ src/MicroweberPackages/LiveEdit/routes/api.php
@@ -2,3 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::name('api.live-edit.')
+    ->prefix('api/live-edit')
+    ->middleware(['api', 'admin'])
+    ->group(function () {
+
+        Route::namespace('MicroweberPackages\LiveEdit\Http\Controllers\Api')->group(function () {
+            Route::get('get-top-right-menu', 'LiveEditMenusApi@getTopRightMenu')->name('get-top-right-menu');
+        });
+
+    });
