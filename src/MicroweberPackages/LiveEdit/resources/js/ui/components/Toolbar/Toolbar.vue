d72b52199840a9ea1d3d1602d0050ea97392fbf1
Nikolay <rachkuw@gmail.com>
2023-07-04 11:53:04 +0300

    update

--- src/MicroweberPackages/LiveEdit/resources/js/ui/components/Toolbar/Toolbar.vue
+++ src/MicroweberPackages/LiveEdit/resources/js/ui/components/Toolbar/Toolbar.vue
@@ -1,19 +1,15 @@
 
 <template>
     <div id="toolbar" class="shadow-sm">
-        
+
         <div class="toolbar-nav toolbar-nav-hover">
-            <a href="./" class="mw-live-edit-toolbar-link mw-live-edit-toolbar-link--arrowed">
+            <a class="mw-live-edit-toolbar-link mw-live-edit-toolbar-link--arrowed" href="./">
                 Admin
-                <svg xmlns="http://www.w3.org/2000/svg" class="mw-live-edit-toolbar-arrow-icon" height="24" viewBox="0 -960 960 960"
-                     width="24">
-                    <g fill="none" stroke="#2175FF" stroke-width="1.5" stroke-linejoin="round" stroke-miterlimit="10">
-
+                <svg class="mw-live-edit-toolbar-arrow-icon" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32">
+                    <g fill="none" stroke-width="1.5" stroke-linejoin="round" stroke-miterlimit="10">
                         <circle class="arrow-icon--circle" cx="16" cy="16" r="15.12"></circle>
-
-                        <path class="arrow-icon--arrow" d="M480-160 160-480l320-320 42 42-248 248h526v60H274l248 248-42 42Z"></path>
+                        <path class="arrow-icon--arrow" d="M16.14 9.93L22.21 16l-6.07 6.07M8.23 16h13.98"></path>
                     </g>
-
                 </svg>
             </a>
         </div>
