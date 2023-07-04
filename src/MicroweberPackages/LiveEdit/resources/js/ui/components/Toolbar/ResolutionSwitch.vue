896ea7744631fd61a4fbd5e71b9f9cc7c9159e17
Nikolay <rachkuw@gmail.com>
2023-07-04 18:05:20 +0300

    update

--- src/MicroweberPackages/LiveEdit/resources/js/ui/components/Toolbar/ResolutionSwitch.vue
+++ src/MicroweberPackages/LiveEdit/resources/js/ui/components/Toolbar/ResolutionSwitch.vue
@@ -44,15 +44,14 @@ export default {
     <nav id="preview-nav" class="toolbar-nav mw-live-edit-resolutions-wrapper toolbar-nav-hover">
 
         <div>
-           <span class="btn btn-icon live-edit-toolbar-buttons" v-on:click="setPreviewMode('desktop')" data-preview="desktop"
+           <span class="btn btn-icon live-edit-toolbar-buttons me-1" v-on:click="setPreviewMode('desktop')" data-preview="desktop"
                  :class="[previewMode=='desktop' ? 'live-edit-resolution-active': '']">
                 <svg fill="currentColor" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 96 960 960" width="24"><path d="M320 936v-80h80v-80H160q-33 0-56.5-23.5T80 696V296q0-33 23.5-56.5T160 216h640q33 0 56.5 23.5T880 296v400q0 33-23.5 56.5T800 776H560v80h80v80H320ZM160 696h640V296H160v400Zm0 0V296v400Z"/></svg>
             </span>
 
             <span class="btn btn-icon live-edit-toolbar-buttons" v-on:click="setPreviewMode('phone')" data-preview="phone"
                   :class="[previewMode=='phone' ? 'live-edit-resolution-active': '']">
-              <svg fill="currentColor" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 96 960 960" width="24"><path d="M320 936v-80h80v-80H160q-33 0-56.5-23.5T80 696V296q0-33 23.5-56.5T160 216h640q33 0 56.5 23.5T880 296v400q0 33-23.5 56.5T800 776H560v80h80v80H320ZM160 696h640V296H160v400Zm0 0V296v400Z"/></svg>
-
+                <svg xmlns="http://www.w3.org/2000/svg" width="24px" height="24px" viewBox="0 0 24 24"><g fill="currentColor"><path d="M6 5a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H8a2 2 0 0 1-2-2V5zm10 0H8v14h8V5z"/><path d="M13 17a1 1 0 1 1-2 0a1 1 0 0 1 2 0z"/></g></svg>
             </span>
         </div>
     </nav>
