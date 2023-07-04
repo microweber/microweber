7f04a9af46a29a936c407459b90db1b315bf4504
Peter Ivanov <peter@microweber.com>
2023-07-04 15:11:56 +0300

    update

---
+++ src/MicroweberPackages/LiveEdit/resources/js/ui/components/Toolbar/ContentSearchNav.vue
@@ -0,0 +1,48 @@
+<script>
+export default {
+
+    data() {
+
+    },
+    mounted() {
+        var liveEditSearchContentField = new mw.autoComplete({
+            element: "#mw-live-edit-search-content",
+            placeholder: "Search content",
+            ajaxConfig: {
+                method: 'get',
+                url: mw.settings.api_url + 'get_content_admin?get_extra_data=1&keyword=${val}'
+            },
+            map: {
+                value: 'id',
+                title: 'title',
+                image: 'picture'
+            },
+            selected: [
+                {
+                    id: 0,
+                    title: '',
+                    placeholder: 'All'
+                }
+            ]
+        });
+
+        $(liveEditSearchContentField).on("change", function (e, val) {
+            var urlTogo = false;
+            if (typeof (val[0]) !== 'undefined') {
+                urlTogo = val[0].url;
+                mw.app.canvas.getFrame().src = urlTogo  ;
+            }
+        })
+
+    },
+    methods: {
+
+
+    }
+}
+</script>
+
+
+<template>
+    <div id="mw-live-edit-search-content"></div>
+</template>
