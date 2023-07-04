1f0f9c8ea5fc98357ad0fc3769b167dac64b5fa9
Nikolay <rachkuw@gmail.com>
2023-07-04 12:06:13 +0300

    update

--- src/MicroweberPackages/LiveEdit/resources/js/ui/components/Toolbar/Toolbar.vue
+++ src/MicroweberPackages/LiveEdit/resources/js/ui/components/Toolbar/Toolbar.vue
@@ -4,13 +4,13 @@
 
         <div class="toolbar-nav toolbar-nav-hover">
             <a class="mw-live-edit-toolbar-link mw-live-edit-toolbar-link--arrowed" href="./">
-                Admin
                 <svg class="mw-live-edit-toolbar-arrow-icon" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32">
                     <g fill="none" stroke-width="1.5" stroke-linejoin="round" stroke-miterlimit="10">
                         <circle class="arrow-icon--circle" cx="16" cy="16" r="15.12"></circle>
                         <path class="arrow-icon--arrow" d="M16.14 9.93L22.21 16l-6.07 6.07M8.23 16h13.98"></path>
                     </g>
                 </svg>
+                Admin
             </a>
         </div>
 
