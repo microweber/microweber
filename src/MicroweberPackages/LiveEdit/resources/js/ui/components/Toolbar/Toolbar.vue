ab22942d2511745bbb046b5668c6f3c73b4236ab
Bozhidar Slaveykov <bobi@microweber.com>
2023-07-04 10:55:09 +0300

    update

--- src/MicroweberPackages/LiveEdit/resources/js/ui/components/Toolbar/Toolbar.vue
+++ src/MicroweberPackages/LiveEdit/resources/js/ui/components/Toolbar/Toolbar.vue
@@ -36,6 +36,15 @@
                             <span v-html="menuItem.icon_html"></span>
                             {{ menuItem.title }}
                         </a>
+                        <a onclick="document.body.classList.toggle('theme-dark')">
+                            <span>
+                            <svg fill="currentColor" xmlns="http://www.w3.org/2000/svg" height="36" viewBox="0 96 960 960" width="36">
+                                <path
+                                    d="M480 936q-150 0-255-105T120 576q0-150 105-255t255-105q14 0 27.5 1t26.5 3q-41 29-65.5 75.5T444 396q0 90 63 153t153 63q55 0 101-24.5t75-65.5q2 13 3 26.5t1 27.5q0 150-105 255T480 936Zm0-80q88 0 158-48.5T740 681q-20 5-40 8t-40 3q-123 0-209.5-86.5T364 396q0-20 3-40t8-40q-78 32-126.5 102T200 576q0 116 82 198t198 82Zm-10-270Z"></path>
+                            </svg>
+                            </span>
+                            Dark mode
+                        </a>
                     </nav>
                 </div>
             </div>
--- src/MicroweberPackages/LiveEdit/resources/js/ui/components/Toolbar/UndoRedo.vue
+++ src/MicroweberPackages/LiveEdit/resources/js/ui/components/Toolbar/UndoRedo.vue
@@ -1,10 +1,4 @@
 <template>
-    <button class="btn  btn-icon" onclick="document.body.classList.toggle('theme-dark')">
-        <svg fill="currentColor" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 96 960 960" width="24">
-            <path
-                d="M480 936q-150 0-255-105T120 576q0-150 105-255t255-105q14 0 27.5 1t26.5 3q-41 29-65.5 75.5T444 396q0 90 63 153t153 63q55 0 101-24.5t75-65.5q2 13 3 26.5t1 27.5q0 150-105 255T480 936Zm0-80q88 0 158-48.5T740 681q-20 5-40 8t-40 3q-123 0-209.5-86.5T364 396q0-20 3-40t8-40q-78 32-126.5 102T200 576q0 116 82 198t198 82Zm-10-270Z"></path>
-        </svg>
-    </button>
     <button v-on:click="undo()" class=" btn  btn-icon" id="vue-toolbar-undo" :disabled='undoIsDisabled'>
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" width="24px">
             <path
