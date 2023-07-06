6c5221e5e999c0c8deabc20a4b244567fdf48464
Bozhidar Slaveykov <bobi@microweber.com>
2023-07-06 16:03:30 +0300

    update

--- userfiles/modules/btn/src/resources/views/livewire/settings.blade.php
+++ userfiles/modules/btn/src/resources/views/livewire/settings.blade.php
@@ -1,88 +1,35 @@
-<div>
+<div class="px-2 py-2" x-data="{showEditTab: 'content'}">
+    <div class="d-flex justify-content-between align-items-center mb-4 collapseNav-initialized">
+        <div class="d-flex flex-wrap gap-md-4 gap-3">
+            <a href="#" x-on:click="showEditTab = 'content'" :class="{ 'active': showEditTab == 'content' }"
+               class="btn btn-link text-decoration-none mw-admin-action-links active">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M21 6v2H3V6h18M3 18h9v-2H3v2m0-5h18v-2H3v2Z"/></svg>
+                Content
+            </a>
 
-    <ul class="nav nav-tabs">
-        <li class="nav-item" wire:ignore>
-            <a href="#text" class="nav-link active" data-bs-toggle="tab">Text</a>
-        </li>
-
-        <li class="nav-item" wire:ignore>
-            <a href="#design" class="nav-link" data-bs-toggle="tab">Design</a>
-        </li>
-    </ul>
-    <div class="tab-content">
-        <div class="tab-pane fade show active" wire:ignore.self id="text">
-
-
-            <div>
-                Text:
-                <livewire:microweber-module-option::text optionName="text"  :moduleId="$moduleId" :moduleType="$moduleType"  />
-            </div>
-
-            <div>
-                Url:
-                <livewire:microweber-module-option::text optionName="url"  :moduleId="$moduleId" :moduleType="$moduleType"    />
-            </div>
-
-            <div>
-                Popup content:
-                <livewire:microweber-module-option::text optionName="popupcontent"  :moduleId="$moduleId" :moduleType="$moduleType"    />
-            </div>
-
-
-            <?php
-            /*
-             *    <livewire:microweber-module-option::text optionName="url"  :moduleId="$moduleId" :moduleType="$moduleType" translatable="true"  />
-            <livewire:microweber-module-option::text optionName="button_action"  :moduleId="$moduleId" :moduleType="$moduleType" translatable="true"  />
-            <livewire:microweber-module-option::text optionName="button_onclick"  :moduleId="$moduleId" :moduleType="$moduleType" translatable="true"  />
-            <livewire:microweber-module-option::text optionName="popupcontent"  :moduleId="$moduleId" :moduleType="$moduleType" translatable="true"  />
-            <livewire:microweber-module-option::text optionName="url_blank"  :moduleId="$moduleId" :moduleType="$moduleType" translatable="true"  />
-            <livewire:microweber-module-option::text optionName="icon"  :moduleId="$moduleId" :moduleType="$moduleType" translatable="true"  />
-
-             * */
-            ?>
-
-
-
-
-
-  <?php
-
-  /*
-
-
-
-            <livewire:microweber-module-option::text optionName="url"  moduleId="{{$moduleId}}" moduleType="{{$moduleType}}" translatable="true"  />
-            <livewire:microweber-module-option::text optionName="button_text"  :moduleId="$moduleId" :moduleType="$moduleType" translatable="true"  />
-
-   <livewire:microweber-module-btn::settings-form :moduleId="$moduleId" :moduleType="$moduleType" /> */
-  ?>
+            <a href="#" x-on:click="showEditTab = 'design'" :class="{ 'active': showEditTab == 'design' }"
+               class="btn btn-link text-decoration-none mw-admin-action-links">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M3 17.25V21h3.75L17.81 9.93l-3.75-3.75L3 17.25m19.61 1.11l-4.25 4.25l-5.2-5.2l1.77-1.77l1 1l2.47-2.48l1.42 1.42L18.36 17l1.06 1l1.42-1.4l1.77 1.76m-16-7.53L1.39 5.64l4.25-4.25L7.4 3.16L4.93 5.64L6 6.7l2.46-2.48l1.42 1.42l-1.42 1.41l1 1l-2.85 2.78M20.71 7c.39-.39.39-1 0-1.41l-2.34-2.3c-.37-.39-1.02-.39-1.41 0l-1.84 1.83l3.75 3.75L20.71 7Z"/></svg>
+               Design
+            </a>
 
         </div>
 
-
-        <div class="tab-pane fade" wire:ignore.self id="design">
-
-            <div class="mt-4">
-                <div>
-
-                    <livewire:microweber-live-edit::module-select-template :moduleId="$moduleId"
-                                                                           :moduleType="$moduleType"/>
-                </div>
-
-
-                <div>
-                    <livewire:microweber-module-btn::settings-form-design :moduleId="$moduleId" :moduleType="$moduleType" />
-                </div>
-
-                <div>
-                    <x-microweber-module-btn::btn-align :settings="$settings"/>
-
-                    <x-microweber-ui::icon-picker wire:model="settings.icon" :value="$settings['icon']"/>
-                </div>
-            </div>
-        </div>
     </div>
 
 
-</div>
+    <div x-show="showEditTab=='content'">
+        <div>
+            Text:
+            <livewire:microweber-module-option::text optionName="text"  :moduleId="$moduleId" :moduleType="$moduleType"  />
+        </div>
+        <div>
+            Link:
+            <livewire:microweber-module-option::text optionName="url"  :moduleId="$moduleId" :moduleType="$moduleType"    />
+        </div>
+    </div>
 
+    <div x-show="showEditTab=='design'">
+    </div>
+
+</div>
