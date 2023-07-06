e6b4fab4c596543d82f597b1dd36d92a254e5f32
Peter Ivanov <peter@microweber.com>
2023-07-06 15:56:45 +0300

    update

--- src/MicroweberPackages/LiveEdit/Http/Livewire/ModuleSettingsComponent.php
+++ src/MicroweberPackages/LiveEdit/Http/Livewire/ModuleSettingsComponent.php
@@ -19,29 +19,42 @@ class ModuleSettingsComponent extends AdminComponent
 
     ];
 
-//    public function mount()
-//    {
-//        if ($this->settings) {
-//            foreach ($this->settings as $key => $setting) {
-//                $val = get_module_option($key, $this->moduleId);
-//                $this->settings[$key] = $val;
-//                $this->state['settings'][$key] = $val;
-//            }
-//        }
-//    }
-//
-//
-//
-//    public function saveModuleOption($key, $value, $moduleId = false,$lang = false){
-//        $option = array();
-//        $option['option_value'] = $value;
-//        $option['option_key'] = $key;
-//        $option['option_group'] = $moduleId;
-//        if($lang){
-//            $option['lang'] = $lang;
-//        }
-//        return app()->option_manager->save($option);
-//    }
+    public function mount()
+    {
+        if ($this->settings) {
+            foreach ($this->settings as $key => $setting) {
+                $val = get_module_option($key, $this->moduleId);
+                $this->settings[$key] = $val;
+                $this->state['settings'][$key] = $val;
+            }
+        }
+    }
+
+
+
+    public function updatedSettings($settings)
+    {
+        if ($this->settings) {
+            foreach ($this->settings as $key => $setting) {
+                $this->saveModuleOption($key, $setting, $this->moduleId);
+            }
+        }
+        $this->emit('settingsChanged', ['moduleId' => $this->moduleId, 'settings' => $this->settings]);
+
+    }
+
+
+    public function saveModuleOption($key, $value, $moduleId = false,$lang = false){
+        $option = array();
+        $option['option_value'] = $value;
+        $option['option_key'] = $key;
+        $option['option_group'] = $moduleId;
+        if($lang){
+            $option['lang'] = $lang;
+        }
+        return app()->option_manager->save($option);
+    }
+
 
     public function render()
     {
