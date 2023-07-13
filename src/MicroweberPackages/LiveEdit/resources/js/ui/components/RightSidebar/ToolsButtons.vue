05cce71e118f0eb039e3da84a7fa69ce313d292d
Peter Ivanov <peter@microweber.com>
2023-07-13 16:08:11 +0300

    update

---
+++ src/MicroweberPackages/LiveEdit/resources/js/ui/components/RightSidebar/ToolsButtons.vue
@@ -0,0 +1,10 @@
+<template>
+    <h4>Tools</h4>
+
+    <button v-on:click="show('style-editor')">Open CSS Editor</button>
+    <button v-on:click="show('html-editor')">Open html Editor</button>
+    Open html editor<br>
+    clear cache<br>
+    content revisions<br>
+</template>
+hggggggggghg
