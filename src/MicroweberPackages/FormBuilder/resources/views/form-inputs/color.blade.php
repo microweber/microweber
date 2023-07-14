3707182fa8f2f2bc2646b51f10595a427d183cae
Bozhidar Slaveykov <bobi@microweber.com>
2023-07-14 16:30:14 +0300

    update

--- src/MicroweberPackages/FormBuilder/resources/views/form-inputs/color.blade.php
+++ src/MicroweberPackages/FormBuilder/resources/views/form-inputs/color.blade.php
@@ -17,68 +17,12 @@
 
 <script>
 
-    function getElementPositionInFrames(element) {
-        var x = 0;
-        var y = 0;
-        var currentWindow = window;
-
-        while (currentWindow !== top) {
-            var iframe = currentWindow.frameElement;
-            var iframeRect = iframe.getBoundingClientRect();
-            var elementRect = element.getBoundingClientRect();
-
-            x += iframeRect.left + elementRect.left;
-            y += iframeRect.top + elementRect.top;
-
-            currentWindow = currentWindow.parent;
-        }
-
-        return { x: x, y: y };
-    }
-
-    function getElementPositionOnScreen(element) {
-        var rect = element.getBoundingClientRect();
-
-        var scrollLeft = window.pageXOffset || document.documentElement.scrollLeft;
-        var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-
-        var x = rect.left + scrollLeft;
-        var y = rect.top + scrollTop;
-
-        // Adjust position to stay within the visible screen area
-        var screenWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
-        var screenHeight = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
-
-        x = Math.min(Math.max(x, 0), screenWidth - element.offsetWidth);
-        y = Math.min(Math.max(y, 0), screenHeight - element.offsetHeight);
-
-        return { x: x, y: y };
-    }
-
     $(document).ready(function () {
         let element = document.getElementById('open-color-picker-{{$md5name}}');
         element.addEventListener('click', function () {
 
             let colorPicker = mw.app.colorPicker;
-
-            var position = {};
-            if (self !== top) {
-                position = getElementPositionInFrames(element);
-            } else {
-                position = getElementPositionOnScreen(element);
-            }
-
-            let newPositionX = position.x + 40;
-            let newPositionY = position.y;
-
-            if ((window.top.innerWidth - newPositionX) < 200) {
-                newPositionX = newPositionX - 250 - (window.top.innerWidth - newPositionX);
-            }
-            if ((window.top.innerHeight - newPositionY) < 400) {
-                newPositionY = newPositionY - 400 + (window.top.innerHeight - newPositionY);
-            }
-
-            colorPicker.setPosition(newPositionX, newPositionY);
+            colorPicker.positionToElement(element);
             colorPicker.selectColor('#{{$md5name}}', function(color) {
                 element.style.backgroundColor = color;
             });
--- src/MicroweberPackages/LiveEdit/resources/js/api-core/services/services/color-picker.js
+++ src/MicroweberPackages/LiveEdit/resources/js/api-core/services/services/color-picker.js
@@ -15,6 +15,29 @@ export class ColorPicker extends MicroweberBaseClass {
         };
     }
 
+    positionToElement(targetElementSelector) {
+        var element = $(targetElementSelector)[0];
+
+        var position = {};
+        if (self !== top) {
+            position = this.getElementPositionInFrames(element);
+        } else {
+            position = this.getElementPositionOnScreen(element);
+        }
+
+        let newPositionX = position.x + 40;
+        let newPositionY = position.y;
+
+        if ((window.top.innerWidth - newPositionX) < 200) {
+            newPositionX = newPositionX - 250 - (window.top.innerWidth - newPositionX);
+        }
+        if ((window.top.innerHeight - newPositionY) < 400) {
+            newPositionY = newPositionY - 400 + (window.top.innerHeight - newPositionY);
+        }
+
+        this.setPosition(newPositionX, newPositionY); 
+    }
+
     selectColor(targetElementSelector, callback = false) {
 
         if (this.colorPickerInstances.length > 0) {
@@ -71,4 +94,41 @@ export class ColorPicker extends MicroweberBaseClass {
         return result;
     }
 
+    getElementPositionInFrames(element) {
+        var x = 0;
+        var y = 0;
+        var currentWindow = window;
+
+        while (currentWindow !== top) {
+            var iframe = currentWindow.frameElement;
+            var iframeRect = iframe.getBoundingClientRect();
+            var elementRect = element.getBoundingClientRect();
+
+            x += iframeRect.left + elementRect.left;
+            y += iframeRect.top + elementRect.top;
+
+            currentWindow = currentWindow.parent;
+        }
+
+        return { x: x, y: y };
+    }
+
+    getElementPositionOnScreen(element) {
+        var rect = element.getBoundingClientRect();
+
+        var scrollLeft = window.pageXOffset || document.documentElement.scrollLeft;
+        var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+        var x = rect.left + scrollLeft;
+        var y = rect.top + scrollTop;
+
+        // Adjust position to stay within the visible screen area
+        var screenWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+        var screenHeight = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+
+        x = Math.min(Math.max(x, 0), screenWidth - element.offsetWidth);
+        y = Math.min(Math.max(y, 0), screenHeight - element.offsetHeight);
+
+        return { x: x, y: y };
+    }
 }
