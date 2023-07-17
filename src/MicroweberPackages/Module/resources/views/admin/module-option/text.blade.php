b44e8ea21277017c26327a0645ae3f01424fbc1f
Nikolay <rachkuw@gmail.com>
2023-07-17 17:06:59 +0300

    update

--- src/MicroweberPackages/Module/resources/views/admin/module-option/text.blade.php
+++ src/MicroweberPackages/Module/resources/views/admin/module-option/text.blade.php
@@ -1,3 +1,5 @@
-<div>
-    <input type="text" class="form-control form-control-live-edit" wire:model.debounce.100ms="state.settings.{{ $this->optionName }}"/>
-</div>
+<label class="form-control-live-edit-label">
+    <input type="text" class="form-control-live-edit-input" wire:model.debounce.100ms="state.settings.{{ $this->optionName }}"/>
+
+    <span class="form-control-live-edit-bottom-effect"></span>
+</label>
