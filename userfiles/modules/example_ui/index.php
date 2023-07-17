4c5398029e1ecd7d15e5d4a93b3f577e63b7b535
Bozhidar Slaveykov <bobi@microweber.com>
2023-07-17 17:08:03 +0300

    update

---
+++ userfiles/modules/example_ui/index.php
@@ -0,0 +1,4 @@
+<div style="padding:30px;background:#a88d0e;color:#fff">
+    Here is an example ui of Microweber.
+    Click to edit the module to see ui.
+</div>
---
+++ userfiles/modules/example_ui/src/resources/views/livewire/settings.blade.php
@@ -0,0 +1,10 @@
+<div>
+    <div>
+        Text
+        <livewire:microweber-module-option::text optionName="text" :moduleId="$moduleId" :moduleType="$moduleType"  />
+    </div>
+    <div class="mt-4 mb-3">
+        <label class="form-label">Link</label>
+        <livewire:microweber-module-option::link-picker optionName="link" :moduleId="$moduleId" :moduleType="$moduleType"  />
+    </div>
+</div>
