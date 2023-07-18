dce28e5db63213269aa2ebe7522b84ccc522276b
Nikolay <rachkuw@gmail.com>
2023-07-18 14:52:10 +0300

    Update textarea.blade.php

--- src/MicroweberPackages/MicroweberUI/resources/views/components/textarea.blade.php
+++ src/MicroweberPackages/MicroweberUI/resources/views/components/textarea.blade.php
@@ -1,7 +1,9 @@
 @props(['disabled' => false])
 
-<div>
-    <textarea {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge([
+<label class="form-control-live-edit-label-wrapper">
 
-]) !!} ></textarea>
-</div>
+    <textarea class=" form-control-live-edit-input" rows="5" cols="50" {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge([]) !!} ></textarea>
+
+    <span class="form-control-live-edit-bottom-effect bottom-effect-textarea"></span>
+
+</label>
