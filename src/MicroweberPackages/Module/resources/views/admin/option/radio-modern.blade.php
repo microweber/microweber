27464d55ed007f00263d9681836f2b0b68670874
Bozhidar Slaveykov <bobi@microweber.com>
2023-07-19 18:18:26 +0300

    update

--- src/MicroweberPackages/Module/resources/views/admin/option/radio-modern.blade.php
+++ src/MicroweberPackages/Module/resources/views/admin/option/radio-modern.blade.php
@@ -1,5 +1,8 @@
 
 
 <div>
-    <x-microweber-ui::radio-modern wire:model.debounce.100ms="state.settings.{{ $this->optionKey }}" :options="$radioModernOptions" />
+    <x-microweber-ui::radio-modern
+        wire:model.debounce.100ms="state.settings.{{ $this->optionKey }}"
+        selectedOption="{{$this->state['settings'][$this->optionKey]}}"
+        :options="$radioModernOptions" />
 </div>
