c6fd0aeae2e74fb4339719ebbba26b11bf3e5e4d
Bozhidar Slaveykov <bobi@microweber.com>
2023-07-19 18:21:36 +0300

    update

--- src/MicroweberPackages/Module/resources/views/admin/option/range-slider.blade.php
+++ src/MicroweberPackages/Module/resources/views/admin/option/range-slider.blade.php
@@ -1,3 +1,8 @@
 <div>
-    <x-microweber-ui::range-slider wire:model.debounce.100ms="state.settings.{{ $this->optionKey }}" />
+    <x-microweber-ui::range-slider
+
+        wire:model.debounce.100ms="state.settings.{{ $this->optionKey }}"
+        selectedRange="{{$this->state['settings'][$this->optionKey]}}"
+
+    />
 </div>
