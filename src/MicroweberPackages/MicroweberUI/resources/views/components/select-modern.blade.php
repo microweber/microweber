06c43c0c49d44c5575b7fdb2b09ad431a9b54744
Nikolay <rachkuw@gmail.com>
2023-07-19 12:29:05 +0300

    update

--- src/MicroweberPackages/MicroweberUI/resources/views/components/select-modern.blade.php
+++ src/MicroweberPackages/MicroweberUI/resources/views/components/select-modern.blade.php
@@ -24,21 +24,21 @@
     <span class="form-control-live-edit-bottom-effect"></span>
 
 
-    <div class="dropdown-menu" :class="[openOptions ? 'show':'']">
+    <div class="dropdown-menu form-control-live-edit-input ps-0" style="width: 95%;" :class="[openOptions ? 'show':'']">
 
         @if(!empty($preformatedOptions))
             @foreach($preformatedOptions as $option)
 
                 <button type="button"
-                        x-on:click="selectedOption = {{json_encode($option)}}; openOptions = false" :class="[selectedOption.key == '{{$option['key']}}' ? 'active':'']" class="dropdown-item">
+                        x-on:click="selectedOption = {{json_encode($option)}}; openOptions = false" :class="[selectedOption.key == '{{$option['key']}}' ? 'active':'']" class="dropdown-item tblr-body-color">
                     {!! $option['value'] !!}
-                    <span x-show="selectedOption.key == '{{$option['key']}}'">checked</span>
+                    <span x-show="selectedOption.key == '{{$option['key']}}'">
+                        <svg class="ms-3" fill="currentColor" xmlns="http://www.w3.org/2000/svg" height="16" viewBox="0 -960 960 960" width="16"><path d="M378-246 154-470l43-43 181 181 384-384 43 43-427 427Z"/></svg>
+                    </span>
                 </button>
 
             @endforeach
         @endif
-
     </div>
-
 
 </div>
