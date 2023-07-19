5c280b7c9acd2732579c59197f542ae172bc9426
Bozhidar Slaveykov <bobi@microweber.com>
2023-07-19 11:04:27 +0300

    Update radio-modern.blade.php

--- src/MicroweberPackages/MicroweberUI/resources/views/components/radio-modern.blade.php
+++ src/MicroweberPackages/MicroweberUI/resources/views/components/radio-modern.blade.php
@@ -1,13 +1,22 @@
 @props(['options'=>[]])
 
-<div class="form-control-live-edit-label-wrapper d-flex mw-live-edit-resolutions-wrapper" style="max-height: unset; gap: 5px !important;">
+@php
+    $selectedOption = '';
+    if (isset($options)) {
+        $selectedOption = key($options);
+    }
+@endphp
+
+<div
+    x-data="{selectedOption: '{{$selectedOption}}'}"
+    class="form-control-live-edit-label-wrapper d-flex mw-live-edit-resolutions-wrapper" style="max-height: unset; gap: 5px !important;">
 
 
     @if(!empty($options))
         @foreach($options as $key => $option)
 
-            <button type="button" class="btn btn-icon live-edit-toolbar-buttons w-100 active">
-                {{$option}}
+            <button type="button" x-on:click="selectedOption = '{{$key}}'" :class="[selectedOption == '{{$key}}' ? 'active':'']" class="btn btn-icon live-edit-toolbar-buttons w-100">
+              {{$option}}
             </button>
 
         @endforeach
