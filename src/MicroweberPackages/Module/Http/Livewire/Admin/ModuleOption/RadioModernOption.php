47b39d142c7ec356f07616a724620b029595ccc7
Bozhidar Slaveykov <bobi@microweber.com>
2023-07-19 11:20:29 +0300

    update

---
+++ src/MicroweberPackages/Module/Http/Livewire/Admin/ModuleOption/RadioModernOption.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace MicroweberPackages\Module\Http\Livewire\Admin\ModuleOption;
+
+class RadioModernOption extends OptionElement
+{
+    public $radioModernOptions = [];
+
+    public string $view = 'module::admin.module-option.radio-modern';
+    // public string $viewTranslatable = 'module::admin.module-option.text-multilanguage';
+
+}
