f99f30ff054d7ecdf661102d9162bd854240f254
Nikolay <rachkuw@gmail.com>
2023-07-19 14:12:11 +0300

    update

--- src/MicroweberPackages/MicroweberUI/resources/views/components/select-modern.blade.php
+++ src/MicroweberPackages/MicroweberUI/resources/views/components/select-modern.blade.php
@@ -21,10 +21,8 @@
             x-on:click="openOptions = !openOptions" x-html="selectedOption.value">
     </button>
 
-    <span class="form-control-live-edit-bottom-effect"></span>
 
-
-    <div class="dropdown-menu form-control-live-edit-input ps-0" style="width: 95%;" :class="[openOptions ? 'show':'']">
+    <div class="dropdown-menu form-control-live-edit-input ps-0" :class="[openOptions ? 'show':'']">
 
         @if(!empty($preformatedOptions))
             @foreach($preformatedOptions as $option)
@@ -32,8 +30,8 @@
                 <button type="button"
                         x-on:click="selectedOption = {{json_encode($option)}}; openOptions = false" :class="[selectedOption.key == '{{$option['key']}}' ? 'active':'']" class="dropdown-item tblr-body-color">
                     {!! $option['value'] !!}
-                    <span x-show="selectedOption.key == '{{$option['key']}}'">
-                        <svg class="ms-3" fill="currentColor" xmlns="http://www.w3.org/2000/svg" height="16" viewBox="0 -960 960 960" width="16"><path d="M378-246 154-470l43-43 181 181 384-384 43 43-427 427Z"/></svg>
+                    <span class="ms-auto" x-show="selectedOption.key == '{{$option['key']}}'">
+                        <svg fill="currentColor" xmlns="http://www.w3.org/2000/svg" height="16" viewBox="0 -960 960 960" width="16"><path d="M378-246 154-470l43-43 181 181 384-384 43 43-427 427Z"/></svg>
                     </span>
                 </button>
 
