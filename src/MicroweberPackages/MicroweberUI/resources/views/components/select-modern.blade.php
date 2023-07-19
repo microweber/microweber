962af3751acb3c0db85ca981eb20841bcd12ed1a
Nikolay <rachkuw@gmail.com>
2023-07-19 12:14:00 +0300

    update

--- src/MicroweberPackages/MicroweberUI/resources/views/components/select-modern.blade.php
+++ src/MicroweberPackages/MicroweberUI/resources/views/components/select-modern.blade.php
@@ -17,9 +17,12 @@
 
 <div class="form-control-live-edit-label-wrapper" x-data="{selectedOption: {{json_encode($selectedOption)}}, openOptions:false}">
 
-    <button type="button" class="btn btn-dark"
+    <button type="button" class="form-select form-control-live-edit-input"
             x-on:click="openOptions = !openOptions" x-html="selectedOption.value">
     </button>
+
+    <span class="form-control-live-edit-bottom-effect"></span>
+
 
     <div class="dropdown-menu" :class="[openOptions ? 'show':'']">
 
@@ -29,10 +32,13 @@
                 <button type="button"
                         x-on:click="selectedOption = {{json_encode($option)}}; openOptions = false" :class="[selectedOption == '{{$option['key']}}' ? 'active':'']" class="dropdown-item">
                     {!! $option['value'] !!}
+                    <span x-show="selectedOption == {{json_encode($option)}}">checked</span>
                 </button>
 
             @endforeach
         @endif
 
     </div>
+
+
 </div>
