ed8539b657d8dbd77a63acb90cf8a4642924ec36
Bozhidar Slaveykov <bobi@microweber.com>
2023-07-20 15:14:57 +0300

    update

--- src/MicroweberPackages/Module/resources/views/admin/option/radio.blade.php
+++ src/MicroweberPackages/Module/resources/views/admin/option/radio.blade.php
@@ -1,3 +1,8 @@
+
+
 <div>
-    <x-microweber-ui::radio :options="$radioOptions" />
+    <x-microweber-ui::radio
+        wire:model.debounce.100ms="state.settings.{{ $this->optionKey }}"
+        selectedOption="{{$this->state['settings'][$this->optionKey]}}"
+        :options="$radioOptions" />
 </div>
