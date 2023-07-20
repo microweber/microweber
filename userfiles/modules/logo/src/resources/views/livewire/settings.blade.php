bde0e75a70fae5c3e2c65ed24fa04acdc9afabf2
Bozhidar Slaveykov <bobi@microweber.com>
2023-07-20 17:38:58 +0300

    Update settings.blade.php

--- userfiles/modules/logo/src/resources/views/livewire/settings.blade.php
+++ userfiles/modules/logo/src/resources/views/livewire/settings.blade.php
@@ -3,14 +3,18 @@
     <div class="card">
         <div x-data="{}" class="card-body" style="padding:5px">
 
-            <div class="form-group">
-                <label class="form-label font-weight-bold"><?php _e("Main Logo"); ?></label>
-                <small class="text-muted d-block mb-2"><?php _e("This logo image will appear every time"); ?></small>
+            <div @mw-option-saved.window="function() {
+                if ($event.detail.optionKey === 'logoimage') {
+                    mw.top().reload_module_everywhere('{{$moduleType}}');
+                }
+                }" >
+
             </div>
 
             <div>
                 <label class="live-edit-label"><?php _e("Main Logo"); ?></label>
-                <livewire:microweber-option::media-picker label="<?php _e("Add Logo"); ?>" optionKey="logoimage" :optionGroup="$moduleId" :module="$moduleType"  />
+                <small class="text-muted d-block mb-2"><?php _e("This logo image will appear every time"); ?></small>
+                <livewire:microweber-option::media-picker optionKey="logoimage" :optionGroup="$moduleId" :module="$moduleType"  />
             </div>
 
         </div>
