efa8a4ed773677e33e38926f9f2b8834875d3633
Peter Ivanov <peter@microweber.com>
2023-07-20 17:44:22 +0300

    update

--- userfiles/modules/accordion/src/Http/Livewire/AccordeonSettingsComponent.php
+++ userfiles/modules/accordion/src/Http/Livewire/AccordeonSettingsComponent.php
@@ -6,6 +6,10 @@ use MicroweberPackages\LiveEdit\Http\Livewire\ModuleSettingsComponent;
 
 class AccordeonSettingsComponent extends ModuleSettingsComponent
 {
+    public $listeners = [
+        'refreshComponent' => '$refresh',
+     ];
+
     public function render()
     {
         return view('microweber-module-accordion::livewire.settings');
--- userfiles/modules/accordion/src/resources/views/livewire/settings.blade.php
+++ userfiles/modules/accordion/src/resources/views/livewire/settings.blade.php
@@ -2,7 +2,13 @@
 showMainEditTab: 'mainSettings'
 }">
 
+
     <?php
+
+    $use_content_from_live_edit = get_option('use_content_from_live_edit', $moduleId);
+
+
+
     $moduleTemplates = module_templates($moduleType);
 
     $editorSettings = [
@@ -32,17 +38,33 @@ showMainEditTab: 'mainSettings'
                 'label' => 'Icon',
                 'placeholder' => 'Icon',
                 'name' => 'icon',
-            ],
-            [
-                'type' => 'info',
-                'label' => 'info',
-                'placeholder' => 'info',
-                'name' => 'info',
-                'help' => 'Use the live edit to drag and drop image, video or something else directly on created accordions.',
             ]
+
         ]
     ];
+
+
+
+    if($use_content_from_live_edit == 1) {
+        $editorSettings['schema'][] = [
+            'type' => 'textarea',
+            'label' => 'Content',
+            'name' => 'content',
+            'placeholder' => 'Enter content',
+            'help' => 'Enter content',
+        ];
+    } else {
+        $editorSettings['schema'][] = [
+            'type' => 'info',
+            'label' => 'info',
+            'placeholder' => 'info',
+            'name' => 'info',
+            'help' => 'Use the live edit to drag and drop image, video or something else directly on created accordions.',
+        ];
+    }
+
     ?>
+
 
 
 
@@ -72,14 +94,41 @@ showMainEditTab: 'mainSettings'
     </div>
 
 
-    @if($moduleTemplates && count($moduleTemplates) >  1)
+
 
         <div x-show="showMainEditTab=='design'" x-transition:enter="tab-pane-slide-right-active">
 
             <div>
+                <div @mw-option-saved.window="function() {
+                        if ($event.detail.optionKey === 'use_content_from_live_edit') {
+                            var use_content_from_live_edit = $event.detail.optionValue;
+                               window.Livewire.emit('refreshComponent');
+                        }
+                    }" class="d-flex col-xl-3 col-md-6 col-12">
+
+                    <livewire:microweber-option::toggle value="1" optionKey="use_content_from_live_edit" :optionGroup="$moduleId" :module="$moduleType" />
+                    <div>
+                        <label class="form-check-label d-flex align-items-center" >
+
+                            @lang('Use tabs content from live edit')
+
+                        </label>
+
+                    </div>
+                </div>
+
+
+
+
+
+
+
+
+                @if($moduleTemplates && count($moduleTemplates) >  1)
                 <livewire:microweber-live-edit::module-select-template :moduleId="$moduleId" :moduleType="$moduleType"/>
+                @endif
             </div>
         </div>
 
-    @endif
+
 </div>
