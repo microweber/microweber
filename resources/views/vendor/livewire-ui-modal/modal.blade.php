ec25eabd0ed1f53e41f6042d265140a627b60058
Bozhidar Slaveykov <bobi@microweber.com>
2023-07-21 17:39:55 +0300

    update

--- resources/views/vendor/livewire-ui-modal/modal.blade.php
+++ resources/views/vendor/livewire-ui-modal/modal.blade.php
@@ -1,5 +1,36 @@
 <div>
+    <style>
+        #js-modal-livewire-ui {
+
+        }
+    </style>
+    <script>
+        let mwDialogLivewire = false;
+
+        Livewire.on('activeModalComponentChanged', (id) => {
+
+            mwDialogLivewire = mw.top().dialog({
+                content: document.getElementById('js-modal-livewire-ui'),
+                width: 900,
+                overlay: true,
+                overlayClose: true,
+                draggableHandle: '#js-modal-livewire-ui-draggable-handle',
+            });
+            mwDialogLivewire.dialogHeader.style.display = 'none';
+            mwDialogLivewire.dialogContainer.style.padding = '0px';
+
+            let modalLivewireUiClose = document.getElementById('js-modal-livewire-ui-close');
+            if (modalLivewireUiClose) {
+                modalLivewireUiClose.addEventListener('click', function () {
+                    mwDialogLivewire.remove();
+                });
+            }
+
+           // document.getElementById('js-modal-livewire-ui').style.display = 'block';
+        });
+    </script>
     <div id="js-modal-livewire-ui">
+
         @forelse($components as $id => $component)
             <div wire:key="{{ $id }}">
                 @livewire($component['name'], $component['attributes'], key($id))
