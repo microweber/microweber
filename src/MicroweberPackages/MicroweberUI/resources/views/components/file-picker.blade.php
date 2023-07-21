8d78f93d494d7ef95abe553e41c44185e80ac9f8
Nikolay <rachkuw@gmail.com>
2023-07-21 10:25:55 +0300

    Update file-picker.blade.php

--- src/MicroweberPackages/MicroweberUI/resources/views/components/file-picker.blade.php
+++ src/MicroweberPackages/MicroweberUI/resources/views/components/file-picker.blade.php
@@ -20,7 +20,7 @@
                     </x-microweber-ui::button>
 
                     <x-microweber-ui::button-action class="js-remove-image-{{$randId}}" type="button">
-                        <svg class="text-danger" fill="currentColor" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0V0z" fill="none"></path><path d="M16 9v10H8V9h8m-1.5-6h-5l-1 1H5v2h14V4h-3.5l-1-1zM18 7H6v12c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7z"></path></svg>
+                        <svg class="text-danger mb-3" fill="currentColor" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0V0z" fill="none"></path><path d="M16 9v10H8V9h8m-1.5-6h-5l-1 1H5v2h14V4h-3.5l-1-1zM18 7H6v12c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7z"></path></svg>
                     </x-microweber-ui::button-action>
                 </div>
             </div>
