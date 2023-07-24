0fb3291cdbd8a98894158bc9451260e4628fcb6a
Bozhidar Slaveykov <bobi@microweber.com>
2023-07-24 16:00:41 +0300

    Update font-picker-modal.blade.php

--- src/MicroweberPackages/MicroweberUI/resources/views/livewire/modals/font-picker-modal.blade.php
+++ src/MicroweberPackages/MicroweberUI/resources/views/livewire/modals/font-picker-modal.blade.php
@@ -46,7 +46,7 @@
                                 </span>
                                </button>
                            </div>
-                            <div class="mr-3" wire:click="favorite('{{$font['family']}}')">
+                            <div class="pr-3" wire:click="favorite('{{$font['family']}}')">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M17.562 21.56a1 1 0 0 1-.465-.116L12 18.764l-5.097 2.68a1 1 0 0 1-1.45-1.053l.973-5.676l-4.124-4.02a1 1 0 0 1 .554-1.705l5.699-.828l2.549-5.164a1.04 1.04 0 0 1 1.793 0l2.548 5.164l5.699.828a1 1 0 0 1 .554 1.705l-4.124 4.02l.974 5.676a1 1 0 0 1-.985 1.169Z"/></svg>
                             </div>
                         </div>
