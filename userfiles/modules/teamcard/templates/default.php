582ae25c46696c66dd7f7eecb2412532bb1201fa
Nikolay <rachkuw@gmail.com>
2023-07-24 14:35:52 +0300

    Update default.php

--- userfiles/modules/teamcard/templates/default.php
+++ userfiles/modules/teamcard/templates/default.php
@@ -1,56 +1,41 @@
+<style>
+    .team-card-item-image {
+        height: 120px;
+        width: 120px;
+        background-size: cover;
+        background-repeat: no-repeat;
+        background-position: center;
+    }
+
+    .team-card-item-bio {
+        line-height: normal;
+    }
+</style>
+
 <div class="team-card-holder">
-    <style scoped="scoped">
-        .team-card-item {
-            width: 33.333%;
-            margin: 0;
-            display: block;
-            background-size: cover;
-            float: left;
-            overflow: hidden;
-            text-align: center;
-            margin-bottom: 20px;
-        }
-
-        .team-card-item-image {
-            display: block;
-            height: 120px;
-            width: 120px;
-            margin: auto;
-            background-size: cover;
-            background-repeat: no-repeat;
-            background-position: center;
-            border-radius: 120px;
-        }
-
-        .team-card-item-name {
-            display: block;
-            font-size: 21px;
-            text-align: center;
-            padding: 10px 15px;
-        }
-
-    </style>
     <?php
     $count = 0;
+
     if (isset($data) AND $data) {
         foreach ($data as $slide) {
             $count++;
             ?>
-            <div class="team-card-item">
+            <div class="team-card-item col-lg-4 col-md-6 col-12 d-block text-center mb-3 overflow-hidden float-start px-md-4 my-5">
                 <?php if ($slide['file']) { ?>
-                    <span class="team-card-item-image" style="background-image: url('<?php print thumbnail($slide['file'], 200); ?>');"></span>
+                    <div class="team-card-item-image m-auto rounded-circle" style="background-image: url('<?php print thumbnail($slide['file'], 200); ?>');"></div>
 
                 <?php } else { ?>
-                <span class="team-card-item-image">
+                <div class="m-auto rounded-circle">
 
                         <img  width="120" height="120" src="<?php print modules_url() ?>teamcard/templates/default-image.svg"/>
-                </span>
+                </div>
                 <?php } ?>
 
 
-                <span class="team-card-item-name"><?php print array_get($slide, 'name'); ?></span>
-                <span class="team-card-item-position"> <?php print array_get($slide, 'role'); ?></span>
-                <span class="team-card-item-bio"> <?php print array_get($slide, 'bio'); ?></span>
+                <div class="team-card-item-name p-3"><?php print array_get($slide, 'name'); ?></div>
+                <div class="team-card-item-position"> <?php print array_get($slide, 'role'); ?></div>
+                <div class="team-card-item-website p-3"> <?php print array_get($slide, 'website'); ?></div>
+                <div class="team-card-item-bio"> <?php print array_get($slide, 'bio'); ?></div>
             </div>
         <?php }
     } ?>
