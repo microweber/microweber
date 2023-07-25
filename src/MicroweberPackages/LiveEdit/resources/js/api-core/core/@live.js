4cd99150bfa943ac0aedd95cfdf4d00df4afcc13
alex <raikov@ymail.com>
2023-07-25 09:47:41 +0300

    liveedit

--- src/MicroweberPackages/LiveEdit/resources/js/api-core/core/@live.js
+++ src/MicroweberPackages/LiveEdit/resources/js/api-core/core/@live.js
@@ -462,13 +462,8 @@ export class LiveEdit {
                 return true;
             } else {
 
-            return (
-                rect.top >= 0 &&
-                rect.left >= 0 &&
-                rect.bottom <= (win.innerHeight || doc.documentElement.clientHeight) &&
-                rect.right <= (win.innerWidth || doc.documentElement.clientWidth)
-        
-            );
+                return  false
+             }
         }
 
 
