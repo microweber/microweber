e99cbb6ab4e0bb8617bf9d6bf78934e55e887bb9
Nikolay <rachkuw@gmail.com>
2023-07-26 16:54:43 +0300

    update

--- userfiles/modules/shop/cart/templates/mw_default.php
+++ userfiles/modules/shop/cart/templates/mw_default.php
@@ -2,7 +2,7 @@
     <div class="checkout-modal-products-wrapper">
         <?php if (is_array($data) and $data) : ?>
             <?php foreach ($data as $item) : ?>
-                    <div class="form-row checkout-modal-product-list-item ">
+                    <div class="form-row checkout-modal-product-list-item row">
                         <div class="col-auto">
                             <?php if (isset($item['item_image']) and $item['item_image'] != false): ?>
                                 <?php $p = $item['item_image']; ?>
@@ -14,21 +14,18 @@
                             <?php endif; ?>
                         </div>
                         <div class="col">
-                            <div class="form-row h-100">
-                                <div class="col-10">
-                                    <div class="form-row align-items-md-center h-100">
-                                        <div class="col-12 col-md-5">
-                                            <div><?php _e($item['title']) ?></div>
-                                        </div>
-                                        <div class="col-6 col-md-4 align-self-center justify-content-md-center">
-                                            <?php print currency_format($item['price']); ?>
-                                        </div>
-                                        <div class="col-6 col-md-3 align-self-center justify-content-md-center mw-qty-field">
-                                            <input min=0 type="number" class="form-control input-sm " name="qty" value="<?php print $item['qty'] ?>"  onchange="mw.cart.qty('<?php print $item['id'] ?>', this.value)" style="width: 70px;"/>
-                                        </div>
-                                    </div>
+                            <div class="d-flex w-100 align-items-center">
+                                <div class="col">
+                                    <span><?php _e($item['title']) ?></span>
                                 </div>
-                                <div class="col-2 justify-content-center align-self-center">
+                                <div class="col-auto col-md-4 align-self-center justify-content-md-center">
+                                    <?php print currency_format($item['price']); ?>
+                                </div>
+                                <div class="col-auto col-md-3 align-self-center justify-content-md-center mw-qty-field">
+                                    <input min=0 type="number" class="form-control input-sm " name="qty" value="<?php print $item['qty'] ?>"  onchange="mw.cart.qty('<?php print $item['id'] ?>', this.value)" style="width: 70px;"/>
+                                </div>
+
+                                <div class="col-auto justify-content-center align-self-center">
                                     <a data-bs-toggle="tooltip" title="<?php _e("Remove"); ?>" href="javascript:mw.cart.remove('<?php print $item['id'] ?>');"><i class="mdi mdi-delete text-danger d-flex justify-content-center justify-content-md-end" style="font-size: x-large"></i></a>
                                 </div>
                             </div>
