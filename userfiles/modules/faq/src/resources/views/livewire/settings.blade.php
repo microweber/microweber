9d32c65f8b14e044c8d4e823ed4223288512a4f4
Bozhidar Slaveykov <bobi@microweber.com>
2023-07-26 11:57:39 +0300

    Update settings.blade.php

--- userfiles/modules/faq/src/resources/views/livewire/settings.blade.php
+++ userfiles/modules/faq/src/resources/views/livewire/settings.blade.php
@@ -1,37 +1,77 @@
-<div>
-    <div x-data="{
-    showEditTab: 'content'
-    }">
+<div x-data="{
+showMainEditTab: 'mainSettings'
+}">
 
+    <?php
+    $moduleTemplates = module_templates($moduleType);
+
+    $editorSettings = [
+        'config' => [
+            'title' => '',
+            'addButtonText' => 'Add Item',
+            'editButtonText' => 'Edit',
+            'deleteButtonText' => 'Delete',
+            'sortItems' => true,
+            'settingsKey' => 'settings',
+            'listColumns' => [
+                'question' => 'Question',
+            ],
+        ],
+        'schema' => [
+            [
+                'type' => 'text',
+                'label' => 'Question',
+                'name' => 'question',
+                'placeholder' => 'Enter question',
+                'help' => 'Enter Question',
+            ],
+            [
+                'type' => 'textarea',
+                'label' => 'Answer',
+                'name' => 'answer',
+                'placeholder' => 'Enter answer',
+                'help' => 'Enter Answer',
+                'maxlength' => '150'
+            ]
+        ]
+    ];
+
+    ?>
+
+
+    @if($moduleTemplates && count($moduleTemplates) >  1)
         <div class="d-flex justify-content-between align-items-center collapseNav-initialized">
             <div class="d-flex flex-wrap gap-md-4 gap-3">
-                <a href="#" x-on:click="showEditTab = 'content'" :class="{ 'active': showEditTab == 'content' }"
-                   class="btn btn-link text-decoration-none mw-admin-action-links mw-adm-liveedit-tabs active">
-                    Content
-                </a>
-                <a href="#" x-on:click="showEditTab = 'settings'" :class="{ 'active': showEditTab == 'settings' }"
-                   class="btn btn-link text-decoration-none mw-admin-action-links mw-adm-liveedit-tabs">
-                    Settings
-                </a>
+                <button x-on:click="showMainEditTab = 'mainSettings'"
+                        :class="{ 'active': showMainEditTab == 'mainSettings' }"
+                        class="btn btn-link text-decoration-none mw-admin-action-links mw-adm-liveedit-tabs active">
+                    @lang('Main settings')
+                </button>
+                <button x-on:click="showMainEditTab = 'design'" :class="{ 'active': showMainEditTab == 'design' }"
+                        class="btn btn-link text-decoration-none mw-admin-action-links mw-adm-liveedit-tabs">
+                    @lang('Design')
+                </button>
             </div>
         </div>
+    @endif
 
-        <div x-show="showEditTab=='content'">
-            <div>
-                <label class="live-edit-label">{{__('Title')}} </label>
-                <livewire:microweber-option::text optionKey="title" :optionGroup="$moduleId" :module="$moduleType"  />
-            </div>
+    <div x-show="showMainEditTab=='mainSettings'" x-transition:enter="tab-pane-slide-left-active">
 
-             <div>
-                  <label class="live-edit-label">{{__('Description')}} </label>
-                  <livewire:microweber-option::textarea optionKey="description" :optionGroup="$moduleId" :module="$moduleType"  />
-              </div>
-
-        </div>
-        <div x-show="showEditTab=='settings'">
-            This is the settings tab
-        </div>
+        <livewire:microweber-live-edit::module-items-editor :moduleId="$moduleId"
+                                                            :moduleType="$moduleType"
+                                                            :editorSettings="$editorSettings"/>
 
     </div>
 
+
+    @if($moduleTemplates && count($moduleTemplates) >  1)
+
+        <div x-show="showMainEditTab=='design'" x-transition:enter="tab-pane-slide-right-active">
+
+            <div>
+                <livewire:microweber-live-edit::module-select-template :moduleId="$moduleId" :moduleType="$moduleType"/>
+            </div>
+        </div>
+
+    @endif
 </div>
