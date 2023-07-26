60e74bd2caef8af8ad8d89154f62539af6b926fb
Nikolay <rachkuw@gmail.com>
2023-07-26 11:59:00 +0300

    Update action-links-top.blade.php

--- src/MicroweberPackages/Order/resources/views/admin/orders/action-links-top.blade.php
+++ src/MicroweberPackages/Order/resources/views/admin/orders/action-links-top.blade.php
@@ -3,7 +3,7 @@
         {{_e('Abandoned Carts')}}
     </a>
     <a href="javascript:mw_admin_add_order_popup()" class="btn btn-dark me-2">
-        <svg fill="currentColor" class="me-2" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 96 960 960" width="24"><path d="M446.667 856V609.333H200v-66.666h246.667V296h66.666v246.667H760v66.666H513.333V856h-66.666Z"/></svg>
-        {{_e("New Order")}}
+        <svg fill="currentColor" class="me-sm-2" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 96 960 960" width="24"><path d="M446.667 856V609.333H200v-66.666h246.667V296h66.666v246.667H760v66.666H513.333V856h-66.666Z"/></svg>
+        <span class="d-sm-block d-none">{{_e("New Order")}}</span>
     </a>
 </div>
