a1fb25353875629ca0316a9ed00b07f4f1d85c26
Bozhidar Slaveykov <bobi@microweber.com>
2023-07-27 14:34:28 +0300

    update

--- userfiles/modules/admin/import_export_tool/src/resources/views/admin/livewire-new-import-modal.blade.php
+++ userfiles/modules/admin/import_export_tool/src/resources/views/admin/livewire-new-import-modal.blade.php
@@ -1,16 +1,23 @@
-<div><form wire:submit.prevent="save">
-    <div class="modal-header">
-        <h5 class="modal-title">Add new feed import</h5>
-        <button type="button" class="btn btn-link" wire:click="$emit('closeModal')">Close</button>
-    </div>
-    <div class="modal-body">
-        <div>
-            Feed Name <br/>
-            <input type="text" class="form-control" wire:model="new_feed_name">
+<div>
+    <form wire:submit.prevent="save">
+        <div class="modal" tabindex="-1" role="dialog">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title">Add new feed import</h5>
+                        <button type="button" class="btn btn-link" wire:click="$emit('closeModal')">Close</button>
+                    </div>
+                    <div class="modal-body">
+                        <div>
+                            Feed Name <br/>
+                            <input type="text" class="form-control" wire:model="new_feed_name">
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="submit" class="btn btn-primary">Save</button>
+                    </div>
+                </div>
+            </div>
         </div>
-    </div>
-    <div class="modal-footer">
-        <button type="submit" class="btn btn-primary">Save</button>
-    </div>
-</form>
+    </form>
 </div>
