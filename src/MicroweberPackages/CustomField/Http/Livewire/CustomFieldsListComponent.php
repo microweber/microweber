ccea730faf460e3693841d68f702e50a8a66e783
Bozhidar Slaveykov <bobi@microweber.com>
2023-07-31 13:10:04 +0300

    Update CustomFieldsListComponent.php

--- src/MicroweberPackages/CustomField/Http/Livewire/CustomFieldsListComponent.php
+++ src/MicroweberPackages/CustomField/Http/Livewire/CustomFieldsListComponent.php
@@ -10,6 +10,17 @@ class CustomFieldsListComponent extends AdminComponent
     public $contentId;
     public $contentType;
 
+    public $listeners = [
+        'executeCustomFieldDelete' => 'executeCustomFieldDelete',
+    ];
+
+    public function executeCustomFieldDelete($id) {
+        $findCustomField = CustomField::where('id', $id)->first();
+        if ($findCustomField) {
+            $findCustomField->delete();
+        }
+    }
+
     public function render()
     {
         $getCustomFields = CustomField::where('rel_type', 'content')
