83e6cc858a5df47fa287bfdba7027e8048e0c8cf
Bozhidar Slaveykov <bobi@microweber.com>
2023-08-04 11:55:17 +0300

    update

--- src/MicroweberPackages/CustomField/Http/Livewire/CustomFieldEditModalComponent.php
+++ src/MicroweberPackages/CustomField/Http/Livewire/CustomFieldEditModalComponent.php
@@ -26,33 +26,10 @@ class CustomFieldEditModalComponent extends AdminModalComponent
 
     public function updatedState()
     {
-        $getCustomField = CustomField::where('id', $this->customFieldId)->first();
-        if ($getCustomField) {
+        $save = mw()->fields_manager->save($this->state);
 
-            if ($getCustomField->type != $this->state['type']) {
-                CustomFieldValue::where('custom_field_id', $this->customFieldId)->delete();
-            }
-
-            $getCustomField->name = $this->state['name'];
-            $getCustomField->options = $this->state['options'];
-            $getCustomField->type = $this->state['type'];
-            $getCustomField->required = $this->state['required'];
-            $getCustomField->error_text = $this->state['error_text'];
-            $getCustomField->show_label = $this->state['show_label'];
-            $getCustomField->placeholder = $this->state['placeholder'];
-
-            if (isset($this->state['value'])) {
-                if (is_string($this->state['value'])) {
-                    $getCustomField->value = $this->state['value'];
-                }
-            }
-
-            $getCustomField->save();
-
-            $this->showSettings($getCustomField->type);
-
-            $this->emit('customFieldUpdated');
-        }
+        $this->showSettings($this->state['type']);
+        $this->emit('customFieldUpdated');
     }
 
     public function showSettings($type)
--- src/MicroweberPackages/CustomField/Http/Livewire/CustomFieldValuesEditComponent.php
+++ src/MicroweberPackages/CustomField/Http/Livewire/CustomFieldValuesEditComponent.php
@@ -49,65 +49,49 @@ class CustomFieldValuesEditComponent extends AdminComponent
 
     }
 
-    public function remove($i)
+    public function remove($id)
     {
 //        if (count($this->inputs) == 1) {
 //            $this->addError('inputs.'.$i, 'You must have at least one input.');
 //            return;
 //        }
 
-        if (isset($this->inputs[$i])) {
-            $findCustomFieldValue = CustomFieldValue::where('custom_field_id', $this->customFieldId)
-                ->where('value', $this->inputs[$i])
-                ->first();
-            if ($findCustomFieldValue) {
-                $findCustomFieldValue->delete();
-            }
+      //  dd($id);
 
-            unset($this->inputs[$i]);
-
-            $this->emit('customFieldUpdated');
-        }
+//        if (isset($this->inputs[$i])) {
+//            $findCustomFieldValue = CustomFieldValue::where('custom_field_id', $this->customFieldId)
+//                ->where('value', $this->inputs[$i])
+//                ->first();
+//            if ($findCustomFieldValue) {
+//                $findCustomFieldValue->delete();
+//            }
+//
+//            //unset($this->inputs[$i]);
+//
+//           // $this->emit('customFieldUpdated');
+//        }
     }
 
     public function mount($customFieldId)
     {
         $this->customFieldId = $customFieldId;
 
-        $getCustomField = CustomField::where('id', $this->customFieldId)->first();
-        foreach($getCustomField->fieldValue as $fieldValue) {
+        $this->customField = CustomField::where('id', $this->customFieldId)->first();
+        foreach($this->customField->fieldValue as $fieldValue) {
           $this->inputs[$fieldValue->id] = $fieldValue->value;
         }
-
-//        if($getCustomField->type == 'checkbox' || $getCustomField->type == 'dropdown' || $getCustomField->type == 'radio') {
-//            if (empty($this->inputs)) {
-//                $values = CustomFieldsHelper::generateFieldNameValues($getCustomField->type);
-//                if (!empty($values)) {
-//                    foreach ($values as $value) {
-//                        $customFieldValue = new CustomFieldValue();
-//                        $customFieldValue->custom_field_id = $getCustomField->id;
-//                        $customFieldValue->value = $value;
-//                        $customFieldValue->save();
-//                    }
-//                }
-//            }
-//        }
-
-        $this->customField = $getCustomField;
 
     }
 
     public function updatedInputs()
     {
         if (!empty($this->inputs)) {
-          $getCustomField = CustomField::where('id', $this->customFieldId)->first();
-          $getCustomField->fieldValue()->delete();
 
-            foreach($this->inputs as $input) {
-                $getCustomField->fieldValue()->create([
-                    'value' => $input
-                ]);
-            }
+            $values = array_values($this->inputs);
+
+            $this->state['value'] = $values;
+
+            mw()->fields_manager->save($this->state);
 
             $this->emit('customFieldUpdated');
         }
@@ -117,9 +101,7 @@ class CustomFieldValuesEditComponent extends AdminComponent
     {
         if (isset($this->state['value'])) {
 
-            $getCustomField = CustomField::where('id', $this->customFieldId)->first();
-            $getCustomField->value = $this->state['value'];
-            $getCustomField->save();
+            mw()->fields_manager->save($this->state);
 
             $this->emit('customFieldUpdated');
         }
@@ -128,17 +110,10 @@ class CustomFieldValuesEditComponent extends AdminComponent
     public function render()
     {
         $this->customField = CustomField::where('id', $this->customFieldId)->first();
-//        if ($this->customField->fieldValue->count() == 0) {
-//            $values = CustomFieldsHelper::generateFieldNameValues($this->customField->type);
-//            if (!empty($values)) {
-//                foreach ($values as $value) {
-//                    $customFieldValue = new CustomFieldValue();
-//                    $customFieldValue->custom_field_id = $this->customField->id;
-//                    $customFieldValue->value = $value;
-//                    $customFieldValue->save();
-//                }
-//            }
-//        }
+        $this->state['type'] = $this->customField->type;
+        $this->state['rel_type'] = $this->customField->rel_type;
+        $this->state['rel_id'] = $this->customField->rel_id;
+        $this->state['id'] = $this->customField->id;
 
         return view('custom_field::livewire.custom-field-values-edit-component');
     }
