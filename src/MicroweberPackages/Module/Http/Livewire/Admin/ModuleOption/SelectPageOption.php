e73deaa25b762eb12fcc98662e09a83d85e7bfd5
Bozhidar Slaveykov <bobi@microweber.com>
2023-08-18 15:34:58 +0300

    update

--- src/MicroweberPackages/Module/Http/Livewire/Admin/ModuleOption/SelectPageOption.php
+++ src/MicroweberPackages/Module/Http/Livewire/Admin/ModuleOption/SelectPageOption.php
@@ -2,8 +2,46 @@
 
 namespace MicroweberPackages\Module\Http\Livewire\Admin\ModuleOption;
 
+use MicroweberPackages\Page\Models\Page;
+
 class SelectPageOption extends OptionElement
 {
+    public $search;
+    public $pagesTree = [];
+
     public string $view = 'module::admin.option.select-page';
+
+    public function updatedSearch()
+    {
+        $this->renderPagesTree();
+    }
+
+    public function selectPage($pageId)
+    {
+        $this->state['settings'][$this->optionKey] = $pageId;
+        $this->updated();
+    }
+
+    public function renderPagesTree()
+    {
+        $getPageQuery = Page::query();
+
+        if ($this->search) {
+            $getPageQuery->where('title', 'LIKE', '%' . $this->search . '%');
+        }
+
+        $getPages = $getPageQuery->get();
+
+        $this->pagesTree = [];
+        foreach ($getPages as $page) {
+            $this->pagesTree[$page->id] = $page->title;
+        }
+    }
+
+    public function mount()
+    {
+        parent::mount();
+        $this->renderPagesTree();
+    }
 
 }
