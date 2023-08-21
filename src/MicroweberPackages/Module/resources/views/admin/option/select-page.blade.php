e9c5eac0cd62bdac0fee031dc4fbd5a297268d7f
Bozhidar Slaveykov <bobi@microweber.com>
2023-08-21 17:23:40 +0300

    Update select-page.blade.php

--- src/MicroweberPackages/Module/resources/views/admin/option/select-page.blade.php
+++ src/MicroweberPackages/Module/resources/views/admin/option/select-page.blade.php
@@ -6,24 +6,17 @@
         $selectedOption = $this->state['settings'][$this->optionKey];
     }
 
-    $selectedPageTree = [
-        'id' => '',
-        'name' => 'Select page'
-    ];
+    $selectedPageName = '';
     if (isset($pagesTree[$selectedOption])) {
-        $selectedPageTree = [
-            'id' => $selectedOption,
-            'name' => $pagesTree[$selectedOption]
-        ];
+        $selectedPageName = $pagesTree[$selectedOption];
     }
-    $selectedPageTree = json_encode($selectedPageTree);
 
 @endphp
 
-<div class="form-control-live-edit-label-wrapper" x-data="{selectedPage: {{$selectedPageTree}}, openOptions:false}">
+<div class="form-control-live-edit-label-wrapper" x-data="{openOptions:false}">
 
-    <button type="button" class="form-select form-control-live-edit-input"
-            x-on:click="openOptions = !openOptions" x-html="selectedPage.name">
+    <button type="button" class="form-select form-control-live-edit-input" x-on:click="openOptions = !openOptions">
+        {{$selectedPageName}}
     </button>
 
     <div class="dropdown-menu form-control-live-edit-input ps-0" style="max-height:300px;overflow-y: scroll" :class="[openOptions ? 'show':'']">
@@ -36,11 +29,15 @@
             @foreach($pagesTree as $pageId=>$pageName)
                 <button type="button"
                         wire:click="selectPage({{$pageId}})"
-                        x-on:click="selectedPage = {{json_encode(['id'=>$pageId, 'name'=>$pageName])}}; openOptions = false" :class="[selectedPage.id == '{{$pageId}}' ? 'active':'']" class="dropdown-item tblr-body-color">
+                        x-on:click="openOptions = false" class="dropdown-item tblr-body-color">
                     {!! $pageName !!}
-                    <span class="ms-auto" x-show="selectedPage.id == '{{$pageId}}'">
+
+                    @if ($pageId == $selectedOption)
+                    <span class="ms-auto">
                         <svg fill="currentColor" xmlns="http://www.w3.org/2000/svg" height="16" viewBox="0 -960 960 960" width="16"><path d="M378-246 154-470l43-43 181 181 384-384 43 43-427 427Z"/></svg>
                     </span>
+                    @endif
+
                 </button>
             @endforeach
         @endif
