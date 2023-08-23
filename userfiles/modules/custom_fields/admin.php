d8dcebd47644cfffa00f84ad1bd72712dbcb0923
Bozhidar Slaveykov <bobi@microweber.com>
2023-08-23 14:48:04 +0300

    Update admin.php

--- userfiles/modules/custom_fields/admin.php
+++ userfiles/modules/custom_fields/admin.php
@@ -1,6 +1,29 @@
 <?php
 must_have_access();
-echo view('microweber-module-custom-fields::admin-module',
-    ['params'=>$params]
-);
 ?>
+
+<div>
+
+    <script>
+        function reloadParentModule() {
+            if (typeof mw !== 'undefined' && mw.top().app && mw.top().app.editor) {
+                mw.top().app.editor.dispatch('onModuleSettingsChanged', ({'moduleId': '<?php print $params['id']  ?>'} || {}))
+                alert('maiko'); 
+            }
+            alert('<?php print $params['id']  ?>');
+        }
+
+        Livewire.on('executeCustomFieldDelete', (e) => {
+            reloadParentModule();
+        });
+        Livewire.on('customFieldAdded', (e) => {
+            reloadParentModule();
+        });
+    </script>
+
+    <?php
+    echo view('microweber-module-custom-fields::admin-module',
+        ['params'=>$params]
+    );
+    ?>
+</div>
