c7f2fa1af33c7d243805cf6a9a75929a003ec26a
Bozhidar Slaveykov <bobi@microweber.com>
2023-08-25 14:52:10 +0300

    update

--- userfiles/modules/testimonials/src/Http/Livewire/TestimonialsSettingsComponent.php
+++ userfiles/modules/testimonials/src/Http/Livewire/TestimonialsSettingsComponent.php
@@ -5,8 +5,72 @@ use MicroweberPackages\LiveEdit\Http\Livewire\ModuleSettingsComponent;
 
 class TestimonialsSettingsComponent extends ModuleSettingsComponent
 {
+    public $editorSettings = [];
+
     public function render()
     {
+        $this->editorSettings = $this->getEditorSettings();
+
        return view('microweber-module-testimonials::livewire.settings');
     }
+
+
+    public function getEditorSettings()
+    {
+
+        $editorSettings = [
+            'config' => [
+                'title' => '',
+                'addButtonText' => 'Add Testimonial',
+                'editButtonText' => 'Edit',
+                'deleteButtonText' => 'Delete',
+                'sortItems' => true,
+                'settingsKey' => 'settings',
+                'listColumns' => [
+                    'name' => 'name',
+                ],
+            ],
+            'schema' => [
+                [
+                    'type' => 'text',
+                    'rules' => 'required|min:2|max:255',
+                    'label' => 'Name',
+                    'name' => 'name',
+                    'placeholder' => 'Name',
+                    'help' => 'Name is required'
+                ],
+                [
+                  'type' => 'textarea',
+                    'rules' => 'required|min:2|max:255',
+                    'label' => 'Content',
+                    'name' => 'content',
+                    'placeholder' => 'Content',
+                    'help' => 'Content is required'
+                ],
+                [
+                    'type' => 'text',
+                    'rules' => 'required|min:2|max:255',
+                    'label' => 'Read more URL',
+                    'name' => 'read_more_url',
+                    'placeholder' => 'Read more URL',
+                    'help' => 'Read more URL is required'
+                ],
+
+            ]
+        ];
+
+//        'id' => "integer",
+//        'name' => "text",
+//        'content' => "text",
+//        'read_more_url' => "text",
+//        'created_on' => "datetime",
+//		'project_name' => "text",
+//	    'client_company' => "text",
+//		'client_role' => "text",
+//		'client_picture' => "text",
+//		'client_website' => "text",
+//        'position' => "integer"
+        return $editorSettings;
+    }
+
 }
--- userfiles/modules/testimonials/src/resources/views/livewire/settings.blade.php
+++ userfiles/modules/testimonials/src/resources/views/livewire/settings.blade.php
@@ -1,13 +1,13 @@
 <div>
     <div x-data="{
-    showEditTab: 'content'
+    showEditTab: 'testimonials'
     }">
 
         <div class="d-flex justify-content-between align-items-center collapseNav-initialized form-control-live-edit-label-wrapper">
             <div class="d-flex flex-wrap gap-md-4 gap-3">
-                <a href="#" x-on:click="showEditTab = 'content'" :class="{ 'active': showEditTab == 'content' }"
+                <a href="#" x-on:click="showEditTab = 'testimonials'" :class="{ 'active': showEditTab == 'testimonials' }"
                    class="btn btn-link text-decoration-none mw-admin-action-links mw-adm-liveedit-tabs active">
-                    Content
+                    Testimonials
                 </a>
                 <a href="#" x-on:click="showEditTab = 'settings'" :class="{ 'active': showEditTab == 'settings' }"
                    class="btn btn-link text-decoration-none mw-admin-action-links mw-adm-liveedit-tabs">
@@ -16,18 +16,34 @@
             </div>
         </div>
 
-        <div x-show="showEditTab=='content'">
-            <div>
-                <label class="live-edit-label">{{__('Title')}} </label>
-                <livewire:microweber-option::text optionKey="title" :optionGroup="$moduleId" :module="$moduleType"  />
+        <div x-show="showEditTab=='testimonials'">
+
+            <div class="mt-2">
+                <x-microweber-ui::button-animation type="button" class="mt-2" x-on:click="showEditTab = 'addNewTestimonial'">
+                    @lang('Add new testimonial')
+                </x-microweber-ui::button-animation>
             </div>
 
-             <div>
-                  <label class="live-edit-label">{{__('Description')}} </label>
-                  <livewire:microweber-option::textarea optionKey="description" :optionGroup="$moduleId" :module="$moduleType"  />
-              </div>
+            @include('microweber-live-edit::module-items-editor-list-items')
 
         </div>
+
+        <div x-show="showEditTab=='addNewTestimonial'">
+            <form wire:submit.prevent="submit">
+
+                <div class="d-flex align-items-center justify-content-between">
+                    <x-microweber-ui::button-animation type="submit">
+                        @lang('Save')
+                    </x-microweber-ui::button-animation>
+                </div>
+
+                @if (isset($editorSettings['schema']))
+                    @include('microweber-live-edit::module-items-editor-edit-item-schema-render')
+                @endif
+
+            </form>
+        </div>
+
         <div x-show="showEditTab=='settings'">
             This is the settings tab
         </div>
