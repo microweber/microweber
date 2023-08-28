b0e57db9d001b5e705cd0ee8223209b108a92235
Bozhidar Slaveykov <bobi@microweber.com>
2023-08-28 17:35:47 +0300

    update

--- userfiles/modules/video/index.php
+++ userfiles/modules/video/index.php
@@ -17,19 +17,15 @@ $code = trim($code);
 $enable_full_page_cache = get_option('enable_full_page_cache','website');
 
 $lazyload = get_option('lazyload', $params['id']);
-
-$lazyload = ((!empty($lazyload) && $lazyload == 'y')? true : false);
-
 $thumb = get_option('upload_thumb', $params['id']);
 
 $use_thumbnail = (!empty(trim($thumb))? true : false);
 
 $show_video_settings_btn = false;
 
-$autoplay = get_option('autoplay', $params['id']) == 'y';
+$autoplay = get_option('autoplay', $params['id']);
 
 $w = get_option('width', $params['id']);
-
 $h = get_option('height', $params['id']);
 
 if ($w == false) {
@@ -43,19 +39,11 @@ if ($h == false) {
         $h = intval($params['height']);
     }
 }
-if ($autoplay == false) {
-    if (isset($params['autoplay'])) {
-        $autoplay = intval($params['autoplay']);
-    }
-}
 if ($w == '') {
     $w = '100%';
 }
 if ($h == '') {
     $h = '350px';
-}
-if ($autoplay == '') {
-    $autoplay = '0';
 }
 if($upload and !$code){
     $prior = 2;
@@ -65,7 +53,7 @@ $video = new \Microweber\Modules\Video\VideoEmbed();
 $video->setId($params['id']);
 $video->setLazyLoad($lazyload);
 $video->setAutoplay($autoplay);
-$video->setThumbnail($thumb);
+$video->setThumbnail($thumb); 
 
 if ($w !== '100%') {
     $video->setWidth($w . 'px');
@@ -84,10 +72,7 @@ if ($upload && !$code) {
     $video->setPlayUploadedVideo(true);
 }
 
-
-
 $code = $video->render();
-
 $provider = $video->getProvider();
 
 $module_template = get_option('data-template', $params['id']);
