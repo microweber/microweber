97a958e96c8c6a3182d2f95eb982f8edc5120cde
Peter Ivanov <peter@microweber.com>
2023-09-01 14:05:20 +0300

    update

--- src/MicroweberPackages/Tag/tests/TagsTest.php
+++ src/MicroweberPackages/Tag/tests/TagsTest.php
@@ -29,8 +29,8 @@ class TagsTest extends TestCase
         $saved_id = intval($saved_id);
 
 
+      //  $c = get_content_by_id($saved_id);
         DB::enableQueryLog();
-        $c = get_content_by_id($saved_id);
         /*
 
         The  "find" method maybe have a bug where the whole table is selected,
