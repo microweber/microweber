6bc83526d0818cfd582b7b6d79a408b08bb826c9
Bozhidar Slaveykov <bobi@microweber.com>
2023-09-01 11:45:53 +0300

    Update CustomerFilter.php

--- src/MicroweberPackages/Customer/Models/ModelFilters/CustomerFilter.php
+++ src/MicroweberPackages/Customer/Models/ModelFilters/CustomerFilter.php
@@ -22,15 +22,10 @@ class CustomerFilter extends ModelFilter
         $model = $this->getModel();
         $table = $model->getTable();
 
-        if (is_numeric($keyword)) {
-            $this->query->where('id', intval($keyword));
-            $this->query->orWhere('phone', intval($keyword));
-        } else {
-            $this->query->where($table.'.first_name', 'LIKE', '%' . $keyword . '%');
-            $this->query->orWhere($table.'.last_name', 'LIKE', '%' . $keyword . '%');
-            $this->query->orWhere($table.'.email', 'LIKE', '%' . $keyword . '%');
-            $this->query->orWhere($table.'.phone', 'LIKE', '%' . $keyword . '%');
-        }
+        $this->query->where($table.'.first_name', 'LIKE', '%' . $keyword . '%');
+        $this->query->orWhere($table.'.last_name', 'LIKE', '%' . $keyword . '%');
+        $this->query->orWhere($table.'.email', 'LIKE', '%' . $keyword . '%');
+        $this->query->orWhere($table.'.phone', 'LIKE', '%' . $keyword . '%');
 
         return $this->query;
     }
