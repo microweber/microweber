fdaf3934e7564716124fc7479fcb64151e0eb251
Peter Ivanov <peter@microweber.com>
2023-09-08 14:24:11 +0300

    update

--- userfiles/modules/spacer/index.php
+++ userfiles/modules/spacer/index.php
@@ -1,1 +1,18 @@
-<div class="mw-le-spacer noelement nodrop no-settings inaccessibleModule" contenteditable="false" id="spacer-item-<?php print $params['id'] ?>"></div>
+<?php
+
+$styles_attr = '';
+$height = '';
+
+if(isset($params['height'])){
+    $height = $params['height'];
+}
+
+$styles = [];
+if($height){
+    $styles[] = 'height: '.$height.'px';
+}
+if(!empty($styles)) {
+    $styles_attr = 'style="' . implode(';', $styles) . '"';
+}
+?>
+<div class="mw-le-spacer noelement nodrop no-settings inaccessibleModule" contenteditable="false" <?php print $styles_attr ?> id="spacer-item-<?php print $params['id'] ?>"></div>
