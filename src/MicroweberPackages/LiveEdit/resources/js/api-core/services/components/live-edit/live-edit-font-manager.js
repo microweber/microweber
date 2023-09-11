903d6bdcdad0d643d3ef84a5756ff4751d14456b
Bozhidar Slaveykov <bobi@microweber.com>
2023-09-11 17:48:21 +0300

    Update live-edit-font-manager.js

--- src/MicroweberPackages/LiveEdit/resources/js/api-core/services/components/live-edit/live-edit-font-manager.js
+++ src/MicroweberPackages/LiveEdit/resources/js/api-core/services/components/live-edit/live-edit-font-manager.js
@@ -1,9 +1,17 @@
 import BaseComponent from "../../containers/base-class";
 
 export class LiveEditFontManager extends BaseComponent {
+
     constructor() {
         super();
 
+        this.fonts = [
+            'Arial',
+            'Tahoma',
+            'Verdana',
+            'Times New Roman',
+            'Georgia',
+        ];
 
         mw.app.on('onLiveEditReady', event =>{
             this.init();
@@ -12,12 +20,14 @@ export class LiveEditFontManager extends BaseComponent {
         mw.app.on('liveEditCanvasLoaded', event =>{
             this.init();
         });
-
-
     }
 
-    init(){
-mw.log('LiveEditFontManager')
+    init() {
+        mw.log('LiveEditFontManager');
+    }
+
+    getFonts() {
+        return this.fonts;
     }
 }
 
