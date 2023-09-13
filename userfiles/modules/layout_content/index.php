d58bb44bab3a7425ddb0ec290a6ce2a7d5c26c7d
Bozhidar Slaveykov <bobi@microweber.com>
2023-09-13 16:26:19 +0300

    update

--- userfiles/modules/layout_content/index.php
+++ userfiles/modules/layout_content/index.php
@@ -1,33 +1,42 @@
 <?php
 
+$moduleName = 'layout_content';
+
 $getContents = get_module_option('contents', $params['id']);
 $contents = json_decode($getContents, true);
 
 if (empty($contents)) {
-    $isDefaultContentApplied = get_option('default_content_is_applied', $params['id']);
-    if (!$isDefaultContentApplied) {
-        $defaultContentFile = dirname(__FILE__) . DS . 'default_content.json';
-        if (is_file($defaultContentFile)) {
-            $checkForDefaultContent = file_get_contents($defaultContentFile);
-            if ($checkForDefaultContent) {
-                $defaultContentData = json_decode($checkForDefaultContent, true);
-                if (!empty($defaultContentData) && is_array($defaultContentData)) {
+    $isDefaultSettingsApplied = get_option('default_settings_is_applied', $params['id']);
+    if (!$isDefaultSettingsApplied) {
+        $defaultSettingsFile = dirname(__FILE__) . DS . 'default_settings.json';
+        if (is_file($defaultSettingsFile)) {
+            $checkForDefaultSettings = file_get_contents($defaultSettingsFile);
+            if ($checkForDefaultSettings) {
+                $defaultSettings = json_decode($checkForDefaultSettings, true);
+                if (!empty($defaultSettings) && is_array($defaultSettings)) {
+
+                    foreach ($defaultSettings as $defaultSettingOptionKey => $defaultSettingOptionValue) {
+                        if ($defaultSettingOptionKey == 'contents') {
+                            continue;
+                        }
+                        save_option([
+                            'option_value' => $defaultSettingOptionValue,
+                            'option_key' => $defaultSettingOptionKey,
+                            'option_group' => $params['id'],
+                            'module'=> $moduleName
+                        ]);
+                    }
+
                     $defaultContentReady = array();
                     $defaultContentDataI = 0;
-                    foreach ($defaultContentData as $key => $value) {
+                    foreach ($defaultSettings['contents'] as $key => $value) {
                         $value['itemId'] = 'mw-module-'.$params['id'].'-'.$defaultContentDataI.time();
                         $defaultContentReady[$key] = $value;
                         $defaultContentDataI++;
                     }
 
                     save_option('contents', json_encode($defaultContentReady), $params['id']);
-                    save_option('default_content_is_applied', true, $params['id']);
-
-                    save_option('title', 'Your story online', $params['id']);
-                    save_option('description', 'The way you tell your story online can make all the difference', $params['id']);
-
-                    save_option('align', 'center', $params['id']);
-                    save_option('max_columns', 3, $params['id']);
+                    save_option('default_settings_is_applied', true, $params['id']);
 
                     $getDefaultContents = get_module_option('contents', $params['id']);
                     $contents = json_decode($getDefaultContents, true);
@@ -40,8 +49,13 @@ if (empty($contents)) {
 $title = get_module_option('title', $params['id']);
 $description = get_module_option('description', $params['id']);
 $align = get_module_option('align', $params['id']);
+if (!$align) {
+    $align = 'center';
+}
 $max_columns = get_module_option('max_columns', $params['id']);
-
+if (!$max_columns) {
+    $max_columns = 3;
+}
 
 if(!$contents){
     $contents = array();
