246852c5525b04836ee03d2f2692c167b8aaca75
Bozhidar Slaveykov <bobi@microweber.com>
2023-09-13 15:03:13 +0300

    update

--- userfiles/modules/layout_content/templates/default.php
+++ userfiles/modules/layout_content/templates/default.php
@@ -15,23 +15,25 @@ description: default
     $count = 0;
 
     if (isset($data) AND $data) {
-        foreach ($data as $slide) {
+        foreach ($data as $content) {
             $count++;
             ?>
             <div class="col-md-4">
-                <?php if ($slide['image']) { ?>
-                    <img src="<?php print thumbnail($slide['image'], 200); ?>" />
+                <?php if ($content['image']) { ?>
+                    <img src="<?php echo thumbnail($content['image'], 600, 400, true); ?>" />
                 <?php } else { ?>
-                    <img src="<?php print modules_url() ?>layout_content/templates/default-image.png" />
+                    <img src="<?php echo modules_url() ?>layout_content/templates/default-image.png" />
                 <?php } ?>
-
-
-                <div class="py-4 fs-4" ><?php print array_get($slide, 'title'); ?></div>
-                <br />
-                <div class="pt-3 italic"> <?php print array_get($slide, 'description'); ?></div>
-                <br />
-                <a href="<?php print $slide['button_link']; ?>" class="btn btn-primary" target="_blank">
-                    <?php print array_get($slide, 'button_text'); ?>
+                <div class="mt-2">
+                    <h4>
+                        <?php echo $content['title']; ?>
+                    </h4>
+                    <p>
+                        <?php echo $content['description']; ?>
+                    </p>
+                </div>
+                <a href="<?php echo $content['button_link']; ?>" class="btn btn-primary" target="_blank">
+                    <?php echo $content['button_text']; ?>
                 </a>
             </div>
         <?php }
