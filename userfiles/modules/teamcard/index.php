d86137e7a693d78a74dd3d63680750165e5b4ea8
Bozhidar Slaveykov <bobi@microweber.com>
2023-09-14 12:17:20 +0300

    add default content for team card

--- userfiles/modules/teamcard/index.php
+++ userfiles/modules/teamcard/index.php
@@ -1,9 +1,20 @@
 <?php
 
-
-use MicroweberPackages\Option\Models\ModuleOption;
-
 $settings = get_module_option('settings', $params['id']);
+if (empty($settings)) {
+
+    $newModuleDefaultSettingsApplied = new \MicroweberPackages\Module\ModuleDefaultSettingsApplier();
+    $newModuleDefaultSettingsApplied->moduleName = 'teamcard';
+    $newModuleDefaultSettingsApplied->modulePath = __DIR__;
+    $newModuleDefaultSettingsApplied->moduleId = $params['id'];
+
+    $applied = $newModuleDefaultSettingsApplied->apply();
+
+    if (isset($applied['success']) && $applied['success']) {
+        $settings = get_module_option('settings', $params['id']);
+    }
+}
+
 
 $defaults = array(
     'name' => 'Name',
