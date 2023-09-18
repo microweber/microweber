9fa13097bf081fde8e5853fee9035e1728b3acce
Nikolay <rachkuw@gmail.com>
2023-09-18 14:46:57 +0300

    Update default.php

--- userfiles/modules/pagination/templates/default.php
+++ userfiles/modules/pagination/templates/default.php
@@ -10,16 +10,17 @@ description: Default
 
 
     <?php
+
     foreach ($pagination_links as $pagination_link):
         ?>
 
         <?php if ($pagination_link['attributes']['current']): ?>
-        <li class="page-item active">
-            <a class="page-link">
-                <?php echo $pagination_link['title']; ?>
-            </a>
-        </li>
-    <?php else: ?>
+            <li class="page-item active">
+                <a class="page-link">
+                    <?php echo $pagination_link['title']; ?>
+                </a>
+            </li>
+        <?php else: ?>
 
         <li class="page-item">
             <a class="page-link" href="<?php echo $pagination_link['attributes']['href']; ?>">
