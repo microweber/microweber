afb56511ca7cd615819dde0550fd98af7a68db70
Bozhidar Slaveykov <bobi@microweber.com>
2023-09-19 17:47:45 +0300

    Update simple-text-editor.blade.php

--- src/MicroweberPackages/MicroweberUI/resources/views/components/simple-text-editor.blade.php
+++ src/MicroweberPackages/MicroweberUI/resources/views/components/simple-text-editor.blade.php
@@ -8,45 +8,47 @@
      <textarea {!! $attributes->merge([]) !!} id="editor-{{$editorId}}"></textarea>
  </div>
 
-    <script>
-        mw.require('editor.js');
-        $(document).ready(function () {
-            let mwEditorId{{$editorId}} = mw.Editor({
-                selector: '#editor-{{$editorId}}',
-                mode: 'div',
-                smallEditor: false,
-                minHeight: 250,
-                maxHeight: '70vh',
-                controls: [
-                    [
-                        'undoRedo', '|',
-                        {
-                            group: {
-                                controller: 'bold',
-                                controls: ['italic', 'underline', 'strikeThrough']
-                            }
-                        },
-                        '|',
-                        {
-                            group: {
-                                icon: 'mdi mdi-format-align-left',
-                                controls: ['align']
-                            }
-                        },
-                        '|', 'format',
-                        '|', 'link', 'unlink', 'removeFormat'
-                    ],
-                ]
-            });
+   <div wire:ignore wire:key="wire-scripts-key-{{$editorId}}">
+       <script>
+           mw.require('editor.js');
+           document.addEventListener('livewire:load', function () {
+               let mwEditorId{{$editorId}} = mw.Editor({
+                   selector: '#editor-{{$editorId}}',
+                   mode: 'div',
+                   smallEditor: false,
+                   minHeight: 250,
+                   maxHeight: '70vh',
+                   controls: [
+                       [
+                           'undoRedo', '|',
+                           {
+                               group: {
+                                   controller: 'bold',
+                                   controls: ['italic', 'underline', 'strikeThrough']
+                               }
+                           },
+                           '|',
+                           {
+                               group: {
+                                   icon: 'mdi mdi-format-align-left',
+                                   controls: ['align']
+                               }
+                           },
+                           '|', 'format',
+                           '|', 'link', 'unlink', 'removeFormat'
+                       ],
+                   ]
+               });
 
-            $(mwEditorId{{$editorId}}).on('change', function (e, val) {
+               $(mwEditorId{{$editorId}}).on('change', function (e, val) {
 
-                let target{{$editorId}} = document.getElementById('editor-{{$editorId}}');
+                   let target{{$editorId}} = document.getElementById('editor-{{$editorId}}');
 
-                var event{{$editorId}} = new Event('input');
-                target{{$editorId}}.dispatchEvent(event{{$editorId}});
+                   var event{{$editorId}} = new Event('input');
+                   target{{$editorId}}.dispatchEvent(event{{$editorId}});
 
-            });
-        });
-    </script>
+               });
+           });
+       </script>
+   </div>
 </div>
