7aca659d20303a58583d944c192c918cd827fde6
Bozhidar Slaveykov <bobi@microweber.com>
2023-09-20 18:07:19 +0300

    update

---
+++ userfiles/modules/slider/src/Providers/SliderServiceProvider.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace MicroweberPackages\Modules\Slider\Providers;
+
+use Livewire\Livewire;
+use MicroweberPackages\Module\Facades\ModuleAdmin;
+use Spatie\LaravelPackageTools\Package;
+use Spatie\LaravelPackageTools\PackageServiceProvider;
+use MicroweberPackages\Modules\Slider\Http\Livewire\SliderSettingsComponent;
+
+class SliderServiceProvider extends PackageServiceProvider
+{
+    public function configurePackage(Package $package): void
+    {
+        $package->name('microweber-module-slider');
+        $package->hasViews('microweber-module-slider');
+    }
+
+    public function register(): void
+    {
+        parent::register();
+        Livewire::component('microweber-module-slider::settings', SliderSettingsComponent::class);
+        ModuleAdmin::registerSettings('slider', 'microweber-module-slider::settings');
+
+    }
+
+}
---
+++ userfiles/modules/slider/src/resources/views/livewire/settings.blade.php
@@ -0,0 +1,107 @@
+<div x-data="{
+showMainEditTab: 'mainSettings'
+}">
+
+    <?php
+    $moduleTemplates = module_templates($moduleType);
+
+    $editorSettings = [
+        'config' => [
+            'title' => '',
+            'addButtonText' => 'Add Slide',
+            'editButtonText' => 'Edit',
+            'deleteButtonText' => 'Delete',
+            'sortItems' => true,
+            'settingsKey' => 'settings',
+            'listColumns' => [
+                'primaryText' => 'Slide Heading',
+            ],
+        ],
+        'schema' => [
+            [
+                'type' => 'image',
+                'label' => 'Image',
+                'name' => 'images',
+                'placeholder' => 'Image',
+                'help' => 'Image',
+            ],
+            [
+                'type' => 'text',
+                'label' => 'Slide Title',
+                'name' => 'primaryText',
+                'placeholder' => 'Slide Title',
+                'help' => 'Slide Title',
+            ],
+//            [
+//                'type' => 'icon',
+//                'label' => 'Icon',
+//                'name' => 'icon',
+//                'placeholder' => 'Icon',
+//                'help' => 'Icon',
+//            ],
+
+             [
+                'type' => 'textarea',
+                'label' => 'Slide Description',
+                'name' => 'secondaryText',
+                'placeholder' => 'Slide Description',
+                'help' => 'Slide Description',
+            ],
+
+            [
+                'type' => 'text',
+                'label' => 'Button text',
+                'name' => 'seemoreText',
+                'placeholder' => 'Button text',
+                'help' => 'Button text',
+            ],
+
+            [
+                'type' => 'url',
+                'label' => 'Button url',
+                'name' => 'url',
+                'placeholder' => 'Button url',
+                'help' => 'Button url',
+            ]
+        ]
+    ];
+
+    ?>
+
+
+
+    @if($moduleTemplates && count($moduleTemplates) >  1)
+        <div class="d-flex justify-content-between align-items-center collapseNav-initialized form-control-live-edit-label-wrapper">
+            <div class="d-flex flex-wrap gap-md-4 gap-3">
+                <button x-on:click="showMainEditTab = 'mainSettings'"
+                        :class="{ 'active': showMainEditTab == 'mainSettings' }"
+                        class="btn btn-link text-decoration-none mw-admin-action-links mw-adm-liveedit-tabs active">
+                    @lang('Main settings')
+                </button>
+                <button x-on:click="showMainEditTab = 'design'" :class="{ 'active': showMainEditTab == 'design' }"
+                        class="btn btn-link text-decoration-none mw-admin-action-links mw-adm-liveedit-tabs">
+                    @lang('Design')
+                </button>
+            </div>
+        </div>
+    @endif
+
+    <div x-show="showMainEditTab=='mainSettings'" x-transition:enter="tab-pane-slide-left-active">
+
+
+        <livewire:microweber-live-edit::module-items-editor :moduleId="$moduleId" :moduleType="$moduleType"
+                                                            :editorSettings="$editorSettings"/>
+    </div>
+
+
+    @if($moduleTemplates && count($moduleTemplates) >  1)
+
+        <div x-show="showMainEditTab=='design'" x-transition:enter="tab-pane-slide-right-active">
+
+            <div>
+                <livewire:microweber-live-edit::module-select-template :moduleId="$moduleId" :moduleType="$moduleType"/>
+            </div>
+        </div>
+
+    @endif
+</div>
