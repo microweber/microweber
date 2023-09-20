bfe656fba449d85e676013389036d2dec58f7cb9
Peter Ivanov <peter@microweber.com>
2023-09-20 12:47:15 +0300

    update

---
+++ src/MicroweberPackages/LiveEdit/resources/views/element-style-editor/element-style-editor-main.blade.php
@@ -0,0 +1,124 @@
+<script>
+
+    var ActiveSelector = false;
+    var ActiveNode = false;
+    var OverlayNode = false;
+    mw.app = mw.top().app;
+    var targetWindow = mw.top().app.canvas.getWindow();
+    if (targetWindow) {
+        var targetMw = targetWindow.mw;
+
+    }
+    var specialCases = function (property, value) {
+        if (!property) return;
+        if (property.includes('col-')) {
+            scColumns(property, value)
+            return true;
+        } else if (OverlayNode && property === 'overlay-color') {
+            OverlayNode.style.backgroundColor = value;
+            mw.top().app.registerChange(OverlayNode);
+            return true;
+        } else if (OverlayNode && property === 'overlay-blend-mode') {
+            OverlayNode.style.mixBlendMode = value;
+            mw.top().app.registerChange(OverlayNode);
+            return true;
+        }
+
+    }
+
+    $(document).on('ready', function () {
+        window.document.addEventListener('refreshSelectedElement', function (e) {
+
+
+            ActiveNode = mw.top().app.liveEdit.getSelectedNode();
+
+            selectNodeInElementStyleEditorApp(ActiveNode);
+        });
+
+        mw.top().app.on('cssEditorSelectElementBySelector', function (selector) {
+            var canvasDocument = mw.top().app.canvas.getDocument();
+
+
+            if (selector) {
+                ActiveNode = canvasDocument.querySelector(selector);
+                if (!ActiveNode) {
+
+//mw.log( $.jseldom(selector).html('dummy element').appendTo('body',canvasDocument))
+                    var newEl =    $.jseldom(selector);
+
+                    var holder = canvasDocument.querySelector('#mw-non-existing-temp-element-holder');
+                    if(!holder){
+                        holder = canvasDocument.createElement('div');
+                        holder.id = 'mw-non-existing-temp-element-holder';
+                        holder.style.display = 'none';
+                        canvasDocument.body.append(holder);
+                    }
+                    if(newEl) {
+                        holder = canvasDocument.getElementById('mw-non-existing-temp-element-holder');
+                        holder.append(newEl[0]);
+                    }
+                    ActiveNode = canvasDocument.querySelector(selector);
+
+                }
+                ActiveSelector= selector;
+                selectNodeInElementStyleEditorApp(ActiveNode);
+
+            }
+        });
+    });
+
+    selectNodeInElementStyleEditorApp = function (ActiveNode) {
+        if (ActiveNode) {
+            mw.top().app.dispatch('mw.elementStyleEditor.selectNode', ActiveNode);
+
+        }
+    }
+
+    mw.top().app.on('mw.elementStyleEditor.applyCssPropertyToNode', function (data) {
+        // node: this.activeNode,
+        //     prop: prop,
+        //     val: val
+
+
+mw.log('mw.elementStyleEditor.applyCssPropertyToNode')
+mw.log(data)
+        output(data.prop, data.val);
+
+    });
+
+
+    var output = function (property, value) {
+        var mwTarget = targetMw;
+
+        if (ActiveNode && ActiveNode.length) {
+            ActiveNode = ActiveNode[0]
+        }
+        if (ActiveNode && ActiveSelector) {
+            if (!specialCases(property, value)) {
+
+                mw.top().app.cssEditor.setPropertyForSelector(ActiveSelector, property.replace(/([a-z])([A-Z])/g, '$1-$2').toLowerCase(), value)
+
+
+                ActiveNode.setAttribute('staticdesign', true);
+            }
+            mw.top().app.registerChange(ActiveNode);
+
+        }
+
+    };
+
+
+
+
+
+
+
+
+</script>
+@vite('src/MicroweberPackages/LiveEdit/resources/js/ui/apps/ElementStyleEditor/element-style-editor-app.js')
+
+
+
+<div id="mw-element-style-editor-app">
+    Loading ...
+</div>
