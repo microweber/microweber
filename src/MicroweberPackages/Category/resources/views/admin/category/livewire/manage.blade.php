7b593aeb67421db02cf45055a3d8f9739d96bf76
Peter Ivanov <peter@microweber.com>
2023-09-21 14:10:58 +0300

    update

--- src/MicroweberPackages/Category/resources/views/admin/category/livewire/manage.blade.php
+++ src/MicroweberPackages/Category/resources/views/admin/category/livewire/manage.blade.php
@@ -4,10 +4,16 @@ $hasPages = false;
 if (isset($isShop) and $isShop){
     $createRoute = route('admin.shop.category.create')."?parent=shop";
     $hasPages = app()->content_repository->getFirstShopPage();
+    if($hasPages) {
+       $createRoute = route('admin.shop.category.create') . "?parent_page_id=" . $hasPages['id'];
+    }
+
 } else {
     $createRoute = route('admin.category.create')."?parent=blog";
     $hasPages = app()->content_repository->getFirstBlogPage();
-
+    if($hasPages) {
+       $createRoute = route('admin.category.create')."?parent_page_id=".$hasPages['id'];
+    }
 }
 
 ?>
@@ -69,7 +75,7 @@ if (isset($isShop) and $isShop){
         </div>
 
         <div wire:ignore>
-            <div class=" mb-5" id="bulk-actions-block" style="display: none;" >
+            <div class=" mb-5" id="category-bulk-actions-block" style="display: none;" >
                 <label  class="form-label"><?php _e("Select action from the field") ?></label>
                 <div class="btn-group">
                     <button type="button" class="btn btn-outline-dark btn-sm dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
@@ -230,9 +236,28 @@ if (isset($isShop) and $isShop){
                         searchInput: document.getElementById('category-tree-search'),
                         skin: 'category-manager',
                         contextMenu: [
-
                             {
-                                title: '<?php _e('edit'); ?>',
+                                title: '<?php _e('Add category'); ?>',
+
+                                icon: 'add-subcategory-icon-tree',
+                                action: function (element, data, menuitem) {
+                                    var loc = '<?php print admin_url('category'); ?>/create?parent_page_id=' + data.id;
+                                    if (data.type === 'page') {
+                                        if(data.is_shop === 1){
+                                            loc = '<?php print admin_url('shop/category'); ?>/create?parent_page_id=' + data.id;
+                                        }
+                                    }
+
+                                    window.location.href = loc;
+
+                                },
+                                filter: function (obj, node) {
+                                    return obj.type === 'page';
+                                },
+                                className: ''
+                            },
+                            {
+                                title: '<?php _e('Edit'); ?>',
 
                                 icon: '<svg class="me-1 ms-0 tblr-body-color" fill="currentColor" data-bs-toggle="tooltip" aria-label="Edit" data-bs-original-title="Edit" xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" height="18px" viewBox="0 0 24 24" width="18px"><g><rect fill="none" height="24" width="24"></rect></g><g><g><g><path d="M3,21l3.75,0L17.81,9.94l-3.75-3.75L3,17.25L3,21z M5,18.08l9.06-9.06l0.92,0.92L5.92,19L5,19L5,18.08z"></path></g><g><path d="M18.37,3.29c-0.39-0.39-1.02-0.39-1.41,0l-1.83,1.83l3.75,3.75l1.83-1.83c0.39-0.39,0.39-1.02,0-1.41L18.37,3.29z"></path></g></g></g></svg>',
                                 action: function (element, data, menuitem) {
@@ -244,7 +269,7 @@ if (isset($isShop) and $isShop){
                                 className: ''
                             },
                             {
-                                title: '<?php _e('delete'); ?>',
+                                title: '<?php _e('Delete'); ?>',
                                 icon: '<svg class=" me-1 ms-0 text-danger" fill="currentColor" data-bs-toggle="tooltip" aria-label="Delete" data-bs-original-title="Delete" xmlns="http://www.w3.org/2000/svg" height="18px" viewBox="0 0 24 24" width="18px"><path d="M0 0h24v24H0V0z" fill="none"></path><path d="M16 9v10H8V9h8m-1.5-6h-5l-1 1H5v2h14V4h-3.5l-1-1zM18 7H6v12c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7z"></path></svg>',
                                 action: function (element, data, menuitem) {
                                     if (data.type === 'category') {
@@ -303,7 +328,7 @@ if (isset($isShop) and $isShop){
                             });
                             $(res.tree).on("selectionChange", function () {
 
-                                var bulk = document.getElementById('bulk-actions-block');
+                                var bulk = document.getElementById('category-bulk-actions-block');
 
                                 if(res.tree.getSelected().length === 0) {
                                     $('.js-hide-when-no-items-selected').hide()
