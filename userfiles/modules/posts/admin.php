118f12c949e8a23d366449488af2a8511d5a0943
Peter Ivanov <peter@microweber.com>
2023-09-21 14:27:31 +0300

    update

--- userfiles/modules/posts/admin.php
+++ userfiles/modules/posts/admin.php
@@ -1,2 +1,18 @@
 <?php
+only_admin_access();
+
+// legacy file, please do not use it
+// currently used only for the old shop/products module and will be removed
+
+$dir_name = normalize_path(modules_path()); ?>
+
+<?php if (isset($params['live_edit_sidebar'])): ?>
+    <?php include_once($dir_name . 'content' . DS . 'admin_live_edit_sidebar.php'); ?>
+<?php elseif (isset($params['backend'])): ?>
+    <?php include_once($dir_name . 'content' . DS. 'admin_backend.php'); ?>
+<?php else: ?>
+    <?php include_once($dir_name. 'content' . DS . 'admin_live_edit.php'); ?>
+<?php endif; ?>
+<?php
+
 
