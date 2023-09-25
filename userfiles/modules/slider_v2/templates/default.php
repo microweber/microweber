8f49cb850862f22591aab7b17d48af8a544344ca
Nikolay <rachkuw@gmail.com>
2023-09-25 12:24:04 +0300

    Update default.php

--- userfiles/modules/slider_v2/templates/default.php
+++ userfiles/modules/slider_v2/templates/default.php
@@ -4,46 +4,46 @@
 
 type: layout
 
-name: Default
+name: skin 1
 
-description: Default
+description: skin 1
 
 */
 ?>
 
-<div id="js-slider-<?php echo $params['id']; ?>" class="swiper">
+<div id="js-slider-<?php echo $params['id']; ?>" class="slider_v2-default swiper">
     <div class="swiper-wrapper">
 
         <?php foreach($slides as $i => $slide): ?>
 
-        <?php print getCssForSlide($slide); ?>
+            <?php print getCssForSlide($slide); ?>
 
-        <div class="swiper-slide">
+            <div class="swiper-slide">
 
-            <div class="js-slide-image-<?php echo $slide['itemId']; ?>"
-                 style="
-                     background-image: url('<?php echo $slide['image'];?>');
-                 ">
-            </div>
-
-            <div style="height:600px" class="d-flex flex-column justify-content-center align-items-center">
-               <div>
-                   <h3 class="js-slide-title-<?php echo $slide['itemId']; ?>" style="font-size:32px">
-                       <?php echo $slide['title'];?>
-                   </h3>
-               </div>
-                <div>
-                    <p class="js-slide-description-<?php echo $slide['itemId']; ?>" style="font-size:20px">
-                        <?php echo $slide['description'];?>
-                    </p>
+                <div class="js-slide-image-<?php echo $slide['itemId']; ?>"
+                     style="
+                         background-image: url('<?php echo $slide['image'];?>');
+                         ">
                 </div>
-            </div>
 
-        </div>
+                <div style="height: calc(100vh - 100px);" class="d-flex flex-column justify-content-center align-items-center">
+                    <div>
+                        <h1 class="js-slide-title-<?php echo $slide['itemId']; ?>">
+                            <?php echo $slide['title'];?>
+                        </h1>
+                    </div>
+                    <div>
+                        <p class="js-slide-description-<?php echo $slide['itemId']; ?>">
+                            <?php echo $slide['description'];?>
+                        </p>
+                    </div>
+                </div>
+
+            </div>
         <?php endforeach; ?>
     </div>
 
     <div id="js-slide-pagination-<?php echo $params['id']; ?>" class="swiper-pagination"></div>
-    <div id="js-slide-pagination-previous-<?php echo $params['id']; ?>" class="swiper-button-prev"></div>
-    <div id="js-slide-pagination-next-<?php echo $params['id']; ?>" class="swiper-button-next"></div>
+    <div id="js-slide-pagination-previous-<?php echo $params['id']; ?>" class="mw-slider-v2-buttons-slide mw-slider-v2-button-prev"></div>
+    <div id="js-slide-pagination-next-<?php echo $params['id']; ?>" class="mw-slider-v2-buttons-slide mw-slider-v2-button-next"></div>
 </div>
