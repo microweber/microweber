37e867d02b9957d41a87fcb7b42d2d6d1487da20
Peter Ivanov <peter@microweber.com>
2023-09-25 11:11:14 +0300

    update

--- userfiles/modules/editor/fonts/stylesheet.php
+++ userfiles/modules/editor/fonts/stylesheet.php
@@ -1,16 +1,5 @@
 <?php
 
-$google_font_domain = \MicroweberPackages\Utils\Misc\GoogleFonts::getDomain();
-$enabled_custom_fonts =  \MicroweberPackages\Utils\Misc\GoogleFonts::getEnabledFonts();
+$fonts_content = app()->template->get_custom_fonts_css_content();
 
-if (!empty($enabled_custom_fonts)) {
-    foreach ($enabled_custom_fonts as $font) {
-        if ($font) {
-            $font = str_replace('%2B', '+', $font);
-            $font_url = urlencode($font);
-            print "@import url(//{$google_font_domain}/css?family={$font_url}:300italic,400italic,600italic,700italic,800italic,400,600,800,700,300&subset=latin,cyrillic-ext,greek-ext,greek,vietnamese,latin-ext,cyrillic);";
-            print "\n";
-            print "\n";
-        }
-    }
-}
+print $fonts_content;
