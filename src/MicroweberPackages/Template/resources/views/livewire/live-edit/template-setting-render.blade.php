9e2cd9c61af521956fbfc1eb74f60149b4c29c97
Peter Ivanov <peter@microweber.com>
2023-10-02 14:25:43 +0300

    update

--- src/MicroweberPackages/Template/resources/views/livewire/live-edit/template-setting-render.blade.php
+++ src/MicroweberPackages/Template/resources/views/livewire/live-edit/template-setting-render.blade.php
@@ -86,7 +86,7 @@
                     propertyValue = propertyValue.replace('px', '');
                     e.target.value = propertyValue;
                 }"
-                x-on:update="(e) => {
+                x-on:input="(e) => {
                     if (mw.top().app.cssEditor) {
                         mw.top().app.cssEditor.setPropertyForSelector('{{end($setting['selectors'])}}', '{{$setting['fieldSettings']['property']}}', event.target.value + 'px');
                     }
