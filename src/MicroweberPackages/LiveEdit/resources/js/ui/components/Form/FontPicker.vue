aa86a93540f0edf8a503ebfa549f95e07d3c05ef
Peter Ivanov <peter@microweber.com>
2023-10-02 16:36:51 +0300

    update

--- src/MicroweberPackages/LiveEdit/resources/js/ui/components/Form/FontPicker.vue
+++ src/MicroweberPackages/LiveEdit/resources/js/ui/components/Form/FontPicker.vue
@@ -1,47 +1,94 @@
 <template>
 
-<div class="d-flex gap-2">
-    <select class="form-select">
-        <option v-for="(fontFamily) in supportedFonts"
-                :value="fontFamily">
-            {{ fontFamily }}
-        </option>
-    </select>
+  <div class="d-flex gap-2">
+
+
+    <div class="dropdown">
+      <button
+          class="btn btn-outline btn-sm dropdown-toggle"
+          type="button"
+          id="fontDropdown"
+          data-bs-toggle="dropdown"
+          aria-haspopup="true"
+          aria-expanded="false"
+      >
+        <div v-show="selectedFontFamily">
+
+          <span class="font-picker-selected-font" :style="{ fontFamily: selectedFontFamily }">
+            {{ selectedFontFamily }}
+          </span>
+        </div>
+        <div v-show="!selectedFontFamily">
+          <span class="font-picker-selected-font">
+            Select Font
+          </span>
+        </div>
+
+
+      </button>
+      <ul class="dropdown-menu" aria-labelledby="fontDropdown">
+        <li
+            v-for="(fontFamily, index) in supportedFonts"
+            :key="index"
+            :class="{ 'active': fontFamily === selectedFontFamily }"
+        >
+        <a class="dropdown-item" href="#" @click="selectFont(fontFamily)"  :style="{ fontFamily: fontFamily }">
+          {{ fontFamily }}
+        </a>
+        </li>
+      </ul>
+    </div>
+
+
     <button type="button" class="btn btn-outline-dark btn-sm" v-on:click="loadMoreFonts()">
-        Load more
+      Load more
     </button>
-</div>
+  </div>
 
 </template>
 
 <script>
 
 export default {
-    methods: {
-        loadMoreFonts() {
-            mw.top().app.fontManager.manageFonts();
-        }
+  props: {
+    selectedFontFamily: String,
+    value: String,
+    label: String,
+  },
+
+  methods: {
+    loadMoreFonts() {
+      mw.top().app.fontManager.manageFonts();
     },
-    mounted() {
 
-        setTimeout(() => {
-
-            this.supportedFonts = mw.top().app.fontManager.getFonts();
-            this.$forceUpdate();
-
-            mw.top().app.fontManager.subscribe((fonts) => {
-                if (fonts) {
-                    this.supportedFonts = fonts;
-                }
-                this.$forceUpdate();
-            });
-
-        }, 1000);
-    },
-    data() {
-        return {
-            supportedFonts: [],
-        }
+    selectFont(fontFamily) {
+      this.selectedFontFamily = fontFamily; // Update the selectedFontFamily data property
+      this.$emit('change', fontFamily); // Emit the 'change' event with the selected font family
     }
+
+  },
+
+  mounted() {
+
+    setTimeout(() => {
+
+      this.supportedFonts = mw.top().app.fontManager.getFonts();
+      this.$forceUpdate();
+
+      mw.top().app.fontManager.subscribe((fonts) => {
+        if (fonts) {
+          this.supportedFonts = fonts;
+        }
+        this.$forceUpdate();
+      });
+
+    }, 1000);
+  },
+  data() {
+    return {
+      supportedFonts: [],
+      selectedFontFamily: null
+    }
+  }
 }
 </script>
