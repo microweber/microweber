02753849db65e0d4f9e75a0588e8f28dc18d4c0b
Bozhidar Slaveykov <bobi@microweber.com>
2023-10-03 12:22:02 +0300

    Update font-picker.blade.php

--- src/MicroweberPackages/MicroweberUI/resources/views/components/font-picker.blade.php
+++ src/MicroweberPackages/MicroweberUI/resources/views/components/font-picker.blade.php
@@ -18,32 +18,38 @@ $randId = time() . rand(111,999);
      class="form-control-live-edit-label-wrapper">
 
     <button type="button" class="form-select form-control-live-edit-input"
-            :style="{ fontFamily: [selectedFont] }" 
+            :style="{ fontFamily: [selectedFont] }"
             x-on:click="openOptions = !openOptions" x-html="selectedFont">
     </button>
 
-    <input type="hidden" {!! $attributes->merge(['class'=>'form-select form-control-live-edit-input']) !!} />
+    <input type="text" id="input-font-{{$randId}}" {!! $attributes->merge([]) !!} />
 
-    <button class="dropdown-menu form-control-live-edit-input ps-0" :class="[openOptions ? 'show':'']">
+    <div style="height:400px;overflow:scroll;" class="dropdown-menu form-control-live-edit-input ps-0" :class="[openOptions ? 'show':'']">
+
         <template x-for="availableFont in availableFonts">
             <button type="button" class="dropdown-item tblr-body-color"
-                    x-on:click="selectedFont = availableFont; openOptions = false"
+                    x-on:click="()=> {
+                        selectedFont = availableFont;
+                        openOptions = false;
+                        inputFontElement = document.getElementById('input-font-{{$randId}}');
+                        inputFontElement.value = availableFont;
+                        inputFontElement.dispatchEvent(new Event('input'));
+                    }"
                     :style="{ fontFamily: [availableFont] }">
                 <span style="font-size:16px" x-text="availableFont"></span>
             </button>
         </template>
-    </div>
 
-    <div class="mt-1 mb-3">
         <button type="button"
                 x-on:click="()=> {
                     mw.top().app && mw.top().app.fontManager.manageFonts({
                          applySelectionToElement: '#{{$randId}}'
                     });
                 }"
-                class="btn btn-link mw-admin-action-links mw-adm-liveedit-tabs">
+                class="dropdown-item tblr-body-color">
             Add more fonts
         </button>
+
     </div>
 
 </div>
