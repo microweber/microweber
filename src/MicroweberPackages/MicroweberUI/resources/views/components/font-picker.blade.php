4295c2da7045729324faf82b8397f6d5ea2ddcec
Bozhidar Slaveykov <bobi@microweber.com>
2023-10-03 12:11:15 +0300

    Update font-picker.blade.php

--- src/MicroweberPackages/MicroweberUI/resources/views/components/font-picker.blade.php
+++ src/MicroweberPackages/MicroweberUI/resources/views/components/font-picker.blade.php
@@ -3,6 +3,7 @@ $randId = time() . rand(111,999);
 @endphp
 
 <div x-data="{
+    selectedFont: 'Inter', openOptions:false,
     availableFonts: {{json_encode(\MicroweberPackages\Utils\Misc\GoogleFonts::getEnabledFonts())}},
 }"
      x-init="
@@ -12,27 +13,37 @@ $randId = time() . rand(111,999);
                 availableFonts = fonts;
                 console.log(fonts);
             });
-
         }
       }"
      class="form-control-live-edit-label-wrapper">
-<!--
-    mw.top().app && mw.top().app.fontManager.manageFonts({
-    applySelectionToElement: '#{{$randId}}'
-    });-->
 
-    <div {!! $attributes->merge(['class'=>'form-select form-control-live-edit-input']) !!} >
+    <button type="button" class="form-select form-control-live-edit-input"
+            :style="{ fontFamily: [selectedFont] }" 
+            x-on:click="openOptions = !openOptions" x-html="selectedFont">
+    </button>
+
+    <input type="hidden" {!! $attributes->merge(['class'=>'form-select form-control-live-edit-input']) !!} />
+
+    <button class="dropdown-menu form-control-live-edit-input ps-0" :class="[openOptions ? 'show':'']">
         <template x-for="availableFont in availableFonts">
-            <div :style="{ fontFamily: [availableFont] }">
-                <span x-text="availableFont"></span>
-            </div>
+            <button type="button" class="dropdown-item tblr-body-color"
+                    x-on:click="selectedFont = availableFont; openOptions = false"
+                    :style="{ fontFamily: [availableFont] }">
+                <span style="font-size:16px" x-text="availableFont"></span>
+            </button>
         </template>
     </div>
 
-<!--    <div class="mt-1 mb-3">
-        <button type="button" class="btn btn-link mw-admin-action-links mw-adm-liveedit-tabs"
-                onclick="Livewire.emit('openModal', 'font-picker-modal')">
+    <div class="mt-1 mb-3">
+        <button type="button"
+                x-on:click="()=> {
+                    mw.top().app && mw.top().app.fontManager.manageFonts({
+                         applySelectionToElement: '#{{$randId}}'
+                    });
+                }"
+                class="btn btn-link mw-admin-action-links mw-adm-liveedit-tabs">
             Add more fonts
         </button>
-    </div>-->
+    </div>
+
 </div>
