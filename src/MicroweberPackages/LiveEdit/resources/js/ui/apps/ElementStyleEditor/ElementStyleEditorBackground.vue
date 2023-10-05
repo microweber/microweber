a9d7f8a6e8a655b3f60f51a65e9b47a047f95671
Peter Ivanov <peter@microweber.com>
2023-10-05 16:31:03 +0300

    update

--- src/MicroweberPackages/LiveEdit/resources/js/ui/apps/ElementStyleEditor/ElementStyleEditorBackground.vue
+++ src/MicroweberPackages/LiveEdit/resources/js/ui/apps/ElementStyleEditor/ElementStyleEditorBackground.vue
@@ -1,34 +1,31 @@
 <template>
-  <div>
-    <br>
+    <div>
+        <br>
 
 
-    <ColorPicker v-model="backgroundColor" v-bind:color=backgroundColor :label="'Background Color'"
-                 @change="handleBackgroundColorChange"/>
+        <ColorPicker v-model="backgroundColor" v-bind:color=backgroundColor :label="'Background Color'"
+                     @change="handleBackgroundColorChange"/>
 
 
-    <ImagePicker label="Background Image" v-model="backgroundImage" v-bind:file="backgroundImageUrl"
-                 @change="handleBackgroundImageChange"/>
+        <ImagePicker label="Background Image" v-model="backgroundImage" v-bind:file="backgroundImageUrl"
+                     @change="handleBackgroundImageChange"/>
 
-    <DropdownSmall v-model="backgroundSize" :options="backgroundSizeOptions" :label="'Background Size'"/>
-
-
-    <DropdownSmall v-model="backgroundRepeat" :options="backgroundRepeatOptions" :label="'Background Repeat'"/>
+        <DropdownSmall v-model="backgroundSize" :options="backgroundSizeOptions" :label="'Background Size'"/>
 
 
-    <DropdownSmall v-model="backgroundPosition" :options="backgroundPositionOptions" :label="'Background Position'"/>
+        <DropdownSmall v-model="backgroundRepeat" :options="backgroundRepeatOptions" :label="'Background Repeat'"/>
 
 
-      // checked if backgroundClip == text
-        <label for="backgroundClip">Background Clip</label>
+        <DropdownSmall v-model="backgroundPosition" :options="backgroundPositionOptions"
+                       :label="'Background Position'"/>
 
-      <input type="radio"  name="backgroundClip" value="text" v-model="backgroundClip">
-      <input type="radio"  name="backgroundClip" value="border-box" v-model="backgroundClip">
-      <input type="radio"  name="backgroundClip" value="content-box" v-model="backgroundClip">
+
+        <DropdownSmall v-model="backgroundClip" :options="backgroundClipOptions"
+                       :label="'Background Clip'"/>
 
 
 
-  </div>
+    </div>
 </template>
 
 <script>
@@ -43,173 +40,178 @@ import FilePicker from "../../components/Form/FilePicker.vue";
 
 export default {
 
-  components: {ColorPicker, FontPicker, Dropdown, Input, Slider, FilePicker, ImagePicker, DropdownSmall},
+    components: {ColorPicker, FontPicker, Dropdown, Input, Slider, FilePicker, ImagePicker, DropdownSmall},
 
-  data() {
-    return {
+    data() {
+        return {
 
-      'backgroundPositionOptions': [
-        {key: "none", value: "None"},
-        {key: "0% 0%", value: "Left Top"},
-        {key: "50% 0%", value: "Center Top"},
-        {key: "100% 0%", value: "Right Top"},
-        {key: "0% 50%", value: "Left Center"},
-        {key: "50% 50%", value: "Center Center"},
-        {key: "100% 50%", value: "Right Center"},
-        {key: "0% 100%", value: "Left Bottom"},
-        {key: "50% 100%", value: "Center Bottom"},
-        {key: "100% 100%", value: "Right Bottom"}
-      ],
-      'backgroundRepeatOptions': [
-        {key: "none", value: "None"},
-        {key: "repeat", value: "repeat"},
-        {key: "no-repeat", value: "no-repeat"},
-        {key: "repeat-x", value: "repeat horizontally"},
-        {key: "repeat-y", value: "repeat vertically"}
-      ],
-      'backgroundSizeOptions': [
-        {key: "none", value: "None"},
-        {key: "auto", value: "Auto"},
-        {key: "contain", value: "Fit"},
-        {key: "cover", value: "Cover"},
-        {key: "100% 100%", value: "Scale"}
-      ],
+            'backgroundPositionOptions': [
+                {key: "none", value: "None"},
+                {key: "0% 0%", value: "Left Top"},
+                {key: "50% 0%", value: "Center Top"},
+                {key: "100% 0%", value: "Right Top"},
+                {key: "0% 50%", value: "Left Center"},
+                {key: "50% 50%", value: "Center Center"},
+                {key: "100% 50%", value: "Right Center"},
+                {key: "0% 100%", value: "Left Bottom"},
+                {key: "50% 100%", value: "Center Bottom"},
+                {key: "100% 100%", value: "Right Bottom"}
+            ],
+            'backgroundRepeatOptions': [
+                {key: "none", value: "None"},
+                {key: "repeat", value: "repeat"},
+                {key: "no-repeat", value: "no-repeat"},
+                {key: "repeat-x", value: "repeat horizontally"},
+                {key: "repeat-y", value: "repeat vertically"}
+            ],
+            'backgroundClipOptions': [
+                {key: "border-box", value: "Border Box"},
+                {key: "content-box", value: "Content Box"},
+                {key: "text", value: "Text"}
+            ],
+            'backgroundSizeOptions': [
+                {key: "none", value: "None"},
+                {key: "auto", value: "Auto"},
+                {key: "contain", value: "Fit"},
+                {key: "cover", value: "Cover"},
+                {key: "100% 100%", value: "Scale"}
+            ],
 
-      'activeNode': null,
-      'isReady': false,
-      'backgroundImage': null,
-      'backgroundColor': null,
-      'backgroundPosition': null,
-      'backgroundRepeat': null,
-      'backgroundSize': null,
-      'backgroundImageUrl': null,
-      'backgroundClip': null,
-    };
-  },
-
-  methods: {
-    resetAllProperties: function () {
-      this.backgroundImage = null;
-      this.backgroundImageUrl = null;
-      this.backgroundColor = null;
-      this.backgroundPosition = null;
-      this.backgroundRepeat = null;
-      this.backgroundSize = null;
-      this.backgroundClip = null;
+            'activeNode': null,
+            'isReady': false,
+            'backgroundImage': null,
+            'backgroundColor': null,
+            'backgroundPosition': null,
+            'backgroundRepeat': null,
+            'backgroundSize': null,
+            'backgroundImageUrl': null,
+            'backgroundClip': null,
+        };
     },
 
-    populateStyleEditor: function (node) {
-      if (node && node && node.nodeType === 1) {
-        var css = mw.CSSParser(node);
-        this.isReady = false;
-        this.resetAllProperties();
-        this.activeNode = node;
+    methods: {
+        resetAllProperties: function () {
+            this.backgroundImage = null;
+            this.backgroundImageUrl = null;
+            this.backgroundColor = null;
+            this.backgroundPosition = null;
+            this.backgroundRepeat = null;
+            this.backgroundSize = null;
+            this.backgroundClip = null;
+        },
 
-        this.populateCssBackground(css);
+        populateStyleEditor: function (node) {
+            if (node && node && node.nodeType === 1) {
+                var css = mw.CSSParser(node);
+                this.isReady = false;
+                this.resetAllProperties();
+                this.activeNode = node;
+
+                this.populateCssBackground(css);
 
 
-          setTimeout(() => {
-              this.isReady = true;
-          }, 100);
-      }
+                setTimeout(() => {
+                    this.isReady = true;
+                }, 100);
+            }
+        },
+        populateCssBackground: function (css) {
+            if (!css || !css.get) return;
+            var bg = css.get.background();
+
+            if (bg.image) {
+                if (bg.image.indexOf('url(') !== -1) {
+                    this.backgroundImageUrl = bg.image.replace('url(', '').replace(')', '');
+                    //also replace "
+                    this.backgroundImageUrl = this.backgroundImageUrl.replace(/\"/g, "");
+                }
+            }
+            this.backgroundImage = bg.image;
+            this.backgroundColor = bg.color;
+            this.backgroundPosition = bg.position;
+            this.backgroundRepeat = bg.repeat;
+            this.backgroundSize = bg.size;
+            this.backgroundClip = bg.clip;
+        },
+
+        handleBackgroundColorChange: function (color) {
+
+            if (typeof (color) != 'string') {
+                return;
+            }
+            this.backgroundColor = color
+        },
+        handleBackgroundImageChange: function (url) {
+            var urlVal = url;
+            if (url && url != '' && url != 'none' && url != 'inherit' && url != 'initial') {
+                //check if contain url(
+                this.backgroundImageUrl = url;
+                if (url.indexOf('url(') === -1) {
+                    urlVal = 'url(' + url + ')';
+                }
+            } else {
+                this.backgroundImageUrl = '';
+            }
+            if (urlVal == null) {
+                urlVal = 'none';
+            }
+            this.backgroundImage = urlVal;
+        },
+
+        applyPropertyToActiveNode: function (prop, val) {
+            if (!this.isReady) {
+                return;
+            }
+
+            if (this.activeNode) {
+                mw.top().app.dispatch('mw.elementStyleEditor.applyCssPropertyToNode', {
+                    node: this.activeNode,
+                    prop: prop,
+                    val: val
+                });
+            }
+        },
+
     },
-    populateCssBackground: function (css) {
-      if (!css || !css.get) return;
-      var bg = css.get.background();
+    mounted() {
 
-      if (bg.image) {
-        if (bg.image.indexOf('url(') !== -1) {
-          this.backgroundImageUrl = bg.image.replace('url(', '').replace(')', '');
-          //also replace "
-          this.backgroundImageUrl = this.backgroundImageUrl.replace(/\"/g, "");
-        }
-      }
-      this.backgroundImage = bg.image;
-      this.backgroundColor = bg.color;
-      this.backgroundPosition = bg.position;
-      this.backgroundRepeat = bg.repeat;
-      this.backgroundSize = bg.size;
-      this.backgroundClip = bg.clip;
-    },
+        mw.top().app.on('mw.elementStyleEditor.selectNode', (element) => {
 
-    handleBackgroundColorChange: function (color) {
+            this.populateStyleEditor(element)
 
-      if (typeof (color) != 'string') {
-        return;
-      }
-      this.backgroundColor = color
-    },
-    handleBackgroundImageChange: function (url) {
-      var urlVal = url;
-      if (url && url != '' && url != 'none' && url != 'inherit' && url != 'initial') {
-        //check if contain url(
-        this.backgroundImageUrl = url;
-        if (url.indexOf('url(') === -1) {
-          urlVal = 'url(' + url + ')';
-        }
-      } else {
-        this.backgroundImageUrl = '';
-      }
-      if (urlVal == null) {
-        urlVal = 'none';
-      }
-      this.backgroundImage = urlVal;
-    },
-
-    applyPropertyToActiveNode: function (prop, val) {
-      if (!this.isReady) {
-        return;
-      }
-
-      if (this.activeNode) {
-        mw.top().app.dispatch('mw.elementStyleEditor.applyCssPropertyToNode', {
-          node: this.activeNode,
-          prop: prop,
-          val: val
         });
-      }
+
     },
 
-  },
-  mounted() {
+    watch: {
+        // Background-related property watchers
+        backgroundImage: function (newValue, oldValue) {
+            this.applyPropertyToActiveNode('backgroundImage', newValue);
+        },
+        backgroundClip: function (newValue, oldValue) {
+            if (newValue == 'text') {
+                this.applyPropertyToActiveNode('backgroundClip', 'text');
+                this.applyPropertyToActiveNode('-webkitBackgroundClip', 'text');
+                this.applyPropertyToActiveNode('color', 'rgba(0,0,0,0)');
+            } else {
+                this.applyPropertyToActiveNode('backgroundClip', newValue);
+                this.applyPropertyToActiveNode('-webkitBackgroundClip', newValue);
+                this.applyPropertyToActiveNode('color', '');
 
-    mw.top().app.on('mw.elementStyleEditor.selectNode', (element) => {
-
-      this.populateStyleEditor(element)
-
-    });
-
-  },
-
-  watch: {
-    // Background-related property watchers
-    backgroundImage: function (newValue, oldValue) {
-      this.applyPropertyToActiveNode('backgroundImage', newValue);
+            }
+        },
+        backgroundColor: function (newValue, oldValue) {
+            this.applyPropertyToActiveNode('backgroundColor', newValue);
+        },
+        backgroundPosition: function (newValue, oldValue) {
+            this.applyPropertyToActiveNode('backgroundPosition', newValue);
+        },
+        backgroundRepeat: function (newValue, oldValue) {
+            this.applyPropertyToActiveNode('backgroundRepeat', newValue);
+        },
+        backgroundSize: function (newValue, oldValue) {
+            this.applyPropertyToActiveNode('backgroundSize', newValue);
+        },
     },
-      backgroundClip: function (newValue, oldValue) {
-        if(newValue == 'text'){
-            this.applyPropertyToActiveNode('backgroundClip', 'text');
-            this.applyPropertyToActiveNode('-webkitBackgroundClip', 'text');
-            this.applyPropertyToActiveNode('color', 'rgba(0,0,0,0)');
-        } else {
-            this.applyPropertyToActiveNode('backgroundClip', newValue);
-            this.applyPropertyToActiveNode('-webkitBackgroundClip', newValue);
-            this.applyPropertyToActiveNode('color', '');
-
-        }
-    },
-    backgroundColor: function (newValue, oldValue) {
-      this.applyPropertyToActiveNode('backgroundColor', newValue);
-    },
-    backgroundPosition: function (newValue, oldValue) {
-      this.applyPropertyToActiveNode('backgroundPosition', newValue);
-    },
-    backgroundRepeat: function (newValue, oldValue) {
-      this.applyPropertyToActiveNode('backgroundRepeat', newValue);
-    },
-    backgroundSize: function (newValue, oldValue) {
-      this.applyPropertyToActiveNode('backgroundSize', newValue);
-    },
-  },
 }
 </script>
