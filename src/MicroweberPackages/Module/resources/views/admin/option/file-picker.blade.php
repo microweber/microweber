503eee8ba831ec4589f3e7730f6cbef16857d075
Bozhidar Slaveykov <bobi@microweber.com>
2023-10-10 11:42:41 +0300

    Update file-picker.blade.php

--- src/MicroweberPackages/Module/resources/views/admin/option/file-picker.blade.php
+++ src/MicroweberPackages/Module/resources/views/admin/option/file-picker.blade.php
@@ -1,7 +1,10 @@
 <div wire:ignore>
     <x-microweber-ui::file-picker
+
         :label="$label"
+
         allowedType="{{$this->allowedType}}"
+
         wire:model.debounce.100ms="state.settings.{{ $this->optionKey }}" />
 
 </div>
