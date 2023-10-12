b3f944ab52ae293df303ad38015b3e4da62e34aa
Peter Ivanov <peter@microweber.com>
2023-10-12 17:51:31 +0300

    update

--- tests/Browser/ShopTests/AdminShopOffersTest.php
+++ tests/Browser/ShopTests/AdminShopOffersTest.php
@@ -3,51 +3,74 @@
 namespace Tests\Browser\ShopTests;
 
 use Laravel\Dusk\Browser;
+use MicroweberPackages\Product\Models\Product;
 use Tests\Browser\Components\AdminLogin;
 use Tests\Browser\Components\ChekForJavascriptErrors;
 use Tests\DuskTestCase;
 
 class AdminShopOffersTest extends DuskTestCase
 {
-    public $siteUrl = 'http://127.0.0.1:8000/';
 
     public function testAddAddOffer()
     {
-        $siteUrl = $this->siteUrl;
+        $siteUrl = site_url();
 
-        $this->browse(function (Browser $browser) use($siteUrl) {
+        $this->browse(function (Browser $browser) use ($siteUrl) {
 
             $browser->within(new AdminLogin, function ($browser) {
                 $browser->fillForm();
             });
-
+            $title = 'offer-test-' . uniqid();
+            $newProduct = new Product();
+            $newProduct->title = $title;
+            $newProduct->setCustomField(
+                [
+                    'type' => 'price',
+                    'name' => 'price',
+                    'value' => '10000',
+                ]
+            );
+            $newProduct->save();
 
 
             $browser->pause(100);
 
-            $browser->visit($siteUrl.'admin/view:shop/action:options?group=shop/settings');
+            $browser->visit($siteUrl . 'admin/settings?group=shop/offers/admin_block');
             $browser->within(new ChekForJavascriptErrors(), function ($browser) {
                 $browser->validate();
             });
 
-            $browser->pause(1000);
-            $browser->clickLink('Promotions');
-            $browser->waitForText('Offers');
-
+            $offerRand = rand(10, 20);
             $browser->assertSee('Add new offer');
             $browser->pause(1000);
             $browser->click('.js-add-new-offer');
 
             $browser->waitForText('Add new offer', 30);
             $browser->waitForText('Offer status');
-            $browser->waitForText('Save',30);
+            $browser->waitForText('Save', 30);
 
             $browser->assertSee('Add new offer');
             $browser->assertSee('Offer status');
             $browser->assertSee('Save');
             $browser->assertSee('Offer price');
             $browser->assertSee('Offer start at');
-            $browser->assertSee('Offer expiry at');
+            $browser->assertSee('Offer expiration date');
+
+            $browser->click('#mw-admin-search-for-products');
+            $browser->pause(300);
+
+            $browser->typeSlowly('#mw-admin-search-for-products input.mw-ui-invisible-field', $title);
+            $browser->pause(1000);
+
+            $browser->typeSlowly('offer_price', $offerRand);
+            $browser->click('.mw-autocomplete-list > li:first-child');
+            $browser->pause(300);
+            $browser->click('.js-save-offer');
+            $browser->pause(1000);
+
+            $browser->visit($siteUrl . 'admin/settings?group=shop/offers/admin_block');
+            $browser->pause(1000);
+            $browser->assertSee($title);
 
 
         });
