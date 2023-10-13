7f24f5062155787e7d58a6716e14d1eca234594a
Bozhidar Slaveykov <bobi@microweber.com>
2023-10-13 16:58:23 +0300

    Update template-settings-sidebar.blade.php

--- src/MicroweberPackages/Template/resources/views/livewire/live-edit/template-settings-sidebar.blade.php
+++ src/MicroweberPackages/Template/resources/views/livewire/live-edit/template-settings-sidebar.blade.php
@@ -34,6 +34,7 @@
             mw.tools.confirm_reset_module_by_id('{{$optionGroupLess}}', function () {
                 // Reset template settings
                 mw.top().app.templateSettings.reloadStylesheet('{{$styleSheetSourceFile}}', '{{$optionGroupLess}}');
+                window.location.reload(); 
             }, askForConfirmText);
 
         }
@@ -166,7 +167,7 @@
                Styles
 
                 <button type="button" x-on:click="()=> {
-                        resetStylesheetSettings(); 
+                        resetStylesheetSettings();
                     }">
                                 <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M440-122q-121-15-200.5-105.5T160-440q0-66 26-126.5T260-672l57 57q-38 34-57.5 79T240-440q0 88 56 155.5T440-202v80Zm80 0v-80q87-16 143.5-83T720-440q0-100-70-170t-170-70h-3l44 44-56 56-140-140 140-140 56 56-44 44h3q134 0 227 93t93 227q0 121-79.5 211.5T520-122Z"/></svg>
 
