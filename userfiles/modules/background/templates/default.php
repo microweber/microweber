d6ca95ef93708aac34b5f3789610c795e19eebfe
Bozhidar Slaveykov <bobi@microweber.com>
2023-10-13 17:41:42 +0300

    update

---
+++ userfiles/modules/background/templates/default.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+
+type: layout
+
+name: Default
+
+description: Default
+
+*/
+?>
+<div class="mw-layout-background-block no-settings inaccessibleModule">
+    <div class="mw-layout-background-node" <?php print $video_attr_parent; ?>  <?php print $style_attr; ?>>
+        <?php print $video_html; ?>
+    </div>
+    <div class="mw-layout-background-overlay" <?php print $style_attr_overlay; ?>>
+
+    </div>
+</div>
---
+++ userfiles/modules/background/templates/in-block.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+
+type: layout
+
+name: in-block
+
+description: in-block
+
+*/
+?>
+<div class="mw-layout-background-block">
+    <div class="mw-layout-background-node" <?php print $video_attr_parent; ?>  <?php print $style_attr; ?>>
+        <?php print $video_html; ?>
+    </div>
+    <div class="mw-layout-background-overlay" <?php print $style_attr_overlay; ?>>
+
+    </div>
+</div>
