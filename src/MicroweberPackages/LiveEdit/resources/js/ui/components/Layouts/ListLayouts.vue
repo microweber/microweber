dc9188b89de83ddc8190896890661526f83f2854
Bozhidar Slaveykov <bobi@microweber.com>
2023-10-16 12:50:15 +0300

    Update ListLayouts.vue

--- src/MicroweberPackages/LiveEdit/resources/js/ui/components/Layouts/ListLayouts.vue
+++ src/MicroweberPackages/LiveEdit/resources/js/ui/components/Layouts/ListLayouts.vue
@@ -176,8 +176,8 @@
 
     <div ref="unlockPremiumLayout">
         <div class="row">
-            <div class="col-md-9 mt-4">
-                
+            <div class="col-md-8 mt-4">
+
                 <h1>Unlock the power of Microweber</h1>
                 <h4>Get all premium layouts of BIG template and make awesome websites!</h4>
 
@@ -201,20 +201,22 @@
                 </div>
 
             </div>
-            <div class="col-md-3"><h4>Include with the premium license</h4>
-                <ul>
-                    <li>+30 Headers</li>
-                    <li>+30 Galleries</li>
-                    <li>+30 Call to actions</li>
-                    <li>+15 Contact forms</li>
-                    <li>+10 Footers</li>
-                    <li>+10 Blogs</li>
-                    <li>+10 Portfolios</li>
-                    <li>+10 Team members</li>
-                    <li>+10 Testimonials</li>
-                    <li>+10 Pricing tables</li>
-                    <li>+10 Services</li>
-                    <li>+10 Features</li>
+            <div class="col-md-4">
+
+                <h4>Include with the premium license</h4>
+                <ul class="list-unstyled space-y-1">
+                    <li><svg xmlns="http://www.w3.org/2000/svg" class="icon text-green" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><path d="M5 12l5 5l10 -10"></path></svg> +30 Headers</li>
+                    <li><svg xmlns="http://www.w3.org/2000/svg" class="icon text-green" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><path d="M5 12l5 5l10 -10"></path></svg> +30 Galleries</li>
+                    <li><svg xmlns="http://www.w3.org/2000/svg" class="icon text-green" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><path d="M5 12l5 5l10 -10"></path></svg> +30 Call to actions</li>
+                    <li><svg xmlns="http://www.w3.org/2000/svg" class="icon text-green" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><path d="M5 12l5 5l10 -10"></path></svg> +15 Contact forms</li>
+                    <li><svg xmlns="http://www.w3.org/2000/svg" class="icon text-green" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><path d="M5 12l5 5l10 -10"></path></svg> +10 Footers</li>
+                    <li><svg xmlns="http://www.w3.org/2000/svg" class="icon text-green" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><path d="M5 12l5 5l10 -10"></path></svg> +10 Blogs</li>
+                    <li><svg xmlns="http://www.w3.org/2000/svg" class="icon text-green" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><path d="M5 12l5 5l10 -10"></path></svg> +10 Portfolios</li>
+                    <li><svg xmlns="http://www.w3.org/2000/svg" class="icon text-green" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><path d="M5 12l5 5l10 -10"></path></svg> +10 Team members</li>
+                    <li><svg xmlns="http://www.w3.org/2000/svg" class="icon text-green" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><path d="M5 12l5 5l10 -10"></path></svg> +10 Testimonials</li>
+                    <li><svg xmlns="http://www.w3.org/2000/svg" class="icon text-green" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><path d="M5 12l5 5l10 -10"></path></svg> +10 Pricing tables</li>
+                    <li><svg xmlns="http://www.w3.org/2000/svg" class="icon text-green" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><path d="M5 12l5 5l10 -10"></path></svg> +10 Services</li>
+                    <li><svg xmlns="http://www.w3.org/2000/svg" class="icon text-green" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><path d="M5 12l5 5l10 -10"></path></svg> +10 Features</li>
                 </ul>
             </div>
         </div>
