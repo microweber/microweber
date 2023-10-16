4e1ab12b3d94fd5bd895fd9ed484f87181b6436d
Peter Ivanov <peter@microweber.com>
2023-10-16 15:32:26 +0300

    update

--- src/MicroweberPackages/LiveEdit/Providers/LiveEditServiceProvider.php
+++ src/MicroweberPackages/LiveEdit/Providers/LiveEditServiceProvider.php
@@ -105,7 +105,7 @@ class LiveEditServiceProvider extends PackageServiceProvider
             ]);
 
 
-        \MicroweberPackages\LiveEdit\Facades\LiveEditManager::getMenuInstance('top_right_menu')
+/*        \MicroweberPackages\LiveEdit\Facades\LiveEditManager::getMenuInstance('top_right_menu')
             ->addChild('Plans and Payments', [
                 'attributes' => [
                     'route' => 'admin.settings.index',
@@ -114,7 +114,7 @@ class LiveEditServiceProvider extends PackageServiceProvider
                                                             d="M40 3v26c0 .8-.3 1.5-.9 2.1-.6.6-1.3.9-2.1.9H3c-.8 0-1.5-.3-2.1-.9-.6-.6-.9-1.3-.9-2.1V3C0 2.2.3 1.5.9.9 1.5.3 2.2 0 3 0h34c.8 0 1.5.3 2.1.9.6.6.9 1.3.9 2.1zM3 8.45h34V3H3v5.45zm0 6.45V29h34V14.9H3zM3 29V3v26z"/>
                                                     </svg>'
                 ]
-            ]);
+            ]);*/
 
         \MicroweberPackages\LiveEdit\Facades\LiveEditManager::getMenuInstance('top_right_menu')
             ->addChild('Website Settings', [
@@ -129,7 +129,7 @@ class LiveEditServiceProvider extends PackageServiceProvider
 
 
         \MicroweberPackages\LiveEdit\Facades\LiveEditManager::getMenuInstance('top_right_menu')
-            ->addChild('Logout', [
+            ->addChild('Log out', [
                 'attributes' => [
                     'href' => logout_url(),
                     'icon' => '<svg viewBox="0 0 36 36.1">
@@ -138,6 +138,19 @@ class LiveEditServiceProvider extends PackageServiceProvider
                                                     </svg>'
                 ]
             ]);
+
+
+        \MicroweberPackages\LiveEdit\Facades\LiveEditManager::getMenuInstance('top_right_menu')
+            ->addChild('See website', [
+                'attributes' => [
+                    'href' => site_url().'?editmode=n',
+                    'icon' => '<svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M480-80q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm-40-82v-78q-33 0-56.5-23.5T360-320v-40L168-552q-3 18-5.5 36t-2.5 36q0 121 79.5 212T440-162Zm276-102q20-22 36-47.5t26.5-53q10.5-27.5 16-56.5t5.5-59q0-98-54.5-179T600-776v16q0 33-23.5 56.5T520-680h-80v80q0 17-11.5 28.5T400-560h-80v80h240q17 0 28.5 11.5T600-440v120h40q26 0 47 15.5t29 40.5Z"/></svg>'
+                ]
+            ]);
+
+
+
+
     }
 
 }
