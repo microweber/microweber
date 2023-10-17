3e3d604a987ca854727d382e3475966a2f4923b2
Nikolay <rachkuw@gmail.com>
2023-10-17 18:11:32 +0300

    Update ImagePicker.vue

--- src/MicroweberPackages/LiveEdit/resources/js/ui/apps/ElementStyleEditor/components/ImagePicker.vue
+++ src/MicroweberPackages/LiveEdit/resources/js/ui/apps/ElementStyleEditor/components/ImagePicker.vue
@@ -1,28 +1,36 @@
 <template>
   <div class="my-4 pb-4">
         <label class="live-edit-label">{{ label }}</label>
-        <div class="s-field-content">
-            <div class="mw-ui-btn-nav background-image-nav">
-        <span
-            class="mw-ui-btn mw-ui-btn-outline mw-ui-btn-small tip mdi mdi-folder-image mdi-17px background-select-item"
-            @click="selectBackgroundImage"
-            data-tip="Select background image"
-        >
-          <span v-if="!selectedFile" class="background-preview" style="background-image: none;"></span>
-          <span v-if="selectedFile" class="background-preview" :style="{ backgroundImage: `url(${selectedFile})` }"></span>
-        </span>
-                <span
-                    class="mw-ui-btn mw-ui-btn-outline mw-ui-btn-small tip mdi mdi-delete"
-                    @click="removeBackgroundImage"
-                    data-tip="Remove background"
-                    data-tipposition="top-right"
-                ></span>
-                <span
-                    class="mw-ui-btn mw-ui-btn-outline mw-ui-btn-small tip mdi mdi-history"
-                    @click="resetBackgroundImage"
-                    data-tip="Reset background"
-                    data-tipposition="top-right"
-                ></span>
+        <div class="ms-2">
+            <div class="background-image-nav d-flex justify-content-between align-items-center">
+                <div>
+                    <span
+                        class="mw-ui-btn mw-ui-btn-outline rounded-0 tip background-select-item" style="height: 70px; width: 70px; border: 1px solid #000;"
+                        @click="selectBackgroundImage"
+                        data-tip="Select background image"
+                    >
+                    <svg xmlns="http://www.w3.org/2000/svg" height="20" viewBox="0 -960 960 960" width="20"><path d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h560q33 0 56.5 23.5T840-760v560q0 33-23.5 56.5T760-120H200Zm0-80h560v-560H200v560Zm40-80h480L570-480 450-320l-90-120-120 160Zm-40 80v-560 560Z"/></svg>
+                  <span v-if="!selectedFile" class="background-preview" style="background-image: none;"></span>
+                  <span v-if="selectedFile" class="background-preview" :style="{ backgroundImage: `url(${selectedFile})` }"></span>
+                </span>
+                </div>
+
+                <div v-if="selectedFile" class="d-flex gap-2 justify-content-end">
+                    <span
+                        @click="removeBackgroundImage"
+                        data-tip="Remove background"
+                        data-tipposition="top-right"
+                    >
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" height="16" viewBox="0 -960 960 960" width="16"><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/></svg>
+                    </span>
+                    <span
+                        @click="resetBackgroundImage"
+                        data-tip="Reset background"
+                        data-tipposition="top-right"
+                    >
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" height="16" viewBox="0 -960 960 960" width="16"><path d="M440-122q-121-15-200.5-105.5T160-440q0-66 26-126.5T260-672l57 57q-38 34-57.5 79T240-440q0 88 56 155.5T440-202v80Zm80 0v-80q87-16 143.5-83T720-440q0-100-70-170t-170-70h-3l44 44-56 56-140-140 140-140 56 56-44 44h3q134 0 227 93t93 227q0 121-79.5 211.5T520-122Z"></path></svg>
+                    </span>
+                </div>
             </div>
         </div>
     </div>
