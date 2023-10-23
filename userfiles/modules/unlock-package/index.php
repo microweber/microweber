d10bcc3603f68584e2fd5067c65029147e2ce598
Bozhidar Slaveykov <bobi@microweber.com>
2023-10-23 16:32:35 +0300

    Update index.php

--- userfiles/modules/unlock-package/index.php
+++ userfiles/modules/unlock-package/index.php
@@ -1,1 +1,16 @@
 <?php
+$template = get_option('data-template', $params['id']);
+
+if ($template == false and isset($params['template'])) {
+    $template = $params['template'];
+}
+if ($template != false) {
+    $template_file = module_templates($config['module'], $template);
+} else {
+    $template_file = module_templates($config['module'], 'default');
+
+}
+if ($template_file != false and is_file($template_file)) {
+    include($template_file);
+}
+?>
