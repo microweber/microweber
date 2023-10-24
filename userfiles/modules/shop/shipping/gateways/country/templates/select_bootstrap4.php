540c8449026ed6f956d342e32e68064e4017f54a
Peter Ivanov <peter@microweber.com>
2023-10-24 10:41:14 +0300

    update

--- userfiles/modules/shop/shipping/gateways/country/templates/select_bootstrap4.php
+++ userfiles/modules/shop/shipping/gateways/country/templates/select_bootstrap4.php
@@ -12,7 +12,7 @@ description: Select country
 
 <div class="ms-3 form-group <?php print $config['module_class'] ?>" id="<?php print $rand; ?>">
     <label class="mt-4 mb-3"><?php _e("Ship to"); ?></label>
-  <select name="country" class="shipping-country-select form-control mb-3">
+  <select name="country" class="shipping-country-select form-control form-select mb-3">
    <option disabled><?php _e("Choose country"); ?></option>
     <?php foreach($data as $item): ?>
     <option value="<?php print $item['shipping_country'] ?>"  <?php if(isset($selected_country) and $selected_country == $item['shipping_country']): ?> selected="selected" <?php endif; ?>><?php print $item['shipping_country'] ?></option>
