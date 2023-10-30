6ef5280191138e4c9820483597c006e363e19d0a
alex <raikov@ymail.com>
2023-10-30 14:25:19 +0200

    live edit

--- src/MicroweberPackages/LiveEdit/resources/js/api-core/core/handle-icons.js
+++ src/MicroweberPackages/LiveEdit/resources/js/api-core/core/handle-icons.js
@@ -54,6 +54,14 @@ export const HandleIcons = function () {
             name: 'color',
             icon: '<span class="mw-le--handle-icon--color"><span class="mw-le--handle-icon--color-color"></span></span>',
         },
+        {
+            name: 'image-fit',
+            icon: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M14,14H19V16H16V19H14V14M5,14H10V19H8V16H5V14M8,5H10V10H5V8H8V5M19,8V10H14V5H16V8H19Z" /></svg>`,
+        },
+        {
+            name: 'image-fill',
+            icon: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M5,5H10V7H7V10H5V5M14,5H19V10H17V7H14V5M17,14H19V19H14V17H17V14M10,17V19H5V14H7V17H10Z" /></svg>`,
+        },
     ];
 
     this.icon = function (iconName) {
