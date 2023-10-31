42053ae9219497ac652dcdbd0a54d9aa349a1053
Peter Ivanov <peter@microweber.com>
2023-10-31 11:05:07 +0200

    update

--- src/MicroweberPackages/Fortify/database/migrations/2023_10_12_200000_add_two_factor_columns_to_users_table.php
+++ src/MicroweberPackages/Fortify/database/migrations/2023_10_12_200000_add_two_factor_columns_to_users_table.php
@@ -14,13 +14,13 @@ class AddTwoFactorColumnsToUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->text('two_factor_secret')
-                    ->after('password')
-                    ->nullable();
+            if (!Schema::hasColumn('users', 'two_factor_secret')) {
+                $table->text('two_factor_secret')->after('password')->nullable();
+            }
 
-            $table->text('two_factor_recovery_codes')
-                    ->after('two_factor_secret')
-                    ->nullable();
+            if (!Schema::hasColumn('users', 'two_factor_recovery_codes')) {
+                $table->text('two_factor_recovery_codes')->after('two_factor_secret')->nullable();
+            }
         });
     }
 
