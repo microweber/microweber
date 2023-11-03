c7150c6c418fd21072dbb6b55ca22b585e2c94dc
Bozhidar Slaveykov <bobi@microweber.com>
2023-11-03 16:05:42 +0200

    update

--- userfiles/modules/site_stats/src/DispatchServerSideTracking.php
+++ userfiles/modules/site_stats/src/DispatchServerSideTracking.php
@@ -22,5 +22,31 @@ class DispatchServerSideTracking
     public function dispatch()
     {
 
+        $analytics = new Analytics();
+
+        $analytics->setProtocolVersion('1')
+            ->setTrackingId(get_option('google-analytics-id', 'website'))
+            ->setClientId($this->visitorId);
+
+
+        $sendPageView = $analytics->setEventCategory('PageView')
+            ->setEventAction('PageView')
+            ->setEventLabel('PageView')
+            ->setEventValue(1)
+            ->sendEvent();
+
+        dd($sendPageView);
+
+        $getStatsEvents = StatsEvent::where('session_id', $this->session_id)->get();
+        if ($getStatsEvents->count() > 0) {
+            foreach ($getStatsEvents as $getStatsEvent) {
+
+
+                dd($getStatsEvent);
+
+              //  $getStatsEvent->delete();
+            }
+
+        }
     }
 }
