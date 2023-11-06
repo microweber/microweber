efdaac1a6d508655716764805e537b67d5df2266
Bozhidar Slaveykov <bobi@microweber.com>
2023-11-06 15:16:36 +0200

    update

--- userfiles/modules/settings/group/seo.php
+++ userfiles/modules/settings/group/seo.php
@@ -14,7 +14,6 @@
     <input name="<?php print $key_name ?>" class="mw_option_field form-control" type="text" option-group="website" title="google-site-verification" value="<?php print get_option($key_name, 'website'); ?>"/>
 </div>
 
-
  <div class="form-group">
     <label class="form-label"><?php _e("Google Analytics ID"); ?></label>
     <small class="text-muted d-block mb-2"><?php _e("Google Analytics' property ID is the identifier associated with your account and used by Google Analytics to collect the data."); ?> <a href="https://support.google.com/analytics/answer/9539598?hl=en" target="_blank"><?php _e("How to find it read here."); ?></a></small>
@@ -22,6 +21,32 @@
     <input name="<?php print $key_name ?>" class="mw_option_field form-control" type="text" option-group="website" value="<?php print get_option($key_name, 'website'); ?>" placeholder="UA-12345678-9"/>
 </div>
 
+<?php
+
+$isGoogleMesurementEnabled = get_option('google-measurement-enabled', 'website') == "y";
+?>
+<div class="">
+    <label class="form-check-label d-block"><?php _e("Enable Google Measurement"); ?></label>
+    <small class="text-muted d-block mb-2"><?php _e("Choose the status of your google measurement"); ?></small>
+    <label class="form-check form-switch">
+        <input name="google-measurement-enabled" class="form-check-input mw_option_field " data-option-group="website" data-value-checked="n" data-value-unchecked="y" type="checkbox" <?php if (!$isGoogleMesurementEnabled): ?> checked="checked" <?php endif; ?>>
+    </label>
+</div>
+
+
+<div class="form-group">
+    <label class="form-label"><?php _e("Google Measurement ID"); ?></label>
+    <small class="text-muted d-block mb-2"><?php _e("Google measurement' property ID is the identifier associated with your account and used by Google Analytics to collect the data."); ?> <a href="https://support.google.com/analytics/answer/9539598?hl=en" target="_blank"><?php _e("How to find it read here."); ?></a></small>
+    <?php $key_name = 'google-measurement-id'; ?>
+    <input name="<?php print $key_name ?>" class="mw_option_field form-control" type="text" option-group="website" value="<?php print get_option($key_name, 'website'); ?>" placeholder="G-12345678"/>
+</div>
+
+<div class="form-group">
+    <label class="form-label"><?php _e("Google Measurement Api Secret"); ?></label>
+    <small class="text-muted d-block mb-2"><?php _e("Google measurement api secret."); ?> <a href="https://support.google.com/analytics/answer/9539598?hl=en" target="_blank"><?php _e("How to find it read here."); ?></a></small>
+    <?php $key_name = 'google-measurement-api-secret'; ?>
+    <input name="<?php print $key_name ?>" class="mw_option_field form-control" type="text" option-group="website" value="<?php print get_option($key_name, 'website'); ?>" placeholder="G-12345678"/>
+</div>
 
 <?php event_trigger('mw.admin.settings.seo') ?>
 
