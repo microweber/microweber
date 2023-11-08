0f6a60d8029d582195918e68c6d18caf7934cffe
Bozhidar Slaveykov <bobi@microweber.com>
2023-11-08 16:05:35 +0200

    update

---
+++ userfiles/modules/settings/group/seo_tab_content.php
@@ -0,0 +1,112 @@
+<?php must_have_access(); ?>
+
+<script type="text/javascript">
+    $(document).ready(function () {
+        mw.options.form('.<?php print $config['module_class'] ?>', function () {
+            mw.notification.success("<?php _ejs("All changes are saved"); ?>.");
+        });
+    });
+</script>
+
+<div class="form-group">
+    <label class="form-label"><?php _e("Google site verification code"); ?> </label>
+    <small class="text-muted d-block mb-2"><?php _e("If you have a Google Tag Manager account, you can verify ownership of a site using your Google Tag Manager container snippet code. To verify ownership using Google Tag Manager: Choose Google Tag Manager in the verification details page for your site, and follow the instructions shown."); ?> <a href="https://support.google.com/webmasters/answer/9008080?hl=en#choose_method&zippy=%2Chtml-tag" target="_blank"><?php _e("Read the article here."); ?></a></small>
+    <?php $key_name = 'google-site-verification-code'; ?>
+    <input name="<?php print $key_name ?>" class="mw_option_field form-control" type="text" option-group="website" title="google-site-verification" value="<?php print get_option($key_name, 'website'); ?>"/>
+</div>
+
+ <div class="form-group">
+    <label class="form-label"><?php _e("Google Analytics ID"); ?></label>
+    <small class="text-muted d-block mb-2"><?php _e("Google Analytics' property ID is the identifier associated with your account and used by Google Analytics to collect the data."); ?> <a href="https://support.google.com/analytics/answer/9539598?hl=en" target="_blank"><?php _e("How to find it read here."); ?></a></small>
+    <?php $key_name = 'google-analytics-id'; ?>
+    <input name="<?php print $key_name ?>" class="mw_option_field form-control" type="text" option-group="website" value="<?php print get_option($key_name, 'website'); ?>" placeholder="UA-12345678-9"/>
+</div>
+
+<?php
+
+$isGoogleMesurementEnabled = get_option('google-measurement-enabled', 'website') == "y";
+?>
+<div class="">
+    <label class="form-check-label d-block"><?php _e("Enable Google Measurement"); ?></label>
+    <small class="text-muted d-block mb-2"><?php _e("Choose the status of your google measurement"); ?></small>
+    <label class="form-check form-switch">
+        <input name="google-measurement-enabled" class="form-check-input mw_option_field " data-option-group="website" data-value-checked="n" data-value-unchecked="y" type="checkbox" <?php if (!$isGoogleMesurementEnabled): ?> checked="checked" <?php endif; ?>>
+    </label>
+</div>
+
+
+<div class="form-group">
+    <label class="form-label"><?php _e("Google Measurement ID"); ?></label>
+    <small class="text-muted d-block mb-2"><?php _e("Google measurement' property ID is the identifier associated with your account and used by Google Analytics to collect the data."); ?> <a href="https://support.google.com/analytics/answer/9539598?hl=en" target="_blank"><?php _e("How to find it read here."); ?></a></small>
+    <?php $key_name = 'google-measurement-id'; ?>
+    <input name="<?php print $key_name ?>" class="mw_option_field form-control" type="text" option-group="website" value="<?php print get_option($key_name, 'website'); ?>" placeholder="G-12345678"/>
+</div>
+
+<div class="form-group">
+    <label class="form-label"><?php _e("Google Measurement Api Secret"); ?></label>
+    <small class="text-muted d-block mb-2"><?php _e("Google measurement api secret."); ?> <a href="https://support.google.com/analytics/answer/9539598?hl=en" target="_blank"><?php _e("How to find it read here."); ?></a></small>
+    <?php $key_name = 'google-measurement-api-secret'; ?>
+    <input name="<?php print $key_name ?>" class="mw_option_field form-control" type="text" option-group="website" value="<?php print get_option($key_name, 'website'); ?>" placeholder="G-12345678"/>
+</div>
+
+<?php event_trigger('mw.admin.settings.seo') ?>
+
+
+
+
+
+
+
+
+<div class="form-group">
+    <label class="form-label"><?php _e("Facebook pixel ID"); ?> </label>
+    <small class="text-muted d-block mb-2"><?php _e("You can find a tutorials in internet where and how to find the code."); ?></small>
+    <?php $key_name = 'facebook-pixel-id'; ?>
+    <input name="<?php print $key_name ?>" class="mw_option_field form-control" type="text" option-group="website" value="<?php print get_option($key_name, 'website'); ?>"/>
+</div>
+
+<a href="javascript:$('.other-site-verification-codes-hidden-toggle').toggle();void(0)" class="btn btn-outline-primary btn-sm my-3"><?php _e('Other search engines'); ?></a>
+<script>
+    $(document).ready(function () {
+        $('#js-static_files_delivery_method_select').on('change', function () {
+            if (this.value == 'cdn_domain' || this.value == 'content_proxy') {
+                $(".js-toggle-content-proxy-settings").show();
+            }
+            else {
+                $(".js-toggle-content-proxy-settings").hide();
+            }
+        });
+    });
+</script>
+
+<div class="other-site-verification-codes-hidden-toggle" style="display: none">
+
+    <div class="form-group">
+        <label class="form-label"><?php _e("Bing"); ?> </label>
+        <small class="text-muted d-block mb-2"><?php _e("You can find a tutorials in internet where and how to find the code."); ?></small>
+        <?php $key_name = 'bing-site-verification-code'; ?>
+        <input name="<?php print $key_name ?>" class="mw_option_field form-control" type="text" option-group="website" value="<?php print get_option($key_name, 'website'); ?>"/>
+    </div>
+
+    <div class="form-group">
+        <label class="form-label"><?php _e("Alexa"); ?> </label>
+        <small class="text-muted d-block mb-2"><?php _e("You can find a tutorials in internet where and how to find the code."); ?></small>
+        <?php $key_name = 'alexa-site-verification-code'; ?>
+        <input name="<?php print $key_name ?>" class="mw_option_field form-control" type="text" option-group="website" value="<?php print get_option($key_name, 'website'); ?>"/>
+    </div>
+
+    <div class="form-group">
+        <label class="form-label"><?php _e("Pinterest"); ?> </label>
+        <small class="text-muted d-block mb-2"><?php _e("You can find a tutorials in internet where and how to find the code."); ?></small>
+        <?php $key_name = 'pinterest-site-verification-code'; ?>
+        <input name="<?php print $key_name ?>" class="mw_option_field form-control" type="text" option-group="website" value="<?php print get_option($key_name, 'website'); ?>"/>
+    </div>
+
+    <div class="form-group">
+        <label class="form-label">
+            <?php _e("Site verification code for"); ?> <?php _e("Yandex"); ?> </label>
+        <small class="text-muted d-block mb-2"><?php _e("You can find a tutorials in internet where and how to find the code."); ?></small>
+        <?php $key_name = 'yandex-site-verification-code'; ?>
+        <input name="<?php print $key_name ?>" class="mw_option_field form-control" type="text" option-group="website" value="<?php print get_option($key_name, 'website'); ?>"/>
+    </div>
+</div>
