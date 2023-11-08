6fe135db83e188705b7f8380f262b56dc1d85acd
Peter Ivanov <peter@microweber.com>
2023-11-08 22:39:39 +0200

    update

--- config/debugbar.php
+++ config/debugbar.php
@@ -14,8 +14,8 @@ return [
      |
      */
 
-//    'enabled' =>    env('DEBUGBAR_ENABLED', false),
-    'enabled' =>    0,
+    'enabled' =>    env('DEBUGBAR_ENABLED', false),
+ //   'enabled' =>    0,
     'except' => [
         'telescope*'
     ],
