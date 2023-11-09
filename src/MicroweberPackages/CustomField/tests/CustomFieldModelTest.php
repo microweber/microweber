4febd85fd37a394e730568b8cf386cb0dc50a5c3
Peter Ivanov <peter@microweber.com>
2023-11-09 12:32:42 +0200

    update

--- src/MicroweberPackages/CustomField/tests/CustomFieldModelTest.php
+++ src/MicroweberPackages/CustomField/tests/CustomFieldModelTest.php
@@ -15,16 +15,16 @@ class CustomFieldModelTest extends TestCase
         $newProduct->setCustomFields(
             [
                 [
-                    'type'=>'price',
-                    'name'=>'цена на едро',
-                    'value'=>['цска', 'цска 1948'],
-                    'options'=>['team1' => 'levski', 'team2' => 'cska'],
+                    'type' => 'price',
+                    'name' => 'цена на едро',
+                    'value' => ['цска', 'цска 1948'],
+                    'options' => ['team1' => 'levski', 'team2' => 'cska'],
                 ],
                 [
-                    'type'=>'dropdown',
-                    'name'=>'цена 2',
-                    'value'=>['цска2', 'цска2 1948'],
-                    'options'=>['team1' => 'levski', 'team2' => 'cska'],
+                    'type' => 'dropdown',
+                    'name' => 'цена 2',
+                    'value' => ['цска2', 'цска2 1948'],
+                    'options' => ['team1' => 'levski', 'team2' => 'cska'],
                 ]
             ]
         );
@@ -42,10 +42,10 @@ class CustomFieldModelTest extends TestCase
 
         $newProduct->setCustomField(
             [
-                'type'=>'price',
-                'name'=>'цена на едро',
-                'value'=>['цска', 'цска 1948'],
-                'options'=>['team1' => 'levski', 'team2' => 'cska'],
+                'type' => 'price',
+                'name' => 'цена на едро',
+                'value' => ['цска', 'цска 1948'],
+                'options' => ['team1' => 'levski', 'team2' => 'cska'],
             ]
         );
 
@@ -56,42 +56,40 @@ class CustomFieldModelTest extends TestCase
 
     public function testGetCustomFieldModel()
     {
-        $title =  'Samo Levski3'.uniqid();
+        $title = 'Samo Levski3' . uniqid();
 
         $newProduct = new Product();
         $newProduct->title = $title;
 
         $newProduct->setCustomField(
             [
-                'type'=>'dropdown',
-                'name'=>'цвят',
-                'value'=>['red', 'blue', 'зелен'],
-                'options'=>[],
+                'type' => 'dropdown',
+                'name' => 'цвят',
+                'value' => ['red', 'blue', 'зелен'],
+                'options' => [],
 
             ]
         );
-       $newProduct->setCustomField(
+        $newProduct->setCustomField(
             [
-                'type'=>'dropdown',
-                'name'=>'size',
-                'value'=>['XL', 'M'],
-                'options'=>[],
+                'type' => 'dropdown',
+                'name' => 'size',
+                'value' => ['XL', 'M'],
+                'options' => [],
 
             ]
         );
 
 
-
-        $some_random =  'some-material-'.uniqid();
-
+        $some_random = 'some-material-' . uniqid();
 
 
         $newProduct->setCustomField(
             [
-                'type'=>'dropdown',
-                'name'=>'material',
-                'value'=>['jeans', 'cotton',$some_random],
-                'options'=>[],
+                'type' => 'dropdown',
+                'name' => 'material',
+                'value' => ['jeans', 'cotton', $some_random],
+                'options' => [],
 
             ]
         );
@@ -100,10 +98,30 @@ class CustomFieldModelTest extends TestCase
         $newProduct->save();
 
         $prod = Product::whereCustomField([
-            'material'=>$some_random,
+            'material' => $some_random,
         ])->first();
 
         $this->assertEquals($prod->title, $title);
 
-     }
+
+        // get all fields
+        $prod = Product::find($newProduct->id);
+        $fields = $prod->customField()->get();
+        $this->assertNotEmpty($fields);
+
+        foreach ($fields as $field) {
+            $this->assertNotEmpty($field->fieldValue()->get());
+        }
+        $fields_arr = $fields->toArray();
+        $name_keys = array_column($fields_arr, 'name_key');
+        $this->assertContains('material', $name_keys);
+        $this->assertContains('size', $name_keys);
+        $this->assertContains('cviat', $name_keys);
+
+        $names = array_column($fields_arr, 'name');
+        $this->assertContains('material', $names);
+        $this->assertContains('size', $names);
+        $this->assertContains('цвят', $names);
+
+    }
 }
