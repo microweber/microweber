cf55fb02ff3e1c0ea3a3ab4d625760a3cf7c2461
Bozhidar Slaveykov <bobi@microweber.com>
2023-11-13 16:47:28 +0200

    update

--- src/MicroweberPackages/Template/helpers/templates.php
+++ src/MicroweberPackages/Template/helpers/templates.php
@@ -290,3 +290,51 @@ function get_template_meta_tags_render()
     return $in->get_template_meta_tags_render();
 
 }
+
+function mw_header_scripts()
+{
+    $header = mw()->template->head(true);
+
+    $template_headers_src_callback = mw()->template->head_callback();
+    if (is_array($template_headers_src_callback) and !empty($template_headers_src_callback)) {
+        foreach ($template_headers_src_callback as $template_headers_src_callback_str) {
+            if (is_string($template_headers_src_callback_str)) {
+                $header .= "\n" . $template_headers_src_callback_str;
+            }
+        }
+    }
+
+    $metaTags = get_template_meta_tags_render();
+    if (!empty($metaTags)) {
+        foreach ($metaTags as $metaTag) {
+            $header .= "\n" . $metaTag;
+        }
+    }
+
+    $getWebsiteHeadOption = get_option('website_head', 'website');
+    if ($getWebsiteHeadOption != false) {
+        $header .= "\n" . $getWebsiteHeadOption;
+    }
+
+    return $header;
+}
+
+function mw_footer_scripts()
+{
+    $footer = mw()->template->foot(true);
+    $template_footer_src_callback = mw()->template->foot_callback();
+    if (is_array($template_footer_src_callback) and !empty($template_footer_src_callback)) {
+        foreach ($template_footer_src_callback as $template_footer_src_callback_str) {
+            if (is_string($template_footer_src_callback_str)) {
+                $footer .= "\n" . $template_footer_src_callback_str;
+            }
+        }
+    }
+
+    $getWebsiteFooterOption = get_option('website_footer', 'website');
+    if ($getWebsiteFooterOption != false) {
+        $footer .= "\n" . $getWebsiteFooterOption;
+    }
+
+    return $footer;
+}
