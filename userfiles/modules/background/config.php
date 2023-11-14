1c78d6b727fd90a2f0d8a94f9427aa6f9a080e16
alex <raikov@ymail.com>
2023-11-14 14:33:32 +0200

    live edit

--- userfiles/modules/background/config.php
+++ userfiles/modules/background/config.php
@@ -7,7 +7,7 @@ $config['description'] = "Microweber";
 $config['website'] = "http://microweber.com/";
 $config['help'] = "http://microweber.info/modules/background";
 $config['version'] = 1.1;
-$config['ui'] = true;
+$config['ui'] = false;
 $config['position'] = 333;
 $config['categories'] = "media";
 
