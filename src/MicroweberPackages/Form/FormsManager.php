515895821998b17399c97a332df4169392a82f2e
Bozhidar Slaveykov <bobi@microweber.com>
2023-11-15 12:57:42 +0200

    Update FormsManager.php

--- src/MicroweberPackages/Form/FormsManager.php
+++ src/MicroweberPackages/Form/FormsManager.php
@@ -611,8 +611,8 @@ class FormsManager
                         break;
                     }
 
+                    $target_path_name = md5($file['name'].time()) . $target_path_name;
                     $targetFileName = $target_path_name . '/' . $file['name'];
-
                     if (is_file($target_path . '/' . $file['name'])) {
                         $targetFileName = $target_path_name . '/' . date('Ymd-His') . $file['name'];
                     }
