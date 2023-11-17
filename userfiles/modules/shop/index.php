fb3fdbd0045456e4c904ed00444ff0a6f862966a
Bozhidar Slaveykov <bobi@microweber.com>
2023-11-17 17:21:16 +0200

    update

--- userfiles/modules/shop/index.php
+++ userfiles/modules/shop/index.php
@@ -1,6 +1,21 @@
 <?php
-echo view('microweber-module-shop::render-livewire-shop',[
-    'moduleId'=> $params['id'],
-    'moduleType'=>'shop',
-]);
+$controller = \Illuminate\Support\Facades\App::make(\MicroweberPackages\Shop\Http\Controllers\ShopController::class);
+
+$request = new \Illuminate\Http\Request();
+$request->merge($params);
+$request->merge($_REQUEST);
+
+$controller->setModuleParams($params);
+$controller->setModuleConfig($config);
+$controller->registerModule();
+
+echo $controller->index($request);
+
+//
+//echo view('microweber-module-shop::render-livewire-shop', [
+//    'moduleId' => $params['id'],
+//    'moduleType' => 'shop',
+//]);
+
+
 ?>
