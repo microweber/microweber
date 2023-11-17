ae0f0ad23dc0189342c8e18b96f33527a122b326
Nikolay <rachkuw@gmail.com>
2023-11-17 17:20:04 +0200

    update

--- src/MicroweberPackages/Customer/resources/views/admin/livewire/display-types/card.blade.php
+++ src/MicroweberPackages/Customer/resources/views/admin/livewire/display-types/card.blade.php
@@ -7,7 +7,7 @@
 
                     <div class="col-auto mx-2 my-md-0 my-2 text-center d-flex align-items-center">
                          <span class="cursor-move-holder me-2 js-move mw_admin_posts_sortable_handle" onmousedown="mw.manage_content_sort()" style="max-width: 80px;">
-                              <span href="javascript:;" class="btn btn-link text-blue-lt">
+                              <span href="javascript:;" class="btn btn-link text-blue-lt tblr-body-color">
                                   <svg class="mdi-cursor-move" fill="currentColor" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 96 960 960" width="24"><path d="M360 896q-33 0-56.5-23.5T280 816q0-33 23.5-56.5T360 736q33 0 56.5 23.5T440 816q0 33-23.5 56.5T360 896Zm240 0q-33 0-56.5-23.5T520 816q0-33 23.5-56.5T600 736q33 0 56.5 23.5T680 816q0 33-23.5 56.5T600 896ZM360 656q-33 0-56.5-23.5T280 576q0-33 23.5-56.5T360 496q33 0 56.5 23.5T440 576q0 33-23.5 56.5T360 656Zm240 0q-33 0-56.5-23.5T520 576q0-33 23.5-56.5T600 496q33 0 56.5 23.5T680 576q0 33-23.5 56.5T600 656ZM360 416q-33 0-56.5-23.5T280 336q0-33 23.5-56.5T360 256q33 0 56.5 23.5T440 336q0 33-23.5 56.5T360 416Zm240 0q-33 0-56.5-23.5T520 336q0-33 23.5-56.5T600 256q33 0 56.5 23.5T680 336q0 33-23.5 56.5T600 416Z"/></svg>
                               </span>
                         </span>
