1e20457830e89f626419893a597a85a40d9c8bc3
Nikolay <rachkuw@gmail.com>
2023-11-17 15:45:45 +0200

    update

--- src/MicroweberPackages/Blog/resources/views/partials/filters_active.blade.php
+++ src/MicroweberPackages/Blog/resources/views/partials/filters_active.blade.php
@@ -7,7 +7,9 @@ $randomId = uniqid();
 
 <div class="card-body px-1">
     @foreach($filtersActive as $filter)
-       <button type="button" data-key="{{$filter->key}}" data-value="{{$filter->value}}" class="btn btn-primary btn-sm mb-2 px-1 js-filter-picked"> &nbsp;&nbsp; {{$filter->name}} <i class="mdi mdi-close ml-2"></i></button>
+       <button type="button" data-key="{{$filter->key}}" data-value="{{$filter->value}}" class="btn btn-outline-primary mb-2 btn-sm px-1 js-filter-picked"> &nbsp;&nbsp; {{$filter->name}}
+           <svg class="me-2" style="position: relative; bottom: 2px;" fill="currentColor" xmlns="http://www.w3.org/2000/svg" height="18" viewBox="0 -960 960 960" width="18"><path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"/></svg>
+       </button>
     @endforeach
 </div>
 
