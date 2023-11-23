09af510a59b25ba312e0e94cd7c92faa2247d8fa
Peter Ivanov <peter@microweber.com>
2023-11-23 14:29:12 +0200

    update

--- src/MicroweberPackages/LiveEdit/resources/js/ui/components/Toolbar/SettingsCustomize.vue
+++ src/MicroweberPackages/LiveEdit/resources/js/ui/components/Toolbar/SettingsCustomize.vue
@@ -10,8 +10,8 @@
 
 
 
-        <div class="dropdown" >
-            <a class="btn-icon live-edit-toolbar-buttons "   role="button" id="dropdownLiveEditMenuLinkMoreSettings" data-bs-toggle="dropdown" aria-expanded="false">
+        <div class="dropdown btn-icon live-edit-toolbar-buttons " >
+            <a role="button" id="dropdownLiveEditMenuLinkMoreSettings" data-bs-toggle="dropdown" aria-expanded="false">
                 <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path
                     d="M480-345 240-585l56-56 184 184 184-184 56 56-240 240Z"/></svg>
             </a>
