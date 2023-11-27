0288af6020410c59563032f86c8a8533efab4d60
Peter Ivanov <peter@microweber.com>
2023-11-27 10:27:38 +0200

    update

--- src/MicroweberPackages/Template/resources/views/livewire/live-edit/template-setting-render-color-palette-item.blade.php
+++ src/MicroweberPackages/Template/resources/views/livewire/live-edit/template-setting-render-color-palette-item.blade.php
@@ -1,6 +1,48 @@
 <div class="list-group list-group list-group-flush overflow-auto"
      style="max-height: 500px; margin-right: 20px; margin-top: 20px;">
 
+    @if(isset($setting['fieldSettings']['colorPaletteFromTemplateFolderLibrary']) and $setting['fieldSettings']['colorPaletteFromTemplateFolderLibrary'])
+        @php
+            if(!isset($setting['fieldSettings']['colors'])) {
+                    $setting['fieldSettings']['colors'] = [];
+                }
+            $folders = $setting['fieldSettings']['colorPaletteFromTemplateFolderLibrary'];
+
+            foreach ($folders as $folder){
+
+               /* "colorPaletteFromTemplateFolderLibrary": [
+                                   "assets/design-styles/colors"
+                               ],*/
+               // get json from folder library and append to colors
+               $templateColorsFolderExits  = templates_dir() . template_name() . DS .$folder;
+               $templateColorsFolderExits = normalize_path($templateColorsFolderExits);
+
+               if(is_dir($templateColorsFolderExits)){
+                   // get json files from folder
+                   $templateColorsFolderExitsContent = glob($templateColorsFolderExits . DS . '*.json');
+                   if(is_array($templateColorsFolderExitsContent)){
+                       foreach ($templateColorsFolderExitsContent as $templateColorsFolderExitsContentItem) {
+
+                           if(is_file($templateColorsFolderExitsContentItem)){
+                               $templateColorsFileExitsContent = @file_get_contents($templateColorsFolderExitsContentItem);
+                               $templateColorsFileExitsContentItem = @json_decode($templateColorsFileExitsContent, true);
+                               if(is_array($templateColorsFileExitsContentItem)){
+
+                                       if(isset($templateColorsFileExitsContentItem['name']) and isset($templateColorsFileExitsContentItem['mainColors'])){
+                                           $setting['fieldSettings']['colors'][] = $templateColorsFileExitsContentItem;
+                                       }
+
+                               }
+                           }
+                       }
+                   }
+               }
+             }
+
+
+        @endphp
+    @endif
+
     @if(isset($setting['fieldSettings']['colorPaletteFromTemplateFilesLibrary']))
 
         @php
@@ -43,7 +85,6 @@
             <div class="list-group-item"
 
 
-
                  x-on:click="(e) => {
                                     mw.top().app.cssEditor.setMultiplePropertiesForSelector('{{end($setting['selectors'])}}', [
                                     @foreach($colorPallete['properties'] as $property=>$propertyValue)
@@ -65,7 +106,6 @@
 
 
             >
-
 
 
                 <div class="row align-items-center">
