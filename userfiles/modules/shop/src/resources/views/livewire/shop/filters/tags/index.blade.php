6442c090d4634bb09d1e9a9100413c37c2f3329d
Bozhidar Slaveykov <bobi@microweber.com>
2023-11-28 12:51:34 +0200

    Update index.blade.php

--- userfiles/modules/shop/src/resources/views/livewire/shop/filters/tags/index.blade.php
+++ userfiles/modules/shop/src/resources/views/livewire/shop/filters/tags/index.blade.php
@@ -1,13 +1,27 @@
 <div class="text-left">
     <div>Tags</div>
     <div>
-        @foreach($availableTags as $tagSlug=>$tagName)
+        @php
+            $limited = 5;
+            $limitedAvailableTags = [];
+            $moreAvailableTags = [];
+            $moreAvailableTagsIndex = 0;
+            foreach ($availableTags as $tagSlug=>$tagName) {
+                if ($moreAvailableTagsIndex >= $limited) {
+                    $moreAvailableTags[] = $tagSlug;
+                    continue;
+                }
+                $limitedAvailableTags[$tagSlug] = $tagName;
+                $moreAvailableTagsIndex++;
+            }
+        @endphp
+        @foreach($limitedAvailableTags as $tagSlug=>$tagName)
             <button type="button" class="btn btn-outline-primary btn-sm mt-2">
                 <span wire:click="filterTag('{{$tagSlug}}')"> {{$tagName}}</span>
                 @if(in_array($tagSlug, $filteredTags))
                     <span wire:click="filterRemoveTag('{{$tagSlug}}')">
-                                    X
-                        </span>
+                                X
+                    </span>
                 @endif
             </button>
         @endforeach
