9f739ddc7476a72d8a5b578d46ad7c057557a18c
Bozhidar Slaveykov <bobi@microweber.com>
2023-12-05 14:01:15 +0200

    update

--- userfiles/modules/newsletter/src/Console/Commands/ProcessCampaigns.php
+++ userfiles/modules/newsletter/src/Console/Commands/ProcessCampaigns.php
@@ -52,6 +52,32 @@ class ProcessCampaigns extends Command
             foreach ($getCampaigns as $campaign) {
                 $this->info('Processing Campaign: ' . $campaign->name);
 
+                $template = newsletter_get_template(array("id"=>$campaign->email_template_id));
+                $subscribers = newsletter_get_subscribers_for_list($campaign->list_id);
+                $sender = newsletter_get_sender(array("id"=>$campaign->sender_account_id));
+
+                foreach($subscribers as $subscriber) {
+
+                    $newsletterMailSender = new \Newsletter\Senders\NewsletterMailSender();
+                    $newsletterMailSender->setCampaign($campaign);
+                    $newsletterMailSender->setSubscriber($subscriber);
+                    $newsletterMailSender->setSender($sender);
+                    $newsletterMailSender->setTemplate($template);
+
+                    $sendMailResponse = $newsletterMailSender->sendMail();
+
+                    echo 'Subscriber: ' . $subscriber['name'] . ' (' . $subscriber['email'] . ') <br />';
+
+                    if ($sendMailResponse['success']) {
+                        echo '<font style="color:green;">' . $sendMailResponse['message'] . '</font>';
+                        newsletter_campaigns_send_log($campaign['id'], $subscriber['id']);
+                    } else {
+                        echo '<font style="color:red;">' . $sendMailResponse['message'] . '</font>';
+                    }
+
+                    echo '<br />';
+                }
+
 
             }
         }
