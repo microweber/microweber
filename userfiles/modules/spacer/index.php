1a60c809a6a288d9d3e432605e22315f85f94e46
alex <raikov@ymail.com>
2023-12-06 16:26:27 +0200

    spacer outline

--- userfiles/modules/spacer/index.php
+++ userfiles/modules/spacer/index.php
@@ -24,4 +24,43 @@ if(!empty($styles)) {
 
 
 
-?><div class="mw-le-spacer noelement nodrop inaccessibleModuleIfFirstParentIsLayout" data-for-module-id="<?php print $params['id'] ?>" contenteditable="false" <?php print $styles_attr ?> id="spacer-item-<?php print $params['id'] ?>"></div>
+?>
+<style>
+    #<?php print $params['id'] ?>, .mw-spacer-disable-settings--<?php print $params['id'] ?>{
+        pointer-events: none;
+    }
+    .mw-spacer-disable-settings--<?php print $params['id'] ?> > *{
+        pointer-events: all;
+    }
+</style>
+
+<div class="mw-le-spacer noelement nodrop inaccessibleModuleIfFirstParentIsLayout" data-for-module-id="<?php print $params['id'] ?>" contenteditable="false" <?php print $styles_attr ?> id="spacer-item-<?php print $params['id'] ?>"></div>
+<script>
+    ;(function(){
+
+        function init() {
+            // todo: move to service
+
+            var spacer = document.getElementById('spacer-item-<?php print $params['id'] ?>');
+            if(spacer) {
+
+                if(mw.top().app && mw.top().app.liveEdit && mw.top().app.liveEdit.elementAnalyzer && !mw.top().app.liveEdit.elementAnalyzer.isInEdit(spacer.parentElement))  {
+                    spacer.classList.add('mw-spacer-disable-settings--<?php print $params['id'] ?>')
+                    spacer.parentElement.classList.add('no-settings', 'no-element')
+                    spacer.parentElement.classList.remove('module')
+                }
+            }
+        }
+
+        if(!mw.top().win.__spacerDisableSettingsEvent) {
+            mw.top().win.__spacerDisableSettingsEvent = true;
+            mw.top().win.addEventListener('load', init)
+        }
+
+
+
+        init()
+
+
+    })();
+</script>
