cb1d4230498b9f429d06960d289601256f2d0f08
Peter Ivanov <peter@microweber.com>
2023-12-07 16:47:00 +0200

    update

--- src/MicroweberPackages/LiveEdit/resources/js/ui/apps/ElementStyleEditor/ElementStyleEditorApp.vue
+++ src/MicroweberPackages/LiveEdit/resources/js/ui/apps/ElementStyleEditor/ElementStyleEditorApp.vue
@@ -2,28 +2,12 @@
 
     <ElementStyleEditorActiveNode></ElementStyleEditorActiveNode>
 
-    <div class="mb-4" v-show="!selectedElement">
-      <div   v-show="!selectedLayout">
-        <div class="alert alert-info" role="alert">
-            <div class="d-flex">
-                <div>
-                    <svg xmlns="http://www.w3.org/2000/svg" class="icon alert-icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><path d="M3 12a9 9 0 1 0 18 0a9 9 0 0 0 -18 0"></path><path d="M12 9h.01"></path><path d="M11 12h1v4h1"></path></svg>
-                </div>
-                <div>
-                    Please select an element
-                </div>
-            </div>
-        </div>
-      </div>
-    </div>
 
-    <div class="mb-4" v-show="selectedLayout">
 
-        <ElementStyleEditorLayoutSettings></ElementStyleEditorLayoutSettings>
 
-    </div>
 
-    <div class="mb-4" v-show="selectedElement">
+
+    <div class="mb-4" :class="{'style-editor-disabled': !selectedElement}">
         <div class="d-flex flex-column">
 
             <div class="mb-4" v-show="showElementSelector">
