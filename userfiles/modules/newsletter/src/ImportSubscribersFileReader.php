e7611c9ef1efaa62ac459354c51df2edb9352df0
Bozhidar Slaveykov <bobi@microweber.com>
2024-01-03 17:44:25 +0200

    update

--- userfiles/modules/newsletter/src/ImportSubscribersFileReader.php
+++ userfiles/modules/newsletter/src/ImportSubscribersFileReader.php
@@ -103,11 +103,23 @@ class ImportSubscribersFileReader
                 if (isset($subscriber['email'])) {
                     $subscriberData['email'] = $subscriber['email'];
                 }
+                if (isset($subscriber['Email'])) {
+                    $subscriberData['Email'] = $subscriber['Email'];
+                }
                 if (isset($subscriber['name'])) {
                     $subscriberData['name'] = $subscriber['name'];
                 }
+                if (isset($subscriber['Name'])) {
+                    $subscriberData['Name'] = $subscriber['Name'];
+                }
                 if (isset($subscriber['first_name'])) {
                     $subscriberData['first_name'] = $subscriber['first_name'];
+                }
+                if (isset($subscriber['FirstName'])) {
+                    $subscriberData['FirstName'] = $subscriber['FirstName'];
+                }
+                if (isset($subscriber['First Name'])) {
+                    $subscriberData['First Name'] = $subscriber['First Name'];
                 }
                 if (isset($subscriber['last_name'])) {
                     $subscriberData['last_name'] = $subscriber['last_name'];
