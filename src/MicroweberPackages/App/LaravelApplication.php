206c0de72eeddf5b5d4b03736509a72fa9bd3ecb
Peter Ivanov <peter@microweber.com>
2024-01-18 17:40:44 +0200

    update

--- src/MicroweberPackages/App/LaravelApplication.php
+++ src/MicroweberPackages/App/LaravelApplication.php
@@ -14,7 +14,7 @@ class LaravelApplication extends Application
 {
 
     //remember to change also in version.txt
-    const APP_VERSION = '2.0.8';
+    const APP_VERSION = '2.0.9';
 
 
     private $base_path_local;
