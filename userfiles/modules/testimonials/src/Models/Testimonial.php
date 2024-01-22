a85648c61d078bf52d0e3df8c6d96acb95924358
Peter Ivanov <peter@microweber.com>
2024-01-22 15:52:12 +0200

    update

--- userfiles/modules/testimonials/src/Models/Testimonial.php
+++ userfiles/modules/testimonials/src/Models/Testimonial.php
@@ -2,6 +2,7 @@
 namespace MicroweberPackages\Modules\Testimonials\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use MicroweberPackages\Database\Casts\ReplaceSiteUrlCast;
 
 class Testimonial extends Model
 {
@@ -19,5 +20,10 @@ class Testimonial extends Model
         'client_website',
         'position'
     ];
+    protected $casts = [
+         'content' => ReplaceSiteUrlCast::class, //Casts like that: http://lorempixel.com/400/200/ =>  {SITE_URL}400/200/
+         'client_picture' => ReplaceSiteUrlCast::class, //Casts like that: http://lorempixel.com/400/200/ =>  {SITE_URL}400/200/
+    ];
+
 
 }
