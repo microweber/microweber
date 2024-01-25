25992f0e9319d80a3e75eaa385fbd27bd3fc15cc
Bozhidar Slaveykov <bobi@microweber.com>
2024-01-25 17:17:13 +0200

    Update config.php

--- userfiles/modules/newsletter/config.php
+++ userfiles/modules/newsletter/config.php
@@ -6,7 +6,7 @@ $config['ui'] = true;
 $config['ui_admin'] = true;
 $config['categories'] = 'marketing';
 $config['position'] = 55;
-$config['version'] = "1.7";
+$config['version'] = "2.0";
 
 $config['tables'] = array (
 
