3cb74f343ceb0a4b29c66ffb646cb0cb8877d9fb
alex <raikov@ymail.com>
2024-01-29 13:11:50 +0200

    form file

--- src/MicroweberPackages/LiveEdit/resources/js/api-core/core/core/forms.js
+++ src/MicroweberPackages/LiveEdit/resources/js/api-core/core/core/forms.js
@@ -1,31 +1,31 @@
 mw.serializeFields =  function(id, ignorenopost){
-    ignorenopost = ignorenopost || false;
+    var ignorenopost = ignorenopost || false;
     var el = mw.$(id);
     var fields = "input[type='text'], input[type='email'], input[type='number'], input[type='tel'], "
-        + "input[type='color'], input[type='url'], input[type='week'], input[type='search'], input[type='range'], "
-        + "input[type='datetime-local'], input[type='month'], "
-        + "input[type='password'], input[type='hidden'], input[type='datetime'], input[type='date'], input[type='time'], "
-        +"input[type='email'],  textarea, select, input[type='checkbox']:checked, input[type='radio']:checked, "
-        +"input[type='checkbox'][data-value-checked][data-value-unchecked]";
+                + "input[type='color'], input[type='url'], input[type='week'], input[type='search'], input[type='range'], "
+                + "input[type='datetime-local'], input[type='month'], input[type='file'], "
+                + "input[type='password'], input[type='hidden'], input[type='datetime'], input[type='date'], input[type='time'], "
+                +"input[type='email'],  textarea, select, input[type='checkbox']:checked, input[type='radio']:checked, "
+                +"input[type='checkbox'][data-value-checked][data-value-unchecked]";
     var data = {};
+
     $(fields, el).each(function(){
-        if(!this.name){
-            console.warn('Name attribute missing on ' + this.outerHTML);
-        }
-        if((!$(this).hasClass('no-post') || ignorenopost) && !this.disabled && this.name && typeof this.name != 'undefined'){
-            var el = this, _el = $(el);
-            var val = _el.val();
-            var name = el.name;
-            if(el.name.contains("[]")){
-                data[name] = data[name] || []
-                data[name].push(val);
-            }
-            else if(el.type === 'checkbox' && el.getAttribute('data-value-checked') ){
-                data[name] = el.checked ? el.getAttribute('data-value-checked') : el.getAttribute('data-value-unchecked');
-            }
-            else{
-                data[name] = val;
-            }
+
+        if((!$(this).hasClass('no-post') || ignorenopost) && !this.disabled && this.name && typeof this.name !== 'undefined'){
+          var el = this, _el = $(el);
+          var val = _el.val();
+          var name = el.name;
+
+          if (el.type === 'file' && el.files[0]) {
+              data[name] = el.files[0];
+          } else if(el.name.contains("[]")){
+              data[name] = data[name] || []
+              data[name].push(val);
+          } else if(el.type === 'checkbox' && el.getAttribute('data-value-checked') ){
+              data[name] = el.checked ? el.getAttribute('data-value-checked') : el.getAttribute('data-value-unchecked');
+          } else{
+            data[name] = val;
+          }
         }
     });
     return data;
--- userfiles/modules/microweber/api/api.js
+++ userfiles/modules/microweber/api/api.js
@@ -955,35 +955,37 @@ mw.requireAsync = (url, key) => {
   mw.get_content = get_content
 
   mw.serializeFields =  function(id, ignorenopost){
-        var ignorenopost = ignorenopost || false;
-        var el = mw.$(id);
-        var fields = "input[type='text'], input[type='email'], input[type='number'], input[type='tel'], "
-                    + "input[type='color'], input[type='url'], input[type='week'], input[type='search'], input[type='range'], "
-                    + "input[type='datetime-local'], input[type='month'], "
-                    + "input[type='password'], input[type='hidden'], input[type='datetime'], input[type='date'], input[type='time'], "
-                    +"input[type='email'],  textarea, select, input[type='checkbox']:checked, input[type='radio']:checked, "
-                    +"input[type='checkbox'][data-value-checked][data-value-unchecked]";
-        var data = {};
-        $(fields, el).each(function(){
+    var ignorenopost = ignorenopost || false;
+    var el = mw.$(id);
+    var fields = "input[type='text'], input[type='email'], input[type='number'], input[type='tel'], "
+                + "input[type='color'], input[type='url'], input[type='week'], input[type='search'], input[type='range'], "
+                + "input[type='datetime-local'], input[type='month'], input[type='file'], "
+                + "input[type='password'], input[type='hidden'], input[type='datetime'], input[type='date'], input[type='time'], "
+                +"input[type='email'],  textarea, select, input[type='checkbox']:checked, input[type='radio']:checked, "
+                +"input[type='checkbox'][data-value-checked][data-value-unchecked]";
+    var data = {};
 
-            if((!$(this).hasClass('no-post') || ignorenopost) && !this.disabled && this.name && typeof this.name !== 'undefined'){
-              var el = this, _el = $(el);
-              var val = _el.val();
-              var name = el.name;
-              if(el.name.contains("[]")){
-                  data[name] = data[name] || []
-                  data[name].push(val);
-              }
-              else if(el.type === 'checkbox' && el.getAttribute('data-value-checked') ){
-                  data[name] = el.checked ? el.getAttribute('data-value-checked') : el.getAttribute('data-value-unchecked');
-              }
-              else{
-                data[name] = val;
-              }
-            }
-        });
-        return data;
-   }
+    $(fields, el).each(function(){
+
+        if((!$(this).hasClass('no-post') || ignorenopost) && !this.disabled && this.name && typeof this.name !== 'undefined'){
+          var el = this, _el = $(el);
+          var val = _el.val();
+          var name = el.name;
+
+          if (el.type === 'file' && el.files[0]) {
+              data[name] = el.files[0];
+          } else if(el.name.contains("[]")){
+              data[name] = data[name] || []
+              data[name].push(val);
+          } else if(el.type === 'checkbox' && el.getAttribute('data-value-checked') ){
+              data[name] = el.checked ? el.getAttribute('data-value-checked') : el.getAttribute('data-value-unchecked');
+          } else{
+            data[name] = val;
+          }
+        }
+    });
+    return data;
+}
 
 mw.response = function(form, data, messages_at_the_bottom){
      messages_at_the_bottom = messages_at_the_bottom || false;
