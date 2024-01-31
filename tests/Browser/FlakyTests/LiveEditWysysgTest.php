43dff71489ce0fb848a7ed0780f942d562b03020
Peter Ivanov <peter@microweber.com>
2024-01-31 18:58:11 +0200

    update

--- tests/Browser/FlakyTests/LiveEditWysysgTest.php
+++ tests/Browser/FlakyTests/LiveEditWysysgTest.php
@@ -3,7 +3,9 @@
 namespace Tests\Browser\FlakyTests;
 
 use Facebook\WebDriver\WebDriverBy;
+use Facebook\WebDriver\WebDriverKeys;
 use Laravel\Dusk\Browser;
+use PHPUnit\Framework\Assert as PHPUnit;
 use Tests\Browser\Components\AdminLogin;
 use Tests\Browser\Components\ChekForJavascriptErrors;
 use Tests\Browser\Components\LiveEditSaveButton;
@@ -11,7 +13,7 @@ use Tests\DuskTestCase;
 
 class LiveEditWysysgTest extends DuskTestCase
 {
-    public function testLiveEditTypingOnSafeModeText()
+    public function testLiveEditTypingInTextWithFont()
     {
         $siteUrl = $this->siteUrl;
 
@@ -21,10 +23,15 @@ class LiveEditWysysgTest extends DuskTestCase
             });
 
             $params = array(
-                'title' => 'My new page ' . time(),
+                'title' => 'My new page for typing ' . time(),
                 'content_type' => 'page',
-                'content' => '<div class="safe-mode"><h1 id="test-element-in-safe-mode">Must not be able to delete element with backspace</h1></div>
-<div><h1 id="test-element-not-safe-mode">Be able to delete element with backspace</h1></div>',
+                'content' => '
+
+<div class="container-fluid col-sm-12 mx-auto mx-lg-0  ">
+   <h6 class="font-weight-normal" id="my-text-parent"><font id="my-text-here" color="#ff0000">Enter text here</font></h6>
+</div>
+
+',
                 'subtype' => 'static',
                 'is_active' => 1,
             );
@@ -48,28 +55,122 @@ class LiveEditWysysgTest extends DuskTestCase
                 $browser->validate();
             });
 
-            $browser->doubleClick('#test-element-in-safe-mode');
-            $browser->pause(500);
+            $browser->doubleClick('#my-text-here');
+            $browser->pause(1500);
 
-            // backspace
-            for ($i = 0; $i < 100; $i++) {
-                $browser->keys('#test-element-in-safe-mode', '{backspace}');
-            }
-            // delete
-            for ($i = 0; $i < 100; $i++) {
-                $browser->keys('#test-element-in-safe-mode', '{delete}');
-            }
+            $browser->keys('#my-text-here', 'New text in my element');
+            $browser->pause(200);
 
-            $browser->typeSlowly('#test-element-in-safe-mode', 'New text in safe mode element');
+            $output = $browser->script("
+            var  isTrue = document.querySelectorAll('#my-text-parent font').length === 1;
+            return isTrue;
+            ");
+            $this->assertEquals($output[0], true, 'The element is not font');
+
+            $output = $browser->script("
+            var  isTrue = document.querySelectorAll('#my-text-parent').firstChild === document.querySelectorAll('#my-text-parent').lastChild;
+            return isTrue;
+            ");
+
+            $this->assertEquals($output[0], true, 'The element must have one font child');
+
+            $browser->pause(100);
+
             $browser->within(new LiveEditSaveButton(), function ($browser) {
                 $browser->clickSaveButton($browser);
             });
             $browser->switchFrameDefault();
+
+            $browser->visit($link . '?editmode=n');
             $browser->pause(1000);
 
-            $browser->visit($link.'?editmode=n');
-            $browser->pause(1000);
-            $browser->waitForText('New text in safe mode element', 30);
+        });
+    }
+
+    public function testLiveEditTypingInTextWithFontAndEnterKey()
+    {
+        $siteUrl = $this->siteUrl;
+
+        $this->browse(function (Browser $browser) use ($siteUrl) {
+            $browser->within(new AdminLogin, function ($browser) {
+                $browser->fillForm();
+            });
+
+            $params = array(
+                'title' => 'My new page for typing ' . time(),
+                'content_type' => 'page',
+                'content' => '
+
+<div class="container-fluid col-sm-12 mx-auto mx-lg-0  ">
+   <h6 class="font-weight-normal" id="my-text-parent"><font id="my-text-here" color="#ff0000">Enter text here and hit enter</font></h6>
+</div>
+
+',
+                'subtype' => 'static',
+                'is_active' => 1,
+            );
+
+            $saved_id = save_content($params);
+            $link = content_link($saved_id);
+
+            $browser->visit($link . '?editmode=y');
+            $browser->pause(4000);
+
+            $browser->waitFor('#live-editor-frame', 30)
+                ->withinFrame('#live-editor-frame', function ($browser) {
+                    $browser->pause(1000);
+                });
+
+            $iframeElement = $browser->driver->findElement(WebDriverBy::id('live-editor-frame'));
+
+            $browser->switchFrame($iframeElement);
+
+            $browser->within(new ChekForJavascriptErrors(), function ($browser) {
+                $browser->validate();
+            });
+
+            $browser->doubleClick('#my-text-here');
+            $browser->pause(1500);
+
+            $browser->keys('#my-text-here', ['New text in my element and hit enter',WebDriverKeys::ENTER]);
+
+
+
+            $output = $browser->script("
+            var  isTrue = document.querySelectorAll('[id=\"my-text-parent\"]').length === 1;
+            return isTrue;
+            ");
+            $this->assertEquals($output[0], true, 'The element has only 1 id');
+            $output = $browser->script("
+            var  isTrue = document.querySelectorAll('[id=\"my-text-here\"]').length === 1;
+            return isTrue;
+            ");
+            $this->assertEquals($output[0], true, 'The element must only have 1 id');
+
+
+            $output = $browser->script("
+            var  isTrue = document.getElementById('my-text-parent').nextElementSibling.nodeName === document.getElementById('my-text-parent').nodeName;
+            return isTrue;
+            ");
+            $this->assertEquals($output[0], true, 'Next element is not the same as the parent');
+
+
+
+            $browser->pause(100);
+
+
+//            $this->assertEquals($output[0], true, 'The element must have one font child');
+//
+//            $browser->pause(100);
+//
+//            $browser->within(new LiveEditSaveButton(), function ($browser) {
+//                $browser->clickSaveButton($browser);
+//            });
+//            $browser->switchFrameDefault();
+//
+//            $browser->visit($link . '?editmode=n');
+//            $browser->pause(1000);
+
         });
     }
 
