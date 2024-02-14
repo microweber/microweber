1e1245521405e6aa07947971286a831fb45677c6
Bozhidar Slaveykov <bobi@microweber.com>
2024-02-14 17:24:36 +0200

    update

---
+++ userfiles/modules/cloudflare/config.php
@@ -0,0 +1,12 @@
+<?php
+
+$config = array();
+$config['name'] = "Cloudflare";
+$config['author'] = "Bozhidar Slaveykov";
+$config['description'] = "Microweber";
+$config['website'] = "http://microweber.com/";
+$config['version'] = 1;
+$config['ui'] = true;
+$config['position'] = 100;
+$config['as_element'] = 1;
+$config['categories'] = "miscellaneous";
---
+++ userfiles/modules/cloudflare/functions.php
@@ -0,0 +1,38 @@
+<?php
+
+event_bind('mw.init', function () {
+
+    if (request()->header('cdn-loop') == 'cloudflare') {
+//        if (\Symfony\Component\HttpFoundation\IpUtils::checkIp(request()->ip(), fetchCloudFlareIps())) {
+//            request()->server->add([
+//                'ORIGINAL_REMOTE_ADDR' => request()->ip(),
+//                'REMOTE_ADDR' => filter_var(request()->header('HTTP_CF_CONNECTING_IP'),
+//                    FILTER_VALIDATE_IP) ?: request()->ip()
+//            ]);
+//        }
+        \Illuminate\Support\Facades\Config::set('trustedproxy.proxies', fetchCloudFlareIps());
+    }
+
+});
+
+function fetchCloudFlareIps()
+{
+    $ips = Cache::get('cloudflare_ips');
+    if ($ips) {
+        return $ips;
+    }
+
+    $guzzle = new \GuzzleHttp\Client();
+    $return = $guzzle->get("https://api.cloudflare.com/client/v4/ips");
+    if ($return->getStatusCode() == 200) {
+
+        $list = json_decode($return->getBody(), true);
+        $ips = array_merge($list["result"]["ipv4_cidrs"], $list["result"]["ipv6_cidrs"]);
+
+        \Illuminate\Support\Facades\Cache::put('cloudflare_ips', $ips, 60 * 60 * 24);
+
+        return $ips;
+    } else {
+        return [];
+    }
+}
