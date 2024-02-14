9fb6037eca9c7e808dd123ec5effef5c39d7b97b
Nikolay <rachkuw@gmail.com>
2024-02-14 12:05:09 +0200

    update

--- src/MicroweberPackages/Admin/Services/AdminManager.php
+++ src/MicroweberPackages/Admin/Services/AdminManager.php
@@ -141,7 +141,7 @@ class AdminManager
         if($showServiceLink && $showPoweredBy) {
 
             $this->menus['left_menu_bottom']->addChild('Get Help', [
-                'uri' => 'https://microweber.com/support',
+                'uri' => 'https://microweber.org/go/feedback/',
                 'attributes' => [
                     'target' => '_blank',
                     'icon' => ' <svg fill="currentColor"class="me-3" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 96 960 960" width="24"><path d="M478 816q21 0 35.5-14.5T528 766q0-21-14.5-35.5T478 716q-21 0-35.5 14.5T428 766q0 21 14.5 35.5T478 816Zm-36-154h74q0-33 7.5-52t42.5-52q26-26 41-49.5t15-56.5q0-56-41-86t-97-30q-57 0-92.5 30T342 438l66 26q5-18 22.5-39t53.5-21q32 0 48 17.5t16 38.5q0 20-12 37.5T506 530q-44 39-54 59t-10 73Zm38 314q-83 0-156-31.5T197 859q-54-54-85.5-127T80 576q0-83 31.5-156T197 293q54-54 127-85.5T480 176q83 0 156 31.5T763 293q54 54 85.5 127T880 576q0 83-31.5 156T763 859q-54 54-127 85.5T480 976Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"/></svg>'
