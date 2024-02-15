f63174f6036a2dc2625131f820bae224448e8fef
Peter Ivanov <peter@microweber.com>
2024-02-15 15:35:53 +0200

    update

--- src/MicroweberPackages/LiveEdit/resources/js/ui/live-edit-page-scripts.js
+++ src/MicroweberPackages/LiveEdit/resources/js/ui/live-edit-page-scripts.js
@@ -510,11 +510,9 @@ if(window.self !== window.top) {
             }
         }
 
-
-
     })
     document.addEventListener('keydown', function (event) {
-        if (event.ctrlKey && event.key === 's') {
+        if (event.ctrlKey && event.keyCode === 83) {
             return mw.top().app.editor.dispatch('Ctrl+S', event);
         }
     });
