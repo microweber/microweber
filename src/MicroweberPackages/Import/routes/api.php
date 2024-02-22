0b22959f8919ab8e5b27fada09f855147e6b6a4e
Bozhidar Slaveykov <bobi@microweber.com>
2024-02-22 11:42:23 +0200

    delete unused files

--- src/MicroweberPackages/Import/routes/api.php
+++ src/MicroweberPackages/Import/routes/api.php
@@ -7,22 +7,11 @@
  */
 
 
-use MicroweberPackages\Modules\Admin\ImportExportTool\ImportMapping\Readers\XmlToArray;
-
 Route::name('admin.import.')
     ->prefix(mw_admin_prefix_url().'/import')
     ->middleware(['admin'])
-    ->namespace('\MicroweberPackages\Backup\Http\Controllers\Admin')
+    ->namespace('\MicroweberPackages\Import\Http\Controllers\Admin')
     ->group(function () {
 
-        Route::get('import', 'BackupController@import')->name('import');
-        Route::get('export', 'BackupController@export')->name('export');
-        Route::get('download', 'BackupController@download')->name('download');
-
-        Route::post('upload', 'BackupController@upload')->name('upload');
-        Route::post('delete', 'BackupController@delete')->name('delete');
-
-        Route::post('/language/export', 'LanguageController@export')->name('language.export');
-        Route::post('/language/upload', 'LanguageController@upload')->name('language.upload');
 
     });
