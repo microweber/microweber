5c0f4207f38203d3d2ad206ecbc2e2942687bfde
Peter Ivanov <peter@microweber.com>
2024-02-28 15:04:00 +0200

    update

--- src/MicroweberPackages/LiveEdit/resources/views/iframe.blade.php
+++ src/MicroweberPackages/LiveEdit/resources/views/iframe.blade.php
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
     <meta name="viewport" content="width=device-width, initial-scale=1"/>
     <meta name="csrf-token" content="{{ csrf_token() }}"/>
+    <meta name="google" content="notranslate">
 
     <?php print mw_admin_header_scripts() ;    ?>
 
