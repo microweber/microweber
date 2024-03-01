3f5004f8f1882219258cce100dcc051b7c0eff47
Bozhidar Slaveykov <bobi@microweber.com>
2024-03-01 15:16:01 +0200

    Update default.php

--- userfiles/modules/marquee/templates/default.php
+++ userfiles/modules/marquee/templates/default.php
@@ -13,8 +13,37 @@
 
 ?>
 
-<div>
-    <marqueuee>
-        Hello world!
-    </marqueuee>
+<style>
+    .wrapper {
+        max-width: 100%;
+        overflow: hidden;
+    }
+
+    .marquee {
+        white-space: nowrap;
+        overflow: hidden;
+        display: inline-block;
+        animation: marquee 10s linear infinite;
+    }
+
+    .marquee div {
+        display: inline-block;
+    }
+
+    @keyframes marquee {
+        0% {
+            transform: translate(0, 0);
+        }
+
+        100% {
+            transform: translate(-50%, 0);
+        }
+    }
+
+</style>
+
+<div class="wrapper">
+    <div class="marquee">
+            Hello world!
+    </div>
 </div>
