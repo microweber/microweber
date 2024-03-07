079627bf53148ebee825ea88b22a2cf88fce69bc
Peter Ivanov <peter@microweber.com>
2024-03-07 14:12:45 +0200

    update

--- src/MicroweberPackages/LiveEdit/resources/js/ui/apps/ElementStyleEditor/ElementStyleEditorPosition.vue
+++ src/MicroweberPackages/LiveEdit/resources/js/ui/apps/ElementStyleEditor/ElementStyleEditorPosition.vue
@@ -1,9 +1,27 @@
 <template>
-    <div>
+    <div class="d-flex">
+
+        <svg fill="currentColor" height="24" width="24" xmlns="http://www.w3.org/2000/svg"
+             xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" x="0px" y="0px" viewBox="0 0 24 24"
+             style="enable-background:new 0 0 24 24;" xml:space="preserve">
+            <path
+                d="M19,19h2v2h-2V19 M19,17h2v-2h-2V17 M3,13h2v-2H3V13 M3,17h2v-2H3V17 M3,9h2V7H3V9 M3,5h2V3H3V5 M7,5h2V3H7V5 M15,21h2v-2  h-2V21 M11,21h2v-2h-2V21 M15,21h2v-2h-2V21 M7,21h2v-2H7V21 M3,21h2v-2H3V21 M21,8c0-2.8-2.2-5-5-5h-5v2h5c1.7,0,3,1.3,3,3v5h2V8z"></path>
+        </svg>
+
+        <b class="mw-admin-action-links ms-3" :class="{'active': showPosition }"
+           v-on:click="togglePosition">
+            Element Position
+        </b>
+    </div>
+
+
+
+
+    <div :class="{'d-none': !showPosition }">
 
         <DropdownSmall v-model="selectedPosition" :options="selectedPositionOptions" :label="'Position'"/>
 
-
+<!--
         <div class="form-group">
             <label for="topInput">Top (px):</label>
             <input id="topInput" class="form-control" type="number" v-model.number="topValue">
@@ -12,7 +30,7 @@
         <div class="form-group">
             <label for="leftInput">Left (px):</label>
             <input id="leftInput" class="form-control" type="number" v-model.number="leftValue">
-        </div>
+        </div>-->
 
         <div class="form-group">
             <label for="zIndexInput">Z-Index:</label>
@@ -60,12 +78,12 @@ export default {
 
             if (!this.activeNode) return;
 
-            if(this.selectedPosition === 'static') {
-                mw.top().app.liveEdit.elementHandleContent.elementActions.destroyFreeDraggableElement(this.activeNode)
-            }
-            else if(this.selectedPosition === 'absolute' || this.selectedPosition === 'relative' || this.selectedPosition === 'fixed' || this.selectedPosition === 'sticky') {
-                mw.top().app.liveEdit.elementHandleContent.elementActions.makeFreeDraggableElement(this.activeNode)
-            }
+            // if(this.selectedPosition === 'static') {
+            //     mw.top().app.liveEdit.elementHandleContent.elementActions.destroyFreeDraggableElement(this.activeNode)
+            // }
+            // else if(this.selectedPosition === 'absolute' || this.selectedPosition === 'relative' || this.selectedPosition === 'fixed' || this.selectedPosition === 'sticky') {
+            //     mw.top().app.liveEdit.elementHandleContent.elementActions.makeFreeDraggableElement(this.activeNode)
+            // }
 
         },
 
@@ -77,8 +95,8 @@ export default {
             if (!this.activeNode) return;
 
             this.applyPropertyToActiveNode('position', this.selectedPosition);
-            this.applyPropertyToActiveNode('top', `${this.topValue}px`);
-            this.applyPropertyToActiveNode('left', `${this.leftValue}px`);
+           // this.applyPropertyToActiveNode('top', `${this.topValue}px`);
+          //  this.applyPropertyToActiveNode('left', `${this.leftValue}px`);
             this.applyPropertyToActiveNode('zIndex', this.zIndexValue);
             this.makeElementFreelyDraggableElementIfPositionAllows();
         },
