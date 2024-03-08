07ba68a9c4defa81d8c6378d678fec5e1748edb6
Bozhidar Slaveykov <bobi@microweber.com>
2024-03-08 17:16:14 +0200

    update

--- userfiles/modules/picture/config.php
+++ userfiles/modules/picture/config.php
@@ -9,5 +9,5 @@ $config['ui'] = true;
 $config['position'] = 3;
 $config['as_element'] = 1;
 $config['categories'] = "essentials";
-
+$config['keywords'] = "picture,gallery,images,photos,slider,carousel,lightbox,photo,pictures";
 
