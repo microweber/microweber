106e660e71422f03c0c48c830ae894b2bfcc523c
Peter Ivanov <peter@microweber.com>
2024-03-09 16:17:05 +0200

    update

--- tests/Browser/Admin/AdminAATest.php
+++ tests/Browser/Admin/AdminAATest.php
@@ -3,6 +3,7 @@
 namespace Tests\Browser\Admin;
 
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Config;
 use Laravel\Dusk\Browser;
 use MicroweberPackages\User\Models\User;
 use Tests\Browser\Components\AdminMakeInstall;
@@ -26,6 +27,26 @@ class AdminAATest extends DuskTestCase
             $user = User::where('is_admin', 1)->first();
             $this->assertNotNull($user);
             Auth::login($user);
+        });
+
+    }
+
+    public function testAdminInstallAfter()
+    {
+        $this->browse(function (Browser $browser)  {
+
+            $user = User::where('is_admin', 1)->first();
+            $this->assertNotNull($user);
+            Auth::login($user);
+
+
+            $browser->visit(admin_url());
+            $this->assertTrue(mw_is_installed());
+
+            //check if locale is en_US
+            $this->assertEquals('en_US', app()->getLocale());
+            $this->assertEquals('en_US', Config::get('app.locale'));
+
         });
 
     }
