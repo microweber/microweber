82c1f913a53b94ec32a074656bfc0db0fc6f614a
Bozhidar Slaveykov <bobi@microweber.com>
2024-03-14 16:50:33 +0200

    update

--- userfiles/modules/schema_org/config.php
+++ userfiles/modules/schema_org/config.php
@@ -10,3 +10,11 @@ $config['ui'] = false;
 $config['ui_admin'] = false;
 $config['position'] = 100;
 $config['categories'] = "miscellaneous";
+
+$config['settings'] = [];
+$config['settings']['autoload_namespace'] = [
+    [
+        'path' => __DIR__ . '/src/',
+        'namespace' => 'MicroweberPackages\Modules\SchemaOrg'
+    ],
+];
