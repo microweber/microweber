e24e44c4cb65db5cf874679e41784d8517c991e1
Peter Ivanov <peter@microweber.com>
2024-03-27 12:14:23 +0200

    update

--- src/MicroweberPackages/LiveEdit/resources/js/ui/components/ElementSettings/ElementSettingsModal.vue
+++ src/MicroweberPackages/LiveEdit/resources/js/ui/components/ElementSettings/ElementSettingsModal.vue
@@ -1,10 +1,11 @@
 <template>
 
-    <VueDragResize :isActive="true" :w="200" :h="200" v-on:resizing="resize" v-on:dragging="resize">
-       <h3>Hello World!</h3>
-        <p>{{ top }} х {{ left }} </p>
-        <p>{{ width }} х {{ height }}</p>>
-    </VueDragResize>
+   <div id="elementSettingsModal" style="display: none">
+
+
+       tyka e modala
+
+   </div>
 
 </template>
 
