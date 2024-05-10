74650bfb0f83e892eb9cb262c65bce984521f9d6
Peter Ivanov <peter@microweber.com>
2024-05-10 14:26:11 +0300

    update

--- src/MicroweberPackages/Content/resources/views/admin/content/livewire/card-header.blade.php
+++ src/MicroweberPackages/Content/resources/views/admin/content/livewire/card-header.blade.php
@@ -5,43 +5,58 @@
             <h1 class="main-pages-title d-lg-flex card-title">
 
                 <a class="@if(isset($currentCategory) and $currentCategory) cursor-pointer text-decoration-none @else cursor-pointer text-decoration-none @endif" onclick="livewire.emit('deselectAllCategories');return false;">
-                    {{_e(ucfirst($this->contentType))}}
+
+
+                    @if($this->contentType == 'content')
+                        {{_e('Content')}}
+                    @elseif($this->contentType == 'post')
+                        {{_e('Posts')}}
+                    @elseif($this->contentType == 'product')
+                        {{_e('Products')}}
+                    @elseif($this->contentType == 'category')
+                        {{_e('Categories')}}
+                    @elseif($this->contentType == 'page')
+                        {{_e('Pages')}}
+                    @else
+                        {{_e(ucfirst($this->contentType))}}
+                    @endif
+
+               </a>
+
+
+            @if(isset($currentPage) and $currentPage)
+                <small class="text-muted"> &nbsp; \ &nbsp; </small>
+                <a class="cursor-pointer form-label text-muted" onclick="livewire.emit('selectPageFromTableList','{{$currentPage['id']}}');return false;">
+                    {{$currentPage['title']}}
                 </a>
+            @endif
 
 
-                @if(isset($currentPage) and $currentPage)
-                    <small class="text-muted"> &nbsp; \ &nbsp; </small>
-                    <a class="cursor-pointer form-label text-muted" onclick="livewire.emit('selectPageFromTableList','{{$currentPage['id']}}');return false;">
-                        {{$currentPage['title']}}
-                    </a>
-                @endif
+            @if(isset($currentCategory) and $currentCategory)
+                <small class="text-muted"> &nbsp; \ &nbsp; </small>
 
-
-                @if(isset($currentCategory) and $currentCategory)
-                    <small class="text-muted"> &nbsp; \ &nbsp; </small>
-
-                    <a class="cursor-pointer form-label text-muted" onclick="livewire.emit('selectCategoryFromTableList','{{$currentCategory['id']}}');return false;">
-                    {{$currentCategory['title']}}
-                    </a>
-                @endif
+                <a class="cursor-pointer form-label text-muted" onclick="livewire.emit('selectCategoryFromTableList','{{$currentCategory['id']}}');return false;">
+                {{$currentCategory['title']}}
+                </a>
+            @endif
 
 
 
-                @if($isInTrashed)
-                    <span class="form-label text-muted">&nbsp; \ &nbsp;
-                  {{ _e('Trash') }}
-                    </span>
-                @endif
+            @if($isInTrashed)
+                <span class="form-label text-muted">&nbsp; \ &nbsp;
+              {{ _e('Trash') }}
+                </span>
+            @endif
 
 {{--                @if(isset($currentCategory) and $currentCategory)--}}
 {{--                    <a class="ms-1 text-muted fs-5"  onclick="livewire.emit('deselectAllCategories');return false;">--}}
 {{--                        <svg fill="currentColor" xmlns="http://www.w3.org/2000/svg" height="20" viewBox="0 -960 960 960" width="20"><path d="m291-240-51-51 189-189-189-189 51-51 189 189 189-189 51 51-189 189 189 189-51 51-189-189-189 189Z"/></svg>--}}
 {{--                    </a>--}}
 {{--                @endif--}}
-            </h1>
+        </h1>
 
-        </div>
-        <div>
+    </div>
+    <div>
 
 {{--            @if(isset($contentType))--}}
 {{--                @include('content::admin.content.livewire.create-content-buttons',['buttonClass'=>'btn btn-outline-primary btn-sm'])--}}
@@ -51,14 +66,14 @@
 
 
 
-            <?php
-            /*  @if(isset($currentCategory) and $currentCategory)
-            <a href="{{category_link($currentCategory['id'])}}" target="_blank" title="{{_e('View category')}}"><span class="fa fa-external-link-alt"></span></a>
-            @endif*/
+        <?php
+        /*  @if(isset($currentCategory) and $currentCategory)
+        <a href="{{category_link($currentCategory['id'])}}" target="_blank" title="{{_e('View category')}}"><span class="fa fa-external-link-alt"></span></a>
+        @endif*/
 
-            ?>
+        ?>
 
-        </div>
     </div>
+</div>
 </div>
 
