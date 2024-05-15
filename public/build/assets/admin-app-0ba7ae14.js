4791cf774e6e81e056f0723f74f14008cae9d1de
Peter Ivanov <peter@microweber.com>
2024-05-15 12:22:36 +0300

    update

--- public/build/assets/admin-app-0ba7ae14.js
+++
@@ -1,2 +0,0 @@
-import"./@core-45d5d3a3.js";mw.app.dispatch("init");mw.app.dispatch("ready");
-//# sourceMappingURL=admin-app-0ba7ae14.js.map
---
+++ public/build/assets/admin-app-d68fd4eb.js
@@ -0,0 +1,2 @@
+import"./@core-c699bf77.js";mw.app.dispatch("init");mw.app.dispatch("ready");
+//# sourceMappingURL=admin-app-d68fd4eb.js.map
