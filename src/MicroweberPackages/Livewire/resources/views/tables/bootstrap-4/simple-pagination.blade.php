98fc29744568ecc4749456f8e73cdadf76e1353e
Bozhidar <bobi@microweber.com>
2024-05-20 11:35:24 +0300

    update

--- src/MicroweberPackages/Livewire/resources/views/tables/bootstrap-4/simple-pagination.blade.php
+++ src/MicroweberPackages/Livewire/resources/views/tables/bootstrap-4/simple-pagination.blade.php
@@ -1,21 +1,53 @@
+@php
+    if (! isset($scrollTo)) {
+        $scrollTo = 'body';
+    }
+
+    $scrollIntoViewJsSnippet = ($scrollTo !== false)
+        ? <<<JS
+           (\$el.closest('{$scrollTo}') || document.querySelector('{$scrollTo}')).scrollIntoView()
+        JS
+        : '';
+@endphp
+
 <div>
     @if ($paginator->hasPages())
-        <nav role="navigation" aria-label="Pagination Navigation">
-            <span>
+        <nav>
+            <ul class="pagination">
+                {{-- Previous Page Link --}}
                 @if ($paginator->onFirstPage())
-                    <span>Previous</span>
+                    <li class="page-item disabled" aria-disabled="true">
+                        <span class="page-link">@lang('pagination.previous')</span>
+                    </li>
                 @else
-                    <button class="btn btn-outline-primary" wire:click="previousPage" wire:loading.attr="disabled" rel="prev">Previous</button>
+                    @if(method_exists($paginator,'getCursorName'))
+                        <li class="page-item">
+                            <button dusk="previousPage" type="button" class="page-link" wire:key="cursor-{{ $paginator->getCursorName() }}-{{ $paginator->previousCursor()->encode() }}" wire:click="setPage('{{$paginator->previousCursor()->encode()}}','{{ $paginator->getCursorName() }}')" x-on:click="{{ $scrollIntoViewJsSnippet }}" wire:loading.attr="disabled">@lang('pagination.previous')</button>
+                        </li>
+                    @else
+                        <li class="page-item">
+                            <button type="button" dusk="previousPage{{ $paginator->getPageName() == 'page' ? '' : '.' . $paginator->getPageName() }}" class="page-link" wire:click="previousPage('{{ $paginator->getPageName() }}')" x-on:click="{{ $scrollIntoViewJsSnippet }}" wire:loading.attr="disabled">@lang('pagination.previous')</button>
+                        </li>
+                    @endif
                 @endif
-            </span>
 
-            <span>
-                @if ($paginator->onLastPage())
-                    <span>Next</span>
+                {{-- Next Page Link --}}
+                @if ($paginator->hasMorePages())
+                    @if(method_exists($paginator,'getCursorName'))
+                        <li class="page-item">
+                            <button dusk="nextPage" type="button" class="page-link" wire:key="cursor-{{ $paginator->getCursorName() }}-{{ $paginator->nextCursor()->encode() }}" wire:click="setPage('{{$paginator->nextCursor()->encode()}}','{{ $paginator->getCursorName() }}')" x-on:click="{{ $scrollIntoViewJsSnippet }}" wire:loading.attr="disabled">@lang('pagination.next')</button>
+                        </li>
+                    @else
+                        <li class="page-item">
+                            <button type="button" dusk="nextPage{{ $paginator->getPageName() == 'page' ? '' : '.' . $paginator->getPageName() }}" class="page-link" wire:click="nextPage('{{ $paginator->getPageName() }}')" x-on:click="{{ $scrollIntoViewJsSnippet }}" wire:loading.attr="disabled">@lang('pagination.next')</button>
+                        </li>
+                    @endif
                 @else
-                    <button class="btn btn-outline-primary" wire:click="nextPage" wire:loading.attr="disabled" rel="next">Next</button>
+                    <li class="page-item disabled" aria-disabled="true">
+                        <span class="page-link">@lang('pagination.next')</span>
+                    </li>
                 @endif
-            </span>
+            </ul>
         </nav>
     @endif
 </div>
