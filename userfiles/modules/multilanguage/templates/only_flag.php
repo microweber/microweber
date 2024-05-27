e270bb8bc33dc41e439fd86a668e88ec89cf5856
Nikolay <rachkuw@gmail.com>
2024-05-27 11:22:16 +0300

    update

--- userfiles/modules/multilanguage/templates/only_flag.php
+++ userfiles/modules/multilanguage/templates/only_flag.php
@@ -1,8 +1,8 @@
 <?php
 /*
 type: layout
-name: Default
-description: MW Default
+name: Only flag
+description: MW Only flag
 */
 ?>
     <style>
@@ -74,9 +74,13 @@ description: MW Default
 
     <div class="mw-dropdown mw-dropdown-default">
     <span class="mw-dropdown-value btn btn-primary btn-sm mw-dropdown-val">
+
+        <?php dump ($supported_languages); ?>
         <?php if (!empty($current_language)): ?>
         <?php if (!empty($current_language['display_icon'])): ?>
             <img src="<?php echo $current_language['display_icon']; ?>" class="multilanguage-display-icon-custom" />
+
+
         <?php else: ?>
             <span class="flag-icon flag-icon-<?php echo get_flag_icon($current_language['locale']); ?>"></span>
         <?php endif; ?>
