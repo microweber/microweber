067cafe71cb83bcd1d6a0d8b246afe9bc0131aae
Bozhidar <bobi@microweber.com>
2024-05-27 17:46:28 +0300

    Update placeholder-image-cropped.blade.php

--- src/MicroweberPackages/Filament/resources/views/filament-forms/components/placeholder-image-cropped.blade.php
+++ src/MicroweberPackages/Filament/resources/views/filament-forms/components/placeholder-image-cropped.blade.php
@@ -1,6 +1,6 @@
 <div class="w-full">
 
-    <div class="image-column-cropped w-full h-[31rem]" style="background-image:url('{!! $image !!}');background-size: cover;background-position: top;">
+    <div class="image-column-cropped w-full h-[36rem]" style="background-image:url('{!! $image !!}');background-size: cover;background-position: top;">
 
     </div>
 
