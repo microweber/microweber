14a26dcafa77ea3d669324126749f0f06e8a3fc9
Bozhidar <bobi@microweber.com>
2024-05-28 12:16:23 +0300

    Update marketplace-installed-item.blade.php

--- resources/views/filament/admin/pages/marketplace-installed-item.blade.php
+++ resources/views/filament/admin/pages/marketplace-installed-item.blade.php
@@ -1,55 +1,52 @@
 <x-filament-panels::page>
 
     <div>
+        <div class="">
+            <div class="text-green-500 text-3xl">
+                Your theme is installed successfully.
+            </div>
+            <div class="text-2xl">
+                {{ $item['name'] }}
+            </div>
+            <div>
+                <img src="{{$item['screenshotUrl']}}" alt="{{ $item['name'] }}"
+                     class="mt-6 w-full h-64 object-cover object-center rounded-lg shadow-md">
+            </div>
+            <div class="mt-6 text-gray-500">
+                {{ $item['description'] }}
+            </div>
+            <div class="mt-6">
+                <x-filament::button
+                    icon="heroicon-m-cog"
+                    href="{{ $item['url'] }}"
+                    tag="a"
+                >
+                    Apply Theme
+                </x-filament::button>
 
-<div class="">
-            <div class="bg-white shadow-xl ">
-                <div class="p-12 bg-white border-b border-gray-200">
-                    <div class="text-green-500 text-3xl">
-                        Your theme is installed successfully.
-                    </div>
-                    <div class="text-2xl">
-                        {{ $item['name'] }}
-                    </div>
-                    <div>
-                        <img src="{{$item['screenshotUrl']}}" alt="{{ $item['name'] }}" class="mt-6 w-full h-64 object-cover object-center rounded-lg shadow-md">
-                    </div>
-                    <div class="mt-6 text-gray-500">
-                        {{ $item['description'] }}
-                    </div>
-                    <div class="mt-6">
-                        <x-filament::button
-                            icon="heroicon-m-cog"
-                            href="{{ $item['url'] }}"
-                            tag="a"
-                        >
-                            Apply Theme
-                        </x-filament::button>
-
-                        <x-filament::button
-                            icon="heroicon-m-x-mark"
-                            color="danger"
-                            href="{{ $item['url'] }}"
-                            tag="a"
-                        >
-                            Unistall Theme
-                        </x-filament::button>
+                <x-filament::button
+                    icon="heroicon-m-x-mark"
+                    color="danger"
+                    href="{{ $item['url'] }}"
+                    tag="a"
+                >
+                    Unistall Theme
+                </x-filament::button>
 
 
-                    </div>
-                    <div>
-                        Author: <a href="{{ $item['authorUrl'] }}" target="_blank" class="text-blue-500 hover:text-blue-700">{{ $item['author']}}</a>
-                    </div>
-                    <div>
-                        Version: {{ $item['version'] }}
-                    </div>
-                    <div>
-                        License: {{ $item['license'] }}
-                    </div>
-                    <div>
-                        Tags: {{ implode(', ', $item['tags']) }}
-                    </div>
-                </div>
+            </div>
+            <div>
+                Author: <a href="{{ $item['authorUrl'] }}" target="_blank"
+                           class="text-blue-500 hover:text-blue-700">{{ $item['author']}}</a>
+            </div>
+            <div>
+                Version: {{ $item['version'] }}
+            </div>
+            <div>
+                License: {{ $item['license'] }}
+            </div>
+            <div>
+                Tags: {{ implode(', ', $item['tags']) }}
             </div>
         </div>
     </div>
