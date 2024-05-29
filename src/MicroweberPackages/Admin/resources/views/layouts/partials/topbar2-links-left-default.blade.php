1eacd4ab1392ef8d208d9377ed93d7a636e55636
Nikolay <rachkuw@gmail.com>
2024-05-29 10:44:15 +0300

    update

--- src/MicroweberPackages/Admin/resources/views/layouts/partials/topbar2-links-left-default.blade.php
+++ src/MicroweberPackages/Admin/resources/views/layouts/partials/topbar2-links-left-default.blade.php
@@ -30,7 +30,7 @@
 <button type="button" class="btn btn-light    border-0 admin-toolbar-buttons"
         data-bs-toggle="modal"  data-bs-target="#modal-add-new-admin" aria-expanded="false">
     <img height="28" width="28" src="<?php print modules_url()?>/microweber/api/libs/mw-ui/assets/img/add-new-button.svg" alt="">
-    <span class="  ms-2" style="font-size: 14px; font-weight: bold;">ADD</span> 
+    <span class="  ms-2" style="font-size: 14px; font-weight: bold;">ADD</span>
 </button>
 
 
