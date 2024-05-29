5ef498b66bd5a34f2737f6452f3fd2fede1a7eb9
Nikolay <rachkuw@gmail.com>
2024-05-29 13:57:15 +0300

    update

--- userfiles/modules/categories/templates/default.php
+++ userfiles/modules/categories/templates/default.php
@@ -14,13 +14,17 @@ description: List Navigation
 
 <?php
     $params['ul_class'] = 'nav-list';
+    $params['active_class'] = 'active';
 	$params['ul_class_deep'] = 'nav-list';
 
 ?>
 
+<script>
+
+</script>
+
+
 <div class="module-categories module-categories-template-default">
-
-		<?php  category_tree($params);  ?>
-
+    <?php  category_tree($params);  ?>
 </div>
 
