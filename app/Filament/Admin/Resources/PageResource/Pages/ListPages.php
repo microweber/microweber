8e48154a8367eeb6a3a6be07301d3a47fba8d8b9
Bozhidar <bobi@microweber.com>
2024-06-06 16:03:22 +0300

    update

--- app/Filament/Admin/Resources/PageResource/Pages/ListPages.php
+++ app/Filament/Admin/Resources/PageResource/Pages/ListPages.php
@@ -5,9 +5,12 @@ namespace App\Filament\Admin\Resources\PageResource\Pages;
 use App\Filament\Admin\Resources\PageResource;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
+use Hydrat\TableLayoutToggle\Concerns\HasToggleableTable;
 
 class ListPages extends ListRecords
 {
+    use HasToggleableTable;
+
     protected static string $resource = PageResource::class;
 
     protected function getHeaderActions(): array
--- app/Filament/Admin/Resources/PostResource/Pages/ListPosts.php
+++ app/Filament/Admin/Resources/PostResource/Pages/ListPosts.php
@@ -5,9 +5,12 @@ namespace App\Filament\Admin\Resources\PostResource\Pages;
 use App\Filament\Admin\Resources\PostResource;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
+use Hydrat\TableLayoutToggle\Concerns\HasToggleableTable;
 
 class ListPosts extends ListRecords
 {
+    use HasToggleableTable;
+
     protected static string $resource = PostResource::class;
 
     protected function getHeaderActions(): array
