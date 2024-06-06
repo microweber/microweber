d616c7d8bc957a8e8821474141812f8a6ed38c36
Peter Ivanov <peter@microweber.com>
2024-06-06 14:21:52 +0300

    update

--- src/MicroweberPackages/LiveEdit/resources/js/ui/admin-filament-app.js
+++ src/MicroweberPackages/LiveEdit/resources/js/ui/admin-filament-app.js
@@ -1,10 +1,11 @@
 
 import "../api-core/core/core/@core.js";
 import {MWUniversalContainer} from "../api-core/services/containers/container.js";
+import {AdminFilament} from "./admin-filament/admin-filament.js";
 
 
 mw.admin = new MWUniversalContainer();
-
+mw.admin.filament = new AdminFilament();
 mw.admin.dispatch('init');
 mw.admin.dispatch('ready');
 
---
+++ src/MicroweberPackages/LiveEdit/resources/js/ui/admin-filament/admin-filament.js
@@ -0,0 +1,15 @@
+import BaseComponent from "../../api-core/services/containers/base-class.js";
+
+export class AdminFilament extends BaseComponent {
+    constructor() {
+        super();
+
+        this.init();
+
+    }
+
+    init() {
+
+        // listen for events from alpine and filament
+    }
+}
