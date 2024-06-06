398a3fc132cf0da36b1412a30a379eca474d2c72
Bozhidar <bobi@microweber.com>
2024-06-06 14:59:14 +0300

    Update SettingsGeneral.php

--- app/Filament/Admin/Pages/SettingsGeneral.php
+++ app/Filament/Admin/Pages/SettingsGeneral.php
@@ -3,12 +3,16 @@
 namespace App\Filament\Admin\Pages;
 
 use App\Filament\Admin\Pages\Abstract\SettingsPageDefault;
+use Filament\Forms\Components\Actions;
+use Filament\Forms\Components\FileUpload;
 use Filament\Forms\Components\MultiSelect;
 use Filament\Forms\Components\Section;
 use Filament\Forms\Components\Select;
+use Filament\Forms\Components\Split;
 use Filament\Forms\Components\TagsInput;
 use Filament\Forms\Components\Textarea;
 use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Toggle;
 use Filament\Forms\Form;
 use Filament\Notifications\Notification;
 use Filament\Pages\Page;
@@ -37,7 +41,7 @@ class SettingsGeneral extends SettingsPageDefault
 
                 Section::make('Seo Settings')
                     ->view('filament-forms::sections.section')
-                    ->description(' Fill in the fields for maximum results when finding your website in search engines.')
+                    ->description('Fill in the fields for maximum results when finding your website in search engines.')
                     ->schema([
 
                         TextInput::make('translatableOptions.website.website_title')
@@ -71,6 +75,96 @@ class SettingsGeneral extends SettingsPageDefault
                             ->placeholder('Select Permalink Structure'),
 
                     ]),
+
+
+                Section::make('General Settings')
+                    ->view('filament-forms::sections.section')
+                    ->description('Set regional settings for your website or online store They will also affect the language you use and the fees for the orders.')
+                    ->schema([
+
+                        Select::make('options.website.date_format')
+                            ->label('Date Format')
+                            ->helperText('Choose a date format for your website')
+                            ->live()
+                            ->options(function () {
+                                $options = [];
+                                foreach (app()->format->get_supported_date_formats() as $item) {
+                                    $options[$item] = date($item, time()) . '- ('.$item.')';
+                                }
+                                return $options;
+                            }),
+
+                        Select::make('options.website.time_zone')
+                            ->label('Time Zone')
+                            ->helperText('Set a time zone')
+                            ->live()
+                            ->options(function () {
+                                return timezone_identifiers_list();
+                            }),
+
+
+                        Select::make('options.website.items_per_page')
+                            ->label('Posts per page')
+                            ->helperText('Select how many posts or products you want to be shown per page?')
+                            ->live()
+                            ->options(function () {
+                                $postsPerPage = [];
+                                for ($i = 5; $i < 40; $i += 5) {
+                                    $postsPerPage[$i] = $i;
+                                }
+                                return $postsPerPage;
+                            }),
+
+                        Split::make([
+                            FileUpload::make('options.website.logo')
+                                ->label('Website Logo')
+                                ->helperText('Select an logo for your website.')
+                                ->live(),
+                            FileUpload::make('options.website.favicon_image')
+                                ->label('Website Favicon')
+                                ->helperText('Select an icon for your website.')
+                                ->live(),
+                        ])->columns(2)
+
+                    ]),
+
+                Section::make('Online Shop')
+                    ->view('filament-forms::sections.section')
+                    ->description('Enable or disable your online shop')
+                    ->schema([
+
+                        Toggle::make('options.website.shop_disabled')
+                            ->label('Enable Online shop')
+                            ->live()
+                            ->helperText('Choose the status of your online shop'),
+
+                    ]),
+
+                Section::make('Maintenance mode')
+                    ->view('filament-forms::sections.section')
+                    ->description('Enable or disable maintenance mode on your site')
+                    ->schema([
+
+                        Toggle::make('options.website.maintenance_mode')
+                            ->label('Maintenance mode')
+                            ->live()
+                            ->helperText('Turn on Under construction mode of your site'),
+
+                    ]),
+
+                Section::make('Powered by Microweber')
+                    ->view('filament-forms::sections.section')
+                    ->description('Control whether or not "Powered by Microweber" links display in the footer of your site and products.')
+                    ->schema([
+
+                        Actions::make([
+                            Actions\Action::make('edit_powered_by_microweber')
+                                ->size('lg')
+                                ->label('Click here to edit the branding settings'),
+                        ])->columnSpanFull(),
+
+                    ]),
+
 
             ]);
     }
