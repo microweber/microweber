183e71cc66c3164ec8fa7effe1293cb06b2a1641
Peter Ivanov <peter@microweber.com>
2024-06-06 11:41:02 +0300

    update

---
+++ app/Filament/Admin/Pages/KitchenSink.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Admin\Pages;
+
+use Filament\Pages\Page;
+
+class KitchenSink extends Page
+{
+    protected static ?string $navigationIcon = 'heroicon-o-document-text';
+
+    protected static string $view = 'filament.admin.pages.kitchen-sink';
+}
---
+++ resources/views/filament/admin/pages/kitchen-sink.blade.php
@@ -0,0 +1,44 @@
+<x-filament-panels::page>
+
+    <h1>
+        Modal
+    </h1>
+
+    <hr>
+
+    <h1>
+        File picker
+    </h1>
+
+    <hr>
+    <h1>
+        Url picker
+    </h1>
+
+    <hr>
+    <h1>
+        Tree
+    </h1>
+
+    <hr>
+    <h1>
+        Editor
+    </h1>
+
+    <hr>
+
+    <h1>
+        Confirm dialog
+    </h1>
+
+    <hr>
+
+
+    <h1>
+        Iframe auto height 
+    </h1>
+
+    <hr>
+
+
+</x-filament-panels::page>
