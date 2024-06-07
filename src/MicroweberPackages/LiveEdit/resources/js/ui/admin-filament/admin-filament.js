4c62ada5d0c21346cfffa701437631aa1d91db99
Peter Ivanov <peter@microweber.com>
2024-06-07 15:57:47 +0300

    update

--- src/MicroweberPackages/LiveEdit/resources/js/ui/admin-filament/admin-filament.js
+++ src/MicroweberPackages/LiveEdit/resources/js/ui/admin-filament/admin-filament.js
@@ -13,6 +13,89 @@ export class AdminFilament extends BaseComponent {
 
     init() {
 
+        this.hookOptionSaved();
+        this.hookLivewireLoadingState();
+
+
+    }
+
+    hookLivewireLoadingState() {
+        // add .mw-livewire-loading class to body when live wire is loading
+
+        Livewire.hook('commit.prepare', ({component, commit}) => {
+            this.addLoadingClassToBody();
+        })
+
+
+        Livewire.hook('request', ({ uri, options, payload, respond, succeed, fail }) => {
+            // Runs after commit payloads are compiled, but before a network request is sent...
+
+            respond(({ status, response }) => {
+              //  this.removeLoadingClassToBody();
+            })
+
+            succeed(({ status, json }) => {
+                this.removeLoadingClassToBody();
+            })
+
+            fail(({ status, content, preventDefault }) => {
+                this.removeLoadingClassToBody();
+            })
+        })
+
+
+
+        // Livewire.hook('commit', ({component, commit, respond, succeed, fail}) => {
+        //
+        //     respond(() => {
+        //      //   this.removeLoadingClassToBody();
+        //     })
+        //
+        //     succeed(({snapshot, effect}) => {
+        //         this.removeLoadingClassToBody();
+        //     })
+        //
+        //     fail(() => {
+        //         this.removeLoadingClassToBody();
+        //     })
+        // })
+        //
+        Livewire.hook('morph.added', ({el, component}) => {
+           // this.removeLoadingClassToBody();
+        })
+        Livewire.hook('morph.adding', () => {
+          //  this.addLoadingClassToBody();
+        })
+
+        Livewire.hook('morph.updated', ({el, component}) => {
+         //   this.removeLoadingClassToBody();
+        })
+        Livewire.hook('morph.updating', () => {
+            this.addLoadingClassToBody();
+        })
+        Livewire.hook('morph.removed', ({el, component}) => {
+          //  this.removeLoadingClassToBody();
+        })
+        Livewire.hook('morph.removing', () => {
+         //   this.addLoadingClassToBody();
+        })
+    }
+
+    addLoadingClassToBody() {
+
+        document.body.classList.add('mw-livewire-loading');
+    }
+    timeout = null;
+    removeLoadingClassToBody() {
+
+        clearTimeout(this.timeout);
+        this.timeout = setTimeout(function () {
+            document.body.classList.remove('mw-livewire-loading');
+        }, 1500);
+
+    }
+
+    hookOptionSaved() {
 
         Livewire.on('mw-option-saved', function ($event) {
             if ($event.optionGroup !== undefined) {
@@ -26,11 +109,8 @@ export class AdminFilament extends BaseComponent {
                     }
                 }
             }
-
-
         });
-
-
-        // listen for events from alpine and filament
     }
+
+
 }
