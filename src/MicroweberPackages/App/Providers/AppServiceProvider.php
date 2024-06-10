6f9adbe6c070f932d4db9b4e5e289bdddbc74ecd
Peter Ivanov <peter@microweber.com>
2024-06-10 14:51:52 +0300

    update

--- src/MicroweberPackages/App/Providers/AppServiceProvider.php
+++ src/MicroweberPackages/App/Providers/AppServiceProvider.php
@@ -172,9 +172,9 @@ class AppServiceProvider extends ServiceProvider
 //        $this->app->instance('config', new ConfigSave($this->app));
          $this->app->register(ConfigExtendedServiceProvider::class);
 
-//        if (!defined('ADMIN_PREFIX')) {
-//            define('ADMIN_PREFIX', mw_admin_prefix_url_legacy());
-//        }
+        if (!defined('ADMIN_PREFIX')) {
+            define('ADMIN_PREFIX', mw_admin_prefix_url_legacy());
+        }
 
         if (is_https() or (Config::get('microweber.force_https') && !is_cli())) {
             URL::forceScheme("https");
