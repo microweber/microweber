eaa038ed3fc225b999200f06cd9fd2b07cf922e1
Peter Ivanov <peter@microweber.com>
2024-06-10 15:27:55 +0300

    update

--- src/MicroweberPackages/Admin/routes/admin.php
+++ src/MicroweberPackages/Admin/routes/admin.php
@@ -5,19 +5,20 @@
 // 'middleware' => 'web',
 Route::group(['middleware' => 'public.web', 'namespace' => '\MicroweberPackages\Admin\Http\Controllers'], function () {
 
-    $admin_url = mw_admin_prefix_url_legacy();
+    $admin_url_legacy = mw_admin_prefix_url_legacy();
 
-    Route::any('/' . $admin_url, 'AdminController@dashboard')->name('admin.home');
-    Route::get($admin_url.'/manifest.json','AdminController@webAppManifest' )->name('admin.web-app-manifest');
+    Route::any('/' . $admin_url_legacy, 'AdminController@dashboard')->name('admin.home');
 
-    Route::any($admin_url, array('as' => 'admin', 'uses' => 'AdminController@dashboard'))->name('admin.index');
+    Route::any($admin_url_legacy, array('as' => 'admin', 'uses' => 'AdminController@dashboard'))->name('admin.index');
 
     Route::any('/editor_tools', 'AdminEditorToolsController@index');
     Route::any('editor_tools/{all}', array('as' => 'editor_tools', 'uses' => 'AdminEditorToolsController@index'))->where('all', '.*');
 
 
-    Route::any($admin_url . '/{all}', array('as' => 'admin', 'uses' => 'AdminController@index'))->where('all', '.*')->name('admin.all');
+    Route::any($admin_url_legacy . '/{all}', array('as' => 'admin', 'uses' => 'AdminController@index'))->where('all', '.*')->name('admin.all');
 
+    $admin_url = mw_admin_prefix_url();
+    Route::get($admin_url.'/manifest.json','AdminController@webAppManifest' )->name('admin.web-app-manifest');
 
 });
 
