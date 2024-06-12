c54a9a4553573dbb2a19591cdc56305f32720545
Bozhidar <bobi@microweber.com>
2024-06-12 17:12:32 +0300

    Update mw-file-upload.blade.php

--- src/MicroweberPackages/Filament/resources/views/filament-forms/components/mw-file-upload.blade.php
+++ src/MicroweberPackages/Filament/resources/views/filament-forms/components/mw-file-upload.blade.php
@@ -38,12 +38,12 @@
 
         }">
             <div>
-                <svg class="w-12" viewBox="0 -1.5 35 35" version="1.1" xmlns="http://www.w3.org/2000/svg">
+                <svg fill="currentColor" class="w-12 fill-gray-500" viewBox="0 -1.5 35 35" version="1.1" xmlns="http://www.w3.org/2000/svg">
                     <path class="text-gray-300"  d="M29.426 15.535c0 0 0.649-8.743-7.361-9.74-6.865-0.701-8.955 5.679-8.955 5.679s-2.067-1.988-4.872-0.364c-2.511 1.55-2.067 4.388-2.067 4.388s-5.576 1.084-5.576 6.768c0.124 5.677 6.054 5.734 6.054 5.734h9.351v-6h-3l5-5 5 5h-3v6h8.467c0 0 5.52 0.006 6.295-5.395 0.369-5.906-5.336-7.070-5.336-7.070z"></path>
                 </svg>
             </div>
             <span>
-                Select media file or <b class="text-yellow-800 font-bold">Upload</b>
+                Select media file or <b class="text-yellow-500 font-bold">Upload</b>
             </span>
         </button>
             <div class="w-full" x-show="typeFile == 'image'">
@@ -51,7 +51,7 @@
                     <div class="absolute w-full h-full top-0 text-white p-2 rounded-t-md bg-gradient-to-b from-black/70 to-black/5"
                          x-html="fileUrlShort">
                     </div>
-                    <img :src="state" class="max-h-[12rem]" />
+                    <img :src="state" class="max-h-[15rem]" />
                 </div>
             </div>
         </div>
