976b31d024ee4cd5b754d5f591607fdbda4b98b8
Bozhidar <bobi@microweber.com>
2024-06-12 17:10:20 +0300

    Update mw-file-upload.blade.php

--- src/MicroweberPackages/Filament/resources/views/filament-forms/components/mw-file-upload.blade.php
+++ src/MicroweberPackages/Filament/resources/views/filament-forms/components/mw-file-upload.blade.php
@@ -8,49 +8,53 @@
 
     <div x-data="{
             typeFile: 'file',
+            fileUrlShort: '',
             state: $wire.{{ $applyStateBindingModifiers("\$entangle('{$statePath}')") }},
         }"
-         x-init="() => {
-
-         function changeTypeFile() {
-           let url = state;
-            let getFileExtension = url.split('.').pop();
-            if (getFileExtension == 'jpg' || getFileExtension == 'jpeg' || getFileExtension == 'png' || getFileExtension == 'gif') {
+         x-effect="() => {
+             if (state && state.length > 0) {
+                  let getFileExtension = state.split('.').pop();
+              if (getFileExtension == 'jpg' || getFileExtension == 'jpeg' || getFileExtension == 'png' || getFileExtension == 'gif') {
                 typeFile = 'image';
             } else {
                 typeFile = 'file';
             }
-        }
-        changeTypeFile();
-
+            fileUrlShort = state.split('/').pop();
+         }
      }"
     >
 
-        <span x-html="state"></span>
 
-        <div x-show="typeFile == 'image'">
-            <img :src="state" style="max-width: 100px; max-height: 100px;" />
-        </div>
+        <div class="w-full flex flex-col items-center justify-center border border-gray-300 rounded-md text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500">
+
 
         <button
-            class="w-full flex flex-col items-center justify-center p-12 border border-gray-300 rounded-md text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500"
-            type="button" x-on:click="function() {
+            class="w-full py-6 full flex flex-col items-center justify-center"
+            type="button" x-on:click="()=> {
 
             mw.filePickerDialog((url) => {
-                this.state = url;
+                state = url;
             });
 
         }">
             <div>
-                <svg fill="primary" class="w-12" viewBox="0 -1.5 35 35" version="1.1" xmlns="http://www.w3.org/2000/svg">
-                    <title>upload1</title>
-                    <path d="M29.426 15.535c0 0 0.649-8.743-7.361-9.74-6.865-0.701-8.955 5.679-8.955 5.679s-2.067-1.988-4.872-0.364c-2.511 1.55-2.067 4.388-2.067 4.388s-5.576 1.084-5.576 6.768c0.124 5.677 6.054 5.734 6.054 5.734h9.351v-6h-3l5-5 5 5h-3v6h8.467c0 0 5.52 0.006 6.295-5.395 0.369-5.906-5.336-7.070-5.336-7.070z"></path>
+                <svg class="w-12" viewBox="0 -1.5 35 35" version="1.1" xmlns="http://www.w3.org/2000/svg">
+                    <path class="text-gray-300"  d="M29.426 15.535c0 0 0.649-8.743-7.361-9.74-6.865-0.701-8.955 5.679-8.955 5.679s-2.067-1.988-4.872-0.364c-2.511 1.55-2.067 4.388-2.067 4.388s-5.576 1.084-5.576 6.768c0.124 5.677 6.054 5.734 6.054 5.734h9.351v-6h-3l5-5 5 5h-3v6h8.467c0 0 5.52 0.006 6.295-5.395 0.369-5.906-5.336-7.070-5.336-7.070z"></path>
                 </svg>
             </div>
             <span>
                 Select media file or <b class="text-yellow-800 font-bold">Upload</b>
             </span>
         </button>
+            <div class="w-full" x-show="typeFile == 'image'">
+                <div class="w-full relative flex flex-col items-center justify-center bg-black/80 rounded-md">
+                    <div class="absolute w-full h-full top-0 text-white p-2 rounded-t-md bg-gradient-to-b from-black/70 to-black/5"
+                         x-html="fileUrlShort">
+                    </div>
+                    <img :src="state" class="max-h-[12rem]" />
+                </div>
+            </div>
+        </div>
 
     </div>
 </div>
