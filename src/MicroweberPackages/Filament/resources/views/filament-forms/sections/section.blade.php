a1a44bb9c9b42465816665fe7acf01064fac7e69
Bozhidar <bobi@microweber.com>
2024-06-13 15:14:54 +0300

    update

--- src/MicroweberPackages/Filament/resources/views/filament-forms/sections/section.blade.php
+++ src/MicroweberPackages/Filament/resources/views/filament-forms/sections/section.blade.php
@@ -1,17 +1,17 @@
 <div>
 
 
-    <div class="flex gap-8 bg-white shadow rounded p-12">
+    <div class="flex gap-8 bg-white dark:bg-white/5 shadow rounded p-12">
         <div class="w-[20rem]">
             <h3 class="font-bold text-xl">
                 {{$getHeading()}}
             </h3>
-            <div class="text-sm text-black/60 mt-4">
+            <div class="text-sm text-black/60 dark:text-white mt-4">
                 {{$getDescription()}}
             </div>
         </div>
 
-        <div class="bg-blue-500/5 p-12 rounded w-full">
+        <div class="bg-blue-500/5 dark:bg-white/5  p-12 rounded w-full">
             {{ $getChildComponentContainer() }}
         </div>
     </div>
