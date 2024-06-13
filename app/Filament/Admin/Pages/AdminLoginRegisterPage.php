9e7216b3634e804967b220e86275ec43860a4aca
Nikolay <rachkuw@gmail.com>
2024-06-13 17:47:22 +0300

    update

--- app/Filament/Admin/Pages/AdminLoginRegisterPage.php
+++ app/Filament/Admin/Pages/AdminLoginRegisterPage.php
@@ -3,7 +3,13 @@
 namespace App\Filament\Admin\Pages;
 
 use App\Filament\Admin\Pages\Abstract\AdminSettingsPage;
+use Filament\Forms\Components\Section;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Toggle;
+use Filament\Forms\Form;
+use Filament\Forms\Get;
 use Filament\Pages\Page;
+use Illuminate\Support\HtmlString;
 
 class AdminLoginRegisterPage extends AdminSettingsPage
 {
@@ -14,4 +20,36 @@ class AdminLoginRegisterPage extends AdminSettingsPage
     protected static ?string $title = 'Login & Register';
 
     protected static string $description = 'Configure your login and registration settings';
+
+
+
+    public function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+
+                Section::make('Register options')
+                    ->view('filament-forms::sections.section')
+                    ->description('Set your settings for proper login and register functionality.')
+                    ->schema([
+
+                        Toggle::make('options.users.enable_user_registration')
+                            ->label('Enable user registration')
+                            ->live()
+                            ->helperText(function () {
+                                return new HtmlString('<small class="text-muted d-block mb-2"> Do you allow users to register on your website? If you choose "yes", they will do that with their email.</small>');
+                            }),
+
+                        Toggle::make('options.users.registration_approval_required')
+                            ->label('Registration email verification')
+                            ->live()
+                            ->helperText(function () {
+                                return new HtmlString('<small class="text-muted d-block mb-2">Ask users for email verification confirmation after their registration. </small>');
+                            }),
+
+                    ]),
+            ]);
+    }
+
 }
+
--- app/Filament/Admin/Pages/AdminPrivacyPolicyPage.php
+++ app/Filament/Admin/Pages/AdminPrivacyPolicyPage.php
@@ -3,7 +3,12 @@
 namespace App\Filament\Admin\Pages;
 
 use App\Filament\Admin\Pages\Abstract\AdminSettingsPage;
+use Filament\Forms\Components\Section;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Toggle;
+use Filament\Forms\Form;
 use Filament\Pages\Page;
+use Illuminate\Support\HtmlString;
 
 class AdminPrivacyPolicyPage extends AdminSettingsPage
 {
@@ -14,4 +19,46 @@ class AdminPrivacyPolicyPage extends AdminSettingsPage
     protected static ?string $title = 'Privacy Policy';
 
     protected static string $description = 'Configure your privacy policy settings';
+
+
+    public function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+
+                Section::make('Privacy policy settings')
+                    ->view('filament-forms::sections.section')
+                    ->description('A Privacy Policy is a legal agreement that explains what kinds of personal information you gather from website visitors, how you use this information, and how you keep it safe. Examples of personal information might include: Names. Dates of birth.
+The General Data Protection Regulation (EU) 2016/679 (GDPR) is a regulation in EU law on data protection and privacy in the European Union (EU) and the European Economic Area (EEA).')
+                    ->schema([
+
+                        Toggle::make('options.users.require_terms')
+                            ->label('Users must agree to the Terms and Conditions')
+                            ->live()
+                            ->helperText(function () {
+                                return new HtmlString('<small class="text-muted d-block mb-2">Should your users agree to the terms of use of the website</small>');
+                            }),
+
+                        TextInput::make('options.users.terms_label')
+                            ->label('Terms and conditions text')
+                            ->live()
+                            ->helperText(function () {
+                                return new HtmlString('<small class="text-muted d-block mb-2">The text will appear to the user</small>');
+                            })
+                            ->placeholder('I agree with the Terms and Conditions'),
+
+                        TextInput::make('options.users.terms_url')
+                            ->label('URL of terms and conditions')
+                            ->live()
+                            ->helperText(function () {
+                                return new HtmlString('<small class="text-muted d-block mb-2">Уou need to create this page and type in the address field.</small>');
+                            })
+                            ->placeholder('https://demo.microweber.org/v2/terms'),
+
+
+
+
+                    ]),
+            ]);
+    }
 }
