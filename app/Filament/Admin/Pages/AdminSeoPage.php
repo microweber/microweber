13579e17d4eea7256933c09011596463ee4554f9
Bozhidar <bobi@microweber.com>
2024-06-13 15:05:11 +0300

    Update AdminSeoPage.php

--- app/Filament/Admin/Pages/AdminSeoPage.php
+++ app/Filament/Admin/Pages/AdminSeoPage.php
@@ -5,7 +5,9 @@ namespace App\Filament\Admin\Pages;
 use App\Filament\Admin\Pages\Abstract\AdminSettingsPage;
 use Filament\Forms\Components\Section;
 use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Toggle;
 use Filament\Forms\Form;
+use Filament\Forms\Get;
 use Filament\Pages\Page;
 use Illuminate\Contracts\Support\Htmlable;
 use Illuminate\Support\HtmlString;
@@ -41,6 +43,20 @@ class AdminSeoPage extends AdminSettingsPage
                                 return new HtmlString('<small>If you have a Google Tag Manager account, you can verify ownership of a site using your Google Tag Manager container snippet code. To verify ownership using Google Tag Manager: Choose Google Tag Manager in the verification details page for your site, and follow the instructions shown. <a href="https://support.google.com/webmasters/answer/9008080?hl=en#choose_method&amp;zippy=%2Chtml-tag" target="_blank">Read the article here.</a></small>');
                             })
                             ->placeholder('Enter your website name'),
+
+                        Toggle::make('options.website.toggle')
+                            ->label('toggle')
+                            ->live()
+                            ->helperText('moeto toggleche'),
+
+
+                        TextInput::make('options.website.qko')
+                            ->label('pokazwa se pri toggle')
+                            ->hidden(function(Get $get) {
+                                return $get('options.website.toggle') !== '1';
+                            })
+                            ->live()
+                            ->helperText('pokazwa se pri toggle'),
 
                     ]),
 
