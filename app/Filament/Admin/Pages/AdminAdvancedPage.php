4059dc6d73a0bf536c6001c140294899c5d5f24c
Nikolay <rachkuw@gmail.com>
2024-06-14 14:52:41 +0300

    Update AdminAdvancedPage.php

--- app/Filament/Admin/Pages/AdminAdvancedPage.php
+++ app/Filament/Admin/Pages/AdminAdvancedPage.php
@@ -3,7 +3,14 @@
 namespace App\Filament\Admin\Pages;
 
 use App\Filament\Admin\Pages\Abstract\AdminSettingsPage;
+use Filament\Forms\Components\Section;
+use Filament\Forms\Components\Textarea;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Toggle;
+use Filament\Forms\Form;
+use Filament\Forms\Get;
 use Filament\Pages\Page;
+use Illuminate\Support\HtmlString;
 
 class AdminAdvancedPage extends AdminSettingsPage
 {
@@ -14,5 +21,53 @@ class AdminAdvancedPage extends AdminSettingsPage
     protected static string $description = 'Configure your advanced settings';
 
     protected static ?string $title = 'Advanced';
+
+    public function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+
+                Section::make('Custom tags')
+                    ->view('filament-forms::sections.section')
+                    ->description('Allows you to insert custom code in the website header and footer. For e.g. Live chat, Google Ads and others.')
+                    ->schema([
+
+                       Textarea::make('options.website.website_head')
+                            ->label('Custom head tags')
+                            ->live()
+                            ->rows(5)
+                            ->cols(5)
+                            ->helperText(function () {
+                                return new HtmlString('<small class="mb-2 text-muted">Advanced functionality. You can put custom html in the site head-tags. Please put only valid meta tags or you can break your site.</small>');
+                            }),
+
+
+                        Textarea::make('options.website.website_footer')
+                            ->label('Custom footer tags')
+                            ->live()
+                            ->rows(5)
+                            ->cols(5)
+                            ->helperText(function () {
+                                return new HtmlString('<small class="mb-2 text-muted">Advanced functionality. You can put custom html in the site footer-tags. Please put only valid meta tags or you can break your site.</small>');
+                            }),
+
+                        Textarea::make('options.website.robots_txt')
+                            ->label('Robots.txt content')
+                            ->live()
+                            ->rows(5)
+                            ->cols(5)
+                            ->helperText(function () {
+                                return new HtmlString('<small class="mb-2 text-muted">The robots. txt file, also known as the robots exclusion protocol or standard, is a text file that tells web robots (most often search engines) which pages on your site to crawl. It also tells web robots which pages not to crawl.</small>');
+                            }),
+
+                        Textarea::make('options.website.ads_txt')
+                            ->label('Ads.txt content')
+                            ->live()
+                            ->rows(5)
+                            ->cols(5),
+                        
+                    ]),
+            ]);
+    }
 
 }
