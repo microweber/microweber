b773c7356245cc3294fdb7e6cfa8be212ac64b19
Peter Ivanov <peter@microweber.com>
2024-06-14 11:10:15 +0300

    update

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -5,7 +5,6 @@ use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 //return Application::configure(basePath: dirname(__DIR__))
 return \MicroweberPackages\App\LaravelApplication::configure(basePath: dirname(__DIR__))
-//return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
