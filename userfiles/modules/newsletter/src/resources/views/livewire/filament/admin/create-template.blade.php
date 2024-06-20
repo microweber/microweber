f7ec59e99bb16bda3af31e44e1517975d06f6888
Bozhidar <bobi@microweber.com>
2024-06-20 18:11:37 +0300

    Update create-template.blade.php

--- userfiles/modules/newsletter/src/resources/views/livewire/filament/admin/create-template.blade.php
+++ userfiles/modules/newsletter/src/resources/views/livewire/filament/admin/create-template.blade.php
@@ -2,7 +2,7 @@
 
 @section('newsletter-content')
 
-    <div class="text-center my-6">
+    <div class="text-center my-12">
         <h1 class="text-2xl font-bold">
             Email templates gallery
         </h1>
@@ -10,11 +10,19 @@
             Choose a template to start with
         </p>
     </div>
+    <script>
+        document.addEventListener('DOMContentLoaded', function () {
+            const iframes = document.querySelectorAll('iframe[data-src]');
+            iframes.forEach(iframe => {
+                iframe.src = iframe.dataset.src;
+            });
+        });
+    </script>
     <div class="grid grid-cols-3 gap-4">
         @foreach($emailTemplates as $template)
 
-            <div class="bg-white rounded shadow w-[20rem]">
-                <div style="width: 100%; height: 800px; overflow: hidden; box-sizing: border-box; transform-origin: left top 0px; transform: scale(0.37);">
+            <div class="justify-content-center items-center bg-white rounded shadow h-[20rem]">
+                <div style="width: 800px; height: 800px; overflow: hidden; box-sizing: border-box; transform-origin: left top 0px; transform: scale(0.37);">
                     <iframe data-src="{{ $template['demoUrl'] }}" scrolling="no" style="height: 100%; width: 100%; display: block; border: 0px transparent;">
 
                     </iframe>
