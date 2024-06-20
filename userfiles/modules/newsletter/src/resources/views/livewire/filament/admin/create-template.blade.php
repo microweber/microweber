59becf084f5d5651a3427edfe0c6e82cd59d88eb
Bozhidar <bobi@microweber.com>
2024-06-20 18:44:12 +0300

    Update create-template.blade.php

--- userfiles/modules/newsletter/src/resources/views/livewire/filament/admin/create-template.blade.php
+++ userfiles/modules/newsletter/src/resources/views/livewire/filament/admin/create-template.blade.php
@@ -23,7 +23,7 @@
 
             <a href="{{route('filament.admin.pages.newsletter.template-editor')}}">
                 <div class="bg-white rounded shadow h-[20rem]">
-                    <div style="pointer-events:none;width: 800px; height: 200px; overflow: hidden; box-sizing: border-box; transform-origin: left top 0px; transform: scale(0.37);">
+                    <div style="pointer-events:none;width: 800px; height: 800px; overflow: hidden; box-sizing: border-box; transform-origin: left top 0px; transform: scale(0.37);">
                         <iframe data-src="{{ $template['demoUrl'] }}" scrolling="no" style="pointer-events:none;height: 100%; width: 100%; display: block; border: 0px transparent;">
 
                         </iframe>
