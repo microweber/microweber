422e395e8b808b94fd9d05409be590ea72537f41
Bozhidar <bobi@microweber.com>
2024-06-21 14:09:33 +0300

    update

--- userfiles/modules/newsletter/src/Filament/Admin/Pages/CreateCampaign.php
+++ userfiles/modules/newsletter/src/Filament/Admin/Pages/CreateCampaign.php
@@ -2,10 +2,19 @@
 
 namespace MicroweberPackages\Modules\Newsletter\Filament\Admin\Pages;
 
+use Filament\Forms\Components\Actions;
+use Filament\Forms\Components\Actions\Action;
+use Filament\Forms\Components\Group;
+use Filament\Forms\Components\Radio;
 use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\Wizard;
 use Filament\Forms\Form;
+use Filament\Forms\Get;
 use Filament\Pages\Page;
+use JaOcero\RadioDeck\Forms\Components\RadioDeck;
+use MicroweberPackages\Filament\Forms\Components\MwFileUpload;
+use MicroweberPackages\FormBuilder\Elements\RadioButton;
 use MicroweberPackages\Modules\Newsletter\Filament\Components\SelectTemplate;
 use MicroweberPackages\Modules\Newsletter\Models\NewsletterCampaign;
 use MicroweberPackages\Modules\Newsletter\Models\NewsletterCampaignsSendLog;
@@ -20,20 +29,67 @@ class CreateCampaign extends Page
 
     protected static bool $shouldRegisterNavigation = false;
 
+    public $template = '';
+    public $recipientsFrom = '';
+
     public function form(Form $form): Form
     {
+
+        $countSubscribers = NewsletterSubscriber::count();
+
         return $form->schema([
 
             Wizard::make([
-//                Wizard\Step::make('Template')
+//                Wizard\Step::make('Email')
+//                    ->icon('heroicon-o-paint-brush')
 //                    ->schema([
 //                        SelectTemplate::make('template'),
 //                    ]),
-                Wizard\Step::make('Campaign Details')
+                Wizard\Step::make('Recipients')
+                    ->icon('heroicon-o-users')
                     ->schema([
-                        // ...
+
+                        RadioDeck::make('recipientsFrom')
+                            ->columns(3)
+                            ->icons([
+                                'all_subscribers' => 'heroicon-o-users',
+                                'specific_lists' => 'heroicon-o-list-bullet',
+                                'import_new_list' => 'heroicon-o-arrow-up-tray',
+                            ])
+                            ->color('primary')
+                            ->live()
+                            ->options([
+                                'all_subscribers' => 'All subscribers' . ' (' . $countSubscribers . ')',
+                                'specific_lists' => 'Specific lists',
+                                'import_new_list' => 'Import new list',
+                            ]),
+
+                        Radio::make('list_id')
+                            ->label('Select list')
+                            ->hidden(function (Get $get) {
+                                if ($get('recipientsFrom') == 'specific_lists') {
+                                    return false;
+                                }
+                                return true;
+                            })
+                            ->options(NewsletterList::all()->pluck('name', 'id')),
+
+                        Group::make([
+                            TextInput::make('list_name')
+                                ->label('List name'),
+                            MwFileUpload::make('upload_list_file')
+                                ->label('Upload list file'),
+                        ])->hidden(function (Get $get) {
+                            if ($get('recipientsFrom') == 'import_new_list') {
+                                return false;
+                            }
+                            return true;
+                        }),
+
+
                     ]),
-                Wizard\Step::make('Schedule')
+                Wizard\Step::make('Delivery')
+                    ->icon('heroicon-o-paper-airplane')
                     ->schema([
                         // ...
                     ]),
--- userfiles/modules/newsletter/src/resources/views/livewire/filament/admin/create-campaign.blade.php
+++ userfiles/modules/newsletter/src/resources/views/livewire/filament/admin/create-campaign.blade.php
@@ -4,13 +4,13 @@
 
     <div class="text-center mt-12 mb-6">
         <h1 class="text-2xl font-bold">
-            Creating a new campaign
+            Creating a new email marketing campaign
         </h1>
         <p class="text-gray-600">
-            Continue with  the wizard to create a new campaign
+            Continue with  the wizard to create a new email marketing campaign
         </p>
     </div>
-    <div>
+    <div class="mb-12">
         {{$this->form}}
     </div>
 
