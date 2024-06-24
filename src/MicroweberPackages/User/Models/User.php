f33de9db8cd7bbd8fd352a7883be79930301f5ed
Peter Ivanov <peter@microweber.com>
2024-06-24 15:28:42 +0300

    update

--- src/MicroweberPackages/User/Models/User.php
+++ src/MicroweberPackages/User/Models/User.php
@@ -236,8 +236,9 @@ class User extends Authenticatable implements MustVerifyEmail
             $this->rules['email'] = [
                 'min:0',
                 'max:500',
-                Rule::unique('users', 'email')->ignore($data['id'], 'email')
+                'unique:users,email,'.$data['id'].',id'
             ];
+
         } else if (isset($data['email']) && !empty($data['email'])) {
             $this->rules['email'] = 'min:0|max:500|unique:users';
         }
