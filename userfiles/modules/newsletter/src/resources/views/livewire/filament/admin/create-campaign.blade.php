ad36f7203f5a70fc4134247c3c53161d473462b2
Bozhidar <bobi@microweber.com>
2024-07-01 18:30:05 +0300

    Update create-campaign.blade.php

--- userfiles/modules/newsletter/src/resources/views/livewire/filament/admin/create-campaign.blade.php
+++ userfiles/modules/newsletter/src/resources/views/livewire/filament/admin/create-campaign.blade.php
@@ -5,14 +5,13 @@
         name: @entangle('name'),
     }" class="my-24 w-[36rem] m-auto">
 
-
-         <h3 class="text-xl mb-4">
+        <h3 class="text-xl mb-4">
             Creating a new email marketing campaign
         </h3>
 
         <div class="mb-4">
             <input class="border-transparent border-b-1 border-b-gray-500/30 bg-white p-0 px-4 rounded-md py-4 w-full text-xl"
-                   placeholder="Enter campaign name" required="required" type="text"
+                   placeholder="Enter your campaign name..." required="required" type="text"
                    wire:keydown.enter="createCampaign"
                    wire:model.live="name">
         </div>
@@ -24,7 +23,6 @@
             </x-filament::button>
 
         </div>
-
 
     </div>
     </div>
