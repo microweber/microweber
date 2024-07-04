7a5c250217b70b04d5de8722f8aae00ad6146004
Bozhidar <bobi@microweber.com>
2024-07-04 15:24:35 +0300

    Update create-new-campaign-btn.blade.php

--- userfiles/modules/newsletter/src/resources/views/livewire/filament/admin/sidebar/create-new-campaign-btn.blade.php
+++ userfiles/modules/newsletter/src/resources/views/livewire/filament/admin/sidebar/create-new-campaign-btn.blade.php
@@ -1,8 +1,8 @@
 <div class="p-4 w-full mb-4">
     <a href="{{admin_url('newsletter/create-campaign')}}"
 
-       class="bg-white hover:shadow rounded-lg border border-gray-500/20 flex items-center gap-2 text-[0.6rem] uppercase w-full py-2 px-4 hover:bg-gray-500/5"
+       class="bg-white hover:shadow rounded-lg border border-gray-500/20 flex items-center gap-2 text-sm w-full py-2 px-4 hover:bg-gray-500/5"
     >
-        @svg("heroicon-o-pencil-square", "h-4 w-4 text-blue-500") Create new campaign
+        @svg("heroicon-o-pencil-square", "h-4 w-4 text-blue-500") Create campaign
     </a>
 </div>
