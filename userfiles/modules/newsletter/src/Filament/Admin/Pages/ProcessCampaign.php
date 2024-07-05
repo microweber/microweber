5fb9b547b4d36e8f49f02cdf672d48c41051ba11
Bozhidar <bobi@microweber.com>
2024-07-05 14:52:55 +0300

    update

--- userfiles/modules/newsletter/src/Filament/Admin/Pages/ProcessCampaign.php
+++ userfiles/modules/newsletter/src/Filament/Admin/Pages/ProcessCampaign.php
@@ -25,6 +25,7 @@ use Filament\Support\Enums\IconSize;
 use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\HtmlString;
 use JaOcero\RadioDeck\Forms\Components\RadioDeck;
+use MicroweberPackages\Backup\Loggers\DefaultLogger;
 use MicroweberPackages\Filament\Forms\Components\MwFileUpload;
 use MicroweberPackages\FormBuilder\Elements\RadioButton;
 use MicroweberPackages\Modules\Newsletter\Filament\Admin\Resources\SenderAccountsResource;
@@ -37,6 +38,7 @@ use MicroweberPackages\Modules\Newsletter\Models\NewsletterSenderAccount;
 use MicroweberPackages\Modules\Newsletter\Models\NewsletterSubscriber;
 use Livewire\Attributes\On;
 use MicroweberPackages\Modules\Newsletter\Models\NewsletterTemplate;
+use MicroweberPackages\Modules\Newsletter\ProcessCampaigns;
 
 class ProcessCampaign extends Page
 {
@@ -45,5 +47,56 @@ class ProcessCampaign extends Page
     protected static string $view = 'microweber-module-newsletter::livewire.filament.admin.process-campaign';
 
     protected static bool $shouldRegisterNavigation = false;
+
+
+    public $log = '';
+    private $logger;
+    public $logPublicUrl = '';
+
+    public $listeners = [
+        'processCampaigns'=>'processCampaigns'
+    ];
+
+    public function mount()
+    {
+        $this->setupLogger();
+    }
+
+    public function setupLogger()
+    {
+        $this->logger = new ProcessCampaignsLogger();
+        $this->logger->clearLog();
+
+        $logPublicUrl = $this->logger->getLogFilepath();
+        $logPublicUrl = str_replace(userfiles_path(), userfiles_url(), $logPublicUrl);
+
+        $this->logPublicUrl = $logPublicUrl;
+    }
+
+    public function processCampaigns()
+    {
+        $this->setupLogger();
+
+        $processCampaigns = new ProcessCampaigns();
+        $processCampaigns->setLogger($this->logger);
+        $processCampaigns->run();
+    }
+
+}
+
+
+class ProcessCampaignsLogger extends DefaultLogger {
+
+    public function info($msg) {
+        $this->setLogInfo($msg . '<br>');
+    }
+
+    public function warn($msg) {
+        $this->setLogInfo($msg. '<br>');
+    }
+
+    public function error($msg) {
+        $this->setLogInfo($msg. '<br>');
+    }
 
 }
--- userfiles/modules/newsletter/src/resources/views/livewire/filament/admin/process-campaign.blade.php
+++ userfiles/modules/newsletter/src/resources/views/livewire/filament/admin/process-campaign.blade.php
@@ -1,11 +1,36 @@
 <div>
-    <div class="mt-2">
+
+    <div class="text-center">
         <h3>
-            Process Campaign
+            Process Campaigns
         </h3>
     </div>
 
-    <div>
-        
+    <div class="mt-4 px-5 pb-5">
+
+        @script
+        <script>
+
+            let runningProcessCampaigns = true;
+            $wire.dispatch('processCampaigns');
+            let log = document.getElementById('js-process-campaigns-log');
+            log.innerHTML = 'Processing...';
+
+            let processCampaignInterval = setInterval(function() {
+                fetch('{{$logPublicUrl}}').then(response => response.text()).then(data => {
+                    log.innerHTML = data;
+                    if (data.indexOf('Process Campaigns Complete') !== -1) {
+                        runningProcessCampaigns = false;
+                        clearInterval(processCampaignInterval);
+                    }
+                });
+            }, 3000);
+
+        </script>
+        @endscript
+
+        <div id="js-process-campaigns-log"></div>
+
     </div>
+
 </div>
