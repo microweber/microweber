378fbc3a8297c29acaa2db667eabf246e0754758
Peter Ivanov <peter@microweber.com>
2024-07-16 16:10:26 +0300

    update

--- src/MicroweberPackages/Menu/resources/views/livewire/admin/menus-list.blade.php
+++ src/MicroweberPackages/Menu/resources/views/livewire/admin/menus-list.blade.php
@@ -18,6 +18,17 @@
     @endscript
     <div>
 
+        <style>
+            ul {
+                list-style-type: none;
+                margin: 0;
+                padding: 0;
+            }
+            ul ul {
+                margin-left: 20px;
+            }
+        </style>
+
         <script>
             function sortableData() {
                 return {
@@ -25,28 +36,35 @@
 
                         var _orderChangeHandle = function (e, ui){
                             setTimeout(function(){
-                                var old = $.extend({},ui.item[0]._data);
-                                var obj = ui.item[0]._data;
-                                var objParent = ui.item[0].parentNode.parentNode._data;
-                                ui.item[0].dataset.parent_id = objParent ? objParent.id : 0;
 
-                                obj.parent_id = objParent ? objParent.id : 0;
-                                obj.parent_type = objParent ? objParent.id : 'page';
-                                var newdata = [];
-                                mw.$('li', scope.list).each(function(){
-                                    if(this._data) newdata.push(this._data);
-                                });
-                                scope.options.data = newdata;
-                                var local = [];
-                                mw.$(ui.item[0].parentNode).children('li').each(function(){
-                                    if(this._data) {
-                                        local.push(this._data.id);
-                                    }
-                                });
-                                //$(scope.list).remove();
-                                //scope.init();
-                                mw.$(scope).trigger('orderChange', [obj, scope.options.data, old, local]);
-                                scope.dispatch('orderChange', [obj, scope.options.data, old, local]);
+                            //
+                               var result = $(ui).toArray({startDepthCount: 0, excludeRoot: false});
+                               console.log(result)
+                                console.log(ui)
+
+
+                                // var old = $.extend({},ui.item[0]._data);
+                                // var obj = ui.item[0]._data;
+                                // var objParent = ui.item[0].parentNode.parentNode._data;
+                                // ui.item[0].dataset.parent_id = objParent ? objParent.id : 0;
+                                //
+                                // obj.parent_id = objParent ? objParent.id : 0;
+                                // obj.parent_type = objParent ? objParent.id : 'page';
+                                // var newdata = [];
+                                // mw.$('li', scope.list).each(function(){
+                                //     if(this._data) newdata.push(this._data);
+                                // });
+                                // scope.options.data = newdata;
+                                // var local = [];
+                                // mw.$(ui.item[0].parentNode).children('li').each(function(){
+                                //     if(this._data) {
+                                //         local.push(this._data.id);
+                                //     }
+                                // });
+                                // //$(scope.list).remove();
+                                // //scope.init();
+                                // mw.$(scope).trigger('orderChange', [obj, scope.options.data, old, local]);
+                                // scope.dispatch('orderChange', [obj, scope.options.data, old, local]);
                             }, 110);
                         };
 
