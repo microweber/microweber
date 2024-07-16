6f002f45a3ba7ba9ca61be8b1b57de7c1e0b345c
Peter Ivanov <peter@microweber.com>
2024-07-16 16:38:49 +0300

    update

--- src/MicroweberPackages/MetaTags/Entities/AdminFilamentJsLibsScriptTag.php
+++ src/MicroweberPackages/MetaTags/Entities/AdminFilamentJsLibsScriptTag.php
@@ -11,8 +11,13 @@ class AdminFilamentJsLibsScriptTag implements TagInterface, \Stringable
     public function toHtml(): string
     {
         $libs = Vite::asset('src/MicroweberPackages/LiveEdit/resources/front-end/js/admin/admin-filament-libs.js');
+        $lib_tynymce = mw_includes_url() . 'api/libs/tinymce/tinymce.min.js';
+
 
         $append_html = '' . "\r\n";
+        $append_html .= '<script src="' . $lib_tynymce . '" id="mw-tynymce-js-libs-scripts"></script>' . "\r\n";
+
+        $append_html .= '' . "\r\n";
         $append_html .= '<script src="' . $libs . '" type="module" id="mw-filament-js-libs-scripts"></script>' . "\r\n";
 
         return $append_html;
