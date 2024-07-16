e0a20c81cd123bb75b04331c195021ae34bc427e
Peter Ivanov <peter@microweber.com>
2024-07-16 14:14:53 +0300

    update

--- src/MicroweberPackages/Media/resources/views/admin/livewire/list-media-for-model.blade.php
+++ src/MicroweberPackages/Media/resources/views/admin/livewire/list-media-for-model.blade.php
@@ -28,7 +28,7 @@
 
 
                     editMediaOptionsById(id) {
-                        this.modalImageSettingsOpen = true;
+                        alert(id)
                     },
 
                     async bulkDeleteSelectedMedia() {
@@ -154,65 +154,6 @@
                                 </div>
 
                             @endforeach
-
-
-                                <div x-show="modalImageSettingsOpen" class="fixed inset-0 z-50 overflow-y-auto" aria-labelledby="modal-title" role="dialog" aria-modal="true">
-                                    <div class="flex items-end justify-center min-h-screen px-4 text-center md:items-center sm:block sm:p-0">
-                                        <div x-cloak @click="modalImageSettingsOpen = false" x-show="modalImageSettingsOpen"
-                                             x-transition:enter="transition ease-out duration-300 transform"
-                                             x-transition:enter-start="opacity-0"
-                                             x-transition:enter-end="opacity-100"
-                                             x-transition:leave="transition ease-in duration-200 transform"
-                                             x-transition:leave-start="opacity-100"
-                                             x-transition:leave-end="opacity-0"
-                                             class="fixed inset-0 transition-opacity bg-gray-500 bg-opacity-40" aria-hidden="true"
-                                        ></div>
-
-                                        <div x-cloak x-show="modalImageSettingsOpen"
-                                             x-transition:enter="transition ease-out duration-300 transform"
-                                             x-transition:enter-start="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-                                             x-transition:enter-end="opacity-100 translate-y-0 sm:scale-100"
-                                             x-transition:leave="transition ease-in duration-200 transform"
-                                             x-transition:leave-start="opacity-100 translate-y-0 sm:scale-100"
-                                             x-transition:leave-end="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-                                             class="inline-block w-full max-w-xl p-8 my-20 overflow-hidden text-left transition-all transform bg-white rounded-lg shadow-xl 2xl:max-w-2xl"
-                                        >
-                                            <div class="flex items-center justify-between space-x-4">
-                                                <h1 class="text-xl font-medium text-gray-800 ">Todo</h1>
-
-                                                <button @click="modalImageSettingsOpen = false" class="text-gray-600 focus:outline-none hover:text-gray-700">
-                                                    <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
-                                                    </svg>
-                                                </button>
-                                            </div>
-
-                                            <p class="mt-2 text-sm text-gray-500 ">
-                                              TODO
-                                            </p>
-
-                                            <form class="mt-5">
-                                                <div>
-                                                    <label for="user name" class="block text-sm text-gray-700 capitalize dark:text-gray-200">Todo</label>
-                                                    <input placeholder="Todo" type="text" class="block w-full px-3 py-2 mt-2 text-gray-600 placeholder-gray-400 bg-white border border-gray-200 rounded-md focus:border-indigo-400 focus:outline-none focus:ring focus:ring-indigo-300 focus:ring-opacity-40">
-                                                </div>
-
-                                                <div class="mt-4">
-                                                    <label for="email" class="block text-sm text-gray-700 capitalize dark:text-gray-200">Todo</label>
-                                                    <input placeholder="Todo" type="email" class="block w-full px-3 py-2 mt-2 text-gray-600 placeholder-gray-400 bg-white border border-gray-200 rounded-md focus:border-indigo-400 focus:outline-none focus:ring focus:ring-indigo-300 focus:ring-opacity-40">
-                                                </div>
-
-
-
-                                                <div class="flex justify-end mt-6">
-                                                    <button type="button" class="px-3 py-2 text-sm tracking-wide text-white capitalize transition-colors duration-200 transform bg-indigo-500 rounded-md dark:bg-indigo-600 dark:hover:bg-indigo-700 dark:focus:bg-indigo-700 hover:bg-indigo-600 focus:outline-none focus:bg-indigo-500 focus:ring focus:ring-indigo-300 focus:ring-opacity-50">
-                                                        Todo
-                                                    </button>
-                                                </div>
-                                            </form>
-                                        </div>
-                                    </div>
-                                </div>
 
 
 
--- src/MicroweberPackages/Menu/resources/views/livewire/admin/menus-list.blade.php
+++ src/MicroweberPackages/Menu/resources/views/livewire/admin/menus-list.blade.php
@@ -1,33 +1,107 @@
 <div>
-    <div wire:ignore>
+
+    @script
+
+
+    <script>
+
+        mw.lib.require('jqueryui')
+        mw.lib.require('nestedSortable')
+        document.addEventListener('livewire:initialized', async () => {
+
+
+
+
+        })
+    </script>
+
+    @endscript
+    <div>
 
         <script>
             function sortableData() {
                 return {
                     init() {
 
-                        //onclick on .menu_element alert
-                        var menuElements = document.querySelectorAll('.admin-menu-items-holder .menu_element_link');
-                        for (var i = 0; i < menuElements.length; i++) {
-                            if (menuElements[i].classList.contains('binded-click')) {
-                                continue;
-                            }
-                            menuElements[i].classList.add('binded-click');
+                        var _orderChangeHandle = function (e, ui){
+                            setTimeout(function(){
+                                var old = $.extend({},ui.item[0]._data);
+                                var obj = ui.item[0]._data;
+                                var objParent = ui.item[0].parentNode.parentNode._data;
+                                ui.item[0].dataset.parent_id = objParent ? objParent.id : 0;
+
+                                obj.parent_id = objParent ? objParent.id : 0;
+                                obj.parent_type = objParent ? objParent.id : 'page';
+                                var newdata = [];
+                                mw.$('li', scope.list).each(function(){
+                                    if(this._data) newdata.push(this._data);
+                                });
+                                scope.options.data = newdata;
+                                var local = [];
+                                mw.$(ui.item[0].parentNode).children('li').each(function(){
+                                    if(this._data) {
+                                        local.push(this._data.id);
+                                    }
+                                });
+                                //$(scope.list).remove();
+                                //scope.init();
+                                mw.$(scope).trigger('orderChange', [obj, scope.options.data, old, local]);
+                                scope.dispatch('orderChange', [obj, scope.options.data, old, local]);
+                            }, 110);
+                        };
+
+                        console.log(1111111111111111111)
+                        console.log($('ul','.admin-menu-items-holder'))
 
 
-                            menuElements[i].addEventListener('click', (e)  =>{
-                                e.stopPropagation();
-                                e.preventDefault();
+                        var sortableLists = $('ul','.admin-menu-items-holder');
 
-                                var id = e.target.getAttribute('data-item-id');
-                              //  Livewire.dispatch('editMenuItem',{id: id});
+                        for (var i = 0; i < sortableLists.length; i++) {
 
-                                this.$wire.mountAction('editAction', { id: id })
-
-
-                                //s  alert(id)
+                            $(sortableLists[i]).nestedSortable({
+                                items: ".menu_element",
+                                listType:'ul',
+                                handle:'.menu_element',
+                                start: function(){ // firefox triggers click when drag ends
+                                    // scope._disableClick = true;
+                                },
+                                stop: function(){
+                                    //  setTimeout(() => {scope._disableClick = false;}, 78)
+                                },
+                                update:function(e, ui){
+                                    _orderChangeHandle(e, ui);
+                                }
                             });
+
+
                         }
+
+
+
+                        //
+                        // //onclick on .menu_element alert
+                        // var menuElements = document.querySelectorAll('.admin-menu-items-holder .menu_element_link');
+                        // for (var i = 0; i < menuElements.length; i++) {
+                        //     if (menuElements[i].classList.contains('binded-click')) {
+                        //         continue;
+                        //     }
+                        //     menuElements[i].classList.add('binded-click');
+                        //
+                        //
+                        //     menuElements[i].addEventListener('click', (e) => {
+                        //         e.stopPropagation();
+                        //         e.preventDefault();
+                        //
+                        //         var id = e.target.getAttribute('data-item-id');
+                        //         //  Livewire.dispatch('editMenuItem',{id: id});
+                        //
+                        //         this.$wire.mountAction('editAction', {id: id})
+                        //
+                        //
+                        //         //s  alert(id)
+                        //     });
+                        //
+                        // }
 
                     },
                     updateOrder(event) {
@@ -42,41 +116,42 @@
 
     <div x-data="sortableData()" x-init="init()">
 
+
+        <div class="admin-menu-items-holder">
+
+
+            @foreach ($menus as $menu)
+
+                <div>
+
+
+                    @php
+                        $params = array(
+                          'menu_id' => $menu->id,
+
+                      );
+                         print  menu_tree($params);
+                    @endphp
+
+
+                    <h2> {{ $menu->id }}  {{ $menu->title }}   {{ $menu->item_type }}</h2>
+
+
+                    {{ ($this->editAction)(['id' => $menu->id]) }}
+
+
+                    {{ ($this->deleteAction)(['id' => $menu->id]) }}
+
+                </div>
+            @endforeach
+
+        </div>
+
+
+        aaaaaa
+
+
     </div>
-
-
-    <div class="admin-menu-items-holder">
-
-
-        @foreach ($menus as $menu)
-
-            <div>
-
-
-                @php
-                    $params = array(
-                      'menu_id' => $menu->id,
-
-                  );
-                     print  menu_tree($params);
-                @endphp
-
-
-                <h2> {{ $menu->id }}  {{ $menu->title }}   {{ $menu->item_type }}</h2>
-
-
-                {{ ($this->editAction)(['id' => $menu->id]) }}
-
-
-                {{ ($this->deleteAction)(['id' => $menu->id]) }}
-
-            </div>
-        @endforeach
-
-    </div>
-
-
-    aaaaaa
 
 
     <x-filament-actions::modals/>
