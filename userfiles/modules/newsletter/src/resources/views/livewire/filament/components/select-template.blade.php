10832e8449b69e866d37b83ab95ad98ad939bb9a
Bozhidar <bobi@microweber.com>
2024-07-23 13:30:13 +0300

    update

--- userfiles/modules/newsletter/src/resources/views/livewire/filament/components/select-template.blade.php
+++ userfiles/modules/newsletter/src/resources/views/livewire/filament/components/select-template.blade.php
@@ -2,7 +2,7 @@
     :component="$getFieldWrapperView()"
     :field="$field"
 >
-<div>
+<div x-data="{ activeTab: 'default' }">
 
     <style>
         .iframe-container {
@@ -60,8 +60,77 @@
         </div>
 
     @else
-       <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
-           @foreach($getEmailTemplates() as $template)
+
+
+        <x-filament::tabs>
+            <x-filament::tabs.item
+                alpine-active="activeTab === 'default'"
+                x-on:click="activeTab = 'default'"
+            >
+                Default Templates
+            </x-filament::tabs.item>
+
+            <x-filament::tabs.item
+                alpine-active="activeTab === 'existing'"
+                x-on:click="activeTab = 'existing'"
+            >
+                Existing Templates
+            </x-filament::tabs.item>
+
+        </x-filament::tabs>
+
+        <div x-show="activeTab === 'default'" class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 mt-4">
+            @foreach($getEmailTemplates() as $template)
+
+                <div class="group rounded-md border border-gray-500/10 hover:shadow-xl">
+                    <div class="relative">
+
+                        <div class="iframe-container rounded-md">
+                            <iframe
+                                loading="lazy"
+                                src="{{admin_url('modules/newsletter/preview-email-template')}}?filename={{$template['name']}}"
+                                scrolling="no"
+                                class="iframe rounded-md"></iframe>
+                        </div>
+
+                        <div class="p-4">
+                            <h3 class="text-lg font-semibold">{{ucfirst($template['name'])}}</h3>
+                        </div>
+
+                        <div class="opacity-0 group-hover:opacity-100 transition absolute top-0 w-full h-full flex items-center gap-4 p-8 backdrop-blur-sm">
+
+                            <div class="w-full flex items-center justify-center gap-4">
+
+                                <x-filament::button
+                                    size="sm"
+                                    color="info"
+                                    wire:click="startWithTemplate('{{$template['name']}}')"
+                                >
+                                    @if($getCampaignId())
+                                        Use
+                                    @else
+                                        Start
+                                    @endif
+                                </x-filament::button>
+
+                                <x-filament::button
+                                    size="sm"
+                                    color="gray"
+                                    href="{{admin_url('modules/newsletter/preview-email-template')}}?filename={{$template['name']}}"
+                                    tag="a"
+                                    target="_blank"
+                                >
+                                    Preview
+                                </x-filament::button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            @endforeach
+        </div>
+       <div x-show="activeTab === 'existing'" class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 mt-4">
+           @foreach(\MicroweberPackages\Modules\Newsletter\Models\NewsletterTemplate::all() as $template)
 
                <div class="group rounded-md border border-gray-500/10 hover:shadow-xl">
                    <div class="relative">
@@ -69,13 +138,13 @@
                        <div class="iframe-container rounded-md">
                            <iframe
                                loading="lazy"
-                               src="{{admin_url('modules/newsletter/preview-email-template')}}?filename={{$template['name']}}"
+                               src=""
                                scrolling="no"
                                class="iframe rounded-md"></iframe>
                        </div>
 
                        <div class="p-4">
-                            <h3 class="text-lg font-semibold">{{ucfirst($template['name'])}}</h3>
+                            <h3 class="text-lg font-semibold">{{ucfirst($template['title'])}}</h3>
                         </div>
 
                        <div class="opacity-0 group-hover:opacity-100 transition absolute top-0 w-full h-full flex items-center gap-4 p-8 backdrop-blur-sm">
@@ -85,7 +154,7 @@
                                <x-filament::button
                                    size="sm"
                                    color="info"
-                                   wire:click="startWithTemplate('{{$template['name']}}')"
+                                   wire:click="startWithTemplateById('{{$template['id']}}')"
                                >
                                   @if($getCampaignId())
                                    Use
@@ -97,7 +166,7 @@
                                <x-filament::button
                                    size="sm"
                                    color="gray"
-                                   href="{{admin_url('modules/newsletter/preview-email-template')}}?filename={{$template['name']}}"
+                                   href=""
                                    tag="a"
                                    target="_blank"
                                >
@@ -109,8 +178,8 @@
                </div>
 
            @endforeach
-
        </div>
+
     @endif
 </div>
 </x-dynamic-component>
