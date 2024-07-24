1e3c0b382b981efb6b25ef173cdd80ae3ab733f0
Bozhidar <bobi@microweber.com>
2024-07-24 16:30:59 +0300

    Update OrderResource.php

--- app/Filament/Admin/Resources/OrderResource.php
+++ app/Filament/Admin/Resources/OrderResource.php
@@ -322,11 +322,11 @@ class OrderResource extends Resource
 //                        }
 
                         return [
-                            Forms\Components\Select::make('field_name')
+                            Forms\Components\Select::make('custom_field_id')
                                 ->label('Field')
                                 ->options($customFieldsOptions)
                                 ->live(),
-                            Forms\Components\Select::make('field_value')
+                            Forms\Components\Select::make('custom_field_value_id')
                                 ->label('Field Value')
                                 ->hidden(function (Forms\Get $get) use($customFieldsOptions) {
                                     if (in_array($get('field_name'), $customFieldsOptions)) {
