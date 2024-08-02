4d3b945ac49216e5f94c48a69e318732ffb34101
Peter Ivanov <peter@microweber.com>
2024-08-02 14:40:50 +0300

    update

---
+++ standalone-packages/microweber-packages/frontend-assets/README.md
@@ -0,0 +1,8 @@
+# Microweber CMS - Microweber frontend assets
+
+Frontend assets for Microweber CMS.
+
+
+```sh
+php artisan vendor:publish --tag=public --force --ansi
+```
---
+++ standalone-packages/microweber-packages/frontend-assets/src/MicroweberFrontendAssetsServiceProvider.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace MicroweberPackages\FrontendAssets;
+
+
+use Spatie\LaravelPackageTools\PackageServiceProvider;
+use Spatie\LaravelPackageTools\Package;
+
+class MicroweberFrontendAssetsServiceProvider extends PackageServiceProvider
+{
+
+    public function configurePackage(Package $package): void
+    {
+        $package
+            ->hasAssets()
+            ->name('microweber-packages/frontend-assets');
+    }
+
+
+    public function register(): void
+    {
+        $this->publishes([
+            __DIR__ . '/../resources/assets' => public_path('vendor/microweber-packages/frontend-assets'),
+        ], 'public');
+
+        parent::register();
+
+
+    }
+
+}
