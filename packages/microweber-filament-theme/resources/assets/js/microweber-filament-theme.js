4dea00667f1719a345ec2733fdb6c82994e6b973
Nikolay <rachkuw@gmail.com>
2024-08-07 14:43:53 +0300

    update

--- packages/microweber-filament-theme/resources/assets/js/microweber-filament-theme.js
+++ packages/microweber-filament-theme/resources/assets/js/microweber-filament-theme.js
@@ -1,1 +1,20 @@
 console.log('microweber-filament-theme.js');
+
+
+// JavaScript to toggle dropdown visibility
+document.addEventListener('DOMContentLoaded', function () {
+    const dropdownButton = document.getElementById('dropdownButton');
+    const dropdownMenu = document.getElementById('dropdownMenu');
+
+    dropdownButton.addEventListener('click', function () {
+        dropdownMenu.classList.toggle('show')M;
+    });
+
+    // Close the dropdown if the user clicks outside of it
+    document.addEventListener('click', function (e) {
+        if (!dropdownButton.contains(e.target) && !dropdownMenu.contains(e.target)) {
+            dropdownMenu.classList.remove('show');
+        }
+    });
+});
+
