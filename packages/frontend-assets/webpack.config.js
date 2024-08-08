04eb64495c4bd0fa90e41bd3bffd9236bbb6b2c7
alex <raikov@ymail.com>
2024-08-08 12:44:54 +0300

    assets

--- packages/frontend-assets/webpack.config.js
+++ packages/frontend-assets/webpack.config.js
@@ -1,22 +1,24 @@
+import RemoveEmptyScriptsPlugin from "webpack-remove-empty-scripts";
 import MiniCssExtractPlugin from "mini-css-extract-plugin";
 import path from "path";
 import { fileURLToPath } from 'url';
 import CopyWebpackPlugin from 'copy-webpack-plugin';
+import {config} from './config.js';
+
+const {entry, output} = config;
 
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const plugins =  [
+    new RemoveEmptyScriptsPlugin(),
     new MiniCssExtractPlugin({
-
-
+        ignoreOrder: true,
         filename: (pathData) => {
-
-
             return '[name].css'
-          },
-        chunkFilename: "[id].css",
+        },
+        // chunkFilename: "[id].css",
     }),
 
 
@@ -30,53 +32,27 @@ const plugins =  [
 
 const module =  {
     rules: [
-        {
-            test: /\.css$/i,
-            use: [
-                "style-loader",
-                "css-loader",
-                {
-                    loader: "postcss-loader",
-                    options: {
-                    postcssOptions: {
-                        plugins: [
-                            [
-                                "postcss-preset-env",
-                            ],
-                        ],
-                    },
-                    },
-                },
-            ],
-        },
+
+
 
         {
             test:  /\.(scss|css)$/,
-            //test: /\.s[ac]ss$/i,
+
             use: [
                 MiniCssExtractPlugin.loader,
               "css-loader",
               "sass-loader",
             ],
         },
-        {
-            test: /\.m?js/,
-            resolve: {
-                fullySpecified: false,
-            },
-        },
+
     ],
 };
 
 export default {
-    entry: {
-        //core: './resources/assets/js/core.js',
-        //admin: './resources/assets/js/admin.js',
-        admincss: './resources/assets/css/admin.scss',
-    },
+    entry,
     output: {
-        path: path.resolve(__dirname, './resources/dist/js'),
-        // filename: '[name].js',
+        path: path.resolve(__dirname, output),
+        filename: '[name].js',
     },
     module,
     plugins,
