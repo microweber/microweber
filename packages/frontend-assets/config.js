8e8ab091748ae15a5259b2505de19e5d7deaebcf
alex <raikov@ymail.com>
2024-08-08 12:45:53 +0300

    config  js

---
+++ packages/frontend-assets/config.js
@@ -0,0 +1,12 @@
+const entry = {
+    core: './resources/assets/js/core.js',
+    admin: './resources/assets/js/admin.js',
+    admincss: './resources/assets/css/admin.scss',
+};
+
+const output = `./resources/dist/js`;
+
+
+export const config = {
+    entry, output
+}
