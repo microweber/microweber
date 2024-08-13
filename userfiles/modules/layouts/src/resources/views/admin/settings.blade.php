e30ea3d0b4a52288e75056dcc2cc19cdcdabe854
Peter Ivanov <peter@microweber.com>
2024-08-13 10:55:39 +0300

    update

--- userfiles/modules/layouts/src/resources/views/admin/settings.blade.php
+++ userfiles/modules/layouts/src/resources/views/admin/settings.blade.php
@@ -1,3 +1,12 @@
-<div>
-    layouts settings
-</div>
+<x-filament::section>
+    <x-slot name="heading">
+        Layout settings
+    </x-slot>
+
+    <div  id="change-background">
+
+        <module type="background/change_layout_background" id="mw-module-skin-settings-module-background"  />
+    </div>
+
+    {{-- Content --}}
+</x-filament::section>
