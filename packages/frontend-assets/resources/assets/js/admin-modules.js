05e0c292b3819f438acc025d9bfd82cb33cf69c1
Peter Ivanov <peter@microweber.com>
2024-08-14 16:06:38 +0300

    update

--- packages/frontend-assets/resources/assets/js/admin-modules.js
+++ packages/frontend-assets/resources/assets/js/admin-modules.js
@@ -2,7 +2,23 @@ import BaseComponent from "../core/base-class.js";
 
 export class AdminModules extends BaseComponent {
 
-    getModuleSettingsUrl(moduleType){
+    getModuleSettingsUrl(moduleType,options = {}) {
+
+        var srcBase = route('live_edit.module_settings');
+
+        if (typeof mw !== 'undefined' && typeof mw.settings !== 'undefined' && typeof mw.settings.liveEditModuleSettingsUrls === 'object' && mw.settings.liveEditModuleSettingsUrls[moduleType]) {
+            if (typeof mw.settings.liveEditModuleSettingsUrls === 'object' && mw.settings.liveEditModuleSettingsUrls[moduleType]) {
+                srcBase = mw.settings.liveEditModuleSettingsUrls[moduleType];
+            }
+        }
+        var src = srcBase;
+        var attrsForSettings = {};
+        if(options && typeof options === 'object' && Object.keys(options).length > 0) {
+            src = srcBase + "?" + json2url(attrsForSettings);
+        }
+
+        return src;
+
 
     }
 }
