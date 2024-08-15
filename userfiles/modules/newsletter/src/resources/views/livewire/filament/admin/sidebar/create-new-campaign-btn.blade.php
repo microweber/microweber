d324bec9ef2a2414ab2c2dc4e3230893cc0fb319
Nikolay <rachkuw@gmail.com>
2024-08-15 11:53:20 +0300

    update

--- userfiles/modules/newsletter/src/resources/views/livewire/filament/admin/sidebar/create-new-campaign-btn.blade.php
+++ userfiles/modules/newsletter/src/resources/views/livewire/filament/admin/sidebar/create-new-campaign-btn.blade.php
@@ -1,11 +1,13 @@
 <div class="p-4 w-full mb-4">
     <a href="{{admin_url('newsletter/create-campaign')}}"
 
-       class="mw-admin-action-links mw-adm-liveedit-tabs flex items-center gap-2"
+       class="mw-admin-create-campaign-button btn-sm btn-primary flex items-center gap-2"
     >
         @svg("mw-add-plus", "h-4 w-4 text-blue-500") Create campaign
     </a>
 </div>
+
+
 
 
 
