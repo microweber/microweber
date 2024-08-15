3e19c349627d65cd7eaf44034ab13b83b1b9a3ce
Nikolay <rachkuw@gmail.com>
2024-08-15 11:18:11 +0300

    update

--- userfiles/modules/newsletter/src/resources/views/livewire/filament/admin/sidebar/create-new-campaign-btn.blade.php
+++ userfiles/modules/newsletter/src/resources/views/livewire/filament/admin/sidebar/create-new-campaign-btn.blade.php
@@ -1,8 +1,11 @@
 <div class="p-4 w-full mb-4">
     <a href="{{admin_url('newsletter/create-campaign')}}"
 
-       class="bg-white hover:shadow rounded-lg border border-gray-500/20 flex items-center gap-2 text-sm w-full py-2 px-4 hover:bg-gray-500/5"
+       class="mw-admin-action-links mw-adm-liveedit-tabs flex items-center gap-2"
     >
-        @svg("heroicon-o-pencil-square", "h-4 w-4 text-blue-500") Create campaign
+        @svg("mw-add-plus", "h-4 w-4 text-blue-500") Create campaign
     </a>
 </div>
+
+
+
