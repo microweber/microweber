08317f3f8e2a13b01da18fd0f31139a01173d892
Peter Ivanov <peter@microweber.com>
2024-09-10 16:13:56 +0300

    update

--- src/MicroweberPackages/LaravelModulesFilament/ModulesPluginFilament.php
+++ src/MicroweberPackages/LaravelModulesFilament/ModulesPluginFilament.php
@@ -11,6 +11,7 @@ use Filament\Panel;
 use Illuminate\Support\ServiceProvider;
 use MicroweberPackages\Filament\Facades\FilamentRegistry;
 use MicroweberPackages\LaravelModules\Repositories\LaravelModulesFileRepository;
+use Nwidart\Modules\Facades\Module;
 
 class ModulesPluginFilament extends ServiceProvider
 {
@@ -23,13 +24,13 @@ class ModulesPluginFilament extends ServiceProvider
             FilamentRegistry::registerPlugin($modulePlugin);
         }
 
-        $pages = $this->getModulePages();
-        if ($pages) {
-            foreach ($pages as $modulePage) {
-
-                FilamentRegistry::registerPage($modulePage, $scope = \MicroweberPackages\Admin\Providers\Filament\FilamentAdminPanelProvider::class);
-            }
-        }
+//        $pages = $this->getModulePages();
+//        if ($pages) {
+//            foreach ($pages as $modulePage) {
+//
+//                FilamentRegistry::registerPage($modulePage, $scope = \MicroweberPackages\Admin\Providers\Filament\FilamentAdminPanelProvider::class);
+//            }
+//        }
 
     }
 
@@ -38,23 +39,51 @@ class ModulesPluginFilament extends ServiceProvider
         if (!config('filament-modules.auto-register-plugins', false)) {
             return [];
         }
-        // get a glob of all Filament plugins
-        $basePath = str(config('modules.paths.modules', 'Modules'));
-        $pattern = $basePath . '/*/app/Filament/*Plugin.php';
-        $pluginPaths = glob($pattern);
-        $filesAll = collect($pluginPaths);
-        $files = $filesAll->map(function ($path) {
-            $mainComposerPathPath = dirname($path, 3);
-            $mainComposerPathPathFile = normalize_path($mainComposerPathPath . '/composer.json', false);
-            $namespace = FilamentModules::convertPathToNamespace($path);
-            $namespace = str_replace('/', '\\', $namespace);
-            if (file_exists($mainComposerPathPathFile)) {
-                LaravelModulesFileRepository::registerNamespacesFromComposer($mainComposerPathPathFile);
-            }
+        $allFiles = [];
+        $enabledModules = Module::allEnabled();
 
-            return $namespace;
-        })->toArray();
-        return $files;
+        if ($enabledModules) {
+            foreach ($enabledModules as $enabledModule) {
+                $path = $enabledModule->getPath();
+                $pluginPath = $path . '/app/Filament/*Plugin.php';
+                $pluginPaths = glob($pluginPath);
+                $filesAll = collect($pluginPaths);
+                $files = $filesAll->map(function ($path) {
+                    $mainComposerPathPath = dirname($path, 3);
+                    $mainComposerPathPathFile = normalize_path($mainComposerPathPath . '/composer.json', false);
+                    $namespace = FilamentModules::convertPathToNamespace($path);
+                    $namespace = str_replace('/', '\\', $namespace);
+                    if (file_exists($mainComposerPathPathFile)) {
+                        LaravelModulesFileRepository::registerNamespacesFromComposer($mainComposerPathPathFile);
+                    }
+
+                    return $namespace;
+                })->toArray();
+
+                $allFiles = array_merge($allFiles, $files);
+            }
+        }
+
+        return $allFiles;
+
+//        // get a glob of all Filament plugins
+//        $basePath = str(config('modules.paths.modules', 'Modules'));
+//        $pattern = $basePath . '/*/app/Filament/*Plugin.php';
+//        $pluginPaths = glob($pattern);
+//        $filesAll = collect($pluginPaths);
+//        $files = $filesAll->map(function ($path) {
+//            $mainComposerPathPath = dirname($path, 3);
+//            $mainComposerPathPathFile = normalize_path($mainComposerPathPath . '/composer.json', false);
+//            $namespace = FilamentModules::convertPathToNamespace($path);
+//            $namespace = str_replace('/', '\\', $namespace);
+//            if (file_exists($mainComposerPathPathFile)) {
+//                LaravelModulesFileRepository::registerNamespacesFromComposer($mainComposerPathPathFile);
+//            }
+//            dump($namespace);
+//
+//            return $namespace;
+//        })->toArray();
+//        return $files;
     }
 
 
