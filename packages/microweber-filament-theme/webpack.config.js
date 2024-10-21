d681984f88dee87a5bd980e4b9090410da6327db
Peter Ivanov <peter@microweber.com>
2024-10-21 16:23:05 +0300

    update

--- packages/microweber-filament-theme/webpack.config.js
+++ packages/microweber-filament-theme/webpack.config.js
@@ -13,12 +13,12 @@ export default {
         path: path.resolve(__dirname, './resources/dist/js'),
         filename: '[name]',
     }
-    // ,
-    // plugins: [
-    //     new CopyWebpackPlugin({
-    //         patterns: [
-    //             { from: path.resolve(__dirname, './resources/dist/'), to: path.resolve(__dirname, '../../public/vendor/microweber-packages/microweber-filament-theme') }
-    //         ]
-    //     })
-    // ]
+    ,
+    plugins: [
+        new CopyWebpackPlugin({
+            patterns: [
+                { from: path.resolve(__dirname, './resources/dist/'), to: path.resolve(__dirname, '../../public/vendor/microweber-packages/microweber-filament-theme') }
+            ]
+        })
+    ]
 }
