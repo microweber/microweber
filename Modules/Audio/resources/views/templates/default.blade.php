898c363d682281cd1126fb3f8247ae34dc6ee4bf
Bozhidar <bobi@microweber.com>
2024-10-22 17:02:05 +0300

    update

--- Modules/Audio/resources/views/templates/default.blade.php
+++ Modules/Audio/resources/views/templates/default.blade.php
@@ -1,27 +1,3 @@
-@php
-$id = "mwaudio-".$params['id'];
-
-$audio = false;
-if (isset($params['data-audio-url'])) {
-    $audio = $params['data-audio-url'];
-}
-$audioUpload = get_module_option('data-audio-upload', $params['id']);
-
-$audioUrl = get_module_option('data-audio-url', $params['id']);
-if ($audioUrl and trim($audioUrl) == '') {
-    $audioUrl = false;
-}
-
-
-if ($audioUpload) {
-    $audio = $audioUpload;
-} else {
-    if ($audioUrl) {
-        $audio = trim($audioUrl);
-    }
-}
-@endphp
-
 <div class="mwembed mw-audio" id="<?php print $id; ?>">
     <?php if ($audio != '') { ?>
     <audio controls src="<?php print $audio; ?>"></audio>
