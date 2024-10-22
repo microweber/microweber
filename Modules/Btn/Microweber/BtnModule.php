3af6e5d1da63f5c7cf376815eb4d8031bdd41cbd
Bozhidar <bobi@microweber.com>
2024-10-22 17:49:40 +0300

    button module migration

---
+++ Modules/Btn/Microweber/BtnModule.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Modules\Btn\Microweber;
+
+use MicroweberPackages\Microweber\Abstract\BaseModule;
+use Modules\Btn\Filament\BtnModuleSettings;
+
+class BtnModule extends BaseModule
+{
+    public static string $name = 'Button';
+    public static string $icon = 'heroicon-o-rectangle-stack';
+    public static string $categories = 'other';
+    public static int $position = 2;
+    public static string $settingsComponent = BtnModuleSettings::class;
+
+    public function render()
+    {
+        $viewData = $this->getViewData();
+
+        return view('modules.btn::templates.default', $viewData);
+
+    }
+
+
+}
