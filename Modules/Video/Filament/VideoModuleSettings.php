dc836f326032019e6bd22e0182c3b44be4dae9da
Bozhidar <bobi@microweber.com>
2024-10-23 16:32:17 +0300

    Update VideoModuleSettings.php

--- Modules/Video/Filament/VideoModuleSettings.php
+++ Modules/Video/Filament/VideoModuleSettings.php
@@ -8,6 +8,7 @@ use Filament\Forms\Components\Select;
 use Filament\Forms\Components\Tabs;
 use Filament\Forms\Components\Textarea;
 use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Toggle;
 use Filament\Forms\Components\ToggleButtons;
 use Filament\Forms\Form;
 use Filament\Forms\Get;
@@ -38,6 +39,49 @@ class VideoModuleSettings extends LiveEditModuleSettings
 
                         Tabs\Tab::make('Settings')
                             ->schema([
+                                TextInput::make('options.width')
+                                    ->label('Width')
+                                    ->live(),
+                                TextInput::make('options.height')
+                                    ->label('Height')
+                                    ->live(),
+
+                                Toggle::make('options.autoplay')
+                                    ->label('Autoplay')
+                                    ->live()
+                                    ->inline()
+                                    ->default('0'),
+
+                                Toggle::make('options.loop')
+                                    ->label('Loop')
+                                    ->live()
+                                    ->inline()
+                                    ->default('0'),
+
+                                Toggle::make('options.muted')
+                                    ->label('Muted')
+                                    ->live()
+                                    ->inline()
+                                    ->default('0'),
+
+                                Toggle::make('options.hide_controls')
+                                    ->label('Hide Controls')
+                                    ->live()
+                                    ->inline()
+                                    ->default('0'),
+
+                                Toggle::make('options.lazy_load')
+                                    ->label('Lazy Load')
+                                    ->live()
+                                    ->inline()
+                                    ->default('0'),
+
+                                MwFileUpload::make('options.thumbnail')
+                                    ->label('Thumbnail')
+                                    ->live(),
+
+
+
                             ]),
                         Tabs\Tab::make('Design')
                             ->schema([
