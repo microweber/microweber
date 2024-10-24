ab76e1c85a31fa8ab098974ccde44984597c4875
Bozhidar <bobi@microweber.com>
2024-10-24 16:18:39 +0300

    Update CaptchaModule.php

--- Modules/Captcha/Microweber/CaptchaModule.php
+++ Modules/Captcha/Microweber/CaptchaModule.php
@@ -16,6 +16,39 @@ class CaptchaModule extends BaseModule
     public function render()
     {
 
+        $viewData = $this->getViewData();
 
+
+        $form_id = uniqid('cap');
+        $captcha_provider = get_option('provider', 'captcha');
+
+        $template = get_option('data-template', $params['id']);
+
+        if (($template == false or ($template == '')) and isset($params['template'])) {
+            $template = $params['template'];
+        }
+
+        $recaptcha_v3_secret_key = get_option('recaptcha_v3_secret_key', 'captcha');
+        $recaptcha_v2_secret_key = get_option('recaptcha_v2_secret_key', 'captcha');
+
+        $viewData['form_id'] = $form_id;
+        $viewData['captcha_provider'] = $captcha_provider;
+        $viewData['template'] = $template;
+        $viewData['recaptcha_v3_secret_key'] = $recaptcha_v3_secret_key;
+        $viewData['recaptcha_v2_secret_key'] = $recaptcha_v2_secret_key;
+
+
+        $moduleTemplate = get_option('template', $this->params['id']);
+        if (empty($moduleTemplate)) {
+            $moduleTemplate = get_option('data-template', $this->params['id']);
+        }
+
+        if ($moduleTemplate == false and isset($this->params['template'])) {
+            $moduleTemplate = $this->params['template'];
+        }
+
+        $viewData['moduleTemplate'] = $moduleTemplate;
+
+        return view('modules.captcha::templates.default', $viewData);
     }
 }
