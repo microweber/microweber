e108d70e6d1ee202e30e2a426298ce34c257be5c
Bozhidar <bobi@microweber.com>
2024-10-24 17:05:40 +0300

    update

--- src/MicroweberPackages/Microweber/Traits/HasMicroweberModuleLiveEditHandleAction.php
+++ src/MicroweberPackages/Microweber/Traits/HasMicroweberModuleLiveEditHandleAction.php
@@ -2,16 +2,32 @@
 
 namespace MicroweberPackages\Microweber\Traits;
 
+
+/**
+ * Trait HasMicroweberModuleLiveEditHandleAction
+ *
+ * Provides functionality to manage live edit actions for modules.
+ */
 trait HasMicroweberModuleLiveEditHandleAction
 {
 
     public array $liveEditActions = [];
 
+    /**
+     * Retrieve the current live edit actions.
+     *
+     * @return array The current live edit actions for the module.
+     */
     public function getliveEditActions()
     {
         return $this->liveEditActions;
     }
 
+    /**
+     * Set the live edit actions for the module.
+     *
+     * @param array $liveEditActions The live edit actions to set for the module.
+     */
     public function setliveEditActions(array $liveEditActions = [])
     {
         $this->liveEditActions = $liveEditActions;
