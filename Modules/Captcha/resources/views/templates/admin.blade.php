d9b0676486f0397d0f065ab5269360a752345092
Bozhidar <bobi@microweber.com>
2024-10-24 16:04:59 +0300

    Update admin.blade.php

--- Modules/Captcha/resources/views/templates/admin.blade.php
+++ Modules/Captcha/resources/views/templates/admin.blade.php
@@ -1,34 +1,32 @@
-<?php
+@php
+    $callback = false;
+    if (isset($params['data-callback'])) {
+        $callback = $params['data-callback'];
+    }
+@endphp
 
-$callback = false;
-if (isset($params['data-callback'])) {
-    $callback = $params['data-callback'];
-}
-?>
-<?php if($callback): ?>
+@if($callback)
     <script type="text/javascript">
         $(document).ready(function () {
-            $('#captcha-<?php print $params['id'] ?>-input').on('input paste keyup blur', function () {
+            $('#captcha-{{ $params['id'] }}-input').on('input paste keyup blur', function () {
                 var val = $(this).val();
                 if (val.length > 0) {
-                    <?php print $callback; ?>(val);
+                    {{ $callback }}(val);
                 }
             })
         });
     </script>
-<?php endif; ?>
-
-
+@endif
 
 <div class="form-group">
     <div class="row">
         <div class="col-auto">
             <a href="javascript:;" class="tip" data-tip="Refresh captcha" data-tipposition="top-center">
-                <img onclick="mw.tools.refresh_image(this);" class="img-fluid" id="captcha-<?php print $form_id; ?>" src="<?php print api_link('captcha') ?>?w=100&h=40&uid=<?php print uniqid($form_id) ?>&rand=<?php print rand(1, 10000) ?>&id=<?php print $params['id'] ?>"/>
+                <img onclick="mw.tools.refresh_image(this);" class="img-fluid" id="captcha-{{ $form_id }}" src="{{ api_link('captcha') }}?w=100&h=40&uid={{ uniqid($form_id) }}&rand={{ rand(1, 10000) }}&id={{ $params['id'] }}"/>
             </a>
         </div>
         <div class="col">
-            <input name="captcha" type="text" required class="mw-captcha-input form-control" placeholder="<?php _e("Security code"); ?>"/>
+            <input name="captcha" type="text" required class="mw-captcha-input form-control" placeholder="@lang('Security code')"/>
         </div>
     </div>
 </div>
