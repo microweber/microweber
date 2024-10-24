9c28c1f9b07d2c2a8c94d61dfcd0592d47352341
Bozhidar <bobi@microweber.com>
2024-10-24 18:07:11 +0300

    update

--- Modules/Btn/Microweber/BtnModule.php
+++ Modules/Btn/Microweber/BtnModule.php
@@ -15,11 +15,61 @@ class BtnModule extends BaseModule
 
     public function render()
     {
-        $viewData = $this->getViewData();
 
-        return view('modules.btn::btn-layout', $viewData);
+        return view('modules.btn::templates.default', $this->templateData());
+    }
 
+    public function templateData()
+    {
+        $templateData = $this->initializeTemplateData();
+        $moduleOptions = $this->getModuleOptions();
 
+        if (!empty($moduleOptions)) {
+            foreach ($moduleOptions as $btnOption) {
+                $templateData[$btnOption['option_key']] = $btnOption['option_value'];
+            }
+        }
+
+        $this->processLinkOptions($templateData);
+
+        return $templateData;
+    }
+
+    private function initializeTemplateData(): array
+    {
+        return [
+            'id' => $this->params['id'],
+            'style' => '',
+            'size' => '',
+            'popupContent' => '',
+            'url' => '',
+            'blank' => '',
+            'text' => '',
+            'icon' => '',
+            'iconPosition' => '',
+            'action' => '',
+            'attributes' => '',
+        ];
+    }
+
+    private function getModuleOptions(): array
+    {
+        return get_module_options($this->params['id']);
+    }
+
+    private function processLinkOptions(array &$templateData): void
+    {
+        if (isset($templateData['link'])) {
+            $btnOptionsLink = json_decode($templateData['link'], true);
+            if (isset($btnOptionsLink['url'])) {
+                $templateData['url'] = $btnOptionsLink['url'];
+            }
+            if (isset($btnOptionsLink['data']['id']) && isset($btnOptionsLink['data']['type']) && $btnOptionsLink['data']['type'] == 'category') {
+                $templateData['url'] = category_link($btnOptionsLink['data']['id']);
+            } elseif (isset($btnOptionsLink['data']['id'])) {
+                $templateData['url'] = content_link($btnOptionsLink['data']['id']);
+            }
+        }
     }
 
 
--- Modules/Btn/resources/views/templates/default.blade.php
+++ Modules/Btn/resources/views/templates/default.blade.php
@@ -11,11 +11,11 @@ description: Default
 @endphp
 
 @if($action == 'submit')
-<button type="submit" id="{{ $btn_id }}" class="btn {{ $style . ' ' . $size }}" {!! $attributes !!}>
+<button type="submit" id="{{ $id }}" class="btn {{ $style . ' ' . $size }}" {!! $attributes !!}>
         {{ $text }}
 </button>
 @else
-<a id="{{ $btn_id }}" href="{{ $url }}" @if ($blank) target="_blank" @endif class="btn {{ $style . ' ' . $size }}" {!! $attributes !!}>
+<a id="{{ $id }}" href="{{ $url }}" @if ($blank) target="_blank" @endif class="btn {{ $style . ' ' . $size }}" {!! $attributes !!}>
         {{ $text }}
 </a>
 @endif
