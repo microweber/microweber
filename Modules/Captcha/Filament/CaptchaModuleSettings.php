01af839f7394eb36b30c251edd7e89db03e8812e
Bozhidar <bobi@microweber.com>
2024-10-24 16:12:43 +0300

    Update CaptchaModuleSettings.php

--- Modules/Captcha/Filament/CaptchaModuleSettings.php
+++ Modules/Captcha/Filament/CaptchaModuleSettings.php
@@ -2,6 +2,7 @@
 
 namespace Modules\Captcha\Filament;
 
+use Filament\Forms\Components\Select;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\Toggle;
 use Filament\Forms\Form;
@@ -15,8 +16,35 @@ class CaptchaModuleSettings extends LiveEditModuleSettings
     {
         return $form
             ->schema([
+                Select::make('options.provider')
+                    ->label(__('Captcha provider'))
+                    ->options([
+                        'microweber' => __('Captcha'),
+                        'google_recaptcha_v2' => __('Google ReCaptcha V2'),
+                        'google_recaptcha_v3' => __('Google ReCaptcha V3'),
+                    ])
+                    ->reactive(),
 
+                TextInput::make('options.recaptcha_v2_site_key')
+                    ->label(__('Google Recaptcha V2 Site Key'))
+                    ->visible(fn ($get) => $get('provider') === 'google_recaptcha_v2'),
 
+                TextInput::make('options.recaptcha_v2_secret_key')
+                    ->label(__('Google ReCaptcha V2 Secret Key'))
+                    ->visible(fn ($get) => $get('provider') === 'google_recaptcha_v2'),
+
+                TextInput::make('options.recaptcha_v3_site_key')
+                    ->label(__('Google Recaptcha V3 Site Key'))
+                    ->visible(fn ($get) => $get('provider') === 'google_recaptcha_v3'),
+
+                TextInput::make('options.recaptcha_v3_secret_key')
+                    ->label(__('Google ReCaptcha V3 Secret Key'))
+                    ->visible(fn ($get) => $get('provider') === 'google_recaptcha_v3'),
+
+                TextInput::make('options.recaptcha_v3_score')
+                    ->label(__('Google ReCaptcha V3 Score'))
+                    ->placeholder('0.5')
+                    ->visible(fn ($get) => $get('provider') === 'google_recaptcha_v3'),
             ]);
     }
 }
