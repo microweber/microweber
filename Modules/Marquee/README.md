c1e50f56c0fe36f4559907c7449aded2b97bb805
Peter Ivanov <peter@microweber.com>
2024-10-30 18:09:05 +0200

    update

--- Modules/Marquee/README.md
+++ Modules/Marquee/README.md
@@ -17,39 +17,3 @@ php artisan module:publish Marquee
 ```
 
 
-
-
-### Use module assets in your .blade.php file
-
-Using vite assets
-```blade
-{{ module_vite('modules/marquee/dist', 'resources/assets/js/app.js') }}
-{{ module_vite('modules/marquee/dist', 'resources/assets/sass/app.scss') }}
-```
-
-
-Using static assets
-```blade
-{{ asset('modules/marquee/img/icon.svg') }}
- ```
-
-### module config values
-```php
-config('modules.marquee.name')
-```
-
-
-
-### Module views
-
-Extend master layout
-
-```php
-@extends('modules.marquee::layouts.master')
-```
-
-Use Module view
-
-```php
-view('modules.marquee::index')
-```
---
+++ Modules/Marquee/Tests/Unit/MarqueeModuleSettingsFilamentTest.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Modules\Marquee\Tests\Unit;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Livewire\Livewire;
+use MicroweberPackages\Option\Models\ModuleOption;
+use Modules\Marquee\Filament\MarqueeModuleSettings;
+use Tests\TestCase;
+
+class MarqueeModuleSettingsFilamentTest extends TestCase
+{
+    public function testMarqueeModuleSettingsForm()
+    {
+        $moduleId = 'module-id-test-' . uniqid();
+        $moduleType = 'marquee';
+
+        ModuleOption::where('option_group', $moduleId)->where('module', $moduleType)->delete();
+        $this->assertDatabaseMissing('options', ['option_group' => $moduleId, 'module' => $moduleType]);
+
+        $params = [
+            'params' => [
+                'id' => $moduleId,
+                'type' => $moduleType
+            ]
+        ];
+
+        Livewire::test(MarqueeModuleSettings::class)
+            ->set($params)
+            ->assertFormFieldExists('options.text')
+            ->assertFormFieldExists('options.fontSize')
+            ->assertFormFieldExists('options.animationSpeed')
+            ->assertFormFieldExists('options.textWeight')
+            ->assertFormFieldExists('options.textStyle')
+            ->assertFormFieldExists('options.textColor');
+
+        $data = [
+            'options.text' => 'Updated marquee text!',
+            'options.fontSize' => '50',
+            'options.animationSpeed' => 'fast',
+            'options.textWeight' => 'bold',
+            'options.textStyle' => 'italic',
+            'options.textColor' => '#FF0000',
+        ];
+
+        Livewire::test(MarqueeModuleSettings::class)
+            ->set($params)
+            ->fillForm($data)
+            ->assertFormSet($data)
+            ->call('save')
+            ->assertHasNoActionErrors()
+            ->assertHasNoFormErrors()
+            ->assertNotified();
+
+        foreach ($data as $key => $value) {
+            $this->assertDatabaseHas('options', ['option_group' => $moduleId, 'module' => $moduleType, 'option_key' => str_replace('options.', '', $key), 'option_value' => $value]);
+        }
+
+        // Clean up
+        ModuleOption::where('option_group', $moduleId)->where('module', $moduleType)->delete();
+        $this->assertDatabaseMissing('options', ['option_group' => $moduleId, 'module' => $moduleType]);
+    }
+}
