6977aaa4abe79e60d63c25eb84dc1f41a74c2b1a
Bozhidar <bobi@microweber.com>
2024-10-30 15:54:52 +0200

    update

---
+++ Modules/ContactForm/Models/Form.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Modules\ContactForm\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Form extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     */
+    protected $fillable = [];
+
+}
---
+++ Modules/ContactForm/database/migrations/2024_10_30_134505_create_forms_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('forms', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->nullable();
+            $table->string('slug')->nullable();
+            $table->integer('list_id')->nullable();
+            $table->longText('description')->nullable();
+            $table->longText('confirmation_message')->nullable();
+            $table->longText('emails_notifications')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('forms');
+    }
+};
