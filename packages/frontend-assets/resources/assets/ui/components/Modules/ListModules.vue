c58212d2a535f5751fb6e22eac83d04b6c3912c9
Nikolay <rachkuw@gmail.com>
2024-11-04 17:02:00 +0200

    Update ListModules.vue

--- packages/frontend-assets/resources/assets/ui/components/Modules/ListModules.vue
+++ packages/frontend-assets/resources/assets/ui/components/Modules/ListModules.vue
@@ -20,7 +20,7 @@
 
 <!--                      <svg fill="none" xmlns="http://www.w3.org/2000/svg" class="icon" width="28" height="28" viewBox="0 0 24 24" stroke-width="3" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><path d="M10 10m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0"></path><path d="M21 21l-6 -6"></path></svg>-->
 <!--                    </span>-->
-
+  
                     <input type="text"
                         autofocus
                            v-model="filterKeyword"
