48f90600a62e79637a51630aaf92a00e51e1bb96
Peter Ivanov <peter@microweber.com>
2024-11-05 15:23:54 +0200

    update

--- public/userfiles/modules/faq/js/default.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {
-    $('.faq-holder.styled .item').on('click', function () {
-        // if ($(this).hasClass('closed')){
-
-        // }
-        $(this).toggleClass('closed');
-    });
-});
