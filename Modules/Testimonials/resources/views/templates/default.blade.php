34cdb6f6ee84ff0ea5aa4b509c75bf2303112bdf
Nikolay <rachkuw@gmail.com>
2024-11-12 16:09:13 +0200

    Update default.blade.php

--- Modules/Testimonials/resources/views/templates/default.blade.php
+++ Modules/Testimonials/resources/views/templates/default.blade.php
@@ -1,20 +1,23 @@
 <div>
 
-    <div id="testimonials-{{ $params['id'] }}">
+    <style>
+       .mw-testimonials-module-default-skin img {
+            width: 100px;
+            height: 100px;
+            border-radius: 50%;
+            object-fit: cover;
+            margin: 0 auto;
+            display: flex;
+        }
+    </style>
+
+    <div class="mw-testimonials-module-default-skin d-flex align-items-center gap-3" id="testimonials-{{ $params['id'] }}">
         @if($testimonials->isEmpty())
             <p>No testimonials available.</p>
         @else
             @foreach($testimonials as $item)
 
-                <div class="card">
-                    <div class="card-header" id="heading-{{ $params['id'] }}-{{ $item->id }}">
-                        @if($item->name)
-                            <h5 class="mb-0">
-                                {{ $item->name }}
-                            </h5>
-                        @endif
-                    </div>
-
+                <div class="card border-0">
                     <div id="collapse-{{ $params['id'] }}-{{ $item->id }}"
                          aria-labelledby="heading-{{ $params['id'] }}-{{ $item->id }}"
                          data-parent="#testimonials-{{ $params['id'] }}">
@@ -23,23 +26,37 @@
                             <img src="{{ $item->client_image }}" class="card-img-top" alt="{{ $item->name }}'s image">
                         @endif
 
-                        <div class="card-body">
 
-                            @if($item->content)
-                                {!! $item->content !!}
-                            @endif
+
+                        <div class="card-body text-center">
+
+                           <div class="mb-5">
+                               <div id="heading-{{ $params['id'] }}-{{ $item->id }}">
+                                   @if($item->name)
+                                       <h5 class="mb-0">
+                                           {{ $item->name }}
+                                       </h5>
+                                   @endif
+                               </div>
+
+                               <div class="my-4">
+                                   @if($item->content)
+                                       {!! $item->content !!}
+                                   @endif
+                               </div>
+
+                           </div>
+
 
                             @if($item->client_company)
-                                <p>{{ $item->client_company }}</p>
+                                <div><span>{{ $item->client_company }}</span></div>
                             @endif
                             @if($item->client_role)
-                                <p>{{ $item->client_role }}</p>
+                                <div><span>{{ $item->client_role }}</span></div>
                             @endif
                             @if($item->client_website)
-                                <p>
-                                    <a href="{{ $item->client_website }}"
-                                       target="_blank">{{ $item->client_website }}</a>
-                                </p>
+                                <div><a href="{{ $item->client_website }}"
+                                   target="_blank">{{ $item->client_website }}</a></div>
                             @endif
 
                         </div>
