3d69b4ed36f15a68c45ab0bce1c9a9ba42745da8
Alex <alexander.raikov@gmail.com>
2024-11-15 14:51:12 +0200

    insert layout

--- packages/frontend-assets/resources/assets/core/modules.js
+++ packages/frontend-assets/resources/assets/core/modules.js
@@ -19,7 +19,32 @@ mw.load_module = function(name, selector, callback, attributes) {
 };
 
 mw.module = {
+    inViewport(el) {
+        if(!el || !el.parentNode) {
+            return false;
+        }
 
+        const doc = el.ownerDocument;
+        const win = doc.defaultView;
+
+
+        const bounding = el.getBoundingClientRect();
+        const elHeight = el.offsetHeight;
+        const elWidth = el.offsetWidth;
+
+
+
+        if (bounding.top >= -elHeight
+            && bounding.left >= -elWidth
+            && bounding.right <= (win.innerWidth || doc.documentElement.clientWidth) + elWidth
+            && bounding.bottom <= (win.innerHeight || doc.documentElement.clientHeight) + elHeight) {
+
+            return true;
+        } else {
+
+            return  false
+         }
+    },
     getData: function (module, options) {
         if(typeof module === 'object') {
             options = module;
@@ -45,8 +70,113 @@ mw.module = {
         }
         return data;
     },
-    insert: function(target, module, config, pos) {
+    insert: function(target, module, config, pos, stateManager, explicitAction) {
 
+
+
+
+        return new Promise(function (resolve) {
+            pos = pos || 'bottom';
+            var action;
+            var id = mw.id('mw-module-'),
+                el = '<div id="' + id + '"></div>';
+
+        if (pos === 'top') {
+            action = 'before';
+            if(mw.tools.hasAnyOfClasses(target, ['allow-drop', 'mw-col'])) {
+                action = 'prepend';
+            }
+        } else if (pos === 'bottom') {
+            action = 'after';
+            if(mw.tools.hasAnyOfClasses(target, ['allow-drop', 'mw-col'])) {
+                action = 'append';
+            }
+        }
+        if(mw.tools.hasAnyOfClasses(target, [ 'mw-col'])) {
+             if(target.firstElementChild.classList.contains('mw-col-container')) {
+                target = target.firstElementChild;
+                if(target.firstElementChild.classList.contains('mw-empty-element')) {
+                    target = target.firstElementChild
+                 }
+             }
+        }
+        //if element is image and has a link
+        if( target.nodeName  == 'IMG' && target.parentNode && target.parentNode.nodeName === 'A'){
+            target = target.parentNode;
+         }
+
+        if(target.classList && target.classList.contains('edit')) {
+            if(pos === 'top' || action === 'before') {
+                action = 'prepend'
+            } else if(pos === 'bottom' || action === 'after') {
+                action = 'append'
+            }
+        }
+
+
+        var parent = mw.$(target).parent().get(0);
+        if(!parent){
+            parent = mw.tools.firstParentOrCurrentWithAnyOfClasses(target, ['edit', 'module']);
+        }
+        if(!parent){
+            parent = target;
+        }
+
+        if(stateManager) {
+            stateManager.record({
+                target: parent,
+                value: parent.innerHTML
+            });
+        }
+
+        if(pos === 'append') {
+            action = 'append'
+        }
+        if(pos === 'prepend') {
+            action = 'prepend'
+        }
+
+
+        if(target.classList.contains('mw-free-layout-container')) {
+            action = 'append'
+        }
+
+
+
+        if(target.nodeName === 'TD' && !explicitAction) {
+            if(action === 'before') {
+                action = 'prepend'
+            } else if(action === 'after') {
+                action = 'append'
+            }
+        }
+
+
+        mw.$(target)[explicitAction || action](el);
+        mw.load_module(module, '#' + id, function (a,b) {
+            if(stateManager) {
+                stateManager.record({
+                    target: parent,
+                    value: parent.innerHTML
+                });
+            }
+
+            if(!mw.module.inViewport(document.querySelector('#' + id))) {
+                mw.tools.scrollTo('#' + id);
+            }
+
+
+
+
+
+            resolve(this);
+        }, config);
+    });
+    },
+    insert_old: function(target, module, config, pos) {
+
+
+        console.log(7772, target, module, config, pos)
 
         return new Promise(function (resolve) {
             pos = pos || 'bottom';
