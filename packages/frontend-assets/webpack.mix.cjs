d3bc30dbb30d2de2ebc130c0b3e7bc9f96f01542
Peter Ivanov <peter@microweber.com>
2024-12-03 10:59:31 +0200

    update

--- packages/frontend-assets/webpack.mix.cjs
+++ packages/frontend-assets/webpack.mix.cjs
@@ -9,11 +9,11 @@ const webpack = require('webpack');
 
 
 mix.webpackConfig({
-    plugins: [
-        new webpack.optimize.LimitChunkCountPlugin({
-            maxChunks:1,
-        }),
-    ],
+    // plugins: [
+    //     new webpack.optimize.LimitChunkCountPlugin({
+    //         maxChunks:1,
+    //     }),
+    // ],
     resolve: {
         modules: [
             path.resolve(__dirname, 'node_modules')
@@ -30,17 +30,17 @@ mix.webpackConfig({
 mix.js('resources/assets/ui/live-edit-app.js', 'resources/dist/build').setPublicPath('resources/dist/build').vue();
 mix.js('resources/assets/ui/apps/ElementStyleEditor/element-style-editor-app.js', 'resources/dist/build').setPublicPath('resources/dist/build').vue();
 mix.sass('resources/assets/ui/apps/ElementStyleEditor/element-style-editor-app.scss', 'resources/dist/build').setPublicPath('resources/dist/build').vue();
-mix.sass('resources/assets/css/scss/liveedit.scss', 'resources/dist/build').setPublicPath('resources/dist/build').vue();
+mix.sass('resources/assets/css/scss/liveedit.scss', 'resources/dist/build').setPublicPath('resources/dist/build');
 mix.css('resources/assets/css/microweber/css/default.css', 'resources/dist/build').setPublicPath('resources/dist/build');
 
 
-mix.js('resources/assets/js/core.js', 'resources/dist/build').setPublicPath('resources/dist/build').vue();
-mix.js('resources/assets/js/admin.js', 'resources/dist/build').setPublicPath('resources/dist/build').vue();
-mix.js('resources/assets/live-edit/live-edit-page-scripts.js', 'resources/dist/build').setPublicPath('resources/dist/build').vue();
-mix.js('resources/assets/js/frontend.js', 'resources/dist/build').setPublicPath('resources/dist/build').vue();
-mix.js('resources/assets/js/admin-filament-libs.js', 'resources/dist/build').setPublicPath('resources/dist/build').vue();
-mix.sass('resources/assets/css/admin.scss', 'resources/dist/build').setPublicPath('resources/dist/build').vue();
-mix.sass('resources/assets/css/install.scss', 'resources/dist/build').setPublicPath('resources/dist/build').vue();
+mix.js('resources/assets/js/core.js', 'resources/dist/build').setPublicPath('resources/dist/build');
+mix.js('resources/assets/js/admin.js', 'resources/dist/build').setPublicPath('resources/dist/build');
+mix.js('resources/assets/live-edit/live-edit-page-scripts.js', 'resources/dist/build').setPublicPath('resources/dist/build');
+mix.js('resources/assets/js/frontend.js', 'resources/dist/build').setPublicPath('resources/dist/build');
+mix.js('resources/assets/js/admin-filament-libs.js', 'resources/dist/build').setPublicPath('resources/dist/build');
+mix.sass('resources/assets/css/admin.scss', 'resources/dist/build').setPublicPath('resources/dist/build');
+mix.sass('resources/assets/css/install.scss', 'resources/dist/build').setPublicPath('resources/dist/build');
 
 
 mix.after(() => {
