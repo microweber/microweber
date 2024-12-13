10472560040f520086d081c87d76b1a8ca03118f
Nikolay <rachkuw@gmail.com>
2024-12-13 12:16:06 +0200

    update

--- Modules/Settings/Filament/Pages/AdminLanguagePage.php
+++ Modules/Settings/Filament/Pages/AdminLanguagePage.php
@@ -14,4 +14,16 @@ class AdminLanguagePage extends AdminSettingsPage
 
     protected static string $description = 'Configure your language settings';
 
+    public static function getNavigation(): array
+    {
+        return [
+            'label' => static::$title,
+            'icon' => 'heroicon-o-language',
+            'url' => static::getUrl(),
+            'group' => 'Settings',
+            'sort' => 1,
+            'iconHtml' => '<i class="mw-language" style="font-size: 30px;"></i>', // Customize font size here
+        ];
+    }
+
 }
