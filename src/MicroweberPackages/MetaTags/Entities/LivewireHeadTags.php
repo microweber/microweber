2c9696bce149079f2c66ff13a1d68b8690e455bd
Peter Ivanov <peter@microweber.com>
2024-12-19 16:20:36 +0200

    update

--- src/MicroweberPackages/MetaTags/Entities/LivewireHeadTags.php
+++ src/MicroweberPackages/MetaTags/Entities/LivewireHeadTags.php
@@ -10,22 +10,26 @@ class LivewireHeadTags implements TagInterface, \Stringable
     public function toHtml(): string
     {
 
-/*
-        $alpineUrl = mw_includes_url() . 'api/libs/alpine/alpine.min.js';
+        /*
+                $alpineUrl = mw_includes_url() . 'api/libs/alpine/alpine.min.js';
 
-        $alpineScript = '<script src="' . $alpineUrl . '" defer></script>';*/
+                $alpineScript = '<script src="' . $alpineUrl . '" defer></script>';*/
 
         $scripts = app()->make(\Livewire\Mechanisms\FrontendAssets\FrontendAssets::class)->scripts();
 
 
-      //  $scripts = \Livewire\Livewire::scripts();
-     //   $styles = \Livewire\Livewire::styles();
+        //  $scripts = \Livewire\Livewire::scripts();
+        //   $styles = \Livewire\Livewire::styles();
         $styles = app()->make(\Livewire\Mechanisms\FrontendAssets\FrontendAssets::class)->styles();
-       // $modal = \Livewire\Livewire::mount('livewire-ui-modal')->html();
+        // $modal = \Livewire\Livewire::mount('livewire-ui-modal')->html();
 
         $append_html = '' . "\r\n";
-        $append_html = '<script id="mw-async-alpine" defer src="https://cdn.jsdelivr.net/npm/async-alpine@2.x.x/dist/async-alpine.script.js"></script>' . "\r\n";
-      //  $append_html .= $alpineScript . "\r\n";
+        // $append_html = '<script id="mw-async-alpine" defer src="https://cdn.jsdelivr.net/npm/async-alpine@2.x.x/dist/async-alpine.script.js"></script>' . "\r\n";
+        $async_alpine_url = public_asset() . 'vendor/microweber-packages/frontend-assets-libs/async-alpine/async-alpine.script.js';
+
+
+        $append_html = '<script id="mw-async-alpine" defer src="' . $async_alpine_url . '"></script>' . "\r\n";
+        //  $append_html .= $alpineScript . "\r\n";
         $append_html .= $scripts . "\r\n";
         $append_html .= $styles . "\r\n";
 
