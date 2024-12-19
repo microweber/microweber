ddc9be8487bef69d8fedf204ba18c4768a27bc3c
Alex <alexander.raikov@gmail.com>
2024-12-19 14:35:35 +0200

    vue lang

--- packages/frontend-assets/resources/assets/ui/components/LeftSidebar/LeftSidebar.vue
+++ packages/frontend-assets/resources/assets/ui/components/LeftSidebar/LeftSidebar.vue
@@ -1,9 +1,11 @@
 <script>
+import Lang from '../i18n/Lang.vue';
+
 
 
 
 export default {
-    components: { },
+    components: { Lang },
     data() {
 
     }
@@ -72,14 +74,14 @@ export default {
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" width="22px" fill="currentColor"><path d="M480-400 40-640l440-240 440 240-440 240Zm0 160L63-467l84-46 333 182 333-182 84 46-417 227Zm0 160L63-307l84-46 333 182 333-182 84 46L480-80Zm0-411 273-149-273-149-273 149 273 149Zm0-149Z"></path></svg>
                     </span>
                     <span class="live-edit-user-bar-nav-item-label">
-                        Layers
+                        <Lang>Layers</Lang>
                     </span>
 
                 </span>
                 <span class="live-edit-user-bar-nav-item">
                     <span class="live-edit-user-bar-nav-item-icon"><svg   xmlns="http://www.w3.org/2000/svg" fill="currentColor"  viewBox="0 96 960 960"><path d="M329.59 801.127h300.82v-50.254H329.59v50.254Zm0-164.871h300.82v-50.255H329.59v50.255Zm-87.025 319.743q-25.788 0-44.176-18.388t-18.388-44.176v-634.87q0-25.788 18.388-44.176t44.176-18.388h337.59l199.844 199.844v497.59q0 25.788-18.388 44.176t-44.176 18.388h-474.87Zm312.462-536.513v-173.23H242.565q-4.616 0-8.462 3.847-3.847 3.846-3.847 8.462v634.87q0 4.616 3.847 8.462 3.846 3.847 8.462 3.847h474.87q4.616 0 8.462-3.847 3.847-3.846 3.847-8.462V419.486H555.027Zm-324.771-173.23v173.23-173.23V905.744 246.256Z"></path></svg></span>
                     <span class="live-edit-user-bar-nav-item-label">
-                        Pages
+                        <Lang>Pages</Lang>
                     </span>
 
                 </span>
---
+++ packages/frontend-assets/resources/assets/ui/components/i18n/Lang.vue
@@ -0,0 +1,25 @@
+
+<script>
+
+ import { useSlots } from 'vue';
+
+export default {
+    components: {   },
+    data() {
+
+    },
+    mounted (){
+
+
+        const slots = useSlots();
+        console.log(this.$slots.default[0] )
+        console.log(slots.default()[0].text )
+    }
+}
+</script>
+
+<template>
+
+    <span class="mw-lang" ref="lang"><slot></slot></span>
+
+</template>
