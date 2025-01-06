2a843f8f07927089babf4eb1461f60eb149dee65
Nikolay <rachkuw@gmail.com>
2025-01-06 16:55:03 +0200

    Create skin-1.blade.php

---
+++ Modules/Product/resources/views/templates/skin-1.blade.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+
+type: layout
+
+name: skin-1
+
+description: skin-1 - 2 Columns
+
+*/
+?>
+<script>
+    mw.require('shop.js');
+</script>
+@include('modules.content::templates.skin-1')
