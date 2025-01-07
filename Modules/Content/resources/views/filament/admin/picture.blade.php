d6c2f49dae4ed67ac6b86d2c66573f2a7b2953ed
Nikolay <rachkuw@gmail.com>
2025-01-07 15:39:20 +0200

    Update picture.blade.php

--- Modules/Content/resources/views/filament/admin/picture.blade.php
+++ Modules/Content/resources/views/filament/admin/picture.blade.php
@@ -1,7 +1,12 @@
 <a href="#">
     @if($content->media()->first())
-
-        <div class="h-12 w-12" style="background-image: url('{{$content->thumbnail(640,480, true)}}')"></div>
+        <div class="h-12 w-12"
+             style="
+             background-image: url('{{$content->thumbnail(640,480, true)}}');
+             background-size: cover;
+             background-position: center center;
+             background-repeat: no-repeat;">
+        </div>
     @else
         @include('modules.content::filament.admin.icon', ['content'=>$content])
     @endif
