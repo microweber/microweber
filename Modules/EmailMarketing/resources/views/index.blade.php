6cf57c37d139d25fd305c37cdd78e24290ccb351
Bozhidar <bobi@microweber.com>
2025-01-08 17:04:54 +0200

    update

---
+++ Modules/EmailMarketing/resources/views/index.blade.php
@@ -0,0 +1,7 @@
+@extends('modules.emailmarketing::layouts.master')
+
+@section('content')
+    <h1>Hello World</h1>
+
+    <p>Module: {!! config('modules.emailmarketing.name') !!}</p>
+@endsection
---
+++ Modules/EmailMarketing/routes/api.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Modules\EmailMarketing\Http\Controllers\EmailMarketingController;
+
+/*
+ *--------------------------------------------------------------------------
+ * API Routes
+ *--------------------------------------------------------------------------
+ *
+ * Here is where you can register API routes for your application. These
+ * routes are loaded by the RouteServiceProvider within a group which
+ * is assigned the "api" middleware group. Enjoy building your API!
+ *
+*/
+
+//Route::middleware(['auth:sanctum'])->prefix('v1')->group(function () {
+//    Route::apiResource('emailmarketing', EmailMarketingController::class)->names('emailmarketing');
+//});
