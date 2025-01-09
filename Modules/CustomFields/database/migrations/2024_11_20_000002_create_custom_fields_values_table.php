12de4c7b812c354597dc205f9dd8b85fc1c6eaba
Bozhidar <bobi@microweber.com>
2025-01-09 18:33:30 +0200

    Update 2024_11_20_000002_create_custom_fields_values_table.php

--- Modules/CustomFields/database/migrations/2024_11_20_000002_create_custom_fields_values_table.php
+++ Modules/CustomFields/database/migrations/2024_11_20_000002_create_custom_fields_values_table.php
@@ -22,7 +22,7 @@ return new class extends Migration {
             $table->integer('position')->nullable();
             $table->timestamps();
 
-            $table->index(['custom_field_id', 'value']);
+//            $table->index(['custom_field_id', 'value']);
         });
     }
 
