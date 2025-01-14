29d2284e6fd0343968f122fd8f895116a24d05f3
Nikolay <rachkuw@gmail.com>
2025-01-14 16:13:46 +0200

    update

--- Modules/Post/resources/views/templates/skin-13.blade.php
+++ Modules/Post/resources/views/templates/skin-13.blade.php
@@ -12,56 +12,46 @@ description: Posts 13
 ?>
 
 <div class="row py-4 blog-posts-13">
-    <?php if (!empty($data)): ?>
-        <?php foreach ($data as $item): ?>
-            <?php $categories = content_categories($item['id']);
-
-            $itemCats = '';
-            if ($categories) {
-                foreach ($categories as $category) {
-                    $itemCats .= '<small class="text-dark bg-body px-2 py-1 font-weight-bold d-inline-block mb-2 me-2" itemprop="category">' . $category['title'] . '</small> ';
+    @if (!empty($data))
+        @foreach ($data as $item)
+            @php
+                $categories = content_categories($item['id']);
+                $itemCats = '';
+                if ($categories) {
+                    foreach ($categories as $category) {
+                        $itemCats .= '<small class="text-dark bg-body px-2 py-1 font-weight-bold d-inline-block mb-2 me-2" itemprop="category">' . $category['title'] . '</small> ';
+                    }
                 }
-            }
-            ?>
+            @endphp
 
-            <div class="mx-auto col-sm-10 mx-md-0 col-md-6 col-lg-4 mb-7" itemscope itemtype="<?php print $schema_org_item_type_tag ?>">
-                <div class=" overflow-hidden h-100 d-flex flex-column bg-body hover-  ">
+            <div class="mx-auto col-sm-10 mx-md-0 col-md-6 col-lg-4 mb-7" itemscope itemtype="{{ $schema_org_item_type_tag }}">
+                <div class="overflow-hidden h-100 d-flex flex-column bg-body hover-">
                     <div class="d-flex flex-column h-100 pt-3 pb-2">
-                        <?php if (!isset($show_fields) or $show_fields == false or in_array('created_at', $show_fields)): ?>
-                            <small class="mb-3 mt-3 d-block" itemprop="dateCreated"><?php echo date_system_format($item['created_at']); ?></small>
-                        <?php endif; ?>
+                        @if (!isset($show_fields) or $show_fields == false or in_array('created_at', $show_fields))
+                            <small class="mb-3 mt-3 d-block" itemprop="dateCreated">{{ date_system_format($item['created_at']) }}</small>
+                        @endif
 
-                        <?php if (!isset($show_fields) or $show_fields == false or in_array('title', $show_fields)): ?>
-                            <a href="<?php print $item['link'] ?>" class="text-dark text-decoration-none"><h4 class="mb-2" itemprop="name"><?php print $item['title'] ?></h4></a>
-                        <?php endif; ?>
+                        @if (!isset($show_fields) or $show_fields == false or in_array('title', $show_fields))
+                            <a href="{{ $item['link'] }}" class="text-dark text-decoration-none"><h4 class="mb-2" itemprop="name">{{ $item['title'] }}</h4></a>
+                        @endif
 
-                        <?php if (!isset($show_fields) or $show_fields == false or in_array('description', $show_fields)): ?>
-                            <p itemprop="description"><?php print $item['description'] ?></p>
-                        <?php endif; ?>
-
-<!--                        <div class="d-flex align-items-center">-->
-<!--                            <div class="d-flex align-items-center me-2"><i class="mw-micon-Eye-2 icon-size-24px text-muted me-2"></i>-->
-<!--                                <module type="site_stats/view_count" content-id="10"/>-->
-<!--                            </div>-->
-<!--                            <div class="d-flex align-items-center me-2"><i class="mdi mdi-forum icon-size-24px text-muted me-2"></i>-->
-<!--                                <module type="comments/comments_count" content-id="--><?php //echo $item['id']; ?><!--" class="d-inline"/>-->
-<!--                            </div>-->
-<!--                        </div>-->
+                        @if (!isset($show_fields) or $show_fields == false or in_array('description', $show_fields))
+                            <p itemprop="description">{{ $item['description'] }}</p>
+                        @endif
                     </div>
-                    <?php if (!isset($show_fields) or $show_fields == false or in_array('thumbnail', $show_fields)): ?>
-                        <a href="<?php print $item['link'] ?>" class="d-block position-relative" itemprop="url">
+                    @if (!isset($show_fields) or $show_fields == false or in_array('thumbnail', $show_fields))
+                        <a href="{{ $item['link'] }}" class="d-block position-relative" itemprop="url">
                             <div class="img-as-background square-75">
-                                <img loading="lazy" src="<?php print $item['image']; ?>" itemprop="image" />
+                                <img loading="lazy" src="{{ $item['image'] }}" itemprop="image" />
                             </div>
                         </a>
-                    <?php endif; ?>
-
+                    @endif
                 </div>
             </div>
-        <?php endforeach; ?>
-    <?php endif; ?>
+        @endforeach
+    @endif
 </div>
 
-<?php if (isset($pages_count) and $pages_count > 1 and isset($paging_param)): ?>
-    <module type="pagination" pages_count="<?php echo $pages_count; ?>" paging_param="<?php echo $paging_param; ?>"/>
-<?php endif; ?>
+@if (isset($pages_count) and $pages_count > 1 and isset($paging_param))
+    <module type="pagination" pages_count="{{ $pages_count }}" paging_param="{{ $paging_param }}"/>
+@endif
--- Modules/Post/resources/views/templates/skin-15.blade.php
+++ Modules/Post/resources/views/templates/skin-15.blade.php
@@ -12,45 +12,42 @@ description: Posts 15
 ?>
 
 <div class="row blog-posts-3">
-    <?php if (!empty($data)): ?>
-        <?php foreach ($data as $item): ?>
-            <?php $categories = content_categories($item['id']);
-
-            $itemCats = '';
-            if ($categories) {
-                foreach ($categories as $category) {
-                    $itemCats .= '<small class="text-dark font-weight-bold d-inline-block mb-2" itemprop="category">' . $category['title'] . '</small> ';
+    @if (!empty($data))
+        @foreach ($data as $item)
+            @php
+                $categories = content_categories($item['id']);
+                $itemCats = '';
+                if ($categories) {
+                    foreach ($categories as $category) {
+                        $itemCats .= '<small class="text-dark font-weight-bold d-inline-block mb-2" itemprop="category">' . $category['title'] . '</small> ';
+                    }
                 }
-            }
-            ?>
-            <div class="mx-auto mx-md-0 col-12 col-xl-4 mb-5" itemscope itemtype="<?php print $schema_org_item_type_tag ?>">
+            @endphp
+            <div class="mx-auto mx-md-0 col-12 col-xl-4 mb-5" itemscope itemtype="{{ $schema_org_item_type_tag }}">
                 <div class="h-100 d-flex flex-column">
-                    <?php if (!isset($show_fields) or $show_fields == false or in_array('thumbnail', $show_fields)): ?>
-                    <a href="<?php print $item['link'] ?>" class="d-block px-md-0 px-2" itemprop="url">
-                        <div class="img-as-background h-600">
-                            <img loading="lazy" src="<?php print $item['image']; ?>" style="top: unset!important; position: relative !important;" itemprop="image"/>
-                        </div>
-                    </a>
-                    <?php endif; ?>
-
+                    @if (!isset($show_fields) or $show_fields == false or in_array('thumbnail', $show_fields))
+                        <a href="{{ $item['link'] }}" class="d-block px-md-0 px-2" itemprop="url">
+                            <div class="img-as-background h-600">
+                                <img loading="lazy" src="{{ $item['image'] }}" style="top: unset!important; position: relative !important;" itemprop="image"/>
+                            </div>
+                        </a>
+                    @endif
 
                     <div class="pt-4 pb-3">
-                        <?php if (!isset($show_fields) or $show_fields == false or in_array('created_at', $show_fields)): ?>
-                            <small class="mb-3 d-block" style="color: #FF7A01;" itemprop="dateCreated"><?php echo date_system_format($item['created_at']); ?></small>
-                        <?php endif; ?>
+                        @if (!isset($show_fields) or $show_fields == false or in_array('created_at', $show_fields))
+                            <small class="mb-3 d-block" style="color: #FF7A01;" itemprop="dateCreated">{{ date_system_format($item['created_at']) }}</small>
+                        @endif
 
-
-                        <?php if (!isset($show_fields) or $show_fields == false or in_array('title', $show_fields)): ?>
-                            <a href="<?php print $item['link'] ?>" class="px-md-0 px-2" itemprop="url"><h4 class="text-start text-left" itemprop="name"><?php print $item['title'] ?></h4></a>
-                        <?php endif; ?>
-
+                        @if (!isset($show_fields) or $show_fields == false or in_array('title', $show_fields))
+                            <a href="{{ $item['link'] }}" class="px-md-0 px-2" itemprop="url"><h4 class="text-start text-left" itemprop="name">{{ $item['title'] }}</h4></a>
+                        @endif
                     </div>
                 </div>
             </div>
-        <?php endforeach; ?>
-    <?php endif; ?>
+        @endforeach
+    @endif
 </div>
 
-<?php if (isset($pages_count) and $pages_count > 1 and isset($paging_param)): ?>
-    <module type="pagination" pages_count="<?php echo $pages_count; ?>" paging_param="<?php echo $paging_param; ?>"/>
-<?php endif; ?>
+@if (isset($pages_count) and $pages_count > 1 and isset($paging_param))
+    <module type="pagination" pages_count="{{ $pages_count }}" paging_param="{{ $paging_param }}"/>
+@endif
