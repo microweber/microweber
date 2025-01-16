03befcb099c49d1e83fe5100391f16edc649873e
Nikolay <rachkuw@gmail.com>
2025-01-16 16:23:01 +0200

    Update default.blade.php

--- Modules/Tabs/resources/views/templates/default.blade.php
+++ Modules/Tabs/resources/views/templates/default.blade.php
@@ -19,7 +19,7 @@ description: Default
             <p>No tab items available.</p>
         @else
             @foreach($tabs as $item)
-                <div class="card">
+                <div class="card mb-2">
                     <div class="card-header" id="heading-{{ $params['id'] }}-{{ $item->id }}">
                         <h5 class="mb-0">
                             <button class="btn btn-link" data-bs-toggle="collapse" data-bs-target="#collapse-{{ $params['id'] }}-{{ $item->id }}" aria-expanded="true" aria-controls="collapse-{{ $params['id'] }}-{{ $item->id }}">
@@ -28,7 +28,6 @@ description: Default
                         </h5>
                     </div>
 
-                    @dump($item);
                     <div id="collapse-{{ $params['id'] }}-{{ $item->id }}" class="collapse" aria-labelledby="heading-{{ $params['id'] }}-{{ $item->id }}" data-parent="#tabs-{{ $params['id'] }}">
                         <div class="card-body">
                             {!! $item->content !!}
