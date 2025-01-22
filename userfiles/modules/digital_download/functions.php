c09bede8bf47ef9b1e33f74e2f204eab5fb124f3
Bozhidar <bobi@microweber.com>
2025-01-22 13:03:31 +0200

    Update functions.php

--- userfiles/modules/digital_download/functions.php
+++ userfiles/modules/digital_download/functions.php
@@ -11,11 +11,31 @@ function digital_download_get($params) {
 
     $email = $params['email'];
     if ($email) {
-        
+
+        $optionGroup = 'digital_download';
+
+        save_option(array(
+            'option_group' => $optionGroup,
+            'option_key' => 'disable_captcha',
+            'option_value' => 'y'
+        ));
+
+        $save = mw()->forms_manager->post([
+            'for_id' => $optionGroup,
+            'for' => 'digital_download_'.'_'.$params['id'],
+            'message' => 'Link is downloaded from ' . $email,
+            'email' => $email,
+            'module_name' => 'digital_download',
+        ]);
+
+        if ($save) {
+            return [
+                'success' => 'Thank you for downloading the file. Check your email for the download link.',
+                'download_url' => $download_url
+            ];
+        }
     }
 
-    return [
-        'download_url' => $download_url
-    ];
+    return [];
 
 }
