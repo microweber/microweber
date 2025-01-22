f260e7f7a8b5f0a3b101efd1323c766110dd9ea7
Bozhidar <bobi@microweber.com>
2025-01-22 12:10:47 +0200

    update

---
+++ userfiles/modules/digital_download/templates/default.php
@@ -0,0 +1,11 @@
+<div
+    style="
+    display:flex;
+    justify-content:<?php print $button_alignment; ?>;
+"
+>
+    <a class="btn btn-default" data-bs-toggle="modal" href="#digital-download-<?php print $params['id']; ?>"
+       data-backdrop="false">
+        <?php echo $button_text; ?>
+    </a>
+</div>
