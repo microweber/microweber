3e67e66d0a599f4b08a6d620ee8c8deb1e61783f
Bozhidar <bobi@microweber.com>
2025-01-22 12:08:31 +0200

    update

---
+++ userfiles/modules/digital_download/config.php
@@ -0,0 +1,12 @@
+<?php
+
+$config = array();
+$config['name'] = "Digital Download";
+$config['author'] = "Microweber";
+$config['no_cache'] = false;
+$config['ui'] = true;
+$config['categories'] = "miscellaneous";
+$config['position'] = 38;
+$config['version'] = "0.6";
+
+
