7f55cbaf9924e9a5016b0541af0cece1c3420260
Peter Ivanov <peter@microweber.com>
2025-01-23 11:46:44 +0200

    update

--- src/MicroweberPackages/Fortify/database/migrations/2023_10_12_100000_create_sessions_table.php
+++ src/MicroweberPackages/Fortify/database/migrations/2023_10_12_100000_create_sessions_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      *
@@ -13,9 +12,9 @@ return new class extends Migration
      */
     public function up()
     {
-        if(!Schema::hasTable('sessions')) {
+        if (!Schema::hasTable('sessions')) {
             Schema::create('sessions', function (Blueprint $table) {
-                $table->string('id',2500)->unique();
+                $table->string('id', 500)->unique();
                 $table->integer('user_id')->unsigned();
                 $table->string('ip_address', 45)->nullable();
                 $table->text('user_agent')->nullable();
