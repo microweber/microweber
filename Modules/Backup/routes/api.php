c95789171fc2900f2a027a3d8d102d53953bd768
Bozhidar Slaveykov <bobi@microweber.com>
2025-02-03 14:52:50 +0200

    update

--- Modules/Backup/routes/api.php
+++ Modules/Backup/routes/api.php
@@ -15,15 +15,7 @@ Route::prefix(admin_url() . '/api/backup')
     ->middleware(['admin'])
     ->group(function () {
 
-        Route::get('restore', BackupController::class . '@restore')->name('admin.backup.restore');
-        Route::get('start', BackupController::class . '@start')->name('admin.backup.start');
-        Route::get('download', BackupController::class . '@download')->name('admin.backup.download');
-        Route::get('generate-session-id', BackupController::class . '@generateSessionId')->name('admin.backup.generate-session-id');
 
-        Route::post('upload', BackupController::class . '@upload')->name('admin.backup.upload');
-        Route::post('delete', BackupController::class . '@delete')->name('admin.backup.delete');
 
-        Route::post('/language/export', LanguageController::class . '@export')->name('admin.backup.language.export');
-        Route::post('/language/upload', LanguageController::class . '@upload')->name('admin.backup.language.upload');
 
     });
