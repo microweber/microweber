abd9cbb3b69eeac1b8140a8874317deaaae402c1
Bozhidar Slaveykov <bobi@microweber.com>
2025-02-03 15:57:15 +0200

    Update CreateBackup.php

--- Modules/Backup/Filament/Pages/CreateBackup.php
+++ Modules/Backup/Filament/Pages/CreateBackup.php
@@ -2,6 +2,8 @@
 
 namespace Modules\Backup\Filament\Pages;
 
+use Filament\Forms\Components\Wizard;
+use Filament\Forms\Form;
 use Filament\Pages\Page;
 
 class CreateBackup extends Page
@@ -10,5 +12,28 @@ class CreateBackup extends Page
 
     protected static string $view = 'modules.backup::filament.pages.create-backup';
 
+
+    public function form(Form $form): Form
+    {
+
+        return $form
+            ->schema([
+                Wizard::make([
+                    Wizard\Step::make('Order')
+                        ->schema([
+                            // ...
+                        ]),
+                    Wizard\Step::make('Delivery')
+                        ->schema([
+                            // ...
+                        ]),
+                    Wizard\Step::make('Billing')
+                        ->schema([
+                            // ...
+                        ]),
+                ])
+            ]);
+
+    }
 
 }
