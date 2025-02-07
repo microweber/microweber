a945a5a1701ec246792fc3aae959e583056a10f7
Nikolay <rachkuw@gmail.com>
2025-02-07 16:43:26 +0200

    Update email.blade.php

--- Modules/CustomFields/resources/views/templates/bootstrap5_flex/email.blade.php
+++ Modules/CustomFields/resources/views/templates/bootstrap5_flex/email.blade.php
@@ -1,5 +1,5 @@
 <div class="col-sm-{{ $settings['field_size_mobile'] }} col-md-{{ $settings['field_size_tablet'] }} col-lg-{{ $settings['field_size_desktop'] }}">
-    <div mb-3 d-flex gap-3 flex-wrap>
+    <div class="mb-3 d-flex gap-3 flex-wrap">
 
         @if($settings['show_label'])
         <label class="form-label me-2 align-self-center mb-0 col-xl-4 col-auto">
