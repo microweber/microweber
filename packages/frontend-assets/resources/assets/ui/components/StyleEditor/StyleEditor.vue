fccc737564371a99ebbd54f5e1a6b45bb2abf637
Nikolay <rachkuw@gmail.com>
2025-02-07 11:00:21 +0200

    Update StyleEditor.vue

--- packages/frontend-assets/resources/assets/ui/components/StyleEditor/StyleEditor.vue
+++ packages/frontend-assets/resources/assets/ui/components/StyleEditor/StyleEditor.vue
@@ -2,7 +2,7 @@
     <div v-if="isOpened">
         <div class="d-flex align-items-center justify-content-between mb-3">
             <h3 class="fs-2 font-weight-bold">Element Style Editor</h3>
-            <span v-on:click="closeStyleEditor" class="x-close-modal-link" style="top: 30px;">
+            <span v-on:click="closeStyleEditor" class="x-close-modal-link" style="top: 30px; right: 25px;">
                 <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="currentColor"><path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"/></svg>
 
             </span>
