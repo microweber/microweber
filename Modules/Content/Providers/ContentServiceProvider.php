08e6fa952ccb97914745c07debe01d3d76fc193a
Peter Ivanov <peter@microweber.com>
2025-02-20 14:50:40 +0200

    update

--- Modules/Content/Providers/ContentServiceProvider.php
+++ Modules/Content/Providers/ContentServiceProvider.php
@@ -68,5 +68,24 @@ class ContentServiceProvider extends BaseModuleServiceProvider
         Microweber::module(ContentModule::class);
 
 
+
+
+        event_bind('mw.front', function ($params = false) {
+            template_foot(function () {
+
+                $contentId = content_id();
+                if ($contentId) {
+                    $graph = new \Spatie\SchemaOrg\Graph();
+                    $graphFilled = getSchemaOrgContentFilled($graph, $contentId);
+
+                    if ($graphFilled) {
+                        return $graphFilled->toScript();
+                    }
+                }
+
+            });
+        });
+
+
     }
 }
