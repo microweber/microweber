1625fb102b8e84e7b7f11ec0fe5aca0811768da6
Nikolay <rachkuw@gmail.com>
2025-02-21 10:52:39 +0200

    Update tags.js

--- packages/frontend-assets/resources/assets/components/tags.js
+++ packages/frontend-assets/resources/assets/components/tags.js
@@ -278,7 +278,7 @@ mw.tags = mw.chips = function(options){
             };
 
             tag_close.className = config.tagBtnClass + ' btn-icon';
-            tag_close.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" fill="#D63939" class="icon icon-tabler icon-tabler-trash" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+            tag_close.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-trash" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
             <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
             <path d="M4 7l16 0"></path>
             <path d="M10 11l0 6"></path>
