5a6ec8188b8f477f323c0376508ca5bdb8f788ee
Peter Ivanov <peter@microweber.com>
2025-02-24 18:08:43 +0200

    update

--- src/MicroweberPackages/Admin/Http/Livewire/FilterItemDateRange.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace MicroweberPackages\Admin\Http\Livewire;
-
-class FilterItemDateRange extends FilterItemValue
-{
-    public $name = 'Date with range';
-    public string $view = 'admin::livewire.filters.filter-item-date-range';
-}
--- src/MicroweberPackages/Admin/Http/Livewire/FilterItemValueRange.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace MicroweberPackages\Admin\Http\Livewire;
-
-class FilterItemValueRange extends FilterItemValue
-{
-    public $name = 'Value with range';
-    public string $view = 'admin::livewire.filters.filter-item-value-range';
-}
