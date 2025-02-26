6e0f4fb98229a881e62ee40cdca9e70e1cecb2eb
Bozhidar Slaveykov <bobi@microweber.com>
2025-02-26 14:05:24 +0200

    Update ListContents.php

--- Modules/Content/Filament/Admin/ContentResource/Pages/ListContents.php
+++ Modules/Content/Filament/Admin/ContentResource/Pages/ListContents.php
@@ -22,13 +22,21 @@ class ListContents extends ListRecords
 
     protected function getHeaderActions(): array
     {
-        return [
+
+        $actions = [];
+        $actions[] =
             Actions\CreateAction::make()
                 ->size('xl')
                 ->icon('heroicon-o-plus')
-                ->color('mw-secondary')
-          //  Actions\LocaleSwitcher::make(),
-        ];
+                ->color('mw-secondary');
+
+
+        $multilanguageIsEnabled = true; // TODO
+        if ($multilanguageIsEnabled) {
+            $actions[] =  Actions\LocaleSwitcher::make();
+        }
+
+        return $actions;
     }
 
 
