dc8b8bd961281d44ee5c3dbef016542547429c66
Nikolay <rachkuw@gmail.com>
2025-02-27 12:26:59 +0200

    Update index.blade.php

--- Modules/Search/resources/views/livewire/search/index.blade.php
+++ Modules/Search/resources/views/livewire/search/index.blade.php
@@ -1,9 +1,9 @@
 <div class="module-search-livewire">
     <div class="search-form mb-4">
         <div class="input-group position-relative">
-            <div class="position-absolute d-flex justify-content-end align-items-center ps-2 pointer-events-none z-10" @style(['top: 10px'])>
+            <div class="position-absolute d-flex justify-content-end align-items-center ps-2 pointer-events-none" @style(['top: 10px; z-index: 20'])>
                 <svg width="16px" height="16px" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"></path>
+                    <path stroke="#5f6368" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"></path>
                 </svg>
             </div>
 
