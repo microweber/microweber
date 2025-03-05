8c114f50ac3ef93a46b2ee2d9c6bfdac0d969de7
Nikolay <rachkuw@gmail.com>
2025-03-05 11:21:33 +0200

    uptade

--- Modules/Elements/resources/views/templates/empty/default.blade.php
+++ Modules/Elements/resources/views/templates/empty/default.blade.php
@@ -1,3 +1,25 @@
+<style>
+    .mw-empty-element {
+        background: #FFFFB9;
+        color: #0088CC;
+        position: relative;
+        min-height: 100px !important;
+        display: block;
+        text-align: left;
+    }
+
+    .mw-empty-element:after {
+        content: 'Enter text or drop element';
+        position: relative;
+        top: 10px;
+        left: 14px;
+        color: #1111114a;
+        font-weight: bold;
+    }
+
+</style>
+
 <div class="element mw-empty-element"></div>
+
 
 
