50e791eb4a41183df41ef23b7ea3c3147fbeb84c
Nikolay <rachkuw@gmail.com>
2025-03-10 16:21:36 +0200

    update

--- src/MicroweberPackages/LiveEdit/resources/views/unlock-package/unlock-package-modal.blade.php
+++ src/MicroweberPackages/LiveEdit/resources/views/unlock-package/unlock-package-modal.blade.php
@@ -19,11 +19,7 @@ $packageName = $params['package_name'] ?? 'microweber-modules/white_label';
 
 @endphp
 
-
-
-<div>
-
-
+<div class="mh-100vh unlock-package-wrapper row">
     <script>
         $(document).ready(function() {
             document.getElementById('js-unlock-package-save-license-<?php echo $params['id'];?>').addEventListener('click', function() {
@@ -43,51 +39,51 @@ $packageName = $params['package_name'] ?? 'microweber-modules/white_label';
         });
     </script>
 
-    <div class="row">
-        <div class="col-md-8 mt-4">
+    <div class="col-md-8 unlock-package-columns">
+       <div class="mb-3">
+           <h1 class="font-weight-bold">Unlock The power of <span class="text-orange">BIG Template</span></h1>
+           <h4>Use all layouts to make an awesome websites!</h4>
+           <h4>Buy license key "Big Template" and unlock 300 more layouts,</h4>
+           <h4>only for $59 per year or $169 lifetime license</h4>
 
-            <h1>Unlock the power of Microweber</h1>
-            <h4>Get all premium features of Microweber and make awesome websites!</h4>
+            <a class="btn btn-primary get-license-btn" target="_blank" href="https://microweber.org/go/market?prefix={{ $packageName }}">Get license key</a>
+       </div>
 
-            <div>
-                <small class="text-muted d-block mb-2">
-                    Don't have a license key? You can get it from here:
-                    <a class="btn btn-link" target="_blank" href="https://microweber.org/go/market?prefix={{ $packageName }}">Get license key</a>
-                </small>
+        <form class="d-flex align-items-center">
+            <input type="text" id="js-unlock-package-license-key-<?php echo $params['id'];?>" name="local_key" autocomplete="on" autofocus="true" class="form-control my-3" placeholder="License key">
+            <div class="ms-md-3">
+                <button id="js-unlock-package-save-license-<?php echo $params['id'];?>" type="button" class="btn btn-outline-primary unlock-big-template-btn">Unlock</button>
             </div>
+        </form>
 
-            <div>
-                <form class="d-flex align-items-center">
-                    <input type="text" id="js-unlock-package-license-key-<?php echo $params['id'];?>" name="local_key" autocomplete="on" autofocus="true" class="form-control my-3" placeholder="License key">
-                    <div class="ms-md-3">
-                        <button id="js-unlock-package-save-license-<?php echo $params['id'];?>" type="button" class="btn btn-success">Unlock</button>
-                    </div>
-                </form>
+        <p class="font-weight-bold">Have a problem with your White Label license key?
+            <a class="text-orange" href="javascript:;" onmousedown="mw.contactForm();">Contact us.</a>
+        </p>
+    </div>
+
+    <div class="col-md-4 px-0 unlock-package-columns pt-0" style="background-color: #f5f5f5;">
+        <div class="unlock-package-right-side-img" style="background-image: url('<?php print modules_url(); ?>unlock-package/assets/img/right-banner.jpg')">
+
+{{--            {{ asset('vendor/microweber-packages/liveEdit') }}--}}
+
+            <div class="ps-5" style="padding-top: 200px;">
+                <h1 class="d-flex gap-2">
+                    <span class="font-weight-bold">350+</span>
+                    <span class="font-weight-normal">Layouts</span>
+                </h1>
+                <h2 class="d-flex gap-2">
+                    <span class="font-weight-bold">20</span>
+                    <span class="font-weight-normal">Categories</span>
+                </h2>
+                <h3 class="d-flex gap-2">
+                    <span class="font-weight-bold">75</span>
+                    <span class="font-weight-normal">Modules</span>
+                </h3>
+                <h4>Fresh Updates</h4>
+                <h4>Live Support</h4>
+                <h4>Theme & Layouts Customization</h4>
+                <h4> And Much More ...</h4>
             </div>
-            <div>
-                <small>Have a problem with your license key?
-                    <a href="javascript:;" onmousedown="mw.contactForm();">Contact us.</a>
-                </small>
-            </div>
-
-        </div>
-        <div class="col-md-4">
-
-            <h4>Include with the premium license</h4>
-            <ul class="list-unstyled space-y-1">
-                <li><svg xmlns="http://www.w3.org/2000/svg" class="icon text-green" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><path d="M5 12l5 5l10 -10"></path></svg> +30 Headers</li>
-                <li><svg xmlns="http://www.w3.org/2000/svg" class="icon text-green" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><path d="M5 12l5 5l10 -10"></path></svg> +30 Galleries</li>
-                <li><svg xmlns="http://www.w3.org/2000/svg" class="icon text-green" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><path d="M5 12l5 5l10 -10"></path></svg> +30 Call to actions</li>
-                <li><svg xmlns="http://www.w3.org/2000/svg" class="icon text-green" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><path d="M5 12l5 5l10 -10"></path></svg> +15 Contact forms</li>
-                <li><svg xmlns="http://www.w3.org/2000/svg" class="icon text-green" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><path d="M5 12l5 5l10 -10"></path></svg> +10 Footers</li>
-                <li><svg xmlns="http://www.w3.org/2000/svg" class="icon text-green" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><path d="M5 12l5 5l10 -10"></path></svg> +10 Blogs</li>
-                <li><svg xmlns="http://www.w3.org/2000/svg" class="icon text-green" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><path d="M5 12l5 5l10 -10"></path></svg> +10 Portfolios</li>
-                <li><svg xmlns="http://www.w3.org/2000/svg" class="icon text-green" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><path d="M5 12l5 5l10 -10"></path></svg> +10 Team members</li>
-                <li><svg xmlns="http://www.w3.org/2000/svg" class="icon text-green" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><path d="M5 12l5 5l10 -10"></path></svg> +10 Testimonials</li>
-                <li><svg xmlns="http://www.w3.org/2000/svg" class="icon text-green" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><path d="M5 12l5 5l10 -10"></path></svg> +10 Pricing tables</li>
-                <li><svg xmlns="http://www.w3.org/2000/svg" class="icon text-green" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><path d="M5 12l5 5l10 -10"></path></svg> +10 Services</li>
-                <li><svg xmlns="http://www.w3.org/2000/svg" class="icon text-green" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><path d="M5 12l5 5l10 -10"></path></svg> +10 Features</li>
-            </ul>
         </div>
     </div>
 </div>
