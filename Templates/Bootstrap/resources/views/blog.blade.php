45db247b4953b9eee39afc87f2287bc2a4c1a22a
Nikolay <rachkuw@gmail.com>
2025-03-10 12:20:11 +0200

    Update blog.blade.php

--- Templates/Bootstrap/resources/views/blog.blade.php
+++ Templates/Bootstrap/resources/views/blog.blade.php
@@ -15,8 +15,11 @@ description: Blog
 @extends('templates.bootstrap::layouts.master')
 
 @section('content')
+    
 
     <div class="edit main-content" data-layout-container rel="content" field="content">
+        <module type="layouts" template="content/skin-6"/>
+
         <module type="layouts" template="blog/skin-3"/>
 
     </div>
