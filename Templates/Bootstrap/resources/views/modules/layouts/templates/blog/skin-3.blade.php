2f974888a175d803f16011cc04d8a90b4f6efa9b
Nikolay <rachkuw@gmail.com>
2025-03-10 12:11:34 +0200

    Update skin-3.blade.php

--- Templates/Bootstrap/resources/views/modules/layouts/templates/blog/skin-3.blade.php
+++ Templates/Bootstrap/resources/views/modules/layouts/templates/blog/skin-3.blade.php
@@ -24,14 +24,20 @@ if (!$classes['padding_bottom']) {
 
 $layout_classes = $layout_classes ?? ''; $layout_classes .= ' ' . $classes['padding_top'] . ' ' . $classes['padding_bottom'] . ' ';
 ?>
+<module type="spacer" id="spacer-layout--{{ $params['id'] ?? '' }}-top" />
 
 
 <section class="section <?php print $layout_classes; ?> edit" field="layout-blog-skin-3-{{ $params['id'] }}" rel="module">
+
     <div class="container">
 
-        <h2>Blog posts</h2>
+      <div class="row">
+          <h1 class="text-center my-3">Blog posts</h1>
 
-        <module type="posts" />
+          <module type="posts" />
+      </div>
 
     </div>
 </section>
+
+<module type="spacer" id="spacer-layout--{{ $params['id'] ?? '' }}-bottom" />
