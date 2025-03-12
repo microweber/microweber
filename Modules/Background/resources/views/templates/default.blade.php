0bb30a31a8d84377c08a1b9dc55cd130ec224d9c
Nikolay <rachkuw@gmail.com>
2025-03-12 12:03:10 +0200

    Update default.blade.php

--- Modules/Background/resources/views/templates/default.blade.php
+++ Modules/Background/resources/views/templates/default.blade.php
@@ -1,49 +1,18 @@
 <div class="mw-layout-background-block">
-
-
         <div class="mw-layout-background-node"
              @if($background_image)
              style="background-image: url('{{ $background_image }}');"
             @endif
         >
         </div>
-
-
-    @if($background_video)
-        <video src="{{ $background_video }}" autoplay muted loop playsinline class="mw-layout-background-video"></video>
-    @endif
-
-    <div class="mw-layout-background-overlay"
-         @if($background_color)
-             style="background-color: {{ $background_color }};"
+        @if($background_video)
+            <video src="{{ $background_video }}" autoplay muted loop playsinline class="mw-layout-background-video"></video>
         @endif
-    >
-     </div>
-</div>
 
-<style>
-    .mw-layout-background-block {
-        position: relative;
-        width: 100%;
-        height: 100vh;
-        background-size: cover;
-        background-position: center;
-    }
-    .mw-layout-background-video {
-        position: absolute;
-        top: 0;
-        left: 0;
-        width: 100%;
-        height: 100%;
-        object-fit: cover;
-        z-index: -1;
-    }
-    .mw-layout-background-overlay {
-        position: absolute;
-        top: 0;
-        left: 0;
-        width: 100%;
-        height: 100%;
-        z-index: 1;
-    }
-</style>
+        <div class="mw-layout-background-overlay"
+             @if($background_color)
+                 style="background-color: {{ $background_color }};"
+            @endif
+        >
+         </div>
+</div>
