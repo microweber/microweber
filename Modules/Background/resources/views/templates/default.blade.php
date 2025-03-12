d0128bb6087a9f69e289b9d6a4f94fa09941c6b5
Nikolay <rachkuw@gmail.com>
2025-03-12 12:04:31 +0200

    Update default.blade.php

--- Modules/Background/resources/views/templates/default.blade.php
+++ Modules/Background/resources/views/templates/default.blade.php
@@ -1,18 +1,18 @@
 <div class="mw-layout-background-block">
-        <div class="mw-layout-background-node"
-             @if($background_image)
-             style="background-image: url('{{ $background_image }}');"
-            @endif
-        >
-        </div>
-        @if($background_video)
-            <video src="{{ $background_video }}" autoplay muted loop playsinline class="mw-layout-background-video"></video>
+    <div class="mw-layout-background-node"
+         @if($background_image)
+         style="background-image: url('{{ $background_image }}');"
         @endif
+    >
+    </div>
+    @if($background_video)
+        <video src="{{ $background_video }}" autoplay muted loop playsinline class="mw-layout-background-video"></video>
+    @endif
 
-        <div class="mw-layout-background-overlay"
-             @if($background_color)
-                 style="background-color: {{ $background_color }};"
-            @endif
-        >
-         </div>
+    <div class="mw-layout-background-overlay"
+         @if($background_color)
+             style="background-color: {{ $background_color }};"
+        @endif
+    >
+     </div>
 </div>
