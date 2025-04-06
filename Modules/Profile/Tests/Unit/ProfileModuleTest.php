97abad754206953b3f7d9aef703409154de9c1d2
Peter Ivanov <peter@microweber.com>
2025-04-06 07:53:12 +0300

    Improve Profile module test coverage (#1148)
    Co-authored-by: mw-bot <bot@microweber.com>

---
+++ Modules/Profile/Tests/Unit/ProfileModuleTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Modules\Profile\Tests\Unit;
+
+use PHPUnit\Framework\Attributes\Test;
+use PHPUnit\Framework\TestCase;
+use Modules\Profile\Models\User;
+use Modules\Profile\Traits\HasTwoFactorAuthentication;
+
+class ProfileModuleTest extends TestCase
+{
+    #[Test]
+    public function testUserModelUsesTwoFactorTrait()
+    {
+        $user = new User();
+        $this->assertContains(
+            HasTwoFactorAuthentication::class,
+            class_uses_recursive($user)
+        );
+    }
+
+    #[Test]
+    public function testUserModelHasFillableFields()
+    {
+        $expected = [
+            'name',
+            'email',
+            'password',
+            'two_factor_secret',
+            'two_factor_recovery_codes',
+            'two_factor_confirmed_at'
+        ];
+        
+        $this->assertEquals($expected, (new User())->getFillable());
+    }
+
+    #[Test]
+    public function testTwoFactorConfigExists()
+    {
+        $configPath = base_path('Modules/Profile/config/twofactor.php');
+        $this->assertFileExists($configPath);
+    }
+}
