b9205177dc6a4c0ffb29c4fc1859f15f52651155
Peter Ivanov <peter@microweber.com>
2025-04-17 19:29:24 +0300

    update

--- src/MicroweberPackages/Filament/resources/views/filament-forms/components/mw-tree.blade.php
+++ src/MicroweberPackages/Filament/resources/views/filament-forms/components/mw-tree.blade.php
@@ -9,111 +9,60 @@
     :field="$field"
     :has-inline-label="$hasInlineLabel"
 >
-
-
     @php
         $suffix = '';
-
         $suffix = $this->getId();
-
-
-
     @endphp
 
-
-
-
-    <script>
-
-
-        document.addEventListener('alpine:init', () => {
-
-            Alpine.data('mwTreeFormComponent', ({state}) => ({
-                state,
-                async init() {
-                    var skip = [];
-                    var selectedData = [];
-                    var options = {
-                        selectable: true
-                    };
-
-                    @if(isset($singleSelect) and $singleSelect)
-
-                        options.singleSelect = true;
-
+    <div
+        x-data="{
+            state: $wire.{{ $applyStateBindingModifiers("\$entangle('{$statePath}')") }},
+            async init() {
+                var skip = [];
+                var selectedData = [];
+                var options = {
+                    selectable: true
+                    @if(isset($singleSelect) && $singleSelect)
+                    , singleSelect: true
                     @endif
+                };
 
-                    @if(isset($selectedPage) and $selectedPage)
-
+                @if(isset($selectedPage) && $selectedPage)
                     selectedData.push({
                         id: '{{$selectedPage}}',
                         type: 'page'
-                    })
+                    });
+                @endif
 
-                    @endif
-
-                    @if(isset($selectedCategories) and $selectedCategories)
-
+                @if(isset($selectedCategories) && $selectedCategories)
                     @foreach($selectedCategories as $selectedCategory)
-
-                    selectedData.push({
-                        id: {{$selectedCategory}},
-                        type: 'category'
-                    })
-
-
+                        selectedData.push({
+                            id: {{$selectedCategory}},
+                            type: 'category'
+                        });
                     @endforeach
+                @endif
 
-                        @endif
-
-                    if (selectedData.length > 0) {
-                        options.selectedData = selectedData;
-                    }
-                    if(skip.length > 0){
-                        options.skip = skip;
-                    }
-
-
-
-                    var opts = {
-                        options
-                    };
-
-                    let pagesTree = await mw.widget.tree('#mw-tree-edit-content-{{$suffix}}', opts);
-                    pagesTree.tree.on('selectionChange', e => {
-                        let result = pagesTree.tree.getSelected();
-                        this.state = result;
-                    })
+                if (selectedData.length > 0) {
+                    options.selectedData = selectedData;
                 }
-            }))
+                if(skip.length > 0){
+                    options.skip = skip;
+                }
 
-        })
-    </script>
+                var opts = {
+                    options
+                };
 
-
-    <?php
-
-    /*
-    @if($this->data)
-    {{json_encode($this->data['mw_parent_page_and_category_state'],JSON_PRETTY_PRINT)}}
-    @endif
-     */
-    ?>
-
-
-    <div
-
-
-        x-data="mwTreeFormComponent({
-                state: $wire.{{ $applyStateBindingModifiers("\$entangle('{$statePath}')") }}
-            })"
+                let pagesTree = await mw.widget.tree('#mw-tree-edit-content-{{$suffix}}', opts);
+                pagesTree.tree.on('selectionChange', e => {
+                    let result = pagesTree.tree.getSelected();
+                    this.state = result;
+                });
+            }
+        }"
     >
-
-        <div wire:ignore id="mw-tree-edit-content-{{$suffix}}"> </div>
-
+        <div wire:ignore id="mw-tree-edit-content-{{$suffix}}"></div>
     </div>
 
-
 </x-dynamic-component>
-
-
