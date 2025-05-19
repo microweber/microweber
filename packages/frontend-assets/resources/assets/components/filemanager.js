6e68bc2561447ae4a01e899eca9f17042bae3110
Nikolay <rachkuw@gmail.com>
2025-05-19 15:46:49 +0300

    update

--- packages/frontend-assets/resources/assets/components/filemanager.js
+++ packages/frontend-assets/resources/assets/components/filemanager.js
@@ -1089,7 +1089,7 @@
                 <div class=" mw-file-manager-search form-control-live-edit-label-wrapper">
                     <input type="search" class="form-control-live-edit-input" placeholder="${mw.lang('Search')}">
                     <button class="btn-icon mw-file-manager-search-button" aria-label="Button">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><path d="M10 10m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0"></path><path d="M21 21l-6 -6"></path></svg>
+                        <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="16" height="16" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><path d="M10 10m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0"></path><path d="M21 21l-6 -6"></path></svg>
                     </button>
                 </div>
             `);
