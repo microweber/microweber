76063dfa76fe61678af0ee76ef1cabd68b8c04fe
Nikolay <rachkuw@gmail.com>
2025-05-22 10:52:51 +0300

    update

--- packages/frontend-assets/resources/assets/ui/apps/ElementStyleEditor/ElementStyleEditorAiChat.vue
+++ packages/frontend-assets/resources/assets/ui/apps/ElementStyleEditor/ElementStyleEditorAiChat.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <span class="mw-admin-action-links mw-adm-liveedit-tabs ms-3">
+        <span class="mw-admin-action-links mw-adm-liveedit-tabs ms-3 mb-3">
             <Lang>AI Style Editor</Lang>
         </span>
         <div id="ai-gui-editor" ref="wrapper"></div>
@@ -11,7 +11,6 @@
                 <rect width='24' height='24' stroke='none' fill='currentColor' opacity='0'/>
                 <g transform="matrix(0.71 0 0 0.71 12 12)">
                     <path
-                        style="stroke: none; stroke-width: 1; stroke-dasharray: none; stroke-linecap: butt; stroke-dashoffset: 0; stroke-linejoin: miter; stroke-miterlimit: 4; fill: rgb(0,0,0); fill-rule: nonzero; opacity: 1;"
                         transform=" translate(-16, -16)"
                         d="M 16 3 C 14.895 3 14 3.895 14 5 C 14 5.7389464 14.404366 6.3763868 15 6.7226562 L 15 9 L 10 9 C 7.2504839 9 5 11.250484 5 14 L 5 15 L 2 15 L 2 22 L 5 22 L 5 27 L 9 27 L 10 27 L 13 27 L 13 25 L 11 25 L 11 23 L 21 23 L 21 24 L 23 24 L 23 21 L 9 21 L 9 25 L 7 25 L 7 14 C 7 12.331516 8.3315161 11 10 11 L 22 11 C 23.668484 11 25 12.331516 25 14 L 25 23 C 25 24.668484 23.668484 26 22 26 L 18.722656 26 C 18.376387 25.404366 17.738946 25 17 25 C 15.895 25 15 25.895 15 27 C 15 28.105 15.895 29 17 29 C 17.738946 29 18.376387 28.595634 18.722656 28 L 22 28 C 24.749516 28 27 25.749516 27 23 L 27 22 L 30 22 L 30 15 L 27 15 L 27 14 C 27 11.250484 24.749516 9 22 9 L 17 9 L 17 6.7226562 C 17.595634 6.3763868 18 5.7389464 18 5 C 18 3.895 17.105 3 16 3 z M 12 14 C 10.895 14 10 14.895 10 16 C 10 17.105 10.895 18 12 18 C 13.105 18 14 17.105 14 16 C 14 14.895 13.105 14 12 14 z M 20 14 C 18.895 14 18 14.895 18 16 C 18 17.105 18.895 18 20 18 C 21.105 18 22 17.105 22 16 C 22 14.895 21.105 14 20 14 z M 4 17 L 5 17 L 5 20 L 4 20 L 4 17 z M 27 17 L 28 17 L 28 20 L 27 20 L 27 17 z"
                         stroke-linecap="round"/>
