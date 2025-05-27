26aef2f4c885dda45454d8ec58deb1463a4ca793
Nikolay <rachkuw@gmail.com>
2025-05-27 11:34:03 +0300

    update

--- packages/frontend-assets/resources/assets/ui/components/Layouts/ListLayouts.vue
+++ packages/frontend-assets/resources/assets/ui/components/Layouts/ListLayouts.vue
@@ -61,7 +61,7 @@
                     <div v-show="layoutsList?.categories?.length">
                         <div class="modules-list-search-block input-icon">
                             <input v-model="filterKeyword" type="text" v-bind:placeholder="$lang('Type to Search') + '...'" class="modules-list-search-field form-control rounded-0">
-                              <span class="input-icon-addon ms-3">
+                              <span class="input-icon-addon list-layouts-search-bar-icon ms-3">
 
                                     <svg fill="none" xmlns="http://www.w3.org/2000/svg" class="icon" width="20" height="20" viewBox="0 0 24 24" stroke-width="3" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><path d="M10 10m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0"></path><path d="M21 21l-6 -6"></path></svg>
                                 </span>
