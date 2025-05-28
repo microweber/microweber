5c0e66405f8dac7c6ff19fc09c385fdaeac872f4
Peter Ivanov <peter@microweber.com>
2025-05-28 12:21:26 +0300

    update

--- packages/frontend-assets/resources/assets/ui/components/RightSidebar/TemplateSettings/TemplateSettings.vue
+++ packages/frontend-assets/resources/assets/ui/components/RightSidebar/TemplateSettings/TemplateSettings.vue
@@ -55,6 +55,23 @@
                 </button>
             </div>
 
+
+            <div v-if="currentPath !== '/'" class="mb-3 mt-3">
+                <button @click="goBack"
+                        class="d-flex gap-2 btn btn-link mw-live-edit-toolbar-link mw-live-edit-toolbar-link--arrowed text-start">
+                    <svg class="mw-live-edit-toolbar-arrow-icon" xmlns="http://www.w3.org/2000/svg" width="32"
+                         height="32" viewBox="0 0 32 32">
+                        <g fill="none" stroke-width="1.5" stroke-linejoin="round" stroke-miterlimit="10">
+                            <circle class="arrow-icon--circle" cx="16" cy="16" r="15.12"></circle>
+                            <path class="arrow-icon--arrow" d="M16.14 9.93L22.21 16l-6.07 6.07M8.23 16h13.98"></path>
+                        </g>
+                    </svg>
+                    <div class="ms-1 font-weight-bold">Back</div>
+                </button>
+            </div>
+
+
+
             <!-- Main settings list when at root path -->
             <div v-if="currentPath === '/' && hasStyleSettings" class="mt-5">
                 <span
