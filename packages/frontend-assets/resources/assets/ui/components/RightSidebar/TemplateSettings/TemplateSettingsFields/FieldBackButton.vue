bde9baaad03d164924272318b60ebbd6a21bca4a
Nikolay <rachkuw@gmail.com>
2025-05-30 13:27:03 +0300

    update

--- packages/frontend-assets/resources/assets/ui/components/RightSidebar/TemplateSettings/TemplateSettingsFields/FieldBackButton.vue
+++ packages/frontend-assets/resources/assets/ui/components/RightSidebar/TemplateSettings/TemplateSettingsFields/FieldBackButton.vue
@@ -1,5 +1,5 @@
 <template>
-    <div v-if="showButton" class="mb-3 mw-live-edit-settings-back-button">
+    <div v-if="showButton">
         <button @click="handleGoBack"
                 class="d-flex gap-2 btn btn-link mw-live-edit-toolbar-link mw-live-edit-toolbar-link--arrowed text-start">
             <svg class="mw-live-edit-toolbar-arrow-icon" xmlns="http://www.w3.org/2000/svg" width="32"
