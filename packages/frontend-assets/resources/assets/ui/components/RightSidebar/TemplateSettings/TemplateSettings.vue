583271dc7cedb7098eb4c5fabe1fef3a0fba5265
Nikolay <rachkuw@gmail.com>
2025-06-02 15:59:18 +0300

    Update TemplateSettings.vue

--- packages/frontend-assets/resources/assets/ui/components/RightSidebar/TemplateSettings/TemplateSettings.vue
+++ packages/frontend-assets/resources/assets/ui/components/RightSidebar/TemplateSettings/TemplateSettings.vue
@@ -54,10 +54,7 @@
                     <button type="button" data-bs-toggle="tooltip" data-bs-placement="top"
                             title="Reset stylesheet settings" class="reset-template-settings-and-stylesheet-button"
                             @click="resetAllDesignSelectorsValuesSettings">
-                        <svg fill="currentColor" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24"
-                             width="24">
-                            <path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z"/>
-                        </svg>
+                        <svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px" fill="currentColor"><path d="M480-160q-134 0-227-93t-93-227q0-134 93-227t227-93q69 0 132 28.5T720-690v-110h80v280H520v-80h168q-32-56-87.5-88T480-720q-100 0-170 70t-70 170q0 100 70 170t170 70q77 0 139-44t87-116h84q-28 106-114 173t-196 67Z"/></svg>
                     </button>
                 </span>
 
