404fe00c43f95d1febde829e6d7eaa8ec0db7661
Nikolay <rachkuw@gmail.com>
2025-06-11 11:06:52 +0300

    update

--- Modules/Newsletter/resources/views/livewire/filament/admin/sidebar/create-new-campaign-btn.blade.php
+++ Modules/Newsletter/resources/views/livewire/filament/admin/sidebar/create-new-campaign-btn.blade.php
@@ -1,9 +1,9 @@
 <x-filament::button
-    href="{{admin_url('newsletter/create-campaign')}}"
+    class="create-campaign-button" href="{{admin_url('newsletter/create-campaign')}}"
     tag="a"
     >
-    <div class="flex gap-2 items-center w-full create-campaign-button">
-        @svg("mw-add-plus", "h-5 w-5 fill-white dark:fill-black") Create campaign
+    <div class="flex gap-2 items-center w-full">
+        @svg("mw-add-plus", "h-5 w-5") Create campaign
     </div>
 
 </x-filament::button>
