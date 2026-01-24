5153c0009df8c202b397dc71f53e4e3253d2f347
Peter Ivanov <peter@microweber.com>
2026-01-24 18:57:02 +0200

    update

--- Modules/Sitemap/Tests/Unit/SitemapRoutesTest.php
+++ Modules/Sitemap/Tests/Unit/SitemapRoutesTest.php
@@ -10,41 +10,59 @@ class SitemapRoutesTest extends TestCase
     {
         $response = $this->get('sitemap.xml');
         $response->assertStatus(200);
-        $response->assertHeader('Content-Type', 'text/xml; charset=UTF-8');
+
+        $actual = $response->headers->get('Content-Type');
+        $actual = str_replace('charset=UTF-8', 'charset=utf-8', $actual);
+        $this->assertEquals('text/xml; charset=utf-8', $actual);
     }
 
     public function testSitemapCategoriesRoute()
     {
         $response = $this->get('sitemap.xml/categories');
         $response->assertStatus(200);
-        $response->assertHeader('Content-Type', 'text/xml; charset=UTF-8');
+
+        $actual = $response->headers->get('Content-Type');
+        $actual = str_replace('charset=UTF-8', 'charset=utf-8', $actual);
+        $this->assertEquals('text/xml; charset=utf-8', $actual);
     }
 
     public function testSitemapTagsRoute()
     {
         $response = $this->get('sitemap.xml/tags');
         $response->assertStatus(200);
-        $response->assertHeader('Content-Type', 'text/xml; charset=UTF-8');
+
+        $actual = $response->headers->get('Content-Type');
+        $actual = str_replace('charset=UTF-8', 'charset=utf-8', $actual);
+        $this->assertEquals('text/xml; charset=utf-8', $actual);
     }
 
     public function testSitemapProductsRoute()
     {
         $response = $this->get('sitemap.xml/products');
         $response->assertStatus(200);
-        $response->assertHeader('Content-Type', 'text/xml; charset=UTF-8');
+
+        $actual = $response->headers->get('Content-Type');
+        $actual = str_replace('charset=UTF-8', 'charset=utf-8', $actual);
+        $this->assertEquals('text/xml; charset=utf-8', $actual);
     }
 
     public function testSitemapPostsRoute()
     {
         $response = $this->get('sitemap.xml/posts');
         $response->assertStatus(200);
-        $response->assertHeader('Content-Type', 'text/xml; charset=UTF-8');
+
+        $actual = $response->headers->get('Content-Type');
+        $actual = str_replace('charset=UTF-8', 'charset=utf-8', $actual);
+        $this->assertEquals('text/xml; charset=utf-8', $actual);
     }
 
     public function testSitemapPagesRoute()
     {
         $response = $this->get('sitemap.xml/pages');
         $response->assertStatus(200);
-        $response->assertHeader('Content-Type', 'text/xml; charset=UTF-8');
+
+        $actual = $response->headers->get('Content-Type');
+        $actual = str_replace('charset=UTF-8', 'charset=utf-8', $actual);
+        $this->assertEquals('text/xml; charset=utf-8', $actual);
     }
 }
